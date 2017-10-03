314679a9d6899968d75472d0678d9dd77d4d648e
Joe Selman <jsselman@google.com>
2017-10-03 16:11:37 -0700

    Create more GOPATH structure

--- run-acceptance-tests.sh
+++ run-acceptance-tests.sh
@@ -8,12 +8,16 @@ export TF_ACC=1
 export GOOGLE_REGION="us-central1"
 # TODO actually use a separate project for xpn resources
 export GOOGLE_XPN_HOST_PROJECT="man-i-wish-i-was-a-real-project"
-
-export GOPATH=${PWD}/terraform-provider-google
+# Setup GOPATH
+export GOPATH=${PWD}/go
 
 # CI sets the contents of our json account secret in our environment; dump it
 # to disk for use in tests.
 echo "${GOOGLE_ACCOUNT_JSON}" > /tmp/google-account.json
 
-cd terraform-provider-google
+# Create GOPATH structure
+mkdir -p "${GOPATH}/src/github.com/terraform-providers"
+ln -s "${PWD}/terraform-provider-google" "${GOPATH}/src/github.com/terraform-providers/terraform-provider-google"
+
+cd "${GOPATH}/src/github.com/terraform-providers/terraform-provider-google"
 make testacc TEST=./google
