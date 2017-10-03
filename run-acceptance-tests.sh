bd39237560cb5d252a58fbcd4db4ef406ff5966f
Joe Selman <jsselman@google.com>
2017-10-03 16:04:57 -0700

    Change name of repo and set GOPATH

--- run-acceptance-tests.sh
+++ run-acceptance-tests.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -e
 
 export GOOGLE_CREDENTIALS_FILE="/tmp/google-account.json"
 export GCLOUD_PROJECT="terraform-ci-acc-tests"
@@ -7,9 +9,11 @@ export GOOGLE_REGION="us-central1"
 # TODO actually use a separate project for xpn resources
 export GOOGLE_XPN_HOST_PROJECT="man-i-wish-i-was-a-real-project"
 
+export GOPATH=${PWD}/terraform-provider-google
+
 # CI sets the contents of our json account secret in our environment; dump it
 # to disk for use in tests.
 echo "${GOOGLE_ACCOUNT_JSON}" > /tmp/google-account.json
 
-cd terraform
+cd terraform-provider-google
 make testacc TEST=./google
