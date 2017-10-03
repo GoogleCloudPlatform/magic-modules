0980fc98e23ffb590b86199ce4ff1495c7c93ed8
Joe Selman <jsselman@google.com>
2017-10-03 16:00:20 -0700

    Bare-bones integration test

--- run-acceptance-tests.sh
+++ run-acceptance-tests.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-echo "Hello world."
+export GOOGLE_CREDENTIALS_FILE="/tmp/google-account.json"
+export GCLOUD_PROJECT="terraform-ci-acc-tests"
+export TF_ACC=1
+export GOOGLE_REGION="us-central1"
+# TODO actually use a separate project for xpn resources
+export GOOGLE_XPN_HOST_PROJECT="man-i-wish-i-was-a-real-project"
+
+# CI sets the contents of our json account secret in our environment; dump it
+# to disk for use in tests.
+echo "${GOOGLE_ACCOUNT_JSON}" > /tmp/google-account.json
+
+cd terraform
+make testacc TEST=./google
