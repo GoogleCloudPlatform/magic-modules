267bd4bb780ff903a7772b5fe1a211afc1ab81c4
Vincent Roseberry <rosbo@google.com>
2017-10-13 14:19:48 -0700

    Setup unit tests

---
+++ run-unit-tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Setup GOPATH
+export GOPATH=${PWD}/go
+
+set -x
+
+# Create GOPATH structure
+mkdir -p "${GOPATH}/src/github.com/terraform-providers"
+ln -s "${PWD}/terraform-provider-google" "${GOPATH}/src/github.com/terraform-providers/terraform-provider-google"
+
+cd "${GOPATH}/src/github.com/terraform-providers/terraform-provider-google"
+
+go test -v ./google -parallel 16 -run '^Test' -timeout 1m
