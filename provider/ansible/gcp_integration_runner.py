df96a47472ec11cdbe1787b17422bbf3e30b603d
Alex Stephen <alexstephen@google.com>
2017-12-12 21:06:38 +0000

    First commit of Ansible Provider.
    * Create submodule for Ansible Core
    * Create base provider
    * Create template that outputs out a list of properties in Ansible doc format
    Change-Id: Iafadf0bc4dd9a2b42e91b214942eeb8c1a59bb27

--- provider/ansible/gcp_integration_runner.py
+++ provider/ansible/gcp_integration_runner.py
@@ -1,5 +1,6 @@
 # Copyright: (c) 2018, Google Inc.
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+<%= compile 'templates/ansible/autogen_notice.erb' -%>
 """GCP plugin for integration tests."""
 from __future__ import absolute_import, print_function
 
