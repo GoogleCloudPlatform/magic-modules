e1c34d6252e085dce3ace460b892c35f1968d973
Alex Stephen <alexstephen@google.com>
2017-12-12 21:06:38 +0000

    First commit of Ansible Provider.
    * Create submodule for Ansible Core
    * Create base provider
    * Create template that outputs out a list of properties in Ansible doc format
    Change-Id: Iafadf0bc4dd9a2b42e91b214942eeb8c1a59bb27

--- provider/ansible/gcp_utils.py
+++ provider/ansible/gcp_utils.py
@@ -1,25 +1,6 @@
 # Copyright (c), Google Inc, 2017
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without modification,
-# are permitted provided that the following conditions are met:
-#
-#    * Redistributions of source code must retain the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#    * Redistributions in binary form must reproduce the above copyright notice,
-#      this list of conditions and the following disclaimer in the documentation
-#      and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-# USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
+# Simplified BSD License (see licenses/simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
+<%= compile 'templates/ansible/autogen_notice.erb' -%>
 
 try:
     import requests
@@ -55,25 +36,11 @@ def navigate_hash(source, path, default=None):
 class GcpAuthentication(object):
     def __init__(self, module):
         self.module = module
-        self._set_values
         self._validate()
 
     def session(self):
         return AuthorizedSession(
             self._credentials().with_scopes(self.module.params['scopes']))
-
-    def _set_values(self):
-        if not self.module.params['auth_kind']:
-            self.module.params['auth_kind'] = os.getenv('GCP_AUTH_KIND')
-
-        if not self.module.params['service_account_email']:
-            self.module.params['service_account_email'] = os.getenv('GCP_SERVICE_ACCOUNT_EMAIL')
-
-        if not self.module.params['service_account_file']:
-            self.module.params['service_account_file'] = os.getenv('GCP_SERVICE_ACCOUNT_FILE')
-
-        if not self.module.params['scopes']:
-            self.module.params['scopes'] = os.getenv('GCP_SCOPES')
 
     def _validate(self):
         if not HAS_REQUESTS:
