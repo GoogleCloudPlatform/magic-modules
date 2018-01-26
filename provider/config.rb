c724b998ffcac08db03f16e1cec0f7c6297dd9cb
Alex Stephen <alexstephen@google.com>
2018-01-26 22:39:02 +0000

    First version of Provider::Objects
    All provider.yamls have a objects field to add custom fields. This allows us to
    change products on a per-provider basis. Currently, this field is a Hash.
    
    This object replicates the Api of Api::HashArray allowing for backwards
    compatability. It will allow us to create functions that use the information
    inside of the objects field and allow for validation.
    
    Change-Id: I089d0c9eddf4ec6c4aec512516123ae5f6099f72

--- provider/config.rb
+++ provider/config.rb
@@ -13,6 +13,7 @@
 
 require 'api/object'
 require 'provider/compiler'
+require 'provider/objects'
 require 'compile/core'
 
 module Provider
@@ -192,7 +193,15 @@ module Provider
       super
       check_property :examples, Api::Resource::HashArray unless @examples.nil?
       check_property :files, Provider::Config::Files unless @files.nil?
-      check_property :objects, Api::Resource::HashArray unless @objects.nil?
+
+      # TODO(alexstephen): Remove HashArray checking when all provider.yamls
+      # using Provider::Objects
+      if @objects.is_a? Provider::Objects
+        check_property :objects, Provider::Objects unless @objects.nil?
+      else
+        check_property :objects, Api::Resource::HashArray unless @objects.nil?
+      end
+
       check_property :test_data, Provider::Config::TestData \
         unless @test_data.nil?
       check_property :tests, Api::Resource::HashArray unless @tests.nil?
