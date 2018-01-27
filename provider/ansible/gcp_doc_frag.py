9cca979230520dadf44cbf492e78bdecdd2fb7bb
Alex Stephen <alexstephen@google.com>
2018-01-27 00:06:22 +0000

    More Ansible changes per Ryan
    * Removing autogenerated license header from several handwritten files.
    * Changing exception handling
    
    Change-Id: I1a12fba4dccea3e7bb998a87f544124c46db51a7

--- provider/ansible/gcp_doc_frag.py
+++ provider/ansible/gcp_doc_frag.py
@@ -1,6 +1,5 @@
 # Copyright: (c) 2018, Google Inc.
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-<%= compile 'templates/ansible/autogen_notice.erb' -%>
 
 
 class ModuleDocFragment(object):
