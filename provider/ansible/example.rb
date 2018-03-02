1f633b6103b28864b23c534e7a6fa8c9fe3df15b
Alex Stephen <alexstephen@google.com>
2018-03-02 01:43:42 +0000

    Ansible: Adding gcloud verification to integration tests
    Change-Id: I094d3cb07e0474484217b4d3ca8c6653091c290b

--- provider/ansible/example.rb
+++ provider/ansible/example.rb
@@ -33,6 +33,74 @@ module Provider
       service_account_file: '/tmp/auth.pem'
     }.freeze
 
+    # Holds all information necessary to run gcloud and verify the creation
+    # of a resource.
+    #
+    # command         - The gcloud command being run
+    # failed_name     - The name of the resource being created
+    #                   (defaults to {{resource_name}})
+    #                   Used for verifying deletion.
+    # failed_verifier - A custom Jinja2 verifier to test if gcloud
+    #                   command post-deletion worked as intended.
+    class Verifier < Api::Object
+      include Compile::Core
+      attr_reader :command
+      attr_reader :failed_name
+      attr_reader :failed_verifier
+
+      def validate
+        check_property :command, String
+        check_optional_property :failed_name, String
+        check_optional_property :failed_verifier, String
+
+        @failed_name ||= '\'{{ resource_name }}\' does not exist'
+        @failed_verifier ||=
+          "\"\\\"#{@failed_name.strip}\\\" in results.stderr\""
+      end
+
+      # rubocop:disable Metrics/MethodLength
+      def build_task(state, object)
+        raise 'State must be present or absent' \
+          unless %w[present absent].include? state
+
+        obj_name = Google::StringUtils.underscore(object.name)
+        verb = verbs[state.to_sym]
+        status = state == 'present' ? 0 : 1
+        [
+          "- name: verify that #{obj_name} was #{verb}",
+          indent([
+            'shell: |',
+            # Ansible doesn't like multiline shell commands in playbooks.
+            indent(@command.tr("\n", ''), 2),
+            'register: results',
+            ('failed_when: results.rc == 0' if state == 'absent')
+          ].compact, 2),
+
+          '- name: verify that command succeeded',
+          indent([
+                   'assert:',
+                   indent([
+                            'that:',
+                            indent([
+                              "- results.rc == #{status}",
+                              ("- #{@failed_verifier}" if state == 'absent')
+                            ].compact, 2)
+                          ], 2)
+                 ], 2)
+        ].compact
+      end
+      # rubocop:enable Metrics/MethodLength
+
+      private
+
+      def verbs
+        {
+          present: 'created',
+          absent: 'deleted'
+        }
+      end
+    end
+
     # Class responsible for holding a single Ansible task. This task may create
     # a GCP resource or create a dependent GCP resource.
     class Task < Api::Object
@@ -64,26 +132,29 @@ module Provider
 
       private
 
+      # rubocop:disable Metrics/MethodLength
       def build_task(state, hash, object, noop = false)
         verb = verbs[state.to_sym]
 
         again = ''
         again = ' that already exists' if noop && state == 'present'
         again = ' that does not exist' if noop && state == 'absent'
+        scopes = object.__product.scopes.map { |x| "- #{x}" }
         [
           "- name: #{verb} a #{object_name_from_module_name(@name)}#{again}",
           indent([
             "#{@name}:",
             indent([
-              compile_string(hash, @code),
-              "scopes:",
-              indent(lines(object.__product.scopes.map { |x| "- #{x}" }), 2),
-              "state: #{state}",
-            ], 4),
+                     compile_string(hash, @code),
+                     'scopes:',
+                     indent(lines(scopes), 2),
+                     "state: #{state}"
+                   ], 4),
             ("register: #{@register}" unless @register.nil?)
           ].compact, 2)
         ]
       end
+      # rubocop:enable Metrics/MethodLength
 
       def object_name_from_module_name(mod_name)
         product_name = mod_name.match(/gcp_[a-z]*_(.*)/).captures[0]
@@ -95,11 +166,13 @@ module Provider
     # examples.
     class Example < Api::Object
       attr_reader :task
+      attr_reader :verifier
       attr_reader :dependencies
 
       def validate
         super
         check_property :task, Task
+        check_optional_property :verifier, Verifier
         check_optional_property_list :dependencies, Task
       end
     end
