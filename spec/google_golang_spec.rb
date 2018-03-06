1ab2869fc96b754fbd8856b5c0cb647333aac639
Vincent Roseberry <rosbo@google.com>
2018-03-06 11:00:27 -0800

    Add support for Default Values
    Also support DiffSuppressFunc and Validation for nested properties.
    
    Change-Id: I508955d4e67903b9a3c212dee0a1cbdc27b28d11

---
+++ spec/google_golang_spec.rb
@@ -0,0 +1,45 @@
+# Copyright 2017 Google Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'spec_helper'
+require 'google/golang_utils'
+
+class Test
+  include Google::GolangUtils
+end
+
+describe Google::GolangUtils do
+  context '#go_literal' do
+    let(:golang) { Test.new }
+
+    describe 'string' do
+      subject { golang.go_literal('foo') }
+      it { is_expected.to eq '"foo"' }
+    end
+
+    describe 'integer' do
+      subject { golang.go_literal(123) }
+      it { is_expected.to eq '123' }
+    end
+
+    describe 'float' do
+      subject { golang.go_literal(0.987) }
+      it { is_expected.to eq '0.987' }
+    end
+
+    describe 'unknown type' do
+      subject { -> { golang.go_literal(Class.new) } }
+      it { is_expected.to raise_error(/Unsupported/) }
+    end
+  end
+end
