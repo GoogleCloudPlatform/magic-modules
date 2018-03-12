e96a7d54bf34baca1f800eb53790729c20e824dd
Nathan McKinley <nmckinley@google.com>
2018-03-12 13:25:37 -0700

    Be smarter about choosing branch names - pick readable ones. (#18)

--- .ci/magic-modules/branch-magic-modules.sh
+++ .ci/magic-modules/branch-magic-modules.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 pushd "magic-modules"
-BRANCH=$(git rev-parse --short HEAD)
+BRANCH="codegen-pr-$(git config --get pullrequest.id)"
 git checkout -B "$BRANCH"
 # ./branchname is intentionally never committed - it isn't necessary once
 # this output is no longer available.
