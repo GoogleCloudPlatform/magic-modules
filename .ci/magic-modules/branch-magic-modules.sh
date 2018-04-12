a56d005c32f12cb69f077988e95f39be89d3ba7c
Nathan McKinley <nmckinley@google.com>
2018-04-11 17:48:50 -0700

    Ensure that downstream PRs get updates correctly. (#50)
    * Ensure that downstream PRs get updates correctly.
    
    Before this PR, downstream PRs were not updated correctly after a
    magic-modules PR was updated.  The current flow:
    
    PR flows into mm-generate, is generated, and is pushed to The
    Magician's fork under a branch name like "codegen-pr-123".
    That push updates the downstream PR, because there is a PR open
    from the Magician's fork (branch name "codegen-pr-123")
    already.
    
    The new flow:
    
    PR flows into mm-generate, is generated.  If a branch named
    "codegen-pr-123" already exists, instead the new generated
    code is pushed into a branch named "codegen-sha-abc123".
    That branch flows to terraform-test, and is tested.  That
    tested code flows to create-prs, where, if the branch name
    is not equal to the original PR's branch name, it will be
    force-pushed over the original PR's branch rather than having
    a new PR created.
    
    More complicated, but also correct.  :)
    
    * Force update magic-modules.
    
    The git resource has to push tags, and pushing tags will fail
    if the tag already exists in the remote (which will be true if
    you are unlucky, your first commit doesn't update the submodule,
    the submodule already points to a tag, and your update to the
    magic-modules PR *also* doesn't update the submodule).
    
    * Better comment for Magic-Modules PR.

--- .ci/magic-modules/branch-magic-modules.sh
+++ .ci/magic-modules/branch-magic-modules.sh
@@ -3,7 +3,15 @@ set -e
 set -x
 
 pushd "magic-modules"
-BRANCH="codegen-pr-$(git config --get pullrequest.id)"
+# "codegen-pr" vs "codegen-sha" is *LOAD-BEARING*.  Don't change
+# them (or introduce other options) unless you also change the
+# logic in create-or-update-pr - because we decide whether to
+# create or to update by which one of these we're prefixed by.
+if git config --get pullrequest.id && [ -z "$USE_SHA" ]; then
+  BRANCH="codegen-pr-$(git config --get pullrequest.id)"
+else
+  BRANCH="codegen-sha-$(git rev-parse --short HEAD)"
+fi
 git checkout -B "$BRANCH"
 # ./branchname is intentionally never committed - it isn't necessary once
 # this output is no longer available.
