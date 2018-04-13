160909e7a0380a884dbf2ba31d419a13b93d3803
Nathan McKinley <nmckinley@google.com>
2018-04-13 17:20:13 +0000

    Clarity and comments.

--- .ci/magic-modules/get_downstream_prs.py
+++ .ci/magic-modules/get_downstream_prs.py
@@ -5,9 +5,18 @@ import re
 import sys
 from github import Github
 
+def append_github_dependencies_to_list(lst, comment_body):
+  list_of_urls = re.findall(r'^depends: (https://github.com/.*)', comment_body, re.MULTILINE)
+  return lst + list_of_urls
+
 if __name__ == '__main__':
   g = Github(os.environ.get('GH_TOKEN'))
-  all_prs = functools.reduce(lambda x,y: x+re.findall(r'\ndepends: (https://github.com/.*)', y.body),
-      g.get_repo('GoogleCloudPlatform/magic-modules').get_pull(int(sys.argv[1])).get_issue_comments(), [])
-  for downstream_pr in all_prs:
+  assert len(sys.argv) == 2
+  pr_number = int(sys.argv[1])
+  pull_request = g.get_repo('GoogleCloudPlatform/magic-modules').get_pull(pr_number)
+  comment_bodies = [c.body for c in pull_request.get_issue_comments()]
+  # "reduce" is "foldl" - apply this function to the result of the previous function and
+  # the next value in the iterable.
+  all_dependencies = functools.reduce(append_github_dependencies_to_list, comment_bodies, [])
+  for downstream_pr in all_dependencies:
     print downstream_pr
