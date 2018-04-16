179abaadc9c7e645573681cf31041fc763a7a958
Nelson Araujo <nelsona@google.com>
2018-04-16 15:21:05 -0700

    Add missing require and property checker (removed on revert RubyObject).

--- provider/config.rb
+++ provider/config.rb
@@ -13,6 +13,8 @@
 
 require 'api/object'
 require 'compile/core'
+require 'provider/resource_override'
+require 'provider/resource_overrides'
 
 module Provider
   # Settings for the provider
@@ -205,6 +207,7 @@ module Provider
       check_property :examples, Api::Resource::HashArray unless @examples.nil?
       check_property :files, Provider::Config::Files unless @files.nil?
       check_property :objects, Api::Resource::HashArray unless @objects.nil?
+      check_optional_property :overrides, Provider::ResourceOverrides
       check_property :test_data, Provider::Config::TestData \
         unless @test_data.nil?
       check_property :tests, Api::Resource::HashArray unless @tests.nil?
