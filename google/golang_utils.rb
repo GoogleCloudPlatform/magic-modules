7e5115c7530d989a79751e836adf96d868d67703
Vincent Roseberry <rosbo@google.com>
2018-04-19 14:26:44 -0700

    Support more stop sentence character
    Change-Id: I459cc2caec5f897d02ea1bef01252b670f71d22b

--- google/golang_utils.rb
+++ google/golang_utils.rb
@@ -25,12 +25,10 @@ module Google
     # quotes in the string like "\"foo\"" which is not a pattern we want to
     # see in our yaml config files.
     def go_literal(value)
-      if value.is_a?(String)
+      if value.is_a?(String) || value.is_a?(Symbol)
         "\"#{value}\""
       elsif value.is_a?(Numeric)
         value.to_s
-      elsif value.is_a?(Symbol)
-        "\"#{value}\""
       else
         raise "Unsupported go literal #{value}"
       end
