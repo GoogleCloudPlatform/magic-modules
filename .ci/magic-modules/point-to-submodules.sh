09b1b1a77cd7d07b46430a15caada251098f8627
Nathan McKinley <nmckinley@google.com>
2018-04-20 15:38:14 -0700

    Generate puppet (compute and sql) with The Magician. (#63)

--- .ci/magic-modules/point-to-submodules.sh
+++ .ci/magic-modules/point-to-submodules.sh
@@ -9,18 +9,25 @@ set -x
 pushd magic-modules-branched
 BRANCH="$(cat ./branchname)"
 # Update this repo to track the submodules we just pushed:
+git config -f .gitmodules submodule.build/puppet/compute.branch "$BRANCH"
+git config -f .gitmodules submodule.build/puppet/compute.url "git@github.com:$GH_USERNAME/puppet-google-compute.git"
+git config -f .gitmodules submodule.build/puppet/sql.branch "$BRANCH"
+git config -f .gitmodules submodule.build/puppet/sql.url "git@github.com:$GH_USERNAME/puppet-google-sql.git"
 git config -f .gitmodules submodule.build/terraform.branch "$BRANCH"
 git config -f .gitmodules submodule.build/terraform.url "git@github.com:$GH_USERNAME/terraform-provider-google.git"
-git submodule sync build/terraform
+git submodule sync build/terraform build/puppet/sql build/puppet/compute
 
-# Download those submodules so we can add them here.
+set +x
+# Don't show the credential in the output.
 echo "$CREDS" > ~/github_private_key
+set -x
 chmod 400 ~/github_private_key
 
-ssh-agent bash -c "ssh-add ~/github_private_key; git submodule update --remote --init build/terraform"
+# Download those submodules so we can add them here.
+ssh-agent bash -c "ssh-add ~/github_private_key; git submodule update --remote --init build/terraform build/puppet/sql build/puppet/compute"
 
 # Commit those changes so that they can be tested in the next phase.
-git add build/terraform
+git add build/terraform build/puppet/compute build/puppet/sql
 git add .gitmodules
 git config --global user.email "magic-modules@google.com"
 git config --global user.name "Modular Magician"
