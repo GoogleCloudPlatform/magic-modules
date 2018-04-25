2d84cda6403085960509e7198e92329504fead1a
Alex Stephen <alexstephen@google.com>
2018-04-25 22:08:16 +0000

    Chef: Kubectl creation
    Change-Id: Iede4d8fab78dfef58d48a724ea0fe95c09aaeea6

--- products/container/examples/chef/kube_config.rb
+++ products/container/examples/chef/kube_config.rb
@@ -16,6 +16,39 @@
 <%= compile 'templates/license.erb' -%>
 
 <%= lines(autogen_notice :chef) -%>
+
+<%= compile 'templates/chef/example~auth.rb.erb' -%>
+
+raise "Missing parameter 'cluster_id'. Please read docs at #{__FILE__}" \
+  unless ENV.key?('cluster_id')
+
+<% res_name = 'mycluster-#{ENV[\'cluster_id\']}' -%>
+gcontainer_cluster <%= quote_string(res_name) -%> do
+  action :create
+  initial_node_count 2
+  master_auth(
+    username: 'cluster_admin',
+    password: 'my-secret-password'
+  )
+  node_config(
+    machine_type: 'n1-standard-4', # we want 4-cores for our cluster
+    disk_size_gb: 500              # ... and a lot of disk space
+  )
+  zone 'us-central1-a'
+  project 'google.com:graphite-playground'
+  credential 'mycred'
+end
+
+directory '/home/alexstephen/.kube' do
+  action :create
+end
 <% end # name == README.md -%>
 
-# TODO(rambleraptor): Add kube_config example here.
+gcontainer_kubeconfig '/home/alexstephen/.kube/config' do
+  action :create
+  context "gke-mycluster-#{ENV['cluster_id']}"
+  cluster "mycluster-#{ENV['cluster_id']}"
+  zone 'us-central1-a'
+  project 'google.com:graphite-playground'
+  credential 'mycred'
+end
