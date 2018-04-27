66d558a065c171339c202059234d7238bdafada5
Nathan McKinley <nmckinley@google.com>
2018-04-26 18:49:24 -0700

    Merge more submodules. (#122)

--- .ci/magic-modules/merge-pr.sh
+++ .ci/magic-modules/merge-pr.sh
@@ -24,12 +24,16 @@ git config pullrequest.id "$ID"
 git checkout "$BRANCH"
 git config --global user.email "magic-modules@google.com"
 git config --global user.name "Modular Magician"
+git config -f .gitmodules submodule.build/puppet/sql.branch master
+git config -f .gitmodules submodule.build/puppet/sql.url "git@github.com:GoogleCloudPlatform/puppet-google-sql.git"
+git config -f .gitmodules submodule.build/puppet/compute.branch master
+git config -f .gitmodules submodule.build/puppet/compute.url "git@github.com:GoogleCloudPlatform/puppet-google-compute.git"
 git config -f .gitmodules submodule.build/terraform.branch master
 git config -f .gitmodules submodule.build/terraform.url "git@github.com:terraform-providers/terraform-provider-google.git"
-ssh-agent bash -c "ssh-add ~/github_private_key; git submodule update --remote --init build/terraform"
+ssh-agent bash -c "ssh-add ~/github_private_key; git submodule update --remote --init build/terraform build/puppet/compute build/puppet/sql"
 
-git add build/terraform
+git add build/terraform build/puppet/compute build/puppet/sql
 git add .gitmodules
 
-git commit -m "Update terraform -> HEAD on $(date)"
+git commit -m "Update tracked submodules -> HEAD on $(date)"
 echo "Merged PR #$ID." > ./commit_message
