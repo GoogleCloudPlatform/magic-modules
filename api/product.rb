1d41e9a9228b4f0493fcfd2b5c33d0b82ea4cc6e
Dana Hoffman <danahoffman@google.com>
2018-05-07 13:57:41 -0700

    replace product base_url with versions object (#158)
    * replace product base_url with versions object
    
    Change-Id: I2eceb85c0b8406f347a839d37cada6cdc4ef6f71
    
    * compilation fixes and allow no default on list with one element
    
    * rubocop fixes
    
    * remove no longer necessary default trues

--- api/product.rb
+++ api/product.rb
@@ -18,22 +18,66 @@ require 'compile/core'
 module Api
   # Repesents a product to be managed
   class Product < Api::Object::Named
-    attr_reader :base_url
     attr_reader :objects
     attr_reader :prefix
     attr_reader :scopes
+    attr_reader :versions
 
     include Compile::Core
 
     def validate
       super
       set_variables @objects, :__product
-      check_property :base_url, String
       check_property :objects, Array
       check_property_list :objects, Api::Resource
       check_property :prefix, String
       check_property :scopes, ::Array
       check_property_list :scopes, String
+
+      check_versions
+    end
+
+    # Represents a version of the API for this product
+    class Version < Api::Object
+      attr_reader :base_url
+      attr_reader :default
+      attr_reader :name
+
+      def validate
+        super
+        @default ||= false
+
+        check_property :base_url, String
+        check_property :name, String
+        check_property :default, :boolean
+      end
+    end
+
+    def default_version
+      @versions.each do |v|
+        return v if v.default
+      end
+
+      return @versions.last if @versions.length == 1
+    end
+
+    private
+
+    def check_versions
+      check_property :versions, Array
+      check_property_list :versions, Api::Product::Version
+
+      # Confirm that at most one version is the default
+      defaults = 0
+      @versions.each do |v|
+        defaults += 1 if v.default
+      end
+
+      raise "Product '#{@name}' must specify at most one default API version" \
+        if defaults > 1
+
+      raise "Product '#{@name}' must specify a default API version" \
+        if defaults.zero? && @versions.length > 1
     end
   end
 end
