132cce70eacbdc13f12acee449133fb10f5680ec
Nathan McKinley <nmckinley@google.com>
2018-05-23 22:42:26 +0000

    Run rspec from inside spec dir.

--- .ci/unit-tests/puppet-chef.sh
+++ .ci/unit-tests/puppet-chef.sh
@@ -3,6 +3,9 @@ set -e
 set -x
 
 pushd "magic-modules/build/$PROVIDER/$PRODUCT"
+if [ -n "$SPEC_DIR" ]; then
+  cd "$SPEC_DIR"
+fi
 bundle install
 bundle exec rspec --exclude_pattern "$EXCLUDE_PATTERN"
 popd
