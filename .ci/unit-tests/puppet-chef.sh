67e40d35e76b66a6b5ea3ac61067552365bdaeeb
Vincent Roseberry <rosbo@google.com>
2018-06-08 11:07:15 -0700

    Split parallel rspec command

--- .ci/unit-tests/puppet-chef.sh
+++ .ci/unit-tests/puppet-chef.sh
@@ -11,12 +11,12 @@ bundle install
 # parallel_rspec doesn't support --exclude_pattern
 if [ -z "$EXCLUDE_PATTERN" ]; then
     echo "No EXCLUDE_PATTERN"
-    filtered=$(find spec -name '*_spec.rb')
+    DISABLE_COVERAGE=true bundle exec parallel_rspec spec/
 else
     echo "Excluding $EXCLUDE_PATTERN"
     IFS="," read -ra excluded <<< "$EXCLUDE_PATTERN"
     filtered=$(find spec -name '*_spec.rb' $(printf "! -wholename %s " ${excluded[@]}))
+    DISABLE_COVERAGE=true bundle exec parallel_rspec ${filtered[@]}
 fi
 
-DISABLE_COVERAGE=true bundle exec parallel_rspec ${filtered[@]}
 popd
