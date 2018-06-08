f9ebd83a6ca2cd61dc72aaeeb809e28c4d13f358
Vincent Roseberry <rosbo@google.com>
2018-06-08 13:28:10 -0700

    Fix chef tests

--- .ci/unit-tests/puppet-chef.sh
+++ .ci/unit-tests/puppet-chef.sh
@@ -5,13 +5,10 @@ set -x
 pushd "magic-modules/build/$PROVIDER/$PRODUCT"
 bundle install
 
-# parallel_rspec doesn't support --exclude_pattern
 if [ -z "$EXCLUDE_PATTERN" ]; then
-    echo "No EXCLUDE_PATTERN"
-    ls -la
     DISABLE_COVERAGE=true bundle exec parallel_rspec spec/
 else
-    echo "Excluding $EXCLUDE_PATTERN"
+    # parallel_rspec doesn't support --exclude_pattern
     IFS="," read -ra excluded <<< "$EXCLUDE_PATTERN"
     filtered=$(find spec -name '*_spec.rb' $(printf "! -wholename %s " ${excluded[@]}))
     DISABLE_COVERAGE=true bundle exec parallel_rspec ${filtered[@]}
