79c528252cee0aa397faa1f7398ac93ca1275365
Alex Stephen <alexstephen@google.com>
2018-06-13 04:39:53 +0000

    use compile/core for erb processing

--- tools/discovery/builder.rb
+++ tools/discovery/builder.rb
@@ -1,12 +1,14 @@
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', '..')
 require 'net/http'
 require 'json'
-require 'pp'
 require 'erb'
+require 'compile/core'
+
+include Compile::Core
 
 DISCOVERY_URL = 'https://www.googleapis.com/discovery/v1/apis/compute/v1/rest'
 uri = URI(DISCOVERY_URL)
 response = Net::HTTP.get(uri)
 results = JSON.parse(response)
 
-renderer = ERB.new(File.read('api.yaml.erb'), nil, '-%>')
-File.write('output.yaml', renderer.result())
+File.write('output.yaml', lines(compile_file({ results: results }, 'api.yaml.erb')))
