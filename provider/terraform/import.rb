d2640ac11a27fa5b7188fa52bee3202f74d64ca6
Nathan McKinley <nmckinley@google.com>
2018-06-14 19:04:53 -0700

    Travis.

--- provider/terraform/import.rb
+++ provider/terraform/import.rb
@@ -28,6 +28,7 @@ module Provider
       # c) short id w/o defaults: {{name}}
       #
       # Fields with default values are `project`, `region` and `zone`.
+      # rubocop:disable Metrics/AbcSize
       def import_id_formats(resource)
         if resource.import_format.nil? || resource.import_format.empty?
           underscored_base_url = resource.base_url.gsub(
@@ -40,7 +41,7 @@ module Provider
           id_formats = resource.import_format
         end
 
-        unless resource.id_format.nil? && resource.id_format.empty?
+        unless resource.id_format.nil? || resource.id_format == '{{name}}'
           id_formats << resource.id_format
         end
 
@@ -54,6 +55,7 @@ module Provider
 
         (id_formats + [short_id_format, short_id_default_format]).uniq
       end
+      # rubocop:enable Metrics/AbcSize
     end
   end
 end
