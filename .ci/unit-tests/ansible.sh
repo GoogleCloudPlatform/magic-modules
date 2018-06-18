e0152d7f1ae6c94e6d1f4910b641e7f3f88c251e
Alex Stephen <alexstephen@google.com>
2018-06-18 22:02:12 +0000

    Changing Ansible script because wrong path

--- .ci/unit-tests/ansible.sh
+++ .ci/unit-tests/ansible.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-cd build/ansible
+pushd "magic-modules/build/ansible"
 source hacking/env-setup
 ansible-test sanity --tox --python 2.7 $(find test/integration/targets -name "gcp_*" -printf '%P ')
