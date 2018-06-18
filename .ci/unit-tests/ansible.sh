222b29c16f543aea50707f2d2ecf047b42661305
Alex Stephen <alexstephen@google.com>
2018-06-18 22:27:58 +0000

    Must install tox first

--- .ci/unit-tests/ansible.sh
+++ .ci/unit-tests/ansible.sh
@@ -4,5 +4,7 @@ set -e
 set -x
 
 pushd "magic-modules/build/ansible"
+pip install tox
+
 source hacking/env-setup
-ansible-test sanity --tox --python 2.7 $(find test/integration/targets -name "gcp_*" -printf '%P ')
+ansible-test sanity -v --tox --python 2.7 $(find test/integration/targets -name "gcp_*" -printf '%P ')
