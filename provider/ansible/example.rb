a2d13639f7b6cb69e32a7b7330b69b8f8e6590d9
Alex Stephen <alexstephen@google.com>
2018-06-20 22:57:46 +0000

    Adding verifier task to integration tests, so that gcloud can be subbed out with facts modules

--- provider/ansible/example.rb
+++ provider/ansible/example.rb
@@ -100,6 +100,69 @@ module Provider
       end
     end
 
+    # Holds all information necessary to run a facts module and verify the
+    # creation / deletion of a resource.
+    #
+    # command         - The gcloud command being run
+    # failed_name     - The name of the resource being created
+    #                   (defaults to {{resource_name}})
+    #                   Used for verifying deletion.
+    # failed_verifier - A custom Jinja2 verifier to test if gcloud
+    #                   command post-deletion worked as intended.
+    class FactsVerifier < Api::Object
+      include Compile::Core
+
+      def validate
+        @failure ||= FailureCondition.new
+
+        check_property :parameters, Hash
+      end
+
+      # rubocop:disable Metrics/AbcSize
+      # rubocop:disable Metrics/MethodLength
+      def build_task(state, object)
+        obj_name = Google::StringUtils.underscore(object.name)
+        verb = verbs[state.to_sym]
+        number = state == 'present' ? 1 : 0
+        module_name = ["gcp_#{object.__product.prefix[1..-1]}",
+         Google::StringUtils.underscore(object.name), 'facts'].join('_')
+        [
+          "- name: verify that #{obj_name} was #{verb}",
+          indent([
+            "#{module_name}:",
+            indent([
+              'filters:',
+              '   - name = "{{ resource_name }}"',
+              @parameters.map { |k, v| "#{k}: #{v}" },
+              'project: "{{ gcp_project }}"',
+              'auth_kind: "{{ gcp_cred_kind }}"',
+              'service_account_file: "{{ gcp_cred_file }}"',
+              'scopes:',
+              "  - #{object.__product.scopes[0]}",
+            ], 4),
+            "register: results"
+          ].flatten.compact, 2),
+
+          '- name: verify that command succeeded',
+          '  assert:',
+          '    that:',
+          indent("- results['items'] | length == #{number}", 6)
+        ].compact
+      end
+      # rubocop:enable Metrics/MethodLength
+      # rubocop:enable Metrics/AbcSize
+
+      private
+
+      def verbs
+        {
+          present: 'created',
+          absent: 'deleted'
+        }
+      end
+    end
+
+
     # A gcloud command failing is not enough to verify that a resource does not
     # exist
     # Stderr should be checked to verify that the resource actually does not
@@ -240,7 +303,7 @@ module Provider
       def validate
         super
         check_property :task, Task
-        check_optional_property :verifier, Verifier
+        check_optional_property :verifier, [Verifier, FactsVerifier]
         check_optional_property_list :dependencies, Task
       end
     end
