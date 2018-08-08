97c0da8d7ed930643e6ac9175bf6881ffa257f82
Nathan McKinley <nmckinley@google.com>
2018-08-07 18:22:22 -0700

    Generate VPN Tunnel in Magic Modules, and enable TargetVPNGateway. (#291)
    Merged PR #291.

---
+++ products/compute/examples/chef/vpn_tunnel.rb
@@ -0,0 +1,51 @@
+<% if false # the license inside this if block assertains to this file -%>
+# Copyright 2017 Google Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+<% end -%>
+<% unless name == 'README.md' -%>
+
+<%= compile 'templates/license.erb' -%>
+
+<%= lines(autogen_notice :chef) -%>
+
+<%= compile 'templates/chef/example~auth.rb.erb' -%>
+<% end -%>
+gcompute_network <%= example_resource_name('mynetwork') -%> do
+  action :create
+  auto_create_subnetworks false
+  project ENV['PROJECT'] # ex: 'my-test-project'
+  credential 'mycred'
+end
+
+gcompute_region <%= example_resource_name('some-region') -%> do
+  action :create
+  r_label 'us-west1'
+  project ENV['PROJECT'] # ex: 'my-test-project'
+  credential 'mycred'
+end
+
+gcompute_target_vpn_gateway <%= example_resource_name('mygateway') -%> do
+  action :create
+  project ENV['PROJECT'] # ex: 'my-test-project'
+  credential 'mycred'
+  network <%= example_resource_name('mynetwork') %>
+  region <%= example_resource_name('some-region') %>
+end
+
+gcompute_vpn_tunnel <%= example_resource_name('mytunnel') -%> do
+  action :create
+  project ENV['PROJECT'] # ex: 'my-test-project'
+  credential 'mycred'
+  target_vpn_gateway <%= example_resource_name('mygateway') %>
+  region <%= example_resource_name('some-region') %>
+end
