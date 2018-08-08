303d0f625d849db2be5c6cc588da33f3de8c3f5b
Nathan McKinley <nmckinley@google.com>
2018-08-07 17:17:40 -0700

    Travis.

--- provider/terraform.rb
+++ provider/terraform.rb
@@ -52,7 +52,7 @@ module Provider
         Api::Type::NestedObject => 'schema.TypeList',
         Api::Type::Array => 'schema.TypeList',
         Api::Type::NameValues => 'schema.TypeMap',
-        Api::Type::Fingerprint => 'schema.TypeString',
+        Api::Type::Fingerprint => 'schema.TypeString'
       }
     end
 
