f60ad22bc6d6f4ba54bf3294fb33a0218ffd6a98
Nathan McKinley <nmckinley@google.com>
2018-09-06 18:21:47 -0700

    Comment in the test.

--- .ci/unit-tests/puppet-chef.sh
+++ .ci/unit-tests/puppet-chef.sh
@@ -10,6 +10,13 @@ if [ $PROVIDER = "chef" ]; then
     # Re-enable chef tests by deleting this if block once the tests are fixed.
     echo "Skipping tests... See issue #236"
 elif [ -z "$EXCLUDE_PATTERN" ]; then
+  # The unit tests are all under e.g. spec/gcompute* - the integration tests
+  # are better run using `bundle exec rake spec` instead of
+  # `bundle exec parallel_rspec`, but if you just let it run the default
+  # set of specs, parallel_rspec will try to run the integration tests
+  # in addition.  This is just running all the tests that aren't excluded -
+  # or, in the event of an empty exclude list, all the tests with the right
+  # names.
   if ls spec/g$PRODUCT* > /dev/null 2&>1; then
     DISABLE_COVERAGE=true bundle exec parallel_rspec spec/g$PRODUCT*
   fi
