619bd690a554c70acce4a718632f93e311e3e6c3
Alex Stephen <alexstephen@google.com>
2018-09-21 16:46:00 -0700

    Ansible: 2.8 changes (#486)
    Merged PR #486.

--- .ci/unit-tests/ansible.sh
+++ .ci/unit-tests/ansible.sh
@@ -3,8 +3,11 @@
 set -e
 set -x
 
+apt-get update
 pushd "magic-modules/build/ansible"
+apt-get install -y man
 pip install tox
+
 
 source hacking/env-setup
 ansible-test sanity -v --tox --python 2.7 --base-branch origin/devel lib/ansible/modules/cloud/google/
