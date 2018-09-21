720782cd0b22674973b957e31768ca62bfa2d266
Alex Stephen <alexstephen@google.com>
2018-09-21 16:16:32 -0700

    installing new man pages

--- .ci/unit-tests/ansible.sh
+++ .ci/unit-tests/ansible.sh
@@ -3,8 +3,11 @@
 set -e
 set -x
 
+apt-get update
+apt-get install man
 pushd "magic-modules/build/ansible"
 pip install tox
+
 
 source hacking/env-setup
 ansible-test sanity -v --tox --python 2.7 --base-branch origin/devel lib/ansible/modules/cloud/google/
