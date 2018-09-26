6ad3718411d1ea43ab9a56d04fbc7417db69737d
Riley Karson <rileykarson@google.com>
2018-09-26 09:05:27 -0700

    goimports test files.

--- provider/terraform.rb
+++ provider/terraform.rb
@@ -165,6 +165,8 @@ module Provider
         default_template: 'templates/terraform/examples/base_configs/test_file.go.erb',
         out_file: filepath
       )
+      # TODO: error check goimports
+      %x(goimports -w #{filepath})
     end
     # rubocop:enable Metrics/AbcSize
   end
