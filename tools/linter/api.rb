6b755027b6fe32f8ca3316dc606514904e1015e1
Alex Stephen <alexstephen@google.com>
2018-09-28 14:23:47 -0700

    docs everywhere!

--- tools/linter/api.rb
+++ tools/linter/api.rb
@@ -1,3 +1,16 @@
+# Copyright 2018 Google Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 require 'api/async'
 require 'api/compiler'
 require 'api/product'
@@ -5,8 +18,8 @@ require 'api/resource'
 require 'api/type'
 require 'google/yaml_validator'
 
+# Responsbible for grabbing api.yaml and getting resources from it
 class ProductApi
-
   attr_reader :api
 
   def initialize(product_name)
@@ -24,6 +37,6 @@ class ProductApi
   private
 
   def get_api(product_name)
-    Api::Compiler.new("products/#{product_name}/api.yaml").run 
+    Api::Compiler.new("products/#{product_name}/api.yaml").run
   end
 end
