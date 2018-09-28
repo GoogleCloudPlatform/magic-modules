ad897cb27c4a0df2d6234a6c23d3e0fbbd70a167
Chris Stephens <chrisst@google.com>
2018-09-28 12:45:07 -0700

    Code review feedback + adding mutex

--- templates/terraform/tests/resource_monitoring_alert_policy_test.go
+++ templates/terraform/tests/resource_monitoring_alert_policy_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
 )
 
 // Stackdriver tests cannot be run in parallel otherwise they will error out with:
@@ -19,7 +20,7 @@ func TestAccMonitoringAlertPolicy_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckComputeHealthCheckDestroy,
+		CheckDestroy: testAccCheckAlertPolicyDestroy,
 		Steps: []resource.TestStep{
 			resource.TestStep{
 				Config: testAccMonitoringAlertPolicy_basic(alertName, conditionName),
@@ -42,7 +43,7 @@ func TestAccMonitoringAlertPolicy_full(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckComputeHealthCheckDestroy,
+		CheckDestroy: testAccCheckAlertPolicyDestroy,
 		Steps: []resource.TestStep{
 			resource.TestStep{
 				Config: testAccMonitoringAlertPolicy_full(alertName, conditionName1, conditionName2),
@@ -56,27 +57,56 @@ func TestAccMonitoringAlertPolicy_full(t *testing.T) {
 	})
 }
 
+func testAccCheckAlertPolicyDestroy(s *terraform.State) error {
+	config := testAccProvider.Meta().(*Config)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "google_monitoring_alert_policy" {
+			continue
+		}
+
+		project, err := getTestProject(rs.Primary, config)
+		if err != nil {
+			return err
+		}
+
+		name := rs.Primary.Attributes["name"]
+
+		url := fmt.Sprintf("hthttps://monitoring.googleapis.com/v3/projects/%s/alertPolicies/%s", project, name)
+		_, err = sendRequest(config, "GET", url, nil)
+
+		if err == nil {
+			return fmt.Errorf("Error, alert policy %s still exists", name)
+		}
+	}
+
+	return nil
+}
+
 func testAccMonitoringAlertPolicy_basic(alertName, conditionName string) string {
 	return fmt.Sprintf(`
 resource "google_monitoring_alert_policy" "basic" {
   display_name = "%s"
-  enabled = true
-  combiner = "OR"
+  enabled      = true
+  combiner     = "OR"
+
   conditions = [
     {
       display_name = "%s"
+
       condition_threshold = {
         aggregations = [
           {
-            alignment_period = "60s"
+            alignment_period   = "60s"
             per_series_aligner = "ALIGN_RATE"
-          }
+          },
         ]
-        duration = "60s"
+
+        duration   = "60s"
         comparison = "COMPARISON_GT"
-        filter = "metric.type=\"compute.googleapis.com/instance/disk/write_bytes_count\" AND resource.type=\"gce_instance\""
+        filter     = "metric.type=\"compute.googleapis.com/instance/disk/write_bytes_count\" AND resource.type=\"gce_instance\""
       }
-    }
+    },
   ]
 }
 `, alertName, conditionName)
@@ -86,56 +116,65 @@ func testAccMonitoringAlertPolicy_full(alertName, conditionName1, conditionName2
 	return fmt.Sprintf(`
 resource "google_monitoring_alert_policy" "full" {
   display_name = "%s"
-  combiner = "OR"
-  enabled = true
+  combiner     = "OR"
+  enabled      = true
+
   conditions = [
     {
       display_name = "%s"
+
       condition_threshold = {
-				threshold_value = 50
+        threshold_value = 50
+
         aggregations = [
           {
-            alignment_period = "60s"
-						per_series_aligner = "ALIGN_RATE"
-						cross_series_reducer = "REDUCE_MEAN"
+            alignment_period     = "60s"
+            per_series_aligner   = "ALIGN_RATE"
+            cross_series_reducer = "REDUCE_MEAN"
+
             group_by_fields = [
               "metric.label.device_name",
               "project",
               "resource.label.instance_id",
-              "resource.label.zone"
+              "resource.label.zone",
             ]
-          }
+          },
         ]
-        duration = "60s"
-				comparison = "COMPARISON_GT"
 
-				trigger = {
+        duration   = "60s"
+        comparison = "COMPARISON_GT"
+
+        trigger = {
           percent = 10
-				}
-				
+        }
+
         filter = "metric.type=\"compute.googleapis.com/instance/disk/write_bytes_count\" AND resource.type=\"gce_instance\""
       }
-		},
-		{
+    },
+    {
       condition_absent {
         duration = "3600s"
-        filter = "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.type=\"gce_instance\""
+        filter   = "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.type=\"gce_instance\""
+
         aggregations {
-          alignment_period = "60s"
+          alignment_period     = "60s"
           cross_series_reducer = "REDUCE_MEAN"
-          per_series_aligner = "ALIGN_MEAN"
+          per_series_aligner   = "ALIGN_MEAN"
+
           group_by_fields = [
             "project",
             "resource.label.instance_id",
-            "resource.label.zone"
+            "resource.label.zone",
           ]
         }
+
         trigger {
           count = 1
         }
       }
+
       display_name = "%s"
-    }
+    },
   ]
 }
 `, alertName, conditionName1, conditionName2)
