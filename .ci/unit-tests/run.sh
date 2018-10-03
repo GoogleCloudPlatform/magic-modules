0fde8ad65e63450ad393a63dbc59cb258becebe7
Dana Hoffman <danahoffman@google.com>
2018-10-02 20:17:16 -0700

    try to run beta provider tests as part of terraform test job

--- .ci/unit-tests/run.sh
+++ .ci/unit-tests/run.sh
@@ -7,10 +7,18 @@ export GOPATH=${PWD}/go
 
 set -x
 
+PROVIDER_NAME="terraform-provider-google"
+SUBMODULE_DIR="terraform"
+
+if [ -n "$VERSION" ]; then
+  PROVIDER_NAME="terraform-provider-google-$VERSION"
+  SUBMODULE_DIR="terraform-$VERSION"
+fi
+
 # Create GOPATH structure
 mkdir -p "${GOPATH}/src/github.com/terraform-providers"
-ln -s "${PWD}/$1" "${GOPATH}/src/github.com/terraform-providers/terraform-provider-google"
+ln -s "${PWD}/magic-modules/build/$SUBMODULE_DIR" "${GOPATH}/src/github.com/terraform-providers/$PROVIDER_NAME"
 
-cd "${GOPATH}/src/github.com/terraform-providers/terraform-provider-google"
+cd "${GOPATH}/src/github.com/terraform-providers/$PROVIDER_NAME"
 
 go test -v ./google -parallel 16 -run '^Test' -timeout 1m
