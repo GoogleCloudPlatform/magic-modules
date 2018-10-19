f9a508a3c94712150608a1964b846e422d73e10f
Alex Stephen <alexstephen@google.com>
2018-10-18 22:18:38 -0700

    starting work on a discovery doc builder

---
+++ tools/linter/builder.rb
@@ -0,0 +1,44 @@
+# Copyright 2018 Google Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Load everything from MM root.
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '../../')
+Dir.chdir(File.join(File.dirname(__FILE__), '../../'))
+
+require 'tools/linter/discovery'
+require 'tools/linter/api'
+
+require 'api/product'
+require 'api/resource'
+require 'api/type'
+
+module Api
+  class Object
+    # Create a setter if the setter doesn't exist
+    # Yes, this isn't pretty and I apologize
+    def method_missing(method_name, *args)
+      matches = /([a-z_]*)=/.match(method_name)
+      super unless matches
+      create_setter(matches[1])
+      method(method_name.to_sym).call(*args)
+    end
+
+    def create_setter(variable)
+      self.class.define_method("#{variable}=") { |val| instance_variable_set("@#{variable}", val) }
+    end
+  end
+end
+
+product = Api::Product.new
+product.objects = ['test']
+puts YAML::dump(product)
