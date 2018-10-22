b2ad7b8b989dfa0db8e074d53836a311f154a189
Sam Levenick <slevenick@google.com>
2018-10-22 14:40:19 -0700

    Add real tests

--- templates/inspec/tests/zone_test.rb
+++ templates/inspec/tests/zone_test.rb
@@ -1,9 +1,37 @@
 require 'google_compute_zone'
 
-RSpec.describe Zone, "zone" do
-	it "first test" do
-		t = Zone.new
-		expect(t.thing).to eq 1
+class ZoneTest < Zone
+	def initialize(data)
+		@fetched = data
+	end
+end
 
+zone_fixture = {"kind"=>"compute#zone",
+ "id"=>"2231",
+ "creationTimestamp"=>"1989-11-28T00:00:00-05:00",
+ "name"=>"us-east1-b",
+ "description"=>"us-east1-b",
+ "status"=>"UP",
+ "region"=>
+  "https://www.googleapis.com/compute/v1/projects/sam-inspec/regions/us-east1",
+ "selfLink"=>
+  "https://www.googleapis.com/compute/v1/projects/sam-inspec/zones/us-east1-b",
+ "availableCpuPlatforms"=>
+  ["Intel Skylake", "Intel Broadwell", "Intel Haswell"]}
+
+RSpec.describe Zone, "zone resource" do
+	it "parse test" do
+		t = ZoneTest.new(zone_fixture)
+		t.parse
+		expect(t.exists?).to be true
+		expect(t.name).to eq 'us-east1-b'
+		expect(t.status).to eq 'UP'
+		expect(t.deprecated.obsolete).to eq nil
+		time = Time.at(628232400).to_datetime
+		expect(t.creation_timestamp).to eq time
+	end
+	it "no response" do
+		t = ZoneTest.new(nil)
+		expect(t.exists?).to be false
 	end
 end
---
+++ templates/inspec/tests/zones_test.rb
@@ -0,0 +1,62 @@
+require 'google_compute_zones'
+
+class ZonesTest < Zones
+	def fetch_resource(data)
+		return data
+	end
+end
+
+zones_fixture = {"kind"=>"compute#zoneList",
+ "id"=>"projects/sam-inspec/zones",
+ "items"=>
+  [{"kind"=>"compute#zone",
+    "id"=>"2231",
+    "creationTimestamp"=>"1969-12-31T16:00:00.000-08:00",
+    "name"=>"us-east1-b",
+    "description"=>"us-east1-b",
+    "status"=>"UP",
+    "region"=>
+     "https://www.googleapis.com/compute/v1/projects/sam-inspec/regions/us-east1",
+    "selfLink"=>
+     "https://www.googleapis.com/compute/v1/projects/sam-inspec/zones/us-east1-b",
+    "availableCpuPlatforms"=>
+     ["Intel Skylake", "Intel Broadwell", "Intel Haswell"]},
+   {"kind"=>"compute#zone",
+    "id"=>"2233",
+    "creationTimestamp"=>"1969-12-31T16:00:00.000-08:00",
+    "name"=>"us-east1-c",
+    "description"=>"us-east1-c",
+    "status"=>"UP",
+    "region"=>
+     "https://www.googleapis.com/compute/v1/projects/sam-inspec/regions/us-east1",
+    "selfLink"=>
+     "https://www.googleapis.com/compute/v1/projects/sam-inspec/zones/us-east1-c",
+    "availableCpuPlatforms"=>
+     ["Intel Skylake", "Intel Broadwell", "Intel Haswell"]},
+   {"kind"=>"compute#zone",
+    "id"=>"2234",
+    "creationTimestamp"=>"1969-12-31T16:00:00.000-08:00",
+    "name"=>"us-east1-d",
+    "description"=>"us-east1-d",
+    "status"=>"UP",
+    "region"=>
+     "https://www.googleapis.com/compute/v1/projects/sam-inspec/regions/us-east1",
+    "selfLink"=>
+     "https://www.googleapis.com/compute/v1/projects/sam-inspec/zones/us-east1-d",
+    "availableCpuPlatforms"=>
+     ["Intel Skylake", "Intel Broadwell", "Intel Haswell"]}]}
+
+RSpec.describe Zones, "zones" do
+	it "first test" do
+		t = ZonesTest.new(zones_fixture)
+		expect(t.names.size).to eq 3
+		expect(t.names).to include 'us-east1-d'
+		expect(t.names).to include 'us-east1-b'
+		expect(t.names).to include 'us-east1-c'
+		expect(t.statuses).to include 'UP'
+		expect(t.statuses.size).to eq 3
+		expect(t.ids).to include '2231'
+		expect(t.ids).to include '2234'
+		expect(t.ids).to include '2233'
+	end
+end
