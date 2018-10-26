b8e82368ed21f82e13258b9dcc139e1789fb4772
Alex Stephen <alexstephen@google.com>
2018-10-26 10:32:14 -0700

    renaming overrides

--- provider/override_runner.rb
+++ provider/override_runner.rb
@@ -45,49 +45,49 @@ module Provider
 
     private
 
-    def build_product(old_prod, overrides)
+    def build_product(old_prod, all_overrides)
       prod = Api::Product.new
       old_prod.instance_variables
               .reject { |o| o == :@objects }.each do |var_name|
-        if (overrides['product'] || {})[var_name]
-          prod.instance_variable_set(var_name, overrides['product'][var_name])
+        if (all_overrides['product'] || {})[var_name]
+          prod.instance_variable_set(var_name, all_overrides['product'][var_name])
         else
           prod.instance_variable_set(var_name, old_prod.instance_variable_get(var_name))
         end
       end
       prod.instance_variable_set('@objects',
-                                 old_prod.objects.map { |o| build_resource(o, overrides[o.name]) })
+                                 old_prod.objects.map { |o| build_resource(o, all_overrides[o.name]) })
       prod
     end
 
-    def build_resource(old_resource, override)
-      override = {} if override.nil?
+    def build_resource(old_resource, res_override)
+      res_override = {} if res_override.nil?
       res = Api::Resource.new
       old_resource.instance_variables.reject { |o| o == :properties || o == :parameters }
                   .each do |var_name|
-        if override[var_name]
-          res.instance_variable_set(var_name, override[var_name])
+        if res_override[var_name]
+          res.instance_variable_set(var_name, res_override[var_name])
         else
           res.instance_variable_set(var_name, old_resource.instance_variable_get(var_name))
         end
       end
-      res.instance_variable_set('@properties', old_resource.properties.map { |p| build_property(p, override.dig('properties', p.name)) })
-      res.instance_variable_set('@parameters', old_resource.parameters.map { |p| build_property(p, override.dig('properties', p.name)) })
+      res.instance_variable_set('@properties', old_resource.properties.map { |p| build_property(p, res_override.dig('properties', p.name)) })
+      res.instance_variable_set('@parameters', old_resource.parameters.map { |p| build_property(p, res_override.dig('properties', p.name)) })
       res
     end
 
-    def build_property(old_property, override)
-      override = {} if override.nil?
-      prop = if override['type']
-               Module.const_get(override['type']).new
+    def build_property(old_property, prop_override)
+      prop_override = {} if prop_override.nil?
+      prop = if prop_override['type']
+               Module.const_get(prop_override['type']).new
              else
                old_property.class.new
              end
 
       old_property.instance_variables.reject { |o| o == :properties }
                                      .each do |var_name|
-        if override[var_name]
-          prop.instance_variable_set(var_name, override[var_name])
+        if prop_override[var_name]
+          prop.instance_variable_set(var_name, prop_override[var_name])
         else
           prop.instance_variable_set(var_name, old_property.instance_variable_get(var_name))
         end
