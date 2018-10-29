3b5720339fe44d58d24c513cc3b5477c9ebfa516
Chris Stephens <chrisst@google.com>
2018-10-29 16:23:12 -0700

    adding require statment for uri (#592)
    Merged PR #592.

--- provider/terraform/custom_code.rb
+++ provider/terraform/custom_code.rb
@@ -11,6 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require 'uri'
 require 'api/object'
 require 'compile/core'
 require 'google/golang_utils'
