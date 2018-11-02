1292aa8965a08183438fe0fe6227443ba88d8708
Riley Karson <rileykarson@users.noreply.github.com>
2018-11-02 11:21:14 -0700

    Only compile Terraform's versioned files once per run on MM. (#660)
    Merged PR #660.

--- provider/core.rb
+++ provider/core.rb
@@ -58,6 +58,9 @@ module Provider
       # Compilation has to be the last step, as some files (e.g.
       # CONTRIBUTING.md) may depend on the list of all files previously copied
       # or compiled.
+      # common-compile.yaml is a special file that will get compiled by the last product
+      # used in a single invocation of the compiled. It should not contain product-specific
+      # information; instead, it should be run-specific such as the version to compile at.
       compile_files(output_folder, version_name) \
         unless @config.files.nil? || @config.files.compile.nil?
 
@@ -77,9 +80,19 @@ module Provider
     def copy_common_files(output_folder, _version_name = nil)
       provider_name = self.class.name.split('::').last.downcase
       return unless File.exist?("provider/#{provider_name}/common~copy.yaml")
-      files = YAML.safe_load(compile("provider/#{provider_name}/common~copy.yaml"))
 
+      Google::LOGGER.info "Copying common files for #{provider_name}"
+      files = YAML.safe_load(compile("provider/#{provider_name}/common~copy.yaml"))
       copy_file_list(output_folder, files)
+    end
+
+    def compile_common_files(output_folder, version_name = nil)
+      provider_name = self.class.name.split('::').last.downcase
+      return unless File.exist?("provider/#{provider_name}/common~compile.yaml")
+
+      Google::LOGGER.info "Compiling common files for #{provider_name}"
+      files = YAML.safe_load(compile("provider/#{provider_name}/common~compile.yaml"))
+      compile_file_list(output_folder, files, version: version_name)
     end
 
     def copy_file_list(output_folder, files)
