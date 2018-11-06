1f31b746f268a6346f1ff7ceb377b86b0abff095
Alex Stephen <alexstephen@google.com>
2018-11-06 11:30:44 -0800

    rubocop fixes

--- google/python_utils.rb
+++ google/python_utils.rb
@@ -24,7 +24,7 @@ module Google
     # quotes becomes a ruby string without quotes unless you explicitly set
     # quotes in the string like "\"foo\"" which is not a pattern we want to
     # see in our yaml config files.
-    def python_literal(value, spaces_to_use=0)
+    def python_literal(value, spaces_to_use = 0)
       if value.is_a?(String) || value.is_a?(Symbol)
         "'#{value}'"
       elsif value.is_a?(Numeric)
@@ -57,12 +57,12 @@ module Google
 
     def array_format(values, spaces_to_use)
       format([
-        ["[#{values.join(', ')}]"],
-        # Place everything on separate lines
-        ["[#{values.first},",
-         values[1..-2].map { |x| "#{indent(x, spaces_to_use)}," },
-         "#{indent(values.last, spaces_to_use)}]"]
-      ], 0, spaces_to_use)
+               ["[#{values.join(', ')}]"],
+               # Place everything on separate lines
+               ["[#{values.first},",
+                values[1..-2].map { |x| "#{indent(x, spaces_to_use)}," },
+                "#{indent(values.last, spaces_to_use)}]"]
+             ], 0, spaces_to_use)
     end
   end
 end
