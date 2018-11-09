4807c72cb3e1fadb77728ca2f30da3647fd5715c
Alex Stephen <alexstephen@google.com>
2018-11-08 21:35:34 -0800

    skipping tests based on nil-checking

--- tools/linter/tests.rb
+++ tools/linter/tests.rb
@@ -5,9 +5,9 @@ RSpec.shared_examples "property_tests" do |disc_prop, api_prop|
     expect(api_prop).to be_truthy
   end
 
-  it 'should have the same type' do
+  it 'should have the same type', skip: api_prop.nil? do
     disc_prop_type = disc_prop.type
     disc_prop_type = 'Api::Type::ResourceRef' if api_prop.class.name == 'Api::Type::ResourceRef'
-    expect(api_prop.class.to_s).to eq(disc_prop.type)
+    expect(api_prop.class.to_s).to eq(disc_prop_type)
   end
 end
