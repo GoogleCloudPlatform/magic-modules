31fb93eccf2b5baaf952c220c1e14e1d3c7d692d
Chris Stephens <chrisst@google.com>
2018-11-19 18:28:40 -0800

    working http check tests

--- third_party/terraform/tests/resource_monitoring_uptime_check_config_test.go
+++ third_party/terraform/tests/resource_monitoring_uptime_check_config_test.go
@@ -10,6 +10,7 @@ import (
 
 func TestAccMonitoringUptimeCheckConfig_update(t *testing.T) {
 	t.Parallel()
+	project := getTestProjectFromEnv()
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -17,74 +18,46 @@ func TestAccMonitoringUptimeCheckConfig_update(t *testing.T) {
 		CheckDestroy: testAccCheckMonitoringUptimeCheckConfigDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccMonitoringUptimeCheckConfig_update(acctest.RandString(10)),
+				Config: testAccMonitoringUptimeCheckConfig_update("mypath", "password1", project),
 			},
 			{
-				ResourceName:      "google_monitoring_uptime_check_config.http",
-				ImportState:       true,
-				ImportStateVerify: true,
+				ResourceName:            "google_monitoring_uptime_check_config.http",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"http_check.0.auth_info.0.password"},
+			},
+			{
+				Config: testAccMonitoringUptimeCheckConfig_update("", "password2", project),
+			},
+			{
+				ResourceName:            "google_monitoring_uptime_check_config.http",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"http_check.0.auth_info.0.password"},
 			},
 		},
 	})
 }
 
-func partialHttpCheck(port, user, pass string) string {
-	auth := ""
-	if user != "" || pass != "" {
-		auth = fmt.Sprintf(`
-    auth_ino = {
-      username = "%s"
-      password = "%s"
-		}
-		`, user, pass)
-	}
-
+func testAccMonitoringUptimeCheckConfig_update(path, project, pwd string) string {
 	return fmt.Sprintf(`
 resource "google_monitoring_uptime_check_config" "http" {
   display_name = "http-uptime-check-%s"
   timeout = "60s"
 
   http_check = {
-    path = "/some-path"
-		port = "%s"
-		%s
-  }
-
-  monitored_resource {
-    type = "uptime_url"
-    labels = {
-      project_id = "chrisst-TODO"
-      host = "192.168.1.1"
-    }
-  }
-
-  content_matchers = {
-    content = "example"
-  }
-}
-`, acctest.RandString(4), port, auth,
-	)
-}
-
-func testAccMonitoringUptimeCheckConfig_update(val string) string {
-	return fmt.Sprintf(`
-resource "google_monitoring_uptime_check_config" "http" {
-  display_name = "http-uptime-check-%s"
-  timeout = "60s"
-
-  http_check = {
-    path = "/some-path"
+    path = "/%s"
     port = "8010"
-    auth_ino = {
+    auth_info = {
       username = "name"
-      password = "basic_auth"
+      password = "%s"
     }
   }
 
   monitored_resource {
     type = "uptime_url"
     labels = {
-      project_id = "chrisst-TODO"
+      project_id = "%s"
       host = "192.168.1.1"
     }
   }
@@ -93,7 +66,7 @@ resource "google_monitoring_uptime_check_config" "http" {
     content = "example"
   }
 }
-`, val,
+`, acctest.RandString(4), path, project, pwd,
 	)
 }
 
