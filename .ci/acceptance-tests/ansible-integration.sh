aa713f23031ddcbc18cccfc61c1168c3fb5e1a04
Alex Stephen <alexstephen@google.com>
2018-11-30 11:11:54 -0800

    Ansible integration test pipeline

---
+++ .ci/acceptance-tests/ansible-integration.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# CI sets the contents of our json account secret in our environment; dump it
+# to disk for use in tests.
+echo "${SERVICE_ACCOUNT_KEY}" > /tmp/google-account.json
+
+pushd magic-modules-new-prs/build/ansible
+
+# Setup Cloud configuration template with variables
+cp test/integration/cloud-config-gcp.yml.template test/integration/cloud-config-gcp.yml
+sed -i 's/@PROJECT/graphite-test-ansible/g' test/integration/cloud-config-gcp.yml
+sed -i 's/@CRED_KIND/serviceaccount/g' test/integration/cloud-config-gcp.yml
+sed -i 's/@CRED_FILE/\/tmp\/google-account.json/g' test/integration/cloud-config-gcp.yml
+
+# Setup ansible
+source hacking/env-setup
+
+# Run ansible
+ansible-test integration -v --allow-unsupported --continue-on-error $(find test/integration/targets -name "gcp*" -type d -printf "%P ")
+
+popd
+exit 100
