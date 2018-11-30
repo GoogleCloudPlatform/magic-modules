4a845cde5a78d00b15d4dbdaaa57e22b3d5a3170
Alex Stephen <alexstephen@google.com>
2018-11-30 14:30:51 -0800

    CSV formatter

--- tools/linter/tests.rb
+++ tools/linter/tests.rb
@@ -14,14 +14,15 @@
 require 'rspec'
 
 RSpec.shared_examples 'property_tests' do |_disc_prop, api_prop|
-  it 'should exist' do
+  it 'should exist', property: true do
     expect(api_prop).to be_truthy
   end
 end
 
 RSpec.shared_examples 'resource_tests' do |disc_res, api_res|
   # This test will be skipped if the Discovery Doc doesn't have a kind listed.
-  it 'should have kind', skip: !disc_res.schema.dig('properties', 'kind', 'default') do
+  it 'should have kind', skip: !disc_res.schema.dig('properties', 'kind', 'default'),
+    resource: true do
     expect(disc_res.schema.dig('properties', 'kind', 'default')).to eq(api_res.kind)
   end
 end
