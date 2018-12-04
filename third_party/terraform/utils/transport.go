6dbc0ab29015a20a02efe28b6811f0879ee54530
Chris Stephens <chrisst@google.com>
2018-12-03 17:17:26 -0800

    Add retries for all synchronous MM generated resources

--- third_party/terraform/utils/transport.go
+++ third_party/terraform/utils/transport.go
@@ -3,11 +3,13 @@ package google
 import (
 	"bytes"
 	"encoding/json"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"reflect"
 	"regexp"
 	"strings"
+	"time"
 
 	"google.golang.org/api/googleapi"
 )
@@ -30,6 +32,30 @@ func isEmptyValue(v reflect.Value) bool {
 	return false
 }
 
+var retriableStatuses = [...]int{
+	http.StatusTooManyRequests,
+	http.StatusBadGateway,
+	http.StatusServiceUnavailable,
+	http.StatusGatewayTimeout,
+}
+
+// The google api client doesn't throw an error for non 200 responses, instead
+// the response is returned with .StatusCode set and the error in the body. This
+// inspects the response for common retriable errors such as 503 (stack driver throttling)
+func isRetriableResponse(res *http.Response) bool {
+	// Return early for any 2**/3** responses
+	if res.StatusCode < 400 {
+		return false
+	}
+
+	for _, code := range retriableStatuses {
+		if res.StatusCode == code {
+			return true
+		}
+	}
+	return false
+}
+
 func sendRequest(config *Config, method, rawurl string, body map[string]interface{}) (map[string]interface{}, error) {
 	reqHeaders := make(http.Header)
 	reqHeaders.Set("User-Agent", config.userAgent)
@@ -48,14 +74,26 @@ func sendRequest(config *Config, method, rawurl string, body map[string]interfac
 		return nil, err
 	}
 
-	req, err := http.NewRequest(method, u, &buf)
-	if err != nil {
-		return nil, err
-	}
-	req.Header = reqHeaders
-	res, err := config.client.Do(req)
-	if err != nil {
-		return nil, err
+	var res *http.Response
+	// Retry each request up to 3 times for retriable failures
+	for i := 0; i < 3; i++ {
+		if i > 0 {
+			// Sleep on all requests other then the first
+			time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
+		}
+		req, err := http.NewRequest(method, u, &buf)
+		if err != nil {
+			return nil, err
+		}
+		req.Header = reqHeaders
+		res, err = config.client.Do(req)
+		if err != nil {
+			return nil, err
+		}
+
+		if !isRetriableResponse(res) {
+			break
+		}
 	}
 	defer googleapi.CloseBody(res)
 	if err := googleapi.CheckResponse(res); err != nil {
