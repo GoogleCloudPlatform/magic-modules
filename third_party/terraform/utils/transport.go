a7d8eeffb8c9afc6c0308a91cbb6234cc726c4f9
Chris Stephens <chrisst@google.com>
2018-12-04 16:04:01 -0800

    Change rety logic to use existing utils

--- third_party/terraform/utils/transport.go
+++ third_party/terraform/utils/transport.go
@@ -3,7 +3,7 @@ package google
 import (
 	"bytes"
 	"encoding/json"
-	"math/rand"
+	"fmt"
 	"net/http"
 	"net/url"
 	"reflect"
@@ -32,80 +32,61 @@ func isEmptyValue(v reflect.Value) bool {
 	return false
 }
 
-var retriableStatuses = [...]int{
-	http.StatusTooManyRequests,
-	http.StatusBadGateway,
-	http.StatusServiceUnavailable,
-	http.StatusGatewayTimeout,
-}
-
-// The google api client doesn't throw an error for non 200 responses, instead
-// the response is returned with .StatusCode set and the error in the body. This
-// inspects the response for common retriable errors such as 503 (stack driver throttling)
-func isRetriableResponse(res *http.Response) bool {
-	// Return early for any 2**/3** responses
-	if res.StatusCode < 400 {
-		return false
-	}
-
-	for _, code := range retriableStatuses {
-		if res.StatusCode == code {
-			return true
-		}
-	}
-	return false
-}
-
 func sendRequest(config *Config, method, rawurl string, body map[string]interface{}) (map[string]interface{}, error) {
 	reqHeaders := make(http.Header)
 	reqHeaders.Set("User-Agent", config.userAgent)
 	reqHeaders.Set("Content-Type", "application/json")
 
-	var buf bytes.Buffer
-	if body != nil {
-		err := json.NewEncoder(&buf).Encode(body)
-		if err != nil {
-			return nil, err
-		}
-	}
+	var res *http.Response
+	err := retryTimeDuration(
+		func() error {
+			var buf bytes.Buffer
+			if body != nil {
+				err := json.NewEncoder(&buf).Encode(body)
+				if err != nil {
+					return err
+				}
+			}
 
-	u, err := addQueryParams(rawurl, map[string]string{"alt": "json"})
+			u, err := addQueryParams(rawurl, map[string]string{"alt": "json"})
+			if err != nil {
+				return err
+			}
+			req, err := http.NewRequest(method, u, &buf)
+			if err != nil {
+				return err
+			}
+
+			req.Header = reqHeaders
+			res, err = config.client.Do(req)
+			if err != nil {
+				return err
+			}
+
+			if err := googleapi.CheckResponse(res); err != nil {
+				googleapi.CloseBody(res)
+				return err
+			}
+
+			return nil
+		},
+		time.Duration(5)*time.Minute,
+	)
 	if err != nil {
 		return nil, err
 	}
 
-	var res *http.Response
-	// Retry each request up to 3 times for retriable failures
-	for i := 0; i < 3; i++ {
-		if i > 0 {
-			// Sleep on all requests other then the first
-			time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
-		}
-		req, err := http.NewRequest(method, u, &buf)
-		if err != nil {
-			return nil, err
-		}
-		req.Header = reqHeaders
-		res, err = config.client.Do(req)
-		if err != nil {
-			return nil, err
-		}
-
-		if !isRetriableResponse(res) {
-			break
-		}
+	if res == nil {
+		return nil, fmt.Errorf("Unable to server response. This is most likely a terraform problem.")
 	}
+	// The defer call must be made outside of the retryFunc otherwise it's closed too soon.
 	defer googleapi.CloseBody(res)
-	if err := googleapi.CheckResponse(res); err != nil {
-		return nil, err
-	}
 
 	// 204 responses will have no body, so we're going to error with "EOF" if we
 	// try to parse it. Instead, we can just return nil.
 	if res.StatusCode == 204 {
 		return nil, nil
 	}
-
 	result := make(map[string]interface{})
 	if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
 		return nil, err
