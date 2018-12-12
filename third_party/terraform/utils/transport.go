55c5ba6b4f1f232dfc1761d505af01ee9f6ddf57
Chris Stephens <chrisst@google.com>
2018-12-12 09:54:02 -0800

    route timeouts into transport.go from upstream

--- third_party/terraform/utils/transport.go
+++ third_party/terraform/utils/transport.go
@@ -2,9 +2,7 @@ package google
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/url"
 	"reflect"
@@ -75,37 +73,12 @@ func sendRequestWithTimeout(config *Config, method, rawurl string, body map[stri
 
 			return nil
 		},
-		// TODO chrisst - use the timeouts specified at the resource level so that a user can override this.
-		time.Duration(5)*time.Minute,
+		timeout,
 	)
 	if err != nil {
 		return nil, err
 	}
 
-	req, err := http.NewRequest(method, u, &buf)
-	if err != nil {
-		return nil, err
-	}
-	req.Header = reqHeaders
-
-	if timeout != 0 {
-		// context.TODO is encouraged for cancellable contexts that aren't passed in by the
-		// surrounding function. See https://golang.org/pkg/context/#TODO
-		ctx, cancel := context.WithTimeout(context.TODO(), timeout)
-		// Cancel must always be called to avoid leaking go routines
-		defer cancel()
-
-		req = req.WithContext(ctx)
-	}
-
-	res, err := config.client.Do(req)
-	if err != nil {
-		// Explicitly catch timeout errors in order to throw a friendlier error message.
-		if uerr, ok := err.(*url.Error); ok && uerr.Err == context.DeadlineExceeded {
-			return nil, fmt.Errorf("%v Timeout Exceeded : %v", timeout, err)
-		}
-		return nil, err
-	}
 	// The defer call must be made outside of the retryFunc otherwise it's closed too soon.
 	defer googleapi.CloseBody(res)
 
