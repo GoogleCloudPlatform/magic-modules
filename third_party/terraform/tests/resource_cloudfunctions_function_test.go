42e8d915d4c38f924f1a061e4f1fba5442ca7278
Dana Hoffman <danahoffman@google.com>
2018-12-12 14:35:10 -0800

    add souce repo support to cloud functions

--- third_party/terraform/tests/resource_cloudfunctions_function_test.go
+++ third_party/terraform/tests/resource_cloudfunctions_function_test.go
@@ -234,6 +234,29 @@ func TestAccCloudFunctionsFunction_firestore(t *testing.T) {
 	})
 }
 
+func TestAccCloudFunctionsFunction_sourceRepo(t *testing.T) {
+	t.Parallel()
+
+	funcResourceName := "google_cloudfunctions_function.function"
+	functionName := fmt.Sprintf("tf-test-%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCloudFunctionsFunctionDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudFunctionsFunction_sourceRepo(functionName),
+			},
+			{
+				ResourceName:      funcResourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func testAccCheckCloudFunctionsFunctionDestroy(s *terraform.State) error {
 	config := testAccProvider.Meta().(*Config)
 
@@ -563,4 +586,19 @@ resource "google_cloudfunctions_function" "function" {
     resource   = "messages/{messageId}"
   }
 }`, bucketName, zipFilePath, functionName)
+}
+
+func testAccCloudFunctionsFunction_sourceRepo(functionName string) string {
+	return fmt.Sprintf(`
+resource "google_cloudfunctions_function" "function" {
+  name = "%s"
+
+  source_repository {
+    url = "https://source.developers.google.com/projects/hc-terraform-testing/repos/cloudfunctions-test-do-not-delete/moveable-aliases/master"
+  }
+
+  trigger_http = true
+  entry_point  = "helloGET"
+}
+`, functionName)
 }
