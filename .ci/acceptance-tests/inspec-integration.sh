1b6252dbbf45050dd880600afd599646f6fc59b6
Sam Levenick <slevenick@google.com>
2018-12-12 12:54:59 -0800

    Cleanup inspec-integration shell script. Use correct directory for building off of master

--- .ci/acceptance-tests/inspec-integration.sh
+++ .ci/acceptance-tests/inspec-integration.sh
@@ -18,21 +18,13 @@ export GCP_PROJECT_ID=${PROJECT_NAME}
 export GCP_PROJECT_NAME=${PROJECT_NAME}
 set -x
 
-pushd magic-modules-new-prs
-
-# Compile inspec because we are running off of new-prs
-bundle install
-for i in $(find products/ -name 'inspec.yaml' -printf '%h\n');
-do
-  bundle exec compiler -p $i -e inspec -o "build/inspec/"
-done
-pushd build/inspec
+pushd magic-modules-gcp/build/inspec
 
 # Setup for using current GCP resources
 export GCP_ZONE=europe-west2-a
 export GCP_LOCATION=europe-west2
 
-bundle
+bundle install
 
 function cleanup {
 	bundle exec rake test:cleanup_integration_tests
@@ -40,3 +32,4 @@ function cleanup {
 
 trap cleanup EXIT
 bundle exec rake test:integration
+popd
