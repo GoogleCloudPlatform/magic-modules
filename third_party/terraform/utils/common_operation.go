7a0a937b2c67da76dffd6e80e2db3ce52264a319
Dana Hoffman <danahoffman@google.com>
2018-12-21 10:17:37 -0800

    add comments and remove CommonOperation struct

--- third_party/terraform/utils/common_operation.go
+++ third_party/terraform/utils/common_operation.go
@@ -6,16 +6,34 @@ import (
 	"time"
 
 	"github.com/hashicorp/terraform/helper/resource"
-	"google.golang.org/api/googleapi"
+	cloudresourcemanager "google.golang.org/api/cloudresourcemanager/v1"
 )
 
 type Waiter interface {
+	// State returns the current status of the operation.
 	State() string
+
+	// Error returns an error embedded in the operation we're waiting on, or nil
+	// if the operation has no current error.
 	Error() error
+
+	// SetOp sets the operation we're waiting on in a Waiter struct so that it
+	// can be used in other methods.
 	SetOp(interface{}) error
+
+	// QueryOp sends a request to the server to get the current status of the
+	// operation.
 	QueryOp() (interface{}, error)
+
+	// OpName is the name of the operation and is used to log its status.
 	OpName() string
+
+	// PendingStates contains the values of State() that cause us to continue
+	// refreshing the operation.
 	PendingStates() []string
+
+	// TargetStates contain the values of State() that cause us to finish
+	// refreshing the operation.
 	TargetStates() []string
 }
 
@@ -65,13 +83,21 @@ func OperationDone(w Waiter) bool {
 
 func CommonRefreshFunc(w Waiter) resource.StateRefreshFunc {
 	return func() (interface{}, string, error) {
+		// First, read the operation from the server.
 		op, err := w.QueryOp()
-		if err != nil && !isGoogleApiErrorWithCode(err, 429) && !isGoogleApiErrorWithCode(err, 503) {
+
+		// If we got a non-retryable error, return it.
+		if err != nil && !isRetryableError(err) {
 			return nil, "", err
 		}
+
+		// Try to set the operation (so we can check it's Error/State),
+		// and fail if we can't.
 		if err = w.SetOp(op); err != nil {
 			return nil, "", err
 		}
+
+		// Fail if the operation object contains an error.
 		if err = w.Error(); err != nil {
 			return nil, "", err
 		}
@@ -112,102 +138,7 @@ func OperationWait(w Waiter, activity string, timeoutMinutes int) error {
 	return nil
 }
 
-type CommonOperation struct {
-	// Done: If the value is `false`, it means the operation is still in
-	// progress.
-	// If `true`, the operation is completed, and either `error` or
-	// `response` is
-	// available.
-	Done bool `json:"done,omitempty"`
-
-	// Error: The error result of the operation in case of failure or
-	// cancellation.
-	Error *CommonOperationStatus `json:"error,omitempty"`
-
-	// Metadata: Service-specific metadata associated with the operation.
-	// It typically
-	// contains progress information and common metadata such as create
-	// time.
-	// Some services might not provide such metadata.  Any method that
-	// returns a
-	// long-running operation should document the metadata type, if any.
-	Metadata googleapi.RawMessage `json:"metadata,omitempty"`
-
-	// Name: The server-assigned name, which is only unique within the same
-	// service that
-	// originally returns it. If you use the default HTTP mapping,
-	// the
-	// `name` should have the format of `operations/some/unique/name`.
-	Name string `json:"name,omitempty"`
-
-	// Response: The normal response of the operation in case of success.
-	// If the original
-	// method returns no data on success, such as `Delete`, the response
-	// is
-	// `google.protobuf.Empty`.  If the original method is
-	// standard
-	// `Get`/`Create`/`Update`, the response should be the resource.  For
-	// other
-	// methods, the response should have the type `XxxResponse`, where
-	// `Xxx`
-	// is the original method name.  For example, if the original method
-	// name
-	// is `TakeSnapshot()`, the inferred response type
-	// is
-	// `TakeSnapshotResponse`.
-	Response googleapi.RawMessage `json:"response,omitempty"`
-
-	// ServerResponse contains the HTTP response code and headers from the
-	// server.
-	googleapi.ServerResponse `json:"-"`
-
-	// ForceSendFields is a list of field names (e.g. "Done") to
-	// unconditionally include in API requests. By default, fields with
-	// empty values are omitted from API requests. However, any non-pointer,
-	// non-interface field appearing in ForceSendFields will be sent to the
-	// server regardless of whether the field is empty or not. This may be
-	// used to include empty fields in Patch requests.
-	ForceSendFields []string `json:"-"`
-
-	// NullFields is a list of field names (e.g. "Done") to include in API
-	// requests with the JSON null value. By default, fields with empty
-	// values are omitted from API requests. However, any field with an
-	// empty value appearing in NullFields will be sent to the server as
-	// null. It is an error if a field in this list has a non-empty value.
-	// This may be used to include null fields in Patch requests.
-	NullFields []string `json:"-"`
-}
-
-type CommonOperationStatus struct {
-	// Code: The status code, which should be an enum value of
-	// google.rpc.Code.
-	Code int64 `json:"code,omitempty"`
-
-	// Details: A list of messages that carry the error details.  There is a
-	// common set of
-	// message types for APIs to use.
-	Details []googleapi.RawMessage `json:"details,omitempty"`
-
-	// Message: A developer-facing error message, which should be in
-	// English. Any
-	// user-facing error message should be localized and sent in
-	// the
-	// google.rpc.Status.details field, or localized by the client.
-	Message string `json:"message,omitempty"`
-
-	// ForceSendFields is a list of field names (e.g. "Code") to
-	// unconditionally include in API requests. By default, fields with
-	// empty values are omitted from API requests. However, any non-pointer,
-	// non-interface field appearing in ForceSendFields will be sent to the
-	// server regardless of whether the field is empty or not. This may be
-	// used to include empty fields in Patch requests.
-	ForceSendFields []string `json:"-"`
-
-	// NullFields is a list of field names (e.g. "Code") to include in API
-	// requests with the JSON null value. By default, fields with empty
-	// values are omitted from API requests. However, any field with an
-	// empty value appearing in NullFields will be sent to the server as
-	// null. It is an error if a field in this list has a non-empty value.
-	// This may be used to include null fields in Patch requests.
-	NullFields []string `json:"-"`
-}
+// The cloud resource manager API operation is an example of one of many
+// interchangeable API operations. Choose it somewhat arbitrarily to represent
+// the "common" operation.
+type CommonOperation cloudresourcemanager.Operation
