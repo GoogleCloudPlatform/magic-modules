9019bcc7f46d5d1489bc4949e0090f8833c3bcbc
jnahelou <nahelou.j@sfeir.com>
2019-01-04 16:39:50 +0100

    Update inspec templates for pubsub

--- templates/inspec/tests/integration/build/gcp-mm.tf
+++ templates/inspec/tests/integration/build/gcp-mm.tf
@@ -2,10 +2,30 @@ variable "ssl_policy" {
   type = "map"
 }
 
+variable "topic" {
+  type = "map"
+}
+
+variable "subscription" {
+  type = "map"
+}
+
 resource "google_compute_ssl_policy" "custom-ssl-policy" {
   name            = "${var.ssl_policy["name"]}"
   min_tls_version = "${var.ssl_policy["min_tls_version"]}"
   profile         = "${var.ssl_policy["profile"]}"
   custom_features = ["${var.ssl_policy["custom_feature"]}", "${var.ssl_policy["custom_feature2"]}"]
+  project         = "${var.gcp_project_id}"
+}
+
+resource "google_pubsub_topic" "topic" {
   project = "${var.gcp_project_id}"
+  name    = "${var.topic["name"]}"
+}
+
+resource "google_pubsub_subscription" "default" {
+  project              = "${var.gcp_project_id}"
+  name                 = "${var.subscription["name"]}"
+  topic                = "${google_pubsub_topic.topic.name}"
+  ack_deadline_seconds = 20
 }
