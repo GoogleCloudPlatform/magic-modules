613f3959b9bd7eaf801aee24942dce0f677b2df8
Alex Stephen <alexstephen@google.com>
2019-01-07 12:46:20 -0800

    adding update mask as a terraform type

--- provider/terraform.rb
+++ provider/terraform.rb
@@ -69,7 +69,8 @@ module Provider
         Api::Type::Array => 'schema.TypeList',
         Api::Type::KeyValuePairs => 'schema.TypeMap',
         Api::Type::Map => 'schema.TypeSet',
-        Api::Type::Fingerprint => 'schema.TypeString'
+        Api::Type::Fingerprint => 'schema.TypeString',
+        Api::Type::UpdateMask => 'schema.TypeString'
       }
     end
 
