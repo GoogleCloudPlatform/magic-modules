5ee65ce653b6c7a51b3543f76bc6ef12880f0476
Sam Levenick <slevenick@google.com>
2019-01-08 17:37:59 -0800

    Add dns managed zone + zones for inspec

--- templates/inspec/tests/integration/build/gcp-mm.tf
+++ templates/inspec/tests/integration/build/gcp-mm.tf
@@ -2,10 +2,25 @@ variable "ssl_policy" {
   type = "map"
 }
 
+variable "managed_zone" {
+	type = "map"
+}
+
 resource "google_compute_ssl_policy" "custom-ssl-policy" {
   name            = "${var.ssl_policy["name"]}"
   min_tls_version = "${var.ssl_policy["min_tls_version"]}"
   profile         = "${var.ssl_policy["profile"]}"
   custom_features = ["${var.ssl_policy["custom_feature"]}", "${var.ssl_policy["custom_feature2"]}"]
+  project = "${var.gcp_project_id}"
+}
+
+resource "google_dns_managed_zone" "prod" {
+  name        = "${var.managed_zone["name"]}"
+  dns_name    = "${var.managed_zone["dns_name"]}"
+  description = "${var.managed_zone["description"]}"
+
+  labels = {
+    key = "${var.managed_zone["label_value"]}"
+  }
   project = "${var.gcp_project_id}"
 }
