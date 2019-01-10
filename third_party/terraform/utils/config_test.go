7733079d925046b7af365410f36fee24c2d6c61e
emily <emilyye@google.com>
2019-01-09 16:32:42 -0800

    [Terraform] Add more OAuth2 credential options.  (#1201)
    Merged PR #1201.

--- third_party/terraform/utils/config_test.go
+++ third_party/terraform/utils/config_test.go
@@ -1,11 +1,14 @@
 package google
 
 import (
+	"context"
+	"golang.org/x/oauth2/google"
 	"io/ioutil"
 	"testing"
 )
 
 const testFakeCredentialsPath = "./test-fixtures/fake_account.json"
+const testOauthScope = "https://www.googleapis.com/auth/compute"
 
 func TestConfigLoadAndValidate_accountFilePath(t *testing.T) {
 	config := Config{
@@ -46,5 +49,57 @@ func TestConfigLoadAndValidate_accountFileJSONInvalid(t *testing.T) {
 
 	if config.loadAndValidate() == nil {
 		t.Fatalf("expected error, but got nil")
+	}
+}
+
+func TestAccConfigLoadValidate_credentials(t *testing.T) {
+	creds := getTestCredsFromEnv()
+	proj := getTestProjectFromEnv()
+
+	config := Config{
+		Credentials: creds,
+		Project:     proj,
+		Region:      "us-central1",
+	}
+
+	err := config.loadAndValidate()
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+
+	_, err = config.clientCompute.Zones.Get(proj, "us-central1-a").Do()
+	if err != nil {
+		t.Fatalf("expected call with loaded config client to work, got error: %s", err)
+	}
+}
+
+func TestAccConfigLoadValidate_accessToken(t *testing.T) {
+	creds := getTestCredsFromEnv()
+	proj := getTestProjectFromEnv()
+
+	c, err := google.CredentialsFromJSON(context.Background(), []byte(creds), testOauthScope)
+	if err != nil {
+		t.Fatalf("invalid test credentials: %s", err)
+	}
+
+	token, err := c.TokenSource.Token()
+	if err != nil {
+		t.Fatalf("Unable to generate test access token: %s", err)
+	}
+
+	config := Config{
+		AccessToken: token.AccessToken,
+		Project:     proj,
+		Region:      "us-central1",
+	}
+
+	err = config.loadAndValidate()
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+
+	_, err = config.clientCompute.Zones.Get(proj, "us-central1-a").Do()
+	if err != nil {
+		t.Fatalf("expected API call with loaded config to work, got error: %s", err)
 	}
 }
