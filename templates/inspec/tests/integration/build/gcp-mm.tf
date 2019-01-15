118c1919f648adcfbb213372b950475c564b7d38
Sam Levenick <slevenick@google.com>
2019-01-15 15:33:38 -0800

    Inspec resource record sets (#1239)
    Merged PR #1239.

--- templates/inspec/tests/integration/build/gcp-mm.tf
+++ templates/inspec/tests/integration/build/gcp-mm.tf
@@ -10,6 +10,14 @@ variable "subscription" {
   type = "map"
 }
 
+variable "managed_zone" {
+	type = "map"
+}
+
+variable "record_set" {
+	type = "map"
+}
+
 resource "google_compute_ssl_policy" "custom-ssl-policy" {
   name            = "${var.ssl_policy["name"]}"
   min_tls_version = "${var.ssl_policy["min_tls_version"]}"
@@ -28,4 +36,25 @@ resource "google_pubsub_subscription" "default" {
   name                 = "${var.subscription["name"]}"
   topic                = "${google_pubsub_topic.topic.name}"
   ack_deadline_seconds = "${var.subscription["ack_deadline_seconds"]}"
+}
+
+resource "google_dns_managed_zone" "prod" {
+  name        = "${var.managed_zone["name"]}"
+  dns_name    = "${var.managed_zone["dns_name"]}"
+  description = "${var.managed_zone["description"]}"
+
+  labels = {
+    key = "${var.managed_zone["label_value"]}"
+  }
+  project = "${var.gcp_project_id}"
+}
+
+resource "google_dns_record_set" "a" {
+  name = "${var.record_set["name"]}"
+  managed_zone = "${google_dns_managed_zone.prod.name}"
+  type = "${var.record_set["type"]}"
+  ttl  = "${var.record_set["ttl"]}"
+
+  rrdatas = ["${var.record_set["rrdatas1"]}", "${var.record_set["rrdatas2"]}"]
+  project = "${var.gcp_project_id}"
 }
