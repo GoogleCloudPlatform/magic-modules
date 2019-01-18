40a558a6921df1ff2881e9279ce3e1ef01593d73
Sam Levenick <slevenick@google.com>
2019-01-17 17:06:18 -0800

    Move custom function to its own .rb file and compile into inspec.yaml

---
+++ templates/inspec/custom_functions/google_compute_target_pool.rb
@@ -0,0 +1,5 @@
+RSpec::Matchers.alias_matcher :has_target_instance, :be_has_target_instance
+
+def has_target_instance?(name, zone)
+  instances.any? { |instance_self_link| instance_self_link.end_with?("zones/#{zone}/instances/#{name}") }
+end
