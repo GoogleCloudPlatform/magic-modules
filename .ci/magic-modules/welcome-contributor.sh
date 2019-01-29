baeb118c937843dcfe180beb1dba55dfc1cf778f
Nathan McKinley <nmckinley@google.com>
2019-01-29 14:39:33 -0800

    Make sure that we don't respond to community PRs more than once. (#1320)
    Merged PR #1320.

--- .ci/magic-modules/welcome-contributor.sh
+++ .ci/magic-modules/welcome-contributor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo > comment/pr_comment << EOF
+cat > comment/pr_comment << EOF
 Hello!  I am a robot who works on Magic Modules PRs.
 
 I have detected that you are a community contributor, so your PR will be assigned to someone with a commit-bit on this repo for initial review.  They will authorize it to run through our CI pipeline, which will generate downstream PRs.
