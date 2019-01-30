fbcb86ff78b996b2a588c4968ceb19cf28811276
Abhijeet Kasurde <akasurde@redhat.com>
2019-01-30 01:11:28 +0100

    Fix URL in documentation (#1022)
    Merged PR #1022.

--- provider/ansible/facts_override.rb
+++ provider/ansible/facts_override.rb
@@ -64,7 +64,7 @@ module Provider
         @name ||= 'filters'
         @description ||= <<-STRING
         A list of filter value pairs. Available filters are listed here
-                U(https://cloud.google.com/sdk/gcloud/reference/topic/filters).
+                https://cloud.google.com/sdk/gcloud/reference/topic/filters.
                 Each additional filter in the list will act be added as an AND condition
                 (filter1 and filter2)
         STRING
