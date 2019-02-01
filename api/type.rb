b93610d867cda6dd5be6c31809c8d06f62fd6f58
Alex Stephen <alexstephen@google.com>
2019-02-01 15:45:52 -0800

    better checks

--- api/type.rb
+++ api/type.rb
@@ -524,6 +524,8 @@ module Api
 
       def check_resource_ref_property_exists
         exported_props = resource_ref.all_user_properties
+        exported_props << Api::Type::String.new('selfLink') \
+          if resource_ref.has_self_link
         raise "'#{@imports}' does not exist on '#{@resource}'" \
           if exported_props.none? { |p| p.name == @imports }
       end
