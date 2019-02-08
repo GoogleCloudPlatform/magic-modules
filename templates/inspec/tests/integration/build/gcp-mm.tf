c0f3a32ab7ebb3ef867557ce03fb180250e34842
Sam Levenick <slevenick@google.com>
2019-02-08 11:44:29 -0800

    Add bigquery table, refactor nested objects to namespace properly

--- templates/inspec/tests/integration/build/gcp-mm.tf
+++ templates/inspec/tests/integration/build/gcp-mm.tf
@@ -102,6 +102,10 @@ variable "dataset" {
   type = "map"
 }
 
+variable "bigquery_table" {
+  type = "map"
+}
+
 resource "google_compute_ssl_policy" "custom-ssl-policy" {
   name            = "${var.ssl_policy["name"]}"
   min_tls_version = "${var.ssl_policy["min_tls_version"]}"
@@ -416,4 +420,22 @@ resource "google_bigquery_dataset" "gcp-inspec-dataset" {
     role           = "${var.dataset["access_writer_role"]}"
     special_group = "${var.dataset["access_writer_special_group"]}"
   }
+
+  access {
+    role           = "OWNER"
+    special_group = "projectOwners"
+  }
+}
+
+resource "google_bigquery_table" "gcp-inspec-bigquery-table" {
+  project    = "${var.gcp_project_id}"
+  dataset_id = "${google_bigquery_dataset.gcp-inspec-dataset.dataset_id}"
+  table_id   = "${var.bigquery_table["table_id"]}"
+
+  time_partitioning {
+    type = "${var.bigquery_table["time_partitioning_type"]}"
+  }
+
+  description = "${var.bigquery_table["description"]}"
+  expiration_time = "${var.bigquery_table["expiration_time"]}"
 }
