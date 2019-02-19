72c1bdbc4a8b11144f26ff6ce19ae201e7e47654
Alex Stephen <alexstephen@google.com>
2019-02-19 14:23:17 -0800

    upgrading hub image + adding ssh keys

--- .ci/magic-modules/release-ansible.sh
+++ .ci/magic-modules/release-ansible.sh
@@ -5,6 +5,18 @@ set -x
 # Install dependencies for Template Generator
 pushd "magic-modules-gcp"
 bundle install
+
+# Setup SSH keys.
+
+# Since these creds are going to be managed externally, we need to pass
+# them into the container as an environment variable.  We'll use
+# ssh-agent to ensure that these are the credentials used to update.
+set +x
+echo "$CREDS" > ~/github_private_key
+set -x
+chmod 400 ~/github_private_key
+
+ssh-agent bash -c "ssh-add ~/github_private_key; git submodule update --force --init"
 popd
 
 pushd "magic-modules-gcp/build/ansible"
