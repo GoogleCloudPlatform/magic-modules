d22e869fcd9baa3a6bb8e4ffed76f00f2b0c5893
Alex Stephen <alexstephen@google.com>
2019-02-26 10:42:18 -0800

    unit test fix on diffing
    I had to build my own differ to check the difference between GCP request
    + Ansible. As part of IAM Roles, I had to make a change to this that
    broke a unit test.
    
    At some point, I'll probably rewrite the differ (it feels brittle), but it has issues so
    rarely that I don't see the need to soon.

--- provider/ansible/gcp_utils.py
+++ provider/ansible/gcp_utils.py
@@ -268,9 +268,15 @@ class GcpRequest(object):
         list2.sort()
         for index in range(len(list1)):
             value1 = list1[index]
-            if index < len(list2):
-                value2 = list2[index]
-                difference.append(self._compare_value(value1, value2))
+            # If items are dicts or arrays, we're assuming the next value
+            # is the correct one.
+            if isinstance(value1, dict) or isinstance(value1, list):
+                if index < len(list2):
+                    value2 = list2[index]
+                    difference.append(self._compare_value(value1, value2))
+            else:
+                if value1 not in list2:
+                    difference.append(value1)
 
         difference2 = []
         for value in difference:
