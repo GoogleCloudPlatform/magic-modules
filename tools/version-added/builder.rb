ae12e56b4771e718949772bec06a994b20052ae8
Alex Stephen <alexstephen@google.com>
2019-03-04 11:12:48 -0800

    builds out an iterative version of version_added file

--- tools/version-added/builder.rb
+++ tools/version-added/builder.rb
@@ -11,21 +11,19 @@ require 'overrides/runner'
 
 CURRENT_ANSBILE_VERSION = '2.8'
 
-def version(object, higher_level)
-  return higher_level unless object
-  return object if object.to_f > higher_level.to_f
-  higher_level
+def version(path, struct)
+  path = path.map(&:to_sym) + [:version_added]
+  struct.dig(*path) || CURRENT_ANSIBLE_VERSION
 end
 
 # Builds out property information (with nesting)
-def property(prop, resource_version_added)
-  version_for_prop = version(prop.version_added, resource_version_added)
+def property(prop, path, struct)
   property_hash = {
-    version_added: version_for_prop,
+    version_added: version(path + [prop.name], struct)
   }
 
   prop.nested_properties.each do |nested_p|
-    property_hash[nested_p.name.to_sym] = property(nested_p, version_for_prop)
+    property_hash[nested_p.name.to_sym] = property(nested_p, path + [prop.name], struct)
   end
   property_hash
 end
@@ -34,12 +32,15 @@ products = Dir["products/**/ansible.yaml"].map { |x| x.split('/')[1] }
 for product in products
   # Get api.yaml
   product_yaml_path = ("products/#{product}/api.yaml")
-  provider_yaml_path = ("products/#{product}/ansible.yaml")
+  version_yaml_path = ("products/#{product}/ansible_version_added.yaml")
   product_api = Api::Compiler.new(product_yaml_path).run
   product_api.validate
 
-  # Get ansible.yaml
-  product_api, provider_config = Provider::Config.parse(provider_yaml_path, product_api, 'ga')
+  versions = if File.exists?(version_yaml_path)
+               YAML.load(File.read(version_yaml_path))
+             else
+               {}
+             end
 
   struct = {
     facts: {},
@@ -49,18 +50,18 @@ for product in products
   # Build out paths for regular modules.
   product_api.objects.each do |obj|
     resource = {
-      version_added: provider_config.manifest.get('version_added', obj)
+      version_added: version([:regular, obj.name], versions)
     }
 
     # Add properties.
     obj.all_user_properties.each do |prop|
-      resource[prop.name.to_sym] = property(prop, resource[:version_added])
+      resource[prop.name.to_sym] = property(prop, [:regular, obj.name], versions)
     end
     struct[:regular][obj.name.to_sym] = resource
 
     # Add facts modules from facts datasources.
     struct[:facts][obj.name.to_sym] = {
-      version_added: version(provider_config.datasources.instance_variable_get("@#{obj.name}").instance_variable_get('@version_added'), provider_config.manifest.get('version_added', obj))
+      version_added: version([:facts, obj.name], versions)
     }
   end
 
