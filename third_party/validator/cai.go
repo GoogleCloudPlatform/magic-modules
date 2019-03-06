bc6abab3d4088cfcebf10e2b9446e6b80e86736f
Nathan McKinley <nmckinley@google.com>
2019-03-06 11:12:09 -0800

    Create the CAI representation in addition to API objects. (#1483)
    Merged PR #1483.

---
+++ third_party/validator/cai.go
@@ -0,0 +1,26 @@
+package google
+
+// Asset is the CAI representation of a resource.
+type Asset struct {
+    // The name, in a peculiar format: `\\<api>.googleapis.com/<self_link>`
+    Name string
+    // The type name in `google.<api>.<resourcename>` format.
+    Type string
+    Resource *AssetResource
+}
+
+// AssetResource is the Asset's Resource field.
+type AssetResource struct {
+    // Api version
+    Version string
+    // URI including scheme for the discovery doc - assembled from
+    // product name and version.
+    DiscoveryDocumentURI string
+    // Resource name.
+    DiscoveryName string
+    // Actual resource state as per Terraform.  Note that this does
+    // not necessarily correspond perfectly with the CAI representation
+    // as there are occasional deviations between CAI and API responses.
+    // This returns the API response values instead.
+    Data map[string]interface{}
+}
