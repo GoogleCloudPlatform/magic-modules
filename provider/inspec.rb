9232c9353a9ce177a7b7e35e1abf4f85d61792fe
Sam Levenick <slevenick@google.com>
2019-03-18 10:24:42 -0700

    Remove extra string interpolation

--- provider/inspec.rb
+++ provider/inspec.rb
@@ -262,7 +262,7 @@ module Provider
 
         return item_from_hash.to_s
       elsif typed_array?(property)
-        class_name = "#{modularized_property_class(property.item_type)}"
+        class_name = modularized_property_class(property.item_type)
         return "#{class_name}Array.parse(#{item_from_hash}, to_s)"
       end
       "#{modularized_property_class(property)}.new(#{item_from_hash}, to_s)"
