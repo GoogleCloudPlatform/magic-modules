5006db001df5f7a5e14badf5d40e090a55083bc0
Sam Levenick <slevenick@google.com>
2019-03-27 14:16:52 -0700

    Google compute subnet policy additions, tests

--- templates/inspec/tests/integration/build/gcp-mm-beta.tf
+++ templates/inspec/tests/integration/build/gcp-mm-beta.tf
@@ -1,27 +1,31 @@
 provider "google-beta" {
-  region      = "${var.gcp_location}",
-  project         = "${var.gcp_project_id}"
+  region  = "${var.gcp_location}",
+  project = "${var.gcp_project_id}"
 }
 
-resource "google_compute_subnetwork" "subnetwork-iam" {
-  name          = "test-subnetwork"
+variable "subnetwork_policy_binding" {
+  type = "map"
+}
+
+resource "google_compute_subnetwork" "inspec-gcp-subnetwork-iam" {
+  name          = "${var.subnetwork_policy_binding["subnet_name"]}"
   ip_cidr_range = "10.2.0.0/16"
-  network       = "${google_compute_network.custom-test.self_link}"
-  project         = "${var.gcp_project_id}"
+  network       = "${google_compute_network.inspec-gcp-network-iam.self_link}"
+  project       = "${var.gcp_project_id}"
 }
 
-resource "google_compute_network" "custom-test" {
+resource "google_compute_network" "inspec-gcp-network-iam" {
+  project                 = "${var.gcp_project_id}"
   name                    = "test-network"
   auto_create_subnetworks = false
-  project         = "${var.gcp_project_id}"
 }
 
-resource "google_compute_subnetwork_iam_binding" "subnet" {
-  subnetwork = "${google_compute_subnetwork.subnetwork-iam.name}"
-  role       = "roles/compute.networkUser"
-	project         = "${var.gcp_project_id}"
+resource "google_compute_subnetwork_iam_binding" "inspec-gcp-subnet" {
+  subnetwork = "${google_compute_subnetwork.inspec-gcp-subnetwork-iam.name}"
+  role       = "${var.subnetwork_policy_binding["role"]}"
+	project    = "${var.gcp_project_id}"
   
   members = [
-    "user:slevenick@google.com",
+    "${var.subnetwork_policy_binding["member"]}"
   ]
 }
