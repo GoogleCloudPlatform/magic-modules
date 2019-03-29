1d632ecefd1e4d36932227c11391ce6c06d96ebc
Junyi Yi <junyi@microsoft.com>
2019-03-28 19:52:50 -0700

    Add acceptance tests for API Management Policy.

--- build/azterraform/azurerm/resource_arm_api_management_policy_test.go
+++ build/azterraform/azurerm/resource_arm_api_management_policy_test.go
@@ -24,6 +24,93 @@ import (
     "github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
+func TestAccAzureRMApiManagementPolicy_basic(t *testing.T) {
+    resourceName := "azurerm_api_management_policy.test"
+    ri := tf.AccRandTimeInt()
+    location := testLocation()
+
+    resource.ParallelTest(t, resource.TestCase{
+        PreCheck:     func() { testAccPreCheck(t) },
+        Providers:    testAccProviders,
+        CheckDestroy: testCheckAzureRMApiManagementPolicyDestroy,
+        Steps: []resource.TestStep{
+            {
+                Config: testAccAzureRMApiManagementPolicy_basic(ri, location),
+                Check: resource.ComposeTestCheckFunc(
+                    testCheckAzureRMApiManagementPolicyExists(resourceName),
+                    resource.TestCheckResourceAttr(resourceName, "xml_content", "<policies></policies>"),
+                ),
+            },
+            {
+                ResourceName:      resourceName,
+                ImportState:       true,
+                ImportStateVerify: true,
+            },
+        },
+    })
+}
+
+func TestAccAzureRMApiManagementPolicy_findReplace(t *testing.T) {
+    resourceName := "azurerm_api_management_policy.test"
+    ri := tf.AccRandTimeInt()
+    location := testLocation()
+
+    resource.ParallelTest(t, resource.TestCase{
+        PreCheck:     func() { testAccPreCheck(t) },
+        Providers:    testAccProviders,
+        CheckDestroy: testCheckAzureRMApiManagementPolicyDestroy,
+        Steps: []resource.TestStep{
+            {
+                Config: testAccAzureRMApiManagementPolicy_findReplace(ri, location),
+                Check: resource.ComposeTestCheckFunc(
+                    testCheckAzureRMApiManagementPolicyExists(resourceName),
+                    resource.TestCheckResourceAttr(resourceName, "xml_content", "<policies><inbound><find-and-replace from='xyz' to='abc' /></inbound></policies>"),
+                ),
+            },
+            {
+                ResourceName:      resourceName,
+                ImportState:       true,
+                ImportStateVerify: true,
+            },
+        },
+    })
+}
+
+func TestAccAzureRMApiManagementPolicy_update(t *testing.T) {
+    resourceName := "azurerm_api_management_policy.test"
+    ri := tf.AccRandTimeInt()
+    location := testLocation()
+
+    resource.ParallelTest(t, resource.TestCase{
+        PreCheck:     func() { testAccPreCheck(t) },
+        Providers:    testAccProviders,
+        CheckDestroy: testCheckAzureRMApiManagementPolicyDestroy,
+        Steps: []resource.TestStep{
+            {
+                Config: testAccAzureRMApiManagementPolicy_basic(ri, location),
+                Check: resource.ComposeTestCheckFunc(
+                    testCheckAzureRMApiManagementPolicyExists(resourceName),
+                    resource.TestCheckResourceAttr(resourceName, "xml_content", "<policies></policies>"),
+                ),
+            },
+            {
+                Config: testAccAzureRMApiManagementPolicy_findReplace(ri, location),
+                Check: resource.ComposeTestCheckFunc(
+                    testCheckAzureRMApiManagementPolicyExists(resourceName),
+                    resource.TestCheckResourceAttr(resourceName, "xml_content", "<policies><inbound><find-and-replace from='xyz' to='abc' /></inbound></policies>"),
+                ),
+            },
+            {
+                Config: testAccAzureRMApiManagementPolicy_basic(ri, location),
+                Check: resource.ComposeTestCheckFunc(
+                    testCheckAzureRMApiManagementPolicyExists(resourceName),
+                    resource.TestCheckResourceAttr(resourceName, "xml_content", "<policies></policies>"),
+                ),
+            },
+        },
+    })
+}
+
 
 func testCheckAzureRMApiManagementPolicyExists(resourceName string) resource.TestCheckFunc {
     return func(s *terraform.State) error {
@@ -71,4 +158,60 @@ func testCheckAzureRMApiManagementPolicyDestroy(s *terraform.State) error {
     }
 
     return nil
+}
+
+func testAccAzureRMApiManagementPolicy_basic(rInt int, location string) string {
+    return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_api_management" "test" {
+  name                = "acctestAM-%d"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  location            = "${azurerm_resource_group.test.location}"
+  publisher_name      = "pub1"
+  publisher_email     = "pub1@email.com"
+
+  sku = {
+    name     = "Developer"
+    capacity = 1
+  }
+}
+
+resource "azurerm_api_management_policy" "test" {
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  api_management_name = "${azurerm_api_management.test.name}"
+  xml_content         = "<policies></policies>"
+}
+`, rInt, location, rInt)
+}
+
+func testAccAzureRMApiManagementPolicy_findReplace(rInt int, location string) string {
+    return fmt.Sprintf(`
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurerm_api_management" "test" {
+  name                = "acctestAM-%d"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  location            = "${azurerm_resource_group.test.location}"
+  publisher_name      = "pub1"
+  publisher_email     = "pub1@email.com"
+
+  sku = {
+    name     = "Developer"
+    capacity = 1
+  }
+}
+
+resource "azurerm_api_management_policy" "test" {
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  api_management_name = "${azurerm_api_management.test.name}"
+  xml_content         = "<policies><inbound><find-and-replace from='xyz' to='abc' /></inbound></policies>"
+}
+`, rInt, location, rInt)
 }
