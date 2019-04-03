676aadd5456f74d873277fe37ca75f1cc9a1dfd7
Junyi Yi <junyi@microsoft.com>
2019-04-03 11:42:31 -0700

    Azure SDK definition overrides kick-off

---
+++ api/azure/sdk_definition_override.rb
@@ -0,0 +1,21 @@
+require 'api/object'
+require 'api/azure/sdk_operation_definition_override'
+
+module Api
+  module Azure
+    module SDKDefinitionOverride < Api::Object
+      attr_reader :create
+      attr_reader :read
+      attr_reader :update
+      attr_reader :delete
+
+      def validate
+        super
+        check_optional_property :create, Api::Azure::SDKOperationDefinitionOverride
+        check_optional_property :read, Api::Azure::SDKOperationDefinitionOverride
+        check_optional_property :update, Api::Azure::SDKOperationDefinitionOverride
+        check_optional_property :delete, Api::Azure::SDKOperationDefinitionOverride
+      end
+    end
+  end
+end
---
+++ api/azure/sdk_operation_definition_override.rb
@@ -0,0 +1,19 @@
+require 'api/object'
+require 'api/azure/sdk_type_definition_override'
+
+module Api
+  module Azure
+    class SDKOperationDefinitionOverride < Api::Object
+      attr_reader :request
+      attr_reader :response
+
+      def validate
+        super
+        check_property :request, Hash
+        check_property_hash :request, String, Api::Azure::SDKTypeDefinitionOverride
+        check_optional_property :response, Hash
+        check_optional_property_hash :response, String, Api::Azure::SDKTypeDefinitionOverride
+      end
+    end
+  end
+end
