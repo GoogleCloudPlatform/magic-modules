47f570cfc94ba9fe2c5e8a32242279c9bef65640
Alex Stephen <alexstephen@google.com>
2019-05-20 17:04:06 -0700

    doc frag has multiples weirdly

--- provider/ansible/gcp_doc_frag.py
+++ provider/ansible/gcp_doc_frag.py
@@ -27,11 +27,6 @@ options:
     service_account_file:
         description:
             - The path of a Service Account JSON file if serviceaccount is selected as type.
-    service_account_contents:
-        description:
-            - A string representing the contents of a Service Account JSON file.
-            - This should not be passed in as a dictionary, but a string has
-              the exact contents of a service account json file (valid JSON).
     service_account_email:
         description:
             - An optional service account email address if machineaccount is selected
@@ -48,8 +43,6 @@ notes:
     c(GCP_SERVICE_ACCOUNT_CONTENTS) env variable.
   - For authentication, you can set service_account_email using the
     C(GCP_SERVICE_ACCOUNT_EMAIL) env variable.
-  - For authentication, you can set service_account_contents using the
-    C(GCP_SERVICE_ACCOUNT_CONTENTS) env variable.
   - For authentication, you can set auth_kind using the C(GCP_AUTH_KIND) env
     variable.
   - For authentication, you can set scopes using the C(GCP_SCOPES) env variable.
