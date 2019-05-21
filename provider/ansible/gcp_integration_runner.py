c015cea3b112cf02538bcf4fc9865ee98c66f282
Alex Stephen <alexstephen@google.com>
2019-05-20 22:46:36 -0700

    2.9 rebase (#1800)
    Merged PR #1800.

--- provider/ansible/gcp_integration_runner.py
+++ provider/ansible/gcp_integration_runner.py
@@ -14,6 +14,7 @@ from lib.util import (
 from lib.cloud import (
     CloudProvider,
     CloudEnvironment,
+    CloudEnvironmentConfig,
 )
 
 from lib.core_ci import (
@@ -46,22 +47,19 @@ class GcpCloudProvider(CloudProvider):
 
 class GcpCloudEnvironment(CloudEnvironment):
     """GCP cloud environment plugin. Updates integration test environment after delegation."""
+    def get_environment_config(self):
+        """
+        :rtype: CloudEnvironmentConfig
+        """
+        parser = ConfigParser()
+        parser.read(self.config_path)
 
-    def configure_environment(self, env, cmd):
-        """
-        :type env: dict[str, str]
-        :type cmd: list[str]
-        """
-        cmd.append('-e')
-        cmd.append('@%s' % self.config_path)
+        ansible_vars = dict(
+            resource_prefix=self.resource_prefix,
+        )
 
-        cmd.append('-e')
-        cmd.append('resource_prefix=%s' % self.resource_prefix)
+        ansible_vars.update(dict(parser.items('default')))
 
-    def on_failure(self, target, tries):
-        """
-        :type target: TestTarget
-        :type tries: int
-        """
-        if not tries and self.managed:
-            display.notice('%s failed' % target.name)
+        return CloudEnvironmentConfig(
+            ansible_vars=ansible_vars,
+        )
