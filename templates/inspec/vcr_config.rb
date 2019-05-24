18914b92a7f4bfafc94662902f503fcd8a2531d0
Sam Levenick <slevenick@google.com>
2019-05-24 13:46:08 -0700

    Fix rubocop newlines

--- templates/inspec/vcr_config.rb
+++ templates/inspec/vcr_config.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # Copyright 2017 Google Inc.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
