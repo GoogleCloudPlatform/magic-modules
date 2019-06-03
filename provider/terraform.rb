4692465c680c63fc0ab7e17a5ba596d8892eb9ad
Riley Karson <rileykarson@google.com>
2019-06-03 14:47:30 -0700

    Remove redundant word

--- provider/terraform.rb
+++ provider/terraform.rb
@@ -110,7 +110,7 @@ module Provider
     # Values marked with % are URL-encoded, and will match any number of /'s.
     #
     # Note: ?P indicates a Python-compatible named capture group. Named groups
-    # aren't isn't common in JS-based regex flavours, but are in Perl-based ones
+    # aren't common in JS-based regex flavours, but are in Perl-based ones
     def format2regex(format)
       format
         .gsub(/{{%([[:word:]]+)}}/, '(?P<\1>.+)')
