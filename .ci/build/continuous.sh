68b4209999a29905e015a90cbca888c63072f832
Chris Stephens <chrisst@google.com>
2019-06-10 16:40:53 -0700

    call the compiler

--- .ci/build/continuous.sh
+++ .ci/build/continuous.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# This file is for builds that are running on kokoro. It is primarily to be a kokoro
+# specific wrapper around existing magic modules commands
+
 # Fail on any error.
 set -e
 # Display commands being run.
 set -x
 
-env | grep KOKORO
+bundle exec compiler -p products/monitoring -e terraform -o /Users/chrisst/work/go/src/github.com/terraform-providers/terraform-provider-google
 
-ruby --version
