143f50935c8c179f757ed8f31c12022633e243b5
Junyi Yi <junyi@microsoft.com>
2019-06-13 16:18:03 -0700

    Fix test and order bugs in Ansible and Terraform

--- provider/azure/terraform/helpers.rb
+++ provider/azure/terraform/helpers.rb
@@ -8,9 +8,9 @@ module Provider
         end
 
         def order_azure_properties(properties)
-          special_props = properties.select{|p| p.name == 'name' || p.name == 'location' || p.name == 'resourceGroupName'}
-          other_props = properties.reject{|p| p.name == 'name' || p.name == 'location' || p.name == 'resourceGroupName'}
-          special_props + order_properties(other_props)
+          special_props = properties.select{|p| p.name == 'name' || p.name == 'location' || p.name == 'resourceGroupName' || p.name == 'resourceGroup'}
+          other_props = properties.reject{|p| p.name == 'name' || p.name == 'location' || p.name == 'resourceGroupName' || p.name == 'resourceGroup'}
+          special_props.sort_by(&:order) + order_properties(other_props)
         end
       end
     end
