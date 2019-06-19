21f721fa7ad6e2f8fa2d4fc8231d84f46be9fbbe
emily <emilyye@google.com>
2019-06-19 11:43:23 -0700

    Refactor CI python scripts (#1869)
    Merged PR #1869.

--- .ci/magic-modules/ensure_downstreams_merged.py
+++ .ci/magic-modules/ensure_downstreams_merged.py
@@ -1,36 +1,22 @@
 #!/usr/bin/env python
+"""
+This script takes the name of a file containing an upstream PR number
+and returns an error if not all of its downstreams have been merged.
 
-import get_downstream_prs
-from github import Github
+Required env vars:
+  GH_TOKEN: Github token
+"""
 import os
-import re
-import operator
-import itertools
 import sys
-
-
-def get_unmerged_prs(g, dependencies):
-  parsed_dependencies = [re.match(r'https://github.com/([\w-]+/[\w-]+)/pull/(\d+)', d).groups()
-          for d in dependencies]
-  parsed_dependencies.sort(key=operator.itemgetter(0))
-  unmerged_dependencies = []
-  # group those dependencies by repo - e.g. [("terraform-provider-google", ["123", "456"]), ...]
-  for r, pulls in itertools.groupby(parsed_dependencies, key=operator.itemgetter(0)):
-    repo = g.get_repo(r)
-    for pull in pulls:
-      # check whether the PR is merged - if it is, add it to the list.
-      pr = repo.get_pull(int(pull[1]))
-      if not pr.is_merged() and not pr.state == "closed":
-        unmerged_dependencies.append(pull)
-  return unmerged_dependencies
-
+from github import Github
+from pyutils import downstreams
 
 if __name__ == '__main__':
-  g = Github(os.environ.get('GH_TOKEN'))
-  assert len(sys.argv) == 2
-  id_filename = sys.argv[1]
-  unmerged = get_unmerged_prs(
-          g, get_downstream_prs.get_github_dependencies(
-              g, int(open(id_filename).read())))
+  assert len(sys.argv) == 2, "expected id filename as argument"
+  with open(sys.argv[1]) as f:
+    pr_num = int(f.read())
+
+  client = Github(os.environ.get('GH_TOKEN'))
+  unmerged = downstreams.find_unmerged_downstreams(client, pr_num)
   if unmerged:
     raise ValueError("some PRs are unmerged", unmerged)
