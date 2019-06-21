41f2c7aedc268fc85ff48f12807f4aabbafbdaba
Alex Stephen <alexstephen@google.com>
2019-06-21 16:07:07 -0700

    rubocop fixes

--- tools/builder/api.rb
+++ tools/builder/api.rb
@@ -1,4 +1,4 @@
-# Copyright 2018 Google Inc.
+# Copyright 2019 Google Inc.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -25,7 +25,9 @@ require 'api/async'
 # Creating an api.yaml involves a lot of setting values.
 # This will create setters for all fields on an api.yaml
 # (but only in the context of the linter)
+# rubocop:disable Style/MissingRespondToMissing
 module Api
+  # Api::Object class being overriden.
   class Object
     # Create a setter if the setter doesn't exist
     # Yes, this isn't pretty and I apologize
@@ -41,6 +43,7 @@ module Api
     end
   end
 end
+# rubocop:enable Style/MissingRespondToMissing
 
 TYPES = {
   'string': 'String',
@@ -49,8 +52,9 @@ TYPES = {
   'integer': 'Integer',
   'number': 'Double',
   'array': 'Array'
-}
+}.freeze
 
+# Handles all logic of merging a discovery + handwritten api.yaml in proper order.
 class HumanApi
   def initialize(discovery, handwritten)
     @discovery = discovery
@@ -59,25 +63,27 @@ class HumanApi
 
   def build
     if @written.nil?
-      return @discovery
+      @discovery
     else
       # For each product, inject extra properties.
       @written.objects.each do |prod|
         matching_object = @discovery.objects.select { |o| o.name == prod.name }.first
         next unless matching_object
+
         add_missing_properties(matching_object.properties, prod.properties) unless @written.nil?
       end
 
       # Inject extra products at end
-      missing_products = @discovery.objects.select { |x| !@written.objects.map(&:name).include?(x.name) }
-      @written.objects.append(missing_products) unless @written.nil?
-      return @written
+      missing_products = @discovery.objects
+                                   .reject { |x| @written.objects.map(&:name).include?(x.name) }
+      @written&.objects&.append(missing_products)
+      @written
     end
   end
 
   def add_missing_properties(disc_props, hand_props)
     # If nested property, recurse.
-    hand_props.select { |p| p.nested_properties? }
+    hand_props.select(&:nested_properties?)
               .each do |p|
       matching_discovery_prop = disc_props.select { |d| d.name == p.name }.first
       if p.is_a?(Api::Type::NestedObject)
@@ -87,11 +93,12 @@ class HumanApi
       end
     end
     # Inject new properties.
-    missing_properties = disc_props.select { |p| !hand_props.any? { |d| d.name == p.name }}
+    missing_properties = disc_props.reject { |p| hand_props.any? { |d| d.name == p.name } }
     hand_props.append(missing_properties)
   end
 end
 
+# Converts a Discovery Doc property to a api.yaml property
 class DiscoveryProperty
   attr_reader :schema
   attr_reader :name
@@ -105,7 +112,7 @@ class DiscoveryProperty
     @__product = product
   end
 
-  def get_property
+  def property
     prop = Module.const_get("Api::Type::#{type}").new
     prop.name = @name
     prop.description = @schema.dig('description')
@@ -119,9 +126,10 @@ class DiscoveryProperty
   private
 
   def type
-    return "NestedObject" if @schema.dig('$ref')
-    return "NestedObject" if @schema.dig('type') == 'object' && @schema.dig('properties')
-    return "Enum" if @schema.dig('enum')
+    return 'NestedObject' if @schema.dig('$ref')
+    return 'NestedObject' if @schema.dig('type') == 'object' && @schema.dig('properties')
+    return 'Enum' if @schema.dig('enum')
+
     TYPES[@schema.dig('type').to_sym]
   end
 
@@ -130,7 +138,7 @@ class DiscoveryProperty
   end
 
   def enum
-    @schema.dig('enum').map { |val| val.to_sym }
+    @schema.dig('enum').map(&:to_sym)
   end
 
   def nested
@@ -145,11 +153,11 @@ class DiscoveryProperty
     schema_type = @schema.dig('items', 'type')
     if (!schema_type && @schema.dig('items', '$ref')) || @schema.dig('items', 'properties')
       prop = Api::Type::NestedObject.new
-      if @schema.dig('items', '$ref')
-        prop.properties = @__product.get_resource(@schema.dig('items', '$ref')).properties
-      else
-        prop.properties = DiscoveryResource.new(@schema.dig('items'), @__product).properties
-      end
+      prop.properties = if @schema.dig('items', '$ref')
+                          @__product.get_resource(@schema.dig('items', '$ref')).properties
+                        else
+                          DiscoveryResource.new(@schema.dig('items'), @__product).properties
+                        end
       return prop
     end
     return "Api::Type::#{TYPES[schema_type.to_sym]}" if schema_type != 'object'
@@ -186,7 +194,7 @@ class DiscoveryResource
   def properties
     @schema.dig('properties')
            .reject { |k, _| k == 'kind' }
-           .map { |k, v| DiscoveryProperty.new(k, v, @__product).get_property }
+           .map { |k, v| DiscoveryProperty.new(k, v, @__product).property }
   end
 
   private
@@ -206,9 +214,10 @@ class DiscoveryProduct
     @object = object
   end
 
-  def get_resources
+  def resources
     @results['schemas'].map do |name, _|
       next if name != @object
+
       get_resource(name).resource
     end.compact
   end
@@ -219,15 +228,18 @@ class DiscoveryProduct
 
   def get_methods_for_resource(resource)
     return if resource.nil?
+
     methods = @results.dig 'resources', resource.pluralize.camelize(:lower), 'methods'
     return methods unless methods.nil?
-    @results.dig 'resources', 'namespaces', 'resources', resource.pluralize.camelize(:lower), 'methods'
+
+    @results.dig 'resources', 'namespaces', 'resources',
+                 resource.pluralize.camelize(:lower), 'methods'
   end
 
-  def get_product
+  def product
     product = Api::Product.new
     product.versions = [version]
-    product.objects = get_resources
+    product.objects = resources
     product
   end
 
