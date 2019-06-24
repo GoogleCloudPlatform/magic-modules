d231348d49c9814abaefc97402dea58ee9a9bd11
Sam Levenick <slevenick@google.com>
2019-06-24 15:59:44 -0700

    Rubocop

--- provider/terraform_object_library.rb
+++ provider/terraform_object_library.rb
@@ -17,7 +17,7 @@ module Provider
   # Code generator for a library converting terraform state to gcp objects.
   class TerraformObjectLibrary < Provider::Terraform
     def generate(output_folder, types, version_name, _product_path, _dump_yaml)
-      version = @api.version_obj_or_default(version_name)
+      @version_name = version_name
       generate_objects(output_folder, types, version_name)
     end
 
