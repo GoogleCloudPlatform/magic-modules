e7131bb70c4dfda5ec455b2c517af9d8c47e778d
Sam Levenick <slevenick@google.com>
2019-06-24 16:18:21 -0700

    Fix version name parsing in terraform mapper

--- provider/terraform_object_library.rb
+++ provider/terraform_object_library.rb
@@ -18,6 +18,7 @@ module Provider
   class TerraformObjectLibrary < Provider::Terraform
     def generate(output_folder, types, version_name, _product_path, _dump_yaml)
       version = @api.version_obj_or_default(version_name)
+      @base_url = version.base_url
       generate_objects(output_folder, types, version.name)
     end
 
