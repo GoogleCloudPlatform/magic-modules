45fd7a44aa2a25769fcbed293ddb4dca9cc1798b
Sam Levenick <slevenick@google.com>
2019-07-08 14:10:09 -0700

    Rename generated iam files

--- provider/terraform.rb
+++ provider/terraform.rb
@@ -205,11 +205,11 @@ module Provider
 
       name = data.object.name.underscore
       product_name = data.product.name.underscore
-      filepath = File.join(target_folder, "resource_#{product_name}_#{name}_iam_policy.go")
+      filepath = File.join(target_folder, "iam_#{product_name}_#{name}.go")
 
       data.generate('templates/terraform/iam_policy.go.erb', filepath, self)
 
-      filepath = File.join(target_folder, "resource_#{product_name}_#{name}_iam_policy_generated_test.go")
+      filepath = File.join(target_folder, "iam_#{product_name}_#{name}_generated_test.go")
       data.generate('templates/terraform/examples/base_configs/iam_test_file.go.erb', filepath, self)
     end
 
