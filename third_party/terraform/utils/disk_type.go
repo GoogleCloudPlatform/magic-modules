4216c1780cc2c8f1984090913c0b8f47950df7bd
Dana Hoffman <danahoffman@google.com>
2019-07-17 16:57:19 -0700

    fix error in conversion repo

--- third_party/terraform/utils/disk_type.go
+++ third_party/terraform/utils/disk_type.go
@@ -1,15 +1,11 @@
 package google
 
-import (
-	"github.com/hashicorp/terraform/helper/schema"
-)
-
 // readDiskType finds the disk type with the given name.
-func readDiskType(c *Config, d *schema.ResourceData, name string) (*ZonalFieldValue, error) {
+func readDiskType(c *Config, d TerraformResourceData, name string) (*ZonalFieldValue, error) {
 	return parseZonalFieldValue("diskTypes", name, "project", "zone", d, c, false)
 }
 
 // readRegionDiskType finds the disk type with the given name.
-func readRegionDiskType(c *Config, d *schema.ResourceData, name string) (*RegionalFieldValue, error) {
+func readRegionDiskType(c *Config, d TerraformResourceData, name string) (*RegionalFieldValue, error) {
 	return parseRegionalFieldValue("diskTypes", name, "project", "region", "zone", d, c, false)
 }
