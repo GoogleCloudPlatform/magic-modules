7c2fc345063b743a6df6ce73e5d6e03e91ca210e
Riley Karson <rileykarson@google.com>
2019-07-23 09:21:39 -0700

    Add additional import formats, extend identity logic (#2058)
    Merged PR #2058.

--- api/resource.rb
+++ api/resource.rb
@@ -86,12 +86,13 @@ module Api
       # within the collection (list) json. Will default to the
       # camelcase pluralize name of the resource.
       attr_reader :collection_url_key
-      # [Optional] This is an array with items that uniquely identify the
-      # resource.
-      # This is useful in case an API returns a list result and we need
-      # to fetch the particular resource we're interested in from that
-      # list.  Otherwise, it's safe to leave empty.
-      # If empty, we assume that `name` is the identifier.
+      # [Optional] An ordered list of names of parameters that uniquely identify
+      # the resource.
+      # Generally, it's safe to leave empty, in which case it defaults to `name`.
+      # Other values are normally useful in cases where an object has a parent
+      # and is identified by some non-name value, such as an ip+port pair.
+      # If you're writing a fine-grained resource (eg with nested_query) a value
+      # must be set.
       attr_reader :identity
       # [Optional] (Api::Resource::NestedQuery) This is useful in case you need
       # to change the query made for GET requests only. In particular, this is
@@ -246,7 +247,7 @@ module Api
       if @identity.nil?
         props.select { |p| p.name == Api::Type::String::NAME.name }
       else
-        props.select { |p| @identity.include?(p.name) }
+        props.select { |p| @identity.include?(p.name) }.sort_by { |p| @identity.index p.name }
       end
     end
 
