9b6969499f655f1c1da717d88e3791e56490cf1c
Alex Stephen <alexstephen@google.com>
2019-08-06 14:27:40 -0700

    rubocop

--- provider/ansible.rb
+++ provider/ansible.rb
@@ -287,8 +287,11 @@ module Provider
                       self)
 
         # Generate symlink for old `facts` modules.
-        deprecated_facts_path = File.join(target_folder, "lib/ansible/modules/cloud/google/_#{name}_facts.py")
-        File.symlink "#{name}_info.py", deprecated_facts_path unless File.exists?(deprecated_facts_path)
+        deprecated_facts_path = File.join(target_folder,
+                                          "lib/ansible/modules/cloud/google/_#{name}_facts.py")
+        return if File.exist?(deprecated_facts_path)
+
+        File.symlink "#{name}_info.py", deprecated_facts_path
       end
 
       def generate_objects(output_folder, types, version_name)
