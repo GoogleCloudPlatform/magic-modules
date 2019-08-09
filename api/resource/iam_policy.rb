342a1bdf87cc9c8da791dfc1409f0e362811a354
Sam Levenick <slevenick@google.com>
2019-08-09 12:52:13 -0700

    Generating iap_web_type iam resources

--- api/resource/iam_policy.rb
+++ api/resource/iam_policy.rb
@@ -30,11 +30,48 @@ module Api
       # While Compute subnetwork uses {resource}/getIamPolicy
       attr_reader :method_name_separator
 
+      # boolean of if a parent resource is required to apply this IAM policy to
+      # Some IAM policies are applied across multiple resources rather than a 
+      # specific resource
+      attr_reader :parent_resource_required
+
+      # The terraform type of the parent resource if it is not the same as the
+      # IAM resource. The IAP product needs these as its IAM policies refer
+      # to compute resources
+      attr_reader :parent_resource_type
+
+      # Some resources allow retrieving the IAM policy with GET requests,
+      # others expect POST requests
+      attr_reader :fetch_iam_policy_verb
+
+      # Certain resources allow different sets of roles to be set with IAM policies
+      # This is a role that is acceptable for the given IAM policy resource for use in tests
+      attr_reader :allowed_iam_role
+
+      # The code that is rendered within qualify{{resource.name}}Url. This is used for IAP
+      # that supports different URL endpoints based values set in the config
+      attr_reader :custom_url_qualifier
+
+      # The code that is rendered within qualify{{resource.name}}Url. This is used for IAP
+      # that supports different URL endpoints based values set in the config
+      attr_reader :custom_id_function
+
+      # Allows for optional properties to be specified that are allowed in the IAM policy
+      # these are needed for IAP that switches URLs based on the presence of these properties
+      attr_reader :optional_properties
+
       def validate
         super
 
         check :exclude, type: :boolean, default: false
         check :method_name_separator, type: String, default: '/'
+        check :parent_resource_required, type: :boolean, default: true
+        check :parent_resource_type, type: String
+        check :fetch_iam_policy_verb, type: String, default: 'GET'
+        check :allowed_iam_role, type: String, default: 'roles/editor'
+        check :custom_url_qualifier, type: String
+        check :custom_id_function, type: String
+        check :optional_properties, type: Array, item_type: Api::Type, default: []
       end
     end
   end
