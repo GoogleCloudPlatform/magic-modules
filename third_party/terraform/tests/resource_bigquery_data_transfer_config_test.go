844e8ee6fce86c40bbeecbba94960c71d59eabaf
Ty Larrabee <tysen@google.com>
2019-08-12 11:15:40 -0700

    Add unique project for update test

--- third_party/terraform/tests/resource_bigquery_data_transfer_config_test.go
+++ third_party/terraform/tests/resource_bigquery_data_transfer_config_test.go
@@ -13,16 +13,19 @@ func TestAccBigqueryDataTransferConfig_scheduledQueryUpdate(t *testing.T) {
 
 	random_suffix := acctest.RandString(10)
 
+	projectOrg := getTestOrgFromEnv(t)
+	projectBillingAccount := getTestBillingAccountFromEnv(t)
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckBigqueryDataTransferConfigDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccBigqueryDataTransferConfig_scheduledQueryUpdate(random_suffix, "first", "y"),
+				Config: testAccBigqueryDataTransferConfig_scheduledQueryUpdate(random_suffix, projectOrg, projectBillingAccount, "first", "y"),
 			},
 			{
-				Config: testAccBigqueryDataTransferConfig_scheduledQueryUpdate(random_suffix, "second", "z"),
+				Config: testAccBigqueryDataTransferConfig_scheduledQueryUpdate(random_suffix, projectOrg, projectBillingAccount, "second", "z"),
 			},
 			{
 				ResourceName:            "google_bigquery_data_transfer_config.query_config",
@@ -34,16 +37,29 @@ func TestAccBigqueryDataTransferConfig_scheduledQueryUpdate(t *testing.T) {
 	})
 }
 
-func testAccBigqueryDataTransferConfig_scheduledQueryUpdate(random_suffix, schedule, letter string) string {
+func testAccBigqueryDataTransferConfig_scheduledQueryUpdate(random_suffix, org, billing, schedule, letter string) string {
 	return fmt.Sprintf(`
-data "google_project" "project" {}
+resource "google_project" "update" {
+  name = "terraform-%s"
+  project_id = "terraform-%s"
+  org_id = "%s"
+  billing_account = "%s"
+}
+
+resource "google_project_services" "update" {
+  project = google_project.update.project_id
+  services = ["bigquerydatatransfer.googleapis.com", "bigquery-json.googleapis.com"]
+}
 
 resource "google_project_iam_member" "permissions" {
+  depends_on = [google_project_services.update]
+  project = google_project.update.project_id
   role = "roles/iam.serviceAccountShortTermTokenMinter"
-  member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-bigquerydatatransfer.iam.gserviceaccount.com"
+  member = "serviceAccount:service-${google_project.update.number}@gcp-sa-bigquerydatatransfer.iam.gserviceaccount.com"
 }
 
 resource "google_bigquery_data_transfer_config" "query_config" {
+  project = google_project.update.project_id
 
   depends_on = [google_project_iam_member.permissions]
 
@@ -51,7 +67,7 @@ resource "google_bigquery_data_transfer_config" "query_config" {
   location = "asia-northeast1"
   data_source_id = "scheduled_query"
   schedule = "%s sunday of quarter 00:00"
-  destination_dataset_id = "${google_bigquery_dataset.my-dataset.dataset_id}"
+  destination_dataset_id = google_bigquery_dataset.my-dataset.dataset_id
   params = {
     destination_table_name_template = "my-table"
     write_disposition = "WRITE_APPEND"
@@ -60,6 +76,7 @@ resource "google_bigquery_data_transfer_config" "query_config" {
 }
 
 resource "google_bigquery_dataset" "my-dataset" {
+  project = google_project.update.project_id
 
   depends_on = [google_project_iam_member.permissions]
 
@@ -68,5 +85,5 @@ resource "google_bigquery_dataset" "my-dataset" {
   description = "bar"
   location = "asia-northeast1"
 }
-`, random_suffix, schedule, letter, random_suffix)
+`, random_suffix, random_suffix, org, billing, random_suffix, schedule, letter, random_suffix)
 }
