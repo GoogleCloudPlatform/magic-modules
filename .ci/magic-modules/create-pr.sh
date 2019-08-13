4d78370477368d0eaeb960bd9efc36c56fee4edb
emily <emilyye@google.com>
2019-08-13 12:38:26 -0700

    Add warning for handwriting conversion files and mv some files in third_party (#2158)
    Merged PR #2158.

--- .ci/magic-modules/create-pr.sh
+++ .ci/magic-modules/create-pr.sh
@@ -46,8 +46,14 @@ LABELS=""
 
 # Check the files between this commit and HEAD
 # If they're only contained in third_party, add the third_party label.
-if [ ! git diff --name-only HEAD^1 | grep -v "third_party" | grep -v ".gitmodules" | grep -r "build/" ]; then
+if [ -z "$(git diff --name-only HEAD^1 | grep -v "third_party" | grep -v ".gitmodules" | grep -r "build/")" ]; then
   LABELS="${LABELS}only_third_party,"
+fi
+
+VALIDATOR_WARN_FILES="$(git show --name-only "${LAST_USER_COMMIT}" | grep -v ".gitmodules" | grep -v "build/" | grep -Ff '.ci/magic-modules/vars/validator_handwritten_files.txt' | sed 's/^/* /')"
+if [ -n "${VALIDATOR_WARN_FILES}" ]; then
+  MESSAGE="${MESSAGE}${NEWLINE}**WARNING**: The following files changed in commit ${LAST_USER_COMMIT} may need corresponding changes in third_party/validator:"
+  MESSAGE="${MESSAGE}${NEWLINE}${VALIDATOR_WARN_FILES}${NEWLINE}"
 fi
 
 # Terraform
