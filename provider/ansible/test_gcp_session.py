2220e117699212de44806d03b1e6879a30bcb586
Alex Stephen <alexstephen@google.com>
2019-08-15 12:26:14 -0700

    all tests

--- provider/ansible/test_gcp_session.py
+++ provider/ansible/test_gcp_session.py
@@ -74,8 +74,7 @@ class GcpSessionTestCase(unittest.TestCase):
 
     @responses.activate
     def test_post(self):
-        url = 'http://www.googleapis.com/compute/test_instance'
-        responses.add(responses.POST, url,
+        responses.add(responses.POST, self.url,
                       status=200, json=self.success_json)
 
         with self.setup_auth():
@@ -83,13 +82,70 @@ class GcpSessionTestCase(unittest.TestCase):
             module = FakeModule({ 'scopes': 'foo', 'service_account_file': 'file_name', 'project': 'test_project', 'auth_kind': 'serviceaccount'})
 
             session = GcpSession(module, 'mock')
-            resp = session.post(url, body=body, headers={'x-added-header': 'my-header'})
+            resp = session.post(self.url, body=body, headers={'x-added-header': 'my-header'})
 
             # Ensure Google header added.
             assert responses.calls[0].request.headers['User-Agent'] == self.user_agent
 
             # Ensure all content was passed along.
             assert responses.calls[0].request.headers['x-added-header'] == 'my-header'
+
+            # Ensure proper request was made.
+            assert resp.json() == self.success_json
+            assert resp.status_code == 200
+
+    @responses.activate
+    def test_delete(self):
+        responses.add(responses.DELETE, self.url,
+                      status=200, json=self.success_json)
+
+        with self.setup_auth():
+            body = {'content': 'some_content'} 
+            module = FakeModule({ 'scopes': 'foo', 'service_account_file': 'file_name', 'project': 'test_project', 'auth_kind': 'serviceaccount'})
+
+            session = GcpSession(module, 'mock')
+            resp = session.delete(self.url)
+
+            # Ensure Google header added.
+            assert responses.calls[0].request.headers['User-Agent'] == self.user_agent
+
+            # Ensure proper request was made.
+            assert resp.json() == self.success_json
+            assert resp.status_code == 200
+
+    @responses.activate
+    def test_put(self):
+        responses.add(responses.PUT, self.url,
+                      status=200, json=self.success_json)
+
+        with self.setup_auth():
+            body = {'content': 'some_content'} 
+            module = FakeModule({ 'scopes': 'foo', 'service_account_file': 'file_name', 'project': 'test_project', 'auth_kind': 'serviceaccount'})
+
+            session = GcpSession(module, 'mock')
+            resp = session.put(self.url, body={'foo': 'bar'})
+
+            # Ensure Google header added.
+            assert responses.calls[0].request.headers['User-Agent'] == self.user_agent
+
+            # Ensure proper request was made.
+            assert resp.json() == self.success_json
+            assert resp.status_code == 200
+
+    @responses.activate
+    def test_put(self):
+        responses.add(responses.PATCH, self.url,
+                      status=200, json=self.success_json)
+
+        with self.setup_auth():
+            body = {'content': 'some_content'} 
+            module = FakeModule({ 'scopes': 'foo', 'service_account_file': 'file_name', 'project': 'test_project', 'auth_kind': 'serviceaccount'})
+
+            session = GcpSession(module, 'mock')
+            resp = session.patch(self.url, body={'foo': 'bar'})
+
+            # Ensure Google header added.
+            assert responses.calls[0].request.headers['User-Agent'] == self.user_agent
 
             # Ensure proper request was made.
             assert resp.json() == self.success_json
