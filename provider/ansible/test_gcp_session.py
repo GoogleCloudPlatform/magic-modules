173175b7433ae0d896be2ebe8444eb19a6f6e5c8
Alex Stephen <alexstephen@google.com>
2019-08-15 12:27:50 -0700

    formatting

--- provider/ansible/test_gcp_session.py
+++ provider/ansible/test_gcp_session.py
@@ -30,18 +30,19 @@ importorskip("responses")
 
 from google.auth.credentials import AnonymousCredentials
 
+
 class FakeModule(object):
     def __init__(self, params):
         self.params = params
 
     def fail_json(self, **kwargs):
-        raise kwargs['msg']
+        raise kwargs["msg"]
 
 
 class GcpSessionTestCase(unittest.TestCase):
-    success_json = {'status': 'SUCCESS'}
-    user_agent = 'Google-Ansible-MM-mock'
-    url = 'http://www.googleapis.com/compute/test_instance'
+    success_json = {"status": "SUCCESS"}
+    user_agent = "Google-Ansible-MM-mock"
+    url = "http://www.googleapis.com/compute/test_instance"
 
     @contextmanager
     def setup_auth(self):
@@ -50,8 +51,12 @@ class GcpSessionTestCase(unittest.TestCase):
         the google-auth library and uses the built-in
         AnonymousCredentials for sending requests.
         """
-        with patch('google.oauth2.service_account.Credentials.from_service_account_file') as mock:
-            with patch.object (AnonymousCredentials, 'with_scopes', create=True) as mock2:
+        with patch(
+            "google.oauth2.service_account.Credentials.from_service_account_file"
+        ) as mock:
+            with patch.object(
+                AnonymousCredentials, "with_scopes", create=True
+            ) as mock2:
                 creds = AnonymousCredentials()
                 mock2.return_value = creds
                 mock.return_value = creds
@@ -59,36 +64,50 @@ class GcpSessionTestCase(unittest.TestCase):
 
     @responses.activate
     def test_get(self):
-        responses.add(responses.GET, self.url,
-                      status=200, json=self.success_json)
+        responses.add(responses.GET, self.url, status=200, json=self.success_json)
 
         with self.setup_auth():
-            module = FakeModule({ 'scopes': 'foo', 'service_account_file': 'file_name', 'project': 'test_project', 'auth_kind': 'serviceaccount'})
+            module = FakeModule(
+                {
+                    "scopes": "foo",
+                    "service_account_file": "file_name",
+                    "project": "test_project",
+                    "auth_kind": "serviceaccount",
+                }
+            )
 
-            session = GcpSession(module, 'mock')
+            session = GcpSession(module, "mock")
             resp = session.get(self.url)
 
-            assert responses.calls[0].request.headers['User-Agent'] == self.user_agent
+            assert responses.calls[0].request.headers["User-Agent"] == self.user_agent
             assert resp.json() == self.success_json
             assert resp.status_code == 200
 
     @responses.activate
     def test_post(self):
-        responses.add(responses.POST, self.url,
-                      status=200, json=self.success_json)
+        responses.add(responses.POST, self.url, status=200, json=self.success_json)
 
         with self.setup_auth():
-            body = {'content': 'some_content'} 
-            module = FakeModule({ 'scopes': 'foo', 'service_account_file': 'file_name', 'project': 'test_project', 'auth_kind': 'serviceaccount'})
+            body = {"content": "some_content"}
+            module = FakeModule(
+                {
+                    "scopes": "foo",
+                    "service_account_file": "file_name",
+                    "project": "test_project",
+                    "auth_kind": "serviceaccount",
+                }
+            )
 
-            session = GcpSession(module, 'mock')
-            resp = session.post(self.url, body=body, headers={'x-added-header': 'my-header'})
+            session = GcpSession(module, "mock")
+            resp = session.post(
+                self.url, body=body, headers={"x-added-header": "my-header"}
+            )
 
             # Ensure Google header added.
-            assert responses.calls[0].request.headers['User-Agent'] == self.user_agent
+            assert responses.calls[0].request.headers["User-Agent"] == self.user_agent
 
             # Ensure all content was passed along.
-            assert responses.calls[0].request.headers['x-added-header'] == 'my-header'
+            assert responses.calls[0].request.headers["x-added-header"] == "my-header"
 
             # Ensure proper request was made.
             assert resp.json() == self.success_json
@@ -96,18 +115,24 @@ class GcpSessionTestCase(unittest.TestCase):
 
     @responses.activate
     def test_delete(self):
-        responses.add(responses.DELETE, self.url,
-                      status=200, json=self.success_json)
+        responses.add(responses.DELETE, self.url, status=200, json=self.success_json)
 
         with self.setup_auth():
-            body = {'content': 'some_content'} 
-            module = FakeModule({ 'scopes': 'foo', 'service_account_file': 'file_name', 'project': 'test_project', 'auth_kind': 'serviceaccount'})
+            body = {"content": "some_content"}
+            module = FakeModule(
+                {
+                    "scopes": "foo",
+                    "service_account_file": "file_name",
+                    "project": "test_project",
+                    "auth_kind": "serviceaccount",
+                }
+            )
 
-            session = GcpSession(module, 'mock')
+            session = GcpSession(module, "mock")
             resp = session.delete(self.url)
 
             # Ensure Google header added.
-            assert responses.calls[0].request.headers['User-Agent'] == self.user_agent
+            assert responses.calls[0].request.headers["User-Agent"] == self.user_agent
 
             # Ensure proper request was made.
             assert resp.json() == self.success_json
@@ -115,37 +140,49 @@ class GcpSessionTestCase(unittest.TestCase):
 
     @responses.activate
     def test_put(self):
-        responses.add(responses.PUT, self.url,
-                      status=200, json=self.success_json)
+        responses.add(responses.PUT, self.url, status=200, json=self.success_json)
 
         with self.setup_auth():
-            body = {'content': 'some_content'} 
-            module = FakeModule({ 'scopes': 'foo', 'service_account_file': 'file_name', 'project': 'test_project', 'auth_kind': 'serviceaccount'})
+            body = {"content": "some_content"}
+            module = FakeModule(
+                {
+                    "scopes": "foo",
+                    "service_account_file": "file_name",
+                    "project": "test_project",
+                    "auth_kind": "serviceaccount",
+                }
+            )
 
-            session = GcpSession(module, 'mock')
-            resp = session.put(self.url, body={'foo': 'bar'})
+            session = GcpSession(module, "mock")
+            resp = session.put(self.url, body={"foo": "bar"})
 
             # Ensure Google header added.
-            assert responses.calls[0].request.headers['User-Agent'] == self.user_agent
+            assert responses.calls[0].request.headers["User-Agent"] == self.user_agent
 
             # Ensure proper request was made.
             assert resp.json() == self.success_json
             assert resp.status_code == 200
 
     @responses.activate
-    def test_put(self):
-        responses.add(responses.PATCH, self.url,
-                      status=200, json=self.success_json)
+    def test_patch(self):
+        responses.add(responses.PATCH, self.url, status=200, json=self.success_json)
 
         with self.setup_auth():
-            body = {'content': 'some_content'} 
-            module = FakeModule({ 'scopes': 'foo', 'service_account_file': 'file_name', 'project': 'test_project', 'auth_kind': 'serviceaccount'})
+            body = {"content": "some_content"}
+            module = FakeModule(
+                {
+                    "scopes": "foo",
+                    "service_account_file": "file_name",
+                    "project": "test_project",
+                    "auth_kind": "serviceaccount",
+                }
+            )
 
-            session = GcpSession(module, 'mock')
-            resp = session.patch(self.url, body={'foo': 'bar'})
+            session = GcpSession(module, "mock")
+            resp = session.patch(self.url, body={"foo": "bar"})
 
             # Ensure Google header added.
-            assert responses.calls[0].request.headers['User-Agent'] == self.user_agent
+            assert responses.calls[0].request.headers["User-Agent"] == self.user_agent
 
             # Ensure proper request was made.
             assert resp.json() == self.success_json
