11e69f03ad6a8e3765e7d44cee58cd71f3408f81
Alex Stephen <alexstephen@google.com>
2019-08-15 11:10:39 -0700

    Ansible Cloud Functions (#2182)
    Merged PR #2182.

--- products/cloudfunctions/helpers/ansible/function_transport.py
+++ products/cloudfunctions/helpers/ansible/function_transport.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Google Inc.
+# Copyright 2019 Google Inc.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -10,13 +10,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Verify that firewall network names match what the API returns
 
---- !ruby/object:Provider::Inspec::Config
-overrides: !ruby/object:Overrides::ResourceOverrides
-  CloudFunction: !ruby/object:Overrides::Inspec::ResourceOverride
-    properties:
-      trigger_http: !ruby/object:Overrides::Inspec::PropertyOverride
-        exclude: true
-files: !ruby/object:Provider::Config::Files
-  copy:
-    'Gemfile': 'provider/inspec/Gemfile'
+def encode_request(request, module):
+    return_vals = {}
+    for k, v in request.items():
+        if v or v is False:
+            return_vals[k] = v
+
+    if module.params['trigger_http'] and not return_vals.get('httpsTrigger'):
+        return_vals['httpsTrigger'] = {}
+
+    return return_vals
