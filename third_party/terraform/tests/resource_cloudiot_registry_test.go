2c6c4206d0ac40acc2e18e715e1676763e489a62
Emily Ye <emilyye@google.com>
2019-08-19 10:47:29 -0700

    move validation test

--- third_party/terraform/tests/resource_cloudiot_registry_test.go
+++ third_party/terraform/tests/resource_cloudiot_registry_test.go
@@ -10,6 +10,30 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 )
 
+func TestValidateCloudIoTID(t *testing.T) {
+	x := []StringValidationTestCase{
+		// No errors
+		{TestName: "basic", Value: "foobar"},
+		{TestName: "with numbers", Value: "foobar123"},
+		{TestName: "short", Value: "foo"},
+		{TestName: "long", Value: "foobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoo"},
+		{TestName: "has a hyphen", Value: "foo-bar"},
+
+		// With errors
+		{TestName: "empty", Value: "", ExpectError: true},
+		{TestName: "starts with a goog", Value: "googfoobar", ExpectError: true},
+		{TestName: "starts with a number", Value: "1foobar", ExpectError: true},
+		{TestName: "has an slash", Value: "foo/bar", ExpectError: true},
+		{TestName: "has an backslash", Value: "foo\bar", ExpectError: true},
+		{TestName: "too long", Value: strings.Repeat("f", 260), ExpectError: true},
+	}
+
+	es := testStringValidationCases(x, validateCloudIotID)
+	if len(es) > 0 {
+		t.Errorf("Failed to validate CloudIoT ID names: %v", es)
+	}
+}
+
 func TestCloudIotRegistryStateUpgradeV0(t *testing.T) {
 	t.Parallel()
 
