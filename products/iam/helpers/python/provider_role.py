ff71f5ca6404e4e6a0670ae55267f53916c5bc94
Alex Stephen <alexstephen@google.com>
2019-08-21 14:34:05 -0700

    some decoders switched over

--- products/iam/helpers/python/provider_role.py
+++ products/iam/helpers/python/provider_role.py
@@ -5,8 +5,3 @@ def resource_to_create(module):
         'roleId': module.params['name'],
         'role': role
     }
-
-def decode_response(response, module):
-    if 'name' in response:
-        response['name'] = response['name'].split('/')[-1]
-    return response
--- templates/ansible/decoders/full_name_to_short_name.py
+++ templates/ansible/decoders/full_name_to_short_name.py
@@ -10,10 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# Short names are given (and expected) by the API
-# but are returned as full names.
-def decode_response(response, module):
-    if 'name' in response and 'metadata' not in response:
+def full_name_to_short(response, module):
+    if 'name' in response:
         response['name'] = response['name'].split('/')[-1]
     return response
