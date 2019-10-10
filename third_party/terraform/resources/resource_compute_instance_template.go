efd1acc49345b177241de2a1ad3de15e3bd7ff8e
Megan Bang <mbang@hashicorp.com>
2019-10-10 11:31:08 -0500

    change single optional fields to required

--- third_party/terraform/resources/resource_compute_instance_template.go
+++ third_party/terraform/resources/resource_compute_instance_template.go
@@ -156,7 +156,7 @@ func resourceComputeInstanceTemplate() *schema.Resource {
 								Schema: map[string]*schema.Schema{
 									"kms_key_self_link": {
 										Type:             schema.TypeString,
-										Optional:         true,
+										Required:         true,
 										ForceNew:         true,
 										DiffSuppressFunc: compareSelfLinkRelativePaths,
 									},
