6cba3626165a1965365b4c1c562726baca510f01
Tone <t.proum@gmail.com>
2019-10-14 15:57:22 +0200

    Add a terraform test case for google pubsub push subscriptions

--- third_party/terraform/tests/resource_pubsub_subscription_test.go
+++ third_party/terraform/tests/resource_pubsub_subscription_test.go
@@ -105,6 +105,32 @@ func TestAccPubsubSubscription_update(t *testing.T) {
 	})
 }
 
+func TestAccPubsubSubscription_push(t *testing.T) {
+	t.Parallel()
+
+	topicFoo := fmt.Sprintf("tf-test-topic-foo-%s", acctest.RandString(10))
+	subscription := fmt.Sprintf("projects/%s/subscriptions/tf-test-topic-foo-%s", getTestProjectFromEnv(), acctest.RandString(10))
+
+	topicBar := fmt.Sprintf("tf-test-topic-bar-%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckPubsubSubscriptionDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPubsubSubscription_push(topicFoo, topicBar, subscription),
+			},
+			{
+				ResourceName:      "google_pubsub_subscription.foo",
+				ImportStateId:     subscription,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func testAccPubsubSubscription_emptyTTL(topic, subscription string) string {
 	return fmt.Sprintf(`
 resource "google_pubsub_topic" "foo" {
@@ -135,6 +161,44 @@ resource "google_pubsub_subscription" "foo" {
 // An easy way to test this would be to create an App Engine Hello World app. With AppEngine, SSL certificate, DNS and domain registry is handled for us.
 // App Engine is not yet supported by Terraform but once it is, it will provide an easy path to testing push configs.
 // Another option would be to use Cloud Functions once Terraform support is added.
+func testAccPubsubSubscription_push(topicFoo string, topicBar string, subscription string) string {
+	return fmt.Sprintf(`
+resource "google_pubsub_topic" "foo" {
+  name = "%s"
+}
+
+resource "google_pubsub_topic" "bar" {
+  name = "%s"
+}
+
+resource "google_service_account" "service_account" {
+  account_id = "my-super-service"
+}
+
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/projects.topics.publish"
+
+    members = [
+      "serviceAccount:${google_service_account.service_account.email}",
+    ]
+  }
+}
+
+resource "google_pubsub_subscription" "foo" {
+  name                 = "%s"
+  topic                = "${google_pubsub_topic.foo.id}"
+  ack_deadline_seconds = 3
+  push_config {
+    push_endpoint = "${google_pubsub_topic.bar.name}"
+    oidcToken {
+      serviceAccountEmail = "${google_service_account.service_account.email}"
+    }
+  }
+}
+`, topicFoo, topicBar, subscription)
+}
+
 
 func testAccPubsubSubscription_fullName(topic, subscription, label string, deadline int) string {
 	return fmt.Sprintf(`
