3c07808c72a8f845c51618a0ea92877f150da2d1
Patel, Prakash P <prakash.p.patel@accenture.com>
2019-10-16 14:43:26 -0400

    removed gitc.sh file

--- gitc.sh
+++
@@ -1,8 +0,0 @@
-git add products/spanner/inspec.yaml
-git add templates/inspec/examples/google_spanner_instance/google_spanner_instance.erb
-git add templates/inspec/examples/google_spanner_instance/google_spanner_instance_attributes.erb
-git add templates/inspec/examples/google_spanner_instance/google_spanner_instances.erb
-git add templates/inspec/tests/integration/build/gcp-mm.tf
-git add templates/inspec/tests/integration/configuration/mm-attributes.yml
-
-
