36d03e147b7e430dfc5d0c525005c1006d61d2e2
Patel, Prakash P <prakash.p.patel@accenture.com>
2019-10-23 16:12:20 -0400

    fixed space issue

--- templates/inspec/tests/integration/build/gcp-mm.tf
+++ templates/inspec/tests/integration/build/gcp-mm.tf
@@ -201,7 +201,7 @@ variable "service" {
   type = "map"
 }
 
-variable "spannerinstance " {
+variable "spannerinstance" {
   type = "map"
 }
 
@@ -880,7 +880,7 @@ resource "google_spanner_instance" "spanner_instance" {
   config       = "${var.spannerinstance["config"]}"
   name         = "${var.spannerinstance["name"]}"
   display_name = "${var.spannerinstance["display_name"]}"
-  num_nodes    = "${var.spannerinstance["node_count"]}"
+  num_nodes    = "${var.spannerinstance["num_nodes"]}"
   labels = {
     "${var.spannerinstance["label_key"]}" = "${var.spannerinstance["label_value"]}"
   }
