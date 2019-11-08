2832ba242bd48beee7e1e436552412212ff5c9a5
Sam Levenick <slevenick@google.com>
2019-11-08 10:56:04 -0800

    (r)igm imports for customdiff

--- third_party/terraform/resources/resource_compute_instance_group_manager.go
+++ third_party/terraform/resources/resource_compute_instance_group_manager.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/hashicorp/terraform-plugin-sdk/helper/customdiff"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
