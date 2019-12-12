c3ac76c59c3b024f208c58e3016f0252753c61d5
Emily Ye <emilyye@google.com>
2019-12-11 16:49:01 -0800

    rename template file

--- third_party/terraform/tests/resource_deployment_manager_deployment_test.go
+++ third_party/terraform/tests/resource_deployment_manager_deployment_test.go
@@ -32,7 +32,7 @@ func TestAccDeploymentManagerDeployment_basicFile(t *testing.T) {
 	randSuffix := acctest.RandString(10)
 	deploymentId := "tf-dm-" + randSuffix
 	accountId := "tf-dm-account-" + randSuffix
-	yamlPath := createYamlConfigFileForTest(t, "test-fixtures/deploymentmanager/service_account.yaml", map[string]interface{}{
+	yamlPath := createYamlConfigFileForTest(t, "test-fixtures/deploymentmanager/service_account.yml.tmpl", map[string]interface{}{
 		"account_id": accountId,
 	})
 
@@ -107,7 +107,7 @@ func TestAccDeploymentManagerDeployment_imports(t *testing.T) {
 	randStr := acctest.RandString(10)
 	deploymentName := "tf-dm-" + randStr
 	accountId := "tf-dm-" + randStr
-	importFilepath := createYamlConfigFileForTest(t, "test-fixtures/deploymentmanager/service_account.yaml", map[string]interface{}{
+	importFilepath := createYamlConfigFileForTest(t, "test-fixtures/deploymentmanager/service_account.yml.tmpl", map[string]interface{}{
 		"account_id": "{{ env['name'] }}",
 	})
 
