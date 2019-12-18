67d6598f7c945816cf8b349f364a45c11f5faa52
Sam Levenick <slevenick@google.com>
2019-12-18 15:39:44 -0800

    Add handwritten update tests

--- third_party/terraform/tests/resource_identity_platform_default_supported_idp_config_test.go
+++ third_party/terraform/tests/resource_identity_platform_default_supported_idp_config_test.go
@@ -1,13 +1,10 @@
 package google
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func TestAccIdentityPlatformDefaultSupportedIdpConfig_defaultSupportedIdpConfigUpdate(t *testing.T) {
@@ -40,31 +37,6 @@ func TestAccIdentityPlatformDefaultSupportedIdpConfig_defaultSupportedIdpConfigU
 			},
 		},
 	})
-}
-
-func testAccCheckIdentityPlatformDefaultSupportedIdpConfigDestroy(s *terraform.State) error {
-	for name, rs := range s.RootModule().Resources {
-		if rs.Type != "google_identity_platform_default_supported_idp_config" {
-			continue
-		}
-		if strings.HasPrefix(name, "data.") {
-			continue
-		}
-
-		config := testAccProvider.Meta().(*Config)
-
-		url, err := replaceVarsForTest(config, rs, "{{IdentityPlatformBasePath}}projects/{{project}}/defaultSupportedIdpConfigs/{{client_id}}")
-		if err != nil {
-			return err
-		}
-
-		_, err = sendRequest(config, "GET", "", url, nil)
-		if err == nil {
-			return fmt.Errorf("IdentityPlatformDefaultSupportedIdpConfig still exists at %s", url)
-		}
-	}
-
-	return nil
 }
 
 func testAccIdentityPlatformDefaultSupportedIdpConfig_defaultSupportedIdpConfigBasic(context map[string]interface{}) string {
