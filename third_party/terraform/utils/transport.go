91272dc82652e61b2b4c9550e8b25cfd28949e6b
Dana Hoffman <danahoffman@google.com>
2020-01-02 14:16:55 -0800

    update isEmptyValue function

--- third_party/terraform/utils/transport.go
+++ third_party/terraform/utils/transport.go
@@ -18,21 +18,7 @@ import (
 var DefaultRequestTimeout = 5 * time.Minute
 
 func isEmptyValue(v reflect.Value) bool {
-	switch v.Kind() {
-	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
-		return v.Len() == 0
-	case reflect.Bool:
-		return !v.Bool()
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return v.Int() == 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return v.Uint() == 0
-	case reflect.Float32, reflect.Float64:
-		return v.Float() == 0
-	case reflect.Interface, reflect.Ptr:
-		return v.IsNil()
-	}
-	return false
+	return !v.IsValid() || v.IsZero()
 }
 
 func sendRequest(config *Config, method, project, rawurl string, body map[string]interface{}, errorRetryPredicates ...RetryErrorPredicateFunc) (map[string]interface{}, error) {
