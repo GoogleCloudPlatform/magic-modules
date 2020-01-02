99eb951856d4ed3a417f026dc235531f1f686a5c
Dana Hoffman <danahoffman@google.com>
2020-01-02 14:54:20 -0800

    never mind, we still use 1.12 in the magician and teamcity

--- third_party/terraform/utils/transport.go
+++ third_party/terraform/utils/transport.go
@@ -18,7 +18,25 @@ import (
 var DefaultRequestTimeout = 5 * time.Minute
 
 func isEmptyValue(v reflect.Value) bool {
-	return !v.IsValid() || v.IsZero()
+	if !v.IsValid() {
+		return true
+	}
+
+	switch v.Kind() {
+	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
+		return v.Len() == 0
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Interface, reflect.Ptr:
+		return v.IsNil()
+	}
+	return false
 }
 
 func sendRequest(config *Config, method, project, rawurl string, body map[string]interface{}, errorRetryPredicates ...RetryErrorPredicateFunc) (map[string]interface{}, error) {
