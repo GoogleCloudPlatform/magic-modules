012c57b372fa8a33997c9ef81a11a4cce947436a
Nathan McKinley <nmckinley@google.com>
2020-01-06 15:13:00 -0800

    Add Downstream pusher on Google Cloud Build (#2876)
    Merged PR #2876.

--- .ci/containers/downstream-builder/generate_downstream.sh
+++ .ci/containers/downstream-builder/generate_downstream.sh
@@ -3,41 +3,45 @@
 set -e
 
 function clone_repo() {
+    SCRATCH_OWNER=modular-magician
     if [ "$REPO" == "terraform" ]; then
         if [ "$VERSION" == "ga" ]; then
-            GITHUB_PATH=https://modular-magician:$GITHUB_TOKEN@github.com/terraform-providers/terraform-provider-google
-            SCRATCH_PATH=https://modular-magician:$GITHUB_TOKEN@github.com/modular-magician/terraform-provider-google
+            UPSTREAM_OWNER=terraform-providers
+            GH_REPO=terraform-provider-google
             LOCAL_PATH=$GOPATH/src/github.com/terraform-providers/terraform-provider-google
         elif [ "$VERSION" == "beta" ]; then
-            GITHUB_PATH=https://modular-magician:$GITHUB_TOKEN@github.com/terraform-providers/terraform-provider-google-beta
-            SCRATCH_PATH=https://modular-magician:$GITHUB_TOKEN@github.com/modular-magician/terraform-provider-google-beta
+            UPSTREAM_OWNER=terraform-providers
+            GH_REPO=terraform-provider-google-beta
             LOCAL_PATH=$GOPATH/src/github.com/terraform-providers/terraform-provider-google-beta
         else
             echo "Unrecognized version $VERSION"
             exit 1
         fi
     elif [ "$REPO" == "tf-conversion" ]; then
-        GITHUB_PATH=https://modular-magician:$GITHUB_TOKEN@github.com/GoogleCloudPlatform/terraform-google-conversion
-        SCRATCH_PATH=https://modular-magician:$GITHUB_TOKEN@github.com/modular-magician/terraform-google-conversion
+        UPSTREAM_OWNER=GoogleCloudPlatform
+        GH_REPO=terraform-google-conversion
         LOCAL_PATH=$GOPATH/src/github.com/GoogleCloudPlatform/terraform-google-conversion
     elif [ "$REPO" == "ansible" ]; then
-        GITHUB_PATH=https://modular-magician:$GITHUB_TOKEN@github.com/ansible-collections/ansible_collections_google
-        SCRATCH_PATH=https://modular-magician:$GITHUB_TOKEN@github.com/modular-magician/ansible_collections_google
+        UPSTREAM_OWNER=ansible-collections
+        GH_REPO=ansible_collections_google
         LOCAL_PATH=$PWD/../ansible
     elif [ "$REPO" == "inspec" ]; then
-        GITHUB_PATH=https://modular-magician:$GITHUB_TOKEN@github.com/modular-magician/inspec-gcp
-        SCRATCH_PATH=$GITHUB_PATH
+        UPSTREAM_OWNER=modular-magician
+        GH_REPO=inspec-gcp
         LOCAL_PATH=$PWD/../inspec
     else
         echo "Unrecognized repo $REPO"
         exit 1
     fi
+
+    GITHUB_PATH=https://modular-magician:$GITHUB_TOKEN@github.com/$UPSTREAM_OWNER/$GH_REPO
+    SCRATCH_PATH=https://modular-magician:$GITHUB_TOKEN@github.com/$SCRATCH_OWNER/$GH_REPO
     mkdir -p "$(dirname $LOCAL_PATH)"
     git clone $GITHUB_PATH $LOCAL_PATH
 }
 
 if [ $# -lt 4 ]; then
-    echo "Usage: $0 (build|diff) (terraform|tf-conversion|ansible|inspec) (ga|beta) (pr number)"
+    echo "Usage: $0 (build|diff|downstream) (terraform|tf-conversion|ansible|inspec) (ga|beta) (pr number|sha)"
     exit 1
 fi
 if [ -z "$GITHUB_TOKEN" ]; then
@@ -48,7 +52,7 @@ fi
 COMMAND=$1
 REPO=$2
 VERSION=$3
-PR_NUMBER=$4
+REFERENCE=$4
 
 clone_repo
 
@@ -56,10 +60,15 @@ git config --global user.name "Modular Magician"
 git config --global user.email "magic-modules@google.com"
 
 if [ "$COMMAND" == "head" ]; then
-    BRANCH=auto-pr-$PR_NUMBER
+    BRANCH=auto-pr-$REFERENCE
+    COMMIT_MESSAGE="New generated code for MM PR $REFERENCE."
 elif [ "$COMMAND" == "base" ]; then
     git checkout HEAD~
-    BRANCH=auto-pr-$PR_NUMBER-old
+    BRANCH=auto-pr-$REFERENCE-old
+    COMMIT_MESSAGE="Old generated code for MM PR $REFERENCE."
+elif [ "$COMMAND" == "downstream" ]; then
+    BRANCH=downstream-pr-$REFERENCE
+    COMMIT_MESSAGE="$(git log -1 --pretty=%B "$REFERENCE")"
 fi
 
 if [ "$REPO" == "terraform" ]; then
@@ -75,10 +84,50 @@ else
     bundle exec compiler -a -e $REPO -o $LOCAL_PATH -v $VERSION
 fi
 
-
 pushd $LOCAL_PATH
 git add .
 git checkout -b $BRANCH
-git commit -m "New generated code for PR $PR_NUMBER." || true
+
+COMMITTED=true
+git commit -m "$COMMIT_MESSAGE" || COMMITTED=false
+
+if [ "$COMMITTED" == "true" ] && [ "$COMMAND" == "downstream" ]; then
+    # Add the changelog entry!
+    PR_NUMBER=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
+        "https://api.github.com/repos/GoogleCloudPlatform/magic-modules/pulls?state=closed&base=master&sort=updated&direction=desc" | \
+        jq -r ".[] | if .merge_commit_sha == \"$REFERENCE\" then .number else empty end")
+    mkdir -p .changelog/
+    curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
+        "https://api.github.com/repos/GoogleCloudPlatform/magic-modules/pulls/$PR_NUMBER" | \
+        jq -r .body | \
+        sed -e '/```release-note/,/```/!d' \
+        > .changelog/$PR_NUMBER.txt
+    git add .changelog
+    git commit --amend --no-edit
+fi
+
 git push $SCRATCH_PATH $BRANCH -f
+
+if [ "$COMMITTED" == "true" ] && [ "$COMMAND" == "downstream" ]; then
+    PR_BODY=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
+        "https://api.github.com/repos/GoogleCloudPlatform/magic-modules/pulls/$PR_NUMBER" | \
+        jq -r .body)
+    PR_TITLE=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
+        "https://api.github.com/repos/GoogleCloudPlatform/magic-modules/pulls/$PR_NUMBER" | \
+        jq -r .title)
+
+    NEW_PR_URL=$(hub pull-request -b $UPSTREAM_OWNER:downstream-master -h $SCRATCH_OWNER:$BRANCH -m "$PR_TITLE" -m "$PR_BODY")
+    if [ $? != 0 ]; then
+        exit $?
+    fi
+    NEW_PR_NUMBER=$(echo $NEW_PR_URL | awk -F '/' '{print $NF}')
+
+    # Wait a few seconds, then merge the PR.
+    sleep 5
+    curl -H "Authorization: token ${GITHUB_TOKEN}" \
+        -X PUT \
+        -d '{"merge_method": "squash"}' \
+        "https://api.github.com/repos/$UPSTREAM_OWNER/$GH_REPO/pulls/$NEW_PR_NUMBER/merge"
+fi
+
 popd
