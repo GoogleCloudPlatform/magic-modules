830044645abc9a2444f8abdb834d91071b9688c6
Alex Stephen <alexstephen@google.com>
2020-02-05 13:57:44 -0800

    bigtable things

--- products/bigtable/helpers/ansible/instance.py
+++ products/bigtable/helpers/ansible/instance.py
@@ -1,7 +1,10 @@
 def resource_to_create(module):
     instance = resource_to_request(module)
+    if 'name' in instance:
+        del instance['name']
+
     return {
-        'instanceId': module.params['name'],
+        'instanceId': module.params['name'].split('/')[-1],
         'instance': instance
     }
 
