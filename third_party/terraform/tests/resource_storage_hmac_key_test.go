f54b5cb70582f3ac9892388272208f8a6825f622
eric-hole <eric.hole@gmail.com>
2020-02-10 11:14:25 -0800

    Adds test and fixes hmac_key id

---
+++ third_party/terraform/tests/resource_storage_hmac_key_test.go
@@ -0,0 +1,68 @@
+package google
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestStorageHmacKey_update(t *testing.T) {
+	t.Parallel()
+
+	saName := saName()
+	bucketName := testBucketName()
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckStorageHmacKeyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testGoogleStorageHmacKeyBasic(saName, bucketName),
+			},
+			{
+				ResourceName:      "google_storage_hmac_key.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func saName() string {
+	return fmt.Sprintf("", "tf-test-bucket", acctest.RandInt())
+}
+
+func testAccStorageHmacKeyDestroy(s *terraform.State) error {
+	config := testAccProvider.Meta().(*Config)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "google_storage_hmac_key" {
+			continue
+		}
+		accessId := rs.Primary.Attributes["accessId"]
+
+		_, err := config.clientStorage.HmacKeys.Get(accessId).Do()
+		if err == nil {
+			return fmt.Errorf("Hmac key still exists.")
+		}
+	}
+
+	return nil
+}
+
+func testGoogleStorageHmacKeyBasic(saName, bucketName) string {
+	return fmt.Sprintf(`
+resource "google_service_account" "service_account" {
+  name = "%s"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name = "%s"
+}
+
+resource "google_storage_hmac_key" "key" {
+	service_account_email = google_service_account.service_account.email
+}
+`, saName, bucketName)
+}
