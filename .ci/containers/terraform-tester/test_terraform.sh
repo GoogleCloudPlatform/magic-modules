143ee2c181b9af2b34ac6ba831e6265aa9d63c98
Nathan McKinley <nmckinley@google.com>
2020-02-10 14:57:50 -0800

    Code cleanliness and speed improvement.

--- .ci/containers/terraform-tester/test_terraform.sh
+++ .ci/containers/terraform-tester/test_terraform.sh
@@ -7,16 +7,15 @@ REFERENCE=$2
 SCRATCH_OWNER=modular-magician
 if [ "$REPO" == "ga" ]; then
     GH_REPO=terraform-provider-google
-    LOCAL_PATH=$GOPATH/src/github.com/terraform-providers/terraform-provider-google
 elif [ "$REPO" == "beta" ]; then
     GH_REPO=terraform-provider-google-beta
-    LOCAL_PATH=$GOPATH/src/github.com/terraform-providers/terraform-provider-google-beta
 else
     echo "no repo, dying."
     exit 1
 fi
 
-SCRATCH_PATH=https://modular-magician:$GITHUB_TOKEN@github.com/$SCRATCH_OWNER/$GH_REPO
+SCRATCH_PATH=https://$SCRATCH_OWNER:$GITHUB_TOKEN@github.com/$SCRATCH_OWNER/$GH_REPO
+LOCAL_PATH=$GOPATH/src/github.com/terraform-providers/$GH_REPO
 mkdir -p "$(dirname $LOCAL_PATH)"
 git clone $SCRATCH_PATH $LOCAL_PATH --single-branch --branch "auto-pr-$REFERENCE" --depth 1
 pushd $LOCAL_PATH
@@ -24,5 +23,4 @@ pushd $LOCAL_PATH
 make tools
 make docscheck
 make lint
-make
 make test
