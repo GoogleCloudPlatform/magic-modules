6fd5a4e848a13ff210d52c51c21859ef3c6b76ef
Chris Sng <chrissng@users.noreply.github.com>
2020-02-10 15:41:05 +0800

    merge upstream changes to sql

--- third_party/terraform/resources/resource_sql_user.go
+++ third_party/terraform/resources/resource_sql_user.go
@@ -179,12 +179,8 @@ func resourceSqlUserUpdate(d *schema.ResourceData, meta interface{}) error {
 		defer mutexKV.Unlock(instanceMutexKey(project, instance))
 		var op *sqladmin.Operation
 		updateFunc := func() error {
-<<<<<<< HEAD
 			op, err = config.clientSqlAdmin.Users.Update(project, instance,
 				user).Host(host).Do()
-=======
-			op, err = config.clientSqlAdmin.Users.Update(project, instance, user).Host(host).Name(name).Do()
->>>>>>> fix sqladmin api usage
 			return err
 		}
 		err = retryTimeDuration(updateFunc, d.Timeout(schema.TimeoutUpdate))
@@ -223,11 +219,7 @@ func resourceSqlUserDelete(d *schema.ResourceData, meta interface{}) error {
 
 	var op *sqladmin.Operation
 	err = retryTimeDuration(func() error {
-<<<<<<< HEAD
 		op, err = config.clientSqlAdmin.Users.Delete(project, instance).Do()
-=======
-		op, err = config.clientSqlAdmin.Users.Delete(project, instance).Host(host).Name(name).Do()
->>>>>>> fix sqladmin api usage
 		return err
 	}, d.Timeout(schema.TimeoutDelete))
 
