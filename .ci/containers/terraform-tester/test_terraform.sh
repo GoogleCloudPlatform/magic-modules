bb12c2e033f0c4a096edb8b5097c490273ecdbd1
Nathan McKinley <nmckinley@google.com>
2020-02-10 14:58:27 -0800

    Test the terraform repos after they get built. (#3103)
    * Test the terraform repos after they get built.
    
    * Code cleanliness and speed improvement.

---
+++ .ci/containers/terraform-tester/test_terraform.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+REPO=$1
+REFERENCE=$2
+SCRATCH_OWNER=modular-magician
+if [ "$REPO" == "ga" ]; then
+    GH_REPO=terraform-provider-google
+elif [ "$REPO" == "beta" ]; then
+    GH_REPO=terraform-provider-google-beta
+else
+    echo "no repo, dying."
+    exit 1
+fi
+
+SCRATCH_PATH=https://$SCRATCH_OWNER:$GITHUB_TOKEN@github.com/$SCRATCH_OWNER/$GH_REPO
+LOCAL_PATH=$GOPATH/src/github.com/terraform-providers/$GH_REPO
+mkdir -p "$(dirname $LOCAL_PATH)"
+git clone $SCRATCH_PATH $LOCAL_PATH --single-branch --branch "auto-pr-$REFERENCE" --depth 1
+pushd $LOCAL_PATH
+
+make tools
+make docscheck
+make lint
+make test
