c4d579743f46a2440d5e59653ddaebe900d29dd2
Megan Bang <mbang@hashicorp.com>
2020-02-11 12:42:00 -0600

    sweeper changes, add website link, etc

--- third_party/terraform/tests/resource_app_engine_flexible_app_version_sweeper_test.go
+++ third_party/terraform/tests/resource_app_engine_flexible_app_version_sweeper_test.go
@@ -3,6 +3,7 @@ package google
 import (
 	"context"
 	"log"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
@@ -41,19 +42,63 @@ func testSweepAppEngineFlexibleAppVersion(region string) error {
 		},
 	}
 
-	deleteTemplate := "https://appengine.googleapis.com/v1/apps/{{project}}/services/{{service}}"
-	deleteUrl, err := replaceVars(d, config, deleteTemplate)
+	listTemplate := strings.Split("https://appengine.googleapis.com/v1/apps/{{project}}/services", "?")[0]
+	listUrl, err := replaceVars(d, config, listTemplate)
 	if err != nil {
-		log.Printf("[INFO][SWEEPER_LOG] error preparing delete url: %s", err)
+		log.Printf("[INFO][SWEEPER_LOG] error preparing sweeper list url: %s", err)
 		return nil
 	}
 
-	// Don't wait on operations as we may have a lot to delete
-	_, err = sendRequest(config, "DELETE", config.Project, deleteUrl, nil)
+	res, err := sendRequest(config, "GET", config.Project, listUrl, nil)
 	if err != nil {
-		log.Printf("[INFO][SWEEPER_LOG] Error deleting for url %s : %s", deleteUrl, err)
-	} else {
-		log.Printf("[INFO][SWEEPER_LOG] Sent delete request for %s resource: %s", resourceName, d.Get("service"))
+		log.Printf("[INFO][SWEEPER_LOG] Error in response from request %s: %s", listUrl, err)
+		return nil
+	}
+
+	resourceList, ok := res["services"]
+	if !ok {
+		log.Printf("[INFO][SWEEPER_LOG] Nothing found in response.")
+		return nil
+	}
+
+	rl := resourceList.([]interface{})
+
+	log.Printf("[INFO][SWEEPER_LOG] Found %d items in %s list response.", len(rl), resourceName)
+	// items who don't match the tf-test prefix
+	nonPrefixCount := 0
+	for _, ri := range rl {
+		obj := ri.(map[string]interface{})
+		if obj["id"] == nil {
+			log.Printf("[INFO][SWEEPER_LOG] %s resource name was nil", resourceName)
+			return nil
+		}
+
+		name := GetResourceNameFromSelfLink(obj["id"].(string))
+		// Only sweep resources with the test prefix
+		if !strings.HasPrefix(name, "tf-test") {
+			nonPrefixCount++
+			continue
+		}
+
+		deleteTemplate := "https://appengine.googleapis.com/v1/apps/{{project}}/services/{{service}}"
+		deleteUrl, err := replaceVars(d, config, deleteTemplate)
+		if err != nil {
+			log.Printf("[INFO][SWEEPER_LOG] error preparing delete url: %s", err)
+			return nil
+		}
+		deleteUrl = deleteUrl + name
+
+		// Don't wait on operations as we may have a lot to delete
+		_, err = sendRequest(config, "DELETE", config.Project, deleteUrl, nil)
+		if err != nil {
+			log.Printf("[INFO][SWEEPER_LOG] Error deleting for url %s : %s", deleteUrl, err)
+		} else {
+			log.Printf("[INFO][SWEEPER_LOG] Sent delete request for %s resource: %s", resourceName, d.Get("service"))
+		}
+	}
+
+	if nonPrefixCount > 0 {
+		log.Printf("[INFO][SWEEPER_LOG] %d items without tf_test prefix remain.", nonPrefixCount)
 	}
 
 	return nil
