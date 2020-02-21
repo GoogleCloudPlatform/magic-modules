89dc51227d04886261b77bb6ccb48defd722821d
Sam Levenick <slevenick@google.com>
2020-02-21 14:56:46 -0800

    Add destroy check producer that can use provided provider

--- third_party/terraform/tests/resource_pubsub_topic_test.go
+++ third_party/terraform/tests/resource_pubsub_topic_test.go
@@ -2,10 +2,13 @@ package google
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func TestAccPubsubTopic_update(t *testing.T) {
@@ -18,7 +21,7 @@ func TestAccPubsubTopic_update(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    providers,
-		CheckDestroy: testAccCheckPubsubTopicDestroy,
+		CheckDestroy: testAccCheckPubsubTopicDestroyProducer(providers["google"].(*schema.Provider)),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccPubsubTopic_update(topic, "foo", "bar"),
@@ -111,4 +114,32 @@ resource "google_pubsub_topic" "topic" {
   kms_key_name = "%s"
 }
 `, pid, topicName, kmsKey)
+}
+
+// Temporary until all destroy functions can be reworked to take a provider as an argument
+func testAccCheckPubsubTopicDestroyProducer(provider *schema.Provider) func(s *terraform.State) error {
+	return func(s *terraform.State) error {
+		for name, rs := range s.RootModule().Resources {
+			if rs.Type != "google_pubsub_topic" {
+				continue
+			}
+			if strings.HasPrefix(name, "data.") {
+				continue
+			}
+
+			config := provider.Meta().(*Config)
+
+			url, err := replaceVarsForTest(config, rs, "{{PubsubBasePath}}projects/{{project}}/topics/{{name}}")
+			if err != nil {
+				return err
+			}
+
+			_, err = sendRequest(config, "GET", "", url, nil, pubsubTopicProjectNotReady)
+			if err == nil {
+				return fmt.Errorf("PubsubTopic still exists at %s", url)
+			}
+		}
+
+		return nil
+	}
 }
