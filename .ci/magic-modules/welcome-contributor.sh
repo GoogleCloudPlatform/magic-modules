f51da597a9368a30932db546a7691de2c0338421
Dana Hoffman <danahoffman@google.com>
2020-03-18 13:05:01 -0700

    Update list of random reviewers + welcome message (#3266)

--- .ci/magic-modules/welcome-contributor.sh
+++ .ci/magic-modules/welcome-contributor.sh
@@ -2,12 +2,12 @@
 
 set -x
 
-ASSIGNEE=$(shuf -n 1 <(printf "danawillow\nrambleraptor\nemilymye\nrileykarson\nSirGitsalot\nslevenick\ntysen"))
+ASSIGNEE=$(shuf -n 1 <(printf "danawillow\nrambleraptor\nemilymye\nrileykarson\nSirGitsalot\nslevenick\nchrisst\nc2thorn\nndmckinley"))
 
 cat > comment/pr_comment << EOF
 Hello!  I am a robot who works on Magic Modules PRs.
 
-I have detected that you are a community contributor, so your PR will be assigned to someone with a commit-bit on this repo for initial review.  They will authorize it to run through our CI pipeline, which will generate downstream PRs.
+I have detected that you are a community contributor, so your PR will be assigned to someone with a commit-bit on this repo for initial review.
 
 Thanks for your contribution!  A human will be with you soon.
 
