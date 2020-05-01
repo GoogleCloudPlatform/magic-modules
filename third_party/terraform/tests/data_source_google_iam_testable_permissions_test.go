3e7bbf02b3de73cf51c1e311e449e87c242541bf
Ryan Canty <jrcanty@gmail.com>
2020-05-01 13:12:53 -0700

    Added tests and docs

---
+++ third_party/terraform/tests/data_source_google_iam_testable_permissions_test.go
@@ -0,0 +1,104 @@
+package google
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccDataSourceGoogleIamTestablePermissions_basic(t *testing.T) {
+	t.Parallel()
+
+	project := getTestProjectFromEnv()
+	vcrTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+			 data "google_iam_testable_permissions" "perms" {
+				full_resource_name = "//cloudresourcemanager.googleapis.com/projects/%s"
+			}
+		`, project),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGoogleIamTestablePermissionsMeta(
+						project,
+						"data.google_iam_testable_permissions.perms",
+						"GA",
+						"",
+					),
+				),
+			},
+			{
+				Config: fmt.Sprintf(`
+			 data "google_iam_testable_permissions" "perms" {
+				full_resource_name   = "//cloudresourcemanager.googleapis.com/projects/%s"
+				custom_support_level = "NOT_SUPPORTED"
+				stage                = "BETA"
+			}
+		`, project),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGoogleIamTestablePermissionsMeta(
+						project,
+						"data.google_iam_testable_permissions.perms",
+						"BETA",
+						"NOT_SUPPORTED",
+					),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckGoogleIamTestablePermissionsMeta(project string, n string, expectedStage string, expectedSupportLevel string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Can't find perms data source: %s", n)
+		}
+		expectedId := fmt.Sprintf("//cloudresourcemanager.googleapis.com/projects/%s", project)
+		if rs.Primary.ID != expectedId {
+			return errors.New("perms data source ID not set.")
+		}
+		attrs := rs.Primary.Attributes
+		count, ok := attrs["permissions.#"]
+		if !ok {
+			return errors.New("can't find 'permsissions' attribute")
+		}
+		permCount, err := strconv.Atoi(count)
+		if err != nil {
+			return err
+		}
+		if permCount < 2 {
+			return errors.New("count should be greater than 2")
+		}
+		foundStage := false
+		foundSupport := false
+
+		for i := 0; i < permCount; i++ {
+			stageKey := "permissions." + strconv.Itoa(i) + ".stage"
+			supportKey := "permissions." + strconv.Itoa(i) + ".custom_support_level"
+			if attrs[stageKey] == expectedStage {
+				foundStage = true
+			}
+			if attrs[supportKey] == expectedSupportLevel {
+				foundSupport = true
+			}
+			if foundSupport && foundStage {
+				return nil
+			}
+		}
+
+		if foundSupport {
+			return errors.New(fmt.Sprintf("Could not find stage %s in output", expectedStage))
+		}
+		if foundStage {
+			return errors.New(fmt.Sprintf("Could not find custom_support_level %s in output", expectedSupportLevel))
+		}
+		return errors.New("Unable to find customeSupportLevel or stage")
+	}
+}
