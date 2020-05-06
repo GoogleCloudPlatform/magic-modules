cbdd8134b37fb022f039a5b11df23592a1f8f510
emily <emilyye@google.com>
2020-05-05 19:53:55 -0700

    Fix bigquery dataset access iam roles with primative equivalent (#3471)
    * account for iam vs primative bigquery roles
    
    * add test, fix description
    
    * s/EDITOR/WRITER
    
    * fix test
    
    * comments

---
+++ templates/terraform/constants/bigquery_dataset_access.go
@@ -0,0 +1,12 @@
+var bigqueryAccessRoleToPrimitiveMap =  map[string]string {
+    "roles/bigquery.dataOwner": "OWNER",
+    "roles/bigquery.dataEditor": "WRITER",
+    "roles/bigquery.dataViewer": "VIEWER",
+}
+
+func resourceBigQueryDatasetAccessRoleDiffSuppress(k, old, new string, d *schema.ResourceData) bool {
+    if primitiveRole, ok := bigqueryAccessRoleToPrimitiveMap[new]; ok {
+        return primitiveRole == old
+    }
+    return false
+}
