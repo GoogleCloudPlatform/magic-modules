1f31f8281ca6b49b7865bda3e900911fc57e0734
Sam Levenick <slevenick@google.com>
2020-05-18 14:16:51 -0700

    Add absolute path, bash

--- .ci/containers/terraform-vcr-tester/vcr_test_terraform.sh
+++ .ci/containers/terraform-vcr-tester/vcr_test_terraform.sh
@@ -3,10 +3,9 @@
 set -e
 
 REFERENCE=$1
-TEAMCITY_TOKEN=$2
 
-sed -i 's/{{PR_NUMBER}}/'"$REFERENCE"'/g' teamcityparams.xml
-curl --header "Accept: application/json" --header "Authorization: Bearer $TEAMCITY_TOKEN" https://ci-oss.hashicorp.engineering/app/rest/buildQueue --request POST --header "Content-Type:application/xml" --data-binary @teamcityparams.xml -o build.json
+sed -i 's/{{PR_NUMBER}}/'"$REFERENCE"'/g' /teamcityparams.xml
+curl --header "Accept: application/json" --header "Authorization: Bearer $TEAMCITY_TOKEN" https://ci-oss.hashicorp.engineering/app/rest/buildQueue --request POST --header "Content-Type:application/xml" --data-binary @/teamcityparams.xml -o build.json
 
 echo $(cat build.json | jq .webUrl)
 
