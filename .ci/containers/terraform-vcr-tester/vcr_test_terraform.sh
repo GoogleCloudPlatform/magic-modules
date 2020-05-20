e4ab97a08d023e48c35fabc1dd9dc1a8bd339ba6
Sam Levenick <slevenick@google.com>
2020-05-20 16:49:00 -0700

    Add comment with url of resulting build

--- .ci/containers/terraform-vcr-tester/vcr_test_terraform.sh
+++ .ci/containers/terraform-vcr-tester/vcr_test_terraform.sh
@@ -2,9 +2,15 @@
 
 set -e
 
-REFERENCE=$1
+PR_NUMBER=$1
 
-sed -i 's/{{PR_NUMBER}}/'"$REFERENCE"'/g' /teamcityparams.xml
+sed -i 's/{{PR_NUMBER}}/'"$PR_NUMBER"'/g' /teamcityparams.xml
 curl --header "Accept: application/json" --header "Authorization: Bearer $TEAMCITY_TOKEN" https://ci-oss.hashicorp.engineering/app/rest/buildQueue --request POST --header "Content-Type:application/xml" --data-binary @/teamcityparams.xml -o build.json
 
-echo $(cat build.json | jq .webUrl)
+URL=echo $(cat build.json | jq .webUrl)
+
+comment="I have triggered VCR tests based on this PR's diffs. See the results here: $URL"
+
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
+      -d "$(jq -r --arg comment "$comment" -n "{body: \$comment}")" \
+      "https://api.github.com/repos/GoogleCloudPlatform/magic-modules/issues/${PR_NUMBER}/comments"
