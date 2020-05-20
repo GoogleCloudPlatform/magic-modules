c8a079c1251b9a4af032dd83aebc670ddbf6260b
Sam Levenick <slevenick@google.com>
2020-05-20 15:19:20 -0700

    Remove commented files

--- .ci/containers/terraform-vcr-tester/vcr_test_terraform.sh
+++ .ci/containers/terraform-vcr-tester/vcr_test_terraform.sh
@@ -8,12 +8,3 @@ sed -i 's/{{PR_NUMBER}}/'"$REFERENCE"'/g' /teamcityparams.xml
 curl --header "Accept: application/json" --header "Authorization: Bearer $TEAMCITY_TOKEN" https://ci-oss.hashicorp.engineering/app/rest/buildQueue --request POST --header "Content-Type:application/xml" --data-binary @/teamcityparams.xml -o build.json
 
 echo $(cat build.json | jq .webUrl)
-
-#BUILD_ID=$(cat build.json | jq .id)
-#STATE=$(cat build.json | jq .state)
-#while [ "$STATE" != "finished" ]
-#do
-#	curl --header "Accept: application/json" --header "Authorization: Bearer $TEAMCITY_TOKEN" https://ci-oss.hashicorp.engineering/app/rest/buildQueue/id:$BUILD_ID -o job_status.json
-#	STATE=$(cat job_status.json | jq .state)
-#done
-
