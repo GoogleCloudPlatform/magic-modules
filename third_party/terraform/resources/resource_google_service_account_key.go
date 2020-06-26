923aa6baa7e12504e08e1587302f02f6fb5c23ba
Thomas Rodgers <notaraptor@gmail.com>
2020-06-26 11:32:05 -0700

    Update third_party/terraform/resources/resource_google_service_account_key.go
    Co-authored-by: emily <emilyye@google.com>

--- third_party/terraform/resources/resource_google_service_account_key.go
+++ third_party/terraform/resources/resource_google_service_account_key.go
@@ -21,7 +21,7 @@ func resourceGoogleServiceAccountKey() *schema.Resource {
 				Type:        schema.TypeString,
 				Required:    true,
 				ForceNew:    true,
-				Description: `The Service account id of the Key Pair. This can be a string in the format {ACCOUNT} or projects/{PROJECT_ID}/serviceAccounts/{ACCOUNT}, where {ACCOUNT} is the email address or unique id of the service account. If the {ACCOUNT} syntax is used, the project will be inferred from the account.`,
+				Description: `The ID of the parent service account of the key. This can be a string in the format {ACCOUNT} or projects/{PROJECT_ID}/serviceAccounts/{ACCOUNT}, where {ACCOUNT} is the email address or unique id of the service account. If the {ACCOUNT} syntax is used, the project will be inferred from the account.`,
 			},
 			// Optional
 			"key_algorithm": {
