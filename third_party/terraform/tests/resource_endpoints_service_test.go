e5c5b9ac3b50f25577db6b3ee91f106b55788095
Nathan Mckinley <nmckinley@google.com>
2020-06-30 15:35:50 -0700

    null provider not available - use random instead.

--- third_party/terraform/tests/resource_endpoints_service_test.go
+++ third_party/terraform/tests/resource_endpoints_service_test.go
@@ -154,10 +154,11 @@ EOF
 
 }
 
-resource "null_resource" "some" {
-  triggers = {
+resource "random_id" "foo" {
+  keepers = {
     config_id = google_endpoints_service.endpoints_service.config_id
   }
+  byte_length = 8
 }
 `, serviceId, project, rev)
 }
