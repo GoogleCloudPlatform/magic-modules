54f372413f2421052da3eccc4e4dffdb43927bc4
Fai Fung <ffung@xebia.com>
2020-06-30 09:20:30 +0200

    fix mixed indentation

--- third_party/terraform/tests/resource_bigquery_table_test.go
+++ third_party/terraform/tests/resource_bigquery_table_test.go
@@ -505,7 +505,7 @@ resource "google_storage_bucket_object" "test" {
 }
 
 resource "google_bigquery_dataset" "test" {
-        dataset_id = "%s"
+	dataset_id = "%s"
 }
 
 resource "google_bigquery_table" "test" {
@@ -513,16 +513,16 @@ resource "google_bigquery_table" "test" {
 	dataset_id = google_bigquery_dataset.test.dataset_id
 
 	external_data_configuration {
-            source_format = "CSV"
-            autodetect = true
-            source_uris= ["gs://${google_storage_bucket.test.name}/*"]
+		source_format = "CSV"
+		autodetect = true
+		source_uris= ["gs://${google_storage_bucket.test.name}/*"]
 
-            hive_partitioning_options {
-                mode = "AUTO"
-                source_uri_prefix = "gs://${google_storage_bucket.test.name}/"
-	    }
+		hive_partitioning_options {
+			mode = "AUTO"
+			source_uri_prefix = "gs://${google_storage_bucket.test.name}/"
+		}
 
-        }
+	}
 	depends_on = ["google_storage_bucket_object.test"]
 }
 `, bucketName, datasetID, tableID)
@@ -537,12 +537,12 @@ resource "google_storage_bucket" "test" {
 
 resource "google_storage_bucket_object" "test" {
 	name    = "key1=20200330/data.json"
-        content = "{\"name\":\"test\", \"last_modification\":\"2020-04-01\"}"
+	content = "{\"name\":\"test\", \"last_modification\":\"2020-04-01\"}"
 	bucket  = google_storage_bucket.test.name
 }
 
 resource "google_bigquery_dataset" "test" {
-        dataset_id = "%s"
+	dataset_id = "%s"
 }
 
 resource "google_bigquery_table" "test" {
@@ -550,16 +550,16 @@ resource "google_bigquery_table" "test" {
 	dataset_id = google_bigquery_dataset.test.dataset_id
 
 	external_data_configuration {
-            source_format = "NEWLINE_DELIMITED_JSON"
-            autodetect = false
-            source_uris= ["gs://${google_storage_bucket.test.name}/*"]
+		source_format = "NEWLINE_DELIMITED_JSON"
+		autodetect = false
+		source_uris= ["gs://${google_storage_bucket.test.name}/*"]
 
-            hive_partitioning_options {
-                mode = "CUSTOM"
-                source_uri_prefix = "gs://${google_storage_bucket.test.name}/{key1:STRING}"
-	    }
+		hive_partitioning_options {
+			mode = "CUSTOM"
+			source_uri_prefix = "gs://${google_storage_bucket.test.name}/{key1:STRING}"
+		}
 
-            schema = <<EOH
+		schema = <<EOH
 [
 	{
 		"name": "name",
