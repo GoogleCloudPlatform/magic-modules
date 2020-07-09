ddcb8be7988c8dc607b8c65061101e37471513bd
Dana Hoffman <danahoffman@google.com>
2020-07-09 14:14:43 -0700

    remove Emily from random assignee rotation and add Megan (#3736)

--- .ci/containers/contributor-checker/check-contributor.sh
+++ .ci/containers/contributor-checker/check-contributor.sh
@@ -31,14 +31,14 @@ USER=$(curl -H "Authorization: token ${GITHUB_TOKEN}" \
 
 # This is where you add users who do not need to have an assignee chosen for
 # them.
-if $(echo $USER | fgrep -wq -e ndmckinley -e danawillow -e emilymye -e megan07 -e paddycarver -e rambleraptor -e SirGitsalot -e slevenick -e c2thorn -e rileykarson); then
+if $(echo $USER | fgrep -wq -e ndmckinley -e danawillow -e megan07 -e paddycarver -e rambleraptor -e SirGitsalot -e slevenick -e c2thorn -e rileykarson); then
   echo "User is on the list, not assigning."
   exit 0
 fi
 
 # This is where you add people to the random-assignee rotation.  This list
 # might not equal the list above.
-ASSIGNEE=$(shuf -n 1 <(printf "danawillow\nemilymye\nrileykarson\nslevenick\nc2thorn\nndmckinley"))
+ASSIGNEE=$(shuf -n 1 <(printf "danawillow\nrileykarson\nslevenick\nc2thorn\nndmckinley\nmegan07"))
 
 comment=$(cat << EOF
 Hello!  I am a robot who works on Magic Modules PRs.
