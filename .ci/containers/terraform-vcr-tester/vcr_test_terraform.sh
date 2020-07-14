2cd5ca439c5f3d3d17d9d28e88c3dd5c77c47094
Sam Levenick <slevenick@google.com>
2020-07-14 16:43:39 -0700

    Check GCP org for membership if user is not on VCR-allowed list

--- .ci/containers/terraform-vcr-tester/vcr_test_terraform.sh
+++ .ci/containers/terraform-vcr-tester/vcr_test_terraform.sh
@@ -9,10 +9,32 @@ USER=$(curl -H "Authorization: token ${GITHUB_TOKEN}" \
 
 # Only run tests for safe users
 if $(echo $USER | fgrep -wq -e ndmckinley -e danawillow -e emilymye -e megan07 -e paddycarver -e rambleraptor -e SirGitsalot -e slevenick -e c2thorn -e rileykarson); then
-  echo "User is on the list, not skipping."
+	echo "User is on the list, not skipping."
 else
-	echo "User is not on the list, skipping."
-  exit 0
+	echo "Checking GCP org membership"
+	GCP_MEMBER=$(curl -sw '%{http_code}' -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/orgs/GoogleCloudPlatform/members/$USER -o /dev/null)
+	if [ "$GCP_MEMBER" != "404" ]; then
+		echo "User is a GCP org member, continuing"
+	else
+		echo "User is not a GCP org member"
+		exit 0
+	fi
+fi
+
+sed -i 's/{{PR_NUMBER}}/'"$PR_NUMBER"'/g' /teamcityparams.xml
+curl --header "Accept: application/json" --header "Authorization: Bearer $TEAMCITY_TOKEN" https://ci-oss.hashicorp.engineering/app/rest/buildQueue --request POST --header "Content-Type:application/xml" --data-bina>
+
+URL=$(cat build.json | jq .webUrl)
+ret=$?
+if [ $ret -ne 0 ]; then
+	echo "Auth failed"
+	exit 1
+else
+	comment="I have triggered VCR tests based on this PR's diffs. See the results here: $URL"
+
+	curl -H "Authorization: token ${GITHUB_TOKEN}" \
+		-d "$(jq -r --arg comment "$comment" -n "{body: \$comment}")" \
+		"https://api.github.com/repos/GoogleCloudPlatform/magic-modules/issues/${PR_NUMBER}/comments"
 fi
 
 sed -i 's/{{PR_NUMBER}}/'"$PR_NUMBER"'/g' /teamcityparams.xml
@@ -53,8 +75,8 @@ set +e
 FAILED_TESTS=$(cat failed.json | jq -r '.testOccurrence | map(.name) | join("|")')
 ret=$?
 if [ $ret -ne 0 ]; then
-  echo "Job failed without failing tests"
-  exit 1
+	echo "Job failed without failing tests"
+	exit 1
 fi
 set -e
 
