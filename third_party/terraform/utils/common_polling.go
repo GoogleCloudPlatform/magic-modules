470652f5ae66d6d10b226c37b5f466036cb50929
Tiffany Shen <tishen@google.com>
2020-07-15 23:32:39 +0000

    added metric descriptor resource and more functionality to PollAsync

--- third_party/terraform/utils/common_polling.go
+++ third_party/terraform/utils/common_polling.go
@@ -3,6 +3,7 @@ package google
 import (
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -31,12 +32,66 @@ func SuccessPollResult() PollResult {
 	return nil
 }
 
-func PollingWaitTime(pollF PollReadFunc, checkResponse PollCheckResponseFunc, activity string, timeout time.Duration) error {
+func PollingWaitTime(pollF PollReadFunc, checkResponse PollCheckResponseFunc, activity string,
+	timeout time.Duration, targetOccurrences int) error {
 	log.Printf("[DEBUG] %s: Polling until expected state is read", activity)
-	return resource.Retry(timeout, func() *resource.RetryError {
+	log.Printf("[DEBUG] Target occurrences: %d", targetOccurrences)
+	return RetryWithTargetOccurrences(timeout, targetOccurrences, func() *resource.RetryError {
 		readResp, readErr := pollF()
 		return checkResponse(readResp, readErr)
 	})
+}
+
+// RetryWithTargetOccurrences is a basic wrapper around StateChangeConf that will just retry
+// a function until it returns the specified amount of target occurrences continuously.
+func RetryWithTargetOccurrences(timeout time.Duration, targetOccurrences int,
+	f resource.RetryFunc) error {
+	// These are used to pull the error out of the function; need a mutex to
+	// avoid a data race.
+	var resultErr error
+	var resultErrMu sync.Mutex
+
+	c := &resource.StateChangeConf{
+		Pending:                   []string{"retryableerror"},
+		Target:                    []string{"success"},
+		Timeout:                   timeout,
+		MinTimeout:                500 * time.Millisecond,
+		ContinuousTargetOccurence: targetOccurrences,
+		Refresh: func() (interface{}, string, error) {
+			rerr := f()
+
+			resultErrMu.Lock()
+			defer resultErrMu.Unlock()
+
+			if rerr == nil {
+				resultErr = nil
+				return 42, "success", nil
+			}
+
+			resultErr = rerr.Err
+
+			if rerr.Retryable {
+				return 42, "retryableerror", nil
+			}
+			return nil, "quit", rerr.Err
+		},
+	}
+
+	_, waitErr := c.WaitForState()
+
+	// Need to acquire the lock here to be able to avoid race using resultErr as
+	// the return value
+	resultErrMu.Lock()
+	defer resultErrMu.Unlock()
+
+	// resultErr may be nil because the wait timed out and resultErr was never
+	// set; this is still an error
+	if resultErr == nil {
+		return waitErr
+	}
+	// resultErr takes precedence over waitErr if both are set because it is
+	// more likely to be useful
+	return resultErr
 }
 
 /**
@@ -52,4 +107,19 @@ func PollCheckForExistence(_ map[string]interface{}, respErr error) PollResult {
 		return ErrorPollResult(respErr)
 	}
 	return SuccessPollResult()
+}
+
+// PollCheckForAbsence waits for a 404 response, continues polling on 200, and returns any other error.
+func PollCheckForAbsence(_ map[string]interface{}, respErr error) PollResult {
+	log.Printf("[DEBUG] PollCheckForAbsence response: %v", respErr) // remove later
+	if respErr != nil {
+		if isGoogleApiErrorWithCode(respErr, 404) {
+			log.Printf("[DEBUG] PollCheckForAbsence response: success") // remove later
+			return SuccessPollResult()
+		}
+		log.Printf("[DEBUG] PollCheckForAbsence response: error") // remove later
+		return ErrorPollResult(respErr)
+	}
+	log.Printf("[DEBUG] PollCheckForAbsence response: pending") // remove later
+	return PendingStatusPollResult("found")
 }
