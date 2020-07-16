f93aec0132c6512960248583116c8a98bc89eb56
Sam Levenick <slevenick@google.com>
2020-07-15 17:00:21 -0700

    Fix typos

--- .ci/containers/terraform-vcr-tester/vcr_test_terraform.sh
+++ .ci/containers/terraform-vcr-tester/vcr_test_terraform.sh
@@ -22,22 +22,6 @@ else
 fi
 
 sed -i 's/{{PR_NUMBER}}/'"$PR_NUMBER"'/g' /teamcityparams.xml
-curl --header "Accept: application/json" --header "Authorization: Bearer $TEAMCITY_TOKEN" https://ci-oss.hashicorp.engineering/app/rest/buildQueue --request POST --header "Content-Type:application/xml" --data-bina>
-
-URL=$(cat build.json | jq .webUrl)
-ret=$?
-if [ $ret -ne 0 ]; then
-	echo "Auth failed"
-	exit 1
-else
-	comment="I have triggered VCR tests based on this PR's diffs. See the results here: $URL"
-
-	curl -H "Authorization: token ${GITHUB_TOKEN}" \
-		-d "$(jq -r --arg comment "$comment" -n "{body: \$comment}")" \
-		"https://api.github.com/repos/GoogleCloudPlatform/magic-modules/issues/${PR_NUMBER}/comments"
-fi
-
-sed -i 's/{{PR_NUMBER}}/'"$PR_NUMBER"'/g' /teamcityparams.xml
 curl --header "Accept: application/json" --header "Authorization: Bearer $TEAMCITY_TOKEN" https://ci-oss.hashicorp.engineering/app/rest/buildQueue --request POST --header "Content-Type:application/xml" --data-binary @/teamcityparams.xml -o build.json
 
 URL=$(cat build.json | jq .webUrl)
