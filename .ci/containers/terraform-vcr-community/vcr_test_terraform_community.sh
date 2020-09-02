2f082a434d00c1c86ccd28c58b5a709e0dc13449
Sam Levenick <slevenick@google.com>
2020-09-02 11:33:32 -0700

    Update vcr-tester, add community PR build

--- .ci/containers/terraform-vcr-community/vcr_test_terraform_community.sh
+++ .ci/containers/terraform-vcr-community/vcr_test_terraform_community.sh
@@ -3,23 +3,6 @@
 set -e
 PR_NUMBER=$1
 
-USER=$(curl -H "Authorization: token ${GITHUB_TOKEN}" \
-  "https://api.github.com/repos/GoogleCloudPlatform/magic-modules/issues/${PR_NUMBER}" | jq -r .user.login)
-
-# Only run tests for safe users
-if $(echo $USER | fgrep -wq -e ndmckinley -e danawillow -e emilymye -e megan07 -e paddycarver -e rambleraptor -e SirGitsalot -e slevenick -e c2thorn -e rileykarson); then
-	echo "User is on the list, not skipping."
-else
-	echo "Checking GCP org membership"
-	GCP_MEMBER=$(curl -sw '%{http_code}' -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/orgs/GoogleCloudPlatform/members/$USER -o /dev/null)
-	if [ "$GCP_MEMBER" != "404" ]; then
-		echo "User is a GCP org member, continuing"
-	else
-		echo "User is not a GCP org member"
-		exit 0
-	fi
-fi
-
 sed -i 's/{{PR_NUMBER}}/'"$PR_NUMBER"'/g' /teamcityparams.xml
 curl --header "Accept: application/json" --header "Authorization: Bearer $TEAMCITY_TOKEN" https://ci-oss.hashicorp.engineering/app/rest/buildQueue --request POST --header "Content-Type:application/xml" --data-binary @/teamcityparams.xml -o build.json
 
