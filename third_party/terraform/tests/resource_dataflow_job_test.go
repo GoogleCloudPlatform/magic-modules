ff0df0349a983d03cafa37ca2ec1a2df925bd4db
venkykuberan <kuberan@google.com>
2020-09-09 11:10:49 -0700

    initial commit - wip

--- third_party/terraform/tests/resource_dataflow_job_test.go
+++ third_party/terraform/tests/resource_dataflow_job_test.go
@@ -65,6 +65,12 @@ func TestAccDataflowJob_withRegion(t *testing.T) {
 					testAccRegionalDataflowJobExists(t, "google_dataflow_job.big_data", "us-central1"),
 				),
 			},
+			{
+				Config: testAccDataflowJob_regionUpdate(bucket, job),
+				Check: resource.ComposeTestCheckFunc(
+					testAccRegionalDataflowJobExists(t, "google_dataflow_job.big_data", "us-central1"),
+				),
+			},
 		},
 	})
 }
@@ -624,6 +630,33 @@ resource "google_dataflow_job" "big_data" {
     inputFile = "%s"
     output    = "${google_storage_bucket.temp.url}/output"
   }
+
+  on_delete = "cancel"
+}
+`, bucket, job, testDataflowJobTemplateWordCountUrl, testDataflowJobSampleFileUrl)
+}
+
+func testAccDataflowJob_regionUpdate(bucket, job string) string {
+	return fmt.Sprintf(`
+resource "google_storage_bucket" "temp" {
+  name = "%s"
+  force_destroy = true
+}
+
+resource "google_dataflow_job" "big_data" {
+  name = "%s"
+  region  = "us-central1"
+
+  template_gcs_path = "%s"
+  temp_gcs_location = google_storage_bucket.temp.url
+  parameters = {
+    inputFile = "%s"
+    output    = "${google_storage_bucket.temp.url}/output"
+  }
+  transform_name_mapping = {
+	name = "test_job"
+	env = "test"
+}
 
   on_delete = "cancel"
 }
