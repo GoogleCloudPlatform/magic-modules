32af2575bc538d4b330967586fdb99fe23aefcdf
Stephen Lewis <stephenrlewis@google.com>
2020-10-15 14:54:22 -0700

    Disabled deletion_protection on various explicitly-created spanner databases in other test resources

--- third_party/terraform/tests/resource_spanner_database_test.go
+++ third_party/terraform/tests/resource_spanner_database_test.go
@@ -87,6 +87,7 @@ resource "google_spanner_database" "basic" {
 	"CREATE TABLE t1 (t1 INT64 NOT NULL,) PRIMARY KEY(t1)",
 	"CREATE TABLE t2 (t2 INT64 NOT NULL,) PRIMARY KEY(t2)",
   ]
+  deletion_protection = false
 }
 `, instanceName, instanceName, databaseName)
 }
@@ -108,6 +109,7 @@ resource "google_spanner_database" "basic" {
 	"CREATE TABLE t2 (t2 INT64 NOT NULL,) PRIMARY KEY(t2)",
 	"CREATE TABLE t3 (t3 INT64 NOT NULL,) PRIMARY KEY(t3)",
   ]
+  deletion_protection = false
 }
 `, instanceName, instanceName, databaseName)
 }
