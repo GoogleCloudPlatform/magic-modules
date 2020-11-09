cf769cd163add0a1534307c7e4ec687259e3e5fa
Stephen Lewis <stephenrlewis@google.com>
2020-11-09 14:19:27 -0800

    Corrected trailing references to google-beta provider in cloud identity test code

--- third_party/terraform/tests/resource_cloud_identity_group_test.go
+++ third_party/terraform/tests/resource_cloud_identity_group_test.go
@@ -33,7 +33,6 @@ func TestAccCloudIdentityGroup_update(t *testing.T) {
 func testAccCloudIdentityGroup_update(context map[string]interface{}) string {
 	return Nprintf(`
 resource "google_cloud_identity_group" "cloud_identity_group_basic" {
-  provider = google-beta
   display_name = "tf-test-my-identity-group%{random_suffix}-update"
   description  = "my-description"
 
