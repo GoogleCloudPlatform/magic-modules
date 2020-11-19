50335e9087caa7d940961da8037d86f57b738efb
Cameron Thornton <camthornton@google.com>
2020-11-19 14:34:43 -0600

    Revert computed, fix cors field not appearing in PATCH

--- third_party/terraform/resources/resource_storage_bucket.go
+++ third_party/terraform/resources/resource_storage_bucket.go
@@ -255,7 +255,6 @@ func resourceStorageBucket() *schema.Resource {
 
 			"cors": {
 				Type:     schema.TypeList,
-				Computed: true,
 				Optional: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
@@ -511,8 +510,12 @@ func resourceStorageBucketUpdate(d *schema.ResourceData, meta interface{}) error
 		}
 	}
 
-	if v, ok := d.GetOk("cors"); ok {
-		sb.Cors = expandCors(v.([]interface{}))
+	if d.HasChange("cors") {
+		if v, ok := d.GetOk("cors"); ok {
+			sb.Cors = expandCors(v.([]interface{}))
+		} else {
+			sb.NullFields = append(sb.NullFields, "Cors")
+		}
 	}
 
 	if d.HasChange("default_event_based_hold") {
