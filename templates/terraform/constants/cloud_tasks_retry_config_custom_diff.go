75bbb8cf6600e4126d3efa1d3d789485130c782c
Scott Suarez <ScottMSuarez@gmail.com>
2020-12-21 09:46:33 -0800

    Update cloud_tasks_retry_config_custom_diff.go

--- templates/terraform/constants/cloud_tasks_retry_config_custom_diff.go
+++ templates/terraform/constants/cloud_tasks_retry_config_custom_diff.go
@@ -1,6 +1,6 @@
 func suppressOmittedMaxDuration(_, old, new string, _ *schema.ResourceData) bool {
 	if old == "" && new == "0s" {
-		log.Printf("[INFO] max retry is 0s and api omitted field, supressing diff")
+		log.Printf("[INFO] max retry is 0s and api omitted field, suppressing diff")
 		return true
 	}
 	return false
