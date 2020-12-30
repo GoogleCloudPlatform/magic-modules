a1b6ec7f135f3322e2108f9e1c5c1969c9f7102f
Scott Suarez <ScottMSuarez@gmail.com>
2020-12-30 11:59:45 -0800

    some changes

--- third_party/terraform/utils/header_transport.go
+++ third_party/terraform/utils/header_transport.go
@@ -6,29 +6,28 @@ import (
 )
 
 // adapted from https://stackoverflow.com/questions/51325704/adding-a-default-http-header-in-go
-
-type headerTrasportLayer struct {
+type headerTransportLayer struct {
 	http.Header
 	baseTransit http.RoundTripper
 }
 
-func headerTrasportLayer(baseTransit http.RoundTripper) headerTrasportLayer {
+func newTransportWithHeaders(baseTransit http.RoundTripper) headerTransportLayer {
 	if baseTransit == nil {
 		baseTransit = http.DefaultTransport
 	}
 
 	headers := make(http.Header)
-	if requestReason := os.Getenv("CLOUDSDK_CORE_REQUEST_REASON"); v != "" {
+	if requestReason := os.Getenv("CLOUDSDK_CORE_REQUEST_REASON"); requestReason != "" {
 		headers.Set("X-Goog-Request-Reason", requestReason)
 	}
 
-	return headerTrasportLayer{Header: headers, baseTransit: baseTransit}
+	return headerTransportLayer{Header: headers, baseTransit: baseTransit}
 }
 
-func (h headerTrasportLayer) RoundTrip(req *http.Request) (*http.Response, error) {
+func (h headerTransportLayer) RoundTrip(req *http.Request) (*http.Response, error) {
 	for key, value := range h.Header {
 		// only set headers that are not previously defined
-		if _, ok := req[key]; !ok {
+		if _, ok := req.Header[key]; !ok {
 			req.Header[key] = value
 		}
 	}
