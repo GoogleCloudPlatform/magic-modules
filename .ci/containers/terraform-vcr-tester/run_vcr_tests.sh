c4c4f2178444ac8a27c40c7bb5dabc9488395edf
Sam Levenick <slevenick@google.com>
2021-02-03 09:08:39 -0800

    Add polling wait on VCR recording mode, bump max run times (#4457)
    * Add polling wait on VCR recording mode, bump max run times
    
    * Skip a test

--- .ci/containers/terraform-vcr-tester/run_vcr_tests.sh
+++ .ci/containers/terraform-vcr-tester/run_vcr_tests.sh
@@ -19,7 +19,7 @@ STATUS=$(cat poll.json | jq .status -r)
 STATE=$(cat poll.json | jq .state -r)
 counter=0
 while [[ "$STATE" != "finished" ]]; do
-	if [ "$counter" -gt "100" ]; then
+	if [ "$counter" -gt "500" ]; then
 		echo "Failed to wait for job to finish, exiting"
 		exit 1
 	fi
@@ -55,3 +55,48 @@ comment="I have triggered VCR tests in RECORDING mode for the following tests th
 curl -H "Authorization: token ${GITHUB_TOKEN}" \
       -d "$(jq -r --arg comment "$comment" -n "{body: \$comment}")" \
       "https://api.github.com/repos/GoogleCloudPlatform/magic-modules/issues/${PR_NUMBER}/comments"
+
+# Reset for checking failed tests
+rm poll.json
+rm failed.json
+
+ID=$(cat record.json | jq .id -r)
+curl --header "Authorization: Bearer $TEAMCITY_TOKEN" --header "Accept: application/json" https://ci-oss.hashicorp.engineering/app/rest/builds/id:$ID --output poll.json
+STATUS=$(cat poll.json | jq .status -r)
+STATE=$(cat poll.json | jq .state -r)
+counter=0
+while [[ "$STATE" != "finished" ]]; do
+	if [ "$counter" -gt "500" ]; then
+		echo "Failed to wait for job to finish, exiting"
+		exit 1
+	fi
+	sleep 30
+	curl --header "Authorization: Bearer $TEAMCITY_TOKEN" --header "Accept: application/json" https://ci-oss.hashicorp.engineering/app/rest/builds/id:$ID --output poll.json
+	STATUS=$(cat poll.json | jq .status -r)
+	STATE=$(cat poll.json | jq .state -r)
+	echo "Trying again, State: $STATE Status: $STATUS"
+	counter=$((counter + 1))
+done
+
+if [ "$STATUS" == "SUCCESS" ]; then
+	echo "Tests succeeded."
+	exit 0
+fi
+
+curl --header "Accept: application/json" --header "Authorization: Bearer $TEAMCITY_TOKEN" http://ci-oss.hashicorp.engineering/app/rest/testOccurrences?locator=build:$ID,status:FAILURE --output failed.json -L
+set +e
+FAILED_TESTS=$(cat failed.json | jq -r '.testOccurrence | map(.name) | join("|")')
+ret=$?
+if [ $ret -ne 0 ]; then
+	echo "Job failed without failing tests"
+	exit 1
+fi
+set -e
+
+comment="Tests failed during RECORDING mode: $FAILED_TESTS Please fix these to complete your PR"
+
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
+      -d "$(jq -r --arg comment "$comment" -n "{body: \$comment}")" \
+      "https://api.github.com/repos/GoogleCloudPlatform/magic-modules/issues/${PR_NUMBER}/comments"
+# Tests failed after recording, exit 1 to display red X on github
+exit 1
