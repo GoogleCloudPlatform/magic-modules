b706f0e6339986de3e52c6da9214f286a1825af7
Sam Levenick <slevenick@google.com>
2021-02-11 12:15:34 -0800

    Add 2021 to copyright regex (#4485)

--- mmv1/spec/copyright.rb
+++ mmv1/spec/copyright.rb
@@ -19,8 +19,8 @@ module Google
     def initialize(files)
       @input_files = files
       @copyrightable_files = {
-        /.*\.yaml$/ => /^# Copyright 20(17|18|19|20)/,
-        /.*\.rb$/ => /^# Copyright 20(17|18|19|20)/
+        /.*\.yaml$/ => /^# Copyright 20(17|18|19|20|21)/,
+        /.*\.rb$/ => /^# Copyright 20(17|18|19|20|21)/
       }
     end
 
