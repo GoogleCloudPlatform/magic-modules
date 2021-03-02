d67cc31e478cde752df178829df3818766e0c90f
Scott Suarez <ScottMSuarez@gmail.com>
2021-03-01 19:26:48 -0800

    add user-agent based on environment variable - opt in

--- mmv1/third_party/terraform/utils/header_transport.go
+++ mmv1/third_party/terraform/utils/header_transport.go
@@ -1,6 +1,7 @@
 package google
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 )
@@ -20,6 +21,10 @@ func newTransportWithHeaders(baseTransit http.RoundTripper) headerTransportLayer
 	if requestReason := os.Getenv("CLOUDSDK_CORE_REQUEST_REASON"); requestReason != "" {
 		headers.Set("X-Goog-Request-Reason", requestReason)
 	}
+	// opt in extension for adding to the User-Agent header
+	if ext := os.Getenv("GOOGLE_TPG_USERAGENT_EXTENSION"); ext != "" {
+		headers.Set("User-Agent", ext)
+	}
 
 	return headerTransportLayer{Header: headers, baseTransit: baseTransit}
 }
@@ -29,6 +34,11 @@ func (h headerTransportLayer) RoundTrip(req *http.Request) (*http.Response, erro
 		// only set headers that are not previously defined
 		if _, ok := req.Header[key]; !ok {
 			req.Header[key] = value
+		} else if key == "User-Agent" {
+			ua := req.Header.Get(key)
+			ext := h.Header.Get(key)
+			newUa := fmt.Sprintf("%s %s", ua, ext)
+			req.Header.Set(key, newUa)
 		}
 	}
 	return h.baseTransit.RoundTrip(req)
