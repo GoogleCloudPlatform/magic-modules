65e6d01c4cef55fc486826c5dc28a6ab8132e1b1
Cameron Thornton <camthornton@google.com>
2021-03-04 12:04:23 -0600

    Documentation and docs substitutions

--- tpgtools/sample.go
+++ tpgtools/sample.go
@@ -4,39 +4,65 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+
+	"github.com/golang/glog"
 )
 
+// Samples is the array of samples read from the DCL
 type Samples []Sample
 
+// Sample is the object containing sample data from DCL samples
 type Sample struct {
-	// name is the name of a sample.
+	// Name is the name of a sample
 	Name *string
 
+	// Description is a short description of the sample
 	Description *string
 
+	// DependencyFileNames contains the filenames of every resource in the sample
 	DependencyFileNames []string `yaml:"dependency"`
 
+	// PrimaryResource is the filename of the sample's primary resource
 	PrimaryResource *string `yaml:"resource"`
 
+	// Substitutions contains every substition in the sample
 	Substitutions []Substitution `yaml:"extra_substitutions"`
 
+	// Dependencies is a list of objects containing metadata for each sample resource
 	Dependencies []Dependency
 }
 
+// Substitution contains metadata that varies for the sample context
 type Substitution struct {
+	// Substitution is the text to be substituted, e.g. topic_name
 	Substitution *string
-	TestValue    *string `yaml:"test_value"`
-	DocsValue    *string `yaml:"docs_value"`
+
+	// TestValue is the value of the substituted text for tests
+	TestValue *string `yaml:"test_value"`
+
+	// TestValue is the value of the substituted text for docs
+	DocsValue *string `yaml:"docs_value"`
 }
 
+// Dependency contains data that describes a single resource in a sample
 type Dependency struct {
-	FileName              string
-	HCLLocalName          string
-	DCLResourceType       string
+	// FileName is the name of the file as it appears in testcases.yaml
+	FileName string
+
+	// HCLLocalName is the local name of the HCL block, e.g. "basic" or "default"
+	HCLLocalName string
+
+	// DCLResourceType is the type represented in the DCL, e.g. "ComputeInstance"
+	DCLResourceType string
+
+	// TerraformResourceType is the type represented in Terraform, e.g. "google_compute_instance"
 	TerraformResourceType string
-	HCLBlock              string
+
+	// HCLBlock is the snippet of HCL config that declares this resource
+	HCLBlock string
 }
 
+// BuildDependency produces a Dependency using a file and filename
 func BuildDependency(fileName string, b []byte) (*Dependency, error) {
 	fileParts := strings.Split(fileName, ".")
 	if len(fileParts) != 4 {
@@ -65,6 +91,8 @@ func BuildDependency(fileName string, b []byte) (*Dependency, error) {
 	return d, nil
 }
 
+// ReplaceReferences substitutes any reference tags for their HCL address
+// This should only be called after every dependency for a sample is built
 func (s Sample) ReplaceReferences(d *Dependency) error {
 	re := regexp.MustCompile(`{{ref:([a-z.]*):(\w*)}}`)
 	matches := re.FindAllStringSubmatch(d.HCLBlock, -1)
@@ -110,11 +138,16 @@ func (s Sample) generateHCLTemplate() (string, error) {
 	return hcl, nil
 }
 
+// GenerateDocsHCL generates sample HCL using docs substitution metadata
 func (s Sample) GenerateDocsHCL() string {
-	hcl, _ := s.generateHCLTemplate()
+	hcl, err := s.generateHCLTemplate()
+	if err != nil {
+		glog.Exit(err)
+	}
+
+	for _, sub := range s.Substitutions {
+		re := regexp.MustCompile(fmt.Sprintf(`{{%s}}`, *sub.Substitution))
+		hcl = re.ReplaceAllString(hcl, *sub.DocsValue)
+	}
 	return hcl
 }
-
-// func generateTestHCL() string{
-
-// }
