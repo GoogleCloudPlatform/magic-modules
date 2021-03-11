dacf7e964d457b4f0fac697cf390a5e8a69b7440
Eric Zhao <zhaoe@google.com>
2021-03-12 03:22:54 +1100

    bigtable: update project placeholder  (#4584)
    * update project placeholder
    
    * update project placeholder

--- mmv1/third_party/validator/bigtable_cluster.go
+++ mmv1/third_party/validator/bigtable_cluster.go
@@ -5,21 +5,21 @@ import (
 )
 
 func GetBigtableClusterCaiObject(d TerraformResourceData, config *Config) ([]Asset, error) {
-	
-	objs, err := GetBigtableClusterApiObjects(d, config); 
-	
+
+	objs, err := GetBigtableClusterApiObjects(d, config)
+
 	if err != nil {
 		return []Asset{}, err
 	}
 
-    assets := []Asset{}
+	assets := []Asset{}
 	for _, obj := range objs {
-		name, err := assetName(d, config, "//bigtable.googleapis.com/projects/{{.Provider.project}}/instances/{{name}}/clusters/{{cluster_id}}")
+		name, err := assetName(d, config, "//bigtable.googleapis.com/projects/{{project}}/instances/{{name}}/clusters/{{cluster_id}}")
 		if err != nil {
 			return []Asset{}, err
 		}
 
-		asset := Asset {
+		asset := Asset{
 			Name: name,
 			Type: "bigtableadmin.googleapis.com/Cluster",
 			Resource: &AssetResource{
@@ -36,52 +36,52 @@ func GetBigtableClusterCaiObject(d TerraformResourceData, config *Config) ([]Ass
 
 func GetBigtableClusterApiObjects(d TerraformResourceData, config *Config) ([]map[string]interface{}, error) {
 	return expandBigtableClusters(d.Get("cluster"), d, config)
-	
+
 }
 
 func expandBigtableClusters(v interface{}, d TerraformResourceData, config *Config) ([]map[string]interface{}, error) {
 	l := v.([]interface{})
-  	if len(l) == 0 || l[0] == nil {
-    	return nil, nil
-  	}
-	
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
 	transformedEntries := []map[string]interface{}{}
 
-    for _, raw := range l {
+	for _, raw := range l {
 		original := raw.(map[string]interface{})
 		transformed := make(map[string]interface{})
-		
+
 		transformedLocation, err := expandBigtableClusterLocation(original["zone"], d, config)
 		if err != nil {
 			return nil, err
 		} else if val := reflect.ValueOf(transformedLocation); val.IsValid() && !isEmptyValue(val) {
-			transformed["location"] = transformedLocation      
+			transformed["location"] = transformedLocation
 		}
 
 		transformedServerNodes, err := expandBigtableClusterServerNodes(original["num_nodes"], d, config)
 		if err != nil {
 			return nil, err
 		} else if val := reflect.ValueOf(transformedServerNodes); val.IsValid() && !isEmptyValue(val) {
-			transformed["serverNodes"] = transformedServerNodes      
+			transformed["serverNodes"] = transformedServerNodes
 		}
 
 		transformedStorageType, err := expandBigtableClusterDefaultStorageType(original["storage_type"], d, config)
 		if err != nil {
 			return nil, err
 		} else if val := reflect.ValueOf(transformedStorageType); val.IsValid() && !isEmptyValue(val) {
-			transformed["defaultStorageType"] = transformedStorageType      
+			transformed["defaultStorageType"] = transformedStorageType
 		}
 
 		transformedName, err := expandBigtableClusterName(original["cluster_id"], d, config)
 		if err != nil {
 			return nil, err
 		} else if val := reflect.ValueOf(transformedName); val.IsValid() && !isEmptyValue(val) {
-			transformed["name"] = transformedName      
+			transformed["name"] = transformedName
 		}
 		transformedEntries = append(transformedEntries, transformed)
 	}
-    
-  	return transformedEntries, nil
+
+	return transformedEntries, nil
 }
 
 func expandBigtableClusterLocation(v interface{}, d TerraformResourceData, config *Config) (interface{}, error) {
@@ -99,7 +99,7 @@ func expandBigtableClusterDefaultStorageType(v interface{}, d TerraformResourceD
 func expandBigtableClusterName(v interface{}, d TerraformResourceData, config *Config) (interface{}, error) {
 	cluster, err := replaceVars(d, config, "projects/{{project}}/instances/{{name}}/clusters/")
 	if err != nil {
-	  return nil, err
+		return nil, err
 	}
 	return cluster + v.(string), nil
 }
