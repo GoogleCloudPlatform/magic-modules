296a601fa20b764ab595d6d1497434cbe569b1f1
Sam Levenick <slevenick@google.com>
2021-04-07 10:42:04 -0700

    Format for the last time!

--- mmv1/third_party/terraform/tests/resource_workflows_workflow_test.go
+++ mmv1/third_party/terraform/tests/resource_workflows_workflow_test.go
@@ -32,35 +32,35 @@ func TestAccWorkflowsWorkflow_basic(t *testing.T) {
 func testAccWorkflowsWorkflow_basic(name string) string {
 	return fmt.Sprintf(`
 resource "google_workflows_workflow" "example" {
-	name          = "%s"
-	region        = "us-central1"
-	description   = "Magic"
-	source_contents = <<-EOF
-	# This is a sample workflow, feel free to replace it with your source code
-	#
-	# This workflow does the following:
-	# - reads current time and date information from an external API and stores
-	#   the response in CurrentDateTime variable
-	# - retrieves a list of Wikipedia articles related to the day of the week
-	#   from CurrentDateTime
-	# - returns the list of articles as an output of the workflow
-	# FYI, In terraform you need to escape the $$ or it will cause errors.
+  name          = "%s"
+  region        = "us-central1"
+  description   = "Magic"
+  source_contents = <<-EOF
+  # This is a sample workflow, feel free to replace it with your source code
+  #
+  # This workflow does the following:
+  # - reads current time and date information from an external API and stores
+  #   the response in CurrentDateTime variable
+  # - retrieves a list of Wikipedia articles related to the day of the week
+  #   from CurrentDateTime
+  # - returns the list of articles as an output of the workflow
+  # FYI, In terraform you need to escape the $$ or it will cause errors.
 
-	- getCurrentTime:
-			call: http.get
-			args:
-					url: https://us-central1-workflowsample.cloudfunctions.net/datetime
-			result: CurrentDateTime
-	- readWikipedia:
-			call: http.get
-			args:
-					url: https://en.wikipedia.org/w/api.php
-					query:
-							action: opensearch
-							search: $${CurrentDateTime.body.dayOfTheWeek}
-			result: WikiResult
-	- returnOutput:
-			return: $${WikiResult.body[1]}
+  - getCurrentTime:
+      call: http.get
+      args:
+          url: https://us-central1-workflowsample.cloudfunctions.net/datetime
+      result: CurrentDateTime
+  - readWikipedia:
+      call: http.get
+      args:
+          url: https://en.wikipedia.org/w/api.php
+          query:
+              action: opensearch
+              search: $${CurrentDateTime.body.dayOfTheWeek}
+      result: WikiResult
+  - returnOutput:
+      return: $${WikiResult.body[1]}
 EOF
 }
 `, name)
@@ -69,39 +69,39 @@ EOF
 func testAccWorkflowsWorkflow_newBucket(name string) string {
 	return fmt.Sprintf(`
 resource "google_storage_bucket" "bucket" {
-	name = "%s"
+  name = "%s"
 }
 
 resource "google_workflows_workflow" "example" {
-	name          = "%s"
-	region        = "us-central1"
-	description   = "Magic"
-	source_contents = <<-EOF
-	# This is a sample workflow, feel free to replace it with your source code
-	#
-	# This workflow does the following:
-	# - reads current time and date information from an external API and stores
-	#   the response in CurrentDateTime variable
-	# - retrieves a list of Wikipedia articles related to the day of the week
-	#   from CurrentDateTime
-	# - returns the list of articles as an output of the workflow
-	# FYI, In terraform you need to escape the $$ or it will cause errors.
+  name          = "%s"
+  region        = "us-central1"
+  description   = "Magic"
+  source_contents = <<-EOF
+  # This is a sample workflow, feel free to replace it with your source code
+  #
+  # This workflow does the following:
+  # - reads current time and date information from an external API and stores
+  #   the response in CurrentDateTime variable
+  # - retrieves a list of Wikipedia articles related to the day of the week
+  #   from CurrentDateTime
+  # - returns the list of articles as an output of the workflow
+  # FYI, In terraform you need to escape the $$ or it will cause errors.
 
-	- getCurrentTime:
-			call: http.get
-			args:
-					url: https://us-central1-workflowsample.cloudfunctions.net/datetime
-			result: CurrentDateTime
-	- readWikipedia:
-			call: http.get
-			args:
-					url: https://en.wikipedia.org/w/api.php
-					query:
-							action: opensearch
-							search: $${CurrentDateTime.body.dayOfTheWeek}
-			result: WikiResult
-	- returnOutput:
-			return: $${WikiResult.body[1]}
+  - getCurrentTime:
+      call: http.get
+      args:
+          url: https://us-central1-workflowsample.cloudfunctions.net/datetime
+      result: CurrentDateTime
+  - readWikipedia:
+      call: http.get
+      args:
+          url: https://en.wikipedia.org/w/api.php
+          query:
+              action: opensearch
+              search: $${CurrentDateTime.body.dayOfTheWeek}
+      result: WikiResult
+  - returnOutput:
+      return: $${WikiResult.body[1]}
 EOF
 }
 `, name, name)
