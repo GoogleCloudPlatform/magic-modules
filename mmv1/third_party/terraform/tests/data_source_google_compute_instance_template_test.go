e42aec62cb05d5aadcccb79016a338f1d94f2b20
Riley Karson <rileykarson@google.com>
2021-04-14 10:12:00 -0700

    Add tf-test prefix to various instance templates

--- mmv1/third_party/terraform/tests/data_source_google_compute_instance_template_test.go
+++ mmv1/third_party/terraform/tests/data_source_google_compute_instance_template_test.go
@@ -72,7 +72,7 @@ func TestAccInstanceTemplateDatasource_filter_mostRecent(t *testing.T) {
 func testAccInstanceTemplate_name(project, suffix string) string {
 	return Nprintf(`
 resource "google_compute_instance_template" "default" {
-  name        = "test-template-%{suffix}"
+  name        = "tf-test-template-%{suffix}"
   description = "Example template."
 
   machine_type = "e2-small"
@@ -100,7 +100,7 @@ data "google_compute_instance_template" "default" {
 func testAccInstanceTemplate_filter(project, suffix string) string {
 	return Nprintf(`
 resource "google_compute_instance_template" "a" {
-  name        = "test-template-a-%{suffix}"
+  name        = "tf-test-template-a-%{suffix}"
   description = "Example template."
 
   machine_type = "e2-small"
@@ -118,7 +118,7 @@ resource "google_compute_instance_template" "a" {
   }
 }
 resource "google_compute_instance_template" "b" {
-  name        = "test-template-b-%{suffix}"
+  name        = "tf-test-template-b-%{suffix}"
   description = "Example template."
 
   machine_type = "e2-small"
@@ -136,7 +136,7 @@ resource "google_compute_instance_template" "b" {
   }
 }
 resource "google_compute_instance_template" "c" {
-  name        = "test-template-c-%{suffix}"
+  name        = "tf-test-template-c-%{suffix}"
   description = "Example template."
 
   machine_type = "e2-small"
@@ -165,7 +165,7 @@ data "google_compute_instance_template" "default" {
 func testAccInstanceTemplate_filter_mostRecent(project, suffix string) string {
 	return Nprintf(`
 resource "google_compute_instance_template" "a" {
-  name        = "test-template-%{suffix}-a"
+  name        = "tf-test-template-%{suffix}-a"
   description = "Example template."
 
   machine_type = "e2-small"
@@ -183,7 +183,7 @@ resource "google_compute_instance_template" "a" {
   }
 }
 resource "google_compute_instance_template" "b" {
-  name        = "test-template-%{suffix}-b"
+  name        = "tf-test-template-%{suffix}-b"
   description = "Example template."
 
   machine_type = "e2-small"
@@ -205,7 +205,7 @@ resource "google_compute_instance_template" "b" {
   ]
 }
 resource "google_compute_instance_template" "c" {
-  name        = "test-template-%{suffix}-c"
+  name        = "tf-test-template-%{suffix}-c"
   description = "Example template."
 
   machine_type = "e2-small"
