4a3fce01892790f9b90490047ef6b1bd63791fd2
upodroid <cy@borg.dev>
2021-04-30 23:42:45 +0100

    change path options

--- mmv1/third_party/terraform/resources/resource_storage_transfer_job.go
+++ mmv1/third_party/terraform/resources/resource_storage_transfer_job.go
@@ -393,7 +393,8 @@ func azureBlobStorageDataSchema() *schema.Resource {
 				Description: `The container to transfer from the Azure Storage account.`,
 			},
 			"path": {
-				Required:    true,
+				Optional:    true,
+				Computed:    true,
 				Type:        schema.TypeString,
 				Description: `Root path to transfer objects. Must be an empty string or full path name that ends with a '/'. This field is treated as an object prefix. As such, it should generally not begin with a '/'.`,
 			},
