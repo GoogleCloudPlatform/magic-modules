1697c42e046df792acf63315d2b3df2b3b04af59
upodroid <cy@borg.dev>
2021-05-26 22:07:16 +0100

    add secret manager secret datasource
    Co-authored-by: upodroid <cy@borg.dev>

---
+++ mmv1/third_party/terraform/data_sources/data_source_secret_manager_secret.go
@@ -0,0 +1,23 @@
+package google
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceSecretManagerSecret() *schema.Resource {
+
+	dsSchema := datasourceSchemaFromResourceSchema(resourceSecretManagerSecret().Schema)
+	addRequiredFieldsToSchema(dsSchema, "secret_id")
+	addOptionalFieldsToSchema(dsSchema, "project")
+
+	return &schema.Resource{
+		Read:   dataSourceSecretManagerSecretRead(),
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceSecretManagerSecretRead(d *schema.ResourceData, meta interface{}) error {
+
+	return resourceSecretManagerSecretRead(d, meta)
+
+}
