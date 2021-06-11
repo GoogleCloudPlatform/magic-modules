d05e368e926d4673ef7a7a82290fdb34250ad329
Stephen Lewis (Burrows) <stephenrlewis@google.com>
2021-06-11 15:28:12 -0700

    Separated out tgc test results as a different status (#4865)

--- .ci/containers/terraform-google-conversion-tester/test_terraform_google_conversion.sh
+++ .ci/containers/terraform-google-conversion-tester/test_terraform_google_conversion.sh
@@ -2,14 +2,56 @@
 
 set -e
 
-PR_NUMBER=$1
-SCRATCH_OWNER=modular-magician
-GH_REPO=terraform-google-conversion
+pr_number=$1
+mm_commit_sha=$2
+build_id=$3
+project_id=$4
+github_username=modular-magician
+gh_repo=terraform-google-conversion
+build_step="21"
 
-SCRATCH_PATH=https://$SCRATCH_OWNER:$GITHUB_TOKEN@github.com/$SCRATCH_OWNER/$GH_REPO
-LOCAL_PATH=$GOPATH/src/github.com/GoogleCloudPlatform/$GH_REPO
-mkdir -p "$(dirname $LOCAL_PATH)"
-git clone $SCRATCH_PATH $LOCAL_PATH --single-branch --branch "auto-pr-$PR_NUMBER" --depth 1
-pushd $LOCAL_PATH
+scratch_path=https://$github_username:$GITHUB_TOKEN@github.com/$github_username/$gh_repo
+local_path=$GOPATH/src/github.com/GoogleCloudPlatform/$gh_repo
+
+post_body="{"
+post_body+='"context":"terraform-google-conversion-test",'
+post_body+='"target_url":"https://console.cloud.google.com/cloud-build/builds;region=global/'"$build_id"';step='"$build_step"'?project='"$project_id"'",'
+post_body+='"state":"success"'
+post_body+="}"
+
+curl \
+  -X POST \
+  -u "$github_username:$GITHUB_TOKEN" \
+  -H "Accept: application/vnd.github.v3+json" \
+  "https://api.github.com/repos/GoogleCloudPlatform/magic-modules/statuses/$mm_commit_sha" \
+  -d "$post_body"
+
+mkdir -p "$(dirname $local_path)"
+git clone $scratch_path $local_path --single-branch --branch "auto-pr-$pr_number" --depth 1
+pushd $local_path
+
+set +e
 
 make test
+exit_code=$?
+
+set -e
+
+if [ $exitCode -ne 0 ]; then
+	state="failure"
+else
+	state="success"
+fi
+
+post_body="{"
+post_body+='"context":"terraform-google-conversion-test",'
+post_body+='"target_url":"https://console.cloud.google.com/cloud-build/builds;region=global/'"$build_id"';step='"$build_step"'?project='"$project_id"'",'
+post_body+='"state":"'"$state"'"'
+post_body+="}"
+
+curl \
+  -X POST \
+  -u "$github_username:$GITHUB_TOKEN" \
+  -H "Accept: application/vnd.github.v3+json" \
+  "https://api.github.com/repos/GoogleCloudPlatform/magic-modules/statuses/$mm_commit_sha" \
+  -d "$post_body"
