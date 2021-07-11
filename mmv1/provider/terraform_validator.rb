9e098a075d566ab8748e458b9adaa458f6e245de
Palani Ram <palani.ram@googlecloud.corp-partner.google.com>
2021-07-11 11:24:14 -0400

    terraform object library, fix the comma and upgrade ruby version to 2.7.3

--- mmv1/provider/terraform_validator.rb
+++ mmv1/provider/terraform_validator.rb
@@ -67,7 +67,7 @@ module Provider
                           ['google/transport.go',
                            'third_party/terraform/utils/transport.go.erb'],
                           ['google/mappers.go',
-                           'templates/validator/mappers/mappers.go.erb']
+                           'templates/validator/mappers/mappers.go.erb'],
                           ['google/iam_kms_key_ring.go',
                            'third_party/terraform/utils/iam_kms_key_ring.go.erb'],
                           ['google/iam_kms_crypto_key.go',
