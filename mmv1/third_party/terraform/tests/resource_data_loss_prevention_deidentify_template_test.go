83fc6d164434e6792e7a466c0f1358580b9a2684
Scott Suarez <ScottMSuarez@gmail.com>
2021-07-14 16:03:00 -0700

    add cryptography fields to dlp deidentify template

--- mmv1/third_party/terraform/tests/resource_data_loss_prevention_deidentify_template_test.go
+++ mmv1/third_party/terraform/tests/resource_data_loss_prevention_deidentify_template_test.go
@@ -102,8 +102,103 @@ resource "google_data_loss_prevention_deidentify_template" "basic" {
 					}
 				}
 			}
+
+      transformations {
+        info_types {
+          name = "CREDIT_CARD_NUMBER2322"
+        }
+
+        primitive_transformation {
+          crypto_deterministic_config {
+            context {
+              name = "sometweak"
+            }
+            crypto_key {
+              kms_wrapped {
+                wrapped_key     = "B64/WRAPPED/TOKENIZATION/KEY"
+                crypto_key_name = google_kms_crypto_key.my_key.id
+              }
+            }
+            surrogate_info_type {
+              name = "abc"
+            }
+          }
+        }
+      }
+      transformations {
+        info_types {
+          name = "CUSTOM_INFO_TYPE"
+        }
+
+        primitive_transformation {
+          crypto_deterministic_config {
+            crypto_key {
+              transient {
+                name = "beep"
+              }
+            }
+            surrogate_info_type {
+              name = "CUSTOM_INFO_TYPE"
+            }
+          }
+        }
+      }
+      transformations {
+        info_types {
+          name = "PHONE_NUMBER2"
+        }
+        primitive_transformation {
+          crypto_replace_ffx_fpe_config {
+            context {
+              name = "someTweak"
+            }
+            crypto_key {
+              kms_wrapped {
+                wrapped_key     = "B64/WRAPPED/TOKENIZATION/KEY"
+                crypto_key_name = google_kms_crypto_key.my_key.id
+              }
+            }
+            radix = 10
+            surrogate_info_type {
+              name = "CUSTOM_INFO_TYPE"
+            }
+          }
+        }
+      }
+
+      transformations {
+        info_types {
+          name = "SSN"
+        }
+        primitive_transformation {
+          crypto_replace_ffx_fpe_config {
+            common_alphabet = "UPPER_CASE_ALPHA_NUMERIC"
+            context {
+              name = "someTweak"
+            }
+            crypto_key {
+              transient {
+                name = "beep"
+              }
+            }
+            surrogate_info_type {
+              name = "CUSTOM_INFO_TYPE"
+            }
+          }
+        }
+      }
 		}
 	}
+}
+
+resource "google_kms_crypto_key" "my_key" {
+  name     = "tf-test-example-k%{random_suffix}"
+  key_ring = google_kms_key_ring.key_ring.id
+}
+
+resource "google_kms_key_ring" "key_ring" {
+  name     = "tf-test-example-keyr%{random_suffix}"
+  location = "global"
 }
 `, context)
 }
@@ -164,8 +259,103 @@ resource "google_data_loss_prevention_deidentify_template" "basic" {
 					}
 				}
 			}
+
+			transformations {
+        info_types {
+          name = "CREDIT_CARD_NUMBERR"
+        }
+
+        primitive_transformation {
+          crypto_deterministic_config {
+            context {
+              name = "sometweakd"
+            }
+            crypto_key {
+              kms_wrapped {
+                wrapped_key     = "B64/WRAPPED/TOKENIZATION/KEY"
+                crypto_key_name = google_kms_crypto_key.my_key.id
+              }
+            }
+            surrogate_info_type {
+              name = "abcd"
+            }
+          }
+        }
+      }
+      transformations {
+        info_types {
+          name = "CUSTOM_INFO_TYPE"
+        }
+
+        primitive_transformation {
+          crypto_deterministic_config {
+            crypto_key {
+              transient {
+                name = "beeper"
+              }
+            }
+            surrogate_info_type {
+              name = "CUSTOM_INFO_TYPEf"
+            }
+          }
+        }
+      }
+      transformations {
+        info_types {
+          name = "PHONE_NUMBER2"
+        }
+        primitive_transformation {
+          crypto_replace_ffx_fpe_config {
+            context {
+              name = "someTweaker"
+            }
+            crypto_key {
+              kms_wrapped {
+                wrapped_key     = "B64/WRAPPED/TOKENIZATION/KEY"
+                crypto_key_name = google_kms_crypto_key.my_key.id
+              }
+            }
+            radix = 10
+            surrogate_info_type {
+              name = "CUSTOM_INFO_TYPEF"
+            }
+          }
+        }
+      }
+
+      transformations {
+        info_types {
+          name = "SSN"
+        }
+        primitive_transformation {
+          crypto_replace_ffx_fpe_config {
+            common_alphabet = "UPPER_CASE_ALPHA_NUMERIC"
+            context {
+              name = "someTweak2"
+            }
+            crypto_key {
+              transient {
+                name = "beepf"
+              }
+            }
+            surrogate_info_type {
+              name = "CUSTOM_INFO_TYPE"
+            }
+          }
+        }
+      }
 		}
 	}
+}
+
+resource "google_kms_crypto_key" "my_key" {
+  name     = "tf-test-example-k%{random_suffix}"
+  key_ring = google_kms_key_ring.key_ring.id
+}
+
+resource "google_kms_key_ring" "key_ring" {
+  name     = "tf-test-example-keyr%{random_suffix}"
+  location = "global"
 }
 `, context)
 }
