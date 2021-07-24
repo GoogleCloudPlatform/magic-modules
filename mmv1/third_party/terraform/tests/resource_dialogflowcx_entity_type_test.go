cb8fbb9d72a180f0ab7dc30a3f016e090bfb1cdd
“Javier <javier-canizalez@outlook.com>
2021-07-23 18:26:35 -0600

    Updated Entity Type tests

--- mmv1/third_party/terraform/tests/resource_dialogflowcx_entity_type_test.go
+++ mmv1/third_party/terraform/tests/resource_dialogflowcx_entity_type_test.go
@@ -1,4 +1,3 @@
-<% autogen_exception -%>
 package google
 
 import (
@@ -24,17 +23,17 @@ func TestAccDialogflowCXEntityType_update(t *testing.T) {
 				Config: testAccDialogflowCXEntityType_basic(context),
 			},
 			{
-				ResourceName:            "google_dialogflow_cx_entity_type.my_entity",
-				ImportState:             true,
-				ImportStateVerify:       true,
+				ResourceName:      "google_dialogflow_cx_entity_type.my_entity",
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 			{
 				Config: testAccDialogflowCXEntityType_full(context),
 			},
 			{
-				ResourceName:            "google_dialogflow_cx_entity_type.my_entity",
-				ImportState:             true,
-				ImportStateVerify:       true,
+				ResourceName:      "google_dialogflow_cx_entity_type.my_entity",
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 		},
 	})
@@ -91,34 +90,35 @@ func testAccDialogflowCXEntityType_full(context map[string]interface{}) string {
 	}
 
 	resource "google_dialogflow_cx_agent" "agent_entity" {
-		display_name = "tf-test-%{random_suffix}update"
+		display_name = "tf-test-%{random_suffix}"
 		location = "global"
 		default_language_code = "en"
-		supported_language_codes = ["no"]
-		time_zone = "Europe/London"
-		description = "Description 2!"
-		avatar_uri = "https://storage.cloud.google.com/dialogflow-test-host-image/cloud-logo-2.png"
-		enable_stackdriver_logging = true
-        enable_spell_correction    = true
-		speech_to_text_settings {
-			enable_speech_adaptation = true
-		}
+		supported_language_codes = ["fr","de","es"]
+		time_zone = "America/New_York"
+		description = "Description 1."
+		avatar_uri = "https://storage.cloud.google.com/dialogflow-test-host-image/cloud-logo.png"
 		depends_on = [google_project_iam_member.agent_create]
 	}
     
 	resource "google_dialogflow_cx_entity_type" "my_entity" {
-        parent       = google_dialogflow_cx_agent.agent_entity.id
-        display_name  = "MyEntity"
-        kind         = "KIND_MAP"
+        parent           = google_dialogflow_cx_agent.agent_entity.id
+        display_name     = "MyEntity"
+        kind             = "KIND_MAP"
         entities {
             value = "value1"
-            synonyms = ["synonym1","synonym2"]
+            synonyms = ["synonym1","synonym2","synonym11","synonym22"]
         }
         entities {
             value = "value2"
             synonyms = ["synonym3","synonym4"]
         }
         enable_fuzzy_extraction = false
+		redact                  = true
+		auto_expansion_mode     = "AUTO_EXPANSION_MODE_DEFAULT"
+		excluded_phrases {
+			value = "excluded1"
+		}
+		
     } 
 	  `, context)
 }
