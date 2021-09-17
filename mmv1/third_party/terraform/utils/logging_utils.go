088322c5769bffb6de7c02e3847853cd2e91e95f
Alexander Makarenko <estliberitas@gmail.com>
2021-09-17 12:23:36 +0200

    ♻️ Make Logging Sink resources work with auto-created sinks

--- mmv1/third_party/terraform/utils/logging_utils.go
+++ mmv1/third_party/terraform/utils/logging_utils.go
@@ -13,31 +13,14 @@ var loggingSinkResourceTypes = []string{
 	"projects",
 }
 
-// LoggingSinkId represents the parts that make up the canonical id used within terraform for a logging resource.
-type LoggingSinkId struct {
-	resourceType string
-	resourceId   string
-	name         string
-}
-
 // loggingSinkIdRegex matches valid logging sink canonical ids
 var loggingSinkIdRegex = regexp.MustCompile("(.+)/(.+)/sinks/(.+)")
 
-// canonicalId returns the LoggingSinkId as the canonical id used within terraform.
-func (l LoggingSinkId) canonicalId() string {
-	return fmt.Sprintf("%s/%s/sinks/%s", l.resourceType, l.resourceId, l.name)
-}
-
-// parent returns the "parent-level" resource that the sink is in (e.g. `folders/foo` for id `folders/foo/sinks/bar`)
-func (l LoggingSinkId) parent() string {
-	return fmt.Sprintf("%s/%s", l.resourceType, l.resourceId)
-}
-
-// parseLoggingSinkId parses a canonical id into a LoggingSinkId, or returns an error on failure.
-func parseLoggingSinkId(id string) (*LoggingSinkId, error) {
+// parseLoggingSinkParentId parses a canonical id to get sink parent resource id
+func parseLoggingSinkParentId(id string) (string, error) {
 	parts := loggingSinkIdRegex.FindStringSubmatch(id)
 	if parts == nil {
-		return nil, fmt.Errorf("unable to parse logging sink id %#v", id)
+		return "", fmt.Errorf("unable to parse logging sink id %#v", id)
 	}
 	// If our resourceType is not a valid logging sink resource type, complain loudly
 	validLoggingSinkResourceType := false
@@ -49,12 +32,8 @@ func parseLoggingSinkId(id string) (*LoggingSinkId, error) {
 	}
 
 	if !validLoggingSinkResourceType {
-		return nil, fmt.Errorf("Logging resource type %s is not valid. Valid resource types: %#v", parts[1],
+		return "", fmt.Errorf("Logging resource type %s is not valid. Valid resource types: %#v", parts[1],
 			loggingSinkResourceTypes)
 	}
-	return &LoggingSinkId{
-		resourceType: parts[1],
-		resourceId:   parts[2],
-		name:         parts[3],
-	}, nil
+	return parts[2], nil
 }
