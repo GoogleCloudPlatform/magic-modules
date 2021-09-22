890fd24081a83ee599a144de515ef91d9cce58f6
Scott Suarez <ScottSuarez@google.com>
2021-09-22 16:46:58 -0700

    Update product.rb
    added reference to comment

--- mmv1/api/product.rb
+++ mmv1/api/product.rb
@@ -49,7 +49,7 @@ module Api
     # A function reference designed for the rare case where you
     # need to use retries in operation calls. Used for the service api
     # as it enables itself (self referential) and can result in occasional
-    # failures on operation_get. see
+    # failures on operation_get. see https://git.io/Jzu04
     attr_reader :operation_retry
 
     # The APIs required to be enabled for this product.
