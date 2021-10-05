7a2851665560c6ab89aa60d51735b4fcda0a7766
Edward Sun <ed2004sun@gmail.com>
2021-10-05 05:45:22 -0700

    change label

--- mmv1/third_party/terraform/tests/resource_billing_budget_test.go
+++ mmv1/third_party/terraform/tests/resource_billing_budget_test.go
@@ -44,7 +44,7 @@ resource "google_billing_budget" "budget" {
   budget_filter {
     projects = ["projects/${data.google_project.project.number}"]
     labels  = {
-      "foo" = "bar"
+      label = "bar"
     }
   }
 
