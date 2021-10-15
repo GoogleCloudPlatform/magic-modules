efe3ad4e3cd7ad36a94572fcfa7fb309d684e73a
Riley Karson <rileykarson@google.com>
2021-10-15 15:07:34 -0700

    Make the project field required for project IAM resources

--- mmv1/third_party/terraform/tests/resource_dialogflowcx_intent_test.go
+++ mmv1/third_party/terraform/tests/resource_dialogflowcx_intent_test.go
@@ -41,11 +41,14 @@ func TestAccDialogflowCXIntent_update(t *testing.T) {
 
 func testAccDialogflowCXIntent_basic(context map[string]interface{}) string {
 	return Nprintf(`
+  data "google_project" "project" {}
+
 	resource "google_service_account" "dialogflowcx_service_account" {
 		account_id = "tf-test-dialogflow-%{random_suffix}"
 	}
-	  
+
 	resource "google_project_iam_member" "agent_create" {
+    project = data.google_project.project.project_id
 		role    = "roles/dialogflow.admin"
 		member  = "serviceAccount:${google_service_account.dialogflowcx_service_account.email}"
 	}
@@ -102,6 +105,7 @@ func testAccDialogflowCXIntent_full(context map[string]interface{}) string {
 	}
 	  
 	resource "google_project_iam_member" "agent_create" {
+    project = data.google_project.project.project_id
 		role    = "roles/dialogflow.admin"
 		member  = "serviceAccount:${google_service_account.dialogflowcx_service_account.email}"
 	}
