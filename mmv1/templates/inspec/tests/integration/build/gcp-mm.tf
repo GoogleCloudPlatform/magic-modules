d83129c09fa1cab3c11b313b65941d6ab43e587e
sa-progress <samir.anand@progress.com>
2021-10-27 14:58:19 +0530

    added resource for google_compute_external_vpn_gateway

--- mmv1/templates/inspec/tests/integration/build/gcp-mm.tf
+++ mmv1/templates/inspec/tests/integration/build/gcp-mm.tf
@@ -1317,3 +1317,13 @@ resource "google_memcache_instance" "instance" {
   }
   node_count = 1
 }
+
+resource "google_compute_external_vpn_gateway" "external_gateway" {
+  name            = "external-gateway"
+  redundancy_type = "SINGLE_IP_INTERNALLY_REDUNDANT"
+  description     = "An externally managed VPN gateway"
+  interface {
+    id         = 0
+    ip_address = "8.8.8.8"
+  }
+}
