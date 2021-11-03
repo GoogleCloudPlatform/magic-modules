1640b51c284baac671bc594e5ed10ae7eb2fb62f
Palani Ram <palani.ram@googlecloud.corp-partner.google.com>
2021-11-03 07:42:26 -0400

    Added compile file for bigquery dataset IAM

--- mmv1/provider/terraform_validator.rb
+++ mmv1/provider/terraform_validator.rb
@@ -192,7 +192,7 @@ module Provider
                        ['converters/google/resources/utils.go',
                         'third_party/terraform/utils/utils.go'],
                        ['converters/google/resources/bigquery_dataset_iam.go',
-                        'third_party/terraform/utils/bigquery_dataset_iam.go'],
+                        'third_party/validator/bigquery_dataset_iam.go'],
                        ['converters/google/resources/kms_key_ring_iam.go',
                         'third_party/validator/kms_key_ring_iam.go'],
                        ['converters/google/resources/kms_crypto_key_iam.go',
