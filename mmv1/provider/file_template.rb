527a6f6d6773daea2c1c0b99f8520831fb13dc2a
Scott Suarez <ScottMSuarez@gmail.com>
2022-01-14 09:53:51 -0800

    ad back gofmt

--- mmv1/provider/file_template.rb
+++ mmv1/provider/file_template.rb
@@ -81,6 +81,8 @@ module Provider
     # generated
     def format_output_file(path, template)
       if path.end_with?('.go') && @env[:goformat_enabled]
+        run_formatter("gofmt -w -s #{path}")
+
         run_formatter("goimports -w #{path}") unless template.include?('third_party/terraform')
       end
     end
