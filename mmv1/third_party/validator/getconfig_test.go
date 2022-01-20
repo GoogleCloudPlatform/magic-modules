dc59fb6df4067adedc2b9379d6df1f10b11aa73e
Stephen Lewis (Burrows) <stephenrlewis@google.com>
2022-01-20 09:33:06 -0800

    Generate GetConfig function into the resources package (#5624)
    This is google-specific code so it makes sense in terms of grouping; it also allows GetConfig to access private attributes on the config object

--- mmv1/third_party/validator/getconfig_test.go
+++ mmv1/third_party/validator/getconfig_test.go
@@ -19,15 +19,6 @@ func getAccessToken(cfg *Config) string {
 func getImpersonateServiceAccount(cfg *Config) string {
 	return cfg.ImpersonateServiceAccount
 }
-func getUserAgent(cfg *Config) string {
-	return cfg.UserAgent()
-}
-func getZoneValue(cfg *Config) string {
-	return cfg.Zone
-}
-func getRegionValue(cfg *Config) string {
-	return cfg.Region
-}
 
 func TestGetConfigExtractsEnvVars(t *testing.T) {
 	ctx := context.Background()
@@ -36,92 +27,37 @@ func TestGetConfigExtractsEnvVars(t *testing.T) {
 		name           string
 		envKey         string
 		envValue       string
-		expected       string
 		getConfigValue configAttrGetter
 	}{
 		{
 			name:           "GOOGLE_CREDENTIALS",
 			envKey:         "GOOGLE_CREDENTIALS",
 			envValue:       "whatever",
-			expected:       "whatever",
 			getConfigValue: getCredentials,
 		},
 		{
 			name:           "GOOGLE_CLOUD_KEYFILE_JSON",
 			envKey:         "GOOGLE_CLOUD_KEYFILE_JSON",
 			envValue:       "whatever",
-			expected:       "whatever",
 			getConfigValue: getCredentials,
 		},
 		{
 			name:           "GCLOUD_KEYFILE_JSON",
 			envKey:         "GCLOUD_KEYFILE_JSON",
 			envValue:       "whatever",
-			expected:       "whatever",
 			getConfigValue: getCredentials,
 		},
 		{
 			name:           "GOOGLE_OAUTH_ACCESS_TOKEN",
 			envKey:         "GOOGLE_OAUTH_ACCESS_TOKEN",
 			envValue:       "whatever",
-			expected:       "whatever",
 			getConfigValue: getAccessToken,
 		},
 		{
 			name:           "GOOGLE_IMPERSONATE_SERVICE_ACCOUNT",
 			envKey:         "GOOGLE_IMPERSONATE_SERVICE_ACCOUNT",
 			envValue:       "whatever",
-			expected:       "whatever",
 			getConfigValue: getImpersonateServiceAccount,
-		},
-		{
-			name:            "GOOGLE_ZONE",
-		        envKey:          "GOOGLE_ZONE",
-		        envValue:        "whatever",
-		        expected:        "whatever",
-		        getConfigValue:  getZoneValue,
-	        },
-		{
-			name:           "GCLOUD_ZONE",
-			envKey:         "GCLOUD_ZONE",
-			envValue:       "whatever",
-			expected:       "whatever",
-			getConfigValue: getZoneValue,
-		},
-		{
-			name:           "CLOUDSDK_COMPUTE_ZONE",
-			envKey:         "CLOUDSDK_COMPUTE_ZONE",
-			envValue:       "whatever",
-			expected:       "whatever",
-			getConfigValue: getZoneValue,
-		},
-		{
-			name:           "GOOGLE_REGION",
-			envKey:         "GOOGLE_REGION",
-			envValue:       "whatever",
-			expected:       "whatever",
-			getConfigValue: getRegionValue,
-		},
-		{
-			name:           "GCLOUD_REGION",
-			envKey:         "GCLOUD_REGION",
-			envValue:       "whatever",
-			expected:       "whatever",
-			getConfigValue: getRegionValue,
-		},
-		{
-			name:           "CLOUDSDK_COMPUTE_REGION",
-			envKey:         "CLOUDSDK_COMPUTE_REGION",
-			envValue:       "whatever",
-			expected:       "whatever",
-			getConfigValue: getRegionValue,
-		},
-		{
-			name:           "GOOGLE_TERRAFORM_VALIDATOR_USERAGENT_EXTENSION",
-			envKey:         "GOOGLE_TERRAFORM_VALIDATOR_USERAGENT_EXTENSION",
-			envValue:       "whatever",
-			expected:       "config-validator-tf/dev whatever",
-			getConfigValue: getUserAgent,
 		},
 	}
 
@@ -138,7 +74,7 @@ func TestGetConfigExtractsEnvVars(t *testing.T) {
 				t.Fatalf("error building converter: %s", err)
 			}
 
-			assert.Equal(t, c.expected, c.getConfigValue(cfg))
+			assert.EqualValues(t, c.getConfigValue(cfg), c.envValue)
 
 			if isSet {
 				err = os.Setenv(c.envKey, originalValue)
