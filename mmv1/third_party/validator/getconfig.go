40d4e9ff150553c83c89b6469c7b2cb46b822cdb
Stephen Lewis (Burrows) <stephenrlewis@google.com>
2022-01-24 09:52:40 -0800

    Allow extension of default user agent in GetConfig (#5639)

--- mmv1/third_party/validator/getconfig.go
+++ mmv1/third_party/validator/getconfig.go
@@ -2,13 +2,23 @@ package google
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"github.com/pkg/errors"
+
+	"github.com/GoogleCloudPlatform/terraform-validator/version"
 )
+
+// Return the value of the private userAgent field
+func (c *Config) UserAgent() string {
+	return c.userAgent
+}
 
 func GetConfig(ctx context.Context, project string, offline bool) (*Config, error) {
 	cfg := &Config{
-		Project: project,
+		Project:   project,
+		userAgent: fmt.Sprintf("config-validator-tf/%s", version.BuildVersion()),
 	}
 
 	// Search for default credentials
@@ -25,6 +35,12 @@ func GetConfig(ctx context.Context, project string, offline bool) (*Config, erro
 	cfg.ImpersonateServiceAccount = multiEnvSearch([]string{
 		"GOOGLE_IMPERSONATE_SERVICE_ACCOUNT",
 	})
+
+	// opt in extension for adding to the User-Agent header
+	if ext := os.Getenv("GOOGLE_TERRAFORM_VALIDATOR_USERAGENT_EXTENSION"); ext != "" {
+		ua := cfg.userAgent
+		cfg.userAgent = fmt.Sprintf("%s %s", ua, ext)
+	}
 
 	if !offline {
 		ConfigureBasePaths(cfg)
