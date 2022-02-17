5eda72a8c9fb6043978cd852079e772928ddfaf0
Himani Khanduja <khimani@google.com>
2022-02-17 17:54:29 +0000

    Add support to enable read replicas on existing redis instances
    Add support to enable read replicas on existing redis instances

--- mmv1/third_party/terraform/tests/resource_redis_instance_test.go
+++ mmv1/third_party/terraform/tests/resource_redis_instance_test.go
@@ -95,7 +95,7 @@ func TestAccRedisInstance_updateReadReplicasModeWithAutoSecondaryIp(t *testing.T
 				ImportStateVerify: true,
 			},
 			{
-				Config: testAccRedisInstanceReadReplicasEnabledWithAutoSecondaryIp(name, true),
+				Config: testAccRedisInstanceReadReplicasEnabledWithAutoSecondaryIP(name, true),
 			},
 			{
 				ResourceName:      "google_redis_instance.test",
@@ -249,6 +249,45 @@ func TestAccRedisInstance_redisInstanceAuthEnabled(t *testing.T) {
 			},
 		},
 	})
+}
+
+func TestSecondaryIpDiffSuppress(t *testing.T) {
+	cases := map[string]struct {
+		Old, New           string
+		ExpectDiffSuppress bool
+	}{
+		"empty strings": {
+			Old:                "",
+			New:                "",
+			ExpectDiffSuppress: true,
+		},
+		"auto range": {
+			Old:                "",
+			New:                "auto",
+			ExpectDiffSuppress: false,
+		},
+		"auto on already applied range": {
+			Old:                "10.0.0.0/28",
+			New:                "auto",
+			ExpectDiffSuppress: true,
+		},
+		"same ranges": {
+			Old:                "10.0.0.0/28",
+			New:                "10.0.0.0/28",
+			ExpectDiffSuppress: true,
+		},
+		"different ranges": {
+			Old:                "10.0.0.0/28",
+			New:                "10.1.2.3/28",
+			ExpectDiffSuppress: false,
+		},
+	}
+
+	for tn, tc := range cases {
+		if secondaryIpDiffSuppress("whatever", tc.Old, tc.New, nil) != tc.ExpectDiffSuppress {
+			t.Fatalf("bad: %s, '%s' => '%s' expect %t", tn, tc.Old, tc.New, tc.ExpectDiffSuppress)
+		}
+	}
 }
 
 func TestAccRedisInstance_downgradeRedisVersion(t *testing.T) {
