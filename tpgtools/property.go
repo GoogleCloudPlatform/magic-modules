6e047f7a74f76c2b60911b6e26f39b70e22d8d01
Sam Levenick <slevenick@google.com>
2022-03-09 13:16:35 -0800

    Add apikeys key resource (#5637)
    * Add apikeys key resource
    
    * Update branch
    
    * Update go.mod & sum for required fields

--- tpgtools/property.go
+++ tpgtools/property.go
@@ -641,7 +641,10 @@ func createPropertiesFromSchema(schema *openapi.Schema, typeFetcher *TypeFetcher
 		}
 
 		if !p.Computed {
-			glog.Infof("Looking for %q in %v.", v.Title, schema.Required)
+			if v.Title == "allowedApplications" {
+				glog.Errorf("Looking for %#v.", schema)
+			}
+			glog.Errorf("Looking for %q in %v.", v.Title, schema.Required)
 			if stringInSlice(v.Title, schema.Required) {
 				p.Required = true
 			} else {
