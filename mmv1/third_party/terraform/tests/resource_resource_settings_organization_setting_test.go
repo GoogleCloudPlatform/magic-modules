cfe11dbb305f96aa3bbce83df944a701903d3ea3
Nick Stogner <nicholas.stogner@gmail.com>
2022-03-28 20:10:42 -0400

    Add resource_settings_organization_setting

---
+++ mmv1/third_party/terraform/tests/resource_resource_settings_organization_setting_test.go
@@ -0,0 +1,82 @@
+package google
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccResourceSettingsOrganizationSetting_update(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"org_id": getTestOrgFromEnv(t),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceSettingsOrganizationSetting_start(context),
+			},
+			{
+				ResourceName:      "google_resource_settings_organization_setting.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccResourceSettingsOrganizationSetting_update(context),
+			},
+			{
+				ResourceName:      "google_resource_settings_organization_setting.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccResourceSettingsOrganizationSetting_end(context),
+			},
+			{
+				ResourceName:      "google_resource_settings_organization_setting.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccResourceSettingsOrganizationSetting_start(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_resource_settings_organization_setting" "default" {
+  organization_id = "%{org_id}"
+  name = "iam-serviceAccountKeyExpiry"
+  local_value {
+    string_value = "24hours"
+  }
+}
+`, context)
+}
+
+func testAccResourceSettingsOrganizationSetting_update(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_resource_settings_organization_setting" "default" {
+  organization_id = "%{org_id}"
+  name = "iam-serviceAccountKeyExpiry"
+  local_value {
+    string_value = "7days"
+  }
+}
+`, context)
+}
+
+func testAccResourceSettingsOrganizationSetting_end(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_resource_settings_organization_setting" "default" {
+  organization_id = "%{org_id}"
+  name = "iam-serviceAccountKeyExpiry"
+  local_value {
+    string_value = "never"
+  }
+}
+`, context)
+}
