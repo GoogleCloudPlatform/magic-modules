6361ecce58a37018a5cd9386ba161a7918dcb923
Yong Cao <yongcy@google.com>
2022-04-12 18:27:01 -0400

    Add a new field `is_staged` to create a CertificateAuthority in `STAGED` state.

--- mmv1/third_party/terraform/tests/resource_privateca_certificate_authority_test.go
+++ mmv1/third_party/terraform/tests/resource_privateca_certificate_authority_test.go
@@ -6,6 +6,38 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
+func TestAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityIsStaged(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"pool_name":           BootstrapSharedCaPoolInLocation(t, "us-central1"),
+		"pool_location":       "us-central1",
+		"deletion_protection": false,
+		"random_suffix":       randString(t, 10),
+	}
+
+	resourceName := "google_privateca_certificate_authority.default"
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckPrivatecaCertificateAuthorityDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityIsStaged(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "state", "STAGED"),
+				),
+			},
+			{
+				Config: testAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityBasicRoot(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "state", "ENABLED"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityUpdate(t *testing.T) {
 	t.Parallel()
 
@@ -22,7 +54,7 @@ func TestAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityUpdate(t 
 		CheckDestroy: testAccCheckPrivatecaCertificateAuthorityDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityStart(context),
+				Config: testAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityBasicRoot(context),
 			},
 			{
 				ResourceName:            "google_privateca_certificate_authority.default",
@@ -40,7 +72,7 @@ func TestAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityUpdate(t 
 				ImportStateVerifyIgnore: []string{"ignore_active_certificates_on_deletion", "location", "certificate_authority_id", "pool"},
 			},
 			{
-				Config: testAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityStart(context),
+				Config: testAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityBasicRoot(context),
 			},
 			{
 				ResourceName:            "google_privateca_certificate_authority.default",
@@ -52,7 +84,7 @@ func TestAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityUpdate(t 
 	})
 }
 
-func testAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityStart(context map[string]interface{}) string {
+func testAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityBasicRoot(context map[string]interface{}) string {
 	return Nprintf(`
 resource "google_privateca_certificate_authority" "default" {
 	// This example assumes this pool already exists.
@@ -154,6 +186,59 @@ resource "google_privateca_certificate_authority" "default" {
 	}
 	labels = {
 		foo = "bar"
+	}
+}
+`, context)
+}
+
+func testAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityIsStaged(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_privateca_certificate_authority" "default" {
+	// This example assumes this pool already exists.
+	// Pools cannot be deleted in normal test circumstances, so we depend on static pools
+	pool = "%{pool_name}"
+	certificate_authority_id = "tf-test-my-certificate-authority-%{random_suffix}"
+	location = "%{pool_location}"
+	is_staged = true
+	config {
+		subject_config {
+		subject {
+			organization = "HashiCorp"
+			common_name = "my-certificate-authority"
+		}
+		subject_alt_name {
+			dns_names = ["hashicorp.com"]
+		}
+		}
+		x509_config {
+		ca_options {
+			is_ca = true
+			max_issuer_path_length = 10
+		}
+		key_usage {
+			base_key_usage {
+			digital_signature = true
+			content_commitment = true
+			key_encipherment = false
+			data_encipherment = true
+			key_agreement = true
+			cert_sign = true
+			crl_sign = true
+			decipher_only = true
+			}
+			extended_key_usage {
+			server_auth = true
+			client_auth = false
+			email_protection = true
+			code_signing = true
+			time_stamping = true
+			}
+		}
+		}
+	}
+	lifetime = "86400s"
+	key_spec {
+		algorithm = "RSA_PKCS1_4096_SHA256"
 	}
 }
 `, context)
