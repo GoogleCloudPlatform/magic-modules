3999c2a1d460e8ba37e2e62a994f532265c167c5
Shuya Ma <shuyama@google.com>
2022-05-16 10:44:50 -0700

    mix merger

--- .ci/containers/vcr-cassette-merger/vcr_merge.sh
+++ .ci/containers/vcr-cassette-merger/vcr_merge.sh
@@ -20,7 +20,7 @@ fi
 gsutil ls gs://ci-vcr-cassettes/beta/refs/heads/auto-pr-$PR_NUMBER/fixtures/
 if [ $? -eq 0 ]; then
   # We have recorded new cassettes for this branch
-  gsutil -m cp gs://ci-vcr-cassettes/beta/refs/heads/auto-pr-$PR_NUMBER/fixtures/* gs://ci-vcr-cassettes/fixtures/
+  gsutil -m cp gs://ci-vcr-cassettes/beta/refs/heads/auto-pr-$PR_NUMBER/fixtures/* gs://ci-vcr-cassettes/beta/fixtures/
   gsutil -m rm -r gs://ci-vcr-cassettes/beta/refs/heads/auto-pr-$PR_NUMBER/
 fi
 
