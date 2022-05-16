8ddef2f11983718bbce847476dc8136eca9987ca
Shuya Ma <shuyama@google.com>
2022-05-16 00:30:28 -0700

    update cassette-merger

--- .ci/containers/vcr-cassette-merger/vcr_merge.sh
+++ .ci/containers/vcr-cassette-merger/vcr_merge.sh
@@ -5,23 +5,23 @@ set -e
 REFERENCE=$1
 
 PR_NUMBER=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
-    "https://api.github.com/repos/GoogleCloudPlatform/magic-modules/pulls?state=closed&base=main&sort=updated&direction=desc" | \
+    "https://api.github.com/repos/GoogleCloudPlatform/magic-modules/pulls?state=closed&base=master&sort=updated&direction=desc" | \
     jq -r ".[] | if .merge_commit_sha == \"$REFERENCE\" then .number else empty end")
 
 set +e
-gsutil ls gs://vcr-$GOOGLE_PROJECT/refs/heads/auto-pr-$PR_NUMBER/fixtures/
+gsutil ls gs://ci-vcr-cassettes/refs/heads/auto-pr-$PR_NUMBER/fixtures/
 if [ $? -eq 0 ]; then
   # We have recorded new cassettes for this branch
-  gsutil -m cp gs://vcr-$GOOGLE_PROJECT/refs/heads/auto-pr-$PR_NUMBER/fixtures/* gs://vcr-$GOOGLE_PROJECT/fixtures/
-  gsutil -m rm -r gs://vcr-$GOOGLE_PROJECT/refs/heads/auto-pr-$PR_NUMBER/
+  gsutil -m cp gs://ci-vcr-cassettes/refs/heads/auto-pr-$PR_NUMBER/fixtures/* gs://ci-vcr-cassettes/fixtures/
+  gsutil -m rm -r gs://ci-vcr-cassettes/refs/heads/auto-pr-$PR_NUMBER/
 fi
 
 # Beta cassettes
-gsutil ls gs://vcr-$GOOGLE_PROJECT/beta/refs/heads/auto-pr-$PR_NUMBER/fixtures/
+gsutil ls gs://ci-vcr-cassettes/beta/refs/heads/auto-pr-$PR_NUMBER/fixtures/
 if [ $? -eq 0 ]; then
   # We have recorded new cassettes for this branch
-  gsutil -m cp gs://vcr-$GOOGLE_PROJECT/beta/refs/heads/auto-pr-$PR_NUMBER/fixtures/* gs://vcr-$GOOGLE_PROJECT/beta/fixtures/
-  gsutil -m rm -r gs://vcr-$GOOGLE_PROJECT/beta/refs/heads/auto-pr-$PR_NUMBER/
+  gsutil -m cp gs://ci-vcr-cassettes/beta/refs/heads/auto-pr-$PR_NUMBER/fixtures/* gs://ci-vcr-cassettes/fixtures/
+  gsutil -m rm -r gs://ci-vcr-cassettes/beta/refs/heads/auto-pr-$PR_NUMBER/
 fi
 
 
