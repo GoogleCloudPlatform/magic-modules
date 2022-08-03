4302c1fc87f6670ba33559e6f8b5b521dae49bd7
Shuya Ma <87669292+shuyama1@users.noreply.github.com>
2022-08-03 11:31:30 -0700

    VCR recording in parallel (#6359)

--- .ci/containers/gcb-terraform-vcr-tester/test_terraform_vcr.sh
+++ .ci/containers/gcb-terraform-vcr-tester/test_terraform_vcr.sh
@@ -165,12 +165,19 @@ if [[ -n $FAILED_TESTS_PATTERN ]]; then
   comment+="#### Action taken ${NEWLINE}"
   comment+="<details> <summary>Triggering VCR tests in RECORDING mode for the tests that failed during VCR. Click here to see the failed tests</summary> $FAILED_TESTS_PATTERN </details>"
   add_comment "${comment}"
-  # RECORDING mode
-  export VCR_MODE=RECORDING
   # Clear fixtures folder
   rm $VCR_PATH/*
-  TF_LOG=DEBUG TF_LOG_PATH_MASK=$local_path/testlog/recording/%s.log TF_ACC=1 TF_SCHEMA_PANIC_ON_ERROR=1 go test ./google-beta -parallel 1 -v -run=$FAILED_TESTS_PATTERN -timeout 240m -ldflags="-X=github.com/hashicorp/terraform-provider-google-beta/version.ProviderVersion=acc" > recording_test.log
-  test_exit_code=$?
+  
+  # RECORDING mode
+  export VCR_MODE=RECORDING
+  FAILED_TESTS=$(grep "^--- FAIL: TestAcc" replaying_test$test_suffix.log | awk '{print $3}')
+  test_exit_code=0
+  for failed_test in $FAILED_TESTS
+  do
+      TF_LOG=DEBUG TF_LOG_PATH_MASK=$local_path/testlog/recording/%s.log TF_ACC=1 TF_SCHEMA_PANIC_ON_ERROR=1 go test ./google-beta -parallel 1 -v -run=$failed_test -timeout 90m -ldflags="-X=github.com/hashicorp/terraform-provider-google-beta/version.ProviderVersion=acc" >> recording_test.log &
+      test_exit_code=$(($test_exit_code || $?))
+  done
+  wait
 
   # store cassettes
   gsutil -m -q cp fixtures/* gs://ci-vcr-cassettes/beta/refs/heads/auto-pr-$pr_number/fixtures/
