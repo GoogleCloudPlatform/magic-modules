ffeb29588f888c0b3fd8073eb3a31152715ebeab
Lorenzo Caggioni <lorenzo.caggioni@gmail.com>
2022-09-03 08:00:32 +0100

    Removing Service Directory test... Location setting not supported ATM over API.

--- mmv1/third_party/terraform/tests/resource_dialogflowcx_webhook_test.go
+++ mmv1/third_party/terraform/tests/resource_dialogflowcx_webhook_test.go
@@ -35,14 +35,6 @@ func TestAccDialogflowCXWebhook_update(t *testing.T) {
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
-			{
-				Config: testAccDialogflowCXWebhook_sd(context),
-			},
-			{
-				ResourceName:      "google_dialogflow_cx_webhook.my_webhook",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
 		},
 	})
 }
@@ -94,35 +86,6 @@ func testAccDialogflowCXWebhook_full(context map[string]interface{}) string {
 			uri = "https://example.com"
 			request_headers = {
 				"Authorization": "Bearer {{access_token}}"
-			}			
-		}
-	}
-	`, context)
-}
-
-func testAccDialogflowCXWebhook_sd(context map[string]interface{}) string {
-	return Nprintf(`
-	data "google_project" "project" {}
-
-	resource "google_dialogflow_cx_agent" "agent_entity" {
-		display_name = "tf-test-%{random_suffix}"
-		location = "europe-west1"
-		default_language_code = "en"
-		supported_language_codes = ["it","de","es"]
-		time_zone = "America/New_York"
-		description = "Description 1."
-		avatar_uri = "https://storage.cloud.google.com/dialogflow-test-host-image/cloud-logo.png"
-	}
-
-	resource "google_dialogflow_cx_webhook" "my_webhook" {
-		parent       = google_dialogflow_cx_agent.agent_entity.id
-		display_name = "MyWebhook"
-		timeout      = "20s"
-		disabled     = false
-		service_directory {
-			service = "projects/project123/locations/europe-west1/namespaces/webhook/services/webhook"
-			generic_web_service {
-				uri = "https://example.com"
 			}
 		}
 	}
