b7211afd96d1835180eb9e3a580d8ca9896e25d2
Alfatah Bheda <alfatah@google.com>
2022-09-13 11:06:18 +0000

    Made changes in the storage_transfer_job test file

--- mmv1/third_party/terraform/tests/resource_storage_transfer_job_test.go
+++ mmv1/third_party/terraform/tests/resource_storage_transfer_job_test.go
@@ -159,7 +159,7 @@ func TestAccStorageTransferJob_transferOptions(t *testing.T) {
 				ImportStateVerify: true,
 			},
 			{
-				Config: testAccStorageTransferJob_transferOptions(getTestProjectFromEnv(), testDataSourceBucketName, testDataSinkName, testTransferJobDescription, false, false, false),
+				Config: testAccStorageTransferJob_transferOptions(getTestProjectFromEnv(), testDataSourceBucketName, testDataSinkName, testTransferJobDescription, false, false, false, testOverwriteWhen[0]),
 			},
 			{
 				ResourceName:      "google_storage_transfer_job.transfer_job",
@@ -167,7 +167,7 @@ func TestAccStorageTransferJob_transferOptions(t *testing.T) {
 				ImportStateVerify: true,
 			},
 			{
-				Config: testAccStorageTransferJob_transferOptions(getTestProjectFromEnv(), testDataSourceBucketName, testDataSinkName, testTransferJobDescription, true, true, false),
+				Config: testAccStorageTransferJob_transferOptions(getTestProjectFromEnv(), testDataSourceBucketName, testDataSinkName, testTransferJobDescription, true, true, false, testOverwriteWhen[1]),
 			},
 			{
 				ResourceName:      "google_storage_transfer_job.transfer_job",
@@ -175,31 +175,7 @@ func TestAccStorageTransferJob_transferOptions(t *testing.T) {
 				ImportStateVerify: true,
 			},
 			{
-				Config: testAccStorageTransferJob_transferOptions(getTestProjectFromEnv(), testDataSourceBucketName, testDataSinkName, testTransferJobDescription, true, false, true),
-			},
-			{
-				ResourceName:      "google_storage_transfer_job.transfer_job",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-			{
-				Config: testAccStorageTransferJob_withOverwriteWhen(getTestProjectFromEnv(), testDataSourceBucketName, testDataSinkName, testTransferJobDescription, testOverwriteWhen[0]),
-			},
-			{
-				ResourceName:      "google_storage_transfer_job.transfer_job",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-			{
-				Config: testAccStorageTransferJob_withOverwriteWhen(getTestProjectFromEnv(), testDataSourceBucketName, testDataSinkName, testTransferJobDescription, testOverwriteWhen[1]),
-			},
-			{
-				ResourceName:      "google_storage_transfer_job.transfer_job",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-			{
-				Config: testAccStorageTransferJob_withOverwriteWhen(getTestProjectFromEnv(), testDataSourceBucketName, testDataSinkName, testTransferJobDescription, testOverwriteWhen[2]),
+				Config: testAccStorageTransferJob_transferOptions(getTestProjectFromEnv(), testDataSourceBucketName, testDataSinkName, testTransferJobDescription, true, false, true, testOverwriteWhen[2]),
 			},
 			{
 				ResourceName:      "google_storage_transfer_job.transfer_job",
@@ -571,7 +547,7 @@ resource "google_storage_transfer_job" "transfer_job" {
 `, project, dataSourceBucketName, project, transferJobDescription, project)
 }
 
-func testAccStorageTransferJob_transferOptions(project string, dataSourceBucketName string, dataSinkBucketName string, transferJobDescription string, overwriteObjectsAlreadyExistingInSink bool, deleteObjectsUniqueInSink bool, deleteObjectsFromSourceAfterTransfer bool) string {
+func testAccStorageTransferJob_transferOptions(project string, dataSourceBucketName string, dataSinkBucketName string, transferJobDescription string, overwriteObjectsAlreadyExistingInSink bool, deleteObjectsUniqueInSink bool, deleteObjectsFromSourceAfterTransfer bool, overwriteWhenVal string) string {
 	return fmt.Sprintf(`
 data "google_storage_transfer_project_service_account" "default" {
   project = "%s"
@@ -620,86 +596,6 @@ resource "google_storage_transfer_job" "transfer_job" {
       overwrite_objects_already_existing_in_sink = %t
       delete_objects_unique_in_sink = %t
       delete_objects_from_source_after_transfer = %t
-    }
-  }
-
-  schedule {
-    schedule_start_date {
-      year  = 2018
-      month = 10
-      day   = 1
-    }
-    schedule_end_date {
-      year  = 2019
-      month = 10
-      day   = 1
-    }
-    start_time_of_day {
-      hours   = 0
-      minutes = 30
-      seconds = 0
-      nanos   = 0
-    }
-	repeat_interval = "604800s"
-  }
-
-  depends_on = [
-    google_storage_bucket_iam_member.data_source,
-    google_storage_bucket_iam_member.data_sink,
-  ]
-}
-`, project, dataSourceBucketName, project, dataSinkBucketName, project, transferJobDescription, project, overwriteObjectsAlreadyExistingInSink, deleteObjectsUniqueInSink, deleteObjectsFromSourceAfterTransfer)
-}
-
-func testAccStorageTransferJob_withOverwriteWhen(project string, dataSourceBucketName string, dataSinkBucketName string, transferJobDescription string, overwriteWhenVal string) string {
-	return fmt.Sprintf(`
-data "google_storage_transfer_project_service_account" "default" {
-  project = "%s"
-}
-
-resource "google_storage_bucket" "data_source" {
-  name          = "%s"
-  project       = "%s"
-  location      = "US"
-  force_destroy = true
-}
-
-resource "google_storage_bucket_iam_member" "data_source" {
-  bucket = google_storage_bucket.data_source.name
-  role   = "roles/storage.admin"
-  member = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
-}
-
-resource "google_storage_bucket" "data_sink" {
-  name          = "%s"
-  project       = "%s"
-  location      = "US"
-  force_destroy = true
-}
-
-resource "google_storage_bucket_iam_member" "data_sink" {
-  bucket = google_storage_bucket.data_sink.name
-  role   = "roles/storage.admin"
-  member = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
-}
-
-resource "google_storage_transfer_job" "transfer_job" {
-  description = "%s"
-  project     = "%s"
-
-  transfer_spec {
-    gcs_data_source {
-      bucket_name = google_storage_bucket.data_source.name
-      path  = "foo/bar/"
-    }
-    gcs_data_sink {
-      bucket_name = google_storage_bucket.data_sink.name
-      path  = "foo/bar/"
-    }
-    transfer_options {
-      overwrite_objects_already_existing_in_sink = false
-      delete_objects_unique_in_sink = false
-      delete_objects_from_source_after_transfer = false
       overwrite_when = "%s"
     }
   }
@@ -729,5 +625,5 @@ resource "google_storage_transfer_job" "transfer_job" {
     google_storage_bucket_iam_member.data_sink,
   ]
 }
-`, project, dataSourceBucketName, project, dataSinkBucketName, project, transferJobDescription, project, overwriteWhenVal)
+`, project, dataSourceBucketName, project, dataSinkBucketName, project, transferJobDescription, project, overwriteObjectsAlreadyExistingInSink, deleteObjectsUniqueInSink, deleteObjectsFromSourceAfterTransfer, overwriteWhenVal)
 }
