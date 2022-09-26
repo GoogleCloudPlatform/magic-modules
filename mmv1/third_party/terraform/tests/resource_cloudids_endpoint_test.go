8058f7326f2eb7a508bfeeedab2fe07402905ba7
Hao Nan Li <haonan@google.com>
2022-09-26 14:53:57 -0700

    [WIP] Add cloud_ids endpoint resource and creation tests

---
+++ mmv1/third_party/terraform/tests/resource_cloudids_endpoint_test.go
@@ -0,0 +1,42 @@
+package google
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestCloudIdsEndpoint_basic(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCloudIdsEndpointDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testCloudIds_basic(context),
+			},
+			{
+				ResourceName:      "google_cloud_ids_endpoint.terraform-test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testCloudIds_basic(context map[string]interface{}) string {
+    return Nprintf(`
+resource "google_cloud_ids_endpoint" "endpoint" {
+	name     = "tf-test-example-endpoint%{random_suffix}"
+	location = "us-central1-f"
+	network  = "src-net"
+    severity = "INFORMATIONAL"
+}
+`, context)
+}
