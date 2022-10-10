6a6c4452c2c97d285163210032e1cd733f9ad635
Scott Suarez <ScottSuarez@google.com>
2022-10-10 09:29:09 -0700

    Safeguard breaking change doc generation from failing tpg(b) generation (#6670)

--- .ci/containers/downstream-builder/generate_downstream.sh
+++ .ci/containers/downstream-builder/generate_downstream.sh
@@ -161,7 +161,9 @@ else
         pushd ../
         make tpgtools OUTPUT_PATH=$LOCAL_PATH VERSION=$VERSION
         pushd ./tools/breaking-change-detector
+        set +e
         go run . -docs -providerFolder="${LOCAL_PATH}/.github/"
+        set -e
         popd
         popd
     fi
