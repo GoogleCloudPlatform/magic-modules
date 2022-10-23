fd049713c1e9ab233cedf387866241553889f329
Julius Kelly <juliuskelly@google.com>
2022-10-23 22:16:54 +0000

    source scheme updated to the resource schema required fields add: name, project, zone, and self_link.

--- mmv1/third_party/terraform/tests/data_source_google_compute_instance_group_manager_test.go
+++ mmv1/third_party/terraform/tests/data_source_google_compute_instance_group_manager_test.go
@@ -6,17 +6,19 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestGoogleComputeInstanceGroupManager(t *testing.T) {
+func TestAccDataSourceGoogleComputeInstanceGroupManager(t *testing.T) {
 	t.Parallel()
 
-	dsName := "tf-test-" + randString(t, 6)
+	zoneName := "us-central1-a"
+	igmName := "tf-tst-igm" + randString(t, 6)
 
 	context := map[string]interface{}{
-		"dsName":       dsName,
-		"igmName":      "tf-test-igm" + randString(t, 6),
-		"baseName":     "tf-test-base" + randString(t, 6),
-		"poolName":     "tf-test-pool" + randString(t, 6),
-		"templateName": "tf-test-templt" + randString(t, 6),
+		"zoneName":     zoneName,
+		"igmName":      igmName,
+		"baseName":     "tf-tst-igm-base" + randString(t, 6),
+		"poolName":     "tf-tst-pool" + randString(t, 6),
+		"templateName": "tf-tst-templt" + randString(t, 6),
+		"autoHealName": "tf-tst-ah-name" + randString(t, 6),
 	}
 
 	vcrTest(t, resource.TestCase{
@@ -26,9 +28,9 @@ func TestGoogleComputeInstanceGroupManager(t *testing.T) {
 			{
 				Config: testAccDataSourceGoogleComputeInstanceGroupManager_basic(context),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.google_compute_instance_group_manager.data_source", "name", dsName),
 					resource.TestCheckResourceAttr("data.google_compute_instance_group_manager.data_source", "project", getTestProjectFromEnv()),
-					resource.TestCheckResourceAttr("data.google_compute_instance_group_manager.data_source", "instances.#", "1")),
+					resource.TestCheckResourceAttr("data.google_compute_instance_group_manager.data_source", "zone", zoneName),
+					resource.TestCheckResourceAttr("data.google_compute_instance_group_manager.data_source", "name", igmName)),
 			},
 		},
 	})
@@ -37,7 +39,7 @@ func TestGoogleComputeInstanceGroupManager(t *testing.T) {
 func testAccDataSourceGoogleComputeInstanceGroupManager_basic(context map[string]interface{}) string {
 	return Nprintf(`
 	resource "google_compute_health_check" "autohealing" {
-		name                = "autohealing-health-check"
+		name                = "%{autoHealName}"
 		check_interval_sec  = 5
 		timeout_sec         = 5
 		healthy_threshold   = 2
@@ -51,12 +53,12 @@ func testAccDataSourceGoogleComputeInstanceGroupManager_basic(context map[string
 
 	resource "google_compute_instance_group_manager" "appserver" {
 		name = "%{igmName}"
-
 		base_instance_name = "%{baseName}"
 		zone               = "us-central1-a"
 
 		version {
 		  instance_template  = google_compute_instance_template.igm-basic.id
+		  name = "primary"
 		}
 
 		all_instances_config {
@@ -83,7 +85,6 @@ func testAccDataSourceGoogleComputeInstanceGroupManager_basic(context map[string
 	}
 
 	data "google_compute_instance_group_manager" "data_source" {
-		name = "%{dsName}"
 		self_link = google_compute_instance_group_manager.appserver.instance_group
 	}
 
