d347a18e7e3f566b90db25246f8c21264422a6bc
Aarsh Dhokai <aarshd@google.com>
2022-10-31 09:27:37 +0000

    adding new test file for location tag binding

--- mmv1/third_party/terraform/tests/resource_tags_test.go
+++ mmv1/third_party/terraform/tests/resource_tags_test.go
@@ -23,7 +23,6 @@ func TestAccTags(t *testing.T) {
 		"tagValueBasic":                     testAccTagsTagValue_tagValueBasic,
 		"tagValueUpdate":                    testAccTagsTagValue_tagValueUpdate,
 		"tagBindingBasic":                   testAccTagsTagBinding_tagBindingBasic,
-		"tagsLocationTagBindingBasic":       testAccTagsLocationTagBinding_locationTagBindingbasic,
 		"tagValueIamBinding":                testAccTagsTagValueIamBinding,
 		"tagValueIamMember":                 testAccTagsTagValueIamMember,
 		"tagValueIamPolicy":                 testAccTagsTagValueIamPolicy,
@@ -774,105 +773,4 @@ resource "google_tags_tag_value_iam_binding" "foo" {
   members = ["user:admin@hashicorptest.com", "user:gterraformtest1@gmail.com"]
 }
 `, context)
-}
-
-func testAccTagsLocationTagBinding_locationTagBindingbasic(t *testing.T) {
-	t.Parallel()
-
-	context := map[string]interface{}{
-		"org_id":        getTestOrgFromEnv(t),
-		"project_id":    "tf-test-" + randString(t, 10),
-		"random_suffix": randString(t, 10),
-
-		"key_short_name":   "tf-test-key-" + randString(t, 10),
-		"value_short_name": "tf-test-value-" + randString(t, 10),
-	}
-
-	vcrTest(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		ExternalProviders: map[string]resource.ExternalProvider{
-			"random": {},
-		},
-		CheckDestroy: testAccCheckTagsLocationTagBindingDestroyProducer(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccTagsLocationTagBinding_locationTagBindingBasicExample(context),
-			},
-		},
-	})
-}
-
-func testAccTagsLocationTagBinding_locationTagBindingBasicExample(context map[string]interface{}) string {
-	return Nprintf(`
-
-resource "google_project" "project" {
-	project_id = "%{project_id}"
-	name       = "%{project_id}"
-	org_id     = "%{org_id}"
-}
-
-resource "google_tags_tag_key" "key" {
-	parent = "organizations/%{org_id}"
-	short_name = "keyname%{random_suffix}"
-	description = "For a certain set of resources."
-}
-
-resource "google_tags_tag_value" "value" {
-	parent = google_tags_tag_key.key.id
-	short_name = "foo%{random_suffix}"
-	description = "For foo%{random_suffix} resources."
-}
-
-resource "google_sql_database_instance" "main" {
-	name             = "tf-test-main-instance"
-	database_version = "POSTGRES_14"
-	region           = "us-central1"
-	deletion_protection = false
-	settings {
-	  # Second-generation instance tiers are based on the machine
-	  # type. See argument reference below.
-	  tier = "db-f1-micro"
-	}
-}
-
-resource "google_tags_location_tag_binding" "binding" {
-	parent = "//sqladmin.googleapis.com/projects/${google_project.project.number}/instances/${google_sql_database_instance.main.id}"
-	tag_value = google_tags_tag_value.value.id
-	location = "us-central1"
-}
-`, context)
-}
-
-func testAccCheckTagsLocationTagBindingDestroyProducer(t *testing.T) func(s *terraform.State) error {
-	return func(s *terraform.State) error {
-		for name, rs := range s.RootModule().Resources {
-			if rs.Type != "google_tags_location_tag_binding" {
-				continue
-			}
-			if strings.HasPrefix(name, "data.") {
-				continue
-			}
-
-			config := googleProviderConfig(t)
-
-			url, err := replaceVarsForTest(config, rs, "{{TagsLocationBasePath}}{{name}}")
-			if err != nil {
-				return err
-			}
-
-			billingProject := ""
-
-			if config.BillingProject != "" {
-				billingProject = config.BillingProject
-			}
-
-			_, err = sendRequest(config, "GET", billingProject, url, config.userAgent, nil)
-			if err == nil {
-				return fmt.Errorf("TagsTagBinding still exists at %s", url)
-			}
-		}
-
-		return nil
-	}
 }
