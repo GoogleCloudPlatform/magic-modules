f0373364afc730caeeb6f0245570d701ed14565e
Alfatah Bheda <alfatah@google.com>
2022-11-16 05:52:45 +0000

    Updated the full test of google_vertex_ai_tensorboard

--- mmv1/third_party/terraform/tests/resource_vertex_ai_tensorboard_test.go
+++ mmv1/third_party/terraform/tests/resource_vertex_ai_tensorboard_test.go
@@ -7,69 +7,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccVertexAITensorboard_Full(t *testing.T) {
-	t.Parallel()
-
-	random_suffix := "tf-test-" + randString(t, 10)
-
-	vcrTest(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckVertexAITensorboardDestroyProducer(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccVertexAITensorboard_Full(random_suffix),
-			},
-		},
-	})
-}
-
-func testAccVertexAITensorboard_Full(random_suffix string) string {
-	return fmt.Sprintf(`
-data "google_project" "project" {
-}
-resource "google_kms_key_ring" "keyring" {
-  name     = "keyring-%s"
-  location = "us-central1"
-}
-resource "google_kms_crypto_key" "example-key" {
-  name            = "crypto-key-%s"
-  key_ring        = google_kms_key_ring.keyring.id
-  rotation_period = "100000s"  
-  lifecycle {
-    prevent_destroy = false
-  }
-}
-resource "google_kms_crypto_key_iam_binding" "crypto_key_encrypt" {
-  crypto_key_id = google_kms_crypto_key.example-key.id
-  role          = "roles/cloudkms.cryptoKeyEncrypter"  
-  members = [
-    "serviceAccount:service-${data.google_project.project.number}@gcp-sa-aiplatform.iam.gserviceaccount.com",
-  ]
-}
-resource "google_kms_crypto_key_iam_binding" "crypto_key_decrypt" {
-  crypto_key_id = google_kms_crypto_key.example-key.id
-  role          = "roles/cloudkms.cryptoKeyDecrypter"
-  members = [
-    "serviceAccount:service-${data.google_project.project.number}@gcp-sa-aiplatform.iam.gserviceaccount.com",
-  ]
-}
-resource "google_vertex_ai_tensorboard" "tensorboard" {
-  depends_on = [google_kms_crypto_key_iam_binding.crypto_key_encrypt, google_kms_crypto_key_iam_binding.crypto_key_decrypt]
-  display_name = "%s"
-  description  = "sample description"
-  labels       = {
-    "key1" : "value1",
-    "key2" : "value2"
-  }
-  region       = "us-central1"
-  encryption_spec {
-	kms_key_name = google_kms_crypto_key.example-key.id
-  }
-}
-`, random_suffix, random_suffix, random_suffix)
-}
-
 func TestAccVertexAITensorboard_Update(t *testing.T) {
 	t.Parallel()
 
