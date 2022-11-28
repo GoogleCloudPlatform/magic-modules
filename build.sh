f0347e422b84bbdcaece0bc5f3626c5cb3117e9c
Fred Zhang <fredzqm@google.com>
2022-11-28 19:51:22 +0000

    use instance

--- build.sh
+++ build.sh
@@ -3,5 +3,13 @@ set -x
 OUTPUT_PATH=$GOPATH/src/github.com/hashicorp/terraform-provider-google-beta
 make terraform VERSION=beta OUTPUT_PATH="$OUTPUT_PATH" PRODUCT=firebasedatabase
 
-cd $OUTPUT_PATH && git status
+cd $OUTPUT_PATH
+git status
+
+export GOOGLE_PROJECT=fredzqm-staging
+export GOOGLE_USE_DEFAULT_CREDENTIALS=1
+export GOOGLE_REGION=us-central1
+export GOOGLE_ZONE=us-central1-c
+
+TF_LOG=TRACE make testacc TEST=./google-beta TESTARGS='-run=TestAccFirebaseDatabaseDatabase' | tee tests.log
 
