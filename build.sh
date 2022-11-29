737037751d95b94aae95c050b7e033ce2e93a40c
Fred Zhang <fredzqm@google.com>
2022-11-29 00:10:00 +0000

    tests

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+# TODO(fredzqm): remove before sending PR for review
 set -x
 
 OUTPUT_PATH=$GOPATH/src/github.com/hashicorp/terraform-provider-google-beta
@@ -7,9 +8,11 @@ cd $OUTPUT_PATH
 git status
 
 export GOOGLE_PROJECT=fredzqm-staging
-export GOOGLE_USE_DEFAULT_CREDENTIALS=1
 export GOOGLE_REGION=us-central1
 export GOOGLE_ZONE=us-central1-c
+export GOOGLE_USE_DEFAULT_CREDENTIALS=True
+export GOOGLE_IMPERSONATE_SERVICE_ACCOUNT=terraform-tester@fredzqm-staging.iam.gserviceaccount.com
 
-TF_LOG=TRACE make testacc TEST=./google-beta TESTARGS='-run=TestAccFirebaseDatabaseInstance' | tee tests.log
+#export TF_LOG=TRACE
+make testacc TEST=./google-beta TESTARGS='-run=TestAccFirebaseDatabaseInstance' | tee tests.log
 
