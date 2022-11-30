e0ee8f6a16a0ff356402cb8e6d3248750d32c98f
Fred Zhang <fredzqm@google.com>
2022-11-30 09:34:19 +0000

    ?immediate=true

--- build.sh
+++ build.sh
@@ -14,6 +14,6 @@ export GOOGLE_ZONE=us-central1-c
 export GOOGLE_USE_DEFAULT_CREDENTIALS=True
 export GOOGLE_IMPERSONATE_SERVICE_ACCOUNT=terraform-tester@fredzqm-staging.iam.gserviceaccount.com
 
-#export TF_LOG=TRACE
+export TF_LOG=TRACE
 make testacc TEST=./google-beta TESTARGS='-run=TestAccFirebaseDatabaseInstance' | tee tests.log
 
