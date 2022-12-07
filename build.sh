baf1cdb279eb02c8f7a22c62da7980e8786898d3
Fred Zhang <fredzqm@google.com>
2022-12-07 11:55:36 -0800

    m

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ set -x
 set -e
 
 OUTPUT_PATH=$GOPATH/src/github.com/hashicorp/terraform-provider-google-beta
-make terraform VERSION=beta OUTPUT_PATH="$OUTPUT_PATH" PRODUCT=firebasedatabase,firebase
+make terraform VERSION=beta OUTPUT_PATH="$OUTPUT_PATH" PRODUCT=firebase
+make terraform VERSION=beta OUTPUT_PATH="$OUTPUT_PATH" PRODUCT=firebasedatabase
 
 cd $OUTPUT_PATH
 git status
@@ -13,6 +14,8 @@ export GOOGLE_REGION=us-central1
 export GOOGLE_ZONE=us-central1-c
 export GOOGLE_USE_DEFAULT_CREDENTIALS=True
 export GOOGLE_IMPERSONATE_SERVICE_ACCOUNT=terraform-tester@fredzqm-staging.iam.gserviceaccount.com
+# https://pantheon.corp.google.com/iam-admin/settings?folder=261046259366
+export GOOGLE_ORG=261046259366
 
 #export TF_LOG=TRACE
 make testacc TEST=./google-beta TESTARGS='-run=TestAccFirebaseDatabaseInstance' | tee tests.log
