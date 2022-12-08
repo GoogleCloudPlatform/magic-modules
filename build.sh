2cd2510f9697c66b02153f471495dcaf9a02fa29
Fred Zhang <fredzqm@google.com>
2022-12-07 19:00:14 -0800

    setup test org

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ export GOOGLE_ZONE=us-central1-c
 export GOOGLE_USE_DEFAULT_CREDENTIALS=True
 export GOOGLE_IMPERSONATE_SERVICE_ACCOUNT=terraform-tester@fredzqm-staging.iam.gserviceaccount.com
 # https://pantheon.corp.google.com/iam-admin/settings?folder=261046259366
-export GOOGLE_ORG=261046259366
+export GOOGLE_ORG=641623452985
 # export GOOGLE_BILLING_ACCOUNT=261046259366
 
 #export TF_LOG=TRACE
