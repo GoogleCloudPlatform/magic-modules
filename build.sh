cf3dcf5c882591b2a2ef15afd5ecca3ead49c3ca
Fred Zhang <fredzqm@google.com>
2022-12-07 20:55:11 -0800

    m

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ cd $OUTPUT_PATH
 git status
 
 export GOOGLE_PROJECT=fredzqm-staging-c
-export GOOGLE_REGION=europe-west1-b
+export GOOGLE_REGION=europe-west1
 export GOOGLE_ZONE=us-central1-c
 export GOOGLE_USE_DEFAULT_CREDENTIALS=True
 export GOOGLE_IMPERSONATE_SERVICE_ACCOUNT=terraform-tester@fredzqm-staging.iam.gserviceaccount.com
