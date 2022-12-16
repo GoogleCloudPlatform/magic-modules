574fd52d5fd8a051c8b970eda23964763f0dd70b
Aarsh Dhokai <aarshd@google.com>
2022-12-16 14:01:01 +0000

    started working on cloudbuild_trigger data source

---
+++ mmv1/third_party/terraform/data_sources/data_source_google_cloudbuild_trigger.go
@@ -0,0 +1,35 @@
+package google
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceresourceCloudBuildTrigger() *schema.Resource {
+	dsSchema := datasourceSchemaFromResourceSchema(resourceCloudBuildTrigger().Schema)
+	addRequiredFieldsToSchema(dsSchema, "project", "trigger_id")
+	dsSchema["location"] = &schema.Schema{
+		Type:     schema.TypeString,
+		Optional: true,
+		ForceNew: true,
+		Description: `The [Cloud Build location](https://cloud.google.com/build/docs/locations) for the trigger.
+	If not specified, "global" is used.`,
+		Default: "global",
+	}
+	return &schema.Resource{
+		Read:   dataSourceGoogleCloudBuildTriggerRead,
+		Schema: dsSchema,
+	}
+
+}
+
+func dataSourceGoogleCloudBuildTriggerRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	id, err := replaceVars(d, config, "projects/{{project}}/locations/{{location}}/triggers/{{trigger_id}}")
+	if err != nil {
+		return fmt.Errorf("Error constructing id: %s", err)
+	}
+	d.SetId(id)
+	return resourceCloudBuildTriggerRead(d, meta)
+}
