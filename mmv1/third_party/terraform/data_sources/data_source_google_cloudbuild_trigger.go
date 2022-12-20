c061796e204f76c6a1f9132ed300fe7dc193a5c5
Aarsh Dhokai <aarshd@google.com>
2022-12-20 10:26:00 +0000

    added data source for cloud build trigger

---
+++ mmv1/third_party/terraform/data_sources/data_source_google_cloudbuild_trigger.go
@@ -0,0 +1,46 @@
+package google
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceGoogleCloudBuildTrigger() *schema.Resource {
+
+	dsSchema := datasourceSchemaFromResourceSchema(resourceCloudBuildTrigger().Schema)
+
+	addRequiredFieldsToSchema(dsSchema, "trigger_id")
+	addOptionalFieldsToSchema(dsSchema, "project")
+
+	//adding location field explicitly in schema because it requires to set default value for it.
+	dsSchema["location"] = &schema.Schema{
+		Type:     schema.TypeString,
+		Optional: true,
+		ForceNew: true,
+		Description: `The [Cloud Build location](https://cloud.google.com/build/docs/locations) for the trigger.
+	If not specified, "global" is used.`,
+		Default: "global",
+	}
+	return &schema.Resource{
+		Read:   dataSourceGoogleCloudBuildTriggerRead,
+		Schema: dsSchema,
+	}
+
+}
+
+func dataSourceGoogleCloudBuildTriggerRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	id, err := replaceVars(d, config, "projects/{{project}}/locations/{{location}}/triggers/{{trigger_id}}")
+	if err != nil {
+		return fmt.Errorf("Error constructing id: %s", err)
+	}
+
+	id = strings.ReplaceAll(id, "/locations//", "/")
+	id = strings.ReplaceAll(id, "/locations/global/", "/")
+
+	d.SetId(id)
+	return resourceCloudBuildTriggerRead(d, meta)
+}
