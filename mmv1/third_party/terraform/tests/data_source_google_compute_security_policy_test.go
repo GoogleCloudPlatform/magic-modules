17bebc3a64fea1f0dc3bb65d09825383f5c55742
Aarsh Dhokai <aarshd@google.com>
2022-12-21 11:35:56 +0000

    added a test for data source of compute security policy

---
+++ mmv1/third_party/terraform/tests/data_source_google_compute_security_policy_test.go
@@ -0,0 +1,66 @@
+package google
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDataSourceGoogleComputeSecurityPolicy_basic(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCloudRunServiceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceGoogleComputeSecurityPolicy_basic(context),
+				Check: resource.ComposeTestCheckFunc(
+					checkDataSourceStateMatchesResourceState("data.google_compute_security_policy.foo", "google_compute_security_policy.policy"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceGoogleComputeSecurityPolicy_basic(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_compute_security_policy" "policy" {
+   name = "my-policy%{random_suffix}"
+
+   rule {
+     action   = "deny(403)"
+     priority = "1000"
+     match {
+       versioned_expr = "SRC_IPS_V1"
+       config {
+	    src_ip_ranges = ["9.9.9.0/24"]
+       }
+      }
+     description = "Deny access to IPs in 9.9.9.0/24"
+    }
+
+   rule {
+     action   = "allow"
+     priority = "2147483647"
+     match {
+     versioned_expr = "SRC_IPS_V1"
+     config {
+	   src_ip_ranges = ["*"]
+     }
+     }
+     description = "default rule"
+    }
+}
+
+data "google_compute_security_policy" "foo" {
+  name = google_compute_security_policy.policy.name
+  project = google_compute_security_policy.policy.project
+}`, context)
+
+}
