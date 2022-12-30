aed2ad0e7f1ee35aed1ab61753a249639f3889da
Aarsh Dhokai <aarshd@google.com>
2022-12-30 10:39:33 +0000

    added data source for compute region instance group manager

---
+++ mmv1/third_party/terraform/data_sources/data_source_google_compute_region_instance_group_manager.go
@@ -0,0 +1,47 @@
+package google
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceGoogleComputeRegionInstanceGroupManager() *schema.Resource {
+
+	dsSchema := datasourceSchemaFromResourceSchema(resourceComputeRegionInstanceGroupManager().Schema)
+	addRequiredFieldsToSchema(dsSchema, "name")
+	addOptionalFieldsToSchema(dsSchema, "project", "region")
+
+	return &schema.Resource{
+		Read:   dataSourceComputeRegionInstanceGroupManagerRead,
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceComputeRegionInstanceGroupManagerRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	if name, ok := d.GetOk("name"); ok {
+		region, err := getRegion(d, config)
+		if err != nil {
+			return err
+		}
+		project, err := getProject(d, config)
+		if err != nil {
+			return err
+		}
+		d.SetId(fmt.Sprintf("projects/%s/regions/%s/instanceGroupManagers/%s", project, region, name.(string)))
+	} else {
+		return errors.New("Must provide either `resource/name`")
+	}
+
+	err := resourceComputeRegionInstanceGroupManagerRead(d, meta)
+
+	if err != nil {
+		return err
+	}
+	if d.Id() == "" {
+		return errors.New("Instance Manager Group not found")
+	}
+	return nil
+}
---
+++ mmv1/third_party/terraform/tests/data_source_google_compute_region_instance_group_manager_test.go
@@ -0,0 +1,86 @@
+package google
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDataSourceGoogleComputeRegionInstanceGroupManager_basic(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckRegionInstanceGroupManagerDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceGoogleComputeRegionInstanceGroupManager_basic(context),
+				Check: resource.ComposeTestCheckFunc(
+					checkDataSourceStateMatchesResourceStateWithIgnores(
+						"data.google_compute_region_instance_group_manager.foo",
+						"google_compute_region_instance_group_manager.appserver",
+						map[string]struct{}{
+							"wait_for_instances": {},
+						},
+					),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceGoogleComputeRegionInstanceGroupManager_basic(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_compute_instance_template" "appserver" {
+  name         = "template%{random_suffix}"
+  machine_type = "e2-medium"
+	
+  disk {
+	source_image = "debian-cloud/debian-11"
+	auto_delete  = true
+	disk_size_gb = 100
+	boot         = true
+  }
+	
+  network_interface {
+	network = "default"
+  }
+	
+  metadata = {
+	foo = "bar"
+  }
+	
+  can_ip_forward = true
+}
+
+resource "google_compute_region_instance_group_manager" "appserver" {
+  name = "appserver-igm%{random_suffix}"
+	
+  base_instance_name         = "app"
+  region                     = "us-central1"
+  distribution_policy_zones  = ["us-central1-a"]
+	
+  version {
+	instance_template = google_compute_instance_template.appserver.id
+  }
+	
+	
+  named_port {
+	name = "custom"
+	port = 8888
+  }
+  wait_for_instances = false
+}
+	
+data "google_compute_region_instance_group_manager" "foo" {
+   name = google_compute_region_instance_group_manager.appserver.name
+}
+
+`, context)
+
+}
