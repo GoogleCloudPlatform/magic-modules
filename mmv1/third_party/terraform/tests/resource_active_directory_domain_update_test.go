67f6f9e46a0e9fccd7933b3833931288474bde49
drfaust92 <ilia.lazebnik@gmail.com>
2022-12-31 21:06:51 +0200

    gen

--- mmv1/third_party/terraform/tests/resource_active_directory_domain_update_test.go
+++ mmv1/third_party/terraform/tests/resource_active_directory_domain_update_test.go
@@ -83,36 +83,3 @@ func testAccADDomainUpdate(context map[string]interface{}) string {
 	`, context)
 
 }
-
-func testAccCheckActiveDirectoryDomainDestroyProducer(t *testing.T) func(s *terraform.State) error {
-	return func(s *terraform.State) error {
-		for name, rs := range s.RootModule().Resources {
-			if rs.Type != "google_active_directory_domain" {
-				continue
-			}
-			if strings.HasPrefix(name, "data.") {
-				continue
-			}
-
-			config := googleProviderConfig(t)
-
-			url, err := replaceVarsForTest(config, rs, "{{ActiveDirectoryBasePath}}{{name}}")
-			if err != nil {
-				return err
-			}
-
-			billingProject := ""
-
-			if config.BillingProject != "" {
-				billingProject = config.BillingProject
-			}
-
-			_, err = sendRequest(config, "GET", billingProject, url, config.userAgent, nil)
-			if err == nil {
-				return fmt.Errorf("ActiveDirectoryDomain still exists at %s", url)
-			}
-		}
-
-		return nil
-	}
-}
