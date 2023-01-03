39d1c48276cca77cd001d930d801daf5a125e2cb
Enku Feleke <enkuwfeleke@google.com>
2023-01-03 15:32:18 -0600

    Remove un-necessary property

--- scripts/make-in-container.sh
+++ scripts/make-in-container.sh
@@ -7,6 +7,7 @@ set -e
 
 BUILDER_IMAGE="gcr.io/graphite-docker-images/downstream-builder:latest"
 DEV_IMAGE="gcr.io/graphite-docker-images/magic-modules-dev:latest"
+GOPATH="/Users/enkuwfeleke/go"
 
 main() {
     if which podman > /dev/null; then
@@ -23,7 +24,7 @@ main() {
         echo "unable to find magic-modules-dev container"
         build_dev_container
     fi
-
+    echo $GOPATH
     ${CONTAINER_EXECUTABLE} run --rm \
     -v "$PWD:/magic-modules" -v "$GOPATH:$GOPATH" \
     -it "${DEV_IMAGE}" "$@"
