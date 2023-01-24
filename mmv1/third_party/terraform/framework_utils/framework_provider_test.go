a3af2b030b14e5b0a2e01e80fa8c4f6940964d63
Megan Bang <mbang@hashicorp.com>
2023-01-24 16:26:14 -0600

    add dns record set, test and provider schema update

--- mmv1/third_party/terraform/framework_utils/framework_provider_test.go
+++ mmv1/third_party/terraform/framework_utils/framework_provider_test.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-mux/tf6to5server"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -17,12 +19,8 @@ import (
 
 	"github.com/dnaeon/go-vcr/cassette"
 	"github.com/dnaeon/go-vcr/recorder"
-	"github.com/hashicorp/terraform-plugin-framework/attr"
-	"github.com/hashicorp/terraform-plugin-framework/diag"
-	"github.com/hashicorp/terraform-plugin-framework/provider"
-	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/hashicorp/terraform-plugin-mux/tf5muxserver"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
@@ -42,7 +40,6 @@ func NewFrameworkTestProvider(testName string) *frameworkTestProvider {
 	}
 }
 
-// Configure is here to overwrite the frameworkProvider configure function for VCR testing
 func (p *frameworkTestProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
 	if isVcrEnabled() {
 		configsLock.RLock()
@@ -64,13 +61,13 @@ func (p *frameworkTestProvider) Configure(ctx context.Context, req provider.Conf
 			// When replaying, set the poll interval low to speed up tests
 			p.ProdProvider.pollInterval = 10 * time.Millisecond
 		default:
-			tflog.Debug(ctx, fmt.Sprintf("No valid environment var set for VCR_MODE, expected RECORDING or REPLAYING, skipping VCR. VCR_MODE: %s", vcrEnv))
+			log.Printf("[DEBUG] No valid environment var set for VCR_MODE, expected RECORDING or REPLAYING, skipping VCR. VCR_MODE: %s", vcrEnv)
 			return
 		}
 
 		envPath := os.Getenv("VCR_PATH")
 		if envPath == "" {
-			tflog.Debug(ctx, "No environment var set for VCR_PATH, skipping VCR")
+			log.Print("[DEBUG] No environment var set for VCR_PATH, skipping VCR")
 			return
 		}
 		path := filepath.Join(envPath, vcrFileName(p.TestName))
@@ -97,7 +94,7 @@ func (p *frameworkTestProvider) Configure(ctx context.Context, req provider.Conf
 
 			var b bytes.Buffer
 			if _, err := b.ReadFrom(r.Body); err != nil {
-				tflog.Debug(ctx, fmt.Sprintf("Failed to read request body from cassette: %v", err))
+				log.Printf("[DEBUG] Failed to read request body from cassette: %v", err)
 				return false
 			}
 			r.Body = ioutil.NopCloser(&b)
@@ -111,11 +108,11 @@ func (p *frameworkTestProvider) Configure(ctx context.Context, req provider.Conf
 			if strings.Contains(contentType, "application/json") {
 				var reqJson, cassetteJson interface{}
 				if err := json.Unmarshal([]byte(reqBody), &reqJson); err != nil {
-					tflog.Debug(ctx, fmt.Sprintf("Failed to unmarshall request json: %v", err))
+					log.Printf("[DEBUG] Failed to unmarshall request json: %v", err)
 					return false
 				}
 				if err := json.Unmarshal([]byte(i.Body), &cassetteJson); err != nil {
-					tflog.Debug(ctx, fmt.Sprintf("Failed to unmarshall cassette json: %v", err))
+					log.Printf("[DEBUG] Failed to unmarshall cassette json: %v", err)
 					return false
 				}
 				return reflect.DeepEqual(reqJson, cassetteJson)
@@ -128,22 +125,37 @@ func (p *frameworkTestProvider) Configure(ctx context.Context, req provider.Conf
 		configsLock.Unlock()
 		return
 	} else {
-		tflog.Debug(ctx, "VCR_PATH or VCR_MODE not set, skipping VCR")
+		log.Print("[DEBUG] VCR_PATH or VCR_MODE not set, skipping VCR")
 	}
 }
 
-func configureApiClient(ctx context.Context, p *frameworkTestProvider, diags *diag.Diagnostics) {
-	var data ProviderModel
-	var d diag.Diagnostics
+// General test utils
+func MuxedProviders(testName string) (func() tfprotov5.ProviderServer, error) {
+	ctx := context.Background()
 
-	// Set defaults if needed - the only attribute without a default is ImpersonateServiceAccountDelegates
-	// this is a bit of a hack, but we'll just initialize it here so that it's been initialized at least
-	data.ImpersonateServiceAccountDelegates, d = types.ListValue(types.StringType, []attr.Value{})
-	diags.Append(d...)
-	if diags.HasError() {
-		return
+	// plugin framework provider
+	downgradedFrameworkProvider, err := tf6to5server.DowngradeServer(
+		context.Background(),
+		providerserver.NewProtocol6(&NewFrameworkTestProvider(testName).ProdProvider),
+	)
+	if err != nil {
+		log.Fatalf(err.Error())
 	}
-	p.ProdProvider.ConfigureWithData(ctx, data, "test", diags)
+
+	providers := []func() tfprotov5.ProviderServer{
+		func() tfprotov5.ProviderServer {
+			return downgradedFrameworkProvider // framework provider
+		},
+		Provider().GRPCProvider, // sdk provider
+	}
+
+	muxServer, err := tf5muxserver.NewMuxServer(ctx, providers...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return muxServer.ProviderServer, nil
 }
 
 func getTestAccFrameworkProviders(testName string, c resource.TestCase) map[string]func() (tfprotov5.ProviderServer, error) {
@@ -154,17 +166,14 @@ func getTestAccFrameworkProviders(testName string, c resource.TestCase) map[stri
 
 	var testProvider string
 	providerMapKeys := reflect.ValueOf(c.ProtoV5ProviderFactories).MapKeys()
-	if len(providerMapKeys) > 0. {
-		if strings.Contains(providerMapKeys[0].String(), "google-beta") {
-			testProvider = "google-beta"
-		} else {
-			testProvider = "google"
-		}
-		return map[string]func() (tfprotov5.ProviderServer, error){
-			testProvider: myFunc,
-		}
+	if strings.Contains(providerMapKeys[0].String(), "google-beta") {
+		testProvider = "google-beta"
+	} else {
+		testProvider = "google"
 	}
-	return map[string]func() (tfprotov5.ProviderServer, error){}
+	return map[string]func() (tfprotov5.ProviderServer, error){
+		testProvider: myFunc,
+	}
 }
 
 func getTestFwProvider(t *testing.T) *frameworkTestProvider {
@@ -175,68 +184,5 @@ func getTestFwProvider(t *testing.T) *frameworkTestProvider {
 		return fwProvider
 	}
 
-	var diags diag.Diagnostics
-	p := NewFrameworkTestProvider(t.Name())
-	configureApiClient(context.Background(), p, &diags)
-	if diags.HasError() {
-		log.Fatalf("%d errors when configuring test provider client: first is %s", diags.ErrorsCount(), diags.Errors()[0].Detail())
-	}
-
-	return p
-}
-
-func TestAccFrameworkProviderMeta_setModuleName(t *testing.T) {
-	t.Parallel()
-
-	moduleName := "my-module"
-	managedZoneName := fmt.Sprintf("tf-test-zone-%s", randString(t, 10))
-
-	vcrTest(t, resource.TestCase{
-		PreCheck: func() { testAccPreCheck(t) },
-		ProtoV5ProviderFactories: map[string]func() (tfprotov5.ProviderServer, error){
-			"google": func() (tfprotov5.ProviderServer, error) {
-				provider, err := MuxedProviders(t.Name())
-				return provider(), err
-			},
-		},
-		// CheckDestroy: testAccCheckComputeAddressDestroyProducer(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccFrameworkProviderMeta_setModuleName(moduleName, managedZoneName, randString(t, 10)),
-			},
-		},
-	})
-}
-
-func testAccFrameworkProviderMeta_setModuleName(key, managedZoneName, recordSetName string) string {
-	return fmt.Sprintf(`
-terraform {
-  provider_meta "google" {
-    module_name = "%s"
-  }
-}
-
-
-provider "google" {}
-
-resource "google_dns_managed_zone" "zone" {
-  name     = "test-zone"
-  dns_name = "%s.hashicorptest.com."
-}
-
-resource "google_dns_record_set" "rs" {
-  managed_zone = google_dns_managed_zone.zone.name
-  name         = "%s.${google_dns_managed_zone.zone.dns_name}"
-  type         = "A"
-  ttl          = 300
-  rrdatas      = [
-	"192.168.1.0",
-  ]
-}
-
-data "google_dns_record_set" "rs" {
-  managed_zone = google_dns_record_set.rs.managed_zone
-  name         = google_dns_record_set.rs.name
-  type         = google_dns_record_set.rs.type
-}`, key, managedZoneName, recordSetName)
+	return NewFrameworkTestProvider(t.Name())
 }
