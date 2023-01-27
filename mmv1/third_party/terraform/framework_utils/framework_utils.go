e2fe0d183e7bf805edae1b1c52a6112e1a04f28e
Megan Bang <mbang@hashicorp.com>
2023-01-27 12:55:16 -0600

    move MultiEnvDefault function so it will hopefully be picked up by the validator

--- mmv1/third_party/terraform/framework_utils/framework_utils.go
+++ mmv1/third_party/terraform/framework_utils/framework_utils.go
@@ -14,19 +14,6 @@ import (
 
 const uaEnvVar = "TF_APPEND_USER_AGENT"
 
-// MultiEnvDefaultFunc is a helper function that returns the value of the first
-// environment variable in the given list that returns a non-empty value. If
-// none of the environment variables return a value, the default value is
-// returned.
-func MultiEnvDefault(ks []string, dv interface{}) interface{} {
-	for _, k := range ks {
-		if v := os.Getenv(k); v != "" {
-			return v
-		}
-	}
-	return dv
-}
-
 func CompileUserAgentString(name, tfVersion, provVersion string) string {
 	ua := fmt.Sprintf("Terraform/%s (+https://www.terraform.io) Terraform-Plugin-SDK/%s %s/%s", tfVersion, "terraform-plugin-framework", name, provVersion)
 
