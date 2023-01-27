873f448cb2e465160db600b926cc23a9a63163c1
Viktor Moros <vgm@google.com>
2023-01-26 21:25:45 -0800

    Improving test & example for authorize orgs desc

---
+++ mmv1/third_party/terraform/tests/resource_access_context_manager_authorized_orgs_desc_test.go
@@ -0,0 +1,86 @@
+package google
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func testAccAccessContextManagerAuthorizedOrgsDesc_basicTest(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAccessContextManagerAuthorizedOrgsDescDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAccessContextManagerAuthorizedOrgsDesc_accessContextManagerAuthorizedOrgsDescBasicExample(context),
+			},
+			{
+				ResourceName:            "google_access_context_manager_authorized_orgs_desc.authorized-orgs-desc",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"parent"},
+			},
+		},
+	})
+}
+
+func testAccAccessContextManagerAuthorizedOrgsDesc_accessContextManagerAuthorizedOrgsDescBasicExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_access_context_manager_authorized_orgs_desc" "authorized-orgs-desc" {
+  parent = "accessPolicies/${google_access_context_manager_access_policy.access-policy.name}"
+  name   = "accessPolicies/${google_access_context_manager_access_policy.access-policy.name}/authorizedOrgsDescs/fakeDescName"
+  authorization_type = "AUTHORIZATION_TYPE_TRUST"
+  asset_type = "ASSET_TYPE_CREDENTIAL_STRENGTH"
+  authorization_direction = "AUTHORIZATION_DIRECTION_TO"
+  orgs = ["organizations/12345", "organizations/98765"]
+}
+
+resource "google_access_context_manager_access_policy" "access-policy" {
+  parent = "organizations/%{org_id}"
+  title  = "my policy"
+}
+`, context)
+}
+
+func testAccCheckAccessContextManagerAuthorizedOrgsDescDestroyProducer(t *testing.T) func(s *terraform.State) error {
+	return func(s *terraform.State) error {
+		for name, rs := range s.RootModule().Resources {
+			if rs.Type != "google_access_context_manager_authorized_orgs_desc" {
+				continue
+			}
+			if strings.HasPrefix(name, "data.") {
+				continue
+			}
+
+			config := googleProviderConfig(t)
+
+			url, err := replaceVarsForTest(config, rs, "{{AccessContextManagerBasePath}}{{name}}")
+			if err != nil {
+				return err
+			}
+
+			billingProject := ""
+
+			if config.BillingProject != "" {
+				billingProject = config.BillingProject
+			}
+
+			_, err = sendRequest(config, "GET", billingProject, url, config.userAgent, nil)
+			if err == nil {
+				return fmt.Errorf("AccessContextManagerAuthorizedOrgsDesc still exists at %s", url)
+			}
+		}
+
+		return nil
+	}
+}
