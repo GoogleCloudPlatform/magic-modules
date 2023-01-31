3ab5c03a9dfd428e004ce32cacc800540ab80ee3
ravisiddhu <ravisiddharth@google.com>
2023-01-31 16:04:05 +0000

    removed client side filtering from databases list datsource

--- mmv1/third_party/terraform/data_sources/data_source_sql_databases.go
+++ mmv1/third_party/terraform/data_sources/data_source_sql_databases.go
@@ -2,7 +2,6 @@ package google
 
 import (
 	"fmt"
-	"regexp"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	sqladmin "google.golang.org/api/sqladmin/v1beta4"
@@ -23,35 +22,6 @@ func dataSourceSqlDatabases() *schema.Resource {
 				Type:        schema.TypeString,
 				Required:    true,
 				Description: `The name of the Cloud SQL database instance in which the database belongs.`,
-			},
-			"filters": {
-				Type:     schema.TypeList,
-				Optional: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"values": {
-							Type:        schema.TypeList,
-							Optional:    true,
-							Description: `Values for the field.`,
-							Elem: &schema.Schema{
-								Type: schema.TypeString,
-							},
-						},
-						"name": {
-							Type:        schema.TypeString,
-							Required:    true,
-							Description: `Name of the field.`,
-						},
-						"exclude_values": {
-							Type:        schema.TypeList,
-							Optional:    true,
-							Description: `The returned list would not include databases which match these values`,
-							Elem: &schema.Schema{
-								Type: schema.TypeString,
-							},
-						},
-					},
-				},
 			},
 			"databases": {
 				Type:     schema.TypeList,
@@ -83,69 +53,12 @@ func dataSourceSqlDatabasesRead(d *schema.ResourceData, meta interface{}) error 
 	if err != nil {
 		return handleNotFoundError(err, d, fmt.Sprintf("Databases in %q instance", d.Get("instance").(string)))
 	}
-	var filteredDatabases []*sqladmin.Database
-	if v, ok := d.GetOk("filters"); ok {
-		filteredDatabases, err = applyFilterOnDatabases(databases.Items, v.([]interface{}))
-		if err != nil {
-			return err
-		}
-	} else {
-		filteredDatabases = databases.Items
-	}
 
-	if err := d.Set("databases", flattenDatabases(filteredDatabases)); err != nil {
+	if err := d.Set("databases", flattenDatabases(databases.Items)); err != nil {
 		return fmt.Errorf("Error setting databases: %s", err)
 	}
-
-	d.SetId(setId(d, project))
+	d.SetId(fmt.Sprintf("project/%s/instance/%s/databases", project, d.Get("instance").(string)))
 	return nil
-}
-
-func applyFilterOnDatabases(databases []*sqladmin.Database, databaseFilters []interface{}) ([]*sqladmin.Database, error) {
-	filteredDatabases := make([]*sqladmin.Database, 0)
-	if len(databases) == 0 {
-		return databases, nil
-	}
-	for _, d := range databases {
-		include := true
-		for _, f := range databaseFilters {
-			if f == nil {
-				continue
-			}
-			if !include {
-				break
-			}
-			filter := f.(map[string]interface{})
-			switch filter["name"].(string) {
-			case "name":
-				i, err := regexMatch(filter, d.Name, include)
-				if err != nil {
-					return filteredDatabases, err
-				}
-				include = i
-			case "charset":
-				i, err := regexMatch(filter, d.Charset, include)
-				if err != nil {
-					return filteredDatabases, err
-				}
-				include = i
-			case "collation":
-				i, err := regexMatch(filter, d.Collation, include)
-				if err != nil {
-					return filteredDatabases, err
-				}
-				include = i
-			default:
-				return filteredDatabases, fmt.Errorf("Invalid filter")
-			}
-		}
-		if include {
-			filteredDatabases = append(filteredDatabases, d)
-		}
-	}
-
-	return filteredDatabases, nil
-
 }
 
 func flattenDatabases(fetchedDatabases []*sqladmin.Database) []map[string]interface{} {
@@ -166,53 +79,4 @@ func flattenDatabases(fetchedDatabases []*sqladmin.Database) []map[string]interf
 		databases = append(databases, database)
 	}
 	return databases
-}
-
-func regexMatch(filter map[string]interface{}, field string, include bool) (bool, error) {
-	b := false
-	for _, regex := range filter["values"].([]interface{}) {
-		match, err := regexp.MatchString(regex.(string), field)
-		if err != nil {
-			return include, fmt.Errorf("Invalid regex %s", regex)
-		}
-		b = b || match
-	}
-	if _, ok := filter["values"]; ok {
-		if len(filter["values"].([]interface{})) > 0 {
-			include = include && b
-		}
-	}
-	//exclude has higher priority than include
-	for _, regex := range filter["exclude_values"].([]interface{}) {
-		match, err := regexp.MatchString(regex.(string), field)
-		if err != nil {
-			return include, fmt.Errorf("Invalid regex %s", regex)
-		}
-		if match {
-			include = false
-		}
-	}
-	return include, nil
-}
-
-func setId(d *schema.ResourceData, project string) string {
-	id := fmt.Sprintf("projects/%s/instances/%s", project, d.Get("instance").(string))
-	if v, ok := d.GetOk("filters"); ok {
-		for i, f := range v.([]interface{}) {
-			filter := f.(map[string]interface{})
-			id = fmt.Sprintf("%s/%d/%s", id, i, filter["name"])
-			if fv, ok := filter["values"]; ok {
-				for j, val := range fv.([]interface{}) {
-					id = fmt.Sprintf("%s/%d/%s", id, j, val.(string))
-				}
-			}
-			if fev, ok := filter["exclude_values"]; ok {
-				for j, val := range fev.([]interface{}) {
-					id = fmt.Sprintf("%s/%d/%s", id, j, val.(string))
-				}
-			}
-
-		}
-	}
-	return id
 }
--- mmv1/third_party/terraform/website/docs/d/sql_databases.html.markdown
+++ mmv1/third_party/terraform/website/docs/d/sql_databases.html.markdown
@@ -7,18 +7,13 @@ description: |-
 
 # google\_sql\_databases
 
-Use this data source to get information about a list of databases in a Cloud SQL instance, you can also apply some filter over this list of databases.
-
+Use this data source to get information about a list of databases in a Cloud SQL instance.
 ## Example Usage
 
 
 ```hcl
 data "google_sql_databases" "qa" {
   instance = google_sql_database_instance.main.name
-  filter{
-    "name" = "name"
-    "values" = ["db-*"]
-  }
 }
 ```
 
@@ -29,16 +24,6 @@ The following arguments are supported:
 * `instance` - (required) The name of the Cloud SQL database instance in which the database belongs.
 
 * `project` - (optional) The ID of the project in which the instance belongs.
-
-The optional `filters` sublist supports:
-
-* `name` - (Required) Name of the filter. supported values include `name`, `charset`, `collation`.
-
-* `values` - (Optional) To include databases which matches at least on of the regex provided in the list for the chosen filter.
-
-* `exclude_values` - (Optional) To exclude databases which matches at least on of the regex provided in the list for the chosen filter.
-
-This filtering is done locally on what GCP returns, and could have a performance impact if the count of databases is large.
 
 ## Attributes Reference
 See [google_sql_database](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database) resource for details of all the available attributes.
