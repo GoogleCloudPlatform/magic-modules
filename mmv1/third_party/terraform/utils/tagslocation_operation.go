68fb6a67fdc4583d30af4de982ecd1bd72ada420
Ghaleb Al-habian <galhabian@google.com>
2023-02-01 22:01:18 +0000

    add handwritten operation.go file and update calls in resource.erb

---
+++ mmv1/third_party/terraform/utils/tagslocation_operation.go
@@ -0,0 +1,59 @@
+package google
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type TagsLocationOperationWaiter struct {
+	Config    *Config
+	UserAgent string
+	CommonOperationWaiter
+}
+
+func (w *TagsLocationOperationWaiter) QueryOp() (interface{}, error) {
+	if w == nil {
+		return nil, fmt.Errorf("Cannot query operation, it's unset or nil.")
+	}
+	// Returns the proper get.
+	url := fmt.Sprintf("%s%s", w.Config.TagsLocationBasePath, w.CommonOperationWaiter.Op.Name)
+
+	return sendRequest(w.Config, "GET", "", url, w.UserAgent, nil)
+}
+
+func createTagsLocationWaiter(config *Config, op map[string]interface{}, activity, userAgent string) (*TagsLocationOperationWaiter, error) {
+	w := &TagsLocationOperationWaiter{
+		Config:    config,
+		UserAgent: userAgent,
+	}
+	if err := w.CommonOperationWaiter.SetOp(op); err != nil {
+		return nil, err
+	}
+	return w, nil
+}
+
+// nolint: deadcode,unused
+func tagsLocationOperationWaitTimeWithResponse(config *Config, op map[string]interface{}, response *map[string]interface{}, activity, userAgent string, timeout time.Duration) error {
+	w, err := createTagsLocationWaiter(config, op, activity, userAgent)
+	if err != nil {
+		return err
+	}
+	if err := OperationWait(w, activity, timeout, config.PollInterval); err != nil {
+		return err
+	}
+	return json.Unmarshal([]byte(w.CommonOperationWaiter.Op.Response), response)
+}
+
+func tagsLocationOperationWaitTime(config *Config, op map[string]interface{}, activity, userAgent string, timeout time.Duration) error {
+	if val, ok := op["name"]; !ok || val == "" {
+		// This was a synchronous call - there is no operation to wait for.
+		return nil
+	}
+	w, err := createTagsLocationWaiter(config, op, activity, userAgent)
+	if err != nil {
+		// If w is nil, the op was synchronous.
+		return err
+	}
+	return OperationWait(w, activity, timeout, config.PollInterval)
+}
