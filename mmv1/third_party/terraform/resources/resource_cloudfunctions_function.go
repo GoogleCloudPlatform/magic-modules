f1a4042fca1c1cca35da7d944d615a09aa02de3a
Edward Sun <sunedward@google.com>
2023-02-25 11:02:40 -0800

    update to fix func errors

--- mmv1/third_party/terraform/resources/resource_cloudfunctions_function.go
+++ mmv1/third_party/terraform/resources/resource_cloudfunctions_function.go
@@ -79,36 +79,43 @@ func validateResourceCloudFunctionsFunctionName(v interface{}, k string) (ws []s
 	return validateRegexp(re)(v, k)
 }
 
-func reverseArray(arr []string) []string {
-	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
-		arr[i], arr[j] = arr[j], arr[i]
+func partsCompare(a, b, reg string) bool {
+
+	regex := regexp.MustCompile(reg)
+	if regex.MatchString(a) && regex.MatchString(b) {
+		aParts := regex.FindStringSubmatch(a)
+		bParts := regex.FindStringSubmatch(b)
+		for i := 0; i < len(aParts); i++ {
+			if aParts[i] != bParts[i] {
+				return false
+			}
+		}
+	} else if regex.MatchString(a) {
+		aParts := regex.FindStringSubmatch(a)
+		if aParts[len(aParts)-1] != b {
+			return false
+		}
+	} else if regex.MatchString(b) {
+		bParts := regex.FindStringSubmatch(b)
+		if bParts[len(bParts)-1] != a {
+			return false
+		}
+	} else {
+		if a != b {
+			return false
+		}
 	}
-	return arr
+
+	return true
 }
 
 // based on compareSelfLinkOrResourceName, but less reusable and allows multi-/
 // strings in the new state (config) part
 func compareSelfLinkOrResourceNameWithMultipleParts(_, old, new string, _ *schema.ResourceData) bool {
-	olist := reverseArray(strings.Split(old, "/"))
-	nlist := reverseArray(strings.Split(new, "/"))
-
-	olen := len(olist)
-	nlen := len(nlist)
-
-	min := olen
-	if min > nlen {
-		min = nlen
-	}
-
-	for i := 0; i < min; i++ {
-		if strings.Compare(olist[i], nlist[i]) != 0 {
-			if i == min-1 && (len(olist[i]) == 0 || len(nlist[i]) == 0) {
-				return true
-			}
-			return false
-		}
-	}
-	return true
+	// two formats based on expandEventTrigger()
+	regex1 := "projects/(.+)/databases/(default)/documents/(.+)"
+	regex2 := "projects/(.+)/(.+)/(.+)"
+	return partsCompare(old, new, regex1) || partsCompare(old, new, regex2)
 }
 
 func ResourceCloudFunctionsFunction() *schema.Resource {
