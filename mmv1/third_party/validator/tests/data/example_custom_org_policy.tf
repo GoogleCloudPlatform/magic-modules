b56cd781409fdd89b43837b263555667a67cb7b9
jashan-g <98514022+jashan-g@users.noreply.github.com>
2023-03-07 01:41:40 +0530

    Update example_custom_org_policy.tf

--- mmv1/third_party/validator/tests/data/example_custom_org_policy.tf
+++ mmv1/third_party/validator/tests/data/example_custom_org_policy.tf
@@ -1,37 +1,36 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "~> {{.Provider.version}}"
-    }
-  }
-}
-
-provider "google" {
-  {{if .Provider.credentials }}credentials = "{{.Provider.credentials}}"{{end}}
-}
-
-resource "google_org_policy_policy" "project_policy" {
-  name   = "projects/{{.Provider.project}}/policies/gcp.resourceLocations"
-  parent = "projects/{{.Provider.project}}"
-  
-  spec {
-    rules {
-      condition {
-        description = "Description the policy"
-        expression  = "resource.matchLabels('label1', 'label2')"
-        location    = "EU"
-        title       = "Title of the condition"
+[
+  {
+    "name": "cloudresourcemanager.googleapis.com/projects/{{.Provider.project}}",
+    "asset_type": "cloudresourcemanager.googleapis.com/Project",
+    "ancestry_path": "{{.Ancestry}}/project/{{.Provider.project}}",
+    "v2_org_policies": [
+      {
+        "name": "policies/gcp.resourceLocations",
+        "spec": {
+          "update_time": "{{.Time.RFC3339Nano}}",
+          "rules": [
+            {
+              "values": {
+                "allowed_values": [
+                  "projects/allowed-project"
+                ],
+                "denied_values": [
+                  "projects/denied-project"
+                ]
+              },
+              "expression": {
+                "expression": "resource.matchLabels('labelKeys/123', 'labelValues/345')",
+                "title": "sample-condition",
+                "description": "A sample condition for the policy",
+                "location": "sample-location.log"
+              }
+            },
+            {
+              "allow_all": true
+            }
+          ]
+        }
       }
-
-      values {
-        allowed_values = ["projects/123","projects/456"]
-        denied_values  = ["projects/789"]
-      }
-    }
-
-    rules {
-      allow_all = "TRUE"
-    }
+    ],
   }
-}
+]
