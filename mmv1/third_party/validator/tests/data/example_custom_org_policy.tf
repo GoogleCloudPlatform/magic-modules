37e89c602dd34da713028d7317f366c858f5d12b
jashan-g <98514022+jashan-g@users.noreply.github.com>
2023-03-07 01:38:44 +0530

    Update example_custom_org_policy.tf

--- mmv1/third_party/validator/tests/data/example_custom_org_policy.tf
+++ mmv1/third_party/validator/tests/data/example_custom_org_policy.tf
@@ -11,40 +11,27 @@ provider "google" {
   {{if .Provider.credentials }}credentials = "{{.Provider.credentials}}"{{end}}
 }
 
-resource "google_org_policy_policy" "primary_policy" {
+resource "google_org_policy_policy" "project_policy" {
   name   = "projects/{{.Provider.project}}/policies/gcp.resourceLocations"
   parent = "projects/{{.Provider.project}}"
+  
   spec {
     rules {
       condition {
-        description = "A sample condition for the policy"
-        expression  = "resource.matchLabels('labelKeys/123', 'labelValues/345')"
-        location    = "sample-location.log"
-        title       = "sample-condition"
+        description = "Description the policy"
+        expression  = "resource.matchLabels('label1', 'label2')"
+        location    = "EU"
+        title       = "Title of the condition"
       }
-      
-    
+
       values {
-        allowed_values = ["projects/allowed-project1", "projects/allowed-project2"]
-        denied_values  = ["projects/denied-project"]
+        allowed_values = ["projects/123","projects/456"]
+        denied_values  = ["projects/789"]
       }
     }
 
     rules {
       allow_all = "TRUE"
     }
-
-    inherit_from_parent = true
-  }
-}
-
-resource "google_org_policy_policy" "secondary_policy" {
-  name   = "folders/{{.FolderID}}/policies/samplePolicy"
-  parent = "folders/{{.FolderID}}"
-  spec {
-    rules {
-      deny_all = "TRUE"
-    }
-    inherit_from_parent = true
   }
 }
