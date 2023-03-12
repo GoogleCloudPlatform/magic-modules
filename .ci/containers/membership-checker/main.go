afe6b72fb098e41bc0e4b7d61e208a61e132dd37
Shuya Ma <shuyama@google.com>
2023-03-12 15:36:00 -0700

    pass args through to cloud build trigger

--- .ci/containers/membership-checker/main.go
+++ .ci/containers/membership-checker/main.go
@@ -11,7 +11,7 @@ func main() {
 		fmt.Println("Did not provide GITHUB_TOKEN environment variable")
 		os.Exit(1)
 	}
-	if len(os.Args) <= 4 {
+	if len(os.Args) <= 7 {
 		fmt.Println("Not enough arguments")
 		os.Exit(1)
 	}
@@ -27,6 +27,23 @@ func main() {
 
 	branchName := os.Args[4]
 	fmt.Println("Branch Name: ", branchName)
+
+	headRepoUrl := os.Args[5]
+	fmt.Println("Branch Name: ", headRepoUrl)
+
+	headBranch := os.Args[6]
+	fmt.Println("Branch Name: ", headBranch)
+
+	baseBranch := os.Args[7]
+	fmt.Println("Branch Name: ", baseBranch)
+
+	substitutions := map[string]string{
+		"$BRANCH_NAME":    branchName,
+		"$_PR_NUMBER":     prNumber,
+		"$_HEAD_REPO_URL": headRepoUrl,
+		"$_HEAD_BRANCH":   headBranch,
+		"$_BASE_BRANCH":   baseBranch,
+	}
 
 	author, err := getPullRequestAuthor(prNumber, GITHUB_TOKEN)
 	if err != nil {
@@ -45,7 +62,7 @@ func main() {
 	trusted := isTrustedUser(author, GITHUB_TOKEN)
 
 	if (target == "check_auto_run_contributor" && trusted) || (target == "check_community_contributor" && !trusted) {
-		err = triggerMMPresubmitRuns("graphite-docker-images", "magic-modules", commitSha, branchName)
+		err = triggerMMPresubmitRuns("graphite-docker-images", "magic-modules", commitSha, substitutions)
 		if err != nil {
 			fmt.Println(err)
 			return
