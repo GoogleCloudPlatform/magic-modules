33f54dd3adfdbcd191560cfbdd0e35848d70fb99
Ryan Oaks <ryanoaks@google.com>
2023-03-14 15:30:49 -0400

    Bump sql_user create timeout

--- mmv1/third_party/terraform/resources/resource_sql_user.go
+++ mmv1/third_party/terraform/resources/resource_sql_user.go
@@ -34,7 +34,7 @@ func ResourceSqlUser() *schema.Resource {
 		},
 
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(10 * time.Minute),
+			Create: schema.DefaultTimeout(20 * time.Minute),
 			Update: schema.DefaultTimeout(10 * time.Minute),
 			Delete: schema.DefaultTimeout(10 * time.Minute),
 		},
