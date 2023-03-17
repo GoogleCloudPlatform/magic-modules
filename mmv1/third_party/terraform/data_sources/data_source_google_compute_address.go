9f974a69fc03db1ee4f92540a96c05fdcd5d1458
Scott Suarez <ScottMSuarez@gmail.com>
2023-03-17 14:04:23 -0400

    fix changes

--- mmv1/third_party/terraform/data_sources/data_source_google_compute_address.go
+++ mmv1/third_party/terraform/data_sources/data_source_google_compute_address.go
@@ -2,13 +2,20 @@ package google
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-func DataSourceGoogleComputeGlobalAddress() *schema.Resource {
+var (
+	computeAddressIdTemplate = "projects/%s/regions/%s/addresses/%s"
+	computeAddressLinkRegex  = regexp.MustCompile("projects/(.+)/regions/(.+)/addresses/(.+)$")
+)
+
+func DataSourceGoogleComputeAddress() *schema.Resource {
 	return &schema.Resource{
-		Read: dataSourceGoogleComputeGlobalAddressRead,
+		Read: dataSourceGoogleComputeAddressRead,
 
 		Schema: map[string]*schema.Schema{
 			"name": {
@@ -66,6 +73,12 @@ func DataSourceGoogleComputeGlobalAddress() *schema.Resource {
 				Computed: true,
 			},
 
+			"region": {
+				Type:     schema.TypeString,
+				Computed: true,
+				Optional: true,
+			},
+
 			"project": {
 				Type:     schema.TypeString,
 				Computed: true,
@@ -75,7 +88,7 @@ func DataSourceGoogleComputeGlobalAddress() *schema.Resource {
 	}
 }
 
-func dataSourceGoogleComputeGlobalAddressRead(d *schema.ResourceData, meta interface{}) error {
+func dataSourceGoogleComputeAddressRead(d *schema.ResourceData, meta interface{}) error {
 	config := meta.(*Config)
 	userAgent, err := generateUserAgentString(d, config.UserAgent)
 	if err != nil {
@@ -86,10 +99,15 @@ func dataSourceGoogleComputeGlobalAddressRead(d *schema.ResourceData, meta inter
 	if err != nil {
 		return err
 	}
-	name := d.Get("name").(string)
-	address, err := config.NewComputeClient(userAgent).GlobalAddresses.Get(project, name).Do()
+	region, err := getRegion(d, config)
 	if err != nil {
-		return handleNotFoundError(err, d, fmt.Sprintf("Global Address Not Found : %s", name))
+		return err
+	}
+	name := d.Get("name").(string)
+
+	address, err := config.NewComputeClient(userAgent).Addresses.Get(project, region, name).Do()
+	if err != nil {
+		return handleNotFoundError(err, d, fmt.Sprintf("Address Not Found : %s", name))
 	}
 
 	if err := d.Set("address", address.Address); err != nil {
@@ -122,6 +140,70 @@ func dataSourceGoogleComputeGlobalAddressRead(d *schema.ResourceData, meta inter
 	if err := d.Set("project", project); err != nil {
 		return fmt.Errorf("Error setting project: %s", err)
 	}
-	d.SetId(fmt.Sprintf("projects/%s/global/addresses/%s", project, name))
+	if err := d.Set("region", region); err != nil {
+		return fmt.Errorf("Error setting region: %s", err)
+	}
+
+	d.SetId(fmt.Sprintf("projects/%s/regions/%s/addresses/%s", project, region, name))
 	return nil
+}
+
+type computeAddressId struct {
+	Project string
+	Region  string
+	Name    string
+}
+
+func (s computeAddressId) canonicalId() string {
+	return fmt.Sprintf(computeAddressIdTemplate, s.Project, s.Region, s.Name)
+}
+
+func parseComputeAddressId(id string, config *Config) (*computeAddressId, error) {
+	var parts []string
+	if computeAddressLinkRegex.MatchString(id) {
+		parts = computeAddressLinkRegex.FindStringSubmatch(id)
+
+		return &computeAddressId{
+			Project: parts[1],
+			Region:  parts[2],
+			Name:    parts[3],
+		}, nil
+	} else {
+		parts = strings.Split(id, "/")
+	}
+
+	if len(parts) == 3 {
+		return &computeAddressId{
+			Project: parts[0],
+			Region:  parts[1],
+			Name:    parts[2],
+		}, nil
+	} else if len(parts) == 2 {
+		// Project is optional.
+		if config.Project == "" {
+			return nil, fmt.Errorf("The default project for the provider must be set when using the `{region}/{name}` id format.")
+		}
+
+		return &computeAddressId{
+			Project: config.Project,
+			Region:  parts[0],
+			Name:    parts[1],
+		}, nil
+	} else if len(parts) == 1 {
+		// Project and region is optional
+		if config.Project == "" {
+			return nil, fmt.Errorf("The default project for the provider must be set when using the `{name}` id format.")
+		}
+		if config.Region == "" {
+			return nil, fmt.Errorf("The default region for the provider must be set when using the `{name}` id format.")
+		}
+
+		return &computeAddressId{
+			Project: config.Project,
+			Region:  config.Region,
+			Name:    parts[0],
+		}, nil
+	}
+
+	return nil, fmt.Errorf("Invalid compute address id. Expecting resource link, `{project}/{region}/{name}`, `{region}/{name}` or `{name}` format.")
 }
