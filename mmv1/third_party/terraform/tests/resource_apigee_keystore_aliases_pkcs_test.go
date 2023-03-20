26431fbf1c807d28a5d90c7c3e874ba3583b23f1
Anuhya P <anuhyap@google.com>
2023-03-20 13:31:13 -0500

    Second commit for selfsigned and initial commit for pkcs

---
+++ mmv1/third_party/terraform/tests/resource_apigee_keystore_aliases_pkcs_test.go
@@ -0,0 +1,151 @@
+package google
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccApigeeEnvKeystoreAliasesPkcs_apigeeKeystoreAliasesPkcsExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"org_id":          GetTestOrgFromEnv(t),
+		"billing_account": GetTestBillingAccountFromEnv(t),
+		"random_suffix":   RandString(t, 10),
+	}
+
+	VcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    TestAccProviders,
+		CheckDestroy: testAccCheckApigeeEnvKeystoreAliasesPkcsDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccApigeeEnvKeystoreAliasesPkcs_apigeeKeystoreAliasesPkcsExample(context),
+			},
+			{
+				ResourceName:            "google_apigee_env_keystore_aliases_pkcs.apigee_environment_keystore",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"keystore_id", "format"},
+			},
+		},
+	})
+}
+
+func testAccApigeeEnvKeystoreAliasesPkcs_apigeeKeystoreAliasesPkcsExample(context map[string]interface{}) string {
+	return Nprintf(`
+// resource "google_project" "project" {
+//   project_id      = "tf-test%{random_suffix}"
+//   name            = "tf-test%{random_suffix}"
+//   org_id          = "%{org_id}"
+//   billing_account = "%{billing_account}"
+// }
+
+// resource "google_project_service" "apigee" {
+//   project = google_project.project.project_id
+//   service = "apigee.googleapis.com"
+// }
+
+// resource "google_project_service" "servicenetworking" {
+//   project = google_project.project.project_id
+//   service = "servicenetworking.googleapis.com"
+//   depends_on = [google_project_service.apigee]
+// }
+
+// resource "google_project_service" "compute" {
+//   project = google_project.project.project_id
+//   service = "compute.googleapis.com"
+//   depends_on = [google_project_service.servicenetworking]
+// }
+
+// resource "google_compute_network" "apigee_network" {
+//   name       = "apigee-network"
+//   project    = google_project.project.project_id
+//   depends_on = [google_project_service.compute]
+// }
+
+// resource "google_compute_global_address" "apigee_range" {
+//   name          = "apigee-range"
+//   purpose       = "VPC_PEERING"
+//   address_type  = "INTERNAL"
+//   prefix_length = 16
+//   network       = google_compute_network.apigee_network.id
+//   project       = google_project.project.project_id
+// }
+
+// resource "google_service_networking_connection" "apigee_vpc_connection" {
+//   network                 = google_compute_network.apigee_network.id
+//   service                 = "servicenetworking.googleapis.com"
+//   reserved_peering_ranges = [google_compute_global_address.apigee_range.name]
+//   depends_on              = [google_project_service.servicenetworking]
+// }
+
+// resource "google_apigee_organization" "apigee_org" {
+//   analytics_region   = "us-central1"
+//   project_id         = google_project.project.project_id
+//   authorized_network = google_compute_network.apigee_network.id
+//   depends_on         = [
+//     google_service_networking_connection.apigee_vpc_connection,
+//     google_project_service.apigee,
+//   ]
+// }
+
+// resource "google_apigee_environment" "apigee_environment_keystore" {
+//   org_id       = google_apigee_organization.apigee_org.id
+//   name         = "tf-test%{random_suffix}"
+//   description  = "Apigee Environment"
+//   display_name = "environment-1"
+// }
+
+resource "google_apigee_env_keystore" "apigee_environment_keystore_alias" {
+  name       = "tf-test-keystore%{random_suffix}"
+  env_id     = "organizations/liquid-fulcrum-363817/environments/prod"
+}
+
+resource "google_apigee_keystore_aliases_pkcs" "ks_ss_alias_1" {
+  environment 			= "prod"
+  org_id				= "liquid-fulcrum-363817"
+  keystore				= google_apigee_env_keystore.apigee_environment_keystore_alias.name
+  alias                 = "aliaspkcstest"
+  file                  = filesha256("./test-fixtures/apigee/keyStore.p12")
+  password              = "abcd"
+}
+`, context)
+}
+
+func testAccCheckApigeeEnvKeystoreAliasesPkcsDestroyProducer(t *testing.T) func(s *terraform.State) error {
+	return func(s *terraform.State) error {
+		for name, rs := range s.RootModule().Resources {
+			if rs.Type != "google_apigee_env_keystore_aliases_pkcs" {
+				continue
+			}
+			if strings.HasPrefix(name, "data.") {
+				continue
+			}
+
+			config := GoogleProviderConfig(t)
+
+			url, err := replaceVarsForTest(config, rs, "{{ApigeeBasePath}}{{keystore_id}}/aliases/{{alias}}")
+			if err != nil {
+				return err
+			}
+
+			billingProject := ""
+
+			if config.BillingProject != "" {
+				billingProject = config.BillingProject
+			}
+
+			_, err = SendRequest(config, "GET", billingProject, url, config.UserAgent, nil)
+			if err == nil {
+				return fmt.Errorf("ApigeeEnvKeystoreAliasesPkcs still exists at %s", url)
+			}
+		}
+
+		return nil
+	}
+}
