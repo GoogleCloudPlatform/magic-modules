e54ecbcf0ebe28656203417c6c6aa24d0e684af0
Anuhya P <anuhyap@google.com>
2023-03-23 05:24:32 -0500

    Update PKCS code

--- mmv1/third_party/terraform/tests/resource_apigee_keystore_aliases_pkcs_test.go
+++ mmv1/third_party/terraform/tests/resource_apigee_keystore_aliases_pkcs_test.go
@@ -27,7 +27,7 @@ func TestAccApigeeEnvKeystoreAliasesPkcs_apigeeKeystoreAliasesPkcsExample(t *tes
 				Config: testAccApigeeEnvKeystoreAliasesPkcs_apigeeKeystoreAliasesPkcsExample(context),
 			},
 			{
-				ResourceName:            "google_apigee_env_keystore_aliases_pkcs.apigee_environment_keystore",
+				ResourceName:            "google_apigee_env_keystore_aliases_pkcs.apigee_environment_keystore_aliases_pkcs",
 				ImportState:             true,
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{"keystore_id", "format"},
@@ -38,81 +38,81 @@ func TestAccApigeeEnvKeystoreAliasesPkcs_apigeeKeystoreAliasesPkcsExample(t *tes
 
 func testAccApigeeEnvKeystoreAliasesPkcs_apigeeKeystoreAliasesPkcsExample(context map[string]interface{}) string {
 	return Nprintf(`
-// resource "google_project" "project" {
-//   project_id      = "tf-test%{random_suffix}"
-//   name            = "tf-test%{random_suffix}"
-//   org_id          = "%{org_id}"
-//   billing_account = "%{billing_account}"
-// }
+resource "google_project" "project" {
+  project_id      = "tf-test%{random_suffix}"
+  name            = "tf-test%{random_suffix}"
+  org_id          = "%{org_id}"
+  billing_account = "%{billing_account}"
+}
 
-// resource "google_project_service" "apigee" {
-//   project = google_project.project.project_id
-//   service = "apigee.googleapis.com"
-// }
+resource "google_project_service" "apigee" {
+  project = google_project.project.project_id
+  service = "apigee.googleapis.com"
+}
 
-// resource "google_project_service" "servicenetworking" {
-//   project = google_project.project.project_id
-//   service = "servicenetworking.googleapis.com"
-//   depends_on = [google_project_service.apigee]
-// }
+resource "google_project_service" "servicenetworking" {
+  project = google_project.project.project_id
+  service = "servicenetworking.googleapis.com"
+  depends_on = [google_project_service.apigee]
+}
 
-// resource "google_project_service" "compute" {
-//   project = google_project.project.project_id
-//   service = "compute.googleapis.com"
-//   depends_on = [google_project_service.servicenetworking]
-// }
+resource "google_project_service" "compute" {
+  project = google_project.project.project_id
+  service = "compute.googleapis.com"
+  depends_on = [google_project_service.servicenetworking]
+}
 
-// resource "google_compute_network" "apigee_network" {
-//   name       = "apigee-network"
-//   project    = google_project.project.project_id
-//   depends_on = [google_project_service.compute]
-// }
+resource "google_compute_network" "apigee_network" {
+  name       = "apigee-network"
+  project    = google_project.project.project_id
+  depends_on = [google_project_service.compute]
+}
 
-// resource "google_compute_global_address" "apigee_range" {
-//   name          = "apigee-range"
-//   purpose       = "VPC_PEERING"
-//   address_type  = "INTERNAL"
-//   prefix_length = 16
-//   network       = google_compute_network.apigee_network.id
-//   project       = google_project.project.project_id
-// }
+resource "google_compute_global_address" "apigee_range" {
+  name          = "apigee-range"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.apigee_network.id
+  project       = google_project.project.project_id
+}
 
-// resource "google_service_networking_connection" "apigee_vpc_connection" {
-//   network                 = google_compute_network.apigee_network.id
-//   service                 = "servicenetworking.googleapis.com"
-//   reserved_peering_ranges = [google_compute_global_address.apigee_range.name]
-//   depends_on              = [google_project_service.servicenetworking]
-// }
+resource "google_service_networking_connection" "apigee_vpc_connection" {
+  network                 = google_compute_network.apigee_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.apigee_range.name]
+  depends_on              = [google_project_service.servicenetworking]
+}
 
-// resource "google_apigee_organization" "apigee_org" {
-//   analytics_region   = "us-central1"
-//   project_id         = google_project.project.project_id
-//   authorized_network = google_compute_network.apigee_network.id
-//   depends_on         = [
-//     google_service_networking_connection.apigee_vpc_connection,
-//     google_project_service.apigee,
-//   ]
-// }
+resource "google_apigee_organization" "apigee_org" {
+  analytics_region   = "us-central1"
+  project_id         = google_project.project.project_id
+  authorized_network = google_compute_network.apigee_network.id
+  depends_on         = [
+    google_service_networking_connection.apigee_vpc_connection,
+    google_project_service.apigee,
+  ]
+}
 
-// resource "google_apigee_environment" "apigee_environment_keystore" {
-//   org_id       = google_apigee_organization.apigee_org.id
-//   name         = "tf-test%{random_suffix}"
-//   description  = "Apigee Environment"
-//   display_name = "environment-1"
-// }
+resource "google_apigee_environment" "apigee_environment_keystore" {
+  org_id       = google_apigee_organization.apigee_org.id
+  name         = "tf-test%{random_suffix}"
+  description  = "Apigee Environment"
+  display_name = "environment-1"
+}
 
 resource "google_apigee_env_keystore" "apigee_environment_keystore_alias" {
   name       = "tf-test-keystore%{random_suffix}"
   env_id     = "organizations/liquid-fulcrum-363817/environments/prod"
 }
 
-resource "google_apigee_keystore_aliases_pkcs" "ks_ss_alias_1" {
+resource "google_apigee_keystore_aliases_pkcs" "apigee_environment_keystore_aliases_pkcs" {
   environment 			= "prod"
   org_id				= "liquid-fulcrum-363817"
   keystore				= google_apigee_env_keystore.apigee_environment_keystore_alias.name
   alias                 = "aliaspkcstest"
   file                  = filesha256("./test-fixtures/apigee/keyStore.p12")
-  password              = "abcd"
+  password              = sensitive("abcd")
 }
 `, context)
 }
