a64cbfa0f3582720eef5b9cd2fabfceedec99848
Andy Lu <luyul@google.com>
2023-03-24 21:48:52 +0100

    fix test for vpn datasource.

--- mmv1/third_party/terraform/tests/data_source_google_compute_ha_vpn_gateway_test.go
+++ mmv1/third_party/terraform/tests/data_source_google_compute_ha_vpn_gateway_test.go
@@ -18,7 +18,14 @@ func TestAccDataSourceComputeHaVpnGateway(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataSourceComputeHaVpnGatewayConfig(gwName),
-				Check:  checkDataSourceStateMatchesResourceState("data.google_compute_ha_vpn_gateway.ha_gateway", "google_compute_ha_vpn_gateway.ha_gateway"),
+				// Check:  checkDataSourceStateMatchesResourceState("data.google_compute_ha_vpn_gateway.ha_gateway", "google_compute_ha_vpn_gateway.ha_gateway"),
+				Check: resource.ComposeTestCheckFunc(
+					checkDataSourceStateMatchesResourceStateWithIgnores(
+						"data.google_compute_ha_vpn_gateway.ha_gateway",
+						"google_compute_ha_vpn_gateway.ha_gateway",
+						map[string]struct{}{"stack_type": {}},
+					),
+				),
 			},
 		},
 	})
