eb3018f307aec64fbb9823b7c9b78b8f5232dbff
Shuya Ma <shuyama@google.com>
2023-03-31 15:20:25 -0700

    ci change

--- .ci/containers/membership-checker/membership.go
+++ .ci/containers/membership-checker/membership.go
@@ -33,6 +33,7 @@ var (
 	// This is for reviewers who are "on vacation" will not receive new review assignments but will still receive re-requests for assigned PRs.
 	onVacationReviewers = []string{
 		"zli82016",
+		"shuyama1",
 	}
 )
 
