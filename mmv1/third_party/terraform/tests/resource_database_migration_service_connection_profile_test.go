8459f969392dd739576925a1d606f118576b9fb7
Nick Elliot <nickelliot@google.com>
2023-04-11 16:47:30 -0700

    rebasing and updating testAccPreCheck to AccTestPreCheck

--- mmv1/third_party/terraform/tests/resource_database_migration_service_connection_profile_test.go
+++ mmv1/third_party/terraform/tests/resource_database_migration_service_connection_profile_test.go
@@ -14,7 +14,7 @@ func TestAccDatabaseMigrationServiceConnectionProfile_update(t *testing.T) {
 	}
 
 	VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
+		PreCheck:                 func() { AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
 		Steps: []resource.TestStep{
 			{
