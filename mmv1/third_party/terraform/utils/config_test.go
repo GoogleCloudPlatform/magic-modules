85e1b85fda5ddfe053ae500bb8739a3d11f2835a
Sarah French <sarah.french@hashicorp.com>
2023-04-11 10:31:07 +0100

    Add tests for how `zone` set on SDK provider

--- mmv1/third_party/terraform/utils/config_test.go
+++ mmv1/third_party/terraform/utils/config_test.go
@@ -366,6 +366,103 @@ func TestHandleSDKDefaults_Region(t *testing.T) {
 	}
 }
 
+func TestHandleSDKDefaults_Zone(t *testing.T) {
+	cases := map[string]struct {
+		ConfigValue      string
+		EnvVariables     map[string]string
+		ExpectedValue    string
+		ValueNotProvided bool
+		ExpectError      bool
+	}{
+		"region value set in the provider config is not overridden by ENVs": {
+			ConfigValue: "zone-from-config",
+			EnvVariables: map[string]string{
+				"GOOGLE_ZONE": "zone-from-env",
+			},
+			ExpectedValue: "zone-from-config",
+		},
+		"zone can be set by environment variable, when no value supplied via the config": {
+			EnvVariables: map[string]string{
+				"GOOGLE_ZONE": "zone-from-env",
+			},
+			ExpectedValue: "zone-from-env",
+		},
+		"when multiple zone environment variables are provided, `GOOGLE_ZONE` is used first": {
+			EnvVariables: map[string]string{
+				"GOOGLE_ZONE":           "zone-from-GOOGLE_ZONE",
+				"GCLOUD_ZONE":           "zone-from-GCLOUD_ZONE",
+				"CLOUDSDK_COMPUTE_ZONE": "zone-from-CLOUDSDK_COMPUTE_ZONE",
+			},
+			ExpectedValue: "zone-from-GOOGLE_ZONE",
+		},
+		"when multiple zone environment variables are provided, `GCLOUD_ZONE` is used second": {
+			EnvVariables: map[string]string{
+				// GOOGLE_ZONE unset
+				"GCLOUD_ZONE":           "zone-from-GCLOUD_ZONE",
+				"CLOUDSDK_COMPUTE_ZONE": "zone-from-CLOUDSDK_COMPUTE_ZONE",
+			},
+			ExpectedValue: "zone-from-GCLOUD_ZONE",
+		},
+		"when multiple zone environment variables are provided, `CLOUDSDK_COMPUTE_ZONE` is the last-used ENV": {
+			EnvVariables: map[string]string{
+				// GOOGLE_ZONE unset
+				// GCLOUD_ZONE unset
+				"CLOUDSDK_COMPUTE_ZONE": "zone-from-CLOUDSDK_COMPUTE_ZONE",
+			},
+			ExpectedValue: "zone-from-CLOUDSDK_COMPUTE_ZONE",
+		},
+		"when no values are provided via config or environment variables, the field remains unset without error": {
+			ValueNotProvided: true,
+		},
+	}
+
+	for tn, tc := range cases {
+		t.Run(tn, func(t *testing.T) {
+
+			// Arrange
+			// Create empty schema.ResourceData using the SDK Provider schema
+			emptyConfigMap := map[string]interface{}{}
+			d := schema.TestResourceDataRaw(t, Provider().Schema, emptyConfigMap)
+
+			// Set config value(s)
+			if tc.ConfigValue != "" {
+				d.Set("zone", tc.ConfigValue)
+			}
+
+			// Set ENVs
+			if len(tc.EnvVariables) > 0 {
+				for k, v := range tc.EnvVariables {
+					t.Setenv(k, v)
+				}
+			}
+
+			// Act
+			err := HandleSDKDefaults(d)
+
+			// Assert
+			if err != nil {
+				if !tc.ExpectError {
+					t.Fatalf("error: %v", err)
+				}
+				return
+			}
+
+			// Assert
+			v, ok := d.GetOk("zone")
+			if !ok && !tc.ValueNotProvided {
+				t.Fatal("expected zone to be set in the provider data")
+			}
+			if ok && tc.ValueNotProvided {
+				t.Fatal("expected zone to not be set in the provider data")
+			}
+
+			if v != tc.ExpectedValue {
+				t.Fatalf("unexpected value: wanted %v, got, %v", tc.ExpectedValue, v)
+			}
+		})
+	}
+}
+
 // The `user_project_override` field is an odd one out, as other provider schema fields tend to be strings
 // and `user_project_override` is a boolean
 func TestHandleSDKDefaults_UserProjectOverride(t *testing.T) {
