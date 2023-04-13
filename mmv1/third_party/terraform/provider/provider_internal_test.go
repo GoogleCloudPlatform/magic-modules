dbb178009474226a178236717c1b9b1029ea13de
Sarah French <sarah.french@hashicorp.com>
2023-04-13 12:13:01 +0100

    Add tests for `providerConfigure` testing `billing_project` config

--- mmv1/third_party/terraform/provider/provider_internal_test.go
+++ mmv1/third_party/terraform/provider/provider_internal_test.go
@@ -5,8 +5,10 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"regexp"
 	"testing"
 
+	"github.com/hashicorp/magic-modules/mmv1/third_party/terraform/acctest"
 	"github.com/hashicorp/terraform-provider-google/google/envvar"
 	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
@@ -687,4 +689,315 @@ func TestProvider_providerConfigure_project(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestProvider_providerConfigure_billingProject(t *testing.T) {
+
+	cases := map[string]struct {
+		ConfigValues     map[string]interface{}
+		EnvVariables     map[string]string
+		ExpectedValue    string
+		ExpectError      bool
+		ExpectFieldUnset bool
+	}{
+		"billing_project value set in the provider config is not overridden by ENVs": {
+			ConfigValue: "my-billing-project-from-config",
+			EnvVariables: map[string]string{
+				"GOOGLE_BILLING_PROJECT": "my-billing-project-from-env",
+			},
+			ExpectedValue: "my-billing-project-from-config",
+		},
+		"billing_project can be set by environment variable, when no value supplied via the config": {
+			EnvVariables: map[string]string{
+				"GOOGLE_BILLING_PROJECT": "my-billing-project-from-env",
+			},
+			ExpectedValue: "my-billing-project-from-env",
+		},
+		"when no values are provided via config or environment variables, the field remains unset without error": {
+			ValueNotProvided: true,
+		},
+	}
+
+	for tn, tc := range cases {
+		t.Run(tn, func(t *testing.T) {
+
+			// Arrange
+			ctx, p, d := setupSDKProviderConfigTest(t, tc.ConfigValues, tc.EnvVariables)
+
+			// Act
+			c, diags := providerConfigure(ctx, d, p)
+
+			// Assert
+			if diags.HasError() && !tc.ExpectError {
+				t.Fatalf("unexpected error(s): %#v", diags)
+			}
+			if !diags.HasError() && tc.ExpectError {
+				t.Fatal("expected error(s) but got none")
+			}
+			if diags.HasError() && tc.ExpectError {
+				v, ok := d.GetOk("billing_project")
+				if ok {
+					val := v.(string)
+					if val != tc.ExpectedValue {
+						t.Fatalf("expected billing_project value set in provider data to be %s, got %s", tc.ExpectedValue, val)
+					}
+					if tc.ExpectFieldUnset {
+						t.Fatalf("expected billing_project value to not be set in provider data, got %s", val)
+					}
+				}
+				// Return early in tests where errors expected
+				return
+			}
+
+			v := d.Get("billing_project")
+			val := v.(string)
+			config := c.(*Config) // Should be non-nil value, as test cases reaching this point experienced no errors
+
+			if val != tc.ExpectedValue {
+				t.Fatalf("expected billing_project value set in provider data to be %s, got %s", tc.ExpectedValue, val)
+			}
+			if config.BillingProject != tc.ExpectedValue {
+				t.Fatalf("expected billing_project value in provider struct to be %s, got %s", tc.ExpectedValue, config.BillingProject)
+			}
+		})
+	}
+}
+
+func TestAccProviderBasePath_setBasePath(t *testing.T) {
+	t.Parallel()
+
+	VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckComputeAddressDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccProviderBasePath_setBasePath("https://www.googleapis.com/compute/beta/", RandString(t, 10)),
+			},
+			{
+				ResourceName:      "google_compute_address.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccProviderBasePath_setInvalidBasePath(t *testing.T) {
+	t.Parallel()
+
+	VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckComputeAddressDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccProviderBasePath_setBasePath("https://www.example.com/compute/beta/", RandString(t, 10)),
+				ExpectError: regexp.MustCompile("got HTTP response code 404 with body"),
+			},
+		},
+	})
+}
+
+func TestAccProviderMeta_setModuleName(t *testing.T) {
+	t.Parallel()
+
+	moduleName := "my-module"
+	VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckComputeAddressDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccProviderMeta_setModuleName(moduleName, RandString(t, 10)),
+			},
+			{
+				ResourceName:      "google_compute_address.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccProviderUserProjectOverride(t *testing.T) {
+	// Parallel fine-grained resource creation
+	acctest.SkipIfVcr(t)
+	t.Parallel()
+
+	org := acctest.GetTestOrgFromEnv(t)
+	billing := acctest.GetTestBillingAccountFromEnv(t)
+	pid := "tf-test-" + RandString(t, 10)
+	topicName := "tf-test-topic-" + RandString(t, 10)
+
+	config := BootstrapConfig(t)
+	accessToken, err := setupProjectsAndGetAccessToken(org, billing, pid, "pubsub", config)
+	if err != nil {
+		t.Error(err)
+	}
+
+	VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
+		// No TestDestroy since that's not really the point of this test
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccProviderUserProjectOverride_step2(accessToken, pid, false, topicName),
+				ExpectError: regexp.MustCompile("Cloud Pub/Sub API has not been used"),
+			},
+			{
+				Config: testAccProviderUserProjectOverride_step2(accessToken, pid, true, topicName),
+			},
+			{
+				ResourceName:      "google_pubsub_topic.project-2-topic",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccProviderUserProjectOverride_step3(accessToken, true),
+			},
+		},
+	})
+}
+
+// Do the same thing as TestAccProviderUserProjectOverride, but using a resource that gets its project via
+// a reference to a different resource instead of a project field.
+func TestAccProviderIndirectUserProjectOverride(t *testing.T) {
+	// Parallel fine-grained resource creation
+	acctest.SkipIfVcr(t)
+	t.Parallel()
+
+	org := acctest.GetTestOrgFromEnv(t)
+	billing := acctest.GetTestBillingAccountFromEnv(t)
+	pid := "tf-test-" + RandString(t, 10)
+
+	config := BootstrapConfig(t)
+	accessToken, err := setupProjectsAndGetAccessToken(org, billing, pid, "cloudkms", config)
+	if err != nil {
+		t.Error(err)
+	}
+
+	VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
+		// No TestDestroy since that's not really the point of this test
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccProviderIndirectUserProjectOverride_step2(pid, accessToken, false),
+				ExpectError: regexp.MustCompile(`Cloud Key Management Service \(KMS\) API has not been used`),
+			},
+			{
+				Config: testAccProviderIndirectUserProjectOverride_step2(pid, accessToken, true),
+			},
+			{
+				ResourceName:      "google_kms_crypto_key.project-2-key",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccProviderIndirectUserProjectOverride_step3(accessToken, true),
+			},
+		},
+	})
+}
+
+func testAccProviderBasePath_setBasePath(endpoint, name string) string {
+	return fmt.Sprintf(`
+provider "google" {
+  alias                   = "compute_custom_endpoint"
+  compute_custom_endpoint = "%s"
+}
+
+resource "google_compute_address" "default" {
+  provider = google.compute_custom_endpoint
+  name     = "tf-test-address-%s"
+}`, endpoint, name)
+}
+
+func testAccProviderMeta_setModuleName(key, name string) string {
+	return fmt.Sprintf(`
+terraform {
+  provider_meta "google" {
+    module_name = "%s"
+  }
+}
+
+resource "google_compute_address" "default" {
+	name = "tf-test-address-%s"
+}`, key, name)
+}
+
+// Set up two projects. Project 1 has a service account that is used to create a
+// pubsub topic in project 2. The pubsub API is only enabled in project 2,
+// which causes the create to fail unless user_project_override is set to true.
+
+func testAccProviderUserProjectOverride_step2(accessToken, pid string, override bool, topicName string) string {
+	return fmt.Sprintf(`
+// See step 3 below, which is really step 2 minus the pubsub topic.
+// Step 3 exists because provider configurations can't be removed while objects
+// created by that provider still exist in state. Step 3 will remove the
+// pubsub topic so the whole config can be deleted.
+%s
+
+resource "google_pubsub_topic" "project-2-topic" {
+	provider = google.project-1-token
+	project  = "%s-2"
+
+	name = "%s"
+	labels = {
+	  foo = "bar"
+	}
+}
+`, testAccProviderUserProjectOverride_step3(accessToken, override), pid, topicName)
+}
+
+func testAccProviderUserProjectOverride_step3(accessToken string, override bool) string {
+	return fmt.Sprintf(`
+provider "google" {
+	alias  = "project-1-token"
+	access_token = "%s"
+	user_project_override = %v
+}
+`, accessToken, override)
+}
+
+func testAccProviderIndirectUserProjectOverride_step2(pid, accessToken string, override bool) string {
+	return fmt.Sprintf(`
+// See step 3 below, which is really step 2 minus the kms resources.
+// Step 3 exists because provider configurations can't be removed while objects
+// created by that provider still exist in state. Step 3 will remove the
+// kms resources so the whole config can be deleted.
+%s
+
+resource "google_kms_key_ring" "project-2-keyring" {
+	provider = google.project-1-token
+	project  = "%s-2"
+
+	name     = "%s"
+	location = "us-central1"
+}
+
+resource "google_kms_crypto_key" "project-2-key" {
+	provider = google.project-1-token
+	name     = "%s"
+	key_ring = google_kms_key_ring.project-2-keyring.id
+}
+
+data "google_kms_secret_ciphertext" "project-2-ciphertext" {
+	provider   = google.project-1-token
+	crypto_key = google_kms_crypto_key.project-2-key.id
+	plaintext  = "my-secret"
+}
+`, testAccProviderIndirectUserProjectOverride_step3(accessToken, override), pid, pid, pid)
+}
+
+func testAccProviderIndirectUserProjectOverride_step3(accessToken string, override bool) string {
+	return fmt.Sprintf(`
+provider "google" {
+	alias = "project-1-token"
+
+	access_token          = "%s"
+	user_project_override = %v
+}
+`, accessToken, override)
 }
