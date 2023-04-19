7080313af37e2debe3f9921b31903d6719539bd9
Gaurav Jain <gauravjainhere@google.com>
2023-04-19 08:56:52 +0000

    Add test to see if primary instance creation succeeds with maximum number of fields

--- mmv1/third_party/terraform/tests/resource_alloydb_instance_test.go
+++ mmv1/third_party/terraform/tests/resource_alloydb_instance_test.go
@@ -153,3 +153,79 @@ resource "google_service_networking_connection" "vpc_connection" {
 }
 `, context)
 }
+
+// This test passes if we are able to create a primary instance with maximum number of fields
+func TestAccAlloydbInstance_createInstanceWithMaximumFields(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": RandString(t, 10),
+	}
+
+	VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckAlloydbInstanceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAlloydbInstance_createInstanceWithMandatoryFields(context),
+			},
+		},
+	})
+}
+
+func testAccAlloydbInstance_createInstanceWithMaximumFields(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_alloydb_instance" "default" {
+  cluster       = google_alloydb_cluster.default.name
+  instance_id   = "tf-test-alloydb-instance%{random_suffix}"
+  instance_type = "PRIMARY"
+  labels = {
+    test_label = "test-alloydb-label"
+  }
+  annotations = {
+    test_annotation = "test-alloydb-annotation"
+  }
+  gce_zone = "us-east1-a"
+  database_flags = {
+	  alloydb.enable_auto_explain = "true"
+  }
+  availability_type = "REGIONAL"
+  machine_config {
+	  cpu_count = 4
+  }
+  depends_on = [google_service_networking_connection.vpc_connection]
+}
+
+resource "google_alloydb_cluster" "default" {
+  cluster_id = "tf-test-alloydb-cluster%{random_suffix}"
+  location   = "us-central1"
+  network    = "projects/${data.google_project.project.number}/global/networks/${google_compute_network.default.name}"
+
+  initial_user {
+    password = "tf-test-alloydb-cluster%{random_suffix}"
+  }
+}
+
+data "google_project" "project" {
+}
+
+resource "google_compute_network" "default" {
+  name = "tf-test-alloydb-cluster%{random_suffix}"
+}
+
+resource "google_compute_global_address" "private_ip_alloc" {
+  name          =  "tf-test-alloydb-cluster%{random_suffix}"
+  address_type  = "INTERNAL"
+  purpose       = "VPC_PEERING"
+  prefix_length = 16
+  network       = google_compute_network.default.id
+}
+
+resource "google_service_networking_connection" "vpc_connection" {
+  network                 = google_compute_network.default.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
+}
+`, context)
+}
