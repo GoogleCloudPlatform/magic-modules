affdc410a018bb0669815198e8fd435f0eb9526d
Stephen Lewis (Burrows) <stephenrlewis@google.com>
2023-04-21 11:01:47 -0700

    Tgc transport fixes (#7804)
    * Fixed tgc references to transport package
    
    * Additional fixes related to the transport changes
    
    * Fixed rubocop issue
    
    * Fixed rubocop issue

--- mmv1/third_party/validator/folder_organization_policy.go
+++ mmv1/third_party/validator/folder_organization_policy.go
@@ -1,7 +1,7 @@
 package google
 
 import (
-	transport_tpg "github.com/GoogleCloudPlatform/terraform-validator/converters/google/resources/transport"
+	transport_tpg "github.com/GoogleCloudPlatform/terraform-google-conversion/v2/tfplan2cai/converters/google/resources/transport"
 )
 
 func resourceConverterFolderOrgPolicy() ResourceConverter {
