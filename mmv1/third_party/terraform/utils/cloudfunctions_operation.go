11dda639352735219d7e464dcda9f32a0ea9db93
Zhenhua Li <zhenhuali@google.com>
2023-05-15 22:06:42 -0700

    Move container_operation.go.erb to the container package

--- mmv1/third_party/terraform/utils/cloudfunctions_operation.go
+++ mmv1/third_party/terraform/utils/cloudfunctions_operation.go
@@ -3,19 +3,19 @@ package google
 import (
 	"time"
 
-	cloudfunctions_tpg "github.com/hashicorp/terraform-provider-google/google/services/cloudfunctions"
+	tpgcloudfunctions "github.com/hashicorp/terraform-provider-google/google/services/cloudfunctions"
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
 	"google.golang.org/api/cloudfunctions/v1"
 )
 
 // Deprecated: For backward compatibility cloudFunctionsOperationWait is still working,
-// but all new code should use CloudFunctionsOperationWait in the cloudfunctions package instead.
+// but all new code should use CloudFunctionsOperationWait in the tpgcloudfunctions package instead.
 func cloudFunctionsOperationWait(config *transport_tpg.Config, op *cloudfunctions.Operation, activity, userAgent string, timeout time.Duration) error {
-	return cloudfunctions_tpg.CloudFunctionsOperationWait(config, op, activity, userAgent, timeout)
+	return tpgcloudfunctions.CloudFunctionsOperationWait(config, op, activity, userAgent, timeout)
 }
 
 // Deprecated: For backward compatibility IsCloudFunctionsSourceCodeError is still working,
-// but all new code should use IsCloudFunctionsSourceCodeError in the cloudfunctions package instead.
+// but all new code should use IsCloudFunctionsSourceCodeError in the tpgcloudfunctions package instead.
 func IsCloudFunctionsSourceCodeError(err error) (bool, string) {
-	return cloudfunctions_tpg.IsCloudFunctionsSourceCodeError(err)
+	return tpgcloudfunctions.IsCloudFunctionsSourceCodeError(err)
 }
