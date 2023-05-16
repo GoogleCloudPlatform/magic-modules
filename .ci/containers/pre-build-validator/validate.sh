ab394cecbd40b6037817000304c91c87acc6547f
Thomas Rodgers <thomasrodgers@google.com>
2023-05-16 14:36:51 -0700

    Add container to call workflow

--- .ci/containers/pre-build-validator/validate.sh
+++ .ci/containers/pre-build-validator/validate.sh
@@ -2,30 +2,29 @@
 
 set -e
 
-pr_number=${PR_NUMBER:-$1}
-github_username=modular-magician
 gh_repo=magic-modules
-new_branch="auto-pr-$pr_number"
-mm_commit_sha=${COMMIT_SHA:-$2}
+new_branch="auto-pr-$PR_NUMBER"
 
-post_body=$(jq -n \
-  --arg owner "$github_username" \
-  --arg repo "$gh_repo" \
-  --arg branch "$new_branch" \
-  --arg sha "$mm_commit_sha" \
-  '{
-    ref: "main",
-    inputs: {
-      owner: $owner,
-      repo: $repo,
-      branch: $branch,
-      sha: $sha,
-    }
-  }')
+if [ $PR_NUMBER == 7874 ]; then
+  post_body=$(jq -n \
+    --arg owner "GoogleCloudPlatform" \
+    --arg repo "$gh_repo" \
+    --arg branch "$new_branch" \
+    --arg sha "$COMMIT_SHA" \
+    '{
+      ref: "main",
+      inputs: {
+        owner: $owner,
+        repo: $repo,
+        branch: $branch,
+        sha: $sha,
+      }
+    }')
 
-curl \
-  -X POST \
-  -u "$github_username:$GITHUB_TOKEN" \
-  -H "Accept: application/vnd.github.v3+json" \
-  "https://api.github.com/repos/GoogleCloudPlatform/magic-modules/actions/workflows/pre-build-validation.yml/dispatches" \
-  -d "$post_body"
+  curl \
+    -X POST \
+    -u "modular-magician:$GITHUB_TOKEN" \
+    -H "Accept: application/vnd.github.v3+json" \
+    "https://api.github.com/repos/GoogleCloudPlatform/magic-modules/actions/workflows/pre-build-validation.yml/dispatches" \
+    -d "$post_body"
+fi
