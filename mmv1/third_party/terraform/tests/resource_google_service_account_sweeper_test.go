b44278f0fb4c33f48fc2c04e5a2627ce691d1108
Cameron Thornton <camthornton@google.com>
2023-05-22 17:13:03 -0500

    fix import

--- mmv1/third_party/terraform/tests/resource_google_service_account_sweeper_test.go
+++ mmv1/third_party/terraform/tests/resource_google_service_account_sweeper_test.go
@@ -6,9 +6,9 @@ import (
 	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-provider-google-beta/google-beta/acctest"
-	"github.com/hashicorp/terraform-provider-google-beta/google-beta/tpgresource"
-	transport_tpg "github.com/hashicorp/terraform-provider-google-beta/google-beta/transport"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
+	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
 )
 
 // This will sweep Service Account resources
