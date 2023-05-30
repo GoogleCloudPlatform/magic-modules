36305a07d35a3878f26d2e50c3ed0f3d0c6f12f0
Ben Fogel <bfogel@google.com>
2023-05-30 13:10:47 +0000

    Add edge container service

--- .ci/infra/terraform/main.tf
+++ .ci/infra/terraform/main.tf
@@ -215,6 +215,7 @@ module "project-services" {
     "dns.googleapis.com",
     "documentai.googleapis.com",
     "edgecache.googleapis.com",
+    "edgecontainer.googleapis.com",
     "essentialcontacts.googleapis.com",
     "eventarc.googleapis.com",
     "eventarcpublishing.googleapis.com",
