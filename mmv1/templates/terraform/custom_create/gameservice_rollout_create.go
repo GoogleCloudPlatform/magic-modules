3aa9628927d535ced56230e4ee371be4771ba464
Sarah French <sarah.french@hashicorp.com>
2023-06-01 15:06:54 +0100

    Address build errors

--- mmv1/templates/terraform/custom_create/gameservice_rollout_create.go
+++ mmv1/templates/terraform/custom_create/gameservice_rollout_create.go
@@ -1,1 +1,3 @@
 fmt.Println("This code is added by custom_create")
+fmt.Println("Print config to avoid build error %#v", config)
+return nil
--- mmv1/templates/terraform/custom_update/gameservice_rollout_update.go
+++ mmv1/templates/terraform/custom_update/gameservice_rollout_update.go
@@ -1,1 +1,3 @@
 fmt.Println("This code is added by custom_update")
+fmt.Println("Print config to avoid build error %#v", config)
+return nil
