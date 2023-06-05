1df99424330e80940e83e814777bae54b2cc5fe8
Kamal Aboul-Hosn <kamal.aboulhosn@gmail.com>
2023-06-05 12:21:20 -0400

    Test update, import

---
+++ mmv1/third_party/terraform/tests/resource_pubsub_schema_test.go
@@ -0,0 +1,59 @@
+package google
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+)
+
+func TestAccPubsubSchema_update(t *testing.T) {
+	t.Parallel()
+
+	schema := fmt.Sprintf("tf-test-schema-%s", RandString(t, 10))
+
+	VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckPubsubSubscriptionDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPubsubSchema_basic(schema),
+			},
+			{
+				ResourceName:      "google_pubsub_schema.foo",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccPubsubSchema_updated(schema),
+			},
+			{
+				ResourceName:      "google_pubsub_schema.foo",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccPubsubSchema_basic(schema string) string {
+	return fmt.Sprintf(`
+	resource "google_pubsub_schema" "foo" {
+		name = "%s"
+		type = "PROTOCOL_BUFFER"
+		definition = "syntax = \"proto3\";\nmessage Results {\nstring message_request = 1;\nstring message_response = 2;\n}"
+	}
+`, schema)
+}
+
+func testAccPubsubSchema_updated(schema string) string {
+	return fmt.Sprintf(`
+	resource "google_pubsub_schema" "foo" {
+		name = "%s"
+		type = "PROTOCOL_BUFFER"
+		definition = "syntax = \"proto3\";\nmessage Results {\nstring message_request = 1;\nstring message_response = 2;\nstring timestamp_request = 3;\n}"
+	}
+`, schema)
+}
