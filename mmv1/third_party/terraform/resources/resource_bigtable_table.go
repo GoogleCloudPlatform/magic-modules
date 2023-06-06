8056707621beee714794b7790163e7037c168169
Ron Gal <rongal@google.com>
2023-06-06 17:59:38 +0000

    Adjust default timeout value

--- mmv1/third_party/terraform/resources/resource_bigtable_table.go
+++ mmv1/third_party/terraform/resources/resource_bigtable_table.go
@@ -27,8 +27,8 @@ func ResourceBigtableTable() *schema.Resource {
 
 		// Set a longer timeout for table creation as adding column families can be slow.
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(45 * time.Minute),
-			Update: schema.DefaultTimeout(45 * time.Minute),
+			Create: schema.DefaultTimeout(20 * time.Minute),
+			Update: schema.DefaultTimeout(20 * time.Minute),
 		},
 
 		// ----------------------------------------------------------------------
