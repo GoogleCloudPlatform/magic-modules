3e1c88e6c77ac349fe6b271edf94dfc1415df0a3
Avi Dave <avidave97@gmail.com>
2023-06-07 18:19:28 +0000

    fix: want to reset state of PR

--- .ci/infra/terraform/main.tf
+++ .ci/infra/terraform/main.tf
@@ -294,6 +294,7 @@ module "project-services" {
     "testing.googleapis.com",
     "tpu.googleapis.com",
     "trafficdirector.googleapis.com",
+    "vmwareengine.googleapis.com",
     "vpcaccess.googleapis.com",
     "websecurityscanner.googleapis.com",
     "workflowexecutions.googleapis.com",
--- mmv1/third_party/terraform/utils/bootstrap_test_utils.go
+++ mmv1/third_party/terraform/utils/bootstrap_test_utils.go
@@ -1,4 +1,4 @@
-package google
+package compute
 
 import (
 	"context"
