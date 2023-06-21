d4854b5c27c7519560575b5c1886c47acc3a6fee
Zhenhua Li <zhenhuali@google.com>
2023-06-20 21:08:59 -0700

    Keep ResourceMap in the google package

--- mmv1/third_party/terraform/utils/provider.go
+++ mmv1/third_party/terraform/utils/provider.go
@@ -6,6 +6,19 @@ import (
 )
 
 // Provider returns a *schema.Provider.
+
+// This function stays in the google package temporarily to not break the terraform-google-conversion.
+// TODO: remove it in a later PR
 func Provider() *schema.Provider {
 	return provider.Provider()
+}
+
+// Generated resources: <%= resource_count %>
+// Generated IAM resources: <%= iam_resource_count %>
+// Total generated resources: <%= resource_count + iam_resource_count %>
+
+// This function stays in the google package temporarily to not break the tools missting tests detector and breaking changes detector.
+// TODO: remove it in a later PR
+func ResourceMap() map[string]*schema.Resource {
+	return provider.ResourceMap()
 }
