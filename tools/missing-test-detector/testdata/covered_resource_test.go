43c2d4068ad79ba8fb3a74eaa85c7fd44fadecb8
Thomas Rodgers <thomasrodgers@google.com>
2023-06-23 21:45:57 +0000

    Handle nested call expressions in config function

--- tools/missing-test-detector/testdata/covered_resource_test.go
+++ tools/missing-test-detector/testdata/covered_resource_test.go
@@ -1,6 +1,7 @@
 package google
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -20,7 +21,7 @@ func TestAccCoveredResource(t *testing.T) {
 }
 
 func testAccCoveredResource() string {
-	return Nprintf(`
+	return fmt.Sprintf(Nprintf(`
 resource "covered_resource" "resource" {
   field_one = "value-one"
   field_four {
@@ -30,7 +31,7 @@ resource "covered_resource" "resource" {
   }
   field_seven = %{bool}
 }
-`, context)
+`, context))
 }
 
 func testAccCoveredResource_update() string {
