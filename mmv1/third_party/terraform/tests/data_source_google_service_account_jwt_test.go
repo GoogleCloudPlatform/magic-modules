1da5379336a65d64d2d55c2db725825f532f32a8
Zhenhua Li <zhenhuali@google.com>
2023-06-30 16:39:57 -0700

    Move bootstrap utility functons to the acctest package (#8244)
    * Move bootstrap utility functons to the acctest package
    
    * Remove unrelated changes.

--- mmv1/third_party/terraform/tests/data_source_google_service_account_jwt_test.go
+++ mmv1/third_party/terraform/tests/data_source_google_service_account_jwt_test.go
@@ -100,7 +100,7 @@ func TestAccDataSourceGoogleServiceAccountJwt(t *testing.T) {
 
 	resourceName := "data.google_service_account_jwt.default"
 	serviceAccount := envvar.GetTestServiceAccountFromEnv(t)
-	targetServiceAccountEmail := BootstrapServiceAccount(t, envvar.GetTestProjectFromEnv(), serviceAccount)
+	targetServiceAccountEmail := acctest.BootstrapServiceAccount(t, envvar.GetTestProjectFromEnv(), serviceAccount)
 
 	staticTime := time.Now()
 
