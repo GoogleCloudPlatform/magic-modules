5038a9204d2395291175e3ade90b5f02b841eb17
Thomas Rodgers <thomasrodgers@google.com>
2023-07-06 18:08:02 +0000

    Add test

--- tools/issue-labeler/main.go
+++ tools/issue-labeler/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
-	"regexp"
-	"strings"
 
-	"github.com/go-yaml/yaml"
 	"github.com/golang/glog"
+	"gopkg.in/yaml.v2"
 )
 
 func main() {
+	issueBody := os.Getenv("ISSUE_BODY")
 	file, err := os.ReadFile("enrolled_teams.yaml")
 	if err != nil {
 		glog.Exitf("Error reading enrolled teams yaml: %v", err)
@@ -20,20 +19,5 @@ func main() {
 	if err != nil {
 		glog.Exitf("Error unmarshalling enrolled teams yaml: %v", err)
 	}
-	issueBody := os.Getenv("ISSUE_BODY")
-	sectionRegexp := regexp.MustCompile(`### (New or )?Affected Resource\(s\)[^#]+`)
-	affectedResources := sectionRegexp.FindString(issueBody)
-	var labels []string
-	for label, resources := range enrolledTeams {
-		for _, resource := range resources {
-			if strings.Contains(affectedResources, resource) {
-				labels = append(labels, "\""+label+"\"")
-				break
-			}
-		}
-	}
-
-	if len(labels) > 0 {
-		fmt.Println("[" + strings.Join(labels, ", ") + "]")
-	}
+	fmt.Println(labels(issueBody, enrolledTeams))
 }
