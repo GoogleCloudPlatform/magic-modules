c9a0e7ce0724e332f63ca2d77cec5c1c980c46f5
Zhenhua Li <zhenhuali@google.com>
2023-07-10 12:19:16 -0700

    Fix rebasing errors

--- mmv1/third_party/terraform/services/compute/data_source_google_cloudfunctions_function_test.go
+++ mmv1/third_party/terraform/services/compute/data_source_google_cloudfunctions_function_test.go
@@ -16,7 +16,7 @@ func TestAccDataSourceGoogleCloudFunctionsFunction_basic(t *testing.T) {
 	funcDataNameHttp := "data.google_cloudfunctions_function.function_http"
 	functionName := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
 	bucketName := fmt.Sprintf("tf-test-bucket-%d", acctest.RandInt(t))
-	zipFilePath := createZIPArchiveForCloudFunctionSource(t, testHTTPTriggerPath)
+	zipFilePath := acctest.CreateZIPArchiveForCloudFunctionSource(t, testHTTPTriggerPath)
 	defer os.Remove(zipFilePath) // clean up
 
 	acctest.VcrTest(t, resource.TestCase{
