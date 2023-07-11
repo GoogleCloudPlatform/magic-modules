523086155b39d67fa6a39bddee28ce4395059f5e
diogoEsteves <diogoesteves@ciandt.com>
2023-07-11 13:48:44 +0000

    adding a testing update, just adding the labels for the resource

---
+++ mmv1/third_party/terraform/tests/resource_network_connectivity_service_connection_policies_test.go
@@ -0,0 +1,153 @@
+package google
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+)
+
+func TestAccNetworkConnectivityServiceConnectionPolicy_update(t *testing.T) {
+	t.Parallel()
+
+  networkName := fmt.Sprintf("tf-test-network-%s", RandString(t, 10))
+  networkProducerName := fmt.Sprintf("tf-test-network-%s", RandString(t, 10))
+  subnetworkConsumerName := fmt.Sprintf("tf-test-subnet-consumer-%s", RandString(t, 10))
+  subnetworkProducerName := fmt.Sprintf("tf-test-subnet-producer-%s", RandString(t, 10)) 
+	serviceClassName := fmt.Sprintf("tf-test-service-class-%s", RandString(t, 10))
+	serviceConnectionPolicyName := fmt.Sprintf("tf-test-service-connection-policy-%s", RandString(t, 10))
+	
+  VcrTest(t, resource.TestCase{
+		PreCheck:     func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
+		CheckDestroy: testAccCheckNetworkConnectivityServiceConnectionPolicyDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetworkConnectivityServiceConnectionPolicy_basic(networkName, subnetworkConsumerName, networkProducerName, subnetworkProducerName, serviceClassName, serviceConnectionPolicyName),
+			},
+			{
+				ResourceName:      "google_network_connectivity_service_connection_policy.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccNetworkConnectivityServiceConnectionPolicy_update(networkName, subnetworkConsumerName, networkProducerName, subnetworkProducerName, serviceClassName, serviceConnectionPolicyName),
+			},
+			{
+				ResourceName:      "google_network_connectivity_service_connection_policy.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccNetworkConnectivityServiceConnectionPolicy_basic(networkName, subnetworkConsumerName, networkProducerName, subnetworkProducerName, serviceClassName, serviceConnectionPolicyName),
+			},
+			{
+				ResourceName:      "google_network_connectivity_service_connection_policy.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccNetworkConnectivityServiceConnectionPolicy_basic(networkName, subnetworkConsumerName, networkProducerName, subnetworkProducerName, serviceClassName, serviceConnectionPolicyName string) string {
+	return fmt.Sprintf(`
+  resource "google_compute_network" "consumer_net" {
+    name                    = "%s"
+    auto_create_subnetworks = false
+  }
+  
+  resource "google_compute_subnetwork" "consumer_subnet" {
+    name          = "%s"
+    ip_cidr_range = "10.0.0.0/16"
+    region        = "us-central1"
+    network       = google_compute_network.consumer_net.id
+  }
+  
+  resource "google_compute_network" "producer_net" {
+    name                    = "%s"
+    auto_create_subnetworks = false
+  }
+  
+  resource "google_compute_subnetwork" "producer_subnet" {
+    name          = "%s"
+    ip_cidr_range = "10.0.0.0/16"
+    region        = "us-central1"
+    network       = google_compute_network.producer_net.id
+  }
+  
+  resource "google_network_connectivity_service_class" "default"{
+    name        = "%s"
+    location    = "us-central1"
+    description = "my-basic serviceClass"
+    labels      = {
+        foo = "bar"
+      }
+  }
+  
+  resource "google_network_connectivity_service_connection_policy" "default" {
+    name = "%s"
+    location = "us-central1"
+    description = "my basic sevice connection policy"
+    service_class = google_network_connectivity_service_class.default.service_class
+    network = google_compute_network.producer_net.id
+    psc_config {
+      subnetworks = [google_compute_subnetwork.producer_subnet.id]
+      limit = 2
+    }
+  }
+`, networkName, subnetworkConsumerName, networkProducerName, subnetworkProducerName, serviceClassName, serviceConnectionPolicyName)
+}
+
+func testAccNetworkConnectivityServiceConnectionPolicy_update(networkName, subnetworkConsumerName, networkProducerName, subnetworkProducerName, serviceClassName, serviceConnectionPolicyName string) string {
+	return fmt.Sprintf(`
+resource "google_compute_network" "consumer_net" {
+  name                    = "%s"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "consumer_subnet" {
+  name          = "%s"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.consumer_net.id
+}
+
+resource "google_compute_network" "producer_net" {
+  name                    = "%s"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "producer_subnet" {
+  name          = "%s"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.producer_net.id
+}
+
+resource "google_network_connectivity_service_class" "default"{
+  name        = "%s"
+  location    = "us-central1"
+  description = "my-basic serviceClass"
+  labels      = {
+    foo = "bar"
+  }
+}
+
+resource "google_network_connectivity_service_connection_policy" "default" {
+  name = "%s"
+  location = "us-central1"
+  description = "my basic sevice connection policy"
+  service_class = google_network_connectivity_service_class.default.service_class
+  network = google_compute_network.producer_net.id
+  psc_config {
+    subnetworks = [google_compute_subnetwork.producer_subnet.id]
+    limit = 2
+  }
+  labels      = {
+    foo = "bar"
+  }
+}
+`, networkName, subnetworkConsumerName, networkProducerName, subnetworkProducerName, serviceClassName, serviceConnectionPolicyName)
+}
