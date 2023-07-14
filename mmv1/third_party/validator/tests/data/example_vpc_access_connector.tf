7ef2d46166af2e7a3945d8db7cb8d51e94aa2dac
Jashan Sudan <jashansudan@gmail.com>
2023-07-14 07:32:28 -0700

    Add Update Connector Support for VPC Access Connectors (#8205)
    * Add Update Connetor Support for VPC Access Connectors
    
    * fix lint
    
    * fix go lint
    
    * fix tests

--- mmv1/third_party/validator/tests/data/example_vpc_access_connector.tf
+++ mmv1/third_party/validator/tests/data/example_vpc_access_connector.tf
@@ -27,8 +27,10 @@ provider "google" {
 }
 
 resource "google_vpc_access_connector" "connector" {
-  name          = "vpc-con"
-  ip_cidr_range = "10.8.0.0/28"
-  network       = "default"
-  region        = "us-central1"
+  name           = "vpc-con"
+  ip_cidr_range  = "10.8.0.0/28"
+  network        = "default"
+  region         = "us-central1"
+  max_throughput = 300
+  min_throughput = 200
 }
