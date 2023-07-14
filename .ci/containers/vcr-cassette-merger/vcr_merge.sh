9564f220c3417042c7f911da5f2e4a07e668e186
Shuya Ma <shuyama@google.com>
2023-07-14 02:39:51 -0700

    fix vcr_merge

--- .ci/containers/vcr-cassette-merger/vcr_merge.sh
+++ .ci/containers/vcr-cassette-merger/vcr_merge.sh
@@ -4,16 +4,16 @@ set -e
 
 REFERENCE=$1
 
-PR_NUMBER=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
-    "https://api.github.com/repos/GoogleCloudPlatform/magic-modules/pulls?state=closed&base=main&sort=updated&direction=desc" | \
-    jq -r ".[] | if .merge_commit_sha == \"$REFERENCE\" then .number else empty end")
-
 # for backwards-compatibility
 if [ -z "$BASE_BRANCH" ]; then
   BASE_BRANCH=main
 else
   echo "BASE_BRANCH: $BASE_BRANCH"
 fi
+
+PR_NUMBER=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
+    "https://api.github.com/repos/GoogleCloudPlatform/magic-modules/pulls?state=closed&base=$BASE_BRANCH&sort=updated&direction=desc" | \
+    jq -r ".[] | if .merge_commit_sha == \"$REFERENCE\" then .number else empty end")
 
 set +e
 gsutil ls gs://ci-vcr-cassettes/refs/heads/auto-pr-$PR_NUMBER/fixtures/
