a08e7d84a4c2adf4bdbcc08ce348837a6894b21b
Scott Suarez <ScottMSuarez@gmail.com>
2023-07-18 13:39:36 -0700

    breaking-change file name changed

--- tools/breaking-change-detector/constants/constants.go
+++ tools/breaking-change-detector/constants/constants.go
@@ -1,7 +1,7 @@
 package constants
 
 const BreakingChangeRelativeLocation = "develop/"
-const BreakingChangeFileName = "breaking-changes"
+const BreakingChangeFileName = "make-a-breaking-change"
 
 var docsite = "https://googlecloudplatform.github.io/magic-modules/"
 
