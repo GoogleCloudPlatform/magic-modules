752b224e2bba309320d472e86fb0949480699d24
Zhenhua Li <zhenhuali@google.com>
2023-07-21 11:37:32 -0700

    Move some handwritten acc tests to service package (#8400)
    * Move function serviceAccountCanonicalEmail
    
    * Move provider tests
    
    * accessapproval tetss
    
    * Move activedirectory_test, appengine_test, bigquery_test, bigquerydatatransfer_test, bigtable_test
    
    * Move composer_test, cloudtasks_test, cloudrun_test, cloudiot_test, cloudids_test
    
    * Move compute_test
    
    * Move firestore_test
    
    * Move redis_test
    
    * Move storagetransfer_test
    
    * Fix errors
    
    * Move vpcaccess_test
    
    * Move apigee_test
    
    * Put function to test_utils.go to not break tgc
    
    * Remove spaces
    
    * Move resource_endpoints_service_test.go back to the google package
    
    * Fix rebasing errors

--- mmv1/third_party/terraform/envvar/envvar_utils.go
+++ mmv1/third_party/terraform/envvar/envvar_utils.go
@@ -1,6 +1,7 @@
 package envvar
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"testing"
@@ -195,4 +196,8 @@ func SkipIfEnvNotSet(t *testing.T, envs ...string) {
 			t.Skipf("Environment variable %s is not set", k)
 		}
 	}
+}
+
+func ServiceAccountCanonicalEmail(account string) string {
+	return fmt.Sprintf("%s@%s.iam.gserviceaccount.com", account, GetTestProjectFromEnv())
 }
