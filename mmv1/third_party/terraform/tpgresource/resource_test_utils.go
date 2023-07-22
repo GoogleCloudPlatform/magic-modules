d47f7f6cfb265c64cb487dbc59fa616acac372eb
Zhenhua Li <zhenhuali@google.com>
2023-07-21 17:32:08 -0700

    Revert "Move some handwritten acc tests to service package (#8400)" (#8426)
    This reverts commit 8ab7bd30b64ab1faba969744dc6667cec63d0653.

--- mmv1/third_party/terraform/tpgresource/resource_test_utils.go
+++ mmv1/third_party/terraform/tpgresource/resource_test_utils.go
@@ -1,7 +1,6 @@
 package tpgresource
 
 import (
-	"fmt"
 	"reflect"
 	"regexp"
 	"strings"
@@ -178,17 +177,4 @@ func SetupTestResourceDataFromConfigMap(t *testing.T, s map[string]*schema.Schem
 	}
 
 	return d
-}
-
-func GetResourceAttributes(n string, s *terraform.State) (map[string]string, error) {
-	rs, ok := s.RootModule().Resources[n]
-	if !ok {
-		return nil, fmt.Errorf("Not found: %s", n)
-	}
-
-	if rs.Primary.ID == "" {
-		return nil, fmt.Errorf("No ID is set")
-	}
-
-	return rs.Primary.Attributes, nil
 }
