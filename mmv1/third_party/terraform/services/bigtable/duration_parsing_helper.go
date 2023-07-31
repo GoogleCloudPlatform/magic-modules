20c82ff60373d15b74cbd119dd20d43777499509
Ron Gal <rongal@google.com>
2023-07-31 19:21:58 +0000

    Add time.UNIT

--- mmv1/third_party/terraform/services/bigtable/duration_parsing_helper.go
+++ mmv1/third_party/terraform/services/bigtable/duration_parsing_helper.go
@@ -6,16 +6,16 @@ import (
 )
 
 var unitMap = map[string]uint64{
-	"ns": uint64(Nanosecond),
-	"us": uint64(Microsecond),
-	"µs": uint64(Microsecond), // U+00B5 = micro symbol
-	"μs": uint64(Microsecond), // U+03BC = Greek letter mu
-	"ms": uint64(Millisecond),
-	"s":  uint64(Second),
-	"m":  uint64(Minute),
-	"h":  uint64(Hour),
-	"d":  uint64(Hour) * 24,
-	"w":  uint64(Hour) * 24 * 7,
+	"ns": uint64(time.Nanosecond),
+	"us": uint64(time.Microsecond),
+	"µs": uint64(time.Microsecond), // U+00B5 = micro symbol
+	"μs": uint64(time.Microsecond), // U+03BC = Greek letter mu
+	"ms": uint64(time.Millisecond),
+	"s":  uint64(time.Second),
+	"m":  uint64(time.Minute),
+	"h":  uint64(time.Hour),
+	"d":  uint64(time.Hour) * 24,
+	"w":  uint64(time.Hour) * 24 * 7,
 }
 
 // ParseDuration parses a duration string.
