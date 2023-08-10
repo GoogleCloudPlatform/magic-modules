b233914c061360275ca1b7316039d7e4d06da419
Nevin Zheng <nevzheng@gmail.com>
2023-08-10 22:58:07 +0000

    fix: add `uniform_bucket_level_access`

--- mmv1/third_party/terraform/tests/resource_bigquery_table_test.go
+++ mmv1/third_party/terraform/tests/resource_bigquery_table_test.go
@@ -2152,6 +2152,7 @@ resource "google_storage_bucket" "test" {
   name          = "%s"
   location      = "US"
   force_destroy = true
+  uniform_bucket_level_access = true
 }
 
 data "google_project" "project" {}
