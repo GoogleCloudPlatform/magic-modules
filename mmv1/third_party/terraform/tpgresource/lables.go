60bbf10bddf368612e4ec8a41a2a41f02b06a2f2
Zhenhua Li <zhenhuali@google.com>
2023-08-10 21:56:03 -0700

    Fix tests

--- mmv1/third_party/terraform/tpgresource/lables.go
+++ mmv1/third_party/terraform/tpgresource/lables.go
@@ -4,15 +4,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-func EffectiveLabelsSchema() *schema.Schema {
-	return &schema.Schema{
-		Type:        schema.TypeMap,
-		Computed:    true,
-		Description: `All of labels (key/value pairs) present on the resource in GCP, including the labels configured through Terraform, other clients and services.`,
-		Elem:        &schema.Schema{Type: schema.TypeString},
-	}
-}
-
 func FlattenLabels(labels map[string]string, d *schema.ResourceData) map[string]interface{} {
 	transformed := make(map[string]interface{})
 
