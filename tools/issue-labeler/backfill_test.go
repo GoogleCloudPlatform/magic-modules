dbf70c82f81b88114a45ab0f2aff76c7943e8be7
Thomas Rodgers <thomasrodgers@google.com>
2023-08-17 20:49:14 +0000

    Add workflow for issue backfill (#8673)
    * Add workflow for issue backfill
    
    * Improve issue labeler output
    
    * Log unknown messages from API
    
    * Add expected date format

--- tools/issue-labeler/backfill_test.go
+++ tools/issue-labeler/backfill_test.go
@@ -125,12 +125,14 @@ func TestComputeIssueUpdates(t *testing.T) {
 			regexpLabels: defaultRegexpLabels,
 			expectedIssueUpdates: []IssueUpdate{
 				{
-					Number: 1,
-					Labels: []string{"forward/review", "service/service1", "service/service2-subteam1"},
+					Number:    1,
+					Labels:    []string{"forward/review", "service/service1", "service/service2-subteam1"},
+					OldLabels: []string{"service/service2-subteam1"},
 				},
 				{
-					Number: 2,
-					Labels: []string{"forward/review", "service/service1", "service/service2-subteam2"},
+					Number:    2,
+					Labels:    []string{"forward/review", "service/service1", "service/service2-subteam2"},
+					OldLabels: []string{"service/service1"},
 				},
 			},
 		},
@@ -146,8 +148,9 @@ func TestComputeIssueUpdates(t *testing.T) {
 			regexpLabels: defaultRegexpLabels,
 			expectedIssueUpdates: []IssueUpdate{
 				{
-					Number: 1,
-					Labels: []string{"forward/linked", "service/service1", "service/service2-subteam1"},
+					Number:    1,
+					Labels:    []string{"forward/linked", "service/service1", "service/service2-subteam1"},
+					OldLabels: []string{"service/service2-subteam1", "forward/linked"},
 				},
 			},
 		},
