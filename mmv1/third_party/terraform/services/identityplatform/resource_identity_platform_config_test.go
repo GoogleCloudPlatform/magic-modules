38123878c2217d1dddb2f78d0f35abe2829a15df
Mohamed (Fouad) Abdelkader <mfouad@google.com>
2023-08-18 14:19:10 -0400

    Add back resource blocks in the update test, and enable VCR.

--- mmv1/third_party/terraform/services/identityplatform/resource_identity_platform_config_test.go
+++ mmv1/third_party/terraform/services/identityplatform/resource_identity_platform_config_test.go
@@ -88,8 +88,23 @@ resource "google_identity_platform_config" "basic" {
 
 func testAccIdentityPlatformConfig_update(context map[string]interface{}) string {
 	return acctest.Nprintf(`
+resource "google_project" "basic" {
+  project_id = "tf-test-my-project%{random_suffix}"
+  name       = "tf-test-my-project%{random_suffix}"
+  org_id     = "%{org_id}"
+  billing_account =  "%{billing_acct}"
+  labels = {
+    firebase = "enabled"
+  }
+}
+
+resource "google_project_service" "identitytoolkit" {
+  project = google_project.basic.project_id
+  service = "identitytoolkit.googleapis.com"
+}
+
 resource "google_identity_platform_config" "update" {
-  project = "tf-test-my-project%{random_suffix}"
+  project = google_project.basic.project_id
   sign_in {
     allow_duplicate_emails = false
 
