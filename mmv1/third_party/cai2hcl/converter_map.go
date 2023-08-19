e180333ba04453e0ffac226c6a8fdcf328076cd2
Amir Romashkin <romashkin@google.com>
2023-08-19 22:04:10 +0000

    Fix conterter_map: remove excessive param

--- mmv1/third_party/cai2hcl/converter_map.go
+++ mmv1/third_party/cai2hcl/converter_map.go
@@ -3,8 +3,6 @@ package generated
 import (
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v2/cai2hcl/generated/converters/common"
 	computeConverters "github.com/GoogleCloudPlatform/terraform-google-conversion/v2/cai2hcl/generated/converters/compute"
-	tfschema "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	tpg "github.com/hashicorp/terraform-provider-google/google"
 )
 
 var ConverterNames = map[string]string{
@@ -16,9 +14,7 @@ var ConverterNames = map[string]string{
 var ConverterMap map[string]common.Converter
 
 func init() {
-	var schemaProvider = tpg.Provider()
-
-	var factoryMap = map[string]func(schema map[string]*tfschema.Schema, name string) common.Converter{
+	var factoryMap = map[string]func(name string) common.Converter{
 		"google_compute_forwarding_rule": computeConverters.NewComputeForwardingRuleConverter,
 		"google_compute_backend_service": computeConverters.NewComputeBackendServiceConverter,
 		"google_compute_health_check":    computeConverters.NewComputeHealthCheckConverter,
@@ -26,8 +22,6 @@ func init() {
 
 	ConverterMap = make(map[string]common.Converter, len(factoryMap))
 	for name, factory := range factoryMap {
-		schema := schemaProvider.ResourcesMap[name].Schema
-
-		ConverterMap[name] = factory(schema, name)
+		ConverterMap[name] = factory(name)
 	}
 }
