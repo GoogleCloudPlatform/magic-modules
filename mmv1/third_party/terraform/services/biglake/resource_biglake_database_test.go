84810b583ed1c3e6b2f77bdcc6d1a94e32a595d1
Nevin Zheng <nevzheng@gmail.com>
2023-08-21 20:49:45 +0000

    fix: missing db name

--- mmv1/third_party/terraform/services/biglake/resource_biglake_database_test.go
+++ mmv1/third_party/terraform/services/biglake/resource_biglake_database_test.go
@@ -67,7 +67,7 @@ resource "google_biglake_catalog" "default" {
     location = "US"
 }
 
-resource "google_biglake_database" "" {
+resource "google_biglake_database" "default" {
 	# Update Database Id
     database_id = "tf-test-my-database%{random_suffix}-%{random_suffix}"
     catalog_id = google_biglake_catalog.default.catalog_id
