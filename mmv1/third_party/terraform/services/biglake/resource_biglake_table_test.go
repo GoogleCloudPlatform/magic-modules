2ed3982b9afb031edc8f6066a70101dfae020671
Nevin Zheng <nevzheng@gmail.com>
2023-08-23 21:36:59 +0000

    Fix: Table Test

--- mmv1/third_party/terraform/services/biglake/resource_biglake_table_test.go
+++ mmv1/third_party/terraform/services/biglake/resource_biglake_table_test.go
@@ -44,10 +44,49 @@ func TestAccBiglakeTable_bigqueryBiglakeTable_update(t *testing.T) {
 
 func testAccBiglakeTable_bigqueryBiglakeTable_update(context map[string]interface{}) string {
 	return acctest.Nprintf(`
+resource "google_biglake_catalog" "catalog" {
+	name = "<%= ctx[:vars]['catalog_id'] %>"
+	location = "US"
+}
+
+resource "google_storage_bucket" "bucket" {
+	name                        = "<%= ctx[:vars]['bucket_id'] %>"
+	location                    = "US"
+	force_destroy               = true
+	uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_object" "metadata_folder" {
+	name    = "metadata/"
+	content = " "
+	bucket  = google_storage_bucket.bucket.name
+}
+
+
+resource "google_storage_bucket_object" "data_folder" {
+	name    = "data/"
+	content = " "
+	bucket  = google_storage_bucket.bucket.name
+}
+
+resource "google_biglake_database" "database" {
+	name = "<%= ctx[:vars]['database_id'] %>"
+	catalog_id = google_biglake_catalog.catalog.catalog_id
+	location = google_biglake_catalog.catalog.location
+	type = "HIVE"
+	hive_options {
+		location_uri = "gs://${google_storage_bucket.bucket.name}/${google_storage_bucket_object.metadata_folder.name}"
+		parameters = {
+			"name" = "wrench"
+		}
+	}
+}
+
+
 resource "google_biglake_table" "table" {
     name = "tf_test_my_table%{random_suffix}"
     catalog_id = google_biglake_catalog.catalog.catalog_id
-    database_id = google_biglake_database.database.database_id
+    database_id = google_biglake_database.database.name
     location = google_biglake_catalog.catalog.location
     type = "HIVE"
     hive_options {
