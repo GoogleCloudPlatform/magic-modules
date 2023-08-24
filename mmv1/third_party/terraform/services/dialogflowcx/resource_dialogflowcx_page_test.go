bbc99517c736e1cf846def2201396e79e5a64e25
Maciej Murakowski <mmurakowski@google.com>
2023-08-24 15:43:08 -0400

    Adding "basic" Page creation, filling out "full" Page creation with (I hope) all params

--- mmv1/third_party/terraform/services/dialogflowcx/resource_dialogflowcx_page_test.go
+++ mmv1/third_party/terraform/services/dialogflowcx/resource_dialogflowcx_page_test.go
@@ -109,8 +109,205 @@ func testAccDialogflowCXPage_full(context map[string]interface{}) string {
 
 		entry_fulfillment {
 			messages {
+				channel = "some-channel"
+				response_type = "ENTRY_PROMPT"
 				text {
 					text = ["Welcome to page"]
+				}
+			}
+			messages {
+				payload = <<EOF
+						{"some-key": "some-value", "other-key": ["other-value"]}
+				EOF
+			}
+			messages {
+				conversation_success {
+					metadata = <<EOF
+							{"some-metadata-key": "some-value", "other-metadata-key": 1234}
+					EOF
+				}
+			}
+			messages {
+				output_audio_text {
+					text = "some output text"
+				}
+			}
+			messages {
+				output_audio_text {
+					ssml = <<EOF
+							<speak>Some example <say-as interpret-as="characters">SSML XML</say-as></speak>
+					EOF
+				}
+			}
+			messages {
+				live_agent_handoff {
+					metadata = <<EOF
+							{"some-metadata-key": "some-value", "other-metadata-key": 1234}
+					EOF
+				}
+			}
+			messages {
+				play_audio {
+					audio_uri = "http://example.com/some-audio-file.mp3"
+				}
+			}
+			messages {
+				telephony_transfer_call {
+					phone_number = "1-234-567-8901"
+				}
+			}
+
+			tag = "some-tag"
+
+			set_parameter_actions {
+				parameter = "some-param"
+				value     = "123.45"
+			}
+			set_parameter_actions {
+				parameter = "another-param"
+				value     = jsonencode("abc")
+			}
+			set_parameter_actions {
+				parameter = "other-param"
+				value     = jsonencode(["foo"])
+			}
+
+			conditional_cases {
+				cases = [
+					jsonencode({
+						condition = "$sys.func.RAND() < 0.5",
+						caseContent = [
+							{
+								message = { text = ["First case"] }
+							},
+							{
+								additionalCases = {
+									cases = [
+										{
+											condition = "$sys.func.RAND() < 0.2"
+											caseContent = [
+												{
+													message = { text = ["Nested case"] }
+												}
+											]
+										}
+									]
+								}
+							}
+						]
+					}),
+					jsonencode({
+						condition = "",
+						caseContent = [
+							{
+								message = { text = ["Final case"] }
+							}
+						]
+					}),
+				]
+			}
+		}
+
+		event_handlers {
+			event = "some-event"
+			trigger_fulfillment {
+				return_partial_responses = true
+				messages {
+					channel = "some-channel"
+					text {
+						text = ["Some text"]
+					}
+				}
+				messages {
+					payload = <<EOF
+						{"some-key": "some-value", "other-key": ["other-value"]}
+					EOF
+				}
+				messages {
+					conversation_success {
+						metadata = <<EOF
+							{"some-metadata-key": "some-value", "other-metadata-key": 1234}
+						EOF
+					}
+				}
+				messages {
+					output_audio_text {
+						text = "some output text"
+					}
+				}
+				messages {
+					output_audio_text {
+						ssml = <<EOF
+							<speak>Some example <say-as interpret-as="characters">SSML XML</say-as></speak>
+						EOF
+					}
+				}
+				messages {
+					live_agent_handoff {
+						metadata = <<EOF
+							{"some-metadata-key": "some-value", "other-metadata-key": 1234}
+						EOF
+					}
+				}
+				messages {
+					play_audio {
+						audio_uri = "http://example.com/some-audio-file.mp3"
+					}
+				}
+				messages {
+					telephony_transfer_call {
+						phone_number = "1-234-567-8901"
+					}
+				}
+
+				tag = "some-tag"
+
+				set_parameter_actions {
+					parameter = "some-param"
+					value     = "123.45"
+				}
+				set_parameter_actions {
+					parameter = "another-param"
+					value     = jsonencode("abc")
+				}
+				set_parameter_actions {
+					parameter = "other-param"
+					value     = jsonencode(["foo"])
+				}
+
+				conditional_cases {
+					cases = [
+						jsonencode({
+							condition = "$sys.func.RAND() < 0.5",
+							caseContent = [
+								{
+									message = { text = ["First case"] }
+								},
+								{
+									additionalCases = {
+										cases = [
+											{
+												condition = "$sys.func.RAND() < 0.2"
+												caseContent = [
+													{
+														message = { text = ["Nested case"] }
+													}
+												]
+											}
+										]
+									}
+								}
+							]
+						}),
+						jsonencode({
+							condition = "",
+							caseContent = [
+								{
+									message = { text = ["Final case"] }
+								}
+							]
+						}),
+					]
 				}
 			}
 		}
@@ -119,13 +316,220 @@ func testAccDialogflowCXPage_full(context map[string]interface{}) string {
 			parameters {
 				display_name = "param1"
 				entity_type  = "projects/-/locations/-/agents/-/entityTypes/sys.date"
+				default_value = "2000-01-01"
 				fill_behavior {
 					initial_prompt_fulfillment {
 						messages {
+							channel = "some-channel"
+							response_type = "PARAMETER_PROMPT"
 							text {
 								text = ["Please provide param1"]
 							}
 						}
+						messages {
+							payload = <<EOF
+								{"some-key": "some-value", "other-key": ["other-value"]}
+							EOF
+						}
+						messages {
+							conversation_success {
+								metadata = <<EOF
+									{"some-metadata-key": "some-value", "other-metadata-key": 1234}
+								EOF
+							}
+						}
+						messages {
+							output_audio_text {
+								text = "some output text"
+							}
+						}
+						messages {
+							output_audio_text {
+								ssml = <<EOF
+									<speak>Some example <say-as interpret-as="characters">SSML XML</say-as></speak>
+								EOF
+							}
+						}
+						messages {
+							live_agent_handoff {
+								metadata = <<EOF
+									{"some-metadata-key": "some-value", "other-metadata-key": 1234}
+								EOF
+							}
+						}
+						messages {
+							play_audio {
+								audio_uri = "http://example.com/some-audio-file.mp3"
+							}
+						}
+						messages {
+							telephony_transfer_call {
+								phone_number = "1-234-567-8901"
+							}
+						}
+
+						tag = "some-tag"
+
+						set_parameter_actions {
+							parameter = "some-param"
+							value     = "123.45"
+						}
+						set_parameter_actions {
+							parameter = "another-param"
+							value     = jsonencode("abc")
+						}
+						set_parameter_actions {
+							parameter = "other-param"
+							value     = jsonencode(["foo"])
+						}
+
+						conditional_cases {
+							cases = [
+								jsonencode({
+									condition = "$sys.func.RAND() < 0.5",
+									caseContent = [
+										{
+											message = { text = ["First case"] }
+										},
+										{
+											additionalCases = {
+												cases = [
+													{
+														condition = "$sys.func.RAND() < 0.2"
+														caseContent = [
+															{
+																message = { text = ["Nested case"] }
+															}
+														]
+													}
+												]
+											}
+										}
+									]
+								}),
+								jsonencode({
+									condition = "",
+									caseContent = [
+										{
+											message = { text = ["Final case"] }
+										}
+									]
+								}),
+							]
+						}
+					}
+					reprompt_event_handlers {
+						event = "sys.no-match-1"
+						trigger_fulfillment {
+							messages {
+								messages {
+									channel = "some-channel"
+									response_type = "PARAMETER_PROMPT"
+									text {
+										text = ["Please provide param1"]
+									}
+								}
+								messages {
+									payload = <<EOF
+										{"some-key": "some-value", "other-key": ["other-value"]}
+									EOF
+								}
+								messages {
+									conversation_success {
+										metadata = <<EOF
+											{"some-metadata-key": "some-value", "other-metadata-key": 1234}
+										EOF
+									}
+								}
+								messages {
+									output_audio_text {
+										text = "some output text"
+									}
+								}
+								messages {
+									output_audio_text {
+										ssml = <<EOF
+											<speak>Some example <say-as interpret-as="characters">SSML XML</say-as></speak>
+										EOF
+									}
+								}
+								messages {
+									live_agent_handoff {
+										metadata = <<EOF
+											{"some-metadata-key": "some-value", "other-metadata-key": 1234}
+										EOF
+									}
+								}
+								messages {
+									play_audio {
+										audio_uri = "http://example.com/some-audio-file.mp3"
+									}
+								}
+								messages {
+									telephony_transfer_call {
+										phone_number = "1-234-567-8901"
+									}
+								}
+
+								tag = "some-tag"
+
+								set_parameter_actions {
+									parameter = "some-param"
+									value     = "123.45"
+								}
+								set_parameter_actions {
+									parameter = "another-param"
+									value     = jsonencode("abc")
+								}
+								set_parameter_actions {
+									parameter = "other-param"
+									value     = jsonencode(["foo"])
+								}
+
+								conditional_cases {
+									cases = [
+										jsonencode({
+											condition = "$sys.func.RAND() < 0.5",
+											caseContent = [
+												{
+													message = { text = ["First case"] }
+												},
+												{
+													additionalCases = {
+														cases = [
+															{
+																condition = "$sys.func.RAND() < 0.2"
+																caseContent = [
+																	{
+																		message = { text = ["Nested case"] }
+																	}
+																]
+															}
+														]
+													}
+												}
+											]
+										}),
+										jsonencode({
+											condition = "",
+											caseContent = [
+												{
+													message = { text = ["Final case"] }
+												}
+											]
+										}),
+									]
+								}
+							}
+						}
+					}
+					reprompt_event_handlers {
+						event = "sys.no-match-2"
+						target_flow = google_dialogflow_cx_agent.agent_page.start_flow
+					}
+					reprompt_event_handlers {
+						event = "sys.no-match-3"
+						target_page = google_dialogflow_cx_page.my_page2.id
 					}
 				}
 				required = "true"
@@ -137,11 +541,29 @@ func testAccDialogflowCXPage_full(context map[string]interface{}) string {
 			condition = "$page.params.status = 'FINAL'"
 			trigger_fulfillment {
 				messages {
+					channel = "some-channel"
+					response_type = "HANDLER_PROMPT"
 					text {
 						text = ["information completed, navigating to page 2"]
 					}
 				}
-	
+				messages {
+					payload = <<EOF
+						{"some-key": "some-value", "other-key": ["other-value"]}
+					EOF
+				}
+				messages {
+					conversation_success {
+						metadata = <<EOF
+							{"some-metadata-key": "some-value", "other-metadata-key": 1234}
+						EOF
+					}
+				}
+				messages {
+					output_audio_text {
+						text = "some output text"
+					}
+				}
 				messages {
 					output_audio_text {
 						ssml = <<EOF
@@ -149,14 +571,39 @@ func testAccDialogflowCXPage_full(context map[string]interface{}) string {
 						EOF
 					}
 				}
-	
+				messages {
+					live_agent_handoff {
+						metadata = <<EOF
+							{"some-metadata-key": "some-value", "other-metadata-key": 1234}
+						EOF
+					}
+				}
+				messages {
+					play_audio {
+						audio_uri = "http://example.com/some-audio-file.mp3"
+					}
+				}
+				messages {
+					telephony_transfer_call {
+						phone_number = "1-234-567-8901"
+					}
+				}
+
 				tag = "some-tag"
-	
+
 				set_parameter_actions {
 					parameter = "some-param"
 					value     = "123.45"
 				}
-	
+				set_parameter_actions {
+					parameter = "another-param"
+					value     = jsonencode("abc")
+				}
+				set_parameter_actions {
+					parameter = "other-param"
+					value     = jsonencode(["foo"])
+				}
+
 				conditional_cases {
 					cases = [
 						jsonencode({
@@ -195,7 +642,7 @@ func testAccDialogflowCXPage_full(context map[string]interface{}) string {
 			target_page = google_dialogflow_cx_page.my_page2.id
 		}
 	}
-	
+
 	resource "google_dialogflow_cx_page" "my_page2" {
 		parent       = google_dialogflow_cx_agent.agent_page.start_flow
 		display_name = "MyPage2"
