ab57c1b5147a5985bbc24a543e594f86f2f4b69a
Maciej Murakowski <mmurakowski@google.com>
2023-08-24 14:49:48 -0400

    Adding "basic" Flow creation, filling out "full" Flow creation with (I hope) all params

--- mmv1/third_party/terraform/services/dialogflowcx/resource_dialogflowcx_flow_test.go
+++ mmv1/third_party/terraform/services/dialogflowcx/resource_dialogflowcx_flow_test.go
@@ -260,6 +260,110 @@ func testAccDialogflowCXFlow_full(context map[string]interface{}) string {
 				}
 			}
 		}
+
+		transition_routes {
+			trigger_fulfillment {
+				return_partial_responses = true
+				messages {
+					channel = "some-channel"
+					text {
+						text = ["Some text"]
+					}
+				}
+				messages {
+					payload = <<EOF
+						{"some-key": "some-value", "other-key": ["other-value"]}
+					EOF
+				}
+				messages {
+					conversation_success {
+						metadata = <<EOF
+							{"some-metadata-key": "some-value", "other-metadata-key": 1234}
+						EOF
+					}
+				}
+				messages {
+					output_audio_text {
+						text = "some output text"
+					}
+				}
+				messages {
+					output_audio_text {
+						ssml = <<EOF
+							<speak>Some example <say-as interpret-as="characters">SSML XML</say-as></speak>
+						EOF
+					}
+				}
+				messages {
+					live_agent_handoff {
+						metadata = <<EOF
+							{"some-metadata-key": "some-value", "other-metadata-key": 1234}
+						EOF
+					}
+				}
+				messages {
+					play_audio {
+						audio_uri = "http://example.com/some-audio-file.mp3"
+					}
+				}
+				messages {
+					telephony_transfer_call {
+						phone_number = "1-234-567-8901"
+					}
+				}
+
+				tag = "some-tag"
+
+				set_parameter_actions {
+					parameter = "some-param"
+					value     = "123.45"
+				}
+				set_parameter_actions {
+					parameter = "another-param"
+					value     = jsonencode("abc")
+				}
+				set_parameter_actions {
+					parameter = "other-param"
+					value     = jsonencode(["foo"])
+				}
+
+				conditional_cases {
+					cases = [
+						jsonencode({
+							condition = "$sys.func.RAND() < 0.5",
+							caseContent = [
+								{
+									message = { text = ["First case"] }
+								},
+								{
+									additionalCases = {
+										cases = [
+											{
+												condition = "$sys.func.RAND() < 0.2"
+												caseContent = [
+													{
+														message = { text = ["Nested case"] }
+													}
+												]
+											}
+										]
+									}
+								}
+							]
+						}),
+						jsonencode({
+							condition = "",
+							caseContent = [
+								{
+									message = { text = ["Final case"] }
+								}
+							]
+						}),
+					]
+				}
+			}
+			target_flow = google_dialogflow_cx_agent.agent_entity.start_flow
+		}
 	}
 `, context)
 }
