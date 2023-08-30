c525b91285baaae785c71bec7c750b247b4754a0
Nevin Zheng <nevzheng@gmail.com>
2023-08-30 16:09:12 +0000

    fix: remove location from resource test.

--- mmv1/third_party/terraform/services/biglake/resource_biglake_table_test.go
+++ mmv1/third_party/terraform/services/biglake/resource_biglake_table_test.go
@@ -27,7 +27,7 @@ func TestAccBiglakeTable_bigqueryBiglakeTable_update(t *testing.T) {
 				ResourceName:            "google_biglake_table.table",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"location", "name", "database", "catalog"},
+				ImportStateVerifyIgnore: []string{"name", "database", "catalog"},
 			},
 			{
 				Config: testAccBiglakeTable_bigqueryBiglakeTable_update(context),
@@ -36,7 +36,7 @@ func TestAccBiglakeTable_bigqueryBiglakeTable_update(t *testing.T) {
 				ResourceName:            "google_biglake_table.table",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"location", "name", "database", "catalog"},
+				ImportStateVerifyIgnore: []string{"name", "database", "catalog"},
 			},
 		},
 	})
@@ -67,7 +67,6 @@ resource "google_storage_bucket_object" "data_folder" {
 resource "google_biglake_database" "database" {
 	name = "<%= ctx[:vars]['database'] %>"
 	catalog = google_biglake_catalog.catalog.id
-	location = google_biglake_catalog.catalog.location
 	type = "HIVE"
 	hive_options {
 		location_uri = "gs://${google_storage_bucket.bucket.name}/${google_storage_bucket_object.metadata_folder.name}"
@@ -79,7 +78,6 @@ resource "google_biglake_database" "database" {
 resource "google_biglake_table" "table" {
     name = "tf_test_my_table%{random_suffix}"
     database = google_biglake_database.database.id
-    location = google_biglake_catalog.catalog.location
     type = "HIVE"
     hive_options {
 		table_type = "MANAGED_TABLE"
