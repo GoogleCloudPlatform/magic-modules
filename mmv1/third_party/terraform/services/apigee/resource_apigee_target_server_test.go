9f1850dca975dccd5802ae10a30129bbf83aaf23
georgepeng1101 <georgepeng1101@gmail.com>
2023-08-30 22:09:31 -0400

    fix : add missing resource fields in test

--- mmv1/third_party/terraform/services/apigee/resource_apigee_target_server_test.go
+++ mmv1/third_party/terraform/services/apigee/resource_apigee_target_server_test.go
@@ -243,6 +243,10 @@ resource "google_apigee_target_server" "apigee_target_server"{
 	 key_store                = google_apigee_env_keystore.apigee_environment_keystore.name
 	 protocols                = ["TLSv1.1"]
 	 trust_store              = google_apigee_env_keystore.apigee_environment_keystore.name
+   common_name{
+    value                   = "testCn"
+    wildcard_match          = true
+   }
 	}
 	depends_on = [ 
     google_apigee_env_keystore.apigee_environment_keystore,
