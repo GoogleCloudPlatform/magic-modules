bfd885a75cdb78ea32d96c361703d8dc32cc4a76
Sarah French <15078782+SarahFrench@users.noreply.github.com>
2023-08-31 15:51:20 +0100

    Fixes to provider config code unit tests (#8819)

--- mmv1/third_party/terraform/provider/provider_internal_test.go
+++ mmv1/third_party/terraform/provider/provider_internal_test.go
@@ -1413,7 +1413,7 @@ func TestProvider_ProviderConfigure_requestTimeout(t *testing.T) {
 
 			// Arrange
 			ctx := context.Background()
-			unsetTestProviderConfigEnvs(t)
+			acctest.UnsetTestProviderConfigEnvs(t)
 			p := provider.Provider()
 			d := tpgresource.SetupTestResourceDataFromConfigMap(t, p.Schema, tc.ConfigValues)
 
@@ -1502,8 +1502,8 @@ func TestProvider_ProviderConfigure_requestReason(t *testing.T) {
 
 			// Arrange
 			ctx := context.Background()
-			unsetTestProviderConfigEnvs(t)
-			setupTestEnvs(t, tc.EnvVariables)
+			acctest.UnsetTestProviderConfigEnvs(t)
+			acctest.SetupTestEnvs(t, tc.EnvVariables)
 			p := provider.Provider()
 			d := tpgresource.SetupTestResourceDataFromConfigMap(t, p.Schema, tc.ConfigValues)
 
@@ -1635,7 +1635,7 @@ func TestProvider_ProviderConfigure_batching(t *testing.T) {
 
 			// Arrange
 			ctx := context.Background()
-			unsetTestProviderConfigEnvs(t)
+			acctest.UnsetTestProviderConfigEnvs(t)
 			p := provider.Provider()
 			d := tpgresource.SetupTestResourceDataFromConfigMap(t, p.Schema, tc.ConfigValues)
 
