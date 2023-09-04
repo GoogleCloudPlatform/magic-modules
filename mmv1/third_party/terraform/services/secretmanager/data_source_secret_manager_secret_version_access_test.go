266f95b007dbe8e1b91aae7d0dd92d94bdac4d92
alfatahbheda_crest <alfatah.bheda@crestdatasys.com>
2023-09-04 15:28:59 +0530

    Changed the name of field from automatic_replication to auto

--- mmv1/third_party/terraform/services/secretmanager/data_source_secret_manager_secret_version_access_test.go
+++ mmv1/third_party/terraform/services/secretmanager/data_source_secret_manager_secret_version_access_test.go
@@ -82,7 +82,7 @@ resource "google_secret_manager_secret" "secret-basic" {
     label = "my-label"
   }
   replication {
-    automatic_replication {}
+    auto {}
   }
 }
 
@@ -112,7 +112,7 @@ resource "google_secret_manager_secret" "secret-basic" {
     label = "my-label"
   }
   replication {
-    automatic_replication {}
+    auto {}
   }
 }
 
