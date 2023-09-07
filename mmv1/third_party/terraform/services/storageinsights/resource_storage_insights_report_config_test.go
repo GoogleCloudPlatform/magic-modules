c91710b3f426ba854af9544e724361971ad3f5bf
Thomas MacLean <thomasmaclean@google.com>
2023-09-06 22:24:30 -0400

    Fixed ReportConfig update handling; added update test

--- mmv1/third_party/terraform/services/storageinsights/resource_storage_insights_report_config_test.go
+++ mmv1/third_party/terraform/services/storageinsights/resource_storage_insights_report_config_test.go
@@ -1,16 +1,13 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
 package storageinsights_test
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
-	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
-	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
 )
 
 func TestAccStorageInsightsReportConfig_update(t *testing.T) {
@@ -23,7 +20,6 @@ func TestAccStorageInsightsReportConfig_update(t *testing.T) {
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckStorageInsightsReportConfigDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccStorageInsightsReportConfig_full(context),
@@ -34,7 +30,7 @@ func TestAccStorageInsightsReportConfig_update(t *testing.T) {
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{"location"},
 			},
-		        {
+			{
 				Config: testAccStorageInsightsReportConfig_update(context),
 			},
 			{
@@ -43,7 +39,6 @@ func TestAccStorageInsightsReportConfig_update(t *testing.T) {
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{"location"},
 			},
-
 		},
 	})
 }
@@ -84,6 +79,9 @@ resource "google_storage_insights_report_config" "config" {
       destination_path = "test-insights-reports"
     }
   }
+  depends_on = [
+	google_storage_bucket_iam_member.admin,
+  ]
 }
 
 resource "google_storage_bucket" "report_bucket" {
@@ -110,7 +108,7 @@ resource "google_storage_insights_report_config" "config" {
   display_name = "Test Report Config Updated"
   location = "us-central1"
   frequency_options {
-    frequency = "MONTHLY"
+    frequency = "DAILY"
     start_date {
       day = 14
       month = 3
@@ -123,29 +121,32 @@ resource "google_storage_insights_report_config" "config" {
     }
   }
   csv_options {
-    record_separator = ";"
+    record_separator = "\r\n"
     delimiter = "."
     header_required = true
   }
   object_metadata_report_options {
-    metadata_fields = ["name", "project"]
+    metadata_fields = ["bucket", "name", "project"]
     storage_filters {
-	  bucket = google_storage_bucket.update_bucket.name
-	}
+      bucket = google_storage_bucket.report_bucket.name
+    }
     storage_destination_options {
-      bucket = google_storage_bucket.update_bucket.name
+      bucket = google_storage_bucket.report_bucket.name
       destination_path = "test-insights-reports-updated"
     }
   }
+  depends_on = [
+	google_storage_bucket_iam_member.admin,
+  ]
 }
 
-resource "google_storage_bucket" "update_bucket" {
-	name                        = "tf-test-update-bucket%{random_suffix}"
-	location                    = "us-central1"
-	force_destroy               = true
-	uniform_bucket_level_access = true
-  }
-
+resource "google_storage_bucket" "report_bucket" {
+  name                        = "tf-test-my-bucket%{random_suffix}"
+  location                    = "us-central1"
+  force_destroy               = true
+  uniform_bucket_level_access = true
+}
+  
 resource "google_storage_bucket_iam_member" "admin" {
   bucket = google_storage_bucket.report_bucket.name
   role   = "roles/storage.admin"
