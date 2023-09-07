4e3c470c56c7afae292bb5d4cffdfedadf880503
Nancy Hong <nancyhong@google.com>
2023-09-07 12:15:44 -0700

    support to create instance with ssl mode and require connectors, with acceptance tests

--- mmv1/third_party/terraform/services/alloydb/resource_alloydb_instance_test.go
+++ mmv1/third_party/terraform/services/alloydb/resource_alloydb_instance_test.go
@@ -406,3 +406,336 @@ resource "google_service_networking_connection" "vpc_connection" {
 }
 `, context)
 }
+
+// This test passes if an instance without specifying the SSL mode correctly
+// sets the default SSL mode, explicitly setting the default after does not
+// change it, and removing the explicitly set ssl mode doesn't change it either.
+func TestAccAlloydbInstance_clientConnectionConfig_sslModeDefault(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+		"network_name":  acctest.BootstrapSharedTestNetwork(t, "alloydbinstance-sslmodedefault"),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckAlloydbInstanceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAlloydbInstance_noClientConnectionConfig(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("google_alloydb_instance.default", "client_connection_config.0.ssl_config.0.ssl_mode", "ENCRYPTED_ONLY"),
+				),
+			},
+			{
+				ResourceName:      "google_alloydb_instance.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{"cluster", "instance_id", "reconciling", "update_time"},
+			},
+			// {
+			// 	Config: testAccAlloydbInstance_defaultClientConnectionConfig(context),
+			// 	Check: resource.ComposeTestCheckFunc(
+			// 		resource.TestCheckResourceAttr("google_alloydb_instance.default", "client_connection_config.0.ssl_config.0.ssl_mode", "ENCRYPTED_ONLY"),
+			// 	),
+			// },
+			// {
+			// 	ResourceName:      "google_alloydb_instance.default",
+			// 	ImportState:       true,
+			// 	ImportStateVerify: true,
+			// 	ImportStateVerifyIgnore: []string{"cluster", "instance_id", "reconciling", "update_time"},
+			// },
+			// {
+			// 	Config: testAccAlloydbInstance_noClientConnectionConfig(context),
+			// 	Check: resource.ComposeTestCheckFunc(
+			// 		resource.TestCheckResourceAttr("google_alloydb_instance.default", "client_connection_config.0.ssl_config.0.ssl_mode", "ENCRYPTED_ONLY"),
+			// 	),
+			// },
+			// {
+			// 	ResourceName:      "google_alloydb_instance.default",
+			// 	ImportState:       true,
+			// 	ImportStateVerify: true,
+			// 	ImportStateVerifyIgnore: []string{"cluster", "instance_id", "reconciling", "update_time"},
+			// },
+			{
+				Config: testAccAlloydbInstance_alloydbInstanceBasicExample(context),
+			},
+		},
+	})
+}
+
+// func TestAccAlloydbInstance_clientConnectionConfig_noChangeIfDefaultIsSet(t *testing.T) {
+// 	t.Parallel()
+//
+// 	context := map[string]interface{}{
+// 		"random_suffix": acctest.RandString(t, 10),
+// 		"network_name":  acctest.BootstrapSharedTestNetwork(t, "alloydbinstance-sslmodefaultset"),
+// 	}
+//
+// 	acctest.VcrTest(t, resource.TestCase{
+// 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+// 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+// 		CheckDestroy:             testAccCheckAlloydbInstanceDestroyProducer(t),
+// 		Steps: []resource.TestStep{
+// 			{
+// 				Config: testAccAlloydbInstance_noClientConnectionConfig(context),
+// 				Check: resource.ComposeTestCheckFunc(
+// 					resource.TestCheckResourceAttr("google_alloydb_instance.default", "client_connection_config.0.ssl_config.0.ssl_mode", "ENCRYPTED_ONLY"),
+// 				),
+// 			},
+// 			{
+// 				ResourceName:      "google_alloydb_instance.default",
+// 				ImportState:       true,
+// 				ImportStateVerify: true,
+// 			},
+// 			{
+// 				Config: testAccAlloydbInstance_defaultClientConnectionConfig(context),
+// 				Check: resource.ComposeTestCheckFunc(
+// 					resource.TestCheckResourceAttr("google_alloydb_instance.default", "client_connection_config.0.ssl_config.0.ssl_mode", "ENCRYPTED_ONLY"),
+// 				),
+// 			},
+// 			{
+// 				ResourceName:      "google_alloydb_instance.default",
+// 				ImportState:       true,
+// 				ImportStateVerify: true,
+// 			},
+// 			{
+// 				Config: testAccAlloydbInstance_alloydbInstanceBasicExample(context),
+// 			},
+// 		},
+// 	})
+// }
+//
+func TestAccAlloydbInstance_clientConnectionConfig_noChangeIfRemoved(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+		"network_name":  acctest.BootstrapSharedTestNetwork(t, "alloydbinstance-sslmodenochangeifremoved"),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckAlloydbInstanceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAlloydbInstance_defaultClientConnectionConfig(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("google_alloydb_instance.default", "client_connection_config.0.ssl_config.0.ssl_mode", "ENCRYPTED_ONLY"),
+				),
+			},
+			{
+				ResourceName:      "google_alloydb_instance.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccAlloydbInstance_alloydbInstanceBasicExample(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("google_alloydb_instance.default", "client_connection_config.0.ssl_config.0.ssl_mode", "ENCRYPTED_ONLY"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAlloydbInstance_clientConnectionConfig_update(t *testing.T) {
+	t.Parallel()
+
+	suffix := acctest.RandString(t, 10)
+	networkName := acctest.BootstrapSharedTestNetwork(t, "alloydbinstance-clientconnectionconfigupdate")
+	context := map[string]interface{}{
+		"random_suffix": suffix,
+		"network_name":  networkName,
+	}
+	context2 := map[string]interface{}{
+		"random_suffix": suffix,
+		"network_name":  networkName,
+		"require_connectors": true,
+		"ssl_mode": "ENCRYPTED_ONLY",
+	}
+	context3 := map[string]interface{}{
+		"random_suffix": suffix,
+		"network_name":  networkName,
+		"require_connectors": false,
+		"ssl_mode": "ALLOW_UNENCRYPTED_AND_ENCRYPTED",
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckAlloydbInstanceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAlloydbInstance_noClientConnectionConfig(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("google_alloydb_instance.default", "client_connection_config.0.ssl_config.0.ssl_mode", "ENCRYPTED_ONLY"),
+				),
+			},
+			{
+				ResourceName:      "google_alloydb_instance.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccAlloydbInstance_clientConnectionConfig(context2),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("google_alloydb_instance.default", "client_connection_config.0.require_connectors", "true"),
+					resource.TestCheckResourceAttr("google_alloydb_instance.default", "client_connection_config.0.ssl_config.0.ssl_mode", "ENCRYPTED_ONLY"),
+				),
+			},
+			{
+				ResourceName:      "google_alloydb_instance.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccAlloydbInstance_clientConnectionConfig(context3),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("google_alloydb_instance.default", "client_connection_config.0.require_connectors", "false"),
+					resource.TestCheckResourceAttr("google_alloydb_instance.default", "client_connection_config.0.ssl_config.0.ssl_mode", "ALLOW_UNENCRYPTED_AND_ENCRYPTED"),
+				),
+			},
+			{
+				ResourceName:      "google_alloydb_instance.default",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccAlloydbInstance_alloydbInstanceBasicExample(context),
+				Check: resource.ComposeTestCheckFunc(
+				),
+			},
+		},
+	})
+}
+
+func testAccAlloydbInstance_noClientConnectionConfig(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_alloydb_instance" "default" {
+  cluster       = google_alloydb_cluster.default.name
+  instance_id   = "tf-test-alloydb-instance%{random_suffix}"
+  instance_type = "PRIMARY"
+
+  depends_on = [google_service_networking_connection.vpc_connection]
+}
+
+resource "google_alloydb_cluster" "default" {
+  cluster_id = "tf-test-alloydb-cluster%{random_suffix}"
+  location   = "us-central1"
+  network    = data.google_compute_network.default.id
+}
+
+data "google_project" "project" {}
+
+data "google_compute_network" "default" {
+	name = "%{network_name}"
+}
+
+resource "google_compute_global_address" "private_ip_alloc" {
+  name          =  "tf-test-alloydb-cluster%{random_suffix}"
+  address_type  = "INTERNAL"
+  purpose       = "VPC_PEERING"
+  prefix_length = 16
+  network       = data.google_compute_network.default.id
+}
+
+resource "google_service_networking_connection" "vpc_connection" {
+  network                 = data.google_compute_network.default.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
+}
+`, context)
+}
+
+func testAccAlloydbInstance_defaultClientConnectionConfig(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_alloydb_instance" "default" {
+  cluster       = google_alloydb_cluster.default.name
+  instance_id   = "tf-test-alloydb-instance%{random_suffix}"
+  instance_type = "PRIMARY"
+
+  depends_on = [google_service_networking_connection.vpc_connection]
+
+	client_connection_config {
+		ssl_config {
+			ssl_mode = "ENCRYPTED_ONLY"
+		}
+	}
+}
+
+resource "google_alloydb_cluster" "default" {
+  cluster_id = "tf-test-alloydb-cluster%{random_suffix}"
+  location   = "us-central1"
+  network    = data.google_compute_network.default.id
+}
+
+data "google_project" "project" {}
+
+data "google_compute_network" "default" {
+	name = "%{network_name}"
+}
+
+resource "google_compute_global_address" "private_ip_alloc" {
+  name          =  "tf-test-alloydb-cluster%{random_suffix}"
+  address_type  = "INTERNAL"
+  purpose       = "VPC_PEERING"
+  prefix_length = 16
+  network       = data.google_compute_network.default.id
+}
+
+resource "google_service_networking_connection" "vpc_connection" {
+  network                 = data.google_compute_network.default.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
+}
+`, context)
+}
+
+func testAccAlloydbInstance_clientConnectionConfig(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_alloydb_instance" "default" {
+  cluster       = google_alloydb_cluster.default.name
+  instance_id   = "tf-test-alloydb-instance%{random_suffix}"
+  instance_type = "PRIMARY"
+
+  depends_on = [google_service_networking_connection.vpc_connection]
+
+	client_connection_config {
+		require_connectors = %{require_connectors}
+		ssl_config {
+			ssl_mode = %{ssl_mode}
+		}
+	}
+}
+
+resource "google_alloydb_cluster" "default" {
+  cluster_id = "tf-test-alloydb-cluster%{random_suffix}"
+  location   = "us-central1"
+  network    = data.google_compute_network.default.id
+}
+
+data "google_project" "project" {}
+
+data "google_compute_network" "default" {
+	name = "%{network_name}"
+}
+
+resource "google_compute_global_address" "private_ip_alloc" {
+  name          =  "tf-test-alloydb-cluster%{random_suffix}"
+  address_type  = "INTERNAL"
+  purpose       = "VPC_PEERING"
+  prefix_length = 16
+  network       = data.google_compute_network.default.id
+}
+
+resource "google_service_networking_connection" "vpc_connection" {
+  network                 = data.google_compute_network.default.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
+}
+`, context)
+}
