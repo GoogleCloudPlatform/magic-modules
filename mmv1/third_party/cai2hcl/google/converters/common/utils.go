555108414872342f0131a317e9fcf8b2144190c4
Amir Romashkin <romashkin@google.com>
2023-09-08 16:50:02 +0000

    Remove excessive result map normalization code.
    It is not yet needed at this stage for manual converters.

--- mmv1/third_party/cai2hcl/google/converters/common/utils.go
+++ mmv1/third_party/cai2hcl/google/converters/common/utils.go
@@ -7,7 +7,6 @@ import (
 
 	hashicorpcty "github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	transport_tpg "github.com/hashicorp/terraform-provider-google-beta/google-beta/transport"
 	"github.com/zclconf/go-cty/cty"
 	ctyjson "github.com/zclconf/go-cty/cty/json"
 )
@@ -37,8 +36,6 @@ func DecodeJSON(data map[string]interface{}, v interface{}) error {
 
 // Converts resource from untyped map format to TF JSON.
 func MapToCtyValWithSchema(m map[string]interface{}, s map[string]*schema.Schema) (cty.Value, error) {
-	m = normalizeFlattenedMap(m).(map[string]interface{})
-
 	b, err := json.Marshal(&m)
 	if err != nil {
 		return cty.NilVal, fmt.Errorf("error marshaling map as JSON: %v", err)
@@ -64,40 +61,4 @@ func hashicorpCtyTypeToZclconfCtyType(t hashicorpcty.Type) (cty.Type, error) {
 		return cty.NilType, err
 	}
 	return ret, nil
-}
-
-// Normalizes flatteners output map by eliminating unmarshallable objects like schema.Map
-func normalizeFlattenedMap(obj interface{}) interface{} {
-	switch obj.(type) {
-	case []interface{}:
-		arr := obj.([]interface{})
-		newArr := make([]interface{}, len(arr))
-		for i := range arr {
-			newArr[i] = normalizeFlattenedMap(arr[i])
-		}
-
-		return newArr
-	case map[string]interface{}:
-		mp := obj.(map[string]interface{})
-		newMap := map[string]interface{}{}
-		for key, value := range mp {
-			newMap[key] = normalizeFlattenedMap(value)
-		}
-		return newMap
-	case *schema.Set:
-		return obj.(*schema.Set).List()
-	default:
-		return obj
-	}
-}
-
-// Initializes configuration object which is used by flatteners.
-func NewConfig() *transport_tpg.Config {
-	// Currently its not needed, but it may change in future.
-	return &transport_tpg.Config{
-		Project:   "",
-		Zone:      "",
-		Region:    "",
-		UserAgent: "",
-	}
 }
