ff5ec47a2b712a1e449eb05b3dca69721f9107c8
Scott Suarez <ScottSuarez@google.com>
2023-09-12 14:35:07 -0700

    migrate membership-checker to be extensible (#8708)

---
+++ .ci/magician/github/set.go
@@ -0,0 +1,96 @@
+package github
+
+import (
+	"fmt"
+	utils "magician/utility"
+	"net/http"
+)
+
+func (gh *github) PostBuildStatus(prNumber, title, state, target_url, commitSha string) error {
+	url := fmt.Sprintf("https://api.github.com/repos/GoogleCloudPlatform/magic-modules/statuses/%s", commitSha)
+
+	postBody := map[string]string{
+		"context":    title,
+		"state":      state,
+		"target_url": target_url,
+	}
+
+	_, err := utils.RequestCall(url, "POST", gh.token, nil, postBody)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Successfully posted build status to pull request %s\n", prNumber)
+
+	return nil
+}
+
+func (gh *github) PostComment(prNumber, comment string) error {
+	url := fmt.Sprintf("https://api.github.com/repos/GoogleCloudPlatform/magic-modules/issues/%s/comments", prNumber)
+
+	body := map[string]string{
+		"body": comment,
+	}
+
+	reqStatusCode, err := utils.RequestCall(url, "POST", gh.token, nil, body)
+	if err != nil {
+		return err
+	}
+
+	if reqStatusCode != http.StatusCreated {
+		return fmt.Errorf("error posting comment for PR %s", prNumber)
+	}
+
+	fmt.Printf("Successfully posted comment to pull request %s\n", prNumber)
+
+	return nil
+}
+
+func (gh *github) RequestPullRequestReviewer(prNumber, assignee string) error {
+	url := fmt.Sprintf("https://api.github.com/repos/GoogleCloudPlatform/magic-modules/pulls/%s/requested_reviewers", prNumber)
+
+	body := map[string][]string{
+		"reviewers":      {assignee},
+		"team_reviewers": {},
+	}
+
+	reqStatusCode, err := utils.RequestCall(url, "POST", gh.token, nil, body)
+	if err != nil {
+		return err
+	}
+
+	if reqStatusCode != http.StatusCreated {
+		return fmt.Errorf("error adding reviewer for PR %s", prNumber)
+	}
+
+	fmt.Printf("Successfully added reviewer %s to pull request %s\n", assignee, prNumber)
+
+	return nil
+}
+
+func (gh *github) AddLabel(prNumber, label string) error {
+	url := fmt.Sprintf("https://api.github.com/repos/GoogleCloudPlatform/magic-modules/issues/%s/labels", prNumber)
+
+	body := map[string][]string{
+		"labels": {label},
+	}
+	_, err := utils.RequestCall(url, "POST", gh.token, nil, body)
+
+	if err != nil {
+		return fmt.Errorf("failed to add %s label: %s", label, err)
+	}
+
+	return nil
+
+}
+
+func (gh *github) RemoveLabel(prNumber, label string) error {
+	url := fmt.Sprintf("https://api.github.com/repos/GoogleCloudPlatform/magic-modules/issues/%s/labels/%s", prNumber, label)
+	_, err := utils.RequestCall(url, "DELETE", gh.token, nil, nil)
+
+	if err != nil {
+		return fmt.Errorf("failed to remove %s label: %s", label, err)
+	}
+
+	return nil
+}
