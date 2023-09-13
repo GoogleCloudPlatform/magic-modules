8c31fd8864f93f7543f95475340b56babcb0e88d
BBBmau <alvarez.mauriciotm@gmail.com>
2023-09-13 09:27:29 -0700

    remove ForceNew

--- mmv1/third_party/terraform/services/storagetransfer/resource_storage_transfer_job.go
+++ mmv1/third_party/terraform/services/storagetransfer/resource_storage_transfer_job.go
@@ -89,20 +89,17 @@ func ResourceStorageTransferJob() *schema.Resource {
 						"name": {
 							Type:        schema.TypeString,
 							Required:    true,
-							ForceNew:    true,
 							Description: "Specifies a unique name of the resource such as AWS SQS ARN in the form 'arn:aws:sqs:region:account_id:queue_name', or Pub/Sub subscription resource name in the form 'projects/{project}/subscriptions/{sub}'",
 						},
 						"event_stream_start_time": {
 							Type:         schema.TypeString,
 							Optional:     true,
-							ForceNew:     true,
 							Description:  "Specifies the date and time that Storage Transfer Service starts listening for events from this stream. If no start time is specified or start time is in the past, Storage Transfer Service starts listening immediately",
 							ValidateFunc: validation.IsRFC3339Time,
 						},
 						"event_stream_expiration_time": {
 							Type:         schema.TypeString,
 							Optional:     true,
-							ForceNew:     true,
 							Description:  "Specifies the data and time at which Storage Transfer Service stops listening for events from this stream. After this time, any transfers in progress will complete, but no new transfers are initiated",
 							ValidateFunc: validation.IsRFC3339Time,
 						},
