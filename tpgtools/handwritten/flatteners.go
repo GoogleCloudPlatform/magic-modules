721c1817bc468a9e19d22931659bfe1937f64004
Nick Elliot <nickelliot@google.com>
2023-09-14 15:09:12 -0700

    moved to tpgresource for version generation

--- tpgtools/handwritten/flatteners.go
+++ tpgtools/handwritten/flatteners.go
@@ -1,13 +1,5 @@
 package tpgdclresource
 
-import (
-	containeraws "github.com/GoogleCloudPlatform/declarative-resource-client-library/services/google/containeraws"
-	containerazure "github.com/GoogleCloudPlatform/declarative-resource-client-library/services/google/containerazure"
-
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
-)
-
 // Returns the terraform representation of a three-state boolean value represented by a pointer to bool in DCL.
 func FlattenEnumBool(v interface{}) string {
 	b, ok := v.(*bool)
@@ -18,34 +10,4 @@ func FlattenEnumBool(v interface{}) string {
 		return "TRUE"
 	}
 	return "FALSE"
-}
-
-func FlattenContainerAwsNodePoolManagement(obj *containeraws.NodePoolManagement, d *schema.ResourceData, config *transport_tpg.Config) interface{} {
-	if obj == nil {
-		return nil
-	}
-	transformed := make(map[string]interface{})
-
-	if obj.AutoRepair == nil || obj.Empty() {
-		transformed["auto_repair"] = false
-	} else {
-		transformed["auto_repair"] = obj.AutoRepair
-	}
-
-	return []interface{}{transformed}
-}
-
-func FlattenContainerAzureNodePoolManagement(obj *containerazure.NodePoolManagement, d *schema.ResourceData, config *transport_tpg.Config) interface{} {
-	if obj == nil {
-		return nil
-	}
-	transformed := make(map[string]interface{})
-
-	if obj.AutoRepair == nil || obj.Empty() {
-		transformed["auto_repair"] = false
-	} else {
-		transformed["auto_repair"] = obj.AutoRepair
-	}
-
-	return []interface{}{transformed}
 }
