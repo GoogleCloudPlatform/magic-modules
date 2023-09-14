148ec76089e55bd0079823225e1fba57d758ba9f
root <root@scs000165904.rtp.openenglab.netapp.com>
2023-09-14 09:48:48 -0400

    Added storage pool

---
+++ mmv1/third_party/terraform/services/netapp/resource_netapp_storage_pools_test.go
@@ -0,0 +1,68 @@
+package netapp_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+)
+
+func TestAccNetappstoragePools_storagePoolCreateExample_update(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetappstoragePools_storagePoolCreateExample_full(context),
+			},
+			{
+				ResourceName:            "google_netapp_storage_pools.test_pool",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "storage_pool_id"},
+			},
+			{
+				Config: testAccNetappstoragePools_storagePoolCreateExample_update(context),
+			},
+			{
+				ResourceName:            "google_netapp_storage_pools.test_pool",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "storage_pool_id"},
+			},
+		},
+	})
+}
+
+func testAccNetappstoragePools_storagePoolCreateExample_full(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+
+resource "google_netapp_storage_pools" "test_pool" {
+  storage_pool_id = "tf-test-test-pool"
+  location = "us-central1"
+  service_level = "PREMIUM"
+  capacity_gib = "2048"
+  network = "projects/{{ProjectNumber}}/global/networks/cxo-automation-vpc-01"
+}
+`, context)
+}
+
+func testAccNetappstoragePools_storagePoolCreateExample_update(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+
+resource "google_netapp_storage_pools" "test_pool" {
+  storage_pool_id = "tf-test-test-pool"
+  location = "us-central1"
+  service_level = "PREMIUM"
+  capacity_gib = "4096"
+  network = "projects/{{ProjectNumber}}/global/networks/cxo-automation-vpc-01"
+}
+`, context)
+}
