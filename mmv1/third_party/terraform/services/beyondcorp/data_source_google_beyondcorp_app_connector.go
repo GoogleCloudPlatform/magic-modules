fe5076457e66f02b9816e753d771aecd1f91027e
Zhenhua Li <zhenhuali@google.com>
2023-09-22 12:13:48 -0700

    Make labels field have all labels in data sources (#9037)

--- mmv1/third_party/terraform/services/beyondcorp/data_source_google_beyondcorp_app_connector.go
+++ mmv1/third_party/terraform/services/beyondcorp/data_source_google_beyondcorp_app_connector.go
@@ -46,6 +46,10 @@ func dataSourceGoogleBeyondcorpAppConnectorRead(d *schema.ResourceData, meta int
 		return err
 	}
 
+	if err := tpgresource.SetDataSourceLabels(d); err != nil {
+		return err
+	}
+
 	if d.Id() == "" {
 		return fmt.Errorf("%s not found", id)
 	}
