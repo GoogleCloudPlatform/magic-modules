aa68092bc7e465015e59aaa4150975b8035d47d3
Shuya Ma <shuyama@google.com>
2023-09-21 17:30:25 -0700

    Add netapp service

--- .ci/infra/terraform/main.tf
+++ .ci/infra/terraform/main.tf
@@ -264,6 +264,7 @@ module "project-services" {
     "mobilecrashreporting.googleapis.com",
     "monitoring.googleapis.com",
     "multiclustermetering.googleapis.com",
+    "netapp.googleapis.com",
     "networkconnectivity.googleapis.com",
     "networkmanagement.googleapis.com",
     "networksecurity.googleapis.com",
