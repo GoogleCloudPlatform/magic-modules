a1247f2edce314a715630cabf502b5d79d91440d
Zhenhua Li <zhenhuali@google.com>
2023-09-21 17:24:01 -0700

    Make labels field have all labels in data sources

--- mmv1/third_party/terraform/services/kms/data_source_google_kms_crypto_key.go
+++ mmv1/third_party/terraform/services/kms/data_source_google_kms_crypto_key.go
@@ -41,6 +41,10 @@ func dataSourceGoogleKmsCryptoKeyRead(d *schema.ResourceData, meta interface{}) 
 		return err
 	}
 
+	if err := tpgresource.SetDataSourceLabels(d); err != nil {
+		return err
+	}
+
 	if d.Id() == "" {
 		return fmt.Errorf("%s not found", id)
 	}
