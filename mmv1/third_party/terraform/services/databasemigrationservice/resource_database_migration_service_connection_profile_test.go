5c566737c26947c4948702c8ccbe4ddfd28447ea
Nick Elliot <nickelliot@google.com>
2023-09-28 12:17:15 -0700

    test problems

--- mmv1/third_party/terraform/services/databasemigrationservice/resource_database_migration_service_connection_profile_test.go
+++ mmv1/third_party/terraform/services/databasemigrationservice/resource_database_migration_service_connection_profile_test.go
@@ -1,10 +1,15 @@
 package databasemigrationservice_test
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccDatabaseMigrationServiceConnectionProfile_update(t *testing.T) {
@@ -59,11 +64,11 @@ func TestAccDatabaseMigrationServiceConnectionProfile_Postgres_PSC(t *testing.T)
 		CheckDestroy:             testAccSqlDatabaseInstanceDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDatabaseMigrationServiceConnectionProfile_Postgres_PSC(instanceName, projectId, orgId, billingAccount, suffix),
+				Config: testAccDatabaseMigrationServiceConnectionProfile_Postgres_PSC(instanceName, projectId, orgId, billingAccount, certName, userName, passWord, profileName, profileDisplay),
 				Check:  resource.ComposeTestCheckFunc(verifyPscOperation("google_sql_database_instance.instance", true, true, []string{envvar.GetTestProjectFromEnv()})),
 			},
 			{
-				ResourceName:            "google_database_migration_service_connection_profile.instance",
+				ResourceName:            "google_database_migration_service_connection_profile.dbms_profile",
 				ImportState:             true,
 				ImportStateVerify:       true,
 				ImportStateIdPrefix:     fmt.Sprintf("%s/", projectId),
@@ -111,7 +116,7 @@ resource "google_database_migration_service_connection_profile" "default" {
 `, context)
 }
 
-func testAccDatabaseMigrationServiceConnectionProfile_Postgres_PSC(instanceName string, projectId string, orgId string, billingAccount string) string {
+func testAccDatabaseMigrationServiceConnectionProfile_Postgres_PSC(instanceName, projectId, orgId, billingAccount, certName, userName, passWord, profileName, profileDisplay string) string {
 	return fmt.Sprintf(`
 resource "google_project" "testproject" {
   name                = "%s"
@@ -181,6 +186,62 @@ resource "google_database_migration_service_connection_profile" "dbms_profile" {
       service_attachment = google_sql_database_instance.postgresqldb.psc_service_attachment_link
     }
   }
+  depends_on = [google_sql_user.sqldb_user]
 
 `, projectId, projectId, orgId, billingAccount, instanceName, projectId, certName, userName, passWord, profileName, profileDisplay, userName, passWord, instanceName)
+}
+
+func testAccSqlDatabaseInstanceDestroyProducer(t *testing.T) func(s *terraform.State) error {
+	return func(s *terraform.State) error {
+		for _, rs := range s.RootModule().Resources {
+			config := acctest.GoogleProviderConfig(t)
+			if rs.Type != "google_sql_database_instance" {
+				continue
+			}
+
+			_, err := config.NewSqlAdminClient(config.UserAgent).Instances.Get(config.Project,
+				rs.Primary.Attributes["name"]).Do()
+			if err == nil {
+				return fmt.Errorf("Database Instance still exists")
+			}
+		}
+
+		return nil
+	}
+}
+
+func verifyPscOperation(resourceName string, isPscConfigExpected bool, expectedPscEnabled bool, expectedAllowedConsumerProjects []string) func(*terraform.State) error {
+	return func(s *terraform.State) error {
+		resource, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("Can't find %s in state", resourceName)
+		}
+
+		resourceAttributes := resource.Primary.Attributes
+		_, ok = resourceAttributes["settings.0.ip_configuration.#"]
+		if !ok {
+			return fmt.Errorf("settings.0.ip_configuration.# block is not present in state for %s", resourceName)
+		}
+
+		if isPscConfigExpected {
+			_, ok := resourceAttributes["settings.0.ip_configuration.0.psc_config.#"]
+			if !ok {
+				return fmt.Errorf("settings.0.ip_configuration.0.psc_config property is not present or set in state of %s", resourceName)
+			}
+
+			pscEnabledStr, ok := resourceAttributes["settings.0.ip_configuration.0.psc_config.0.psc_enabled"]
+			pscEnabled, err := strconv.ParseBool(pscEnabledStr)
+			if err != nil || pscEnabled != expectedPscEnabled {
+				return fmt.Errorf("settings.0.ip_configuration.0.psc_config.0.psc_enabled property value is not set as expected in state of %s, expected %v, actual %v", resourceName, expectedPscEnabled, pscEnabled)
+			}
+
+			allowedConsumerProjectsStr, ok := resourceAttributes["settings.0.ip_configuration.0.psc_config.0.allowed_consumer_projects.#"]
+			allowedConsumerProjects, err := strconv.Atoi(allowedConsumerProjectsStr)
+			if !ok || allowedConsumerProjects != len(expectedAllowedConsumerProjects) {
+				return fmt.Errorf("settings.0.ip_configuration.0.psc_config.0.allowed_consumer_projects property is not present or set as expected in state of %s", resourceName)
+			}
+		}
+
+		return nil
+	}
 }
