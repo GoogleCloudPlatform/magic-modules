7dd152c8805c6ce381e3c99afd118fba3b024cb8
Shubham Sahu <shubhsahu@google.com>
2023-09-28 03:48:10 -0700

    Comment out tests failing due to service networking issue

--- mmv1/third_party/terraform/services/alloydb/resource_alloydb_instance_test.go
+++ mmv1/third_party/terraform/services/alloydb/resource_alloydb_instance_test.go
@@ -95,7 +95,7 @@ resource "google_service_networking_connection" "vpc_connection" {
 }
 
 // This test passes if we are able to create a primary instance with minimal number of fields
-func TestAccAlloydbInstance_createInstanceWithMandatoryFields(t *testing.T) {
+/* func TestAccAlloydbInstance_createInstanceWithMandatoryFields(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
@@ -152,7 +152,7 @@ resource "google_service_networking_connection" "vpc_connection" {
   reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
 }
 `, context)
-}
+}*/
 
 // This test passes if we are able to create a primary instance with maximum number of fields
 /* func TestAccAlloydbInstance_createInstanceWithMaximumFields(t *testing.T) {
@@ -240,7 +240,7 @@ resource "google_service_networking_connection" "vpc_connection" {
 }*/
 
 // This test passes if we are able to create a primary instance with an associated read-pool instance
-func TestAccAlloydbInstance_createPrimaryAndReadPoolInstance(t *testing.T) {
+/* func TestAccAlloydbInstance_createPrimaryAndReadPoolInstance(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
@@ -306,7 +306,7 @@ resource "google_service_networking_connection" "vpc_connection" {
   reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
 }
 `, context)
-}
+}*/
 
 // This test passes if we are able to update a database flag in primary instance
 /*func TestAccAlloydbInstance_updateDatabaseFlagInPrimaryInstance(t *testing.T) {
