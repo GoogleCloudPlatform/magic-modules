b96a76682f45e6aaf0c7be641c98e74562c834d9
alfatahbheda_crest <alfatah.bheda@crestdatasys.com>
2023-10-04 10:25:57 +0530

    Updated the description for filter field

--- mmv1/third_party/terraform/services/secretmanager/data_source_secret_manager_secrets.go
+++ mmv1/third_party/terraform/services/secretmanager/data_source_secret_manager_secrets.go
@@ -23,28 +23,8 @@ func DataSourceSecretManagerSecrets() *schema.Resource {
 			},
 			"filter": {
 				Type: schema.TypeString,
-				Description: `Filter sets the optional parameter "filter": A filter expression that
-filters resources listed in the response. The expression must specify
-the field name, an operator, and the value that you want to use for
-filtering. The value must be a string, a number, or a boolean. The
-operator must be either "=", "!=", ">", "<", "<=", ">=" or ":". For
-example, if you are filtering Compute Engine instances, you can
-exclude instances named "example-instance" by specifying "name !=
-example-instance". The ":" operator can be used with string fields to
-match substrings. For non-string fields it is equivalent to the "="
-operator. The ":*" comparison can be used to test whether a key has
-been defined. For example, to find all objects with "owner" label
-use: """ labels.owner:* """ You can also filter nested fields. For
-example, you could specify "scheduling.automaticRestart = false" to
-include instances only if they are not scheduled for automatic
-restarts. You can use filtering on nested fields to filter based on
-resource labels. To filter on multiple expressions, provide each
-separate expression within parentheses. For example: """
-(scheduling.automaticRestart = true) (cpuPlatform = "Intel Skylake")
-""" By default, each expression is an "AND" expression. However, you
-can include "AND" and "OR" expressions explicitly. For example: """
-(cpuPlatform = "Intel Skylake") OR (cpuPlatform = "Intel Broadwell")
-AND (scheduling.automaticRestart = true) """`,
+				Description: `Filter string, adhering to the rules in List-operation filtering (https://cloud.google.com/secret-manager/docs/filtering).
+List only secrets matching the filter. If filter is empty, all secrets are listed.`,
 				Optional: true,
 			},
 			"secrets": {
