10ce5fc2b4e461ca333ffd95c907bfaf3e1797c4
Stephen Lewis (Burrows) <stephenrlewis@google.com>
2023-10-11 14:48:03 -0700

    Fixed failing TGC integration tests on main branch (#9164)
    * Updatd default provider version to 5.0.0
    
    * Re-enabled tgc integration tests
    
    * Corrected tgc integration test entrypoint
    
    * Set additional env vars during terraform runs
    
    * Added project data into tests that now have a project known due to provider defaults work
    
    * Added network_firewall_policy_enforcement_order default
    
    * Added some more new defaults to testdata
    
    * Corrected expected results for example_compute_instance
    
    * Fixed storage bucket without project tests
    
    * Corrected WithoutDefaultProject test list

--- mmv1/third_party/tgc/tests/source/utils_test.go
+++ mmv1/third_party/tgc/tests/source/utils_test.go
@@ -69,7 +69,12 @@ func terraformShow(t *testing.T, executable, dir, tfplan string) []byte {
 
 func terraformExec(t *testing.T, executable, dir string, args ...string) []byte {
 	cmd := exec.Command(executable, args...)
-	cmd.Env = []string{"HOME=" + filepath.Join(dir, "fakehome")}
+	cmd.Env = []string{
+		"HOME=" + filepath.Join(dir, "fakehome"),
+		"GOOGLE_PROJECT=" + data.Provider["project"],
+		"GOOGLE_FOLDER=" + data.FolderID,
+		"GOOGLE_ORG=" + data.OrgID,
+	}
 	cmd.Dir = dir
 	wantError := false
 	payload, _ := run(t, cmd, wantError)
@@ -177,28 +182,6 @@ func generateTFVconvertedAsset(t *testing.T, testDir, testSlug string) {
 	}
 
 	fmt.Println("created file : " + dstFile)
-}
-
-func getTestPrefix() string {
-	credentials, ok := data.Provider["credentials"]
-	if ok {
-		credentials = "credentials = \"" + credentials + "\""
-	}
-
-	return fmt.Sprintf(`terraform {
-		required_providers {
-			google = {
-				source  = "hashicorp/google"
-				version = "~> %s"
-			}
-		}
-	}
-
-	provider "google" {
-		%s
-	}
-
-`, data.Provider["version"], credentials)
 }
 
 // newTestConfig create a config using the http test server.
