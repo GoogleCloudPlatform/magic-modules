cf51a05dff6ec2ae60be3912c8192c95d8d4767d
Thomas Rodgers <thomasrodgers@google.com>
2023-10-13 18:36:07 +0000

    Migrate generate comment to go (#9060)
    * Migrate generate comment to go
    
    * Capitalize all test error messages
    
    * Move exec to separate package, refactor functions

--- .ci/magician/github/init.go
+++ .ci/magician/github/init.go
@@ -14,8 +14,9 @@ type GithubService interface {
 	GetPullRequestAuthor(prNumber string) (string, error)
 	GetPullRequestRequestedReviewer(prNumber string) (string, error)
 	GetPullRequestPreviousAssignedReviewers(prNumber string) ([]string, error)
+	GetPullRequestLabelIDs(prNumber string) (map[int]struct{}, error)
 	GetUserType(user string) UserType
-	PostBuildStatus(prNumber, title, state, target_url, commitSha string) error
+	PostBuildStatus(prNumber, title, state, targetURL, commitSha string) error
 	PostComment(prNumber, comment string) error
 	RequestPullRequestReviewer(prNumber, assignee string) error
 	AddLabel(prNumber, label string) error
@@ -24,11 +25,11 @@ type GithubService interface {
 }
 
 func NewGithubService() GithubService {
-	GITHUB_TOKEN, ok := os.LookupEnv("GITHUB_TOKEN")
+	githubToken, ok := os.LookupEnv("GITHUB_TOKEN")
 	if !ok {
 		fmt.Println("Did not provide GITHUB_TOKEN environment variable")
 		os.Exit(1)
 	}
 
-	return &github{token: GITHUB_TOKEN}
+	return &github{token: githubToken}
 }
