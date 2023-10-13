3234feb9bc405b586c7496b2754e9062d5f57936
Bhargav Chereddy <bcreddy@google.com>
2023-10-13 00:43:50 +0000

    fix basic test

--- mmv1/templates/terraform/constants/workbench_instance.go
+++ mmv1/templates/terraform/constants/workbench_instance.go
@@ -1,13 +1,30 @@
-var WorkbenchInstanceGoogleProvidedLabels = []string{
+var WorkbenchInstanceProvidedLabels = []string{
 	"consumer-project-id",
 	"consumer-project-number",
 	"notebooks-product",
 	"resource-name",
 }
 
+var WorkbenchInstanceProvidedMetadata = []string{
+	"disable-swap-binaries",
+	"enable-guest-attributes",
+	"enable-oslogin",
+	"install-nvidia-driver",
+	"notebooks-api",
+	"notebooks-api-version",
+	"nvidia-driver-gcs-path",
+	"proxy-backend-id",
+	"proxy-mode",
+	"proxy-registration-url",
+	"proxy-url",
+	"proxy-user-mail",
+	"serial-port-logging-enable",
+	"shutdown-script",
+}
+
 func WorkbenchInstanceLabelDiffSuppress(k, old, new string, d *schema.ResourceData) bool {
-	// Suppress diffs for the labels provided by Google
-	for _, label := range WorkbenchInstanceGoogleProvidedLabels {
+	// Suppress diffs for the labels
+	for _, label := range WorkbenchInstanceProvidedLabels {
 		if strings.Contains(k, label) && new == "" {
 			return true
 		}
@@ -15,6 +32,23 @@ func WorkbenchInstanceLabelDiffSuppress(k, old, new string, d *schema.ResourceDa
 
 	// Let diff be determined by labels (above)
 	if strings.Contains(k, "labels.%") {
+		return true
+	}
+
+	// For other keys, don't suppress diff.
+	return false
+}
+
+func WorkbenchInstanceMetadataDiffSuppress(k, old, new string, d *schema.ResourceData) bool {
+	// Suppress diffs for the metadata
+	for _, metadata := range WorkbenchInstanceProvidedMetadata {
+		if strings.Contains(k, metadata) && new == "" {
+			return true
+		}
+	}
+
+	// Let diff be determined by metadata (above)
+	if strings.Contains(k, "gce_setup.0.metadata.%") {
 		return true
 	}
 
