8ea8110aa5e779312a173448e82b54fb3d923dc7
Hao Nan Li <haonan@google.com>
2023-10-25 12:04:03 -0700

    Add hao-nan-li to on vacation list

--- .ci/magician/github/membership.go
+++ .ci/magician/github/membership.go
@@ -30,7 +30,9 @@ var (
 	trustedContributors = []string{}
 
 	// This is for reviewers who are "on vacation": will not receive new review assignments but will still receive re-requests for assigned PRs.
-	onVacationReviewers = []string{}
+	onVacationReviewers = []string{
+		"hao-nan-li",
+	}
 )
 
 type UserType int64
