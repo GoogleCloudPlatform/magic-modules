c4e3924694f98e291aa549168fb205cde8e6cc79
Amir Romashkin <romashkin@google.com>
2023-10-27 15:28:38 +0000

    Reduce number of generated resources to 4: google_compute_region_backend_service google_compute_backend_service google_compute_forwarding_rule google_compute_region_health_check

--- mmv1/third_party/cai2hcl/services/compute/converter.go
+++ mmv1/third_party/cai2hcl/services/compute/converter.go
@@ -7,7 +7,6 @@ import (
 
 var forwardingRuleMatchers = []matchers.ConverterMatcher{
 	matchers.ByAssetName(ComputeForwardingRuleAssetNameRegex, "google_compute_forwarding_rule"),
-	matchers.ByAssetName(ComputeGlobalForwardingRuleAssetNameRegex, "google_compute_global_forwarding_rule"),
 }
 
 var backendServiceMatchers = []matchers.ConverterMatcher{
@@ -15,24 +14,26 @@ var backendServiceMatchers = []matchers.ConverterMatcher{
 	matchers.ByAssetName(ComputeBackendServiceAssetNameRegex, "google_compute_backend_service"),
 }
 
+var healthCheckMatchers = []matchers.ConverterMatcher{
+	matchers.ByAssetName(ComputeRegionHealthCheckAssetNameRegex, "google_compute_region_health_check"),
+}
+
 var UberConverter = common.UberConverter{
 	ConverterByAssetType: map[string]string{
-		ComputeInstanceAssetType:    "google_compute_instance",
-		ComputeHealthCheckAssetType: "google_compute_health_check",
+		ComputeInstanceAssetType: "google_compute_instance",
 	},
 	ConverterMatchersByAssetType: map[string][]matchers.ConverterMatcher{
-		ComputeForwardingRuleAssetType:       forwardingRuleMatchers,
-		ComputeGlobalForwardingRuleAssetType: forwardingRuleMatchers,
-
-		ComputeBackendServiceAssetType:       backendServiceMatchers,
 		ComputeRegionBackendServiceAssetType: backendServiceMatchers,
+		ComputeBackendServiceAssetType:       backendServiceMatchers,
+
+		ComputeRegionHealthCheckAssetType: healthCheckMatchers,
+		ComputeForwardingRuleAssetType:    forwardingRuleMatchers,
 	},
 	Converters: common.CreateConverterMap(map[string]common.ConverterFactory{
 		"google_compute_instance":               NewComputeInstanceConverter,
-		"google_compute_health_check":           NewComputeHealthCheckConverter,
-		"google_compute_forwarding_rule":        NewComputeForwardingRuleConverter,
-		"google_compute_global_forwarding_rule": NewComputeGlobalForwardingRuleConverter,
 		"google_compute_region_backend_service": NewComputeRegionBackendServiceConverter,
 		"google_compute_backend_service":        NewComputeBackendServiceConverter,
+		"google_compute_forwarding_rule":        NewComputeForwardingRuleConverter,
+		"google_compute_region_health_check":    NewComputeRegionHealthCheckConverter,
 	}),
 }
