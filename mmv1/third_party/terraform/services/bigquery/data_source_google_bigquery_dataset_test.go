7ea1f96adfecb9d247a3d0a9b48a644b67ee42ba
Zhenhua Li <zhenhuali@google.com>
2023-11-07 14:48:30 -0800

    Set labels in data source google_bigquery_dataset

--- mmv1/third_party/terraform/services/bigquery/data_source_google_bigquery_dataset_test.go
+++ mmv1/third_party/terraform/services/bigquery/data_source_google_bigquery_dataset_test.go
@@ -40,6 +40,9 @@ func testAccDataSourceGoogleBigqueryDataset_basic(context map[string]interface{}
     description                 = "This is a test description"
     location                    = "US"
     default_table_expiration_ms = 3600000
+	labels = {
+	  env                       = "foo"
+	}
   }
 
   data "google_bigquery_dataset" "bar" {
