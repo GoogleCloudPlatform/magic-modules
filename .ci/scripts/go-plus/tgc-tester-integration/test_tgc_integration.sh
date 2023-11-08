087dc762c114fcc831cc9abdf513951f4aaae809
Iris Chen <10179943+iyabchen@users.noreply.github.com>
2023-11-08 10:08:21 -0800

    test dev override

--- .ci/scripts/go-plus/tgc-tester-integration/test_tgc_integration.sh
+++ .ci/scripts/go-plus/tgc-tester-integration/test_tgc_integration.sh
@@ -70,7 +70,43 @@ go mod edit -replace github.com/hashicorp/terraform-provider-google-beta=github.
 go mod tidy
 
 make build
-make test-integration
+
+TF_CONFIG_FILE="tf-dev-override.tfrc"
+
+go clean --modcache
+go list -json  -m github.com/hashicorp/terraform-provider-google-beta
+REPLACE_DIR=`go list -json  -m github.com/hashicorp/terraform-provider-google-beta | jq -r '.Dir // empty'`
+VERSION=`go list -json  -m github.com/hashicorp/terraform-provider-google-beta | jq -r .Version`
+
+if [ ! -z "$REPLACE_DIR" ]
+then
+  pushd $REPLACE_DIR
+    go install
+  popd
+else
+  go install github.com/hashicorp/terraform-provider-google-beta@$VERSION
+fi
+
+# create terraform configuration file
+if ! [ -f $TF_CONFIG_FILE ];then
+  cat <<EOF > $TF_CONFIG_FILE
+  provider_installation {
+    # Developer overrides will stop Terraform from downloading the listed
+    # providers their origin provider registries.
+    dev_overrides {
+        "hashicorp/google-beta" = "$GOPATH/bin"
+    }
+    # For all other providers, install them directly from their origin provider
+    # registries as normal. If you omit this, Terraform will _only_ use
+    # the dev_overrides block, and so no other providers will be available.
+    # Without this, show "Failed to query available provider packages"
+    # at terraform init
+    direct{}
+  }
+EOF
+fi
+
+TF_CLI_CONFIG_FILE="${PWD}/${TF_CONFIG_FILE}" go test -run=CLI ./...
 exit_code=$?
 
 set -e
