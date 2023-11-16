381d908d018dca305caa4197735a92ce81cc7db0
G-NamanGupta <98808262+G-NamanGupta@users.noreply.github.com>
2023-11-17 01:09:50 +0530

    Update mmv1/third_party/terraform/services/netapp/resource_netapp_storage_pool_test.go
    Co-authored-by: Shuya Ma <87669292+shuyama1@users.noreply.github.com>

--- mmv1/third_party/terraform/services/netapp/resource_netapp_storage_pool_test.go
+++ mmv1/third_party/terraform/services/netapp/resource_netapp_storage_pool_test.go
@@ -51,7 +51,7 @@ func TestAccNetappstoragePool_storagePoolCreateExample_update(t *testing.T) {
 				ResourceName:            "google_netapp_storage_pool.test_pool",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"location", "name"},
+				ImportStateVerifyIgnore: []string{"location", "name", "labels", "terraform_labels"},
 			},
 		},
 	})
