e03dd640c0bd42c9b1d1b35c63cdeee0b21ca639
Amir Romashkin <romashkin@google.com>
2023-11-19 12:14:47 +0000

    Split current PR into 3 chunks: 1. Refactor of shared files (Uber Converter) 2. Add ForwardingRuleConverter (generated) 3. Add BackendServiceConverters (generated) + ability to match converters by name
    This commit removes part2 and part3, leaving only part1 in this branch.

--- mmv1/third_party/cai2hcl/common/uber_converter.go
+++ mmv1/third_party/cai2hcl/common/uber_converter.go
@@ -1,7 +1,6 @@
 package common
 
 import (
-	"github.com/GoogleCloudPlatform/terraform-google-conversion/v2/cai2hcl/common/matchers"
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v2/caiasset"
 )
 
@@ -11,8 +10,6 @@ type UberConverter struct {
 	// Collection of asset name formats (i.e. projects/(?P<project>[^/]+)/regions/(?P<region>[^/]+)/forwardingRules)
 	// together with corresponding converter name.
 	ConverterByAssetType map[string]string
-
-	ConverterMatchersByAssetType map[string][]matchers.ConverterMatcher
 
 	// Mapping between converter name and converter constructor.
 	Converters map[string]Converter
@@ -25,17 +22,7 @@ func (c UberConverter) Convert(assets []*caiasset.Asset) ([]*HCLResourceBlock, e
 	groups := make(map[string][]*caiasset.Asset)
 	for _, asset := range assets {
 
-		name, found := c.ConverterByAssetType[asset.Type]
-		if !found {
-			matchers, _ := c.ConverterMatchersByAssetType[asset.Type]
-
-			for _, matcher := range matchers {
-				if matcher.Match(asset) {
-					name = matcher.GetConverterName()
-				}
-			}
-		}
-
+		name, _ := c.ConverterByAssetType[asset.Type]
 		if name != "" {
 			groups[name] = append(groups[name], asset)
 		}
--- mmv1/third_party/cai2hcl/services/converter.go
+++ mmv1/third_party/cai2hcl/services/converter.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v2/cai2hcl/common"
-	"github.com/GoogleCloudPlatform/terraform-google-conversion/v2/cai2hcl/common/matchers"
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v2/cai2hcl/services/compute"
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v2/cai2hcl/services/resourcemanager"
 )
@@ -18,19 +17,16 @@ var UberConverter common.UberConverter
 
 func init() {
 	var converterByAssetType = make(map[string]string)
-	var converterMatchersByAssetType = make(map[string][]matchers.ConverterMatcher)
 	var converters = make(map[string]common.Converter)
 
 	for _, uberConverter := range uberConverters {
 		appendMap(converterByAssetType, uberConverter.ConverterByAssetType)
-		appendMap(converterMatchersByAssetType, uberConverter.ConverterMatchersByAssetType)
 		appendMap(converters, uberConverter.Converters)
 	}
 
 	UberConverter = common.UberConverter{
-		ConverterByAssetType:         converterByAssetType,
-		ConverterMatchersByAssetType: converterMatchersByAssetType,
-		Converters:                   converters,
+		ConverterByAssetType: converterByAssetType,
+		Converters:           converters,
 	}
 }
 
