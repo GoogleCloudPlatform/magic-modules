3df5948c1ca1dcb5c5a82351e467f43830c88887
Yi Xing <yxng@google.com>
2023-11-29 18:14:47 +0000

    Change back to use GetTestOrgFromEnv in testing

--- mmv1/third_party/terraform/services/securitycenter/resource_scc_event_threat_detection_custom_module_test.go
+++ mmv1/third_party/terraform/services/securitycenter/resource_scc_event_threat_detection_custom_module_test.go
@@ -18,7 +18,7 @@ func TestAccSecurityCenterEventThreatDetectionCustomModule(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"org_id":        envvar.GetTestOrgTargetFromEnv(t),
+		"org_id":        envvar.GetTestOrgFromEnv(t),
 		"random_suffix": acctest.RandString(t, 10),
 	}
 
