ee88c211f2d677b611385a889bce53ed527100ab
Sarah French <sarah.french@hashicorp.com>
2023-12-04 19:18:12 +0000

    Add unit test for new ordering behaviour of `flattenStatefulPolicyStatefulExternalIps`

--- mmv1/third_party/terraform/services/compute/resource_compute_instance_group_manager_internal_test.go
+++ mmv1/third_party/terraform/services/compute/resource_compute_instance_group_manager_internal_test.go
@@ -1,7 +1,11 @@
 package compute
 
 import (
+	"reflect"
 	"testing"
+
+	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
+	"google.golang.org/api/compute/v1"
 )
 
 func TestInstanceGroupManager_parseUniqueId(t *testing.T) {
@@ -74,5 +78,157 @@ func TestInstanceGroupManager_convertUniqueId(t *testing.T) {
 		if actual != expected {
 			t.Fatalf("invalid return value by ConvertToUniqueIdWhenPresent for input %v; expected: %v, actual: %v", input, expected, actual)
 		}
+	}
+}
+
+func TestFlattenStatefulPolicyStatefulExternalIps(t *testing.T) {
+	cases := map[string]struct {
+		ConfigValues map[string]interface{}
+		ExternalIPs  map[string]compute.StatefulPolicyPreservedStateNetworkIp
+		Expected     []map[string]interface{}
+	}{
+		"Single external IP (nic0) in config and API-side": {
+			ConfigValues: map[string]interface{}{
+				"stateful_external_ip": []interface{}{
+					map[string]interface{}{
+						"interface_name": "nic0",
+						"delete_rule":    "NEVER",
+					},
+				},
+			},
+			ExternalIPs: map[string]compute.StatefulPolicyPreservedStateNetworkIp{
+				"nic0": {
+					AutoDelete: "NEVER",
+				},
+			},
+			Expected: []map[string]interface{}{
+				{
+					"interface_name": "nic0",
+					"delete_rule":    "NEVER",
+				},
+			},
+		},
+		"Two external IPs (nic0, nic1). Unordered in config and sorted API-side": {
+			ConfigValues: map[string]interface{}{
+				"stateful_external_ip": []interface{}{
+					map[string]interface{}{
+						"interface_name": "nic1",
+						"delete_rule":    "NEVER",
+					},
+					map[string]interface{}{
+						"interface_name": "nic0",
+						"delete_rule":    "NEVER",
+					},
+				},
+			},
+			ExternalIPs: map[string]compute.StatefulPolicyPreservedStateNetworkIp{
+				"nic0": {
+					AutoDelete: "NEVER",
+				},
+				"nic1": {
+					AutoDelete: "NEVER",
+				},
+			},
+			Expected: []map[string]interface{}{
+				{
+					"interface_name": "nic1",
+					"delete_rule":    "NEVER",
+				},
+				{
+					"interface_name": "nic0",
+					"delete_rule":    "NEVER",
+				},
+			},
+		},
+		"Two external IPs (nic0, nic1). Only nic0 in config and both stored API-side": {
+			ConfigValues: map[string]interface{}{
+				"stateful_external_ip": []interface{}{
+					map[string]interface{}{
+						"interface_name": "nic0",
+						"delete_rule":    "NEVER",
+					},
+				},
+			},
+			ExternalIPs: map[string]compute.StatefulPolicyPreservedStateNetworkIp{
+				"nic0": {
+					AutoDelete: "NEVER",
+				},
+				"nic1": {
+					AutoDelete: "NEVER",
+				},
+			},
+			Expected: []map[string]interface{}{
+				{
+					"interface_name": "nic0",
+					"delete_rule":    "NEVER",
+				},
+				{
+					"interface_name": "nic1",
+					"delete_rule":    "NEVER",
+				},
+			},
+		},
+		"Three external IPs (nic0, nic1, nic2). Only nic1, nic2 in config and all 3 stored API-side": {
+			ConfigValues: map[string]interface{}{
+				"stateful_external_ip": []interface{}{
+					map[string]interface{}{
+						"interface_name": "nic1",
+						"delete_rule":    "NEVER",
+					},
+					map[string]interface{}{
+						"interface_name": "nic2",
+						"delete_rule":    "NEVER",
+					},
+				},
+			},
+			ExternalIPs: map[string]compute.StatefulPolicyPreservedStateNetworkIp{
+				"nic0": {
+					AutoDelete: "NEVER",
+				},
+				"nic1": {
+					AutoDelete: "NEVER",
+				},
+				"nic2": {
+					AutoDelete: "NEVER",
+				},
+			},
+			Expected: []map[string]interface{}{
+				{
+					"interface_name": "nic1",
+					"delete_rule":    "NEVER",
+				},
+				{
+					"interface_name": "nic2",
+					"delete_rule":    "NEVER",
+				},
+				{
+					"interface_name": "nic0",
+					"delete_rule":    "NEVER",
+				},
+			},
+		},
+	}
+
+	for tn, tc := range cases {
+		t.Run(tn, func(t *testing.T) {
+
+			// Terraform config
+			schema := ResourceComputeRegionInstanceGroupManager().Schema
+			d := tpgresource.SetupTestResourceDataFromConfigMap(t, schema, tc.ConfigValues)
+
+			// API response
+			statefulPolicyPreservedState := compute.StatefulPolicyPreservedState{
+				ExternalIPs: tc.ExternalIPs,
+			}
+			statefulPolicy := compute.StatefulPolicy{
+				PreservedState: &statefulPolicyPreservedState,
+			}
+
+			output := flattenStatefulPolicyStatefulExternalIps(d, &statefulPolicy)
+
+			if !reflect.DeepEqual(tc.Expected, output) {
+				t.Fatalf("expected output to be %#v, but got %#v", tc.Expected, output)
+			}
+		})
 	}
 }
