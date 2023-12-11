fcceefdb62d01cb55bc530b320d1640444082369
Sarah French <15078782+SarahFrench@users.noreply.github.com>
2023-12-11 13:32:11 +0000

    Update mmv1/third_party/terraform/services/cloudrunv2/data_source_google_cloud_run_v2_service_test.go

--- mmv1/third_party/terraform/services/cloudrunv2/data_source_google_cloud_run_v2_service_test.go
+++ mmv1/third_party/terraform/services/cloudrunv2/data_source_google_cloud_run_v2_service_test.go
@@ -68,7 +68,7 @@ func TestAccDataSourceGoogleCloudRunV2Service_bindIAMPermission(t *testing.T) {
 
 	project := envvar.GetTestProjectFromEnv()
 
-	name := fmt.Sprintf("tf-test-%d", acctest.RandString(t, 10))
+	name := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
 	location := "us-central1"
 	id := fmt.Sprintf("projects/%s/locations/%s/services/%s", project, location, name)
 
