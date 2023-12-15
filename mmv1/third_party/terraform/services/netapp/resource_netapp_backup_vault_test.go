e269bbc336ce7d9482ff123a14ac68454dc40827
Oliver Krause <okrause@netapp.com>
2023-12-15 14:10:13 +0100

    netapp-backup-vault: First commit

---
+++ mmv1/third_party/terraform/services/netapp/resource_netapp_backup_vault_test.go
@@ -0,0 +1,64 @@
+package netapp_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+)
+
+func TestAccNetappbackupVault_netappBackupVaultExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetappbackupVault_netappBackupVaultExample_basic(context),
+			},
+			{
+				ResourceName:            "google_netapp_backup_vault.test_backup_vault",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "name", "labels", "terraform_labels"},
+			},
+			{
+				Config: testAccNetappbackupVault_netappBackupVaultExample_update(context),
+			},
+			{
+				ResourceName:            "google_netapp_backup_vault.test_backup_vault",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "name", "labels", "terraform_labels"},
+			},
+		},
+	})
+}
+
+func testAccNetappbackupVault_netappBackupVaultExample_basic(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_netapp_backup_vault" "test_backup_vault" {
+  name = "tf-test-test-backup-vault%{random_suffix}"
+  location = "us-central1"
+}
+`, context)
+}
+
+func testAccNetappbackupVault_netappBackupVaultExample_update(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_netapp_backup_vault" "test_backup_vault" {
+  name = "tf-test-test-backup-vault%{random_suffix}"
+  location = "us-central1"
+  description = "Terraform created vault"
+  labels = { 
+    "creator": "testuser"
+  }
+}
+`, context)
+}
