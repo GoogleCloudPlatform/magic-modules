ee97be3a5573a03fb47bb3ca44abb80062e6fa87
BBBmau <alvarez.mauriciotm@gmail.com>
2023-12-18 09:35:44 -0800

    simplify data source test

--- mmv1/third_party/terraform/services/compute/data_source_google_compute_reservation_test.go
+++ mmv1/third_party/terraform/services/compute/data_source_google_compute_reservation_test.go
@@ -6,10 +6,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 )
 
@@ -19,8 +16,8 @@ func TestAccDataSourceComputeReservation(t *testing.T) {
 	reservationName := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
 
 	rsName := "foobar"
-	rsFullName := fmt.Sprintf("google_compute_reservation.%s", rsName)
 	dsName := "my_reservation"
+	rsFullName := fmt.Sprintf("google_compute_reservation.%s", rsName)
 	dsFullName := fmt.Sprintf("data.google_compute_reservation.%s", dsName)
 
 	acctest.VcrTest(t, resource.TestCase{
@@ -31,54 +28,12 @@ func TestAccDataSourceComputeReservation(t *testing.T) {
 			{
 				Config: testAccDataSourceComputeReservationConfig(reservationName, rsName, dsName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccDataSourceComputeReservationCheck(t, dsFullName, rsFullName),
+					resource.TestCheckResourceAttr(dsFullName, "status", "READY"),
+					acctest.CheckDataSourceStateMatchesResourceState(dsFullName, rsFullName),
 				),
 			},
 		},
 	})
-}
-
-func testAccDataSourceComputeReservationCheck(t *testing.T, data_source_name string, resource_name string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		ds, ok := s.RootModule().Resources[data_source_name]
-		if !ok {
-			return fmt.Errorf("root module has no resource called %s", data_source_name)
-		}
-
-		rs, ok := s.RootModule().Resources[resource_name]
-		if !ok {
-			return fmt.Errorf("can't find %s in state", resource_name)
-		}
-
-		ds_attr := ds.Primary.Attributes
-		rs_attr := rs.Primary.Attributes
-
-		reservation_attrs_to_test := []string{
-			"name",
-			"specific_reservation",
-		}
-
-		for _, attr_to_check := range reservation_attrs_to_test {
-			if ds_attr[attr_to_check] != rs_attr[attr_to_check] {
-				return fmt.Errorf(
-					"%s is %s; want %s",
-					attr_to_check,
-					ds_attr[attr_to_check],
-					rs_attr[attr_to_check],
-				)
-			}
-		}
-
-		if !tpgresource.CompareSelfLinkOrResourceName("", ds_attr["self_link"], rs_attr["self_link"], nil) && ds_attr["self_link"] != rs_attr["self_link"] {
-			return fmt.Errorf("self link does not match: %s vs %s", ds_attr["self_link"], rs_attr["self_link"])
-		}
-
-		if ds_attr["status"] != "READY" {
-			return fmt.Errorf("status is %s; want READY", ds_attr["status"])
-		}
-
-		return nil
-	}
 }
 
 func testAccDataSourceComputeReservationConfig(reservationName, rsName, dsName string) string {
