6cda3126692c1eb6af03cc72b3b0615406346ff3
BBBmau <alvarez.mauriciotm@gmail.com>
2023-12-18 10:00:10 -0800

    add ReservationIdParsing Test

--- mmv1/third_party/terraform/services/compute/data_source_google_compute_reservation.go
+++ mmv1/third_party/terraform/services/compute/data_source_google_compute_reservation.go
@@ -3,6 +3,7 @@ package compute
 import (
 	"fmt"
 	"regexp"
+	"strings"
 
 	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
@@ -60,4 +61,54 @@ type ComputeReservationId struct {
 
 func (s ComputeReservationId) CanonicalId() string {
 	return fmt.Sprintf(computeReservationIdTemplate, s.Project, s.Zone, s.Name)
+}
+
+func ParseComputeReservationId(id string, config *transport_tpg.Config) (*ComputeReservationId, error) {
+	var parts []string
+	if computeReservationLinkRegex.MatchString(id) {
+		parts = computeReservationLinkRegex.FindStringSubmatch(id)
+
+		return &ComputeReservationId{
+			Project: parts[1],
+			Zone:    parts[2],
+			Name:    parts[3],
+		}, nil
+	} else {
+		parts = strings.Split(id, "/")
+	}
+
+	if len(parts) == 3 {
+		return &ComputeReservationId{
+			Project: parts[0],
+			Zone:    parts[1],
+			Name:    parts[2],
+		}, nil
+	} else if len(parts) == 2 {
+		// Project is optional.
+		if config.Project == "" {
+			return nil, fmt.Errorf("The default project for the provider must be set when using the `{zone}/{name}` id format.")
+		}
+
+		return &ComputeReservationId{
+			Project: config.Project,
+			Zone:    parts[0],
+			Name:    parts[1],
+		}, nil
+	} else if len(parts) == 1 {
+		// Project and region is optional
+		if config.Project == "" {
+			return nil, fmt.Errorf("The default project for the provider must be set when using the `{name}` id format.")
+		}
+		if config.Zone == "" {
+			return nil, fmt.Errorf("The default zone for the provider must be set when using the `{name}` id format.")
+		}
+
+		return &ComputeReservationId{
+			Project: config.Project,
+			Zone:    config.Zone,
+			Name:    parts[0],
+		}, nil
+	}
+
+	return nil, fmt.Errorf("Invalid compute reservation id. Expecting resource link, `{project}/{zone}/{name}`, `{zone}/{name}` or `{name}` format.")
 }
--- mmv1/third_party/terraform/services/compute/data_source_google_compute_reservation_test.go
+++ mmv1/third_party/terraform/services/compute/data_source_google_compute_reservation_test.go
@@ -6,9 +6,72 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/hashicorp/terraform-provider-google/google/services/compute"
+	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 )
+
+func TestComputeReservationIdParsing(t *testing.T) {
+	cases := map[string]struct {
+		ImportId            string
+		ExpectedError       bool
+		ExpectedCanonicalId string
+		Config              *transport_tpg.Config
+	}{
+		"id is a full self link": {
+			ImportId:            "https://www.googleapis.com/compute/v1/projects/test-project/zones/us-central1-a/reservations/test-reservation",
+			ExpectedError:       false,
+			ExpectedCanonicalId: "projects/test-project/zones/us-central1-a/reservations/test-reservation",
+		},
+		"id is a partial self link": {
+			ImportId:            "projects/test-project/zones/us-central1-a/reservations/test-reservation",
+			ExpectedError:       false,
+			ExpectedCanonicalId: "projects/test-project/zones/us-central1-a/reservations/test-reservation",
+		},
+		"id is project/region/address": {
+			ImportId:            "test-project/us-central1-a/test-reservation",
+			ExpectedError:       false,
+			ExpectedCanonicalId: "projects/test-project/zones/us-central1-a/reservations/test-reservation",
+		},
+		"id is region/address": {
+			ImportId:            "us-central1-a/test-reservation",
+			ExpectedError:       false,
+			ExpectedCanonicalId: "projects/default-project/zones/us-central1-a/reservations/test-reservation",
+			Config:              &transport_tpg.Config{Project: "default-project"},
+		},
+		"id is address": {
+			ImportId:            "test-reservation",
+			ExpectedError:       false,
+			ExpectedCanonicalId: "projects/default-project/zones/us-east1-a/reservations/test-reservation",
+			Config:              &transport_tpg.Config{Project: "default-project", Zone: "us-east1-a"},
+		},
+		"id has invalid format": {
+			ImportId:      "i/n/v/a/l/i/d",
+			ExpectedError: true,
+		},
+	}
+
+	for tn, tc := range cases {
+		addressId, err := compute.ParseComputeReservationId(tc.ImportId, tc.Config)
+
+		if tc.ExpectedError && err == nil {
+			t.Fatalf("bad: %s, expected an error", tn)
+		}
+
+		if err != nil {
+			if tc.ExpectedError {
+				continue
+			}
+			t.Fatalf("bad: %s, err: %#v", tn, err)
+		}
+
+		if addressId.CanonicalId() != tc.ExpectedCanonicalId {
+			t.Fatalf("bad: %s, expected canonical id to be `%s` but is `%s`", tn, tc.ExpectedCanonicalId, addressId.CanonicalId())
+		}
+	}
+}
 
 func TestAccDataSourceComputeReservation(t *testing.T) {
 	t.Parallel()
