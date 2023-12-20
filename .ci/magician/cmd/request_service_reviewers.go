9053f6e7f37f36368366332ad68a1efcec740f7a
Stephen Lewis <stephenrlewis@google.com>
2023-12-19 16:20:03 -0800

    Fixed build errors

--- .ci/magician/cmd/request_service_reviewers.go
+++ .ci/magician/cmd/request_service_reviewers.go
@@ -1,6 +1,18 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
+* Copyright 2023 Google LLC. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+ */
 package cmd
 
 import (
@@ -12,6 +24,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/GoogleCloudPlatform/magic-modules/tools/issue-labeler/labeler"
+	"gopkg.in/yaml.v2"
 )
 
 // requestServiceReviewersCmd represents the requestServiceReviewers command
@@ -27,12 +40,12 @@ var requestServiceReviewersCmd = &cobra.Command{
 		prNumber := args[0]
 		fmt.Println("PR Number: ", prNumber)
 
-		gh := github.NewGithubService()
+		gh := github.NewClient()
 		execRequestServiceReviewers(prNumber, gh, labeler.EnrolledTeamsYaml)
 	},
 }
 
-func execRequestServiceReviewers(prNumber string, gh github.GithubService, enrolledTeamsYaml []byte) {
+func execRequestServiceReviewers(prNumber string, gh GithubClient, enrolledTeamsYaml []byte) {
 	pullRequest, err := gh.GetPullRequest(prNumber)
 	if err != nil {
 		fmt.Println(err)
@@ -45,7 +58,13 @@ func execRequestServiceReviewers(prNumber string, gh github.GithubService, enrol
 		os.Exit(1)
 	}
 
-	previousReviewers, err := gh.GetPullRequestPreviousAssignedReviewers(prNumber)
+	requestedReviewers, err := gh.GetPullRequestRequestedReviewers(prNumber)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	previousReviewers, err := gh.GetPullRequestPreviousReviewers(prNumber)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -60,8 +79,8 @@ func execRequestServiceReviewers(prNumber string, gh github.GithubService, enrol
 			continue
 		}
 		teamCount += 1
-		if labelData, ok := enrolledTeams[label]; ok {
-			githubTeams[labelData] = struct{}{}
+		if labelData, ok := enrolledTeams[label.Name]; ok {
+			githubTeams[labelData.Team] = struct{}{}
 		}
 	}
 
@@ -70,12 +89,15 @@ func execRequestServiceReviewers(prNumber string, gh github.GithubService, enrol
 		return
 	}
 
-	// For each service team, check if there is already a reviewer in the team list. Rerequest
+	// For each service team, check if one of the team members is already a reviewer. Rerequest
 	// review if there is and choose a random reviewer from the list if there isn't.
-	previousReviewersMap = make(map[string]struct{})
+	reviewersMap := make(map[string]struct{})
 	toRequest := []string{}
+	for _, reviewer := range requestedReviewers {
+		reviewersMap[reviewer.Login] = struct{}{}
+	}
 	for _, reviewer := range previousReviewers {
-		previousReviewersMap[reviewer] = struct{}{}
+		reviewersMap[reviewer.Login] = struct{}{}
 	}
 
 	exitCode := 0
@@ -88,14 +110,14 @@ func execRequestServiceReviewers(prNumber string, gh github.GithubService, enrol
 		}
 		hasReviewer := false
 		for _, member := range members {
-			if previousReviewersMap[member] {
+			if _, ok := reviewersMap[member.Login]; ok {
 				hasReviewer = true
-				toRequest = append(toRequest, member)
+				toRequest = append(toRequest, member.Login)
 			}
 		}
 
 		if !hasReviewer {
-			toRequest = append(toRequest, members[rand.Intn(len(members))])
+			toRequest = append(toRequest, members[rand.Intn(len(members))].Login)
 		}
 	}
 
