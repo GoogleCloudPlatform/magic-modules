5b3634d72e9273b0b02ee8789a83b3ca9383d9e4
Kodanda Rama <kodrama@google.com>
2023-12-20 16:43:20 +0530

    support vertexai featureonlinestore_featureview GA

---
+++ mmv1/third_party/terraform/services/vertexai/resource_vertex_ai_feature_onlinestore_featureview_test.go
@@ -0,0 +1,196 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package vertexai_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
+)
+
+func TestAccVertexAIFeatureOnlinestoreFeatureview_vertexAiFeatureonlinestoreFeatureview_updated(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"org_id":          envvar.GetTestOrgFromEnv(t),
+		"billing_account": envvar.GetTestBillingAccountFromEnv(t),
+		"random_suffix":   acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVertexAIFeatureOnlinestoreFeatureview_vertexAiFeatureonlinestoreFeatureview_basic(context),
+			},
+			{
+				ResourceName:            "google_vertex_ai_feature_onlinestore_featureview.featureview",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name", "etag", "feature_online_store", "labels", "terraform_labels"},
+			},
+			{
+				Config: testAccVertexAIFeatureOnlinestoreFeatureview_vertexAiFeatureonlinestoreFeatureview_update(context),
+			},
+			{
+				ResourceName:            "google_vertex_ai_feature_onlinestore_featureview.featureview",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name", "etag", "feature_online_store", "labels", "terraform_labels"},
+			},
+		},
+	})
+}
+
+func testAccVertexAIFeatureOnlinestoreFeatureview_vertexAiFeatureonlinestoreFeatureview_basic(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_vertex_ai_feature_online_store" "featureonlinestore" {
+  name     = "tf_test_featureonlinestore12"
+  labels = {
+    foo = "bar"
+  }
+  region   = "us-central1"
+  bigtable {
+   auto_scaling {
+   min_node_count = 1
+   max_node_count = 2
+   cpu_utilization_target = 80
+ }
+}
+  force_destroy = true
+}
+
+resource "google_bigquery_dataset" "tf-test-dataset" {
+
+  dataset_id                  = "tf_test_dataset1_featureview1"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "US"
+}
+
+resource "google_bigquery_table" "tf-test-table" {
+  deletion_protection = false
+
+  dataset_id = google_bigquery_dataset.tf-test-dataset.dataset_id
+  table_id   = "tf_test_bq_table"
+  schema = <<EOF
+  [
+  {
+    "name": "entity_id",
+    "mode": "NULLABLE",
+    "type": "STRING",
+    "description": "Test default entity_id"
+  },
+    {
+    "name": "test_entity_column",
+    "mode": "NULLABLE",
+    "type": "STRING",
+    "description": "test secondary entity column"
+  },
+  {
+    "name": "feature_timestamp",
+    "mode": "NULLABLE",
+    "type": "TIMESTAMP",
+    "description": "Default timestamp value"
+  }
+]
+EOF
+}
+
+resource "google_vertex_ai_feature_onlinestore_featureview" "featureview" {
+   name = "tf_test_tf_test_fv1%{random_suffix}"
+    feature_online_store = google_vertex_ai_feature_online_store.featureonlinestore.id
+    sync_config {
+      cron = "0 0 * * *"
+    }
+    big_query_source {
+        uri = "bq://${google_bigquery_table.tf-test-table.project}.${google_bigquery_table.tf-test-table.dataset_id}.${google_bigquery_table.tf-test-table.table_id}"
+        entity_id_columns = ["test_entity_column"]
+
+    }
+}
+
+data "google_project" "project" {
+  provider = google
+}
+`, context)
+}
+
+func testAccVertexAIFeatureOnlinestoreFeatureview_vertexAiFeatureonlinestoreFeatureview_update(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_vertex_ai_feature_online_store" "featureonlinestore" {
+name     = "tf_test_featureonlinestore12"
+labels = {
+foo = "bar"
+}
+region   = "us-central1"
+bigtable {
+auto_scaling {
+min_node_count = 1
+max_node_count = 2
+cpu_utilization_target = 80
+}
+}
+force_destroy = true
+}
+
+resource "google_bigquery_dataset" "tf-test-dataset" {
+
+dataset_id                  = "tf_test_dataset1_featureview1"
+friendly_name               = "test"
+description                 = "This is a test description"
+location                    = "US"
+}
+
+resource "google_bigquery_table" "tf-test-table" {
+deletion_protection = false
+
+dataset_id = google_bigquery_dataset.tf-test-dataset.dataset_id
+table_id   = "tf_test_bq_table"
+schema = <<EOF
+[
+{
+"name": "entity_id",
+"mode": "NULLABLE",
+"type": "STRING",
+"description": "Test default entity_id"
+},
+{
+"name": "test_entity_column",
+"mode": "NULLABLE",
+"type": "STRING",
+"description": "test secondary entity column"
+},
+{
+"name": "feature_timestamp",
+"mode": "NULLABLE",
+"type": "TIMESTAMP",
+"description": "Default timestamp value"
+}
+]
+EOF
+}
+
+resource "google_vertex_ai_feature_onlinestore_featureview" "featureview" {
+name = "tf_test_tf_test_fv1%{random_suffix}"
+feature_online_store = google_vertex_ai_feature_online_store.featureonlinestore.id
+sync_config {
+  cron = "0 4 * * *"
+}
+big_query_source {
+	uri = "bq://${google_bigquery_table.tf-test-table.project}.${google_bigquery_table.tf-test-table.dataset_id}.${google_bigquery_table.tf-test-table.table_id}"
+	entity_id_columns = ["test_entity_column"]
+
+}
+}
+
+data "google_project" "project" {
+provider = google
+}
+`, context)
+}
