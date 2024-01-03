a17f1d52ba4dbaebcc1629a89d8bf7e26007b018
Cameron Thornton <camthornton@google.com>
2024-01-03 10:56:04 -0600

    move local variables in resource.erb (#9735)

--- mmv1/api/resource.rb
+++ mmv1/api/resource.rb
@@ -647,6 +647,52 @@ Please refer to the field `effective_#{title}` for all of the #{title} present o
       end
     end
 
+    def resource_name
+      __product.name + name
+    end
+
+    # Filter the properties to keep only the ones don't have custom update
+    # method and group them by update url & verb.
+    def properties_without_custom_update(properties)
+      properties.select do |p|
+        p.update_url.nil? || p.update_verb.nil? || p.update_verb == :NOOP
+      end
+    end
+
+    def update_body_properties
+      update_prop = properties_without_custom_update(settable_properties)
+      update_prop = update_prop.reject(&:immutable) if update_verb == :PATCH
+      update_prop
+    end
+
+    # Handwritten TF Operation objects will be shaped like accessContextManager
+    # while the Google Go Client will have a name like accesscontextmanager
+    def client_name_pascal
+      client_name = __product.client_name || __product.name
+      client_name.camelize(:upper)
+    end
+
+    # In order of preference, use TF override,
+    # general defined timeouts, or default Timeouts
+    def timeouts
+      timeouts_filtered = @timeouts
+      timeouts_filtered ||= async&.operation&.timeouts
+      timeouts_filtered ||= Api::Timeouts.new
+      timeouts_filtered
+    end
+
+    def project?
+      base_url.include?('{{project}}') || create_url&.include?('{{project}}')
+    end
+
+    def region?
+      base_url.include?('{{region}}') && parameters.any? { |p| p.name == 'region' && p.ignore_read }
+    end
+
+    def zone?
+      base_url.include?('{{zone}}') && parameters.any? { |p| p.name == 'zone' && p.ignore_read }
+    end
+
     def merge(other)
       result = self.class.new
       instance_variables.each do |v|
