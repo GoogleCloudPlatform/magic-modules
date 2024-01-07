24fe61bff05f404d07309f8b1ce185f43bc1eb70
Sanmay Mishra <sanmaym@google.com>
2024-01-07 19:22:54 +0000

    service attachment

---
+++ mmv1/third_party/terraform/services/compute/data_source_compute_service_attachment.go
@@ -0,0 +1,70 @@
+package compute
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
+	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
+)
+
+func DataSourceGoogleComputeAttachments() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceGoogleComputeAttachmentsRead,
+
+		Schema: map[string]*schema.Schema{
+
+			"self_link": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"project": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"region": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceGoogleComputeAttachmentsRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*transport_tpg.Config)
+	userAgent, err := tpgresource.GenerateUserAgentString(d, config.UserAgent)
+	if err != nil {
+		return err
+	}
+
+	project, err := tpgresource.GetProject(d, config)
+	if err != nil {
+		return err
+	}
+	name := d.Get("name").(string)
+	region := d.Get("region").(string)
+	id := fmt.Sprintf("projects/%s/regions/%s/serviceAttachments/%s", project, region, name)
+
+	attachment, err := config.NewComputeClient(userAgent).serviceAttachments.Get(project, region, name).Do()
+	if err != nil {
+		return transport_tpg.HandleDataSourceNotFoundError(err, d, fmt.Sprintf("Service Attachment Not Found : %s", name), id)
+	}
+	if err := d.Set("self_link", attachment.SelfLink); err != nil {
+		return fmt.Errorf("Error setting self_link: %s", err)
+	}
+	if err := d.Set("description", attachment.description); err != nil {
+		return fmt.Errorf("Error setting description: %s", err)
+	}
+
+	d.SetId(id)
+	return nil
+}
