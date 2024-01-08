25772d19143536dca68eef5ee59ca2a524e08878
Kodanda Rama <kodrama@google.com>
2024-01-09 00:49:42 +0530

    fix spacing in test

--- mmv1/third_party/terraform/services/vertexai/resource_vertex_ai_feature_online_store_featureview_test.go
+++ mmv1/third_party/terraform/services/vertexai/resource_vertex_ai_feature_online_store_featureview_test.go
@@ -47,154 +47,154 @@ func TestAccVertexAIFeatureOnlineStoreFeatureview_vertexAiFeatureonlinestoreFeat
 
 func testAccVertexAIFeatureOnlineStoreFeatureview_vertexAiFeatureonlinestoreFeatureview_basic(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_vertex_ai_feature_online_store" "featureonlinestore" {
-  name     = "tf_test_featureonlinestore%{random_suffix}"
-  labels = {
-    foo = "bar"
+  resource "google_vertex_ai_feature_online_store" "featureonlinestore" {
+    name = "tf_test_featureonlinestore%{random_suffix}"
+    labels = {
+      foo = "bar"
+    }
+    region = "us-central1"
+    bigtable {
+      auto_scaling {
+        min_node_count         = 1
+        max_node_count         = 2
+        cpu_utilization_target = 80
+      }
+    }
   }
-  region   = "us-central1"
-  bigtable {
-   auto_scaling {
-   min_node_count = 1
-   max_node_count = 2
-   cpu_utilization_target = 80
- }
-}
-}
-
-resource "google_bigquery_dataset" "tf-test-dataset" {
-
-  dataset_id                  = "tf_test_dataset1_featureview%{random_suffix}"
-  friendly_name               = "test"
-  description                 = "This is a test description"
-  location                    = "US"
-}
-
-resource "google_bigquery_table" "tf-test-table" {
-  deletion_protection = false
-
-  dataset_id = google_bigquery_dataset.tf-test-dataset.dataset_id
-  table_id   = "tf_test_bq_table%{random_suffix}"
-  schema = <<EOF
-  [
-  {
-    "name": "entity_id",
-    "mode": "NULLABLE",
-    "type": "STRING",
-    "description": "Test default entity_id"
-  },
-    {
-    "name": "test_entity_column",
-    "mode": "NULLABLE",
-    "type": "STRING",
-    "description": "test secondary entity column"
-  },
-  {
-    "name": "feature_timestamp",
-    "mode": "NULLABLE",
-    "type": "TIMESTAMP",
-    "description": "Default timestamp value"
+  
+  resource "google_bigquery_dataset" "tf-test-dataset" {
+  
+    dataset_id    = "tf_test_dataset1_featureview%{random_suffix}"
+    friendly_name = "test"
+    description   = "This is a test description"
+    location      = "US"
   }
-]
-EOF
-}
-
-resource "google_vertex_ai_feature_online_store_featureview" "featureview" {
-   name = "tf_test_fv%{random_suffix}"
-   region   = "us-central1"
-   labels = {
-    foo = "bar"
+  
+  resource "google_bigquery_table" "tf-test-table" {
+    deletion_protection = false
+  
+    dataset_id = google_bigquery_dataset.tf-test-dataset.dataset_id
+    table_id   = "tf_test_bq_table%{random_suffix}"
+    schema     = <<EOF
+      [
+      {
+        "name": "entity_id",
+        "mode": "NULLABLE",
+        "type": "STRING",
+        "description": "Test default entity_id"
+      },
+        {
+        "name": "test_entity_column",
+        "mode": "NULLABLE",
+        "type": "STRING",
+        "description": "test secondary entity column"
+      },
+      {
+        "name": "feature_timestamp",
+        "mode": "NULLABLE",
+        "type": "TIMESTAMP",
+        "description": "Default timestamp value"
+      }
+    ]
+    EOF
   }
+  
+  resource "google_vertex_ai_feature_online_store_featureview" "featureview" {
+    name   = "tf_test_fv%{random_suffix}"
+    region = "us-central1"
+    labels = {
+      foo = "bar"
+    }
     feature_online_store = google_vertex_ai_feature_online_store.featureonlinestore.name
     sync_config {
       cron = "0 0 * * *"
     }
     big_query_source {
-        uri = "bq://${google_bigquery_table.tf-test-table.project}.${google_bigquery_table.tf-test-table.dataset_id}.${google_bigquery_table.tf-test-table.table_id}"
-        entity_id_columns = ["test_entity_column"]
-
+      uri               = "bq://${google_bigquery_table.tf-test-table.project}.${google_bigquery_table.tf-test-table.dataset_id}.${google_bigquery_table.tf-test-table.table_id}"
+      entity_id_columns = ["test_entity_column"]
+  
     }
-}
-
-data "google_project" "project" {
-  provider = google
-}
+  }
+  
+  data "google_project" "project" {
+    provider = google
+  }  
 `, context)
 }
 
 func testAccVertexAIFeatureOnlineStoreFeatureview_vertexAiFeatureonlinestoreFeatureview_update(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_vertex_ai_feature_online_store" "featureonlinestore" {
-name     = "tf_test_featureonlinestore%{random_suffix}"
-labels = {
-foo1 = "bar1"
-}
-region   = "us-central1"
-bigtable {
-auto_scaling {
-min_node_count = 1
-max_node_count = 2
-cpu_utilization_target = 80
-}
-}
-}
-
-resource "google_bigquery_dataset" "tf-test-dataset" {
-
-dataset_id                  = "tf_test_dataset1_featureview%{random_suffix}"
-friendly_name               = "test"
-description                 = "This is a test description"
-location                    = "US"
-}
-
-resource "google_bigquery_table" "tf-test-table" {
-deletion_protection = false
-
-dataset_id = google_bigquery_dataset.tf-test-dataset.dataset_id
-table_id   = "tf_test_bq_table%{random_suffix}"
-schema = <<EOF
-[
-{
-"name": "entity_id",
-"mode": "NULLABLE",
-"type": "STRING",
-"description": "Test default entity_id"
-},
-{
-"name": "test_entity_column",
-"mode": "NULLABLE",
-"type": "STRING",
-"description": "test secondary entity column"
-},
-{
-"name": "feature_timestamp",
-"mode": "NULLABLE",
-"type": "TIMESTAMP",
-"description": "Default timestamp value"
-}
-]
-EOF
-}
-
-resource "google_vertex_ai_feature_online_store_featureview" "featureview" {
-name = "tf_test_fv%{random_suffix}"
-region   = "us-central1"
-labels = {
-  foo1 = "bar1"
+  resource "google_vertex_ai_feature_online_store" "featureonlinestore" {
+    name = "tf_test_featureonlinestore%{random_suffix}"
+    labels = {
+      foo = "bar"
+    }
+    region = "us-central1"
+    bigtable {
+      auto_scaling {
+        min_node_count         = 1
+        max_node_count         = 2
+        cpu_utilization_target = 80
+      }
+    }
   }
-feature_online_store = google_vertex_ai_feature_online_store.featureonlinestore.name
-sync_config {
-  cron = "0 4 * * *"
-}
-big_query_source {
-	uri = "bq://${google_bigquery_table.tf-test-table.project}.${google_bigquery_table.tf-test-table.dataset_id}.${google_bigquery_table.tf-test-table.table_id}"
-	entity_id_columns = ["test_entity_column"]
-
-}
-}
-
-data "google_project" "project" {
-provider = google
-}
+  
+  resource "google_bigquery_dataset" "tf-test-dataset" {
+  
+    dataset_id    = "tf_test_dataset1_featureview%{random_suffix}"
+    friendly_name = "test"
+    description   = "This is a test description"
+    location      = "US"
+  }
+  
+  resource "google_bigquery_table" "tf-test-table" {
+    deletion_protection = false
+  
+    dataset_id = google_bigquery_dataset.tf-test-dataset.dataset_id
+    table_id   = "tf_test_bq_table%{random_suffix}"
+    schema     = <<EOF
+  [
+  {
+  "name": "entity_id",
+  "mode": "NULLABLE",
+  "type": "STRING",
+  "description": "Test default entity_id"
+  },
+  {
+  "name": "test_entity_column",
+  "mode": "NULLABLE",
+  "type": "STRING",
+  "description": "test secondary entity column"
+  },
+  {
+  "name": "feature_timestamp",
+  "mode": "NULLABLE",
+  "type": "TIMESTAMP",
+  "description": "Default timestamp value"
+  }
+  ]
+  EOF
+  }
+  
+  resource "google_vertex_ai_feature_online_store_featureview" "featureview" {
+    name   = "tf_test_fv%{random_suffix}"
+    region = "us-central1"
+    labels = {
+      foo1 = "bar1"
+    }
+    feature_online_store = google_vertex_ai_feature_online_store.featureonlinestore.name
+    sync_config {
+      cron = "0 4 * * *"
+    }
+    big_query_source {
+      uri               = "bq://${google_bigquery_table.tf-test-table.project}.${google_bigquery_table.tf-test-table.dataset_id}.${google_bigquery_table.tf-test-table.table_id}"
+      entity_id_columns = ["test_entity_column"]
+  
+    }
+  }
+  
+  data "google_project" "project" {
+    provider = google
+  }
 `, context)
 }
