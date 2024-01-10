1f3305df170fb84cd7bb8be8730d23cd3159b4d5
Thomas Rodgers <thomasrodgers@google.com>
2024-01-10 15:06:46 -0800

    Apply suggestions from code review

--- .ci/magician/cmd/generate_downstream.go
+++ .ci/magician/cmd/generate_downstream.go
@@ -72,14 +72,9 @@ func listGDEnvironmentVariables() string {
 	return result
 }
 
-func execGenerateDownstream(baseBranch, command, repo, version, ref string, gh GithubClient, rnr *exec.Runner, ctlr *source.Controller) {
+func execGenerateDownstream(baseBranch, command, repo, version, ref string, gh GithubClient, rnr ExecRunner, ctlr *source.Controller) {
 	if baseBranch == "" {
 		baseBranch = "main"
-	}
-	upstreamRepo, scratchRepo, err := cloneRepo(baseBranch, repo, version, ctlr)
-	if err != nil {
-		fmt.Println("Error cloning repo: ", err)
-		os.Exit(1)
 	}
 
 	mmLocalPath := filepath.Join(rnr.GetCWD(), "..", "..")
@@ -88,95 +83,98 @@ func execGenerateDownstream(baseBranch, command, repo, version, ref string, gh G
 		fmt.Println("Error copying magic modules: ", err)
 		os.Exit(1)
 	}
+	mmRepo := &source.Repo{
+		Name: "magic-modules",
+		Path: mmCopyPath,
+	}
+
+	downstreamRepo, scratchRepo, commitMessage, err := cloneRepo(mmRepo, baseBranch, repo, version, command, ref, rnr, ctlr)
+	if err != nil {
+		fmt.Println("Error cloning repo: ", err)
+		os.Exit(1)
+	}
 
 	if err := rnr.PushDir(mmCopyPath); err != nil {
 		fmt.Println("Error changing directory to copied magic modules: ", err)
 		os.Exit(1)
 	}
 
-	if err := setConfig(rnr); err != nil {
+	if err := setGitConfig(rnr); err != nil {
 		fmt.Println("Error setting config: ", err)
 		os.Exit(1)
 	}
 
-	mmRepo := &source.Repo{
-		Name: "magic-modules",
-		Path: mmCopyPath,
-	}
-	commitMessage, err := branchAndCommitMessage(mmRepo, scratchRepo, command, ref, rnr, ctlr)
-	if err != nil {
-		fmt.Println("Error getting branch and commit message: ", err)
-		os.Exit(1)
-	}
-
-	if err := runMake(upstreamRepo, command, rnr); err != nil {
+	if err := runMake(downstreamRepo, command, rnr); err != nil {
 		fmt.Println("Error running make: ", err)
 		os.Exit(1)
 	}
 
-	if err := pushCommit(upstreamRepo, scratchRepo, baseBranch, command, commitMessage, ref, gh, rnr, ctlr); err != nil {
-		fmt.Println("Error pushing commit: ", err)
-		os.Exit(1)
+	if command == "downstream" {
+		pullRequest, err := getPullRequest(baseBranch, ref, gh)
+		if err != nil {
+			fmt.Println("Error getting pull request: ", err)
+			os.Exit(1)
+		}
+
+		if err := pushCommit(downstreamRepo, scratchRepo, pullRequest, baseBranch, commitMessage, ref, gh, rnr, ctlr); err != nil {
+			fmt.Println("Error pushing commit: ", err)
+			os.Exit(1)
+		}
+
+		if err := mergePullRequest(downstreamRepo, scratchRepo, pullRequest, rnr, gh); err != nil {
+			fmt.Println("Error merging pull request: ", err)
+			os.Exit(1)
+		}
+	} else {
+		if _, err := rnr.Run("git", []string{"push", ctlr.URL(scratchRepo), scratchRepo.Branch, "-f"}, nil); err != nil {
+			fmt.Println("Error pushing to scratch repo: ", err)
+			os.Exit(1)
+		}
 	}
 }
 
-func cloneRepo(baseBranch, repo, version string, ctlr *source.Controller) (*source.Repo, *source.Repo, error) {
-	upstreamRepo := &source.Repo{
+func cloneRepo(mmRepo *source.Repo, baseBranch, repo, version, command, ref string, rnr ExecRunner, ctlr *source.Controller) (*source.Repo, *source.Repo, string, error) {
+	downstreamRepo := &source.Repo{
 		Title:  repo,
 		Branch: baseBranch,
 	}
 	switch repo {
 	case "terraform":
 		if version == "ga" {
-			upstreamRepo.Name = "terraform-provider-google"
-			upstreamRepo.Version = provider.GA
+			downstreamRepo.Name = "terraform-provider-google"
+			downstreamRepo.Version = provider.GA
 		} else if version == "beta" {
-			upstreamRepo.Name = "terraform-provider-google-beta"
-			upstreamRepo.Version = provider.Beta
+			downstreamRepo.Name = "terraform-provider-google-beta"
+			downstreamRepo.Version = provider.Beta
 		} else {
-			return nil, nil, fmt.Errorf("unrecognized version %s", version)
+			return nil, nil, "", fmt.Errorf("unrecognized version %s", version)
 		}
-		upstreamRepo.Owner = "hashicorp"
+		downstreamRepo.Owner = "hashicorp"
 	case "terraform-google-conversion":
-		upstreamRepo.Name = "terraform-google-conversion"
-		upstreamRepo.Owner = "GoogleCloudPlatform"
+		downstreamRepo.Name = "terraform-google-conversion"
+		downstreamRepo.Owner = "GoogleCloudPlatform"
 	case "tf-oics":
-		if upstreamRepo.Branch == "main" {
-			upstreamRepo.Branch = "master"
+		if downstreamRepo.Branch == "main" {
+			downstreamRepo.Branch = "master"
 		}
-		upstreamRepo.Name = "docs-examples"
-		upstreamRepo.Owner = "terraform-google-modules"
+		downstreamRepo.Name = "docs-examples"
+		downstreamRepo.Owner = "terraform-google-modules"
 	case "tf-cloud-docs":
 		fmt.Println(repo, " is no longer available.")
-		return nil, nil, nil
+		return nil, nil, "", nil
 	default:
-		return nil, nil, fmt.Errorf("unrecognized repo %s", repo)
+		return nil, nil, "", fmt.Errorf("unrecognized repo %s", repo)
 	}
-	ctlr.SetPath(upstreamRepo)
-	if err := ctlr.Clone(upstreamRepo); err != nil {
-		return nil, nil, err
+	ctlr.SetPath(downstreamRepo)
+	if err := ctlr.Clone(downstreamRepo); err != nil {
+		return nil, nil, "", err
 	}
 	scratchRepo := &source.Repo{
-		Name:    upstreamRepo.Name,
-		Branch:  baseBranch,
+		Name:    downstreamRepo.Name,
 		Owner:   "modular-magician",
-		Path:    upstreamRepo.Path,
-		Version: upstreamRepo.Version,
+		Path:    downstreamRepo.Path,
+		Version: downstreamRepo.Version,
 	}
-	return upstreamRepo, scratchRepo, nil
-}
-
-func setConfig(rnr *exec.Runner) error {
-	if _, err := rnr.Run("git", []string{"config", "--local", "user.name", "Modular Magician"}, nil); err != nil {
-		return err
-	}
-	if _, err := rnr.Run("git", []string{"config", "--local", "user.email", "magic-modules@google.com"}, nil); err != nil {
-		return err
-	}
-	return nil
-}
-
-func branchAndCommitMessage(mmRepo, scratchRepo *source.Repo, command, ref string, rnr *exec.Runner, ctlr *source.Controller) (string, error) {
 	var commitMessage string
 	switch command {
 	case "head":
@@ -187,7 +185,7 @@ func branchAndCommitMessage(mmRepo, scratchRepo *source.Repo, command, ref strin
 		// and the *left* side of it is the old main branch.
 		// the *right* side of it is the code to be merged.
 		if err := ctlr.Checkout(mmRepo, "HEAD~"); err != nil {
-			return "", err
+			return nil, nil, "", err
 		}
 		scratchRepo.Branch = fmt.Sprintf("auto-pr-%s-old", ref)
 		commitMessage = fmt.Sprintf("Old generated code for MM PR %s.", ref)
@@ -195,27 +193,37 @@ func branchAndCommitMessage(mmRepo, scratchRepo *source.Repo, command, ref strin
 		scratchRepo.Branch = "downstream-pr-" + ref
 		originalMessage, err := rnr.Run("git", []string{"log", "-1", "--pretty=%B", ref}, nil)
 		if err != nil {
-			return "", err
+			return nil, nil, "", err
 		}
 		commitMessage = fmt.Sprintf("%s\n[upstream:%s]", originalMessage, ref)
 	}
-	return commitMessage, nil
+	return downstreamRepo, scratchRepo, commitMessage, nil
 }
 
-func runMake(upstreamRepo *source.Repo, command string, rnr *exec.Runner) error {
-	switch upstreamRepo.Title {
+func setGitConfig(rnr ExecRunner) error {
+	if _, err := rnr.Run("git", []string{"config", "--local", "user.name", "Modular Magician"}, nil); err != nil {
+		return err
+	}
+	if _, err := rnr.Run("git", []string{"config", "--local", "user.email", "magic-modules@google.com"}, nil); err != nil {
+		return err
+	}
+	return nil
+}
+
+func runMake(downstreamRepo *source.Repo, command string, rnr ExecRunner) error {
+	switch downstreamRepo.Title {
 	case "terraform-google-conversion":
-		if _, err := rnr.Run("make", []string{"clean-tgc", "OUTPUT_PATH=" + upstreamRepo.Path}, nil); err != nil {
+		if _, err := rnr.Run("make", []string{"clean-tgc", "OUTPUT_PATH=" + downstreamRepo.Path}, nil); err != nil {
 			return err
 		}
-		if _, err := rnr.Run("make", []string{"tgc", "OUTPUT_PATH=" + upstreamRepo.Path}, nil); err != nil {
+		if _, err := rnr.Run("make", []string{"tgc", "OUTPUT_PATH=" + downstreamRepo.Path}, nil); err != nil {
 			return err
 		}
 		if command == "downstream" {
-			if err := rnr.PushDir(upstreamRepo.Path); err != nil {
+			if err := rnr.PushDir(downstreamRepo.Path); err != nil {
 				return err
 			}
-			if _, err := rnr.Run("go", []string{"get", "-d", "github.com/hashicorp/terraform-provider-google-beta@" + upstreamRepo.Branch}, nil); err != nil {
+			if _, err := rnr.Run("go", []string{"get", "-d", "github.com/hashicorp/terraform-provider-google-beta@" + downstreamRepo.Branch}, nil); err != nil {
 				return err
 			}
 			if _, err := rnr.Run("go", []string{"mod", "tidy"}, nil); err != nil {
@@ -229,25 +237,38 @@ func runMake(upstreamRepo *source.Repo, command string, rnr *exec.Runner) error 
 			}
 		}
 	case "tf-oics":
-		if _, err := rnr.Run("make", []string{"tf-oics", "OUTPUT_PATH=" + upstreamRepo.Path}, nil); err != nil {
+		if _, err := rnr.Run("make", []string{"tf-oics", "OUTPUT_PATH=" + downstreamRepo.Path}, nil); err != nil {
 			return err
 		}
 	case "terraform":
-		if _, err := rnr.Run("make", []string{"clean-provider", "OUTPUT_PATH=" + upstreamRepo.Path}, nil); err != nil {
+		if _, err := rnr.Run("make", []string{"clean-provider", "OUTPUT_PATH=" + downstreamRepo.Path}, nil); err != nil {
 			return err
 		}
-		if _, err := rnr.Run("make", []string{"provider", "OUTPUT_PATH=" + upstreamRepo.Path, fmt.Sprintf("VERSION=%s", upstreamRepo.Version)}, nil); err != nil {
+		if _, err := rnr.Run("make", []string{"provider", "OUTPUT_PATH=" + downstreamRepo.Path, fmt.Sprintf("VERSION=%s", downstreamRepo.Version)}, nil); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func pushCommit(upstreamRepo, scratchRepo *source.Repo, baseBranch, command, commitMessage, ref string, gh GithubClient, rnr *exec.Runner, ctlr *source.Controller) error {
+func getPullRequest(baseBranch, ref string, gh GithubClient) (*github.PullRequest, error) {
+	prs, err := gh.GetPullRequests("closed", baseBranch, "updated", "desc")
+	if err != nil {
+		return nil, err
+	}
+	for _, pr := range prs {
+		if pr.MergeCommitSha == ref {
+			return &pr, nil
+		}
+	}
+	return nil, fmt.Errorf("no pr found with merge commit sha %s and base branch %s", ref, baseBranch)
+}
+
+func pushCommit(downstreamRepo, scratchRepo *source.Repo, pullRequest *github.PullRequest, baseBranch, commitMessage, ref string, gh GithubClient, rnr ExecRunner, ctlr *source.Controller) error {
 	if err := rnr.PushDir(scratchRepo.Path); err != nil {
 		return err
 	}
-	if err := setConfig(rnr); err != nil {
+	if err := setGitConfig(rnr); err != nil {
 		return err
 	}
 
@@ -268,19 +289,7 @@ func pushCommit(upstreamRepo, scratchRepo *source.Repo, baseBranch, command, com
 		return err
 	}
 
-	prs, err := gh.GetPullRequests("closed", baseBranch, "updated", "desc")
-	if err != nil {
-		return err
-	}
-	var pullRequest github.PullRequest
-	for _, pr := range prs {
-		if pr.MergeCommitSha == ref {
-			pullRequest = pr
-			break
-		}
-	}
-
-	if command == "downstream" && upstreamRepo.Title == "terraform" {
+	if downstreamRepo.Title == "terraform" {
 		// Add the changelog entry.
 		rnr.Mkdir(".changelog")
 		if err := rnr.WriteFile(filepath.Join(".changelog", fmt.Sprintf("%d.txt", pullRequest.Number)), strings.Join(changelogExp.FindAllString(pullRequest.Body, -1), "\n")); err != nil {
@@ -298,40 +307,42 @@ func pushCommit(upstreamRepo, scratchRepo *source.Repo, baseBranch, command, com
 		return err
 	}
 
-	if command == "downstream" {
-		fmt.Printf(`Base: %s:%s
-Head: %s:%s
-`, upstreamRepo.Owner, upstreamRepo.Branch, scratchRepo.Owner, scratchRepo.Branch)
-		newPRURL, err := rnr.Run("hub", []string{
-			"pull-request",
-			"-b",
-			fmt.Sprintf("%s:%s",
-				upstreamRepo.Owner,
-				upstreamRepo.Branch),
-			"-h",
-			fmt.Sprintf("%s:%s",
-				scratchRepo.Owner,
-				scratchRepo.Branch),
-			"-m",
-			pullRequest.Title,
-			"-m",
-			pullRequest.Body,
-			"-m",
-			"Derived from " + pullRequest.HTMLUrl,
-		}, nil)
-		if err != nil {
-			return err
-		}
-		fmt.Println("Created PR ", newPRURL)
-		newPRURLParts := strings.Split(newPRURL, "/")
-		newPRNumber := strings.TrimSuffix(newPRURLParts[len(newPRURLParts)-1], "\n")
+	return nil
+}
 
-		// Wait a few seconds, then merge the PR.
-		time.Sleep(5 * time.Second)
-		fmt.Println("Merging PR ", newPRURL)
-		if err := gh.MergePullRequest(upstreamRepo.Owner, upstreamRepo.Name, newPRNumber); err != nil {
-			return err
-		}
+func mergePullRequest(downstreamRepo, scratchRepo *source.Repo, pullRequest *github.PullRequest, rnr ExecRunner, gh GithubClient) error {
+	fmt.Printf(`Base: %s:%s
+Head: %s:%s
+`, downstreamRepo.Owner, downstreamRepo.Branch, scratchRepo.Owner, scratchRepo.Branch)
+	newPRURL, err := rnr.Run("hub", []string{
+		"pull-request",
+		"-b",
+		fmt.Sprintf("%s:%s",
+			downstreamRepo.Owner,
+			downstreamRepo.Branch),
+		"-h",
+		fmt.Sprintf("%s:%s",
+			scratchRepo.Owner,
+			scratchRepo.Branch),
+		"-m",
+		pullRequest.Title,
+		"-m",
+		pullRequest.Body,
+		"-m",
+		"Derived from " + pullRequest.HTMLUrl,
+	}, nil)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Created PR ", newPRURL)
+	newPRURLParts := strings.Split(newPRURL, "/")
+	newPRNumber := strings.TrimSuffix(newPRURLParts[len(newPRURLParts)-1], "\n")
+
+	// Wait a few seconds, then merge the PR.
+	time.Sleep(5 * time.Second)
+	fmt.Println("Merging PR ", newPRURL)
+	if err := gh.MergePullRequest(downstreamRepo.Owner, downstreamRepo.Name, newPRNumber); err != nil {
+		return err
 	}
 	return nil
 }
--- .ci/magician/cmd/interfaces.go
+++ .ci/magician/cmd/interfaces.go
@@ -44,9 +44,11 @@ type CloudbuildClient interface {
 type ExecRunner interface {
 	GetCWD() string
 	Copy(src, dest string) error
+	Mkdir(path string) error
 	RemoveAll(path string) error
 	PushDir(path string) error
 	PopDir() error
+	WriteFile(name, data string) error
 	Run(name string, args []string, env map[string]string) (string, error)
 	MustRun(name string, args []string, env map[string]string) string
 }
