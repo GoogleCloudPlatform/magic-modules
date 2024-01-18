93fe4b199805c3c76e314f62a9cdd3a102c27a3f
Sarah French <sarah.french@hashicorp.com>
2024-01-18 14:42:15 +0000

    Add missing `ExternalProviders` config on acc test

--- mmv1/third_party/terraform/services/vmwareengine/data_source_google_vmwareengine_network_test.go
+++ mmv1/third_party/terraform/services/vmwareengine/data_source_google_vmwareengine_network_test.go
@@ -21,7 +21,10 @@ func TestAccDataSourceVmwareEngineNetwork_basic(t *testing.T) {
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckVmwareengineNetworkDestroyProducer(t),
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"time": {},
+		},
+		CheckDestroy: testAccCheckVmwareengineNetworkDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataSourceVmwareEngineNetworkConfig(context),
