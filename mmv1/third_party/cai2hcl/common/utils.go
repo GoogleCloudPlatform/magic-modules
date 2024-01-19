c59d5f6f30640fb8b9e3091238a9a07233b365b8
Amir Romashkin <romashkin@google.com>
2024-01-20 00:01:16 +0100

    Replace manual ComputeForwardingRuleConverter with a generated converter (#9755)
    * Add cai2hcl generated converters for 3 Compute resources.
    
    These are: BackendService, GlobalBackendService, ForwardingRule.
    
    This is the output of cai2hcl provider, commited as is
    without the generator Ruby code itself, as agreed with
    library owners.
    Reason: limited capacity of the supporting team to
    perform code reviews and the future need to port this
    solution to golang as a part of migration to go. As soon
    the staffing and/or migration is done, we will come back
    to implement the generated converters as
    originally planned.
    
    * Split current PR into 3 chunks:
    1. Refactor of shared files (Uber Converter)
    2. Add ForwardingRuleConverter (generated)
    3. Add BackendServiceConverters (generated)
    + ability to match converters by name
    
    This commit removes part2 and part3, leaving only part1 in this branch.
    
    * Fix versions after merge conflict
    
    * Remove auto-generated code header from FR
    
    * Remove unused util function
    
    * Fix merge conflict resolution issue
    
    * Remove unnecessary result map normalization (will needed later with generated converters)
    
    * Simplify cai2hcl by reworking UberConverter's as a single ConverterMap.
    
    1. Rename UberConverter to ConverterMap.
    2. Remove ConverterMap's from each API folder and replace them with a single top-level ConverterMap
    
    * Simplify converters
    
    * Flatten converter_map.go and use strings as converter names instead of constants.
    
    * Replace manual ComputeForwardingRuleConverter with a generated one.
    
    * Remove "AUTO GENERATED CODE" header
    
    * Rename tpg_transport to transport_tpg
    
    * Add unit test
    
    * Remove output normalization from ComputeInstance conversion.
    
    * Update docs
    
    * Cai2hcl: use MapToCtyValWithSchema for all converters
    
    + minor refactor of normalizeFlattenedObj()
    
    * Minor rename: flattenComputeForwardingRule
    
    * cai2hcl normalization: recurse over schema instead
    of object.
    
    * Add unit tests for MapToCtyValWithSchema
    
    * Formatting fix

--- mmv1/third_party/cai2hcl/common/utils.go
+++ mmv1/third_party/cai2hcl/common/utils.go
@@ -7,6 +7,7 @@ import (
 
 	hashicorpcty "github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	transport_tpg "github.com/hashicorp/terraform-provider-google-beta/google-beta/transport"
 	"github.com/zclconf/go-cty/cty"
 	ctyjson "github.com/zclconf/go-cty/cty/json"
 )
@@ -34,8 +35,14 @@ func DecodeJSON(data map[string]interface{}, v interface{}) error {
 	return nil
 }
 
-// MapToCtyValWithSchema converts resource from untyped map format to TF JSON.
+// MapToCtyValWithSchema normalizes and converts resource from untyped map format to TF JSON.
+//
+// Normalization is a post-processing of the output map, which does the following:
+// * Converts unmarshallable "schema.Set" to marshallable counterpart.
+// * Strips out properties, which are not part ofthe resource TF schema.
 func MapToCtyValWithSchema(m map[string]interface{}, s map[string]*schema.Schema) (cty.Value, error) {
+	m = normalizeFlattenedObj(m, s).(map[string]interface{})
+
 	b, err := json.Marshal(&m)
 	if err != nil {
 		return cty.NilVal, fmt.Errorf("error marshaling map as JSON: %v", err)
@@ -62,4 +69,60 @@ func hashicorpCtyTypeToZclconfCtyType(t hashicorpcty.Type) (cty.Type, error) {
 		return cty.NilType, err
 	}
 	return ret, nil
+}
+
+func NewConfig() *transport_tpg.Config {
+	return &transport_tpg.Config{}
+}
+
+// normalizeFlattenedObj traverses the output map recursively, removes fields which are
+// not a part of TF schema and converts unmarshallable "schema.Set" objects to arrays.
+func normalizeFlattenedObj(obj interface{}, schemaPerProp map[string]*schema.Schema) interface{} {
+	obj = convertToMarshallableObj(obj)
+
+	if schemaPerProp == nil {
+		// Schema for leaf nodes was already checked.
+		return obj
+	}
+
+	switch obj.(type) {
+	case map[string]interface{}:
+		objMap := obj.(map[string]interface{})
+		objMapNew := map[string]interface{}{}
+
+		for property, propertySchema := range schemaPerProp {
+			propertyValue := objMap[property]
+
+			switch propertySchema.Elem.(type) {
+			case *schema.Resource:
+				objMapNew[property] = normalizeFlattenedObj(propertyValue, propertySchema.Elem.(*schema.Resource).Schema)
+			case *schema.ValueType:
+			default:
+				objMapNew[property] = normalizeFlattenedObj(propertyValue, nil)
+			}
+		}
+		return objMapNew
+	case []interface{}:
+		arr := obj.([]interface{})
+		arrNew := make([]interface{}, len(arr))
+
+		for i := range arr {
+			arrNew[i] = normalizeFlattenedObj(arr[i], schemaPerProp)
+		}
+
+		return arrNew
+	default:
+		return obj
+	}
+}
+
+func convertToMarshallableObj(node interface{}) interface{} {
+	switch node.(type) {
+	case *schema.Set:
+		nodeSet := node.(*schema.Set)
+
+		return nodeSet.List()
+	default:
+		return node
+	}
 }
