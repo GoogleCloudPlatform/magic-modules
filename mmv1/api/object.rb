c28ac543ae3b81bb7a659a8714bbaa0dd4246a5a
Zhenhua Li <zhenhuali@google.com>
2024-01-19 10:41:23 -0800

    Remove Constant, SelfLink types and Api::Object class (#9842)
    * Remove Constant type
    
    * Remove Api::Object
    
    * Remove SelfLink type

--- mmv1/api/object.rb
+++ mmv1/api/object.rb
@@ -16,74 +16,67 @@ require 'google/logger'
 require 'google/yaml_validator'
 
 module Api
-  # Represents a base object
-  class Object < Google::YamlValidator
-    # Represents an object that has a (mandatory) name
-    class Named < Api::Object
-      # The list of properties (attr_reader) that can be overridden in
-      # <provider>.yaml.
-      module Properties
-        attr_reader :name
+  # Represents an object that has a (mandatory) name
+  class NamedObject < Google::YamlValidator
+    # The list of properties (attr_reader) that can be overridden in
+    # <provider>.yaml.
+    module Properties
+      attr_reader :name
+    end
+
+    def string_array?(arr)
+      types = arr.map(&:class).uniq
+      types.size == 1 && types[0] == String
+    end
+
+    def deep_merge(arr1, arr2)
+      # Scopes is an array of standard strings. In which case return the
+      # version in the overrides. This allows scopes to be removed rather
+      # than allowing for a merge of the two arrays
+      if string_array?(arr1)
+        return arr2.nil? ? arr1 : arr2
       end
 
-      def string_array?(arr)
-        types = arr.map(&:class).uniq
-        types.size == 1 && types[0] == String
+      # Merge any elements that exist in both
+      result = arr1.map do |el1|
+        other = arr2.select { |el2| el1.name == el2.name }.first
+        other.nil? ? el1 : el1.merge(other)
       end
 
-      def deep_merge(arr1, arr2)
-        # Scopes is an array of standard strings. In which case return the
-        # version in the overrides. This allows scopes to be removed rather
-        # than allowing for a merge of the two arrays
-        if string_array?(arr1)
-          return arr2.nil? ? arr1 : arr2
-        end
-
-        # Merge any elements that exist in both
-        result = arr1.map do |el1|
-          other = arr2.select { |el2| el1.name == el2.name }.first
-          other.nil? ? el1 : el1.merge(other)
-        end
-
-        # Add any elements of arr2 that don't exist in arr1
-        result + arr2.reject do |el2|
-          arr1.any? { |el1| el2.name == el1.name }
-        end
-      end
-
-      def merge(other)
-        result = self.class.new
-        instance_variables.each do |v|
-          result.instance_variable_set(v, instance_variable_get(v))
-        end
-
-        other.instance_variables.each do |v|
-          if other.instance_variable_get(v).instance_of?(Array)
-            result.instance_variable_set(v, deep_merge(result.instance_variable_get(v),
-                                                       other.instance_variable_get(v)))
-          else
-            result.instance_variable_set(v, other.instance_variable_get(v))
-          end
-        end
-
-        result
-      end
-
-      include Properties
-
-      # original value of :name before the provider override happens
-      # same as :name if not overridden in provider
-      attr_reader :api_name
-
-      def validate
-        super
-        check :name, type: String, required: true
-        check :api_name, type: String, default: @name
+      # Add any elements of arr2 that don't exist in arr1
+      result + arr2.reject do |el2|
+        arr1.any? { |el1| el2.name == el1.name }
       end
     end
 
-    def out_name
-      @name.underscore
+    def merge(other)
+      result = self.class.new
+      instance_variables.each do |v|
+        result.instance_variable_set(v, instance_variable_get(v))
+      end
+
+      other.instance_variables.each do |v|
+        if other.instance_variable_get(v).instance_of?(Array)
+          result.instance_variable_set(v, deep_merge(result.instance_variable_get(v),
+                                                     other.instance_variable_get(v)))
+        else
+          result.instance_variable_set(v, other.instance_variable_get(v))
+        end
+      end
+
+      result
+    end
+
+    include Properties
+
+    # original value of :name before the provider override happens
+    # same as :name if not overridden in provider
+    attr_reader :api_name
+
+    def validate
+      super
+      check :name, type: String, required: true
+      check :api_name, type: String, default: @name
     end
   end
 end
