c7d66dc533813abcc3fd2001241f18db43317131
Ray Xu <xrc@google.com>
2024-01-30 23:14:39 +0000

    remove redundant test

--- mmv1/third_party/terraform/services/discoveryengine/resource_discovery_engine_chat_engine_test.go
+++ mmv1/third_party/terraform/services/discoveryengine/resource_discovery_engine_chat_engine_test.go
@@ -39,43 +39,10 @@ func TestAccDiscoveryEngineChatEngine_discoveryengineChatengine_update(t *testin
 	})
 }
 
-func TestAccDiscoveryEngineChatEngine_discoveryengineChatengine_update_location(t *testing.T) {
-	t.Parallel()
-
-	context := map[string]interface{}{
-		"random_suffix": acctest.RandString(t, 10),
-	}
-
-	acctest.VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccDiscoveryEngineChatEngine_discoveryengineChatengine_update(context),
-			},
-			{
-				ResourceName:            "google_discovery_engine_chat_engine.primary",
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"chat_engine_config"},
-			},
-			{
-				Config: testAccDiscoveryEngineChatEngine_discoveryengineChatengine_update_location(context),
-			},
-			{
-				ResourceName:            "google_discovery_engine_chat_engine.primary",
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"chat_engine_config"},
-			},
-		},
-	})
-}
-
 func testAccDiscoveryEngineChatEngine_discoveryengineChatengine_basic(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 	resource "google_discovery_engine_data_store" "test_data_store" {
-		location                    = "global"
+		location                    = "eu"
 		data_store_id               = "tf-test-data-store-id%{random_suffix}"
 		display_name                = "tf-test-structured-datastore"
 		industry_vertical           = "GENERIC"
@@ -105,47 +72,6 @@ func testAccDiscoveryEngineChatEngine_discoveryengineChatengine_basic(context ma
 }
 
 func testAccDiscoveryEngineChatEngine_discoveryengineChatengine_update(context map[string]interface{}) string {
-	return acctest.Nprintf(`
-	resource "google_discovery_engine_data_store" "test_data_store" {
-		location                    = "global"
-		data_store_id               = "tf-test-data-store-id%{random_suffix}"
-		display_name                = "tf-test-structured-datastore"
-		industry_vertical           = "GENERIC"
-		content_config              = "NO_CONTENT"
-		solution_types              = ["SOLUTION_TYPE_CHAT"]
-	}
-
-	resource "google_discovery_engine_data_store" "test_data_store_2" {
-		location                    = google_discovery_engine_data_store.test_data_store.location
-		data_store_id               = "tf-test-data-store-2-id%{random_suffix}"
-		display_name                = "tf-test-structured-datastore"
-		industry_vertical           = "GENERIC"
-		content_config              = "NO_CONTENT"
-		solution_types              = ["SOLUTION_TYPE_CHAT"]
-	}
-
-	resource "google_discovery_engine_chat_engine" "primary" {
-		engine_id = "tf-test-chat-engine-id%{random_suffix}"
-		collection_id = "default_collection"
-		location = google_discovery_engine_data_store.test_data_store.location
-		display_name = "tf-test-chat-engine-name-2%{random_suffix}"
-		industry_vertical = "GENERIC"
-		data_store_ids = [google_discovery_engine_data_store.test_data_store.data_store_id, google_discovery_engine_data_store.test_data_store_2.data_store_id]
-		common_config {
-		  company_name = "test-company"
-		}
-		chat_engine_config {
-		  agent_creation_config {
-			business = "test business name"
-			default_language_code = "en"
-			time_zone = "America/Los_Angeles"
-		  }
-		}
-	}
-	`, context)
-}
-
-func testAccDiscoveryEngineChatEngine_discoveryengineChatengine_update_location(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 	resource "google_discovery_engine_data_store" "test_data_store" {
 		location                    = "eu"
@@ -180,7 +106,6 @@ func testAccDiscoveryEngineChatEngine_discoveryengineChatengine_update_location(
 			business = "test business name"
 			default_language_code = "en"
 			time_zone = "America/Los_Angeles"
-			location = google_discovery_engine_data_store.test_data_store.location
 		  }
 		}
 	}
