4d1302d60be61b171b3657ca10c1f0bcee2614b5
Bhargav Chereddy <bcreddy@google.com>
2024-02-02 21:24:52 +0000

    fix notebooks instances recreating for scopes and CMEK

---
+++ mmv1/templates/terraform/constants/notebooks_instance.go
@@ -0,0 +1,28 @@
+var NotebooksInstanceProvidedScopes = []string{
+	"https://www.googleapis.com/auth/cloud-platform",
+	"https://www.googleapis.com/auth/userinfo.email",
+}
+
+func NotebooksInstanceScopesDiffSuppress(_, _, _ string, d *schema.ResourceData) bool {
+  old, new := d.GetChange("service_account_scopes")
+	oldValue := old.([]interface{})
+	newValue := new.([]interface{})
+	oldValueList := []string{}
+	newValueList := []string{}
+
+	for _, item := range oldValue {
+		oldValueList = append(oldValueList,item.(string))
+	}
+
+	for _, item := range newValue {
+		newValueList = append(newValueList,item.(string))
+	}
+	newValueList= append(newValueList,NotebooksInstanceProvidedScopes...)
+
+	sort.Strings(oldValueList)
+	sort.Strings(newValueList)
+	if reflect.DeepEqual(oldValueList, newValueList) {
+		return true
+	}
+	return false
+}
