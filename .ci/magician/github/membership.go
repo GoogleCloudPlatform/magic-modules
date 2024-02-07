ce3731127f734e3f05e57a462b88c622a7bd10a4
Nick Elliot <nickelliot@google.com>
2024-02-07 14:26:15 -0800

    add NickElliot to vacation reviewers (#9942)
    * Update membership.go
    
    * Update membership.go

--- .ci/magician/github/membership.go
+++ .ci/magician/github/membership.go
@@ -49,6 +49,7 @@ var (
 	// This is for reviewers who are "on vacation": will not receive new review assignments but will still receive re-requests for assigned PRs.
 	onVacationReviewers = []string{
 		"zli82016",
+		"NickElliot",
 	}
 )
 
