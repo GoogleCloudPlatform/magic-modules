3e298951201d29a7be42bbedbf872ec5430ea7a8
Risham Chokshi <rishamchokshi@gmail.com>
2024-02-26 20:59:37 +0000

    adding roles for service accounts

--- mmv1/third_party/terraform/services/kms/resource_kms_ekm_connection_test.go
+++ mmv1/third_party/terraform/services/kms/resource_kms_ekm_connection_test.go
@@ -43,18 +43,6 @@ func TestAccKMSEkmConnection_kmsEkmConnectionBasicExample_update(t *testing.T) {
 
 func testAccKMSEkmConnection_kmsEkmConnectionBasicExample_full(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_kms_ekm_connection" "example-ekmconnection" {
-  name            	= "tf_test_ekmconnection_example%{random_suffix}"
-  location		= "us-central1"
-  key_management_mode 	= "MANUAL"
-  service_resolvers  	{
-      service_directory_service  = data.google_secret_manager_secret_version.servicedirectoryservice.secret_data
-      hostname 			 = data.google_secret_manager_secret_version.hostname.secret_data
-      server_certificates        {
-      		raw_der	= data.google_secret_manager_secret_version.raw_der.secret_data
-      	}
-    }
-}
 data "google_secret_manager_secret_version" "raw_der" {
   secret = "playground-cert"
   project = "315636579862"
@@ -66,6 +54,33 @@ data "google_secret_manager_secret_version" "hostname" {
 data "google_secret_manager_secret_version" "servicedirectoryservice" {
   secret = "external-servicedirectoryservice"
   project = "315636579862"
+}
+data "google_project" "vpc-project" {
+  project_id = "cloud-ekm-refekm-playground"
+}
+data "google_project" "project" {
+}
+resource "google_project_iam_member" "add_sdviewer" {
+  project = data.google_project.vpc-project.number
+  role    = "roles/servicedirectory.viewer"
+  member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-ekms.iam.gserviceaccount.com"
+}
+resource "google_project_iam_member" "add_pscAuthorizedService" {
+  project = data.google_project.vpc-project.number
+  role    = "roles/servicedirectory.pscAuthorizedService"
+  member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-ekms.iam.gserviceaccount.com"
+}
+resource "google_kms_ekm_connection" "example-ekmconnection" {
+  name            	= "tf_test_ekmconnection_example%{random_suffix}"
+  location		= "us-central1"
+  key_management_mode 	= "MANUAL"
+  service_resolvers  	{
+      service_directory_service  = data.google_secret_manager_secret_version.servicedirectoryservice.secret_data
+      hostname 			 = data.google_secret_manager_secret_version.hostname.secret_data
+      server_certificates        {
+      		raw_der	= data.google_secret_manager_secret_version.raw_der.secret_data
+      	}
+    }
 }
 `, context)
 }
