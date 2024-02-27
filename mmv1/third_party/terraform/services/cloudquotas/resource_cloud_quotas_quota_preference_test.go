2c57c14fceda9b68478a49313c7117e4ada66923
Lola Liu <liulola@google.com>
2024-02-27 13:55:09 -0800

    update yaml config

--- mmv1/third_party/terraform/services/cloudquotas/resource_cloud_quotas_quota_preference_test.go
+++ mmv1/third_party/terraform/services/cloudquotas/resource_cloud_quotas_quota_preference_test.go
@@ -1,3 +1,5 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
 package cloudquotas_test
 
 import (
@@ -13,23 +15,13 @@ func TestAccCloudQuotasQuotaPreference_cloudquotasQuotaPreferenceBasicExample_up
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"project":       envvar.GetTestProjectFromEnv(),
-		"random_suffix": acctest.RandString(t, 10),
+		"project": envvar.GetTestProjectFromEnv(),
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		Steps: []resource.TestStep{
-			{
-				Config: testAccCloudQuotasQuotaPreference_cloudquotasQuotaPreferenceBasicExample_basic(context),
-			},
-			{
-				ResourceName:            "google_cloud_quotas_quota_preference.preference",
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"parent", "quota_preference_id", "allow_missing", "validate_only", "ignore_safety_checks"},
-			},
 			{
 				Config: testAccCloudQuotasQuotaPreference_cloudquotasQuotaPreferenceBasicExample_update(context),
 			},
@@ -43,40 +35,19 @@ func TestAccCloudQuotasQuotaPreference_cloudquotasQuotaPreferenceBasicExample_up
 	})
 }
 
-func testAccCloudQuotasQuotaPreference_cloudquotasQuotaPreferenceBasicExample_basic(context map[string]interface{}) string {
-	return acctest.Nprintf(`
-resource "google_cloud_quotas_quota_preference" "preference" {
-  name          = "projects/%{project}/locations/global/quotaPreferences/tf-test-compute_googleapis_com-CPUS-per-project_us-central2%{random_suffix}"
-  quota_config  {
-    preferred_value = 210
-  }
-  dimensions = {
-    region = "us-central1"
-  }
-  service       = "compute.googleapis.com"
-  quota_id      = "CPUS-per-project-region"
-  contact_email = "liulola@google.com"
-  justification = "Increase quota for terraform testing."
-}
-`, context)
-}
-
 func testAccCloudQuotasQuotaPreference_cloudquotasQuotaPreferenceBasicExample_update(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_cloud_quotas_quota_preference" "preference" {
-  name          = "projects/%{project}/locations/global/quotaPreferences/tf-test-compute_CPUS-per-project_compute_googleapis_com-CPUS-per-project_us-east1%{random_suffix}"
+resource "google_cloud_quotas_quota_preference" "my-preference" {
+  name          = "projects/%{project}/locations/global/quotaPreferences/compute_googleapis_com-CPUS-per-project-us-central1"
   quota_config  {
     preferred_value = 200
   }
-  dimensions = {
-    region = "us-east1"
-  }
+  dimensions 	= { region = "us-central1" }
   service       = "compute.googleapis.com"
   quota_id      = "CPUS-per-project-region"
   contact_email = "liulola@google.com"
   justification = "Increase quota for terraform testing."
   validate_only = true
-  allow_missing = true
 }
 `, context)
 }
