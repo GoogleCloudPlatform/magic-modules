890be8d505593a719ba8094b1e843a0941d835ff
Thomas Rodgers <thomasrodgers@google.com>
2024-02-28 14:32:56 -0800

    Split github tokens (#9988)
    * Split github tokens
    
    * Update .ci/gcb-generate-diffs-new.yml
    
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    
    * Remove redundant downstreams token
    
    * Make diff processor use new token
    
    * Update path to markdown file
    
    * Replace GITHUB_TOKEN
    
    * Make github tokens optional for generate downstream
    
    * Allow either github token to be used
    
    * Replace GITHUB_TOKEN
    
    * Move environment variable lookup out of constructor
    
    * Update .ci/magician/vcr/tester.go
    
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    
    * Add downstream token
    
    * Make request reviewer use GITHUB_TOKEN and tgc integration use GITHUB_TOKEN_CLASSIC
    
    * Apply suggestions from code review
    
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    
    ---------
    
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>

--- .ci/magician/vcr/tester.go
+++ .ci/magician/vcr/tester.go
@@ -199,7 +199,7 @@ func (vt *Tester) Run(mode Mode, version provider.Version, testDirs []string) (*
 	}
 	var printedEnv string
 	for ev, val := range env {
-		if ev == "SA_KEY" || ev == "GITHUB_TOKEN" {
+		if ev == "SA_KEY" || strings.HasPrefix(ev, "GITHUB_TOKEN") {
 			val = "{hidden}"
 		}
 		printedEnv += fmt.Sprintf("%s=%s\n", ev, val)
