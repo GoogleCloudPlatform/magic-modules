653cb8cb8ced70f6fb5f2c57bad17f55e600ad1e
Sarah French <15078782+SarahFrench@users.noreply.github.com>
2024-03-05 11:14:28 +0000

    Address dependency issues in TestAccFirestoreField_* tests (#9957)
    * Add additional wait in TestAccFirestoreField_* tests
    
    * Boost wait in test to 6 minutes
    
    * Add dependency between database and service to control delete order
    
    * Update dependency to explicitly include project
    
    * Make firestore fields be removed from state when they're 'deleted'
    
    * Add `destroy_duration`
    
    * Remove from state after log line that uses id value
    
    * Update destory check to accept a 403 as valid
    
    * Remove unneeded changes in PR
    
    * Remove call to SetId

--- mmv1/third_party/terraform/services/firestore/resource_firestore_field_test.go
+++ mmv1/third_party/terraform/services/firestore/resource_firestore_field_test.go
@@ -103,7 +103,11 @@ resource "google_firestore_database" "database" {
 	location_id = "nam5"
 	type        = "FIRESTORE_NATIVE"
 
-	depends_on = [google_project_service.firestore]
+	# used to control delete order
+	depends_on = [
+		google_project_service.firestore,
+		google_project.project
+	]
 }
 `, context)
 	} else {
@@ -115,7 +119,7 @@ resource "google_firestore_database" "database" {
 	type        = "FIRESTORE_NATIVE"
 
 	delete_protection_state = "DELETE_PROTECTION_DISABLED"
-    deletion_policy         = "DELETE"
+	deletion_policy         = "DELETE"
 }
 `, context)
 	}
