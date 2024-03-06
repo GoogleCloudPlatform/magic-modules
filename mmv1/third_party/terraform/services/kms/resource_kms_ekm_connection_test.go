110a2649ca58347eeebd6fa08588bac1fd6bcd88
rishamchokshi <44479344+rishamchokshi@users.noreply.github.com>
2024-03-06 14:31:57 -0700

    Added service_resolvers as part of the update ekmconnection resource resource_kms_ekm_connection_test.go

--- mmv1/third_party/terraform/services/kms/resource_kms_ekm_connection_test.go
+++ mmv1/third_party/terraform/services/kms/resource_kms_ekm_connection_test.go
@@ -70,16 +70,6 @@ resource "google_project_iam_member" "add_pscAuthorizedService" {
   role    = "roles/servicedirectory.pscAuthorizedService"
   member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-ekms.iam.gserviceaccount.com"
 }
-resource "google_project_iam_member" "add_sdviewer_role" {
-  project = data.google_project.vpc-project.number
-  role    = "roles/servicedirectory.viewer"
-  member  = "serviceAccount:service-1067888929963@gcp-sa-ekms.iam.gserviceaccount.com"
-}
-resource "google_project_iam_member" "add_pscAuthorizedService_role" {
-  project = data.google_project.vpc-project.number
-  role    = "roles/servicedirectory.pscAuthorizedService"
-  member  = "serviceAccount:service-1067888929963@gcp-sa-ekms.iam.gserviceaccount.com"
-}
 resource "google_kms_ekm_connection" "example-ekmconnection" {
   name            	= "tf_test_ekmconnection_example%{random_suffix}"
   location		= "us-central1"
@@ -108,6 +98,18 @@ data "google_project" "vpc-project" {
 }
 data "google_project" "project" {
 }
+data "google_secret_manager_secret_version" "raw_der" {
+  secret = "playground-cert"
+  project = "315636579862"
+}
+data "google_secret_manager_secret_version" "hostname" {
+  secret = "external-uri"
+  project = "315636579862"
+}
+data "google_secret_manager_secret_version" "servicedirectoryservice" {
+  secret = "external-servicedirectoryservice"
+  project = "315636579862"
+}
 resource "google_project_iam_member" "add_sdviewer_updateekmconnection" {
   project = data.google_project.vpc-project.number
   role    = "roles/servicedirectory.viewer"
@@ -118,24 +120,19 @@ resource "google_project_iam_member" "add_pscAuthorizedService_updateekmconnecti
   role    = "roles/servicedirectory.pscAuthorizedService"
   member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-ekms.iam.gserviceaccount.com"
 }
-resource "google_project_iam_member" "add_sdviewer_role_updateekmconnection" {
-  project = data.google_project.vpc-project.number
-  role    = "roles/servicedirectory.viewer"
-  member  = "serviceAccount:service-1067888929963@gcp-sa-ekms.iam.gserviceaccount.com"
-}
-resource "google_project_iam_member" "add_pscAuthorizedService_role_updateekmconnection" {
-  project = data.google_project.vpc-project.number
-  role    = "roles/servicedirectory.pscAuthorizedService"
-  member  = "serviceAccount:service-1067888929963@gcp-sa-ekms.iam.gserviceaccount.com"
-}
 resource "google_kms_ekm_connection" "example-ekmconnection" {
   name            	= "tf_test_ekmconnection_example%{random_suffix}"
   location     		= "us-central1"
   key_management_mode 	= "CLOUD_KMS"
   crypto_space_path	= "v0/longlived/crypto-space-placeholder"
+  service_resolvers  	{
+      service_directory_service  = data.google_secret_manager_secret_version.servicedirectoryservice.secret_data
+      hostname 			 = data.google_secret_manager_secret_version.hostname.secret_data
+      server_certificates        {
+      		raw_der	= data.google_secret_manager_secret_version.raw_der.secret_data
+      	}
+  }
   depends_on = [
-  	google_project_iam_member.add_pscAuthorizedService_role_updateekmconnection,
-   	google_project_iam_member.add_sdviewer_role_updateekmconnection,
     	google_project_iam_member.add_pscAuthorizedService_updateekmconnection,
    	google_project_iam_member.add_sdviewer_updateekmconnection
    ]
