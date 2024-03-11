6e6abfb2059d8d38f8d2915aa4b039b1c2c9d294
Krishnan Gopal <krishgopal@google.com>
2024-03-11 16:12:45 +0000

    this change is unrelated to the feature

--- .ci/infra/terraform/main.tf
+++ .ci/infra/terraform/main.tf
@@ -135,12 +135,6 @@ resource "google_organization_iam_member" "sa_storage_admin" {
   member = google_service_account.sa.member
 }
 
-resource "google_organization_iam_member" "apphub_admin" {
-  org_id = data.google_organization.org.org_id
-  role               = "roles/apphub.admin"
-  member             = google_service_account.sa.member
-}
-
 resource "google_billing_account_iam_member" "sa_master_billing_admin" {
   billing_account_id = data.google_billing_account.master_acct.id
   role               = "roles/billing.admin"
