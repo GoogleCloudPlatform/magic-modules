334c512ae5b0ef56ce8794113193b0f8202cc7d7
Krishnan Gopal <krishgopal@google.com>
2024-03-12 21:57:38 +0000

    Batch update tests to improve speed

--- mmv1/third_party/terraform/services/apphub/resource_apphub_service_test.go
+++ mmv1/third_party/terraform/services/apphub/resource_apphub_service_test.go
@@ -37,16 +37,7 @@ func TestAccApphubService_serviceUpdate(t *testing.T) {
 				ImportStateVerifyIgnore: []string{"location", "application_id", "service_id"},
 			},
 			{
-				Config: testAccApphubService_apphubServiceUpdateCriticality(context),
-			},
-			{
-				ResourceName:            "google_apphub_service.example",
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"location", "application_id", "service_id"},
-			},
-			{
-				Config: testAccApphubService_apphubServiceUpdateOwners(context),
+				Config: testAccApphubService_apphubServiceUpdate(context),
 			},
 			{
 				ResourceName:            "google_apphub_service.example",
@@ -58,7 +49,7 @@ func TestAccApphubService_serviceUpdate(t *testing.T) {
 	})
 }
 
-func testAccApphubService_apphubServiceUpdateCriticality(context map[string]interface{}) string {
+func testAccApphubService_apphubServiceUpdate(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 resource "google_apphub_application" "application" {
   location = "us-east1"
@@ -110,163 +101,6 @@ resource "google_apphub_service" "example" {
   application_id = google_apphub_application.application.application_id
   service_id = google_compute_forwarding_rule.forwarding_rule.name
   discovered_service = data.google_apphub_discovered_service.catalog-service.name
-  display_name = "Example Service Full%{random_suffix}"
-  description = "Register service for testing%{random_suffix}"
-  attributes {
-    environment {
-      type = "STAGING"
-    }
-    criticality {  
-        type = "MEDIUM"
-    }
-    business_owners {
-        display_name =  "Alice%{random_suffix}"
-        email        =  "alice@google.com%{random_suffix}"
-    }
-    developer_owners {
-        display_name =  "Bob%{random_suffix}"
-        email        =  "bob@google.com%{random_suffix}"
-    }
-    operator_owners {
-        display_name =  "Charlie%{random_suffix}"
-        email        =  "charlie@google.com%{random_suffix}"
-    }
-  }
-}
-
-
-#creates service
-
-
-# VPC network
-resource "google_compute_network" "ilb_network" {
-  name                    = "tf-test-l7-ilb-network%{random_suffix}"
-  project                 = google_project.service_project.project_id
-  auto_create_subnetworks = false
-  depends_on = [time_sleep.wait_120s]
-}
-
-
-# backend subnet
-resource "google_compute_subnetwork" "ilb_subnet" {
-  name          = "tf-test-l7-ilb-subnet%{random_suffix}"
-  project       = google_project.service_project.project_id
-  ip_cidr_range = "10.0.1.0/24"
-  region        = "us-east1"
-  network       = google_compute_network.ilb_network.id
-}
-
-# forwarding rule
-resource "google_compute_forwarding_rule" "forwarding_rule" {
-  name                  ="tf-test-l7-ilb-forwarding-rule%{random_suffix}"
-  project               = google_project.service_project.project_id
-  region                = "us-east1"
-  ip_version            = "IPV4"
-  load_balancing_scheme = "INTERNAL"
-  all_ports             = true
-  backend_service       = google_compute_region_backend_service.backend.id
-  network               = google_compute_network.ilb_network.id
-  subnetwork            = google_compute_subnetwork.ilb_subnet.id
-}
-
-
-
-# backend service
-resource "google_compute_region_backend_service" "backend" {
-  name                  = "tf-test-l7-ilb-backend-subnet%{random_suffix}"
-  project               = google_project.service_project.project_id
-  region                = "us-east1"
-  health_checks         = [google_compute_health_check.default.id]
-}
-
-# health check
-resource "google_compute_health_check" "default" {
-  name     = "tf-test-l7-ilb-hc%{random_suffix}"
-  project  = google_project.service_project.project_id
-  check_interval_sec = 1
-  timeout_sec        = 1
-  tcp_health_check {
-    port = "80"
-  }
-  depends_on = [time_sleep.wait_120s]
-}
-`, context)
-}
-
-func testAccApphubService_apphubServiceUpdateOwners(context map[string]interface{}) string {
-	return acctest.Nprintf(`
-resource "google_apphub_application" "application" {
-  location = "us-east1"
-  application_id = "tf-test-example-application-1%{random_suffix}"
-  scope {
-    type = "REGIONAL"
-  }
-}
-
-resource "google_project" "service_project" {
-  project_id ="tf-test-project-1%{random_suffix}"
-  name = "Service Project"
-  org_id = "%{org_id}"
-  billing_account = "%{billing_account}"
-}
-
-# Enable Compute API
-resource "google_project_service" "compute_service_project" {
-  project = google_project.service_project.project_id
-  service = "compute.googleapis.com"
-}
-
-resource "time_sleep" "wait_120s" {
-  depends_on = [google_project_service.compute_service_project]
-
-  create_duration = "120s"
-}
-
-resource "google_apphub_service_project_attachment" "service_project_attachment" {
-  service_project_attachment_id = google_project.service_project.project_id
-  depends_on = [time_sleep.wait_120s]
-}
-
-# discovered service block
-data "google_apphub_discovered_service" "catalog-service" {
-  provider = google
-  location = "us-east1"
-  service_uri = "//compute.googleapis.com/${google_compute_forwarding_rule.forwarding_rule.id}"
-  depends_on = [google_apphub_service_project_attachment.service_project_attachment, time_sleep.wait_120s_for_resource_ingestion]
-}
-
-resource "time_sleep" "wait_120s_for_resource_ingestion" {
-  depends_on = [google_compute_forwarding_rule.forwarding_rule]
-  create_duration = "120s"
-}
-
-resource "google_apphub_service" "example" {
-  location = "us-east1"
-  application_id = google_apphub_application.application.application_id
-  service_id = google_compute_forwarding_rule.forwarding_rule.name
-  discovered_service = data.google_apphub_discovered_service.catalog-service.name
-  display_name = "Example Service Full%{random_suffix}"
-  description = "Register service for testing%{random_suffix}"
-  attributes {
-    environment {
-      type = "STAGING"
-    }
-    criticality {  
-        type = "MEDIUM"
-    }
-    business_owners {
-        display_name =  "Charlie%{random_suffix}"
-        email        =  "charlie@google.com%{random_suffix}"
-    }
-    developer_owners {
-        display_name =  "Bob%{random_suffix}"
-        email        =  "bob@google.com%{random_suffix}"
-    }
-    developer_owners {
-        display_name =  "Derek%{random_suffix}"
-        email        =  "derek@google.com%{random_suffix}"
-    }
-  }
 }
 
 
