087f4daee64c807a41246f9ab4f8d0424ac2104e
Ryan Oaks <ryanoaks@google.com>
2024-03-13 13:20:33 -0400

    Improve the error messages when mmv1 compilation fails (#10191)

--- mmv1/compiler.rb
+++ mmv1/compiler.rb
@@ -192,6 +192,9 @@ products_for_version = Parallel.map(all_product_files, in_processes: 8) do |prod
       )
       resource.validate
       resources.push(resource)
+    rescue StandardError => e
+      Google::LOGGER.error "Failed to compile #{file_path}: #{e}"
+      raise e
     end
 
     if override_dir
@@ -221,6 +224,9 @@ products_for_version = Parallel.map(all_product_files, in_processes: 8) do |prod
         )
         resource.validate
         resources.push(resource)
+      rescue StandardError => e
+        Google::LOGGER.error "Failed to compile using override #{override_path}: #{e}"
+        raise e
       end
     end
     resources = resources.sort_by(&:name)
