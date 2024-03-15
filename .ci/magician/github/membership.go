522c5334a0cbb9e9e2e04f33ee7d8ab442df3fc7
Sarah French <15078782+SarahFrench@users.noreply.github.com>
2024-03-15 14:46:45 +0000

    Add SarahFrench to on vacation list (#10189)

--- .ci/magician/github/membership.go
+++ .ci/magician/github/membership.go
@@ -46,7 +46,9 @@ var (
 	trustedContributors = []string{}
 
 	// This is for reviewers who are "on vacation": will not receive new review assignments but will still receive re-requests for assigned PRs.
-	onVacationReviewers = []string{}
+	onVacationReviewers = []string{
+		"SarahFrench",
+	}
 )
 
 type UserType int64
