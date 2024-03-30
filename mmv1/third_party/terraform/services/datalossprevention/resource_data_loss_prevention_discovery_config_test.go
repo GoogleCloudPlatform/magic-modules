f026df1d2d8faa196077f2a44c6a28b964813ea5
Patrick Moy <patrickmoy@google.com>
2024-03-29 17:42:01 -0700

    Add funcs for remaining examples for full coverage.

--- mmv1/third_party/terraform/services/datalossprevention/resource_data_loss_prevention_discovery_config_test.go
+++ mmv1/third_party/terraform/services/datalossprevention/resource_data_loss_prevention_discovery_config_test.go
@@ -80,3 +80,171 @@ resource "google_data_loss_prevention_discovery_config" "basic" {
 }
 `, context)
 }
+
+func testAccDataLossPreventionDiscoveryConfig_dlpDiscoveryConfigActions(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_data_loss_prevention_discovery_config" "basic" {
+	parent = "projects/%{project}"
+
+    targets {
+        big_query_target {
+            filter {
+                other_tables {}
+            }
+        }
+    }
+	actions {
+        export_data {
+            profile_table {
+                project_id = "project"
+                dataset_id = "dataset"
+                table_id = "table"
+            }
+        }
+    }
+    actions { 
+        pub_sub_notification {
+                topic = "fake-topic"
+                event = "NEW_PROFILE"
+                pub_sub_condition {
+                    expressions {
+                        logical_operator = "OR"
+                        conditions {
+                            minimum_risk_score = "HIGH"
+                            minimum_sensitivity_score = "HIGH"
+                        }
+                    }
+                }
+                detail_of_message = "TABLE_PROFILE"
+            }
+    }
+    inspect_templates = ["FAKE"]
+}
+`, context)
+}
+
+func testAccDataLossPreventionDiscoveryConfig_dlpDiscoveryConfigOrgRunning(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_data_loss_prevention_discovery_config" "basic" {
+	parent = "organizations/%{organization}"
+
+    targets {
+        big_query_target {
+            filter {
+                other_tables {}
+            }
+        }
+    }
+	org_config {
+		project_id = "%{project}"
+		location {
+			organization_id = "%{organization}"
+		}
+	}
+    inspect_templates = ["FAKE"]
+	status = "RUNNING"
+}
+`, context)
+}
+
+func testAccDataLossPreventionDiscoveryConfig_dlpDiscoveryConfigOrgFolderPaused(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_data_loss_prevention_discovery_config" "basic" {
+	parent = "organizations/%{organization}"
+
+    targets {
+        big_query_target {
+            filter {
+                other_tables {}
+            }
+        }
+    }
+	org_config {
+		project_id = "%{project}"
+		location {
+			folder_id = 123
+		}
+	}
+    inspect_templates = ["FAKE"]
+	status = "PAUSED"
+}
+`, context)
+}
+
+func testAccDataLossPreventionDiscoveryConfig_dlpDiscoveryConfigConditionsCadence(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_data_loss_prevention_discovery_config" "basic" {
+	parent = "projects/%{project}"
+
+	targets {
+        big_query_target {
+            filter {
+                other_tables {}
+            }
+            conditions {
+                type_collection = "BIG_QUERY_COLLECTION_ALL_TYPES"
+            }
+            cadence {
+                schema_modified_cadence {
+                    types = ["SCHEMA_NEW_COLUMNS"]
+                    frequency = "UPDATE_FREQUENCY_DAILY"
+                }
+                table_modified_cadence {
+                    types = ["TABLE_MODIFIED_TIMESTAMP"]
+                    frequency = "UPDATE_FREQUENCY_DAILY"
+                }
+            }
+        }
+    }
+    inspect_templates = ["FAKE_NEW"]
+}
+`, context)
+}
+
+func testAccDataLossPreventionDiscoveryConfig_dlpDiscoveryConfigFilterRegexesAndConditions(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_data_loss_prevention_discovery_config" "basic" {
+	parent = "projects/%{project}"
+
+	targets {
+        big_query_target {
+            filter {
+                tables {
+                    include_regexes {
+                        patterns {
+                            project_id_regex = ".*"
+                            dataset_id_regex = ".*"
+                            table_id_regex = ".*"
+                        }
+                    }
+                }
+            }
+            conditions {
+                created_after {
+                    seconds = 1
+                    nanos = 1
+                }
+                types {
+                    types = ["BIG_QUERY_TABLE_TYPE_TABLE", "BIG_QUERY_TABLE_TYPE_EXTERNAL_BIG_LAKE"]
+                }
+                or_conditions {
+                    min_row_count = 10
+                    min_age {
+                        seconds = 1
+                        nanos = 1
+                    }
+                }
+            }
+        }
+    }
+    targets {
+        big_query_target {
+            filter {
+                other_tables {}
+            }
+        }
+    }
+    inspect_templates = ["FAKE_NEW"]
+}
+`, context)
+}
