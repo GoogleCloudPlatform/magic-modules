a357029ba182c60646d9a4f0ffd5455344947e8b
Stephen Lewis <stephenrlewis@google.com>
2024-04-04 13:55:07 -0700

    Don't request reviewers if there are none to request.

--- .ci/magician/cmd/request_reviewer_test.go
+++ .ci/magician/cmd/request_reviewer_test.go
@@ -92,6 +92,9 @@ func TestExecRequestReviewer(t *testing.T) {
 
 			if tc.expectSpecificReviewers != nil {
 				assert.ElementsMatch(t, tc.expectSpecificReviewers, actualReviewers)
+				if len(tc.expectSpecificReviewers) == 0 {
+					assert.Len(t, gh.calledMethods["RequestPullRequestReviewers"], 0)
+				}
 			}
 			if tc.expectReviewersFromList != nil {
 				for _, reviewer := range actualReviewers {
