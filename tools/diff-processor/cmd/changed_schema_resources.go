36729a1b5d4e863209e5d25f8f82c38ab45f1ab5
Stephen Lewis (Burrows) <stephenrlewis@google.com>
2024-04-09 14:58:40 -0700

    Return affected resources from diff-processor instead of labels (#10311)

--- tools/diff-processor/cmd/changed_schema_resources.go
+++ tools/diff-processor/cmd/changed_schema_resources.go
@@ -10,33 +10,30 @@ import (
 	"os"
 
 	"github.com/GoogleCloudPlatform/magic-modules/tools/diff-processor/diff"
-	"github.com/GoogleCloudPlatform/magic-modules/tools/issue-labeler/labeler"
 	"github.com/spf13/cobra"
 	"golang.org/x/exp/maps"
 )
 
-const changedSchemaLabelsDesc = `Compute service labels based on schema changes.`
+const changedSchemaResourcesDesc = `Compute list of resources with changed schemas.`
 
-type changedSchemaLabelsOptions struct {
+type changedSchemaResourcesOptions struct {
 	rootOptions       *rootOptions
 	computeSchemaDiff func() diff.SchemaDiff
-	enrolledTeamsYaml []byte
 	stdout            io.Writer
 }
 
-func newChangedSchemaLabelsCmd(rootOptions *rootOptions) *cobra.Command {
-	o := &changedSchemaLabelsOptions{
+func newChangedSchemaResourcesCmd(rootOptions *rootOptions) *cobra.Command {
+	o := &changedSchemaResourcesOptions{
 		rootOptions: rootOptions,
 		computeSchemaDiff: func() diff.SchemaDiff {
 			return diff.ComputeSchemaDiff(oldProvider.ResourceMap(), newProvider.ResourceMap())
 		},
-		enrolledTeamsYaml: labeler.EnrolledTeamsYaml,
-		stdout:            os.Stdout,
+		stdout: os.Stdout,
 	}
 	cmd := &cobra.Command{
-		Use:   "changed-schema-labels",
-		Short: changedSchemaLabelsDesc,
-		Long:  changedSchemaLabelsDesc,
+		Use:   "changed-schema-resources",
+		Short: changedSchemaResourcesDesc,
+		Long:  changedSchemaResourcesDesc,
 		Args:  cobra.NoArgs,
 		RunE: func(c *cobra.Command, args []string) error {
 			return o.run()
@@ -44,17 +41,11 @@ func newChangedSchemaLabelsCmd(rootOptions *rootOptions) *cobra.Command {
 	}
 	return cmd
 }
-func (o *changedSchemaLabelsOptions) run() error {
+func (o *changedSchemaResourcesOptions) run() error {
 	schemaDiff := o.computeSchemaDiff()
 	affectedResources := maps.Keys(schemaDiff)
-	regexpLabels, err := labeler.BuildRegexLabels(o.enrolledTeamsYaml)
-	if err != nil {
-		return fmt.Errorf("Error building regex labels: %w", err)
-	}
 
-	labels := labeler.ComputeLabels(affectedResources, regexpLabels)
-
-	if err = json.NewEncoder(o.stdout).Encode(labels); err != nil {
+	if err := json.NewEncoder(o.stdout).Encode(affectedResources); err != nil {
 		return fmt.Errorf("Error encoding json: %w", err)
 	}
 
