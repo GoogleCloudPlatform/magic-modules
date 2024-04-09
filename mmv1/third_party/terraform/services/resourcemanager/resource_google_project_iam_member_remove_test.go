7be8fd32767fc13e8bd7b6be737005495f68c24c
Hao Nan Li <haonan@google.com>
2024-04-09 13:07:57 -0700

    Fix test

--- mmv1/third_party/terraform/services/resourcemanager/resource_google_project_iam_member_remove_test.go
+++ mmv1/third_party/terraform/services/resourcemanager/resource_google_project_iam_member_remove_test.go
@@ -12,28 +12,51 @@ import (
 func TestAccProjectIamMemberRemove_basic(t *testing.T) {
 	t.Parallel()
 
+	org := envar.GetTestOrgFromEnv(t)
 	project := envvar.GetTestProjectFromEnv()
 	role := "roles/editor"
-	members:= "default-gce-sa@example.com"
+	members:= "user:gterraformtest7@gmail.com"
+	random_suffix := acctest.RandString(t, 10)
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		CheckDestroy:             testAccCheckGoogleProjectIamCustomRoleDestroyProducer(t),
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"time": {},
+		},
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckGoogleProjectIamMemberRemove_basic(role, project, members),
+				Config: testAccCheckGoogleProjectIamMemberRemove_basic(project, name, org, project, project, members),
 			},
 		},
 	})
 }
 
-func testAccCheckGoogleProjectIamMemberRemove_basic(roleId, project, members string) string {
+func testAccCheckGoogleProjectIamMemberRemove_basic(project1, name, org, project2, project3, members string) string {
 	return fmt.Sprintf(`
+resource "google_project" "project" {
+  project_id = "tf-test%s"
+  name       = "tf-test%s"
+  org_id     = "%s"
+}
+
+resource "google_project_iam_binding" "bar" {
+  project = "%s"
+  members = ["user:gterraformtest1@gmail.com"]
+  role    = "roles/editor"
+}
+
+resource "time_sleep" "wait_20s" {
+  depends_on = [google_project_iam_binding.bar]
+  create_duration = "20s"
+}
+
 resource "google_project_iam_member_remove" "foo" {
   role     = "%s"
   project  = "%s"
   member  = "%s"
+  depends_on = [time_sleep.wait_20s]
 }
-`, roleId, project, members)
+`, project1, org, name, roleId, project2, members)
 }
