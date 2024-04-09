6f4506fb7e9ab16366301acc8f48981eb74bc7ff
Stephen Lewis <stephenrlewis@google.com>
2024-04-09 15:28:55 -0700

    Allowed modifying request headers via pre_create, pre_update, or pre_delete custom code

--- mmv1/third_party/terraform/transport/transport.go
+++ mmv1/third_party/terraform/transport/transport.go
@@ -24,12 +24,13 @@ type SendRequestOptions struct {
 	UserAgent            string
 	Body                 map[string]any
 	Timeout              time.Duration
+	Headers              http.Header
 	ErrorRetryPredicates []RetryErrorPredicateFunc
 	ErrorAbortPredicates []RetryErrorPredicateFunc
 }
 
 func SendRequest(opt SendRequestOptions) (map[string]interface{}, error) {
-	reqHeaders := make(http.Header)
+	reqHeaders := opt.Headers
 	reqHeaders.Set("User-Agent", opt.UserAgent)
 	reqHeaders.Set("Content-Type", "application/json")
 
