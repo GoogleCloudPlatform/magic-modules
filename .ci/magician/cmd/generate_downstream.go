cb330e8a4b874d190bd964606f630d4b5c615f25
Scott Suarez <ScottSuarez@google.com>
2024-04-18 13:00:51 -0700

    Pass commit SHA to merge pull request

--- .ci/magician/cmd/generate_downstream.go
+++ .ci/magician/cmd/generate_downstream.go
@@ -161,7 +161,7 @@ func execGenerateDownstream(baseBranch, command, repo, version, ref string, gh G
 	}
 
 	if commitErr == nil && command == "downstream" {
-		if err := mergePullRequest(downstreamRepo, scratchRepo, pullRequest, rnr, gh); err != nil {
+		if err := mergePullRequest(downstreamRepo, scratchRepo, pullRequest, rnr, gh, ctlr); err != nil {
 			fmt.Println("Error merging pull request: ", err)
 			os.Exit(1)
 		}
@@ -335,7 +335,7 @@ func addChangelogEntry(pullRequest *github.PullRequest, rnr ExecRunner) error {
 	return nil
 }
 
-func mergePullRequest(downstreamRepo, scratchRepo *source.Repo, pullRequest *github.PullRequest, rnr ExecRunner, gh GithubClient) error {
+func mergePullRequest(downstreamRepo, scratchRepo *source.Repo, pullRequest *github.PullRequest, rnr ExecRunner, gh GithubClient, ctlr *source.Controller) error {
 	fmt.Printf(`Base: %s:%s
 Head: %s:%s
 `, downstreamRepo.Owner, downstreamRepo.Branch, scratchRepo.Owner, scratchRepo.Branch)
@@ -363,10 +363,15 @@ Head: %s:%s
 	newPRURLParts := strings.Split(newPRURL, "/")
 	newPRNumber := strings.TrimSuffix(newPRURLParts[len(newPRURLParts)-1], "\n")
 
+	scratchCommitSha, err := ctlr.GetCommitSHA(scratchRepo)
+	if err != nil {
+		return err
+	}
+
 	// Wait a few seconds, then merge the PR.
 	time.Sleep(5 * time.Second)
 	fmt.Println("Merging PR ", newPRURL)
-	if err := gh.MergePullRequest(downstreamRepo.Owner, downstreamRepo.Name, newPRNumber); err != nil {
+	if err := gh.MergePullRequest(downstreamRepo.Owner, downstreamRepo.Name, newPRNumber, scratchCommitSha); err != nil {
 		return err
 	}
 	return nil
