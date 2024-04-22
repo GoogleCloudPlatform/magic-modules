3146511820ce47932defac4bb4d4396f40014a4a
Salome Papiashvili <spapiashvili@google.com>
2024-04-22 12:59:12 +0200

    add test for datasource

---
+++ mmv1/third_party/terraform/services/composer/data_source_google_composer_user_workloads_secret_test.go
@@ -0,0 +1,60 @@
+package composer_test
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+)
+
+func TestAccDataSourceComposerUserWorkloadsSecret_basic(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"env_name": fmt.Sprintf("%s-%d", testComposerEnvironmentPrefix, acctest.RandInt(t)),
+		"secret_name": fmt.Sprintf("%s-%d", testComposerUserWorkloadsSecretPrefix, acctest.RandInt(t))
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceComposerUserWorkloadsSecret_basic(context),
+				Check: resource.ComposeTestCheckFunc(
+					acctest.CheckDataSourceStateMatchesResourceState("data.google_composer_user_workloads_secret.test",
+						"google_composer_user_workloads_secret.test"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceComposerUserWorkloadsSecret_basic(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_composer_environment" "test" {
+	name   = "%{env_name}"
+	config {
+		software_config {
+			image_version = "composer-1-airflow-2"
+		}
+	}
+}
+resource "google_composer_user_workloads_secret" "test" {
+  environment = google_composer_environment.test.name
+  name = "%{secret_name}"
+  data = {
+    username: base64encode("username"),
+    password: base64encode("password"),
+  }
+}
+data "google_composer_user_workloads_secret" "test" {
+	name   = google_composer_user_workloads_secret.test.name
+	region = google_composer_environment.test.region
+}
+`, context)
+}
