b57ce32db1a9f9bb6f384915f3b76593b745945d
katiexzhang <katiexzhang@gmail.com>
2024-04-23 11:42:15 -0400

    Update resource_clouddeploy_custom_target_type_test.go to include cloud build repo option

--- mmv1/third_party/terraform/services/clouddeploy/resource_clouddeploy_custom_target_type_test.go
+++ mmv1/third_party/terraform/services/clouddeploy/resource_clouddeploy_custom_target_type_test.go
@@ -80,6 +80,14 @@ resource "google_clouddeploy_custom_target_type" "custom-target-type" {
           ref = "main"
         }
       }
+      include_skaffold_modules {
+        configs = ["my-config3"]
+        google_cloud_build_repo {
+          repository = "projects/777/locations/us-central1/connections/git/repositories/repo"
+          path = "configs/skaffold.yaml"
+          ref = "main"
+        }
+      }
     }
 }
 `, context)
