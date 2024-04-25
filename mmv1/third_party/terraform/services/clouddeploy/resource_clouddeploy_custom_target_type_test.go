25a35208530ff416cee37d6d6f56354f278bc226
katiexzhang <katiexzhang@gmail.com>
2024-04-25 14:40:46 -0400

    Update resource_clouddeploy_custom_target_type_test.go

--- mmv1/third_party/terraform/services/clouddeploy/resource_clouddeploy_custom_target_type_test.go
+++ mmv1/third_party/terraform/services/clouddeploy/resource_clouddeploy_custom_target_type_test.go
@@ -91,8 +91,11 @@ resource "google_clouddeploy_custom_target_type" "custom-target-type" {
       }
       include_skaffold_modules {
         configs = ["my-config3"]
-        google_cloud_build_repo {
-          repository = "projects/777/locations/us-central1/connections/git/repositories/repo"
+        google_cloud_build_repo_v2 {
+	  projectID = "777"
+	  region = "us-central1"
+	  connection = "git"
+	  repo = "repo"
           path = "configs/skaffold.yaml"
           ref = "main"
         }
