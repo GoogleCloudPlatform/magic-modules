055fd185bb49277bb2bccfbb172b28ca155ae8dd
Shuya Ma <shuyama@google.com>
2024-04-30 13:29:21 -0700

    update

--- mmv1/third_party/terraform/.teamcity/components/builds/build_triggers.kt
+++ mmv1/third_party/terraform/.teamcity/components/builds/build_triggers.kt
@@ -26,7 +26,8 @@ class NightlyTriggerConfiguration(
 fun Triggers.runNightly(config: NightlyTriggerConfiguration) {
 
     schedule{
-        enabled = config.nightlyTestsEnabled
+        // enabled = config.nightlyTestsEnabled
+        enabled = false
         branchFilter = "+:" + config.branch // returns "+:/refs/heads/main" if default
         triggerBuild = always() // Run build even if no new commits/pending changes
         withPendingChangesOnly = false
