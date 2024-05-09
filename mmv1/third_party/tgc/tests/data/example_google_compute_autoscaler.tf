ecf61a2abfd2cca6567debaf9b23d7d7e3259a88
Francisco Neto <delimaneto@google.com>
2024-05-09 19:05:47 +0000

    update tf file to fix unit tests

--- mmv1/third_party/tgc/tests/data/example_google_compute_autoscaler.tf
+++ mmv1/third_party/tgc/tests/data/example_google_compute_autoscaler.tf
@@ -51,7 +51,7 @@ resource "google_compute_instance_template" "foobar" {
   tags = ["foo", "bar"]
 
   disk {
-    source_image = data.google_compute_image.debian_9.id
+    source_image = "default"
   }
 
   network_interface {
@@ -83,9 +83,4 @@ resource "google_compute_instance_group_manager" "foobar" {
 
   target_pools       = [google_compute_target_pool.foobar.id]
   base_instance_name = "foobar"
-}
-
-data "google_compute_image" "debian_9" {
-  family  = "debian-11"
-  project = "debian-cloud"
 }
