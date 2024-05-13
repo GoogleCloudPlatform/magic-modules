16c06014ce51901cfd7a95d230c16aedd899eca3
Salome Papiashvili <spapiashvili@google.com>
2024-05-13 15:23:29 +0200

    cleanup

--- mmv1/third_party/terraform/services/composer/data_source_google_composer_user_workloads_config_map_test.go
+++ mmv1/third_party/terraform/services/composer/data_source_google_composer_user_workloads_config_map_test.go
@@ -1,13 +1,10 @@
 package composer_test
 
 import (
-	"errors"
 	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 )
 
@@ -33,46 +30,6 @@ func TestAccDataSourceComposerUserWorkloadsConfigMap_basic(t *testing.T) {
 		},
 	})
 }
-
-/*
-func checkConfigMapDataSourceMatchesResource() resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		ds, ok := s.RootModule().Resources["data.google_composer_user_workloads_config_map.test"]
-		if !ok {
-			return fmt.Errorf("can't find %s in state", "data.google_composer_user_workloads_config_map.test")
-		}
-		rs, ok := s.RootModule().Resources["google_composer_user_workloads_config_map.test"]
-		if !ok {
-			return fmt.Errorf("can't find %s in state", "google_composer_user_workloads_config_map.test")
-		}
-
-		dsAttr := ds.Primary.Attributes
-		rsAttr := rs.Primary.Attributes
-		errMsg := ""
-
-		for k := range rsAttr {
-			if k == "%" {
-				continue
-			}
-			// ignore diff if it's due to secrets being masked.
-			if strings.HasPrefix(k, "data.") && k != "data.%" {
-				if dsAttr[k] == "**********" {
-					continue
-				}
-			}
-			if dsAttr[k] != rsAttr[k] {
-				errMsg += fmt.Sprintf("%s is %s; want %s\n", k, dsAttr[k], rsAttr[k])
-			}
-		}
-
-		if errMsg != "" {
-			return errors.New(errMsg)
-		}
-
-		return nil
-	}
-}
-*/
 
 func testAccDataSourceComposerUserWorkloadsConfigMap_basic(context map[string]interface{}) string {
 	return acctest.Nprintf(`
