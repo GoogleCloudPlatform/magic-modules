9f1b69ae9a113107aae7b30600a3bf97ad48362c
Francisco Neto <delimaneto@google.com>
2024-05-15 14:23:41 +0000

    Add Suport for `google_dataflow_job` to TGC

--- mmv1/provider/terraform_tgc.rb
+++ mmv1/provider/terraform_tgc.rb
@@ -305,7 +305,9 @@ module Provider
                        ['converters/google/convert.go',
                         'third_party/tgc/convert.go'],
                        ['converters/google/convert_test.go',
-                        'third_party/tgc/convert_test.go']
+                        'third_party/tgc/convert_test.go'],
+                      ['converters/google/resources/job.go',
+                        'third_party/tgc/job.go']
                      ])
     end
 
