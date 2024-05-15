0fc294ed97365dc454334219e856f73140695ab4
samir-cit <samir@ciandt.com>
2024-05-15 12:01:49 -0300

    Basic test

---
+++ mmv1/third_party/terraform/services/networkservices/resource_network_services_service_lb_policies_test.go
@@ -0,0 +1,67 @@
+package networkservices_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccNetworkServicesLBPolicies_update(t *testing.T) {
+	t.Parallel()
+
+	gatewayName := fmt.Sprintf("tf-test-gateway-%s", acctest.RandString(t, 10))
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckNetworkServicesServiceLBPoliciesDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetworkServicesLBPolicies_basic(gatewayName),
+			},
+			{
+				ResourceName:            "google_network_services_service_lb_policies.foobar",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "terraform_labels"},
+			},
+			{
+				Config: testAccNetworkServicesLBPolicies_update(gatewayName),
+			},
+			{
+				ResourceName:            "google_network_services_service_lb_policies.foobar",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "terraform_labels"},
+			},
+		},
+	})
+}
+
+func testAccNetworkServicesLBPolicies_basic(gatewayName string) string {
+	return fmt.Sprintf(`
+resource "google_network_services_service_lb_policies" "foobar" {
+  name        = "%s"
+  location    = "global"
+  description = "my description"
+}
+`, gatewayName)
+}
+
+func testAccNetworkServicesLBPolicies_update(gatewayName string) string {
+	return fmt.Sprintf(`
+resource "google_network_services_service_lb_policies" "foobar" {
+  name                   = "%s"
+  location               = "global"
+  description            = "my description"
+  loadBalancingAlgorithm = "SPRAY_TO_REGION"
+  
+  labels = {
+    foo = "bar"
+  }
+}
+`, gatewayName)
+}
