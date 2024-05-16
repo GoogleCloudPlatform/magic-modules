6654761e34f9483d8323bf4922e83fbd9a83ed3e
Cameron Thornton <camthornton@google.com>
2024-05-16 09:02:51 -0500

    Check credentials JSON when applicable

--- mmv1/third_party/terraform/provider/universe/universe_domain_compute_test.go
+++ mmv1/third_party/terraform/provider/universe/universe_domain_compute_test.go
@@ -49,9 +49,6 @@ func TestAccDefaultUniverseDomainDisk(t *testing.T) {
 }
 
 func TestAccDefaultUniverseDomain_doesNotMatchExplicit(t *testing.T) {
-	// Need a way to mock credentials or only run in an environment with TPC creds
-	t.Skip()
-
 	universeDomainFake := "fakedomain.test"
 
 	acctest.VcrTest(t, resource.TestCase{
