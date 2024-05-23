dd6ec504ab50ae9046bc32bfb4e948e6cabe188e
Vijaykanth Melugiri <vijaykanthm@google.com>
2024-05-23 21:25:16 +0000

    Remove api from main.tf

--- .ci/infra/terraform/main.tf
+++ .ci/infra/terraform/main.tf
@@ -314,7 +314,6 @@ module "project-services" {
     "securesourcemanager.googleapis.com",
     "securetoken.googleapis.com",
     "securitycenter.googleapis.com",
-    "securitycentermanagement.googleapis.com",
     "securityposture.googleapis.com",
     "serviceconsumermanagement.googleapis.com",
     "servicecontrol.googleapis.com",
