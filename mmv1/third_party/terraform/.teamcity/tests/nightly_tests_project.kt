788bcaf9b42e037788592649be98084ef3d603dc
BBBmau <alvarez.mauriciotm@gmail.com>
2024-05-24 10:00:42 -0700

    add UTC in branch name

--- mmv1/third_party/terraform/.teamcity/tests/nightly_tests_project.kt
+++ mmv1/third_party/terraform/.teamcity/tests/nightly_tests_project.kt
@@ -41,7 +41,7 @@ class NightlyTestProjectsTests {
 
             // Check that nightly test is being ran on a nightly-test-* branch
             var isNightlyTestBranch: Boolean = false
-            if (schedulingTrigger.branchFilter == "+:refs/heads/nightly-test-*"){
+            if (schedulingTrigger.branchFilter == "+:UTC-nightly-test-*"){
                 isNightlyTestBranch = true
             }
             assertTrue("Build configuration `${bt.name}` is using the nightly-test branch;", isNightlyTestBranch)
