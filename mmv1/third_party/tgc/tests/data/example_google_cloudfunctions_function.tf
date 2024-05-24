0401ec9f8fc906401695d2508e843adb3f0b5601
Debora Ferreira <dfdossantos@google.com>
2024-05-24 14:07:15 +0000

    Add new fields for google_cloudfunctions_function to TGC

--- mmv1/third_party/tgc/tests/data/example_google_cloudfunctions_function.tf
+++ mmv1/third_party/tgc/tests/data/example_google_cloudfunctions_function.tf
@@ -1,19 +1,3 @@
-/**
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 terraform {
   required_providers {
     google = {
@@ -41,6 +25,8 @@ resource "google_cloudfunctions_function" "function" {
   labels = {
     my-label = "my-label-value"
   }
+  vpc_connector = "projects/tf-deployer-2"
+  vpc_connector_egress_settings = "ALL_TRAFFIC"
 
   environment_variables = {
     MY_ENV_VAR = "my-env-var-value"
