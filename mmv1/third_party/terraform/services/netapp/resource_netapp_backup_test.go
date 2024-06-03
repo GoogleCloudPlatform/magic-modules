6830d34384532e9efbb63d742ce5f3b8b58a0330
Roopkamal Tyagi <roopkamal@google.com>
2024-06-03 17:04:40 +0000

    Adding description,label fields in the google_netapp_backup test

--- mmv1/third_party/terraform/services/netapp/resource_netapp_backup_test.go
+++ mmv1/third_party/terraform/services/netapp/resource_netapp_backup_test.go
@@ -80,10 +80,15 @@ resource "google_netapp_volume_snapshot" "default" {
 
 resource "google_netapp_backup" "test_backup" {
   name = "tf-test-test-backup%{random_suffix}"
+  description = "This is a test backup"
   source_volume = google_netapp_volume.default.id
   location = google_netapp_backup_vault.default.location
   vault_name = google_netapp_backup_vault.default.name
   source_snapshot = google_netapp_volume_snapshot.default.id
+  labels = {
+	key= "test"
+	value= "backup"
+  }
 }
 `, context)
 }
