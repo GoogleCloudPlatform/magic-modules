fd38d8a357aa9c7d3acaad6da9e285e5e57302d5
Debora Ferreira <dfdossantos@google.com>
2024-06-04 19:25:48 +0000

    Fix test

--- mmv1/third_party/tgc/tests/data/instance.tf
+++ mmv1/third_party/tgc/tests/data/instance.tf
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 terraform {
   required_providers {
     google = {
@@ -11,7 +27,7 @@ provider "google" {
   {{if .Provider.credentials }}credentials = "{{.Provider.credentials}}"{{end}}
 }
 
-resource "google_compute_instance" "new-compute-instance" {
+resource "google_compute_instance" "my-test-instance"{
   name         = "my-compute-instance"
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
@@ -20,7 +36,7 @@ resource "google_compute_instance" "new-compute-instance" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "projects/debian-cloud/global/images/debian-8-jessie-v20170523"
     }
   }
 
