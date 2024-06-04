6e83b09855a922462a9717c9ac9fb0e2778ee37c
Vinod Thokala <vthokala@google.com>
2024-06-04 22:46:04 +0000

    removed undefined import file

--- mmv1/third_party/terraform/services/securitycenterv2/resource_scc_v2_notification_config_test.go
+++ mmv1/third_party/terraform/services/securitycenterv2/resource_scc_v2_notification_config_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
-	"github.com/hashicorp/terraform-provider-google/google/client"
 	"github.com/hashicorp/terraform-provider-google/google/envvar"
 	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
