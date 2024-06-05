11cc6ccbfca5c3f11d9ad70253258c00dd80df53
Vinod Thokala <vthokala@google.com>
2024-06-05 00:13:09 +0000

    source file

---
+++ mmv1/third_party/terraform/services/securitycenterv2/resource_scc_v2_organization_source_test.go
@@ -0,0 +1,66 @@
+package securitycenterv2_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
+)
+
+func TestAccSCCOrganizationSource_complete(t *testing.T) {
+	t.Parallel()
+
+	orgId := envvar.GetTestOrgFromEnv(t)
+	suffix := acctest.RandString(t, 10)
+	canonicalName := fmt.Sprintf("organizations/%s/sources/source-%s", orgId, suffix)
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.AccTestPreCheck(t)
+		},
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSCCOrganizationSourceCompleteExample(orgId, suffix, "My description", canonicalName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("google_scc_organization_source.custom_source", "display_name", fmt.Sprintf("TFSrc %s", suffix)),
+					resource.TestCheckResourceAttr("google_scc_organization_source.custom_source", "organization", orgId),
+					resource.TestCheckResourceAttr("google_scc_organization_source.custom_source", "description", "My description"),
+					resource.TestCheckResourceAttr("google_scc_organization_source.custom_source", "canonical_name", canonicalName),
+				),
+			},
+			{
+				ResourceName:      "google_scc_organization_source.custom_source",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccSCCOrganizationSourceCompleteExample(orgId, suffix, "", canonicalName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("google_scc_organization_source.custom_source", "display_name", fmt.Sprintf("TFSrc %s", suffix)),
+					resource.TestCheckResourceAttr("google_scc_organization_source.custom_source", "organization", orgId),
+					resource.TestCheckResourceAttr("google_scc_organization_source.custom_source", "description", ""),
+					resource.TestCheckResourceAttr("google_scc_organization_source.custom_source", "canonical_name", canonicalName),
+				),
+			},
+			{
+				ResourceName:      "google_scc_organization_source.custom_source",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccSCCOrganizationSourceCompleteExample(orgId, suffix, description, canonicalName string) string {
+	return fmt.Sprintf(`
+resource "google_scc_organization_source" "custom_source" {
+  display_name  = "TFSrc %s"
+  organization  = "%s"
+  description   = "%s"
+  canonical_name = "%s"
+}
+`, suffix, orgId, description, canonicalName)
+}
