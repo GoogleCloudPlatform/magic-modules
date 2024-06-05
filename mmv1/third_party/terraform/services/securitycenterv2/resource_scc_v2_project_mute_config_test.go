e5542efc78f52ca81591cce63fc63e03159a6f37
Vinod Thokala <vthokala@google.com>
2024-06-05 20:29:27 +0000

    removed unused imports

--- mmv1/third_party/terraform/services/securitycenterv2/resource_scc_v2_project_mute_config_test.go
+++ mmv1/third_party/terraform/services/securitycenterv2/resource_scc_v2_project_mute_config_test.go
@@ -7,10 +7,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 	"github.com/hashicorp/terraform-provider-google/google/envvar"
-	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
-	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
-	"google.golang.org/api/securitycenter/v1"
-
 )
 
 func TestAccSecurityCenterV2ProjectMuteConfig_basic(t *testing.T) {
