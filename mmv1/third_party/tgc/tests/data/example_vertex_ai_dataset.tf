25adaa277197d5b8011cd34fcd6b54cac1e739c3
Danilo Fonseca <danilofonseca@google.com>
2024-06-05 11:32:20 +0000

    solve pr comments

--- mmv1/third_party/tgc/tests/data/example_vertex_ai_dataset.tf
+++ mmv1/third_party/tgc/tests/data/example_vertex_ai_dataset.tf
@@ -11,6 +11,7 @@ provider "google" {
   {{if .Provider.credentials }}credentials = "{{.Provider.credentials}}"{{end}}
 }
 
+
 resource "google_vertex_ai_dataset" "dataset" {
   display_name          = "terraform"
   metadata_schema_uri   = "gs://google-cloud-aiplatform/schema/dataset/metadata/image_1.0.0.yaml"
