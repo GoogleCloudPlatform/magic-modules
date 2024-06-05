46a71c54ca8710440aec9b9a0cb171bb0e72f7ab
Vijaykanth Melugiri <vijaykanthm@google.com>
2024-06-05 18:50:09 +0000

    Fix the test case

--- mmv1/third_party/terraform/services/securitycentermanagement/resource_scc_management_organization_project_security_health_analytics_custom_module_test.go
+++ mmv1/third_party/terraform/services/securitycentermanagement/resource_scc_management_organization_project_security_health_analytics_custom_module_test.go
@@ -15,40 +15,40 @@ import (
 
 // Custom Module tests cannot be run in parallel without running into 409 Conflict reponses.
 // Run them as individual steps of an update test instead.
-func TestAccSecurityCenterManagementOrganizationProjectSecurityHealthAnalyticsCustomModule(t *testing.T) {
+func TestAccSecurityCenterManagementProjectSecurityHealthAnalyticsCustomModule(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
 		"random_suffix": acctest.RandString(t, 10),
-		"location":      "us-central1",
+		"location":      "global",
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckSecurityCenterManagementOrganizationProjectSecurityHealthAnalyticsCustomModuleDestroyProducer(t),
+		CheckDestroy:             testAccCheckSecurityCenterManagementProjectSecurityHealthAnalyticsCustomModuleDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckSecurityCenterManagementOrganizationProjectSecurityHealthAnalyticsCustomModule_sccProjectCustomModuleBasicExample(context),
+				Config: testAccCheckSecurityCenterManagementProjectSecurityHealthAnalyticsCustomModule_sccProjectCustomModuleBasicExample(context),
 			},
 			{
-				ResourceName:      "google_scc_management_organization_project_security_health_analytics_custom_module.example",
+				ResourceName:      "google_scc_management_project_security_health_analytics_custom_module.example",
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
 			{
-				Config: testAccCheckSecurityCenterManagementOrganizationProjectSecurityHealthAnalyticsCustomModule_sccProjectCustomModuleFullExample(context),
+				Config: testAccCheckSecurityCenterManagementProjectSecurityHealthAnalyticsCustomModule_sccProjectCustomModuleFullExample(context),
 			},
 			{
-				ResourceName:      "google_scc_management_organization_project_security_health_analytics_custom_module.example",
+				ResourceName:      "google_scc_management_project_security_health_analytics_custom_module.example",
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
 			{
-				Config: testAccCheckSecurityCenterManagementOrganizationProjectSecurityHealthAnalyticsCustomModule_sccProjectCustomModuleUpdate(context),
+				Config: testAccCheckSecurityCenterManagementProjectSecurityHealthAnalyticsCustomModule_sccProjectCustomModuleUpdate(context),
 			},
 			{
-				ResourceName:      "google_scc_management_organization_project_security_health_analytics_custom_module.example",
+				ResourceName:      "google_scc_management_project_security_health_analytics_custom_module.example",
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
@@ -56,9 +56,9 @@ func TestAccSecurityCenterManagementOrganizationProjectSecurityHealthAnalyticsCu
 	})
 }
 
-func testAccCheckSecurityCenterManagementOrganizationProjectSecurityHealthAnalyticsCustomModule_sccProjectCustomModuleBasicExample(context map[string]interface{}) string {
+func testAccCheckSecurityCenterManagementProjectSecurityHealthAnalyticsCustomModule_sccProjectCustomModuleBasicExample(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_scc_management_organization_project_security_health_analytics_custom_module" "example" {
+resource "google_scc_management_project_security_health_analytics_custom_module" "example" {
 	display_name = "tf_test_basic_custom_module%{random_suffix}"
 	enablement_state = "ENABLED"
 	location = "%{location}"
@@ -79,9 +79,9 @@ resource "google_scc_management_organization_project_security_health_analytics_c
 `, context)
 }
 
-func testAccCheckSecurityCenterManagementOrganizationProjectSecurityHealthAnalyticsCustomModule_sccProjectCustomModuleFullExample(context map[string]interface{}) string {
+func testAccCheckSecurityCenterManagementProjectSecurityHealthAnalyticsCustomModule_sccProjectCustomModuleFullExample(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_scc_management_organization_project_security_health_analytics_custom_module" "example" {
+resource "google_scc_management_project_security_health_analytics_custom_module" "example" {
 	display_name = "tf_test_full_custom_module%{random_suffix}"
 	enablement_state = "ENABLED"
 	location = "%{location}"
@@ -116,9 +116,9 @@ resource "google_scc_management_organization_project_security_health_analytics_c
 `, context)
 }
 
-func testAccCheckSecurityCenterManagementOrganizationProjectSecurityHealthAnalyticsCustomModule_sccProjectCustomModuleUpdate(context map[string]interface{}) string {
+func testAccCheckSecurityCenterManagementProjectSecurityHealthAnalyticsCustomModule_sccProjectCustomModuleUpdate(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_scc_management_organization_project_security_health_analytics_custom_module" "example" {
+resource "google_scc_management_project_security_health_analytics_custom_module" "example" {
 	location = "%{location}"
 	display_name = "full_custom_module"
 	enablement_state = "DISABLED"
@@ -153,10 +153,10 @@ resource "google_scc_management_organization_project_security_health_analytics_c
 `, context)
 }
 
-func testAccCheckSecurityCenterManagementOrganizationProjectSecurityHealthAnalyticsCustomModuleDestroyProducer(t *testing.T) func(s *terraform.State) error {
+func testAccCheckSecurityCenterManagementProjectSecurityHealthAnalyticsCustomModuleDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
-			if rs.Type != "google_scc_project_custom_module" {
+			if rs.Type != "google_scc_management_project_security_health_analytics_custom_module" {
 				continue
 			}
 			if strings.HasPrefix(name, "data.") {
@@ -187,7 +187,7 @@ func testAccCheckSecurityCenterManagementOrganizationProjectSecurityHealthAnalyt
 				UserAgent: config.UserAgent,
 			})
 			if err == nil {
-				return fmt.Errorf("SecurityCenterManagementOrganizationProjectSecurityHealthAnalyticsCustomModule still exists at %s", url)
+				return fmt.Errorf("SecurityCenterManagementProjectSecurityHealthAnalyticsCustomModule still exists at %s", url)
 			}
 		}
 
