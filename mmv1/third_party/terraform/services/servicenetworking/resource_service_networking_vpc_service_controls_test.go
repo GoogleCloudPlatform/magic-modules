8b229aad9bcbc8f0cb64067a5b0b3bd42767f13b
Julio Castillo <jccb@google.com>
2024-06-05 13:19:47 +0200

    Add Service Networking VPC Service Controls resource.

---
+++ mmv1/third_party/terraform/services/servicenetworking/resource_service_networking_vpc_service_controls_test.go
@@ -0,0 +1,74 @@
+package servicenetworking_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+)
+
+func TestAccServiceNetworkingVPCServiceControls_update(t *testing.T) {
+	t.Parallel()
+
+	network := fmt.Sprintf("tf-test-example-network%s", acctest.RandString(t, 10))
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccServiceNetworkingVPCServiceControls_full(network, "true"),
+			},
+			{
+				ResourceName:            "google_service_networking_vpc_service_controls.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"network", "project", "service"},
+			},
+			{
+				Config: testAccServiceNetworkingVPCServiceControls_full(network, "false"),
+			},
+			{
+				ResourceName:            "google_service_networking_vpc_service_controls.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"network", "project", "service"},
+			},
+		},
+	})
+}
+
+func testAccServiceNetworkingVPCServiceControls_full(network, enabled string) string {
+	return fmt.Sprintf(`
+# Create a VPC
+resource "google_compute_network" "default" {
+  name = "%s"
+}
+
+# Create an IP address
+resource "google_compute_global_address" "default" {
+  name          = "psa-range"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.default.id
+}
+
+# Create a private connection
+resource "google_service_networking_connection" "default" {
+  network                 = google_compute_network.default.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.default.name]
+}
+
+# Enable VPC-SC on the producer network
+resource "google_service_networking_vpc_service_controls" "default" {
+  network    = google_compute_network.default.name
+  service    = "servicenetworking.googleapis.com"
+  enabled    = %s
+  depends_on = [google_service_networking_connection.default]
+}
+`, network, enabled)
+}
