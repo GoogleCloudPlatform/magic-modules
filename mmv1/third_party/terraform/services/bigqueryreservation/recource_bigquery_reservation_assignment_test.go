808bdccaba999b40372191afc36948961f4e7177
Obada Alabbadi <oabbadi@google.com>
2024-06-06 13:13:38 +0000

    fix bigquery assignment

--- mmv1/third_party/terraform/services/bigqueryreservation/recource_bigquery_reservation_assignment_test.go
+++ mmv1/third_party/terraform/services/bigqueryreservation/recource_bigquery_reservation_assignment_test.go
@@ -1,21 +1,3 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
-// ----------------------------------------------------------------------------
-//
-//     ***     AUTO GENERATED CODE    ***    Type: DCL     ***
-//
-// ----------------------------------------------------------------------------
-//
-//     This file is managed by Magic Modules (https://github.com/GoogleCloudPlatform/magic-modules)
-//     and is based on the DCL (https://github.com/GoogleCloudPlatform/declarative-resource-client-library).
-//     Changes will need to be made to the DCL or Magic Modules instead of here.
-//
-//     We are not currently able to accept contributions to this file. If changes
-//     are required, please file an issue at https://github.com/hashicorp/terraform-provider-google/issues/new/choose
-//
-// ----------------------------------------------------------------------------
-
 package bigqueryreservation_test
 
 import (
