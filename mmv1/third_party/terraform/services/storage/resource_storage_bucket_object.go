4a354f80e30b1aa1c5e1a883665ae71005db2d6c
Maarc-D <escasgorille@aol.com>
2024-06-07 08:15:03 +0200

    fix(storage_bucket_object): make detect_md5 computed

--- mmv1/third_party/terraform/services/storage/resource_storage_bucket_object.go
+++ mmv1/third_party/terraform/services/storage/resource_storage_bucket_object.go
@@ -117,8 +117,8 @@ func ResourceStorageBucketObject() *schema.Resource {
 
 			// Detect changes to local file or changes made outside of Terraform to the file stored on the server.
 			"detect_md5hash": {
-				Type: schema.TypeString,
-				// This field is not Computed because it needs to trigger a diff.
+				Type:     schema.TypeString,
+				Computed: true,
 				Optional: true,
 				// Makes the diff message nicer:
 				// detect_md5hash:       "1XcnP/iFw/hNrbhXi7QTmQ==" => "different hash" (forces new resource)
