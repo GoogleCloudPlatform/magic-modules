3dd586c8cb4b1e12479eed07b40eee58f46f9d70
Vijaykanth Melugiri <vijaykanthm@google.com>
2024-06-14 05:57:36 +0000

    Add Resource SCC V2 Project Nofitification Config

---
+++ mmv1/third_party/terraform/services/securitycenterv2/resource_scc_v2_project_notification_config_test.go
@@ -0,0 +1,65 @@
+package securitycenterv2_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
+)
+
+func TestAccSecurityCenterV2ProjectNotificationConfig_updateStreamingConfigFilter(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"project_id":    envvar.GetTestProjectFromEnv(t),
+		"location":      "global",
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckSecurityCenterV2ProjectNotificationConfigDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecurityCenterV2ProjectNotificationConfig_sccV2ProjectNotificationConfigBasicExample(context),
+			},
+			{
+				ResourceName:            "google_scc_v2_project_notification_config.custom_notification_config",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"project", "config_id"},
+			},
+			{
+				Config: testAccSecurityCenterV2ProjectNotificationConfig_updateStreamingConfigFilter(context),
+			},
+			{
+				ResourceName:            "google_scc_v2_project_notification_config.custom_notification_config",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"project", "config_id"},
+			},
+		},
+	})
+}
+
+func testAccSecurityCenterV2ProjectNotificationConfig_updateStreamingConfigFilter(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_pubsub_topic" "scc_v2_project_notification" {
+  name = "tf-test-my-topic%{random_suffix}"
+}
+
+resource "google_scc_v2_project_notification_config" "custom_notification_config" {
+  config_id    = "tf-test-my-config%{random_suffix}"
+  project      = "%{project_id}"
+  description  = "My custom Cloud Security Command Center Finding Notification Configuration"
+  pubsub_topic =  google_pubsub_topic.scc_v2_project_notification.id
+  location     = "global"
+
+  streaming_config {
+    filter = "category = \"OPEN_FIREWALL\""
+  }
+}
+`, context)
+}
