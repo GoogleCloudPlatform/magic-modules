3636e12b491d290ccb15a1df7b0e4ad33789bc3e
Sarah French <sarah.french@hashicorp.com>
2024-06-20 14:00:13 +0100

    Update google_vmwareengine_cluster sweeper to find all parent resources (private clouds) and look for and delete clusters in them

--- mmv1/third_party/terraform/services/vmwareengine/resource_vmwareengine_cluster_sweeper.go
+++ mmv1/third_party/terraform/services/vmwareengine/resource_vmwareengine_cluster_sweeper.go
@@ -2,6 +2,7 @@ package vmwareengine
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"strings"
 	"testing"
@@ -55,76 +56,119 @@ func testSweepVmwareengineCluster(region string) error {
 			},
 		}
 
-		listTemplate := strings.Split("https://vmwareengine.googleapis.com/v1/{{parent}}/clusters", "?")[0]
-		listUrl, err := tpgresource.ReplaceVars(d, config, listTemplate)
+		log.Printf("[INFO][SWEEPER_LOG] looking for parent resources in location '%s'.", location)
+		privateCloudNames, err := listPrivateCloudsInLocation(d, config)
 		if err != nil {
-			log.Printf("[INFO][SWEEPER_LOG] error preparing sweeper list url: %s", err)
+			log.Printf("[INFO][SWEEPER_LOG] error finding parental resources in location %s: %s", location, err)
 			continue
 		}
+		for _, parent := range privateCloudNames {
 
-		res, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
-			Config:    config,
-			Method:    "GET",
-			Project:   config.Project,
-			RawURL:    listUrl,
-			UserAgent: config.UserAgent,
-		})
-		if err != nil {
-			log.Printf("[INFO][SWEEPER_LOG] Error in response from request %s: %s", listUrl, err)
-			continue
-		}
+			// `parent` will be string of form projects/my-project/locations/us-central1-a/privateClouds/my-cloud
+			listUrl := fmt.Sprintf("https://vmwareengine.googleapis.com/v1/projects/%s/clusters", parent)
 
-		resourceList, ok := res["clusters"]
-		if !ok {
-			log.Printf("[INFO][SWEEPER_LOG] Nothing found in response.")
-			continue
-		}
-
-		rl := resourceList.([]interface{})
-
-		log.Printf("[INFO][SWEEPER_LOG] Found %d items in %s list response.", len(rl), resourceName)
-		// Keep count of items that aren't sweepable for logging.
-		nonPrefixCount := 0
-		for _, ri := range rl {
-			obj := ri.(map[string]interface{})
-			if obj["name"] == nil {
-				log.Printf("[INFO][SWEEPER_LOG] %s resource name was nil", resourceName)
-				continue
-			}
-
-			name := tpgresource.GetResourceNameFromSelfLink(obj["name"].(string))
-			// Skip resources that shouldn't be sweeped
-			if !sweeper.IsSweepableTestResource(name) {
-				nonPrefixCount++
-				continue
-			}
-
-			deleteTemplate := "https://vmwareengine.googleapis.com/v1/{{parent}}/clusters/{{name}}"
-			deleteUrl, err := tpgresource.ReplaceVars(d, config, deleteTemplate)
-			if err != nil {
-				log.Printf("[INFO][SWEEPER_LOG] error preparing delete url: %s", err)
-				continue
-			}
-			deleteUrl = deleteUrl + name
-
-			// Don't wait on operations as we may have a lot to delete
-			_, err = transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
+			res, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
 				Config:    config,
-				Method:    "DELETE",
+				Method:    "GET",
 				Project:   config.Project,
-				RawURL:    deleteUrl,
+				RawURL:    listUrl,
 				UserAgent: config.UserAgent,
 			})
 			if err != nil {
-				log.Printf("[INFO][SWEEPER_LOG] Error deleting for url %s : %s", deleteUrl, err)
-			} else {
-				log.Printf("[INFO][SWEEPER_LOG] Sent delete request for %s resource: %s", resourceName, name)
+				log.Printf("[INFO][SWEEPER_LOG] Error in response from request %s: %s", listUrl, err)
+				continue
 			}
-		}
 
-		if nonPrefixCount > 0 {
-			log.Printf("[INFO][SWEEPER_LOG] %d items were non-sweepable and skipped.", nonPrefixCount)
+			resourceList, ok := res["clusters"]
+			if !ok {
+				log.Printf("[INFO][SWEEPER_LOG] Nothing found in response.")
+				continue
+			}
+
+			rl := resourceList.([]interface{})
+
+			log.Printf("[INFO][SWEEPER_LOG] Found %d items in %s list response.", len(rl), resourceName)
+			// Keep count of items that aren't sweepable for logging.
+			nonPrefixCount := 0
+			for _, ri := range rl {
+				obj := ri.(map[string]interface{})
+				if obj["name"] == nil {
+					log.Printf("[INFO][SWEEPER_LOG] %s resource name was nil", resourceName)
+					continue
+				}
+
+				name := tpgresource.GetResourceNameFromSelfLink(obj["name"].(string))
+				// Skip resources that shouldn't be sweeped
+				if !sweeper.IsSweepableTestResource(name) {
+					nonPrefixCount++
+					continue
+				}
+
+				deleteTemplate := "https://vmwareengine.googleapis.com/v1/{{parent}}/clusters/{{name}}"
+				deleteUrl, err := tpgresource.ReplaceVars(d, config, deleteTemplate)
+				if err != nil {
+					log.Printf("[INFO][SWEEPER_LOG] error preparing delete url: %s", err)
+					continue
+				}
+				deleteUrl = deleteUrl + name
+
+				// Don't wait on operations as we may have a lot to delete
+				_, err = transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
+					Config:    config,
+					Method:    "DELETE",
+					Project:   config.Project,
+					RawURL:    deleteUrl,
+					UserAgent: config.UserAgent,
+				})
+				if err != nil {
+					log.Printf("[INFO][SWEEPER_LOG] Error deleting for url %s : %s", deleteUrl, err)
+				} else {
+					log.Printf("[INFO][SWEEPER_LOG] Sent delete request for %s resource: %s", resourceName, name)
+				}
+			}
+
+			if nonPrefixCount > 0 {
+				log.Printf("[INFO][SWEEPER_LOG] %d items were non-sweepable and skipped.", nonPrefixCount)
+			}
 		}
 	}
 	return nil
+}
+
+func listPrivateCloudsInLocation(d *tpgresource.ResourceDataMock, config *transport_tpg.Config) ([]string, error) {
+	listTemplate := strings.Split("https://vmwareengine.googleapis.com/v1/projects/{{project}}/locations/{{location}}/privateClouds", "?")[0]
+	listUrl, err := tpgresource.ReplaceVars(d, config, listTemplate)
+	if err != nil {
+		log.Printf("[INFO][SWEEPER_LOG] error preparing sweeper list url: %s", err)
+		return nil, err
+	}
+
+	res, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
+		Config:    config,
+		Method:    "GET",
+		Project:   config.Project,
+		RawURL:    listUrl,
+		UserAgent: config.UserAgent,
+	})
+	if err != nil {
+		log.Printf("[INFO][SWEEPER_LOG] Error in response from request %s: %s", listUrl, err)
+		return nil, err
+	}
+
+	resourceList, ok := res["privateClouds"]
+	if !ok {
+		log.Printf("[INFO][SWEEPER_LOG] Nothing found in response.")
+		return nil, fmt.Errorf("nothing found in response")
+	}
+
+	rl := resourceList.([]interface{})
+	privateCloudNames := []string{}
+	for _, r := range rl {
+		resource := r.(map[string]interface{})
+		if name, ok := resource["name"]; ok {
+			privateCloudNames = append(privateCloudNames, name.(string))
+		}
+
+	}
+	return privateCloudNames, nil
 }
