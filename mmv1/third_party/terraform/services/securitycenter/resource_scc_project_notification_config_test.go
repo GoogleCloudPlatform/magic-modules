b0d753eddebbd7623c758a5870a6a3d8b39acd9d
Vijaykanth Melugiri <vijaykanthm@google.com>
2024-06-24 06:00:32 +0000

    Address the errors

--- mmv1/third_party/terraform/services/securitycenter/resource_scc_project_notification_config_test.go
+++ mmv1/third_party/terraform/services/securitycenter/resource_scc_project_notification_config_test.go
@@ -12,7 +12,7 @@ func TestAccSecurityCenterProjectNotificationConfig_updateStreamingConfigFilter(
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"project_id":    envvar.GetTestProjectFromEnv(t),
+		"project_id":    envvar.GetTestProjectFromEnv(),
 		"random_suffix": acctest.RandString(t, 10),
 	}
 
@@ -28,7 +28,7 @@ func TestAccSecurityCenterProjectNotificationConfig_updateStreamingConfigFilter(
 				ResourceName:            "google_scc_project_notification_config.custom_notification_config",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"project", "config_id"},
+				ImportStateVerifyIgnore: []string{"projectId", "config_id"},
 			},
 			{
 				Config: testAccSecurityCenterProjectNotificationConfig_updateStreamingConfigFilter(context),
@@ -37,7 +37,7 @@ func TestAccSecurityCenterProjectNotificationConfig_updateStreamingConfigFilter(
 				ResourceName:            "google_scc_project_notification_config.custom_notification_config",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"project", "config_id"},
+				ImportStateVerifyIgnore: []string{"projectId", "config_id"},
 			},
 		},
 	})
@@ -51,7 +51,7 @@ resource "google_pubsub_topic" "scc_project_notification" {
 
 resource "google_scc_project_notification_config" "custom_notification_config" {
   config_id    = "tf-test-my-config%{random_suffix}"
-  project      = "%{project_id}"
+  projectId      = "%{project_id}"
   description  = "My custom Cloud Security Command Center Finding Notification Configuration"
   pubsub_topic =  google_pubsub_topic.scc_project_notification.id
 
