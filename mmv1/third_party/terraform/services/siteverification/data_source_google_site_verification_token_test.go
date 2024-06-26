5789f36976759b0cc4230a82db88f59f1b55549e
Chris Hawk <hawk@google.com>
2024-06-25 18:04:47 -0700

    Temporarily remove the site test

--- mmv1/third_party/terraform/services/siteverification/data_source_google_site_verification_token_test.go
+++ mmv1/third_party/terraform/services/siteverification/data_source_google_site_verification_token_test.go
@@ -10,61 +10,6 @@ import (
 	"github.com/hashicorp/terraform-provider-google/google/envvar"
 )
 
-func TestAccSiteVerificationToken_siteverificationTokenSite(t *testing.T) {
-	t.Parallel()
-
-	serviceAccount := envvar.GetTestServiceAccountFromEnv(t)
-	targetServiceAccountEmail := acctest.BootstrapServiceAccount(t, "siteverify", serviceAccount)
-
-	context := map[string]interface{}{
-		"site":    "https://www.example.com",
-		"account": targetServiceAccountEmail,
-	}
-
-	acctest.VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccSiteVerificationToken_siteverificationTokenSite(context),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet("data.google_site_verification_token.site_meta", "token"),
-					resource.TestCheckResourceAttr("data.google_site_verification_token.site_meta", "type", "SITE"),
-					resource.TestCheckResourceAttr("data.google_site_verification_token.site_meta", "identifier", context["site"].(string)),
-					resource.TestCheckResourceAttr("data.google_site_verification_token.site_meta", "verification_method", "META"),
-				),
-			},
-		},
-	})
-}
-
-func testAccSiteVerificationToken_siteverificationTokenSite(context map[string]interface{}) string {
-	return acctest.Nprintf(`
-data "google_service_account_access_token" "impersonated" {
-  target_service_account = "%{account}"
-  scopes = [
-    "https://www.googleapis.com/auth/siteverification.verify_only",
-    "https://www.googleapis.com/auth/cloud-platform",
-    "https://www.googleapis.com/auth/userinfo.email",
-  ]
-  lifetime = "300s"
-}
-
-provider "google" {
-  alias                 = "impersonated"
-  user_project_override = true
-  access_token          = data.google_service_account_access_token.impersonated.access_token
-}
-
-data "google_site_verification_token" "site_meta" {
-  provider            = google.impersonated
-  type                = "SITE"
-  identifier          = "%{site}"
-  verification_method = "META"
-}
-`, context)
-}
-
 func TestAccSiteVerificationToken_siteverificationTokenDomain(t *testing.T) {
 	t.Parallel()
 
