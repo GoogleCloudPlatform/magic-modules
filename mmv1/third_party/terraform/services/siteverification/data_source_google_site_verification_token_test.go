2bf450334930d77b05d921ddf4d83901441a7d4c
Chris Hawk <hawk@google.com>
2024-06-28 14:10:03 -0700

    Throw in the towel and use provider scopes

--- mmv1/third_party/terraform/services/siteverification/data_source_google_site_verification_token_test.go
+++ mmv1/third_party/terraform/services/siteverification/data_source_google_site_verification_token_test.go
@@ -7,18 +7,58 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
-	"github.com/hashicorp/terraform-provider-google/google/envvar"
 )
+
+func TestAccSiteVerificationToken_siteverificationTokenSite(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"site": "https://www.example.com",
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSiteVerificationToken_siteverificationTokenSite(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.google_site_verification_token.site_meta", "token"),
+					resource.TestCheckResourceAttr("data.google_site_verification_token.site_meta", "type", "SITE"),
+					resource.TestCheckResourceAttr("data.google_site_verification_token.site_meta", "identifier", context["site"].(string)),
+					resource.TestCheckResourceAttr("data.google_site_verification_token.site_meta", "verification_method", "META"),
+				),
+			},
+		},
+	})
+}
+
+func testAccSiteVerificationToken_siteverificationTokenSite(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+provider "google" {
+  alias                 = "scoped"
+  user_project_override = true
+  scopes = [
+    "https://www.googleapis.com/auth/siteverification",
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/userinfo.email",
+  ]
+}
+
+data "google_site_verification_token" "site_meta" {
+  provider            = google.scoped
+  type                = "SITE"
+  identifier          = "%{site}"
+  verification_method = "META"
+}
+`, context)
+}
 
 func TestAccSiteVerificationToken_siteverificationTokenDomain(t *testing.T) {
 	t.Parallel()
 
-	serviceAccount := envvar.GetTestServiceAccountFromEnv(t)
-	targetServiceAccountEmail := acctest.BootstrapServiceAccount(t, "siteverify", serviceAccount)
-
 	context := map[string]interface{}{
-		"domain":  "www.example.com",
-		"account": targetServiceAccountEmail,
+		"domain": "www.example.com",
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
@@ -43,24 +83,18 @@ func TestAccSiteVerificationToken_siteverificationTokenDomain(t *testing.T) {
 
 func testAccSiteVerificationToken_siteverificationTokenDomain(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-data "google_service_account_access_token" "impersonated" {
-  target_service_account = "%{account}"
+provider "google" {
+  alias                 = "scoped"
+  user_project_override = true
   scopes = [
-    "https://www.googleapis.com/auth/siteverification.verify_only",
+    "https://www.googleapis.com/auth/siteverification",
     "https://www.googleapis.com/auth/cloud-platform",
     "https://www.googleapis.com/auth/userinfo.email",
   ]
-  lifetime = "300s"
-}
-
-provider "google" {
-  alias                 = "impersonated"
-  user_project_override = true
-  access_token          = data.google_service_account_access_token.impersonated.access_token
 }
 
 data "google_site_verification_token" "dns_text" {
-  provider            = google.impersonated
+  provider            = google.scoped
   type                = "INET_DOMAIN"
   identifier          = "%{domain}"
   verification_method = "DNS_TXT"
