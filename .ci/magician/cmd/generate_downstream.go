7563d148c30c68759ed0aaec2e2ca6c32b546eed
Shuya Ma <shuyama@google.com>
2024-07-02 11:32:50 -0700

    only check if commit exists in sync branches in downstream builds

--- .ci/magician/cmd/generate_downstream.go
+++ .ci/magician/cmd/generate_downstream.go
@@ -97,23 +97,24 @@ func execGenerateDownstream(baseBranch, command, repo, version, ref string, gh G
 	if baseBranch == "" {
 		baseBranch = "main"
 	}
-
-	var syncBranchPrefix string
-	if repo == "terraform" {
-		if version == "beta" {
-			syncBranchPrefix = "tpgb-sync"
-		} else if version == "ga" {
-			syncBranchPrefix = "tpg-sync"
+	if command == "downstream" {
+		var syncBranchPrefix string
+		if repo == "terraform" {
+			if version == "beta" {
+				syncBranchPrefix = "tpgb-sync"
+			} else if version == "ga" {
+				syncBranchPrefix = "tpg-sync"
+			}
+		} else if repo == "terraform-google-conversion" {
+			syncBranchPrefix = "tgc-sync"
+		} else if repo == "tf-oics" {
+			syncBranchPrefix = "tf-oics-sync"
 		}
-	} else if repo == "terraform-google-conversion" {
-		syncBranchPrefix = "tgc-sync"
-	} else if repo == "tf-oics" {
-		syncBranchPrefix = "tf-oics-sync"
-	}
-	syncBranch := getSyncBranch(syncBranchPrefix, baseBranch)
-	if syncBranchHasCommit(ref, syncBranch, rnr) {
-		fmt.Printf("Sync branch %s already has commit %s, skipping generation\n", syncBranch, ref)
-		os.Exit(0)
+		syncBranch := getSyncBranch(syncBranchPrefix, baseBranch)
+		if syncBranchHasCommit(ref, syncBranch, rnr) {
+			fmt.Printf("Sync branch %s already has commit %s, skipping generation\n", syncBranch, ref)
+			os.Exit(0)
+		}
 	}
 
 	mmLocalPath := filepath.Join(rnr.GetCWD(), "..", "..")
