7027630857883a5fdfd1767b74151ce23fe2fcd5
Vijaykanth Melugiri <vijaykanthm@google.com>
2024-07-25 05:48:16 +0000

    Add flag to ignore project property

--- mmv1/third_party/terraform/services/securitycenterv2/resource_scc_v2_project_notification_config_test.go
+++ mmv1/third_party/terraform/services/securitycenterv2/resource_scc_v2_project_notification_config_test.go
@@ -19,7 +19,7 @@ func TestAccSecurityCenterV2ProjectNotificationConfig_updateStreamingConfigFilte
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),		
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccSecurityCenterV2ProjectNotificationConfig_sccV2ProjectNotificationConfigBasicExample(context),
