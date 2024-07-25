e015a985e4a8e7dd0c008da06bd0e986f843fa99
BBBmau <alvarez.mauriciotm@gmail.com>
2024-07-24 19:31:55 -0700

    remove checkDestroyer()

--- mmv1/third_party/terraform/services/vertexai/resource_vertex_ai_index_endpoint_deployed_index_test.go
+++ mmv1/third_party/terraform/services/vertexai/resource_vertex_ai_index_endpoint_deployed_index_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 )
 
-func TestAccVertexAIIndexEndpointDeployedIndex_updated(t *testing.T) {
+func TestAccVertexAIIndexEndpointDeployedIndex_mutated(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
@@ -18,7 +18,7 @@ func TestAccVertexAIIndexEndpointDeployedIndex_updated(t *testing.T) {
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckVertexAIIndexEndpointDestroyProducer(t),
+		CheckDestroy:             testAccCheckVertexAIIndexEndpointDeployedIndexDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccVertexAIIndexEndpointDeployedIndex_basic(context),
