d0e6aa4338be9c95cee957c05ae811c682d5b266
Jialei Chen <jialeichen@google.com>
2024-07-31 20:29:27 +0000

    Delete the *DestroyProducer function.

--- mmv1/third_party/terraform/services/discoveryengine/resource_discovery_engine_data_store_test.go
+++ mmv1/third_party/terraform/services/discoveryengine/resource_discovery_engine_data_store_test.go
@@ -17,7 +17,6 @@ func TestAccDiscoveryEngineDataStore_discoveryengineDatastoreBasicExample_update
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckDiscoveryEngineDataStoreDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDiscoveryEngineDataStore_discoveryengineDatastoreBasicExample_basic(context),
