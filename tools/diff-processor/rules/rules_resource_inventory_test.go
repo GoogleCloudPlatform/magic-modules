c0299ff1ecd4a6850fac7566591139004a10bbb9
Stephen Lewis (Burrows) <stephenrlewis@google.com>
2024-08-01 16:04:32 -0700

    Simplified breaking changes logic (#11307)

--- tools/diff-processor/rules/rules_resource_inventory_test.go
+++ tools/diff-processor/rules/rules_resource_inventory_test.go
@@ -7,42 +7,39 @@ import (
 )
 
 type resourceInventoryTestCase struct {
-	name     string
-	old      *schema.Resource
-	new      *schema.Resource
-	expected bool
+	name           string
+	old            *schema.Resource
+	new            *schema.Resource
+	wantViolations bool
 }
 
 func TestResourceInventoryRule_RemovingAResource(t *testing.T) {
-	for _, tc := range resourceInventoryRule_RemovingAResourceTestCases {
-		tc.check(resourceInventoryRule_RemovingAResource, t)
+	for _, tc := range resourceConfigRemovingAResourceTestCases {
+		got := ResourceConfigRemovingAResource.Messages("resource", tc.old, tc.new)
+		gotViolations := len(got) > 0
+		if tc.wantViolations != gotViolations {
+			t.Errorf("ResourceConfigRemovingAResource.Messages(%v) violations not expected. Got %v, want %v", tc.name, gotViolations, tc.wantViolations)
+		}
 	}
 }
 
-var resourceInventoryRule_RemovingAResourceTestCases = []resourceInventoryTestCase{
+var resourceConfigRemovingAResourceTestCases = []resourceInventoryTestCase{
 	{
-		name:     "control",
-		old:      &schema.Resource{},
-		new:      &schema.Resource{},
-		expected: false,
+		name:           "control",
+		old:            &schema.Resource{},
+		new:            &schema.Resource{},
+		wantViolations: false,
 	},
 	{
-		name:     "resource added",
-		old:      nil,
-		new:      &schema.Resource{},
-		expected: false,
+		name:           "resource added",
+		old:            nil,
+		new:            &schema.Resource{},
+		wantViolations: false,
 	},
 	{
-		name:     "resource removed",
-		old:      &schema.Resource{},
-		new:      nil,
-		expected: true,
+		name:           "resource removed",
+		old:            &schema.Resource{},
+		new:            nil,
+		wantViolations: true,
 	},
-}
-
-func (tc *resourceInventoryTestCase) check(rule ResourceInventoryRule, t *testing.T) {
-	got := rule.isRuleBreak(tc.old, tc.new)
-	if tc.expected != got {
-		t.Errorf("Test `%s` failed: want %t, got %t", tc.name, tc.expected, got)
-	}
 }
