d68f2fcee1f094a89162f82d6134a375046f2e28
avianap <aviana.polsky@gmail.com>
2024-08-01 11:17:42 -0700

    revert deletion protection for jobs

--- mmv1/third_party/terraform/services/cloudrunv2/data_source_google_cloud_run_v2_job_test.go
+++ mmv1/third_party/terraform/services/cloudrunv2/data_source_google_cloud_run_v2_job_test.go
@@ -39,7 +39,6 @@ func testAccDataSourceGoogleCloudRunV2Job_basic(name, location string) string {
 resource "google_cloud_run_v2_job" "hello" {
   name     = "%s"
   location = "%s"
-  deletion_protection = false
 
   template {
     template {
@@ -101,7 +100,6 @@ func testAccDataSourceGoogleCloudRunV2Job_bindIAMPermission(name, location strin
 resource "google_cloud_run_v2_job" "hello" {
   name     = "%s"
   location = "%s"
-  deletion_protection = false
 
   template {
     template {
