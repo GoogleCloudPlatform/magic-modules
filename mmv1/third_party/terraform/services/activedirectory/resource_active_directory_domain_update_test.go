bd43e309812b860324ff218f5c8995fce7d1100d
Ankit Bhadu <ankitbhadu@google.com>
2024-08-01 12:14:06 +0530

    add acceptance test

--- mmv1/third_party/terraform/services/activedirectory/resource_active_directory_domain_update_test.go
+++ mmv1/third_party/terraform/services/activedirectory/resource_active_directory_domain_update_test.go
@@ -2,11 +2,13 @@ package activedirectory_test
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
 
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
@@ -63,6 +65,44 @@ func TestAccActiveDirectoryDomain_update(t *testing.T) {
 	})
 }
 
+func TestAccActiveDirectoryDomain_tags(t *testing.T) {
+	t.Parallel()
+
+	domain := fmt.Sprintf("tf-test%s.org1.com", acctest.RandString(t, 5))
+	context := map[string]interface{}{
+		"domain":        domain,
+		"resource_name": "ad-domain",
+	}
+
+	resourceName := acctest.Nprintf("google_active_directory_domain.%{resource_name}", context)
+	org := envvar.GetTestOrgFromEnv(t)
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckActiveDirectoryDomainDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccADDomainTags(context, map[string]string{org + "/env": "test"}),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"domain_name", "labels", "terraform_labels", "deletion_protection"},
+			},
+			// Update tags tries to replace AD domain but fails due to deletion protection
+			{
+				Config:      testAccADDomainTags(context, map[string]string{org + "/env": "staging"}),
+				ExpectError: regexp.MustCompile("deletion_protection"),
+			},
+			{
+				Config: testAccADDomainTags_allowDestroy(context, map[string]string{org + "/env": "test"}),
+			},
+		},
+	})
+}
+
 func testAccADDomainBasic(context map[string]interface{}) string {
 
 	return acctest.Nprintf(`
@@ -72,6 +112,43 @@ func testAccADDomainBasic(context map[string]interface{}) string {
 	  reserved_ip_range = "192.168.255.0/24" 
 	}
 	`, context)
+}
+
+func testAccADDomainTags(context map[string]interface{}, tags map[string]string) string {
+
+	r := acctest.Nprintf(`
+	resource "google_active_directory_domain" "%{resource_name}" {
+	  domain_name       = "%{domain}"
+	  locations         = ["us-central1"]
+	  reserved_ip_range = "192.168.255.0/24" 
+	  tags = {`, context)
+
+	l := ""
+	for key, value := range tags {
+		l += fmt.Sprintf("%q = %q\n", key, value)
+	}
+
+	l += fmt.Sprintf("}\n}")
+	return r + l
+}
+
+func testAccADDomainTags_allowDestroy(context map[string]interface{}, tags map[string]string) string {
+
+	r := acctest.Nprintf(`
+	resource "google_active_directory_domain" "%{resource_name}" {
+	  domain_name       = "%{domain}"
+	  locations         = ["us-central1"]
+	  reserved_ip_range = "192.168.255.0/24" 
+	  deletion_protection = false
+	  tags = {`, context)
+
+	l := ""
+	for key, value := range tags {
+		l += fmt.Sprintf("%q = %q\n", key, value)
+	}
+
+	l += fmt.Sprintf("}\n}")
+	return r + l
 }
 
 func testAccADDomainUpdate(context map[string]interface{}) string {
