2bee7e379ffb99a5ec39a21504d6536f1dee0455
Vinod Thokala <vthokala@google.com>
2024-08-02 03:09:40 +0000

    modified code

--- mmv1/third_party/terraform/services/securitycenterv2/resource_scc_v2_organization_source_test.go
+++ mmv1/third_party/terraform/services/securitycenterv2/resource_scc_v2_organization_source_test.go
@@ -4,38 +4,31 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 	"github.com/hashicorp/terraform-provider-google/google/envvar"
 )
 
-func TestAccSCCOrganizationSource_complete(t *testing.T) {
+func TestAccSecurityCenterV2OrganizationSource_basic(t *testing.T) {
 	t.Parallel()
 
 	orgId := envvar.GetTestOrgFromEnv(t)
 	suffix := acctest.RandString(t, 10)
-	displayName := fmt.Sprintf("TFSrc %s", suffix)
 
 	acctest.VcrTest(t, resource.TestCase{
-		PreCheck: func() {
-			acctest.AccTestPreCheck(t)
-		},
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccSCCOrganizationSourceCompleteExample(orgId, suffix, "My description"),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("google_scc_v2_organization_source.custom_source", "display_name", displayName),
-					resource.TestCheckResourceAttr("google_scc_v2_organization_source.custom_source", "organization", orgId),
-					resource.TestCheckResourceAttr("google_scc_v2_organization_source.custom_source", "description", "My description"),
-					resource.TestCheckResourceAttr("google_scc_v2_organization_source.custom_source", "canonical_name", fmt.Sprintf("organizations/%s/sources/source-%s", orgId, suffix)),
-				),
+				Config: testAccSecurityCenterSource_sccSourceBasicExample(orgId, suffix, "My description"),
 			},
 			{
-				Config: testAccSCCOrganizationSourceCompleteExample(orgId, suffix, "My updated description"),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("google_scc_v2_organization_source.custom_source", "description", "My updated description"),
-				),
+				ResourceName:      "google_scc_v2_organization_source.custom_source",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccSecurityCenterSource_sccSourceBasicExample(orgId, suffix, ""),
 			},
 			{
 				ResourceName:      "google_scc_v2_organization_source.custom_source",
@@ -46,13 +39,12 @@ func TestAccSCCOrganizationSource_complete(t *testing.T) {
 	})
 }
 
-func testAccSCCOrganizationSourceCompleteExample(orgId, suffix, description string) string {
+func testAccSecurityCenterSource_sccSourceBasicExample(orgId, suffix, description string) string {
 	return fmt.Sprintf(`
 resource "google_scc_v2_organization_source" "custom_source" {
-  display_name  = "TFSrc %s"
-  organization  = "%s"
-  description   = "%s"
-  canonical_name = "organizations/%s/sources/source-%s"
-}  
-`, suffix, orgId, description, orgId, suffix)
+  display_name = "TFSrc %s"
+  organization = "%s"
+  description  = "%s"
+}
+`, suffix, orgId, description)
 }
