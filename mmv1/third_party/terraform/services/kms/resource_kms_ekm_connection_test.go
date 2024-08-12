35d2a2222ad6913c9c76b5db5a0ca8f992292d8e
Leonardo Henrique Romanini <leonardo.romanini@ciandt.com>
2024-08-12 14:08:06 -0300

    Add conditions

--- mmv1/third_party/terraform/services/kms/resource_kms_ekm_connection_test.go
+++ mmv1/third_party/terraform/services/kms/resource_kms_ekm_connection_test.go
@@ -61,7 +61,8 @@ data "google_project" "vpc-project" {
 data "google_project" "project" {
 }
 resource "google_kms_ekm_connection" "example-ekmconnection" {
-  name            	= "tf_test_ekmconnection_example%{random_suffix}"
+  count = 3
+  name          = "tf_test_ekmconnection_example%{random_suffix}${count.index}"
   location		= "us-central1"
   key_management_mode 	= "MANUAL"
   service_resolvers  	{
@@ -72,96 +73,46 @@ resource "google_kms_ekm_connection" "example-ekmconnection" {
       }
   }
 }
+
 resource "google_kms_ekm_connection_iam_member" "add_viewer" {
-  name = google_kms_ekm_connection.example-ekmconnection.id
+  name = google_kms_ekm_connection.example-ekmconnection[0].id
+  location     		= "us-central1"
   role    = "roles/viewer"
   member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-ekms.iam.gserviceaccount.com"
-}
-`, context)
-}
 
-func testAccKMSEkmConnection_kmsEkmConnectionBasicExample_iam_binding(context map[string]interface{}) string {
-	return acctest.Nprintf(`
-data "google_secret_manager_secret_version" "raw_der" {
-  secret = "playground-cert"
-  project = "315636579862"
-}
-data "google_secret_manager_secret_version" "hostname" {
-  secret = "external-uri"
-  project = "315636579862"
-}
-data "google_secret_manager_secret_version" "servicedirectoryservice" {
-  secret = "external-servicedirectoryservice"
-  project = "315636579862"
-}
-data "google_project" "vpc-project" {
-  project_id = "cloud-ekm-refekm-playground"
-}
-data "google_project" "project" {
-}
-resource "google_kms_ekm_connection" "example-ekmconnection" {
-  name            	= "tf_test_ekmconnection_example%{random_suffix}"
-  location		= "us-central1"
-  key_management_mode 	= "MANUAL"
-  service_resolvers  	{
-      service_directory_service  = data.google_secret_manager_secret_version.servicedirectoryservice.secret_data
-      hostname 			 = data.google_secret_manager_secret_version.hostname.secret_data
-      server_certificates        {
-      		raw_der	= data.google_secret_manager_secret_version.raw_der.secret_data
-      }
+  condition {
+      title       = "expires_after_2029_12_31"
+      description = "Expiring at midnight of 2029-12-31"
+      expression  = "request.time < timestamp(\"2030-01-01T00:00:00Z\")"
+    }
   }
-}
+
 resource "google_kms_ekm_connection_iam_binding" "ekm_admin" {
-  name = google_kms_ekm_connection.example-ekmconnection.id
-  role    = "roles/cloudkms.ekmConnectionsAdmin"
+  name = google_kms_ekm_connection.example-ekmconnection[1].id
+  location     		= "us-central1"
+  role = "roles/cloudkms.ekmConnectionsAdmin"
   members  = ["serviceAccount:service-${data.google_project.project.number}@gcp-sa-ekms.iam.gserviceaccount.com"]
-}
-`, context)
-}
 
-func testAccKMSEkmConnection_kmsEkmConnectionBasicExample_iam_policy(context map[string]interface{}) string {
-	return acctest.Nprintf(`
-data "google_secret_manager_secret_version" "raw_der" {
-  secret = "playground-cert"
-  project = "315636579862"
-}
-data "google_secret_manager_secret_version" "hostname" {
-  secret = "external-uri"
-  project = "315636579862"
-}
-data "google_secret_manager_secret_version" "servicedirectoryservice" {
-  secret = "external-servicedirectoryservice"
-  project = "315636579862"
-}
-data "google_project" "vpc-project" {
-  project_id = "cloud-ekm-refekm-playground"
-}
-data "google_project" "project" {
-}
-resource "google_kms_ekm_connection" "example-ekmconnection" {
-  name            	= "tf_test_ekmconnection_example%{random_suffix}"
-  location		= "us-central1"
-  key_management_mode 	= "MANUAL"
-  service_resolvers  	{
-      service_directory_service  = data.google_secret_manager_secret_version.servicedirectoryservice.secret_data
-      hostname 			 = data.google_secret_manager_secret_version.hostname.secret_data
-      server_certificates        {
-      		raw_der	= data.google_secret_manager_secret_version.raw_der.secret_data
-      }
+  condition {
+      title       = "expires_after_2029_12_31"
+      description = "Expiring at midnight of 2029-12-31"
+      expression  = "request.time < timestamp(\"2030-01-01T00:00:00Z\")"
+    }
   }
-}
-data "google_iam_policy" "ekm_admin" {
-  binding {
-    role = "roles/cloudkms.ekmConnectionsAdmin"
 
+data "google_iam_policy" "ekm_sa" {
+  binding {
+    role = "roles/cloudcontrolspartner.ekmServiceAgent"
     members = [
       "serviceAccount:service-${data.google_project.project.number}@gcp-sa-ekms.iam.gserviceaccount.com",
     ]
   }
 }
+
 resource "google_kms_ekm_connection_iam_policy" "policy" {
-  name = google_kms_ekm_connection.example-ekmconnection.id
-  policy_data = data.google_iam_policy.ekm_admin.policy_data
+  name = google_kms_ekm_connection.example-ekmconnection[2].id
+  policy_data = data.google_iam_policy.ekm_sa.policy_data
+  location     		= "us-central1"
 }
 `, context)
 }
