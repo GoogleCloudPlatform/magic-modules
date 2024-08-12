35bc8cb060da89d64a88f0a66098101fbbc30da4
John Yackovich <yack@google.com>
2024-08-12 13:04:18 -0400

    When creating a log bucket, omit the 'enable_analytics' option unless explicitly specified by user.

--- mmv1/third_party/terraform/services/logging/resource_logging_project_bucket_config.go
+++ mmv1/third_party/terraform/services/logging/resource_logging_project_bucket_config.go
@@ -170,7 +170,6 @@ func resourceLoggingProjectBucketConfigAcquireOrCreate(parentType string, iDFunc
 		if err != nil {
 			return err
 		}
-
 		if parentType == "project" {
 			//logging bucket can be created only at the project level, in future api may allow for folder, org and other parent resources
 
@@ -187,7 +186,7 @@ func resourceLoggingProjectBucketConfigAcquireOrCreate(parentType string, iDFunc
 				UserAgent: userAgent,
 			})
 			if res == nil {
-				log.Printf("[DEGUG] Loggin Bucket not exist %s", id)
+				log.Printf("[DEBUG] Logging Bucket does not exist %s", id)
 				// we need to pass the id in here because we don't want to set it in state
 				// until we know there won't be any errors on create
 				return resourceLoggingProjectBucketConfigCreate(d, meta, id)
@@ -212,7 +211,11 @@ func resourceLoggingProjectBucketConfigCreate(d *schema.ResourceData, meta inter
 	obj["description"] = d.Get("description")
 	obj["locked"] = d.Get("locked")
 	obj["retentionDays"] = d.Get("retention_days")
-	obj["analyticsEnabled"] = d.Get("enable_analytics")
+	// Only set analyticsEnabled if it has been explicitly preferenced.
+	analyticsRawValue := d.GetRawConfig().GetAttr("enable_analytics")
+	if !analyticsRawValue.IsNull() {
+		obj["analyticsEnabled"] = analyticsRawValue.True()
+	}
 	obj["cmekSettings"] = expandCmekSettings(d.Get("cmek_settings"))
 	obj["indexConfigs"] = expandIndexConfigs(d.Get("index_configs"))
 
