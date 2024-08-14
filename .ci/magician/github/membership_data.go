03ec2bc7676da28f4764cd37c3e7823da183ac4d
Thomas Rodgers <thomasrodgers@google.com>
2024-08-14 20:00:17 +0000

    Split reviewer data out from membship.go

---
+++ .ci/magician/github/membership_data.go
@@ -0,0 +1,89 @@
+package github
+
+import "time"
+
+var (
+	// This is for the random-assignee rotation.
+	reviewerRotation = map[string]struct{}{
+		"slevenick":   {},
+		"c2thorn":     {},
+		"rileykarson": {},
+		"melinath":    {},
+		"ScottSuarez": {},
+		"shuyama1":    {},
+		"SarahFrench": {},
+		"roaks3":      {},
+		"zli82016":    {},
+		"trodge":      {},
+		"hao-nan-li":  {},
+		"NickElliot":  {},
+		"BBBmau":      {},
+	}
+
+	// This is for new team members who are onboarding
+	trustedContributors = map[string]struct{}{}
+
+	// This is for reviewers who are "on vacation": will not receive new review assignments but will still receive re-requests for assigned PRs.
+	// User can specify the time zone like this, and following the example below:
+	pdtLoc, _           = time.LoadLocation("America/Los_Angeles")
+	bstLoc, _           = time.LoadLocation("Europe/London")
+	onVacationReviewers = []onVacationReviewer{
+		// Example: taking vacation from 2024-03-28 to 2024-04-02 in pdt time zone.
+		// both ends are inclusive:
+		// {
+		// 	id:        "xyz",
+		// 	startDate: newDate(2024, 3, 28, pdtLoc),
+		// 	endDate:   newDate(2024, 4, 2, pdtLoc),
+		// },
+		{
+			id:        "hao-nan-li",
+			startDate: newDate(2024, 4, 11, pdtLoc),
+			endDate:   newDate(2024, 6, 14, pdtLoc),
+		},
+		{
+			id:        "ScottSuarez",
+			startDate: newDate(2024, 4, 30, pdtLoc),
+			endDate:   newDate(2024, 7, 31, pdtLoc),
+		},
+		{
+			id:        "SarahFrench",
+			startDate: newDate(2024, 8, 2, bstLoc),
+			endDate:   newDate(2024, 8, 6, bstLoc),
+		},
+		{
+			id:        "shuyama1",
+			startDate: newDate(2024, 5, 22, pdtLoc),
+			endDate:   newDate(2024, 5, 28, pdtLoc),
+		},
+		{
+			id:        "melinath",
+			startDate: newDate(2024, 6, 26, pdtLoc),
+			endDate:   newDate(2024, 7, 22, pdtLoc),
+		},
+		{
+			id:        "slevenick",
+			startDate: newDate(2024, 7, 5, pdtLoc),
+			endDate:   newDate(2024, 7, 16, pdtLoc),
+		},
+		{
+			id:        "c2thorn",
+			startDate: newDate(2024, 7, 10, pdtLoc),
+			endDate:   newDate(2024, 7, 16, pdtLoc),
+		},
+		{
+			id:        "rileykarson",
+			startDate: newDate(2024, 7, 18, pdtLoc),
+			endDate:   newDate(2024, 8, 10, pdtLoc),
+		},
+		{
+			id:        "roaks3",
+			startDate: newDate(2024, 8, 2, pdtLoc),
+			endDate:   newDate(2024, 8, 9, pdtLoc),
+		},
+		{
+			id:        "slevenick",
+			startDate: newDate(2024, 8, 10, pdtLoc),
+			endDate:   newDate(2024, 8, 17, pdtLoc),
+		},
+	}
+)
