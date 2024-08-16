eb570f763c7305189b1ba4bfe18ab63d17d9423d
Sarah French <sarah.french@hashicorp.com>
2024-08-16 14:30:00 +0100

    Add acceptance test looking at handling of bad creds in data.google_client_config

--- mmv1/third_party/terraform/services/resourcemanager/data_source_google_client_config_test.go
+++ mmv1/third_party/terraform/services/resourcemanager/data_source_google_client_config_test.go
@@ -1,10 +1,12 @@
 package resourcemanager_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
 )
 
 func TestAccDataSourceGoogleClientConfig_basic(t *testing.T) {
@@ -49,6 +51,47 @@ func TestAccDataSourceGoogleClientConfig_omitLocation(t *testing.T) {
 	})
 }
 
+// Test checks how the data source behaves when credentials are set in the provider block,
+// including when the credentials are valid and not valid
+func TestAccDataSourceGoogleClientConfig_credentialsInProviderConfig(t *testing.T) {
+	// t.Parallel() - Cannot use as we change ENVs
+
+	goodCreds := envvar.GetTestCredsFromEnv()
+	t.Setenv("GOOGLE_CREDENTIALS", "")
+
+	badCreds := acctest.GenerateEscapedFakeCredentialsJson("test") // Need to double escape quotes
+
+	resourceName := "data.google_client_config.current"
+
+	acctest.VcrTest(t, resource.TestCase{
+		// PreCheck cannot be set as usual, as test has GOOGLE_CREDENTIALS unset
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckGoogleClientConfig_credentialsInProviderConfig(goodCreds),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "access_token"),
+				),
+			},
+			{
+				Config: testAccCheckGoogleClientConfig_credentialsInProviderConfig(badCreds),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "access_token"),
+				),
+			},
+		},
+	})
+}
+
 const testAccCheckGoogleClientConfig_basic = `
 data "google_client_config" "current" { }
 `
+
+func testAccCheckGoogleClientConfig_credentialsInProviderConfig(creds string) string {
+	return fmt.Sprintf(`
+provider "google" {
+  credentials = "%s"
+}
+
+data "google_client_config" "current" {}`, creds)
+}
