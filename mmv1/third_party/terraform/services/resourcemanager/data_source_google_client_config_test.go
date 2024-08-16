69a5075067678d96f1bd1e1a27dcceedaf244601
Sarah French <sarah.french@hashicorp.com>
2024-08-16 19:55:47 +0100

    Update test to provide the bad creds via ENV

--- mmv1/third_party/terraform/services/resourcemanager/data_source_google_client_config_test.go
+++ mmv1/third_party/terraform/services/resourcemanager/data_source_google_client_config_test.go
@@ -1,13 +1,11 @@
 package resourcemanager_test
 
 import (
-	"fmt"
 	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
-	"github.com/hashicorp/terraform-provider-google/google/envvar"
 )
 
 func TestAccDataSourceGoogleClientConfig_basic(t *testing.T) {
@@ -52,31 +50,20 @@ func TestAccDataSourceGoogleClientConfig_omitLocation(t *testing.T) {
 	})
 }
 
-// Test checks how the data source behaves when credentials are set in the provider block,
-// including when the credentials are valid and not valid
-func TestAccDataSourceGoogleClientConfig_credentialsInProviderConfig(t *testing.T) {
+// Test checks how the data source behaves when invalid credentials are used
+// This test sets them as an ENV
+func TestAccDataSourceGoogleClientConfig_invalidCredentials(t *testing.T) {
 	// t.Parallel() - Cannot use as we change ENVs
 
-	goodCreds := envvar.GetTestCredsFromEnv()
-	t.Setenv("GOOGLE_CREDENTIALS", "")
-
-	badCreds := acctest.GenerateEscapedFakeCredentialsJson("test") // Need to double escape quotes
-
-	resourceName := "data.google_client_config.current"
+	badCreds := acctest.GenerateFakeCredentialsJson("test")
+	t.Setenv("GOOGLE_CREDENTIALS", badCreds)
 
 	acctest.VcrTest(t, resource.TestCase{
 		// PreCheck cannot be set as usual, as test has GOOGLE_CREDENTIALS unset
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckGoogleClientConfig_credentialsInProviderConfig(goodCreds),
-				Check: resource.ComposeTestCheckFunc(
-					// This field is set when credentials are valid
-					resource.TestCheckResourceAttrSet(resourceName, "access_token"),
-				),
-			},
-			{
-				Config:      testAccCheckGoogleClientConfig_credentialsInProviderConfig(badCreds),
+				Config:      testAccCheckGoogleClientConfig_credentialsInProviderConfig(),
 				ExpectError: regexp.MustCompile("Error setting access_token"),
 			},
 		},
@@ -87,11 +74,6 @@ const testAccCheckGoogleClientConfig_basic = `
 data "google_client_config" "current" { }
 `
 
-func testAccCheckGoogleClientConfig_credentialsInProviderConfig(creds string) string {
-	return fmt.Sprintf(`
-provider "google" {
-  credentials = "%s"
-}
-
-data "google_client_config" "current" {}`, creds)
+func testAccCheckGoogleClientConfig_credentialsInProviderConfig() string {
+	return `data "google_client_config" "current" {}`
 }
