eba06a4cbcbd7e9a0e939e16ada7c0aac9b40194
Huy Pham <11185878+hdp617@users.noreply.github.com>
2024-08-21 11:13:34 -0700

    feat(containerattached): Add security_posture_config

--- mmv1/third_party/terraform/services/containerattached/resource_container_attached_cluster_update_test.go
+++ mmv1/third_party/terraform/services/containerattached/resource_container_attached_cluster_update_test.go
@@ -118,6 +118,9 @@ resource "google_container_attached_cluster" "primary" {
       namespace = "default"
     }
   }
+  security_posture_config {
+    vulnerability_mode = "VULNERABILITY_ENTERPRISE"
+  }
 }
 `, context)
 }
@@ -165,6 +168,9 @@ resource "google_container_attached_cluster" "primary" {
       name = "new-proxy-config"
       namespace = "custom-ns"
     }
+  }
+  security_posture_config {
+    vulnerability_mode = "VULNERABILITY_DISABLED"
   }
   lifecycle {
     prevent_destroy = true
@@ -216,6 +222,9 @@ resource "google_container_attached_cluster" "primary" {
       namespace = "custom-ns"
     }
   }
+  security_posture_config {
+    vulnerability_mode = "VULNERABILITY_DISABLED"
+  }
   lifecycle {
     prevent_destroy = true
   }
@@ -262,6 +271,9 @@ resource "google_container_attached_cluster" "primary" {
       name = "new-proxy-config"
       namespace = "custom-ns"
     }
+  }
+  security_posture_config {
+    vulnerability_mode = "VULNERABILITY_DISABLED"
   }
   lifecycle {
     prevent_destroy = true
@@ -311,6 +323,9 @@ resource "google_container_attached_cluster" "primary" {
       name = "new-proxy-config"
       namespace = "custom-ns"
     }
+  }
+  security_posture_config {
+    vulnerability_mode = "VULNERABILITY_DISABLED"
   }
 }
 `, context)
