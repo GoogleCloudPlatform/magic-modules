44da0e69833f6464e1981d585e107e9e12888a48
Taniguchi Tomoya <taniguchi.tomoya@cloud-ace.jp>
2024-08-21 13:14:58 +0900

    test: fix update test

--- mmv1/third_party/terraform/services/apigee/resource_apigee_instance_update_test.go
+++ mmv1/third_party/terraform/services/apigee/resource_apigee_instance_update_test.go
@@ -1,6 +1,4 @@
-<% autogen_exception -%>
 package apigee_test
-<% unless version == 'ga' -%>
 
 import (
 	"testing"
@@ -17,13 +15,13 @@ func TestAccApigeeInstance_updateConsumerAcceptList(t *testing.T) {
 	context := map[string]interface{}{
 		"billing_account": envvar.GetTestBillingAccountFromEnv(t),
 		"org_id":          envvar.GetTestOrgFromEnv(t),
-		"random_suffix_1":      acctest.RandString(t, 10),
-		"random_suffix_2":      acctest.RandString(t, 10),
+		"random_suffix_1": acctest.RandString(t, 10),
+		"random_suffix_2": acctest.RandString(t, 10),
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		CheckDestroy:             testAccCheckApigeeInstanceDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
@@ -51,8 +49,6 @@ func TestAccApigeeInstance_updateConsumerAcceptList(t *testing.T) {
 func testAccApigeeInstance_basic(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 resource "google_project" "project1" {
-  provider = google-beta
-
   project_id      = "tf-test%{random_suffix_1}"
   name            = "tf-test%{random_suffix_1}"
   org_id          = "%{org_id}"
@@ -60,113 +56,58 @@ resource "google_project" "project1" {
 }
 
 resource "google_project_service" "apigee" {
-  provider = google-beta
-
   project = google_project.project1.project_id
   service = "apigee.googleapis.com"
 }
 
 resource "google_project_service" "compute" {
-  provider = google-beta
-
   project = google_project.project1.project_id
   service = "compute.googleapis.com"
 }
 
 resource "google_project_service" "servicenetworking" {
-  provider = google-beta
-
   project = google_project.project1.project_id
   service = "servicenetworking.googleapis.com"
 }
 
-resource "google_project_service" "kms" {
-  provider = google-beta
-
-  project = google_project.project1.project_id
-  service = "cloudkms.googleapis.com"
-}
-
 resource "google_compute_network" "apigee_network" {
-  provider = google-beta
-
   name       = "apigee-network"
   project    = google_project.project1.project_id
   depends_on = [google_project_service.compute]
 }
 
 resource "google_compute_global_address" "apigee_range" {
-  provider = google-beta
-
-  name          = "tf-test-apigee-range%{random_suffix_1}"
+  name          = "apigee-range"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
-  prefix_length = 16
+  prefix_length = 21
   network       = google_compute_network.apigee_network.id
   project       = google_project.project1.project_id
 }
 
 resource "google_service_networking_connection" "apigee_vpc_connection" {
-  provider = google-beta
-
   network                 = google_compute_network.apigee_network.id
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.apigee_range.name]
   depends_on              = [google_project_service.servicenetworking]
 }
 
-resource "google_kms_key_ring" "apigee_keyring" {
-  provider = google-beta
-
-  name       = "apigee-keyring"
-  location   = "us-central1"
-  project    = google_project.project1.project_id
-  depends_on = [google_project_service.kms]
-}
-
-resource "google_kms_crypto_key" "apigee_key" {
-  provider = google-beta
-
-  name     = "apigee-key"
-  key_ring = google_kms_key_ring.apigee_keyring.id
-}
-
-resource "google_project_service_identity" "apigee_sa" {
-  provider = google-beta
-
-  project = google_project.project1.project_id
-  service = google_project_service.apigee.service
-}
-
-resource "google_kms_crypto_key_iam_member" "apigee_sa_keyuser" {
-  provider = google-beta
-
-  crypto_key_id = google_kms_crypto_key.apigee_key.id
-  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-
-  member = google_project_service_identity.apigee_sa.member
-}
-
 resource "google_apigee_organization" "apigee_org" {
-  provider = google-beta
-
-  analytics_region                     = "us-central1"
-  project_id                           = google_project.project1.project_id
-  authorized_network                   = google_compute_network.apigee_network.id
-  billing_type                         = "PAYG"
-  runtime_database_encryption_key_name = google_kms_crypto_key.apigee_key.id
-
+  analytics_region   = "us-central1"
+  project_id         = google_project.project1.project_id
+  authorized_network = google_compute_network.apigee_network.id
+  billing_type       = "EVALUATION"
   depends_on = [
     google_service_networking_connection.apigee_vpc_connection,
     google_project_service.apigee,
-    google_kms_crypto_key_iam_member.apigee_sa_keyuser,
   ]
 }
 
 resource "google_apigee_instance" "apigee_instance" {
-  name                 = "tf-test%{random_suffix_1}"
-  location             = "us-central1"
-  org_id               = google_apigee_organization.apigee_org.id
+  name     = "tf-test%{random_suffix_1}"
+  location = "us-central1"
+  org_id   = google_apigee_organization.apigee_org.id
+  ip_range = "${google_compute_global_address.apigee_range.address}/22"
   consumer_accept_list = [
     google_project.project1.project_id,
   ]
@@ -177,8 +118,6 @@ resource "google_apigee_instance" "apigee_instance" {
 func testAccApigeeInstance_updateConsumerAcceptList(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 resource "google_project" "project1" {
-  provider = google-beta
-
   project_id      = "tf-test%{random_suffix_1}"
   name            = "tf-test%{random_suffix_1}"
   org_id          = "%{org_id}"
@@ -186,8 +125,6 @@ resource "google_project" "project1" {
 }
 
 resource "google_project" "project2" {
-  provider = google-beta
-
   project_id      = "tf-test%{random_suffix_2}"
   name            = "tf-test%{random_suffix_2}"
   org_id          = "%{org_id}"
@@ -195,113 +132,58 @@ resource "google_project" "project2" {
 }
 
 resource "google_project_service" "apigee" {
-  provider = google-beta
-
   project = google_project.project1.project_id
   service = "apigee.googleapis.com"
 }
 
 resource "google_project_service" "compute" {
-  provider = google-beta
-
   project = google_project.project1.project_id
   service = "compute.googleapis.com"
 }
 
 resource "google_project_service" "servicenetworking" {
-  provider = google-beta
-
   project = google_project.project1.project_id
   service = "servicenetworking.googleapis.com"
 }
 
-resource "google_project_service" "kms" {
-  provider = google-beta
-
-  project = google_project.project1.project_id
-  service = "cloudkms.googleapis.com"
-}
-
 resource "google_compute_network" "apigee_network" {
-  provider = google-beta
-
   name       = "apigee-network"
   project    = google_project.project1.project_id
   depends_on = [google_project_service.compute]
 }
 
 resource "google_compute_global_address" "apigee_range" {
-  provider = google-beta
-
-  name          = "tf-test-apigee-range%{random_suffix_1}"
+  name          = "apigee-range"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
-  prefix_length = 16
+  prefix_length = 21
   network       = google_compute_network.apigee_network.id
   project       = google_project.project1.project_id
 }
 
 resource "google_service_networking_connection" "apigee_vpc_connection" {
-  provider = google-beta
-
   network                 = google_compute_network.apigee_network.id
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.apigee_range.name]
   depends_on              = [google_project_service.servicenetworking]
 }
 
-resource "google_kms_key_ring" "apigee_keyring" {
-  provider = google-beta
-
-  name       = "apigee-keyring"
-  location   = "us-central1"
-  project    = google_project.project1.project_id
-  depends_on = [google_project_service.kms]
-}
-
-resource "google_kms_crypto_key" "apigee_key" {
-  provider = google-beta
-
-  name     = "apigee-key"
-  key_ring = google_kms_key_ring.apigee_keyring.id
-}
-
-resource "google_project_service_identity" "apigee_sa" {
-  provider = google-beta
-
-  project = google_project.project1.project_id
-  service = google_project_service.apigee.service
-}
-
-resource "google_kms_crypto_key_iam_member" "apigee_sa_keyuser" {
-  provider = google-beta
-
-  crypto_key_id = google_kms_crypto_key.apigee_key.id
-  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-
-  member = google_project_service_identity.apigee_sa.member
-}
-
 resource "google_apigee_organization" "apigee_org" {
-  provider = google-beta
-
-  analytics_region                     = "us-central1"
-  project_id                           = google_project.project1.project_id
-  authorized_network                   = google_compute_network.apigee_network.id
-  billing_type                         = "PAYG"
-  runtime_database_encryption_key_name = google_kms_crypto_key.apigee_key.id
-
+  analytics_region   = "us-central1"
+  project_id         = google_project.project1.project_id
+  authorized_network = google_compute_network.apigee_network.id
+  billing_type       = "EVALUATION"
   depends_on = [
     google_service_networking_connection.apigee_vpc_connection,
     google_project_service.apigee,
-    google_kms_crypto_key_iam_member.apigee_sa_keyuser,
   ]
 }
 
 resource "google_apigee_instance" "apigee_instance" {
-  name                 = "tf-test%{random_suffix_1}"
-  location             = "us-central1"
-  org_id               = google_apigee_organization.apigee_org.id
+  name     = "tf-test%{random_suffix_1}"
+  location = "us-central1"
+  org_id   = google_apigee_organization.apigee_org.id
+  ip_range = "${google_compute_global_address.apigee_range.address}/22"
   consumer_accept_list = [
     google_project.project1.project_id,
     google_project.project2.project_id,
@@ -309,4 +191,3 @@ resource "google_apigee_instance" "apigee_instance" {
 }
 `, context)
 }
-<% end -%>
