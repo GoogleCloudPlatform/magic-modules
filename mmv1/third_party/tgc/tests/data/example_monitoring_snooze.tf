0b78170cfbd24ebbd4b9e988ef5ba65139553c65
Matt Searle <mattsearle@ymail.com>
2024-08-30 10:56:35 +0100

    Update mmv1/third_party/tgc/tests/data/example_monitoring_snooze.tf
    Co-authored-by: Thomas Rodgers <thomasrodgers@google.com>

--- mmv1/third_party/tgc/tests/data/example_monitoring_snooze.tf
+++ mmv1/third_party/tgc/tests/data/example_monitoring_snooze.tf
@@ -17,7 +17,7 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
+      source  = "hashicorp/google-beta"
       version = "~> {{.Provider.version}}"
     }
   }
