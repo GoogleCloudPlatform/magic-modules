3cec17272a7972ed3185d4ebd80820fc9973336a
Cameron Thornton <camthornton@google.com>
2024-09-16 15:58:56 -0500

    template rename

---
+++ scripts/rewrite-switchover/switchover-rename-templates.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+for subitem in mmv1/templates/terraform/*; do
+    for filename in $subitem/*; do
+        if [[ $filename != *"*"* ]]; then
+            if [[ $filename == *".erb"* ]]; then
+                echo "renaming $filename to ${filename/.erb/.tmpl}"
+                mv $filename ${filename/.erb/.tmpl}
+            fi
+        fi
+    done
+done
+
+
+for subitem in mmv1/third_party/terraform/*; do
+    for filename in $subitem/*; do
+        if [[ $filename != *"*"* ]]; then
+            if [[ $filename == *".erb"* ]]; then
+                echo "renaming $filename to ${filename/.erb/.tmpl}"
+                mv $filename ${filename/.erb/.tmpl}
+            fi
+        fi
+    done
+done
