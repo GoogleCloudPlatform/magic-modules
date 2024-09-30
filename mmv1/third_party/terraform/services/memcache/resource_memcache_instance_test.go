6111dadb8995183bfa557c89967b67d9ad590261
Cameron Thornton <camthornton@google.com>
2024-09-30 16:00:47 -0500

    Convert from Ruby to Go
    deletion_protection_Memcache

--- mmv1/third_party/terraform/services/memcache/resource_memcache_instance_test.go
+++ mmv1/third_party/terraform/services/memcache/resource_memcache_instance_test.go
@@ -3,9 +3,11 @@ package memcache_test
 import (
 	"fmt"
 	"testing"
+        "regexp"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
 )
 
 func TestAccMemcacheInstance_update(t *testing.T) {
@@ -27,7 +29,7 @@ func TestAccMemcacheInstance_update(t *testing.T) {
 				ResourceName:            "google_memcache_instance.test",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"reserved_ip_range_id"},
+				ImportStateVerifyIgnore: []string{"reserved_ip_range_id", "deletion_protection"},
 			},
 			{
 				Config: testAccMemcacheInstance_update2(prefix, name, network),
@@ -47,6 +49,7 @@ resource "google_memcache_instance" "test" {
   name = "%s"
   region = "us-central1"
   authorized_network = data.google_compute_network.memcache_network.id
+  deletion_protection = true
 
   node_config {
     cpu_count      = 1
@@ -97,3 +100,66 @@ data "google_compute_network" "memcache_network" {
 }
 `, name, network)
 }
+
+func TestAccMemcacheInstance_deletionprotection(t *testing.T) {
+	t.Parallel()
+
+	prefix := fmt.Sprintf("%d", acctest.RandInt(t))
+	name := fmt.Sprintf("tf-test-%s", prefix)
+	network := acctest.BootstrapSharedServiceNetworkingConnection(t, "memcache-instance-update-1")
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckMemcacheInstanceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccMemcacheInstance_deletionprotection(prefix, name, network, "us-central1"),
+			},
+			{
+				ResourceName:            "google_memcache_instance.test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"reserved_ip_range_id", "deletion_protection"},
+			},
+			{
+                                Config: testAccMemcacheInstance_deletionprotection(prefix, name, network, "us-west2"),
+                                ExpectError: regexp.MustCompile("deletion_protection"),
+                        },
+		},
+	})
+}
+
+func testAccMemcacheInstance_deletionprotection(prefix, name, network, region string) string {
+	return fmt.Sprintf(`
+ provider "google" {
+  project                 = "tags-partner-integ-test"
+  user_project_override   = true
+}
+resource "google_memcache_instance" "test" {
+  name = "%s"
+  region = "%s"
+  authorized_network = data.google_compute_network.memcache_network.id
+  deletion_protection = true
+
+  node_config {
+    cpu_count      = 1
+    memory_size_mb = 1024
+  }
+  node_count = 1
+
+  memcache_parameters {
+    params = {
+      "listen-backlog" = "2048"
+      "max-item-size" = "8388608"
+    }
+  }
+  reserved_ip_range_id = ["tf-bootstrap-addr-memcache-instance-update-1"]
+}
+
+data "google_compute_network" "memcache_network" {
+  name = "%s"
+}
+`, name, region, network)
+}
+
