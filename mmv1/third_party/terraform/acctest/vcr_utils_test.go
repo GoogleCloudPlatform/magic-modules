d6763fa23bc23db90341adc36195d266e43b0d0f
Sarah French <sarah.french@hashicorp.com>
2024-10-02 12:43:02 +0100

    Add initial tests showing the matcher detecting matches and mismatches

---
+++ mmv1/third_party/terraform/acctest/vcr_utils_test.go
@@ -0,0 +1,164 @@
+package acctest_test
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/dnaeon/go-vcr/cassette"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+)
+
+func TestNewMatcherFunc_canDetectMatches(t *testing.T) {
+
+	// Same description used to make both structs being compared,
+	// so everything should be determined as a match
+	cases := map[string]requestDescription{
+		"matching POST requests with empty body": {
+			scheme: "https",
+			method: "POST",
+			host:   "example.com",
+			path:   "foobar",
+			body:   "{}",
+		},
+		"matching POST requests with body": {
+			scheme: "https",
+			method: "POST",
+			host:   "example.com",
+			path:   "foobar",
+			body:   "{\"field\":\"value\"}",
+		},
+		"matching GET requests": {
+			scheme: "https",
+			method: "GET",
+			host:   "example.com",
+			path:   "foobar",
+		},
+	}
+
+	for tn, tc := range cases {
+		t.Run(tn, func(t *testing.T) {
+			// Make matcher
+			ctx := context.Background()
+			req := prepareHttpRequest(tc)
+			cassetteReq := prepareCassetteRequest(tc)
+			matcher := acctest.NewMatcherFunc(ctx)
+
+			// Act - use matcher
+			matchDetected := matcher(req, cassetteReq)
+
+			// Assert match
+			if !matchDetected {
+				t.Fatalf("expected matcher to match the requests")
+			}
+		})
+	}
+}
+
+func TestNewMatcherFunc_canDetectMismatches(t *testing.T) {
+
+	cases := map[string]struct {
+		httpRequest     requestDescription
+		cassetteRequest requestDescription
+	}{
+		"different methods": {
+			httpRequest: requestDescription{
+				scheme: "https",
+				method: "POST",
+				host:   "example.com",
+				path:   "foobar",
+				body:   "{}",
+			},
+			cassetteRequest: requestDescription{
+				scheme: "https",
+				method: "PUT",
+				host:   "example.com",
+				path:   "foobar",
+				body:   "{}",
+			},
+		},
+		"different bodies": {
+			httpRequest: requestDescription{
+				scheme: "https",
+				method: "POST",
+				host:   "example.com",
+				path:   "foobar",
+				body:   "{\"field\":\"value is ABCDEFG\"}",
+			},
+			cassetteRequest: requestDescription{
+				scheme: "https",
+				method: "POST",
+				host:   "example.com",
+				path:   "foobar",
+				body:   "{\"field\":\"value is MNLOP\"}",
+			},
+		},
+	}
+
+	for tn, tc := range cases {
+		t.Run(tn, func(t *testing.T) {
+			// Make matcher
+			ctx := context.Background()
+			req := prepareHttpRequest(tc.httpRequest)
+			cassetteReq := prepareCassetteRequest(tc.cassetteRequest)
+			matcher := acctest.NewMatcherFunc(ctx)
+
+			// Act - use matcher
+			matchDetected := matcher(req, cassetteReq)
+
+			// Assert match
+			if matchDetected {
+				t.Fatalf("expected matcher to not match the requests")
+			}
+		})
+	}
+}
+
+type requestDescription struct {
+	scheme string
+	method string
+	host   string
+	path   string
+	body   string
+}
+
+func prepareHttpRequest(d requestDescription) *http.Request {
+	url := &url.URL{
+		Scheme: d.scheme,
+		Host:   d.host,
+		Path:   d.path,
+	}
+
+	req := &http.Request{
+		Method: d.method,
+		URL:    url,
+	}
+
+	// Conditionally set a body
+	if d.body != "" {
+		body := io.NopCloser(bytes.NewBufferString(d.body))
+		req.Body = body
+	}
+
+	return req
+}
+
+func prepareCassetteRequest(d requestDescription) cassette.Request {
+	fullUrl := fmt.Sprintf("%s://%s/%s", d.scheme, d.host, d.path)
+
+	req := cassette.Request{
+		Method: d.method,
+		URL:    fullUrl,
+	}
+
+	// Conditionally set a body
+	if d.body != "" {
+		req.Body = d.body
+	}
+
+	return req
+}
