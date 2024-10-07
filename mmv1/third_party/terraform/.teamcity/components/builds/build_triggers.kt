5678969ee56624e7782a2037aa98bbdac8f81f68
Sarah French <15078782+SarahFrench@users.noreply.github.com>
2024-10-07 10:29:54 +0100

    Resume running cron triggers

--- mmv1/third_party/terraform/.teamcity/components/builds/build_triggers.kt
+++ mmv1/third_party/terraform/.teamcity/components/builds/build_triggers.kt
@@ -36,8 +36,7 @@ class NightlyTriggerConfiguration(
 fun Triggers.runNightly(config: NightlyTriggerConfiguration) {
 
     schedule{
-        // enabled = config.nightlyTestsEnabled
-        enabled = false
+        enabled = config.nightlyTestsEnabled
         branchFilter = "+:" + config.branch // returns "+:/refs/heads/nightly-test" if default
         triggerBuild = always() // Run build even if no new commits/pending changes
         withPendingChangesOnly = false
