1f3165872fa83a0810bd15c8978d1dae8e90968b
Sarah French <sarah.french@hashicorp.com>
2024-10-23 14:22:19 +0100

    Skip user_override + billing_account test in VCR
    See https://github.com/GoogleCloudPlatform/magic-modules/pull/11610#discussion_r1783271457

--- mmv1/third_party/terraform/provider/provider_billing_project_test.go
+++ mmv1/third_party/terraform/provider/provider_billing_project_test.go
@@ -140,6 +140,9 @@ func testAccSdkProvider_billing_project_emptyStringValidation(t *testing.T) {
 }
 
 func testAccSdkProvider_billing_project_useWithAndWithoutUserProjectOverride(t *testing.T) {
+	// Test does interact with APIs but experienced errors when run in VCR mode
+	// See: https://github.com/GoogleCloudPlatform/magic-modules/pull/11610#discussion_r1783271457
+	acctest.SkipIfVcr(t)
 
 	randomString := acctest.RandString(t, 10)
 	contextUserProjectOverrideFalse := map[string]interface{}{
