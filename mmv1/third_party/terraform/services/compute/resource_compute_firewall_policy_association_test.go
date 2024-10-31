497882cf9dbe2c070121f3bbafa24ca76ba750c4
samir-cit <samir@ciandt.com>
2024-10-31 16:13:09 -0300

    Fix review

--- mmv1/third_party/terraform/services/compute/resource_compute_firewall_policy_association_test.go
+++ mmv1/third_party/terraform/services/compute/resource_compute_firewall_policy_association_test.go
@@ -37,6 +37,12 @@ func TestAccComputeFirewallPolicyAssociation_basic(t *testing.T) {
 
 func testAccComputeFirewallPolicyAssociation_basic(context map[string]interface{}) string {
 	return acctest.Nprintf(`
+resource "google_folder" "folder" {
+  display_name = "tf-test-folder-%{random_suffix}"
+  parent       = "%{org_name}"
+  deletion_protection = false
+}
+
 resource "google_folder" "target_folder" {
   display_name = "tf-test-target-%{random_suffix}"
   parent       = "%{org_name}"
@@ -44,7 +50,7 @@ resource "google_folder" "target_folder" {
 }
 
 resource "google_compute_firewall_policy" "default" {
-  parent      = "%{org_name}"
+  parent      = google_folder.folder.id
   short_name  = "tf-test-policy-%{random_suffix}"
   description = "Resource created for Terraform acceptance testing"
 }
