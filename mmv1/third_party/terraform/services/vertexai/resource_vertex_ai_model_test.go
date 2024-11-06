0daae85e5efbbc81b5317e983bdde07b9a3023f5
Sarah French <sarah.french@hashicorp.com>
2024-11-06 12:30:40 +0000

    Stop using async operations for updates, add basic update acceptance test

--- mmv1/third_party/terraform/services/vertexai/resource_vertex_ai_model_test.go
+++ mmv1/third_party/terraform/services/vertexai/resource_vertex_ai_model_test.go
@@ -14,41 +14,7 @@ import (
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
 )
 
-func TestAccVertexAIModel_modelIdNotProvided(t *testing.T) {
-	t.Parallel()
-
-	context := map[string]interface{}{
-		"project_name":  envvar.GetTestProjectFromEnv(),
-		"random_suffix": acctest.RandString(t, 10),
-	}
-
-	acctest.VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckVertexAIModelDestroyProducer(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccVertexAIModel_modelIdNotProvided(context),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrSet("google_vertex_ai_model.model", "model_id"),
-				),
-			},
-		},
-	})
-}
-
-func testAccVertexAIModel_modelIdNotProvided(context map[string]interface{}) string {
-	return acctest.Nprintf(`
-resource "google_vertex_ai_model" "model" {
-  project = "%{project_name}"
-  source_model = "projects/%{project_name}/locations/us-central1/models/6033738282699849728"
-
-  region       = "us-central1"
-}
-`, context)
-}
-
-func TestAccVertexAIModel_modelIdProvided(t *testing.T) {
+func TestAccVertexAIModel_postCreationUpdates(t *testing.T) {
 	t.Parallel()
 
 	randomString := acctest.RandString(t, 10)
@@ -63,7 +29,73 @@ func TestAccVertexAIModel_modelIdProvided(t *testing.T) {
 		CheckDestroy:             testAccCheckVertexAIModelDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccVertexAIModel_modelIdProvided(context),
+				Config: testAccVertexAIModel_modelIdProvided_create(context),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("google_vertex_ai_model.model", "model_id", context["model_id"].(string)),
+				),
+			},
+			{
+				Config: testAccVertexAIModel_modelIdProvided_update(context),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("google_vertex_ai_model.model", "model_id", context["model_id"].(string)),
+					resource.TestCheckResourceAttr("google_vertex_ai_model.model", "description", "updated"),
+					resource.TestCheckResourceAttr("google_vertex_ai_model.model", "display_name", "updated"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccVertexAIModel_modelIdNotProvidedAtCreateTime(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"project_name":  envvar.GetTestProjectFromEnv(),
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckVertexAIModelDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVertexAIModel_modelIdNotProvided_create(context),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("google_vertex_ai_model.model", "model_id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccVertexAIModel_modelIdNotProvided_create(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_vertex_ai_model" "model" {
+  project = "%{project_name}"
+  source_model = "projects/%{project_name}/locations/us-central1/models/6033738282699849728"
+
+  region       = "us-central1"
+}
+`, context)
+}
+
+func TestAccVertexAIModel_modelIdProvidedAtCreateTime(t *testing.T) {
+	t.Parallel()
+
+	randomString := acctest.RandString(t, 10)
+	context := map[string]interface{}{
+		"project_name": envvar.GetTestProjectFromEnv(),
+		"model_id":     fmt.Sprintf("tf-test-test-model%s", randomString),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckVertexAIModelDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVertexAIModel_modelIdProvided_create(context),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("google_vertex_ai_model.model", "model_id", context["model_id"].(string)),
 				),
@@ -72,7 +104,7 @@ func TestAccVertexAIModel_modelIdProvided(t *testing.T) {
 	})
 }
 
-func testAccVertexAIModel_modelIdProvided(context map[string]interface{}) string {
+func testAccVertexAIModel_modelIdProvided_create(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 resource "google_vertex_ai_model" "model" {
   model_id = "%{model_id}"
@@ -80,6 +112,21 @@ resource "google_vertex_ai_model" "model" {
   source_model = "projects/%{project_name}/locations/us-central1/models/6033738282699849728"
 
   region       = "us-central1"
+}
+`, context)
+}
+
+func testAccVertexAIModel_modelIdProvided_update(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_vertex_ai_model" "model" {
+  model_id = "%{model_id}"
+  project = "%{project_name}"
+  source_model = "projects/%{project_name}/locations/us-central1/models/6033738282699849728"
+
+  region       = "us-central1"
+
+  description = "updated"
+  display_name = "updated"
 }
 `, context)
 }
