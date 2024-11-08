32a57cc8bb33895ae32c1ecb7fbf4fe3b75ba947
BBBmau <alvarez.mauriciotm@gmail.com>
2024-11-08 00:53:45 -0800

    improve delegates test

--- mmv1/third_party/terraform/services/resourcemanager/ephemeral_google_service_account_id_token_test.go
+++ mmv1/third_party/terraform/services/resourcemanager/ephemeral_google_service_account_id_token_test.go
@@ -32,8 +32,11 @@ func TestEphemeralServiceAccountIdToken_basic(t *testing.T) {
 func TestEphemeralServiceAccountIdToken_withDelegates(t *testing.T) {
 	t.Parallel()
 
-	serviceAccount := envvar.GetTestServiceAccountFromEnv(t)
-	targetServiceAccountEmail := acctest.BootstrapServiceAccount(t, "target", serviceAccount)
+	project := envvar.GetTestProjectFromEnv()
+	initialServiceAccount := envvar.GetTestServiceAccountFromEnv(t)
+	delegateServiceAccountEmailOne := acctest.BootstrapServiceAccount(t, "delegate1", initialServiceAccount)          // SA_2
+	delegateServiceAccountEmailTwo := acctest.BootstrapServiceAccount(t, "delegate2", delegateServiceAccountEmailOne) // SA_3
+	targetServiceAccountEmail := acctest.BootstrapServiceAccount(t, "target", delegateServiceAccountEmailTwo)         // SA_4
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
@@ -43,7 +46,10 @@ func TestEphemeralServiceAccountIdToken_withDelegates(t *testing.T) {
 		},
 		Steps: []resource.TestStep{
 			{
-				Config: testAccEphemeralServiceAccountIdToken_withDelegates(targetServiceAccountEmail),
+				Config: testAccEphemeralServiceAccountIdToken_delegatesSetup(initialServiceAccount, delegateServiceAccountEmailOne, delegateServiceAccountEmailTwo, targetServiceAccountEmail, project),
+			},
+			{
+				Config: testAccEphemeralServiceAccountIdToken_withDelegates(initialServiceAccount, delegateServiceAccountEmailOne, delegateServiceAccountEmailTwo, targetServiceAccountEmail, project),
 			},
 		},
 	})
@@ -78,14 +84,111 @@ ephemeral "google_service_account_id_token" "token" {
 `, serviceAccountEmail)
 }
 
-func testAccEphemeralServiceAccountIdToken_withDelegates(serviceAccountEmail string) string {
+func testAccEphemeralServiceAccountIdToken_withDelegates(initialServiceAccountEmail, delegateServiceAccountEmailOne, delegateServiceAccountEmailTwo, targetServiceAccountEmail, project string) string {
 	return fmt.Sprintf(`
-ephemeral "google_service_account_id_token" "token" {
-  target_service_account = "%s"
-  target_audience       = "https://example.com"
-  delegates            = ["%[1]s"]
+resource "google_service_account_iam_binding" "sa2_to_sa3" {
+  service_account_id = "projects/%[5]s/serviceAccounts/%[4]s"
+  role               = "roles/iam.serviceAccountTokenCreator"
+  members            = [
+    "serviceAccount:%[3]s"
+  ]
+  depends_on = [google_service_account_iam_binding.sa1_to_sa2]
 }
-`, serviceAccountEmail)
+
+resource "google_service_account_iam_binding" "sa1_to_sa2" {
+  service_account_id = "projects/%[5]s/serviceAccounts/%[3]s"
+  role               = "roles/iam.serviceAccountTokenCreator"
+  members            = [
+    "serviceAccount:%[2]s"
+  ]
+  depends_on = [google_service_account_iam_binding.terraform_to_delegate1]
+}
+
+resource "google_service_account_iam_binding" "terraform_to_delegate1" {
+  service_account_id = "projects/%[5]s/serviceAccounts/%[2]s"
+  role               = "roles/iam.serviceAccountTokenCreator"
+  members            = [
+    "serviceAccount:%[1]s"
+  ]
+  depends_on = [google_project_iam_member.terraform_sa_token_creator]
+}
+
+resource "google_project_iam_member" "terraform_sa_token_creator" {
+  project = "%[5]s"
+  role    = "roles/iam.serviceAccountTokenCreator"
+  member  = "serviceAccount:%[1]s"
+}
+
+resource "time_sleep" "wait_60_seconds" {
+  depends_on = [
+    google_service_account_iam_binding.sa1_to_sa2,
+    google_service_account_iam_binding.sa2_to_sa3,
+    google_project_iam_member.terraform_sa_token_creator,
+  ]
+  create_duration = "60s"
+}
+
+ephemeral "google_service_account_id_token" "token" {
+  target_service_account = "%[4]s"
+  delegates = [
+    "%[3]s",
+    "%[2]s",
+  ]
+  target_audience       = "https://example.com"
+}
+
+# The delegation chain is:
+# SA_1 (initialServiceAccountEmail) -> SA_2 (delegateServiceAccountEmailOne) -> SA_3 (delegateServiceAccountEmailTwo) -> SA_4 (targetServiceAccountEmail)
+`, initialServiceAccountEmail, delegateServiceAccountEmailOne, delegateServiceAccountEmailTwo, targetServiceAccountEmail, project)
+}
+
+func testAccEphemeralServiceAccountIdToken_delegatesSetup(initialServiceAccountEmail, delegateServiceAccountEmailOne, delegateServiceAccountEmailTwo, targetServiceAccountEmail, project string) string {
+	return fmt.Sprintf(`
+resource "google_service_account_iam_binding" "sa2_to_sa3" {
+  service_account_id = "projects/%[5]s/serviceAccounts/%[4]s"
+  role               = "roles/iam.serviceAccountTokenCreator"
+  members            = [
+    "serviceAccount:%[3]s"
+  ]
+  depends_on = [google_service_account_iam_binding.sa1_to_sa2]
+}
+
+resource "google_service_account_iam_binding" "sa1_to_sa2" {
+  service_account_id = "projects/%[5]s/serviceAccounts/%[3]s"
+  role               = "roles/iam.serviceAccountTokenCreator"
+  members            = [
+    "serviceAccount:%[2]s"
+  ]
+  depends_on = [google_service_account_iam_binding.terraform_to_delegate1]
+}
+
+resource "google_service_account_iam_binding" "terraform_to_delegate1" {
+  service_account_id = "projects/%[5]s/serviceAccounts/%[2]s"
+  role               = "roles/iam.serviceAccountTokenCreator"
+  members            = [
+    "serviceAccount:%[1]s"
+  ]
+  depends_on = [google_project_iam_member.terraform_sa_token_creator]
+}
+
+resource "google_project_iam_member" "terraform_sa_token_creator" {
+  project = "%[5]s"
+  role    = "roles/iam.serviceAccountTokenCreator"
+  member  = "serviceAccount:%[1]s"
+}
+
+resource "time_sleep" "wait_60_seconds" {
+  depends_on = [
+    google_service_account_iam_binding.sa1_to_sa2,
+    google_service_account_iam_binding.sa2_to_sa3,
+    google_project_iam_member.terraform_sa_token_creator,
+  ]
+  create_duration = "60s"
+}
+
+# The delegation chain is:
+# SA_1 (initialServiceAccountEmail) -> SA_2 (delegateServiceAccountEmailOne) -> SA_3 (delegateServiceAccountEmailTwo) -> SA_4 (targetServiceAccountEmail)
+`, initialServiceAccountEmail, delegateServiceAccountEmailOne, delegateServiceAccountEmailTwo, targetServiceAccountEmail, project)
 }
 
 func testAccEphemeralServiceAccountIdToken_withIncludeEmail(serviceAccountEmail string) string {
