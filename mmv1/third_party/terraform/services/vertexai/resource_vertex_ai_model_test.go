94daf9ee99fe50c4e8065ddb2f43b61898a8eb85
BBBmau <alvarez.mauriciotm@gmail.com>
2024-11-21 14:22:22 -0800

    update tests to use model name in update_tests

--- mmv1/third_party/terraform/services/vertexai/resource_vertex_ai_model_test.go
+++ mmv1/third_party/terraform/services/vertexai/resource_vertex_ai_model_test.go
@@ -73,7 +73,7 @@ func testAccVertexAIModel_modelIdNotProvided_create(context map[string]interface
 	return acctest.Nprintf(`
 resource "google_vertex_ai_model" "model" {
   project = "%{project_name}"
-  source_model = "projects/%{project_name}/locations/us-central1/models/6033738282699849728"
+  source_model = "projects/%{project_name}/locations/us-central1/models/tf-static-1"
 
   region       = "us-central1"
 }
@@ -109,7 +109,7 @@ func testAccVertexAIModel_modelIdProvided_create(context map[string]interface{})
 resource "google_vertex_ai_model" "model" {
   model_id = "%{model_id}"
   project = "%{project_name}"
-  source_model = "projects/%{project_name}/locations/us-central1/models/6033738282699849728"
+  source_model = "projects/%{project_name}/locations/us-central1/models/tf-static-1"
 
   region       = "us-central1"
 }
@@ -121,7 +121,7 @@ func testAccVertexAIModel_modelIdProvided_update(context map[string]interface{})
 resource "google_vertex_ai_model" "model" {
   model_id = "%{model_id}"
   project = "%{project_name}"
-  source_model = "projects/%{project_name}/locations/us-central1/models/6033738282699849728"
+  source_model = "projects/%{project_name}/locations/us-central1/models/tf-static-1"
 
   region       = "us-central1"
 
