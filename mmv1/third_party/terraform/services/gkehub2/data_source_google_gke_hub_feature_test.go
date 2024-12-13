4e97e6c6b7cdc1525bfddd2484ca8affd0f1ae9e
Michael Lopez <mickael.lopez@gmail.com>
2024-12-13 11:08:06 +0100

    Fixing tests

--- mmv1/third_party/terraform/services/gkehub2/data_source_google_gke_hub_feature_test.go
+++ mmv1/third_party/terraform/services/gkehub2/data_source_google_gke_hub_feature_test.go
@@ -1,10 +1,15 @@
 package gkehub2_test
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
+	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
 )
 
 func TestAccDataSourceGoogleGkeHubFeature_basic(t *testing.T) {
@@ -34,7 +39,7 @@ func testAccDataSourceGoogleGkeHubFeature_basic(context map[string]interface{}) 
 resource "google_gke_hub_feature" "example" {
   location = "us-central1"
   project  = "%{project}"
-  name     = "configmanagement"
+  name     = "servicemesh"
 }
 
 data "google_gke_hub_feature" "example" {
@@ -43,4 +48,43 @@ data "google_gke_hub_feature" "example" {
   name     = google_gke_hub_feature.feature.name
 }
 `, context)
+}
+
+func testAccCheckGoogleGkeHubFeatureDestroyProducer(t *testing.T) func(s *terraform.State) error {
+	return func(s *terraform.State) error {
+		for name, rs := range s.RootModule().Resources {
+			if rs.Type != "google_gke_hub_feature" {
+				continue
+			}
+			if strings.HasPrefix(name, "data.") {
+				continue
+			}
+
+			config := acctest.GoogleProviderConfig(t)
+
+			url, err := tpgresource.ReplaceVarsForTest(config, rs, "{{GKEHub2BasePath}}projects/{{project}}/locations/{{location}}/features/{{name}}")
+			if err != nil {
+				return err
+			}
+
+			billingProject := ""
+
+			if config.BillingProject != "" {
+				billingProject = config.BillingProject
+			}
+
+			_, err = transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
+				Config:    config,
+				Method:    "GET",
+				Project:   billingProject,
+				RawURL:    url,
+				UserAgent: config.UserAgent,
+			})
+			if err == nil {
+				return fmt.Errorf("GKEHub2Feature still exists at %s", url)
+			}
+		}
+
+		return nil
+	}
 }
