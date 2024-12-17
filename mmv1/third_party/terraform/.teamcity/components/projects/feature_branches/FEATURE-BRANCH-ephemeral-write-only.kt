dadf0d364650ce087db4b13f36daf91cdb88b776
BBBmau <alvarez.mauriciotm@gmail.com>
2024-12-16 20:33:47 -0800

    rewrite to use getServices

--- mmv1/third_party/terraform/.teamcity/components/projects/feature_branches/FEATURE-BRANCH-ephemeral-write-only.kt
+++ mmv1/third_party/terraform/.teamcity/components/projects/feature_branches/FEATURE-BRANCH-ephemeral-write-only.kt
@@ -21,6 +21,7 @@ import vcs_roots.HashiCorpVCSRootBeta
 import vcs_roots.HashiCorpVCSRootGa
 import vcs_roots.ModularMagicianVCSRootBeta
 import vcs_roots.ModularMagicianVCSRootGa
+import getServicesList
 
 const val featureBranchEphemeralWriteOnly = "FEATURE-BRANCH-ephemeral-write-only"
 const val EphemeralWriteOnlyTfCoreVersion = "1.11.0-alpha20241211"
@@ -35,109 +36,25 @@ fun featureBranchEphemeralWriteOnlySubProject(allConfig: AllContextParameters): 
     // GA
     val gaConfig = getGaAcceptanceTestConfig(allConfig)
     // These are the packages that have resources that will use write-only attributes
-    var ServicesListWriteOnlyGa = mapOf(
-        "compute" to mapOf(
-            "name" to "compute",
-            "displayName" to "Compute",
-            "path" to "./google/services/compute"
-        ),
-        "secretmanager" to mapOf(
-            "name" to "secretmanager",
-            "displayName" to "Secretmanager",
-            "path" to "./google/services/secretmanager"
-        ),
-        "sql" to mapOf(
-            "name" to "sql",
-            "displayName" to "Sql",
-            "path" to "./google/services/sql"
-        ),
-        "bigquery_datatransfer" to mapOf(
-            "name" to "bigquery_datatransfer",
-            "displayName" to "Bigquery Datatransfer",
-            "path" to "./google/services/bigquery_datatransfer"
-        )
-    )
+    var ServicesListWriteOnlyGA = getServicesList(arrayOf("compute", "secretmanager", "sql", "bigquerydatatransfer"), "GA")
 
-    val buildConfigsGa = BuildConfigurationsForPackages(ServicesListWriteOnlyGa, ProviderNameGa, "EphemeralWriteOnlyGa - HC", HashiCorpVCSRootGa, listOf(SharedResourceNameGa), gaConfig)
+    val buildConfigsGa = BuildConfigurationsForPackages(ServicesListWriteOnlyGA, ProviderNameGa, "EphemeralWriteOnlyGa - HC", HashiCorpVCSRootGa, listOf(SharedResourceNameGa), gaConfig)
     buildConfigsGa.forEach{ builds ->
         builds.addTrigger(trigger)
     }
 
-    var ServicesListWriteOnlyGaMM = mapOf(
-        "compute" to mapOf(
-            "name" to "compute",
-            "displayName" to "Compute - MM",
-            "path" to "./google/services/compute"
-        ),
-        "secretmanager" to mapOf(
-            "name" to "secretmanager",
-            "displayName" to "Secretmanager - MM",
-            "path" to "./google/services/secretmanager"
-        ),
-        "sql" to mapOf(
-            "name" to "sql",
-            "displayName" to "Sql - MM",
-            "path" to "./google/services/sql"
-        ),
-        "bigquery_datatransfer" to mapOf(
-            "name" to "bigquery_datatransfer",
-            "displayName" to "Bigquery Datatransfer - MM",
-            "path" to "./google/services/bigquery_datatransfer"
-        )
-    )
+    var ServicesListWriteOnlyGaMM = getServicesList(arrayOf("compute", "secretmanager", "sql", "bigquerydatatransfer"), "GA-MM")
     val buildConfigsMMGa = BuildConfigurationsForPackages(ServicesListWriteOnlyGaMM, ProviderNameGa, "EphemeralWriteOnlyGa - MM", ModularMagicianVCSRootGa, listOf(SharedResourceNameGa), vcrConfig)
 
     // Beta
     val betaConfig = getBetaAcceptanceTestConfig(allConfig)
-    var ServicesListWriteOnlyBeta = mapOf(
-        "compute" to mapOf(
-            "name" to "compute",
-            "displayName" to "Compute - Beta",
-            "path" to "./google-beta/services/compute"
-        ),
-        "secretmanager" to mapOf(
-            "name" to "secretmanager",
-            "displayName" to "Secretmanager - Beta",
-            "path" to "./google-beta/services/secretmanager"
-        ),
-        "sql" to mapOf(
-            "name" to "sql",
-            "displayName" to "Sql - Beta",
-            "path" to "./google-beta/services/sql"
-        ),
-        "bigquery_datatransfer" to mapOf(
-            "name" to "bigquery_datatransfer",
-            "displayName" to "Bigquery Datatransfer - Beta",
-            "path" to "./google-beta/services/bigquery_datatransfer"
-        )
-    )
+    var ServicesListWriteOnlyBeta = getServicesList(arrayOf("compute", "secretmanager", "sql", "bigquerydatatransfer"), "Beta")
     val buildConfigsBeta = BuildConfigurationsForPackages(ServicesListWriteOnlyBeta, ProviderNameBeta, "EphemeralWriteOnlyBeta - HC", HashiCorpVCSRootBeta, listOf(SharedResourceNameBeta), betaConfig)
     buildConfigsBeta.forEach{ builds ->
         builds.addTrigger(trigger)
     }
 
-    var ServicesListWriteOnlyBetaMM = mapOf(
-        "compute" to mapOf(
-            "name" to "compute",
-            "displayName" to "Compute - Beta - MM",
-            "path" to "./google-beta/services/compute"
-        ),
-        "secretmanager" to mapOf(
-            "name" to "secretmanager",
-            "displayName" to "Secretmanager - Beta - MM",
-            "path" to "./google-beta/services/secretmanager"
-        ),
-        "sql" to mapOf(
-            "name" to "sql",
-            "displayName" to "Sql - Beta - MM",
-            "path" to "./google-beta/services/sql"
-        ),
-        "bigquery_datatransfer" to mapOf(
-            "name" to "bigquery_datatransfer",
-            "displayName" to "Bigquery Datatransfer - Beta - MM",
-            "path" to "./google-beta/services/bigquery_datatransfer"
-        )
-    )
+    var ServicesListWriteOnlyBetaMM = getServicesList(arrayOf("compute", "secretmanager", "sql", "bigquerydatatransfer"), "Beta-MM")
     val buildConfigsMMBeta = BuildConfigurationsForPackages(ServicesListWriteOnlyBetaMM, ProviderNameBeta, "EphemeralWriteOnlyBeta - MM", ModularMagicianVCSRootBeta, listOf(SharedResourceNameBeta), vcrConfig)
 
     // Make all builds use a 1.11.0-ish version of TF core
