8b64a9b954267f046b67b847314efb762ddd272a
himanikh <himani.arora78@gmail.com>
2024-12-19 00:46:13 +0000

    fixes

--- mmv1/third_party/terraform/tpgresource/common_diff_suppress_test.go
+++ mmv1/third_party/terraform/tpgresource/common_diff_suppress_test.go
@@ -67,30 +67,6 @@ func TestDurationDiffSuppress(t *testing.T) {
 	}
 }
 
-func TestProjectIDDiffSuppress(t *testing.T) {
-	cases := map[string]struct {
-		Old, New           string
-		ExpectDiffSuppress bool
-	}{
-		"different project identifiers": {
-			Old:                "projects/ten-tp/locations/abc/serviceAttachments/xyz",
-			New:                "projects/1234/locations/abc/serviceAttachments/xyz",
-			ExpectDiffSuppress: true,
-		},
-		"different resources": {
-			Old:                "projects/ten-tp/locations/abc/serviceAttachments/xyz",
-			New:                "projects/1234/locations/abc/serviceAttachments/jkl",
-			ExpectDiffSuppress: false,
-		},
-	}
-
-	for tn, tc := range cases {
-		if ProjectIDDiffSuppress("diffSuppress", tc.Old, tc.New, nil) != tc.ExpectDiffSuppress {
-			t.Fatalf("bad: %s, '%s' => '%s' expect %t", tn, tc.Old, tc.New, tc.ExpectDiffSuppress)
-		}
-	}
-}
-
 func TestEmptyOrUnsetBlockDiffSuppress(t *testing.T) {
 	cases := map[string]struct {
 		Key, Old, New      string
