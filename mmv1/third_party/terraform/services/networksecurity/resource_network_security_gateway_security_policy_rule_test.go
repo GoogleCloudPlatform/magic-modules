8296e9e60eb617ca2941c3d4c342b5ea7ff65eb1
samir-cit <samir@ciandt.com>
2025-01-07 13:07:15 -0300

    Add sleep after delete and multiple rules test

--- mmv1/third_party/terraform/services/networksecurity/resource_network_security_gateway_security_policy_rule_test.go
+++ mmv1/third_party/terraform/services/networksecurity/resource_network_security_gateway_security_policy_rule_test.go
@@ -48,6 +48,35 @@ func TestAccNetworkSecurityGatewaySecurityPolicyRule_update(t *testing.T) {
 	})
 }
 
+func TestAccNetworkSecurityGatewaySecurityPolicyRule_multiple(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckNetworkSecurityGatewaySecurityPolicyRuleDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetworkSecurityGatewaySecurityPolicyRule_multiple(context),
+			},
+			{
+				ResourceName:      "google_network_security_gateway_security_policy_rule.rule1",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				ResourceName:      "google_network_security_gateway_security_policy_rule.rule2",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func testAccNetworkSecurityGatewaySecurityPolicyRule_basic(gatewaySecurityPolicyName, gatewaySecurityPolicyRuleName string) string {
 	return fmt.Sprintf(`
 resource "google_network_security_gateway_security_policy" "default" {
@@ -91,4 +120,39 @@ resource "google_network_security_gateway_security_policy_rule" "foobar" {
   basic_profile           = "DENY"
 }
 `, gatewaySecurityPolicyName, gatewaySecurityPolicyRuleName)
+}
+
+func testAccNetworkSecurityGatewaySecurityPolicyRule_multiple(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_network_security_gateway_security_policy" "default" {
+  name        = "tf-test-gateway-sp-%{random_suffix}"
+  location    = "us-central1"
+  description = "gateway security policy created to be used as reference by the rule."
+}
+
+resource "google_network_security_gateway_security_policy_rule" "rule1" {
+  name                    = "tf-test-gateway-sp-rule1-%{random_suffix}"
+  location                = "us-central1"
+  gateway_security_policy = google_network_security_gateway_security_policy.default.name
+  enabled                 = true  
+  description             = "First rule"
+  priority                = 1
+  session_matcher         = "host() == 'example.com'"
+  application_matcher     = "request.method == 'POST'"
+  basic_profile           = "ALLOW"
+}
+
+resource "google_network_security_gateway_security_policy_rule" "rule2" {
+  name                    = "tf-test-gateway-sp-rule2-%{random_suffix}"
+  location                = "us-central1"
+  gateway_security_policy = google_network_security_gateway_security_policy.default.name
+  enabled                 = true  
+  description             = "Second rule"
+  priority                = 2
+  session_matcher         = "host() == 'example.com'"
+  application_matcher     = "request.method == 'GET'"
+  tls_inspection_enabled  = false
+  basic_profile           = "DENY"
+}
+`, context)
 }
