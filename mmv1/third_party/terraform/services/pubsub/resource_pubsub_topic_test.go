3a2900aa9480e947f38c2836e849b4d2a4f33b81
Andrea Lee <andreajlee@google.com>
2025-01-15 12:13:37 -0500

    Add new fields and fix errors.

--- mmv1/third_party/terraform/services/pubsub/resource_pubsub_topic_test.go
+++ mmv1/third_party/terraform/services/pubsub/resource_pubsub_topic_test.go
@@ -429,13 +429,13 @@ resource "google_pubsub_topic" "foo" {
   # Outside of automated terraform-provider-google CI tests, these values must be of actual Cloud Storage resources for the test to pass.
   ingestion_data_source_settings {
   	azure_event_hubs {
-		resource_group = "azure-ingestion-resource-group"
-		namespace = "azure-ingestion-namespace"
-		event_hub = "azure-ingestion-event-hub"
-		client_id = "aZZZZZZZ-YYYY-HHHH-GGGG-abcdef569123"
-		tenant_id = "0XXXXXXX-YYYY-HHHH-GGGG-123456789123"
-		subscription_id = "bXXXXXXX-YYYY-HHHH-GGGG-123456789123"
-		gcp_service_account = "fake-service-account@fake-gcp-project.iam.gserviceaccount.com"
+		resource_group = "ingestion-resource-group"
+		namespace = "ingestion-namespace"
+		event_hub = "ingestion-event-hub"
+		client_id = "aZZZZZZZ-YYYY-HHHH-GGGG-abcdef123456"
+		tenant_id = "0XXXXXXX-YYYY-HHHH-GGGG-123456123456"
+		subscription_id = "bXXXXXXX-YYYY-HHHH-GGGG-123456123456"
+		gcp_service_account = "fake-account@new-gcp-project.iam.gserviceaccount.com"
     }
   }
 }
