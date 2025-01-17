a429cd63c4b70519ad24d7bc5cf027442894c559
Stephen Lewis <stephenrlewis@google.com>
2025-01-17 11:58:36 -0800

    Implemented simpler IAM bootstrapping logic
    This should make it easier when looking at tests to see what is being bootstrapped

--- mmv1/third_party/terraform/acctest/bootstrap_iam_test_utils.go
+++ mmv1/third_party/terraform/acctest/bootstrap_iam_test_utils.go
@@ -4,20 +4,22 @@ import (
 	"fmt"
 	"log"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-provider-google/google/envvar"
 	"github.com/hashicorp/terraform-provider-google/google/tpgiamresource"
 	cloudresourcemanager "google.golang.org/api/cloudresourcemanager/v1"
 )
 
-// BootstrapAllPSARoles ensures that the given project's IAM
-// policy grants the given service agents the given roles.
-// prefix is usually "service-" and indicates the service agent should have the
-// given prefix before the project number.
-// This is important to bootstrap because using iam policy resources means that
-// deleting them removes permissions for concurrent tests.
-// Return whether the bindings changed.
-func BootstrapAllPSARoles(t *testing.T, prefix string, agentNames, roles []string) bool {
+type IamMember struct {
+	member, role string
+}
+
+// BootstrapIamMembers ensures that a given set of member/role pairs exist in the default
+// test project. This should be used to avoid race conditions that can happen on the
+// default project due to parallel tests managing the same member/role pairings. Members
+// will have `{project_number}` replaced with the default test project's project number.
+func BootstrapIamMembers(t *testing.T, members []IamMember) {
 	config := BootstrapConfig(t)
 	if config == nil {
 		t.Fatal("Could not bootstrap a config for BootstrapAllPSARoles.")
@@ -36,17 +38,12 @@ func BootstrapAllPSARoles(t *testing.T, prefix string, agentNames, roles []strin
 		t.Fatalf("Error getting project iam policy: %v", err)
 	}
 
-	members := make([]string, len(agentNames))
-	for i, agentName := range agentNames {
-		members[i] = fmt.Sprintf("serviceAccount:%s%d@%s.iam.gserviceaccount.com", prefix, project.ProjectNumber, agentName)
-	}
-
 	// Create the bindings we need to add to the policy.
 	var newBindings []*cloudresourcemanager.Binding
-	for _, role := range roles {
+	for _, member := range members {
 		newBindings = append(newBindings, &cloudresourcemanager.Binding{
-			Role:    role,
-			Members: members,
+			Role:    member.role,
+			Members: []string{strings.Replace(member.member, "{project_number}", project.ProjectNumber)},
 		})
 	}
 
@@ -68,10 +65,32 @@ func BootstrapAllPSARoles(t *testing.T, prefix string, agentNames, roles []strin
 		for _, binding := range addedBindings {
 			msg += fmt.Sprintf("Members: %q, Role: %q\n", binding.Members, binding.Role)
 		}
-		msg += "Retry the test in a few minutes."
-		t.Error(msg)
-		return true
+		msg += "Waiting for IAM to propagate."
+		t.Log(msg)
+		time.Sleep(3 * time.Minute)
 	}
+}
+
+// BootstrapAllPSARoles ensures that the given project's IAM
+// policy grants the given service agents the given roles.
+// prefix is usually "service-" and indicates the service agent should have the
+// given prefix before the project number.
+// This is important to bootstrap because using iam policy resources means that
+// deleting them removes permissions for concurrent tests.
+// Return whether the bindings changed.
+func BootstrapAllPSARoles(t *testing.T, prefix string, agentNames, roles []string) bool {
+	var members []IamMember
+	for i, agentName := range agentNames {
+		member := fmt.Sprintf("serviceAccount:%s{project_number}@%s.iam.gserviceaccount.com", prefix, agentName)
+		for _, role := range roles {
+			members = append(members, IamMember{
+				member: member,
+				role: role,
+			})
+		}
+	}
+	BootstrapIamMembers(t, members, roles)
+	// Always return false because we now wait for IAM propagation.
 	return false
 }
 
