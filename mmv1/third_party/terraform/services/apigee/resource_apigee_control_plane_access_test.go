7d99a0c4a51a101a60cbe32b2ee799bc631175c0
Akshay Pai <akshaypai@google.com>
2025-01-21 21:26:39 +0000

    Add ControlPlaneAccess support for Apigee

---
+++ mmv1/third_party/terraform/services/apigee/resource_apigee_control_plane_access_test.go
@@ -0,0 +1,155 @@
+package apigee_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
+)
+
+func TestAccApigeeControlPlaneAccess_update(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"billing_account": envvar.GetTestBillingAccountFromEnv(t),
+		"org_id":          envvar.GetTestOrgFromEnv(t),
+		"random_suffix":   acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccApigeeControlPlaneAccess_full(context),
+			},
+			{
+				ResourceName:            "google_apigee_control_plane_access.apigee_control_plane_access",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name"},
+			},
+			{
+				Config: testAccApigeeControlPlaneAccess_update(context),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("google_apigee_control_plane_access.apigee_control_plane_access", plancheck.ResourceActionUpdate),
+					},
+				},
+			},
+			{
+				ResourceName:            "google_apigee_control_plane_access.apigee_control_plane_access",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name"},
+			},
+		},
+	})
+}
+
+func testAccApigeeControlPlaneAccess_full(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_project" "project" {
+  project_id      = "tf-test-my-project%{random_suffix}"
+  name            = "tf-test-my-project%{random_suffix}"
+  org_id          = "%{org_id}"
+  billing_account = "%{billing_account}"
+  deletion_policy = "DELETE"
+}
+
+resource "google_project_service" "apigee" {
+  project = google_project.project.project_id
+  service = "apigee.googleapis.com"
+}
+
+resource "google_apigee_organization" "apigee_org" {
+  analytics_region   = "us-central1"
+  project_id         = google_project.project.project_id
+
+  runtime_type       = "HYBRID"
+  depends_on         = [google_project_service.apigee]
+}
+
+resource "google_service_account" "service_account" {
+  account_id   = "sa-%{random_suffix}"
+  display_name = "Service Account"
+}
+
+resource "google_apigee_control_plane_access" "apigee_control_plane_access" {
+  name       = google_apigee_organization.apigee_org.name
+  synchronizer_identities = [
+    "serviceAccount:${google_service_account.service_account.email}",
+  ]
+  analytics_publisher_identities = [
+    "serviceAccount:${google_service_account.service_account.email}",
+  ]
+  depends_on = [google_service_account.service_account]
+}
+`, context)
+}
+
+func testAccApigeeControlPlaneAccess_update(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_project" "project" {
+  project_id      = "tf-test-my-project%{random_suffix}"
+  name            = "tf-test-my-project%{random_suffix}"
+  org_id          = "%{org_id}"
+  billing_account = "%{billing_account}"
+  deletion_policy = "DELETE"
+}
+
+resource "google_project_service" "apigee" {
+  project = google_project.project.project_id
+  service = "apigee.googleapis.com"
+}
+
+resource "google_apigee_organization" "apigee_org" {
+  analytics_region   = "us-central1"
+  project_id         = google_project.project.project_id
+
+  runtime_type       = "HYBRID"
+  depends_on         = [google_project_service.apigee]
+}
+
+resource "google_service_account" "ax_service_account1" {
+  account_id   = "sa-ax1%{random_suffix}"
+  display_name = "Service Account"
+}
+
+resource "google_service_account" "ax_service_account2" {
+  account_id   = "sa-ax2%{random_suffix}"
+  display_name = "Service Account"
+}
+
+resource "google_service_account" "sync_server_account1" {
+  account_id   = "sa-sync1%{random_suffix}"
+  display_name = "Service Account"
+}
+
+resource "google_service_account" "sync_server_account2" {
+  account_id   = "sa-sync2%{random_suffix}"
+  display_name = "Service Account"
+}
+
+resource "google_apigee_control_plane_access" "apigee_control_plane_access" {
+  name       = google_apigee_organization.apigee_org.name
+  synchronizer_identities = [
+    "serviceAccount:${google_service_account.ax_service_account1.email}",
+    "serviceAccount:${google_service_account.ax_service_account2.email}",
+  ]
+  analytics_publisher_identities = [
+    "serviceAccount:${google_service_account.sync_server_account1.email}",
+    "serviceAccount:${google_service_account.sync_server_account2.email}",
+  ]
+  depends_on = [
+  	google_service_account.ax_service_account1,
+  	google_service_account.ax_service_account2,
+  	google_service_account.sync_server_account1,
+  	google_service_account.sync_server_account2,
+  ]
+}
+`, context)
+}
