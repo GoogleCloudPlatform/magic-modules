d271a32b4c3e2f08065d17aec2cb2dfe57b6cc25
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2025-01-22 18:02:25 +0100

    feat: Serverless VPC Access Connector update fields without recreate

--- mmv1/third_party/terraform/services/vpcaccess/data_source_vpc_access_connector_test.go
+++ mmv1/third_party/terraform/services/vpcaccess/data_source_vpc_access_connector_test.go
@@ -1,7 +1,7 @@
 package vpcaccess_test
 
 import (
-	"fmt"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -11,41 +11,109 @@ import (
 func TestAccVPCAccessConnectorDatasource_basic(t *testing.T) {
 	t.Parallel()
 
+	context := map[string]interface{}{
+		"network_name":  acctest.BootstrapSharedServiceNetworkingConnection(t, "vpc-access-connector"),
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckVPCAccessConnectorDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccVPCAccessConnectorDatasourceConfig(acctest.RandString(t, 10)),
-				Check: resource.ComposeTestCheckFunc(
-					acctest.CheckDataSourceStateMatchesResourceStateWithIgnores(
-						"data.google_vpc_access_connector.connector",
-						"google_vpc_access_connector.connector",
-						map[string]struct{}{
-							// Ignore fields not returned in response
-							"self_link": {},
-							"region":    {},
-						},
-					),
-				),
+				Config: testAccVPCAccessConnectorDatasourceConfig_basic(context),
+			},
+			{
+				ResourceName:            "google_vpc_access_connector.connector",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"region", "self_link"},
 			},
 		},
 	})
 }
 
-func testAccVPCAccessConnectorDatasourceConfig(suffix string) string {
-	return fmt.Sprintf(`
+func TestAccVPCAccessConnectorDatasource_update(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"network_name":  acctest.BootstrapSharedServiceNetworkingConnection(t, "vpc-access-connector"),
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckVPCAccessConnectorDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVPCAccessConnectorDatasourceConfig_basic(context),
+			},
+			{
+				ResourceName:            "google_vpc_access_connector.connector",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"region", "self_link"},
+			},
+			{
+				Config: testAccVPCAccessConnectorDatasourceConfig_update(context),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("google_vpc_access_connector.connector", plancheck.ResourceActionUpdate),
+					},
+				},
+			},
+			{
+				ResourceName:            "google_vpc_access_connector.connector",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"region", "self_link"},
+			},
+		},
+	})
+}
+
+func testAccVPCAccessConnectorDatasourceConfig_basic(context map[string]interface{}) string {
+	return acctest.Nprintf(`
 resource "google_vpc_access_connector" "connector" {
-  name          = "tf-test-%s"
-  ip_cidr_range  = "10.8.0.32/28"
-  network        = "default"
-  region         = "us-central1"
-  min_throughput  = 200
-  max_throughput = 300
+  name          	= "tf-test-%{random_suffix}"
+  ip_cidr_range 	= "10.8.0.32/28"
+  network 			= data.google_compute_network.default.id
+  region        	= "us-central1"
+  machine_type      = "e2-micro"
+  min_instances     = 2
+  max_instances     = 3
 }
 
 data "google_vpc_access_connector" "connector" {
   name = google_vpc_access_connector.connector.name
 }
-`, suffix)
+
+data "google_compute_network" "default" {
+  name = "%{network_name}"
+}
+`, context)
+}
+
+func testAccVPCAccessConnectorDatasourceConfig_update(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_vpc_access_connector" "connector" {
+  name          	= "tf-test-%{random_suffix}"
+  ip_cidr_range 	= "10.8.0.32/28"
+  network 			= data.google_compute_network.default.id
+  region        	= "us-central1"
+  machine_type      = "f1-micro"
+  min_instances     = 3
+  max_instances     = 5
+}
+
+data "google_vpc_access_connector" "connector" {
+  name = google_vpc_access_connector.connector.name
+}
+
+data "google_compute_network" "default" {
+  name = "%{network_name}"
+}
+`, context)
 }
