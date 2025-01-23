43c3b575f7fec422f20f8e2cae6f9213fc074844
Tommy Reddad <tommy.reddad@gmail.com>
2025-01-23 15:20:39 +0000

    Improve generated test coverage

--- mmv1/third_party/terraform/services/eventarc/resource_eventarc_trigger_test.go
+++ mmv1/third_party/terraform/services/eventarc/resource_eventarc_trigger_test.go
@@ -1,7 +1,6 @@
 package eventarc_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
@@ -10,168 +9,10 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-func TestAccEventarcTrigger_channel(t *testing.T) {
-	t.Parallel()
-
-	region := envvar.GetTestRegionFromEnv()
-	key := acctest.BootstrapKMSKeyWithPurposeInLocationAndName(t, "ENCRYPT_DECRYPT", region, "tf-bootstrap-eventarc-trigger-key")
-
-	context := map[string]interface{}{
-		"region":          region,
-		"project_name":    envvar.GetTestProjectFromEnv(),
-		"project_number":  envvar.GetTestProjectNumberFromEnv(),
-		"service_account": envvar.GetTestServiceAccountFromEnv(t),
-		"key_name":        key.CryptoKey.Name,
-		"random_suffix":   acctest.RandString(t, 10),
-	}
-
-	acctest.VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccEventarcTrigger_createTriggerWithChannelName(context),
-			},
-			{
-				ResourceName:      "google_eventarc_trigger.primary",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-		},
-	})
-}
-
-func TestAccEventarcTrigger_HttpDest(t *testing.T) {
-	t.Parallel()
-
-	region := envvar.GetTestRegionFromEnv()
-
-	testNetworkName := acctest.BootstrapSharedTestNetwork(t, "attachment-network")
-	subnetName := acctest.BootstrapSubnet(t, "tf-test-subnet", testNetworkName)
-	networkAttachmentName := acctest.BootstrapNetworkAttachment(t, "tf-test-attachment", subnetName)
-
-	// Need to have the full network attachment name in the format project/{project_id}/regions/{region_id}/networkAttachments/{networkAttachmentName}
-	fullFormNetworkAttachmentName := fmt.Sprintf("projects/%s/regions/%s/networkAttachments/%s", envvar.GetTestProjectFromEnv(), envvar.GetTestRegionFromEnv(), networkAttachmentName)
-
-	context := map[string]interface{}{
-		"region":             region,
-		"project_name":       envvar.GetTestProjectFromEnv(),
-		"project_number":     envvar.GetTestProjectNumberFromEnv(),
-		"service_account":    envvar.GetTestServiceAccountFromEnv(t),
-		"network_attachment": fullFormNetworkAttachmentName,
-		"random_suffix":      acctest.RandString(t, 10),
-	}
-
-	acctest.VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccEventarcTrigger_createTriggerWithHttpDest(context),
-			},
-			{
-				ResourceName:      "google_eventarc_trigger.primary",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-		},
-	})
-}
-
-func testAccEventarcTrigger_createTriggerWithChannelName(context map[string]interface{}) string {
-	return acctest.Nprintf(`
-resource "google_kms_crypto_key_iam_member" "key_member" {
-	crypto_key_id = "%{key_name}"
-	role      = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-	member = "serviceAccount:service-%{project_number}@gcp-sa-eventarc.iam.gserviceaccount.com"
-}
-
-resource "google_eventarc_channel" "test_channel" {
-	location = "%{region}"
-	name     = "tf-test-channel%{random_suffix}"
-	crypto_key_name = "%{key_name}"
-	third_party_provider = "projects/%{project_name}/locations/%{region}/providers/datadog"
-	depends_on = [google_kms_crypto_key_iam_member.key_member]
-}
-
-resource "google_cloud_run_service" "default" {
-	name     = "tf-test-eventarc-service%{random_suffix}"
-	location = "%{region}"
-
-	metadata {
-		namespace = "%{project_name}"
-	}
-
-	template {
-		spec {
-			containers {
-				image = "gcr.io/cloudrun/hello"
-				ports {
-					container_port = 8080
-				}
-			}
-			container_concurrency = 50
-			timeout_seconds = 100
-		}
-	}
-
-	traffic {
-		percent         = 100
-		latest_revision = true
-	}
-}
-
-resource "google_eventarc_trigger" "primary" {
-	name = "tf-test-trigger%{random_suffix}"
-	location = "%{region}"
-	matching_criteria {
-		attribute = "type"
-		value = "datadog.v1.alert"
-	}
-	destination {
-		cloud_run_service {
-			service = google_cloud_run_service.default.name
-			region = "%{region}"
-		}
-	}
-	service_account = "%{service_account}"
-
-	channel = "projects/%{project_name}/locations/%{region}/channels/${google_eventarc_channel.test_channel.name}"
-
-    depends_on = [google_cloud_run_service.default,google_eventarc_channel.test_channel]
-}
-`, context)
-}
-
-func testAccEventarcTrigger_createTriggerWithHttpDest(context map[string]interface{}) string {
-	return acctest.Nprintf(`
-resource "google_eventarc_trigger" "primary" {
-	name = "tf-test-trigger%{random_suffix}"
-	location = "%{region}"
-	matching_criteria {
-		attribute = "type"
-		value = "google.cloud.pubsub.topic.v1.messagePublished"
-	}
-	destination {
-		http_endpoint {
-			uri = "http://10.10.10.8:80/route"
-		}
-                network_config {
-                        network_attachment = "%{network_attachment}"
-                }
-
-	}
-	service_account = "%{service_account}"
-
-}
-`, context)
-}
-
 func TestAccEventarcTrigger_BasicHandWritten(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"project_name":  envvar.GetTestProjectFromEnv(),
 		"region":        envvar.GetTestRegionFromEnv(),
 		"random_suffix": acctest.RandString(t, 10),
 	}
@@ -239,10 +80,6 @@ resource "google_cloud_run_service" "default" {
 	name     = "tf-test-eventarc-service%{random_suffix}"
 	location = "europe-west1"
 
-	metadata {
-		namespace = "%{project_name}"
-	}
-
 	template {
 		spec {
 			containers {
@@ -295,10 +132,6 @@ resource "google_cloud_run_service" "default" {
 	name     = "tf-test-eventarc-service%{random_suffix}"
 	location = "europe-west1"
 
-	metadata {
-		namespace = "%{project_name}"
-	}
-
 	template {
 		spec {
 			containers {
@@ -321,10 +154,6 @@ resource "google_cloud_run_service" "default" {
 resource "google_cloud_run_service" "default2" {
 	name     = "tf-test-eventarc-service%{random_suffix}2"
 	location = "europe-north1"
-
-	metadata {
-		namespace = "%{project_name}"
-	}
 
 	template {
 		spec {
@@ -387,10 +216,6 @@ resource "google_cloud_run_service" "default" {
 	name     = "tf-test-eventarc-service%{random_suffix}"
 	location = "europe-west1"
 
-	metadata {
-		namespace = "%{project_name}"
-	}
-
 	template {
 		spec {
 			containers {
@@ -414,10 +239,6 @@ resource "google_cloud_run_service" "default2" {
 	name     = "tf-test-eventarc-service%{random_suffix}2"
 	location = "europe-north1"
 
-	metadata {
-		namespace = "%{project_name}"
-	}
-
 	template {
 		spec {
 			containers {
@@ -440,84 +261,7 @@ resource "google_cloud_run_service" "default2" {
 `, context)
 }
 
-func TestAccEventarcTrigger_workflow(t *testing.T) {
-	t.Parallel()
-
-	context := map[string]interface{}{
-		"project_name":    envvar.GetTestProjectFromEnv(),
-		"service_account": envvar.GetTestServiceAccountFromEnv(t),
-		"region":          envvar.GetTestRegionFromEnv(),
-		"random_suffix":   acctest.RandString(t, 10),
-	}
-
-	acctest.VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccEventarcTrigger_workflow(context),
-			},
-			{
-				ResourceName:            "google_eventarc_trigger.primary",
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"labels", "terraform_labels"},
-			},
-		},
-	})
-}
-
-func testAccEventarcTrigger_workflow(context map[string]interface{}) string {
-	return acctest.Nprintf(`
-resource "google_eventarc_trigger" "primary" {
-	name = "tf-test-name%{random_suffix}"
-	location = "%{region}"
-	matching_criteria {
-		attribute = "type"
-		value = "google.cloud.pubsub.topic.v1.messagePublished"
-	}
-	destination {
-		workflow = google_workflows_workflow.example.id
-	}
-	service_account = "%{service_account}"
-}
-
-resource "google_workflows_workflow" "example" {
-  name = "tf-test-eventarc-workflow%{random_suffix}"
-  deletion_protection = false
-  region = "%{region}"
-  source_contents = <<-EOF
-  # This is a sample workflow, feel free to replace it with your source code
-  #
-  # This workflow does the following:
-  # - reads current time and date information from an external API and stores
-  #   the response in CurrentDateTime variable
-  # - retrieves a list of Wikipedia articles related to the day of the week
-  #   from CurrentDateTime
-  # - returns the list of articles as an output of the workflow
-  # FYI, In terraform you need to escape the $$ or it will cause errors.
-
-  - getCurrentTime:
-      call: http.get
-      args:
-          url: $${sys.get_env("url")}
-      result: CurrentDateTime
-  - readWikipedia:
-      call: http.get
-      args:
-          url: https://en.wikipedia.org/w/api.php
-          query:
-              action: opensearch
-              search: $${CurrentDateTime.body.dayOfTheWeek}
-      result: WikiResult
-  - returnOutput:
-      return: $${WikiResult.body[1]}
-EOF
-}
-`, context)
-}
-
-func TestAccEventarcTrigger_alternateForm(t *testing.T) {
+func TestAccEventarcTrigger_AlternateForm(t *testing.T) {
 	t.Parallel()
 
 	region := envvar.GetTestRegionFromEnv()
@@ -533,7 +277,7 @@ func TestAccEventarcTrigger_alternateForm(t *testing.T) {
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccEventarcTrigger_alternateForm(context),
+				Config: testAccEventarcTrigger_AlternateForm(context),
 			},
 			{
 				ResourceName:      "google_eventarc_trigger.primary",
@@ -546,7 +290,7 @@ func TestAccEventarcTrigger_alternateForm(t *testing.T) {
 	})
 }
 
-func testAccEventarcTrigger_alternateForm(context map[string]interface{}) string {
+func testAccEventarcTrigger_AlternateForm(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 resource "google_pubsub_topic" "foo" {
 	name = "tf-test-topic%{random_suffix}"
@@ -555,10 +299,6 @@ resource "google_pubsub_topic" "foo" {
 resource "google_cloud_run_service" "default" {
 	name     = "tf-test-eventarc-service%{random_suffix}"
 	location = "%{region}"
-
-	metadata {
-		namespace = "%{project_name}"
-	}
 
 	template {
 		spec {
@@ -603,7 +343,7 @@ resource "google_eventarc_trigger" "primary" {
 `, context)
 }
 
-func TestAccEventarcTrigger_shortFormWorkflow(t *testing.T) {
+func TestAccEventarcTrigger_ShortFormWorkflow(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
@@ -618,7 +358,7 @@ func TestAccEventarcTrigger_shortFormWorkflow(t *testing.T) {
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccEventarcTrigger_shortFormWorkflow(context),
+				Config: testAccEventarcTrigger_ShortFormWorkflow(context),
 			},
 			{
 				ResourceName:            "google_eventarc_trigger.primary",
@@ -630,7 +370,7 @@ func TestAccEventarcTrigger_shortFormWorkflow(t *testing.T) {
 	})
 }
 
-func testAccEventarcTrigger_shortFormWorkflow(context map[string]interface{}) string {
+func testAccEventarcTrigger_ShortFormWorkflow(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 resource "google_eventarc_trigger" "primary" {
 	name = "tf-test-name%{random_suffix}"
