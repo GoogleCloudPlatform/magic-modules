a60b6688afc3918dca6b3d0c78758f15c42806b6
Tommy Reddad <tommy.reddad@gmail.com>
2025-01-23 16:23:38 +0000

    Initial commit for Channel DCL MMv1 migration

--- mmv1/third_party/terraform/services/eventarc/resource_eventarc_channel_test.go
+++ mmv1/third_party/terraform/services/eventarc/resource_eventarc_channel_test.go
@@ -1,20 +1,13 @@
 package eventarc_test
 
 import (
-	"context"
-	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 	"github.com/hashicorp/terraform-provider-google/google/envvar"
 	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
 
-	dcl "github.com/GoogleCloudPlatform/declarative-resource-client-library/dcl"
-	eventarc "github.com/GoogleCloudPlatform/declarative-resource-client-library/services/google/eventarc{{ $.DCLVersion }}"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/terraform"
-	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
 )
 
 func TestAccEventarcChannel_basic(t *testing.T) {
@@ -29,7 +22,6 @@ func TestAccEventarcChannel_basic(t *testing.T) {
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckEventarcChannelDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccEventarcChannel_basic(context),
@@ -62,7 +54,6 @@ func TestAccEventarcChannel_cryptoKeyUpdate(t *testing.T) {
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckEventarcChannelDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccEventarcChannel_setCryptoKey(context),
@@ -163,45 +154,4 @@ resource "google_eventarc_channel" "primary" {
 	depends_on = [google_kms_crypto_key_iam_member.key2_member]
 }
 `, context)
-}
-
-func testAccCheckEventarcChannelDestroyProducer(t *testing.T) func(s *terraform.State) error {
-	return func(s *terraform.State) error {
-		for name, rs := range s.RootModule().Resources {
-			if rs.Type != "rs.google_eventarc_channel" {
-				continue
-			}
-			if strings.HasPrefix(name, "data.") {
-				continue
-			}
-
-			config := acctest.GoogleProviderConfig(t)
-
-			billingProject := ""
-			if config.BillingProject != "" {
-				billingProject = config.BillingProject
-			}
-
-			obj := &eventarc.Channel{
-				Location:           dcl.String(rs.Primary.Attributes["location"]),
-				Name:               dcl.String(rs.Primary.Attributes["name"]),
-				CryptoKeyName:      dcl.String(rs.Primary.Attributes["crypto_key_name"]),
-				Project:            dcl.StringOrNil(rs.Primary.Attributes["project"]),
-				ThirdPartyProvider: dcl.String(rs.Primary.Attributes["third_party_provider"]),
-				ActivationToken:    dcl.StringOrNil(rs.Primary.Attributes["activation_token"]),
-				CreateTime:         dcl.StringOrNil(rs.Primary.Attributes["create_time"]),
-				PubsubTopic:        dcl.StringOrNil(rs.Primary.Attributes["pubsub_topic"]),
-				State:              eventarc.ChannelStateEnumRef(rs.Primary.Attributes["state"]),
-				Uid:                dcl.StringOrNil(rs.Primary.Attributes["uid"]),
-				UpdateTime:         dcl.StringOrNil(rs.Primary.Attributes["update_time"]),
-			}
-
-			client := transport_tpg.NewDCLEventarcClient(config, config.UserAgent, billingProject, 0)
-			_, err := client.GetChannel(context.Background(), obj)
-			if err == nil {
-				return fmt.Errorf("google_eventarc_channel still exists %v", obj)
-			}
-		}
-		return nil
-	}
 }
