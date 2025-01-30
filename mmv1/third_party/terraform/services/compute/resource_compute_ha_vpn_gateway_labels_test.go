69827dec05ad17223197e01d5fbf67475fe28a36
Amit Srivastava <iamamit@google.com>
2025-01-30 04:42:40 +0000

    Add update test

---
+++ mmv1/third_party/terraform/services/compute/resource_compute_ha_vpn_gateway_labels_test.go
@@ -0,0 +1,64 @@
+package compute_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+)
+
+func TestAccComputeHaVpnGateway_updateLabels(t *testing.T) {
+	t.Parallel()
+
+	rnd := acctest.RandString(t, 10)
+	resourceName := "google_compute_ha_vpn_gateway.ha_gateway1"
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeHaVpnGateway_updateLabels(rnd, "test", "test"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "labels.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "labels.test", "test"),
+				),
+			},
+			{
+				ResourceName:            resourceName
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "network", "region", "terraform_labels"},
+			},
+			{
+				Config: testAccComputeHaVpnGateway_updateLabels(rnd, "test-updated", "test-updated"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "labels.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "labels.test-updated", "test-updated"),
+				),
+			},
+			{
+				ResourceName:            resourceName
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "network", "region", "terraform_labels"},
+			},
+		},
+	})
+}
+
+func testAccComputeHaVpnGateway_updateLabels(suffix, key, value string) string {
+	return acctest.Nprintf(`
+resource "google_compute_ha_vpn_gateway" "ha_gateway1" {
+  region   = "us-central1"
+  name     = "tf-test-ha-vpn-1%s"
+  network  = google_compute_network.network1.id
+  labels = {
+    %s = "%s"
+  }
+}
+`, suffix, key, value)
+}
