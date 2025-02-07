03d7aff962c82bd69ea0cd3e68758e1bbda56692
Cameron Thornton <camthornton@google.com>
2025-02-07 14:13:07 -0600

    make TestAccFilestoreInstance_replication sweepable

--- mmv1/third_party/terraform/services/filestore/resource_filestore_instance_test.go
+++ mmv1/third_party/terraform/services/filestore/resource_filestore_instance_test.go
@@ -491,7 +491,7 @@ func TestAccFilestoreInstance_replication(t *testing.T) {
 func testAccFilestoreInstance_replication(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 resource "google_filestore_instance" "instance" {
-  name             = "tf-instance-%{random_suffix}"
+  name             = "tf-test-instance-%{random_suffix}"
   location         = "%{location_1}"
   tier             = "%{tier}"
   description      = "An instance created during testing."
@@ -508,7 +508,7 @@ resource "google_filestore_instance" "instance" {
 }
 
 resource "google_filestore_instance" "replica-instance" {
-  name          	= "tf-rinstance-%{random_suffix}"
+  name          	= "tf-test-instance-%{random_suffix}"
   location      	= "%{location_2}"
   tier          	= "%{tier}"
   description   	= "An replica instance created during testing."
