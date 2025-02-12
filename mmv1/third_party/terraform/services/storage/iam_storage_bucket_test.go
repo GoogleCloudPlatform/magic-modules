3ea40af5a45e6e0995b4f8e4434379715d86dcbf
gurusai-voleti <gvoleti@google.com>
2025-02-12 16:12:17 +0530

    Update iam_storage_bucket_test.go

--- mmv1/third_party/terraform/services/storage/iam_storage_bucket_test.go
+++ mmv1/third_party/terraform/services/storage/iam_storage_bucket_test.go
@@ -14,80 +14,6 @@ import (
 	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
 )
 
-func TestAccStorageBucketIamPolicy_destroy(t *testing.T) {
-	t.Parallel()
-
-	acctest.VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccStorageBucketIamPolicy_destroy(),
-			},
-		},
-	})
-}
-
-func testAccStorageBucketIamPolicy_destroy() string {
-	return fmt.Sprintf(`
-resource "google_service_account" "accessor" {
-  account_id = "pub-sub-test-service-account"
-}
-
-resource "google_storage_bucket" "test_bucket" {
-  name          = "sd-pubsub-test-bucket"
-  location      = "US"
-  storage_class = "STANDARD"
-
-  uniform_bucket_level_access = true
-  public_access_prevention    = "enforced"
-
-  force_destroy = true
-}
-
-data "google_iam_policy" "bucket_policy_data" {
-  binding {
-    role = "roles/storage.admin"
-
-    members = ["serviceAccount:${google_service_account.accessor.email}"]
-  }
-}
-
-resource "google_storage_bucket_iam_policy" "bucket_policy" {
-  bucket      = google_storage_bucket.test_bucket.name
-  policy_data = data.google_iam_policy.bucket_policy_data.policy_data
-}
-
-resource "google_pubsub_topic" "topic" {
-  name = "sd-pubsub-test-bucket-topic"
-}
-
-resource "google_storage_notification" "storage_notification" {
-  bucket         = google_storage_bucket.test_bucket.name
-  payload_format = "JSON_API_V1"
-  topic          = google_pubsub_topic.topic.id
-
-  depends_on = [google_pubsub_topic_iam_policy.topic_policy]
-}
-
-data "google_storage_project_service_account" "gcs_account" {}
-
-data "google_iam_policy" "topic_policy_data" {
-  binding {
-    role = "roles/pubsub.publisher"
-    members = [
-      "serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"
-    ]
-  }
-}
-
-resource "google_pubsub_topic_iam_policy" "topic_policy" {
-  topic       = google_pubsub_topic.topic.name
-  policy_data = data.google_iam_policy.topic_policy_data.policy_data
-}
-`)
-}
-
 func TestAccStorageBucket_basicIamBinding(t *testing.T) {
 	t.Parallel()
 
@@ -401,6 +327,20 @@ func TestAccStorageBucket_iamPolicyGeneratedWithCondition(t *testing.T) {
 	})
 }
 
+func TestAccStorageBucketIamPolicy_destroy(t *testing.T) {
+	t.Parallel()
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccStorageBucketIamPolicy_destroy(),
+			},
+		},
+	})
+}
+
 func testAccStorageBucket_basicIamMember(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 resource "google_storage_bucket" "default" {
@@ -659,4 +599,64 @@ resource "google_storage_bucket_iam_policy" "foo" {
   policy_data = data.google_iam_policy.foo.policy_data
 }
 `, context)
+}
+
+func testAccStorageBucketIamPolicy_destroy() string {
+	return fmt.Sprintf(`
+resource "google_service_account" "accessor" {
+  account_id = "pub-sub-test-service-account"
+}
+
+resource "google_storage_bucket" "test_bucket" {
+  name          = "sd-pubsub-test-bucket"
+  location      = "US"
+  storage_class = "STANDARD"
+
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+
+  force_destroy = true
+}
+
+data "google_iam_policy" "bucket_policy_data" {
+  binding {
+    role = "roles/storage.admin"
+
+    members = ["serviceAccount:${google_service_account.accessor.email}"]
+  }
+}
+
+resource "google_storage_bucket_iam_policy" "bucket_policy" {
+  bucket      = google_storage_bucket.test_bucket.name
+  policy_data = data.google_iam_policy.bucket_policy_data.policy_data
+}
+
+resource "google_pubsub_topic" "topic" {
+  name = "sd-pubsub-test-bucket-topic"
+}
+
+resource "google_storage_notification" "storage_notification" {
+  bucket         = google_storage_bucket.test_bucket.name
+  payload_format = "JSON_API_V1"
+  topic          = google_pubsub_topic.topic.id
+
+  depends_on = [google_pubsub_topic_iam_policy.topic_policy]
+}
+
+data "google_storage_project_service_account" "gcs_account" {}
+
+data "google_iam_policy" "topic_policy_data" {
+  binding {
+    role = "roles/pubsub.publisher"
+    members = [
+      "serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"
+    ]
+  }
+}
+
+resource "google_pubsub_topic_iam_policy" "topic_policy" {
+  topic       = google_pubsub_topic.topic.name
+  policy_data = data.google_iam_policy.topic_policy_data.policy_data
+}
+`)
 }
