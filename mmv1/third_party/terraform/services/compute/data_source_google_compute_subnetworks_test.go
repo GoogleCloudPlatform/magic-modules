136d731ef9c0640e54b67bfdcb35d5c2ddfcfac4
gvijbha <151831882+gvijbha@users.noreply.github.com>
2025-02-18 12:25:52 +0530

    Update data_source_google_compute_subnetworks_test.go

--- mmv1/third_party/terraform/services/compute/data_source_google_compute_subnetworks_test.go
+++ mmv1/third_party/terraform/services/compute/data_source_google_compute_subnetworks_test.go
@@ -51,10 +51,10 @@ func TestAccDataSourceGoogleSubnetworks_update(t *testing.T) {
 			{
 				Config: testAccCheckGoogleSubnetworksConfig(context),
 				Check: resource.ComposeTestCheckFunc(
-				// Test schema
-				resource.TestCheckResourceAttrSet("data.google_compute_subnetworks.all", "subnetworks.1.purpose"),
-				// Test Content
-				resource.TestCheckResourceAttr("data.google_compute_subnetworks.two", "subnetworks.0.purpose", "PEER_MIGRATION"),
+					// Test schema
+					resource.TestCheckResourceAttrSet("data.google_compute_subnetworks.all", "subnetworks.1.purpose"),
+					// Test Content
+					resource.TestCheckResourceAttr("data.google_compute_subnetworks.two", "subnetworks.0.purpose", "PEER_MIGRATION"),
 				),
 			},
 			{
@@ -66,12 +66,12 @@ func TestAccDataSourceGoogleSubnetworks_update(t *testing.T) {
 			{
 				Config: testAccCheckGoogleSubnetworks_update(context),
 				Check: resource.ComposeTestCheckFunc(
-				// Test schema
-				resource.TestCheckResourceAttrSet("data.google_compute_subnetworks.all", "subnetworks.1.purpose"),
-				// Test Content
-				resource.TestCheckResourceAttr("data.google_compute_subnetworks.all", "id", id),
-				resource.TestCheckResourceAttr("data.google_compute_subnetworks.one", "subnetworks.0.purpose", "PRIVATE"),
-	                        resource.TestCheckResourceAttr("data.google_compute_subnetworks.two", "subnetworks.0.purpose", "PRIVATE"),
+					// Test schema
+					resource.TestCheckResourceAttrSet("data.google_compute_subnetworks.all", "subnetworks.1.purpose"),
+					// Test Content
+					resource.TestCheckResourceAttr("data.google_compute_subnetworks.all", "id", id),
+					resource.TestCheckResourceAttr("data.google_compute_subnetworks.one", "subnetworks.0.purpose", "PRIVATE"),
+					resource.TestCheckResourceAttr("data.google_compute_subnetworks.two", "subnetworks.0.purpose", "PRIVATE"),
 				),
 			},
 			{
