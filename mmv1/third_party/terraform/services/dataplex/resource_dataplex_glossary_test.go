1534689df89fe946913fa8261a809b318f21cfbc
Sachin Rungta <rungtasachin@google.com>
2025-02-18 16:35:52 +0000

    update test

--- mmv1/third_party/terraform/services/dataplex/resource_dataplex_glossary_test.go
+++ mmv1/third_party/terraform/services/dataplex/resource_dataplex_glossary_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-provider-google/google/envvar"
 )
 
-func TestAccDataplexGlossary_dataplexGlossaryBasic(t *testing.T) {
+func TestAccDataplexGlossary_update(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
@@ -23,7 +23,7 @@ func TestAccDataplexGlossary_dataplexGlossaryBasic(t *testing.T) {
 		CheckDestroy:             testAccCheckDataplexGlossaryDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataplexGlossary_dataplexGlossaryBasic(context),
+				Config: testAccDataplexGlossary_update(context),
 			},
 			{
 				ResourceName:            "google_dataplex_glossary.glossary_test_id",
@@ -31,33 +31,8 @@ func TestAccDataplexGlossary_dataplexGlossaryBasic(t *testing.T) {
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{"glossary_id", "labels", "location", "terraform_labels"},
 			},
-		},
-	})
-}
-
-func testAccDataplexGlossary_dataplexGlossaryBasic(context map[string]interface{}) string {
-	return acctest.Nprintf(`
-resource "google_dataplex_glossary" "glossary_test_id" {
-  glossary_id = "tf-test-glossary-basic%{random_suffix}"
-}
-`, context)
-}
-
-func TestAccDataplexGlossary_dataplexGlossaryFull(t *testing.T) {
-	t.Parallel()
-
-	context := map[string]interface{}{
-		"project_name":  envvar.GetTestProjectFromEnv(),
-		"random_suffix": acctest.RandString(t, 10),
-	}
-
-	acctest.VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckDataplexGlossaryDestroyProducer(t),
-		Steps: []resource.TestStep{
 			{
-				Config: testAccDataplexGlossary_dataplexGlossaryFull(context),
+				Config: testAccDataplexGlossary_full(context),
 			},
 			{
 				ResourceName:            "google_dataplex_glossary.glossary_test_id_full",
@@ -69,10 +44,23 @@ func TestAccDataplexGlossary_dataplexGlossaryFull(t *testing.T) {
 	})
 }
 
-func testAccDataplexGlossary_dataplexGlossaryFull(context map[string]interface{}) string {
+func testAccDataplexGlossary_update(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_dataplex_glossary" "glossary_test_id" {
+  glossary_id = "tf-test-glossary-basic%{random_suffix}"
+  location     = "us-central1"
+  project      = "%{project_name}"
+}
+`, context)
+}
+
+func testAccDataplexGlossary_full(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 resource "google_dataplex_glossary" "glossary_test_id_full" {
   glossary_id = "tf-test-glossary-full%{random_suffix}"
+  location     = "us-central1"
+  project      = "%{project_name}"
+
   labels = { "tag": "test-tf" }
   display_name = "terraform glossary"
   description = "glossary created by Terraform"
