0d23ebb655bebadee22d8f5946ab674379533fa5
Sachin Rungta <rungtasachin@google.com>
2025-02-19 12:40:28 +0000

    typo in provider

--- mmv1/third_party/terraform/services/dataplex/resource_dataplex_glossary_test.go
+++ mmv1/third_party/terraform/services/dataplex/resource_dataplex_glossary_test.go
@@ -19,7 +19,7 @@ func TestAccDataplexGlossary_update(t *testing.T) {
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5BetaProviderFactories(t),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataplexGlossary_update(context),
