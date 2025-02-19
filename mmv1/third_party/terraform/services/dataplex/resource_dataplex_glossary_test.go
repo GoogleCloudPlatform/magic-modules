d6198b023c1463cb889bd87a6d398c8895da4c8e
Sachin Rungta <rungtasachin@google.com>
2025-02-19 12:29:23 +0000

    update status to beta

--- mmv1/third_party/terraform/services/dataplex/resource_dataplex_glossary_test.go
+++ mmv1/third_party/terraform/services/dataplex/resource_dataplex_glossary_test.go
@@ -19,8 +19,7 @@ func TestAccDataplexGlossary_update(t *testing.T) {
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckDataplexGlossaryDestroyProducer(t),
+		ProtoV5ProviderFactories: acctest.ProtoV5BetaProviderFactories(t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataplexGlossary_update(context),
