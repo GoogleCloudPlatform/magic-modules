f7b18a8671b5bb97dc5f4b8f8bcf6566d9c80f9e
gvijbha <151831882+gvijbha@users.noreply.github.com>
2025-02-20 17:44:49 +0530

    Update data_source_google_compute_subnetworks.go

--- mmv1/third_party/terraform/services/compute/data_source_google_compute_subnetworks.go
+++ mmv1/third_party/terraform/services/compute/data_source_google_compute_subnetworks.go
@@ -57,10 +57,6 @@ func DataSourceGoogleComputeSubnetworks() *schema.Resource {
 							Type:     schema.TypeBool,
 							Computed: true,
 						},
-						"purpose": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
 						"self_link": {
 							Type:     schema.TypeString,
 							Computed: true,
