d125fa2eea0489b2535ba347b344f75dc360b594
Chris Hawk <hawk@google.com>
2025-02-28 12:05:19 -0800

    Reset org settings before attempting folder creation

--- mmv1/third_party/terraform/services/logging/resource_logging_bucket_config_test.go
+++ mmv1/third_party/terraform/services/logging/resource_logging_bucket_config_test.go
@@ -16,15 +16,11 @@ func TestAccLoggingBucketConfigFolder_basic(t *testing.T) {
 		"random_suffix": acctest.RandString(t, 10),
 		"folder_name":   "tf-test-" + acctest.RandString(t, 10),
 		"org_id":        envvar.GetTestOrgFromEnv(t),
-		"bucket_id":     "tf-test-bucket-" + acctest.RandString(t, 10),
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		ExternalProviders: map[string]resource.ExternalProvider{
-			"time": {},
-		},
 		Steps: []resource.TestStep{
 			{
 				Config: testAccLoggingBucketConfigFolder_basic(context, 30),
@@ -256,19 +252,16 @@ func TestAccLoggingBucketConfigOrganization_basic(t *testing.T) {
 
 func testAccLoggingBucketConfigFolder_basic(context map[string]interface{}, retention int) string {
 	return fmt.Sprintf(acctest.Nprintf(`
+// Reset the default bucket and location settings, which may have been changed by other tests.
+resource "google_logging_organization_settings" "default" {
+  organization = "%{org_id}"
+}
+
 resource "google_folder" "default" {
 	display_name = "%{folder_name}"
 	parent       = "organizations/%{org_id}"
 	deletion_protection = false
-}
-
-// Give the _Default bucket a chance to be created
-resource "time_sleep" "wait_1_minute" {
-	create_duration = "1m"
-
-	depends_on = [
-	  google_folder.default,
-	]
+	depends_on = [google_logging_organization_settings.default]
 }
 
 resource "google_logging_folder_bucket_config" "basic" {
@@ -276,9 +269,7 @@ resource "google_logging_folder_bucket_config" "basic" {
 	location  = "global"
 	retention_days = %d
 	description = "retention test %d days"
-	bucket_id = "%{bucket_id}"
-
-	depends_on = [time_sleep.wait_1_minute]
+	bucket_id = "_Default"
 }
 `, context), retention, retention)
 }
