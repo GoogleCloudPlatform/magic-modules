5a6a531cbed2b7c116ca295e0202c05b0f448efc
Tommy Reddad <tommy.reddad@gmail.com>
2025-03-03 16:13:07 +0000

    Bootstrap IAM in pipeline tests

--- mmv1/third_party/terraform/services/eventarc/resource_eventarc_pipeline_test.go
+++ mmv1/third_party/terraform/services/eventarc/resource_eventarc_pipeline_test.go
@@ -17,20 +17,22 @@ func TestAccEventarcPipeline_update(t *testing.T) {
 	context := map[string]interface{}{
 		"region":                  region,
 		"project_id":              envvar.GetTestProjectFromEnv(),
-		"project_number":          envvar.GetTestProjectNumberFromEnv(),
 		"key1_name":               acctest.BootstrapKMSKeyWithPurposeInLocationAndName(t, "ENCRYPT_DECRYPT", region, "tf-bootstrap-eventarc-pipeline-key1").CryptoKey.Name,
 		"key2_name":               acctest.BootstrapKMSKeyWithPurposeInLocationAndName(t, "ENCRYPT_DECRYPT", region, "tf-bootstrap-eventarc-pipeline-key2").CryptoKey.Name,
 		"network_attachment_name": acctest.BootstrapNetworkAttachment(t, "tf-test-eventarc-pipeline-na", acctest.BootstrapSubnet(t, "tf-test-eventarc-pipeline-subnet", acctest.BootstrapSharedTestNetwork(t, "tf-test-eventarc-pipeline-network"))),
 		"random_suffix":           acctest.RandString(t, 10),
 	}
+	acctest.BootstrapIamMembers(t, []acctest.IamMember{
+		{
+			Member: "serviceAccount:service-{project_number}@gcp-sa-eventarc.iam.gserviceaccount.com",
+			Role:   "roles/cloudkms.cryptoKeyEncrypterDecrypter",
+		},
+	})
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		CheckDestroy:             testAccCheckEventarcPipelineDestroyProducer(t),
-		ExternalProviders: map[string]resource.ExternalProvider{
-			"time": {},
-		},
 		Steps: []resource.TestStep{
 			{
 				Config: testAccEventarcPipeline_full(context),
@@ -61,22 +63,14 @@ func TestAccEventarcPipeline_update(t *testing.T) {
 
 func testAccEventarcPipeline_full(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_kms_crypto_key_iam_member" "key1_member" {
-  crypto_key_id = "%{key1_name}"
-  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member        = "serviceAccount:service-%{project_number}@gcp-sa-eventarc.iam.gserviceaccount.com"
-}
-
-resource "google_kms_crypto_key_iam_member" "key2_member" {
-  crypto_key_id = "%{key2_name}"
-  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member        = "serviceAccount:service-%{project_number}@gcp-sa-eventarc.iam.gserviceaccount.com"
-}
-
 resource "google_eventarc_pipeline" "primary" {
   location        = "%{region}"
   pipeline_id     = "tf-test-some-pipeline%{random_suffix}"
   crypto_key_name = "%{key1_name}"
+  display_name    = "some pipeline"
+  logging_config {
+    log_severity = "ALERT"
+  }
   destinations {
     http_endpoint {
       uri = "https://10.77.0.0:80/route"
@@ -85,33 +79,20 @@ resource "google_eventarc_pipeline" "primary" {
       network_attachment = "projects/%{project_id}/regions/%{region}/networkAttachments/%{network_attachment_name}"
     }
   }
-  depends_on = [google_kms_crypto_key_iam_member.key1_member, google_kms_crypto_key_iam_member.key2_member]
 }
 `, context)
 }
 
 func testAccEventarcPipeline_update(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_kms_crypto_key_iam_member" "key1_member" {
-  crypto_key_id = "%{key1_name}"
-  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member        = "serviceAccount:service-%{project_number}@gcp-sa-eventarc.iam.gserviceaccount.com"
-}
-
-resource "google_kms_crypto_key_iam_member" "key2_member" {
-  crypto_key_id = "%{key2_name}"
-  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member        = "serviceAccount:service-%{project_number}@gcp-sa-eventarc.iam.gserviceaccount.com"
-}
-
-resource "time_sleep" "pre_wait_600s" {
-  create_duration = "600s"
-}
-
 resource "google_eventarc_pipeline" "primary" {
   location        = "%{region}"
   pipeline_id     = "tf-test-some-pipeline%{random_suffix}"
   crypto_key_name = "%{key2_name}"
+  display_name    = "updated pipeline"
+  logging_config {
+    log_severity = "DEBUG"
+  }
   destinations {
     http_endpoint {
       uri = "https://10.77.0.0:80/route"
@@ -120,12 +101,6 @@ resource "google_eventarc_pipeline" "primary" {
       network_attachment = "projects/%{project_id}/regions/%{region}/networkAttachments/%{network_attachment_name}"
     }
   }
-  depends_on = [time_sleep.pre_wait_600s, google_kms_crypto_key_iam_member.key1_member, google_kms_crypto_key_iam_member.key2_member]
-}
-
-resource "time_sleep" "post_wait_600s" {
-  create_duration = "600s"
-  depends_on      = [google_eventarc_pipeline.primary]
 }
 `, context)
 }
