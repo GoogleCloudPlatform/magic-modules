9bb055361f02e1d21c06bbef3321e800ed5890ad
Tommy Reddad <tommy.reddad@gmail.com>
2025-03-06 13:35:36 +0000

    Rename some steps and add sleep after creating service agent

--- mmv1/third_party/terraform/services/eventarc/resource_eventarc_google_api_source_test.go
+++ mmv1/third_party/terraform/services/eventarc/resource_eventarc_google_api_source_test.go
@@ -78,7 +78,7 @@ func TestAccEventarcGoogleApiSource_update(t *testing.T) {
 // to a MessageBus in the same project
 func testAccEventarcGoogleApiSource_full(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_project" "project_1" {
+resource "google_project" "project" {
   project_id      = "tf-test%{random_suffix}"
   name            = "tf-test%{random_suffix}"
   org_id          = "%{org_id}"
@@ -86,83 +86,89 @@ resource "google_project" "project_1" {
   deletion_policy = "DELETE"
 }
 
-resource "time_sleep" "wait_create_project_1" {
+resource "time_sleep" "wait_create_project" {
   create_duration = "60s"
-  depends_on      = [google_project.project_1]
+  depends_on      = [google_project.project]
 }
 
-resource "google_project_service" "compute_1" {
-  project    = google_project.project_1.project_id
+resource "google_project_service" "compute" {
+  project    = google_project.project.project_id
   service    = "compute.googleapis.com"
-  depends_on = [time_sleep.wait_create_project_1]
+  depends_on = [time_sleep.wait_create_project]
 }
 
-resource "google_project_service" "servicenetworking_1" {
-  project   = google_project.project_1.project_id
+resource "google_project_service" "servicenetworking" {
+  project   = google_project.project.project_id
   service   = "servicenetworking.googleapis.com"
-  depends_on = [google_project_service.compute_1]
+  depends_on = [google_project_service.compute]
 }
 
-resource "google_project_service" "kms_1" {
-  project    = google_project.project_1.project_id
+resource "google_project_service" "kms" {
+  project    = google_project.project.project_id
   service    = "cloudkms.googleapis.com"
-  depends_on = [google_project_service.servicenetworking_1]
+  depends_on = [google_project_service.servicenetworking]
 }
 
-resource "google_project_service" "eventarc_1" {
-  project    = google_project.project_1.project_id
+resource "google_project_service" "eventarc" {
+  project    = google_project.project.project_id
   service    = "eventarc.googleapis.com"
-  depends_on = [google_project_service.kms_1]
+  depends_on = [google_project_service.kms]
 }
 
-resource "time_sleep" "wait_enable_service_1" {
+resource "time_sleep" "wait_enable_service" {
   create_duration = "60s"
-  depends_on      = [google_project_service.eventarc_1]
+  depends_on      = [google_project_service.eventarc]
 }
 
-resource "google_kms_key_ring" "keyring_1" {
+resource "google_kms_key_ring" "keyring" {
   name       = "keyring"
   location   = "%{region}"
-  project    = google_project.project_1.project_id
-  depends_on = [google_project_service.kms_1]
+  project    = google_project.project.project_id
+  depends_on = [google_project_service.kms]
 }
 
-resource "google_kms_crypto_key" "key_1" {
+resource "google_kms_crypto_key" "key" {
   name     = "key1"
-  key_ring = google_kms_key_ring.keyring_1.id
+  key_ring = google_kms_key_ring.keyring.id
 }
 
-resource "google_project_service_identity" "eventarc_sa_1" {
+resource "google_project_service_identity" "eventarc_sa" {
   service    = "eventarc.googleapis.com"
-  project    = google_project.project_1.project_id
-  depends_on = [time_sleep.wait_enable_service_1]
+  project    = google_project.project.project_id
+  depends_on = [time_sleep.wait_enable_service]
 }
 
-resource "google_kms_crypto_key_iam_member" "eventarc_sa_keyuser_1" {
-  crypto_key_id = google_kms_crypto_key.key_1.id
-  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member        = google_project_service_identity.eventarc_sa_1.member
-}
-
-resource "time_sleep" "wait_create_sa_1" {
+resource "time_sleep" "wait_create_sa" {
   create_duration = "60s"
-  depends_on      = [google_project_service_identity.eventarc_sa_1, google_kms_crypto_key_iam_member.eventarc_sa_keyuser_1]
+  depends_on      = [google_project_service_identity.eventarc_sa]
 }
 
-resource "google_eventarc_message_bus" "message_bus_1" {
+resource "google_kms_crypto_key_iam_member" "eventarc_sa_keyuser" {
+  crypto_key_id = google_kms_crypto_key.key.id
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  member        = google_project_service_identity.eventarc_sa.member
+  depends_on    = [time_sleep.wait_create_sa]
+}
+
+resource "time_sleep" "wait_propagate_iam" {
+  create_duration = "60s"
+  depends_on      = [google_kms_crypto_key_iam_member.eventarc_sa_keyuser]
+}
+
+resource "google_eventarc_message_bus" "message_bus" {
   location       = "%{region}"
   message_bus_id = "tf-test-messagebus%{random_suffix}"
-  project        = google_project.project_1.project_id
-  depends_on     = [time_sleep.wait_create_sa_1]
+  project        = google_project.project.project_id
+  depends_on     = [time_sleep.wait_propagate_iam]
 }
 
 resource "google_eventarc_google_api_source" "primary" {
   location             = "%{region}"
   google_api_source_id = "tf-test-googleapisource%{random_suffix}"
-  project              = google_project.project_1.project_id
+  project              = google_project.project.project_id
   display_name         = "basic google api source"
-  destination          = google_eventarc_message_bus.message_bus_1.id
-  crypto_key_name      = google_kms_crypto_key.key_1.id
+  destination          = google_eventarc_message_bus.message_bus.id
+  crypto_key_name      = google_kms_crypto_key.key.id
   labels = {
     test_label = "test-eventarc-label"
   }
@@ -180,7 +186,7 @@ resource "google_eventarc_google_api_source" "primary" {
 // CMEK key (in the same project) and a new MessageBus (in a different project)
 func testAccEventarcGoogleApiSource_update(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_project" "project_1" {
+resource "google_project" "project" {
   project_id      = "tf-test%{random_suffix}"
   name            = "tf-test%{random_suffix}"
   org_id          = "%{org_id}"
@@ -188,70 +194,70 @@ resource "google_project" "project_1" {
   deletion_policy = "DELETE"
 }
 
-resource "google_project_service" "compute_1" {
-  project    = google_project.project_1.project_id
+resource "google_project_service" "compute" {
+  project    = google_project.project.project_id
   service    = "compute.googleapis.com"
 }
 
-resource "google_project_service" "servicenetworking_1" {
-  project   = google_project.project_1.project_id
+resource "google_project_service" "servicenetworking" {
+  project   = google_project.project.project_id
   service   = "servicenetworking.googleapis.com"
-  depends_on = [google_project_service.compute_1]
+  depends_on = [google_project_service.compute]
 }
 
-resource "google_project_service" "kms_1" {
-  project    = google_project.project_1.project_id
+resource "google_project_service" "kms" {
+  project    = google_project.project.project_id
   service    = "cloudkms.googleapis.com"
-  depends_on = [google_project_service.servicenetworking_1]
+  depends_on = [google_project_service.servicenetworking]
 }
 
-resource "google_project_service" "eventarc_1" {
-  project    = google_project.project_1.project_id
+resource "google_project_service" "eventarc" {
+  project    = google_project.project.project_id
   service    = "eventarc.googleapis.com"
-  depends_on = [google_project_service.kms_1]
+  depends_on = [google_project_service.kms]
 }
 
-resource "google_kms_key_ring" "keyring_1" {
+resource "google_kms_key_ring" "keyring" {
   name       = "keyring"
   location   = "%{region}"
-  project    = google_project.project_1.project_id
-  depends_on = [google_project_service.kms_1]
+  project    = google_project.project.project_id
+  depends_on = [google_project_service.kms]
 }
 
-resource "google_kms_crypto_key" "key_1" {
+resource "google_kms_crypto_key" "key" {
   name     = "key1"
-  key_ring = google_kms_key_ring.keyring_1.id
+  key_ring = google_kms_key_ring.keyring.id
 }
 
-resource "google_kms_crypto_key" "key_2" {
+resource "google_kms_crypto_key" "key_update" {
   name     = "key2"
-  key_ring = google_kms_key_ring.keyring_1.id
+  key_ring = google_kms_key_ring.keyring.id
 }
 
-resource "google_project_service_identity" "eventarc_sa_1" {
+resource "google_project_service_identity" "eventarc_sa" {
   service    = "eventarc.googleapis.com"
-  project    = google_project.project_1.project_id
+  project    = google_project.project.project_id
 }
 
-resource "google_kms_crypto_key_iam_member" "eventarc_sa_keyuser_1" {
-  crypto_key_id = google_kms_crypto_key.key_1.id
+resource "google_kms_crypto_key_iam_member" "eventarc_sa_keyuser" {
+  crypto_key_id = google_kms_crypto_key.key.id
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member        = google_project_service_identity.eventarc_sa_1.member
+  member        = google_project_service_identity.eventarc_sa.member
 }
 
-resource "google_kms_crypto_key_iam_member" "eventarc_sa_keyuser_2" {
-  crypto_key_id = google_kms_crypto_key.key_2.id
+resource "google_kms_crypto_key_iam_member" "eventarc_sa_keyuser_update" {
+  crypto_key_id = google_kms_crypto_key.key_update.id
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member        = google_project_service_identity.eventarc_sa_1.member
+  member        = google_project_service_identity.eventarc_sa.member
 }
 
-resource "time_sleep" "wait_cmek_2" {
+resource "time_sleep" "wait_propagate_iam_update" {
   create_duration = "60s"
-  depends_on      = [google_kms_crypto_key_iam_member.eventarc_sa_keyuser_2]
+  depends_on      = [google_kms_crypto_key_iam_member.eventarc_sa_keyuser_update]
 }
 
 # Create a separate project to contain another MessageBus.
-resource "google_project" "project_2" {
+resource "google_project" "project_update" {
   project_id      = "tf-test2%{random_suffix}"
   name            = "tf-test2%{random_suffix}"
   org_id          = "%{org_id}"
@@ -259,47 +265,47 @@ resource "google_project" "project_2" {
   deletion_policy = "DELETE"
 }
 
-resource "time_sleep" "wait_create_project_2" {
+resource "time_sleep" "wait_create_project_update" {
   create_duration = "60s"
-  depends_on      = [google_project.project_2]
+  depends_on      = [google_project.project_update]
 }
 
-resource "google_project_service" "eventarc_2" {
-  project    = google_project.project_2.project_id
+resource "google_project_service" "eventarc_update" {
+  project    = google_project.project_update.project_id
   service    = "eventarc.googleapis.com"
-  depends_on = [time_sleep.wait_create_project_2]
+  depends_on = [time_sleep.wait_create_project_update]
 }
 
-resource "time_sleep" "wait_enable_service_2" {
+resource "time_sleep" "wait_enable_service_update" {
   create_duration = "60s"
-  depends_on      = [google_project_service.eventarc_2]
+  depends_on      = [google_project_service.eventarc_update]
 }
 
-resource "google_project_service_identity" "eventarc_sa_2" {
-  project    = google_project.project_2.project_id
+resource "google_project_service_identity" "eventarc_sa_update" {
+  project    = google_project.project_update.project_id
   service    = "eventarc.googleapis.com"
-  depends_on = [time_sleep.wait_enable_service_2]
+  depends_on = [time_sleep.wait_enable_service_update]
 }
 
-resource "time_sleep" "wait_create_sa_2" {
+resource "time_sleep" "wait_create_sa_update" {
   create_duration = "60s"
-  depends_on      = [google_project_service_identity.eventarc_sa_2]
+  depends_on      = [google_project_service_identity.eventarc_sa_update]
 }
 
-resource "google_eventarc_message_bus" "message_bus_2" {
+resource "google_eventarc_message_bus" "message_bus_update" {
   location       = "%{region}"
   message_bus_id = "tf-test-messagebus2%{random_suffix}"
-  project        = google_project.project_2.project_id
-  depends_on     = [time_sleep.wait_create_sa_2]
+  project        = google_project.project_update.project_id
+  depends_on     = [time_sleep.wait_create_sa_update]
 }
 
 resource "google_eventarc_google_api_source" "primary" {
   location             = "%{region}"
   google_api_source_id = "tf-test-googleapisource%{random_suffix}"
-  project              = google_project.project_1.project_id
+  project              = google_project.project.project_id
   display_name         = "updated google api source"
-  destination          = google_eventarc_message_bus.message_bus_2.id
-  crypto_key_name      = google_kms_crypto_key.key_2.id
+  destination          = google_eventarc_message_bus.message_bus_update.id
+  crypto_key_name      = google_kms_crypto_key.key_update.id
   labels = {
     updated_label = "updated-test-eventarc-label"
   }
@@ -309,7 +315,7 @@ resource "google_eventarc_google_api_source" "primary" {
   logging_config {
     log_severity = "ALERT"
   }
-  depends_on = [time_sleep.wait_cmek_2]
+  depends_on = [time_sleep.wait_propagate_iam_update]
 }
 `, context)
 }
@@ -317,7 +323,7 @@ resource "google_eventarc_google_api_source" "primary" {
 // Unsets as many fields as possible in the GoogleApiSource.
 func testAccEventarcGoogleApiSource_unset(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_project" "project_1" {
+resource "google_project" "project" {
   project_id      = "tf-test%{random_suffix}"
   name            = "tf-test%{random_suffix}"
   org_id          = "%{org_id}"
@@ -325,64 +331,64 @@ resource "google_project" "project_1" {
   deletion_policy = "DELETE"
 }
 
-resource "google_project_service" "compute_1" {
-  project    = google_project.project_1.project_id
+resource "google_project_service" "compute" {
+  project    = google_project.project.project_id
   service    = "compute.googleapis.com"
 }
 
-resource "google_project_service" "servicenetworking_1" {
-  project   = google_project.project_1.project_id
+resource "google_project_service" "servicenetworking" {
+  project   = google_project.project.project_id
   service   = "servicenetworking.googleapis.com"
-  depends_on = [google_project_service.compute_1]
+  depends_on = [google_project_service.compute]
 }
 
-resource "google_project_service" "kms_1" {
-  project    = google_project.project_1.project_id
+resource "google_project_service" "kms" {
+  project    = google_project.project.project_id
   service    = "cloudkms.googleapis.com"
-  depends_on = [google_project_service.servicenetworking_1]
+  depends_on = [google_project_service.servicenetworking]
 }
 
-resource "google_project_service" "eventarc_1" {
-  project    = google_project.project_1.project_id
+resource "google_project_service" "eventarc" {
+  project    = google_project.project.project_id
   service    = "eventarc.googleapis.com"
-  depends_on = [google_project_service.kms_1]
+  depends_on = [google_project_service.kms]
 }
 
-resource "google_kms_key_ring" "keyring_1" {
+resource "google_kms_key_ring" "keyring" {
   name       = "keyring"
   location   = "%{region}"
-  project    = google_project.project_1.project_id
-  depends_on = [google_project_service.kms_1]
+  project    = google_project.project.project_id
+  depends_on = [google_project_service.kms]
 }
 
-resource "google_kms_crypto_key" "key_1" {
+resource "google_kms_crypto_key" "key" {
   name     = "key1"
-  key_ring = google_kms_key_ring.keyring_1.id
+  key_ring = google_kms_key_ring.keyring.id
 }
 
-resource "google_kms_crypto_key" "key_2" {
+resource "google_kms_crypto_key" "key_update" {
   name     = "key2"
-  key_ring = google_kms_key_ring.keyring_1.id
+  key_ring = google_kms_key_ring.keyring.id
 }
 
-resource "google_project_service_identity" "eventarc_sa_1" {
+resource "google_project_service_identity" "eventarc_sa" {
   service    = "eventarc.googleapis.com"
-  project    = google_project.project_1.project_id
+  project    = google_project.project.project_id
 }
 
-resource "google_kms_crypto_key_iam_member" "eventarc_sa_keyuser_1" {
-  crypto_key_id = google_kms_crypto_key.key_1.id
+resource "google_kms_crypto_key_iam_member" "eventarc_sa_keyuser" {
+  crypto_key_id = google_kms_crypto_key.key.id
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member        = google_project_service_identity.eventarc_sa_1.member
+  member        = google_project_service_identity.eventarc_sa.member
 }
 
-resource "google_kms_crypto_key_iam_member" "eventarc_sa_keyuser_2" {
-  crypto_key_id = google_kms_crypto_key.key_2.id
+resource "google_kms_crypto_key_iam_member" "eventarc_sa_keyuser_update" {
+  crypto_key_id = google_kms_crypto_key.key_update.id
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member        = google_project_service_identity.eventarc_sa_1.member
+  member        = google_project_service_identity.eventarc_sa.member
 }
 
-resource "google_project" "project_2" {
+resource "google_project" "project_update" {
   project_id      = "tf-test2%{random_suffix}"
   name            = "tf-test2%{random_suffix}"
   org_id          = "%{org_id}"
@@ -390,27 +396,27 @@ resource "google_project" "project_2" {
   deletion_policy = "DELETE"
 }
 
-resource "google_project_service" "eventarc_2" {
-  project    = google_project.project_2.project_id
+resource "google_project_service" "eventarc_update" {
+  project    = google_project.project_update.project_id
   service    = "eventarc.googleapis.com"
 }
 
-resource "google_project_service_identity" "eventarc_sa_2" {
-  project    = google_project.project_2.project_id
+resource "google_project_service_identity" "eventarc_sa_update" {
+  project    = google_project.project_update.project_id
   service    = "eventarc.googleapis.com"
 }
 
-resource "google_eventarc_message_bus" "message_bus_2" {
+resource "google_eventarc_message_bus" "message_bus_update" {
   location       = "%{region}"
   message_bus_id = "tf-test-messagebus2%{random_suffix}"
-  project        = google_project.project_2.project_id
+  project        = google_project.project_update.project_id
 }
 
 resource "google_eventarc_google_api_source" "primary" {
   location             = "%{region}"
   google_api_source_id = "tf-test-googleapisource%{random_suffix}"
-  project              = google_project.project_1.project_id
-  destination          = google_eventarc_message_bus.message_bus_2.id
+  project              = google_project.project.project_id
+  destination          = google_eventarc_message_bus.message_bus_update.id
 }
 `, context)
 }
