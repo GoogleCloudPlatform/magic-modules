194b05e6a2586fcb27d2f0e11f2a5b86eb06f1ea
Chris Hawk <hawk@google.com>
2025-03-07 16:53:40 -0800

    Use the main test org, I suppose

--- mmv1/third_party/terraform/services/logging/resource_logging_folder_settings_test.go
+++ mmv1/third_party/terraform/services/logging/resource_logging_folder_settings_test.go
@@ -13,7 +13,7 @@ func TestAccLoggingFolderSettings_update(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"org_id":        envvar.GetTestOrgTargetFromEnv(t),
+		"org_id":        envvar.GetTestOrgFromEnv(t),
 		"random_suffix": acctest.RandString(t, 10),
 		"original_key":  acctest.BootstrapKMSKeyInLocation(t, "us-central1").CryptoKey.Name,
 		"updated_key":   acctest.BootstrapKMSKeyInLocation(t, "us-east1").CryptoKey.Name,
