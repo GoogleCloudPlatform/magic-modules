5fd161dd0e55b88579b214d04bafcb50fb6a1991
Guru Sai Rama Subbarao Voleti <gvoleti@google.com>
2025-03-17 11:10:48 +0000

    update

--- mmv1/third_party/terraform/services/storagetransfer/resource_storage_transfer_job_test.go
+++ mmv1/third_party/terraform/services/storagetransfer/resource_storage_transfer_job_test.go
@@ -1,9 +1,5 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
 package storagetransfer_test
 
 import (
