4218d4970342c671ea708c825c7ba10da5ef04f4
Guru Sai Rama Subbarao Voleti <gvoleti@google.com>
2025-03-17 12:52:49 +0000

    update

--- mmv1/third_party/terraform/services/storagetransfer/resource_storage_transfer_job_test.go
+++ mmv1/third_party/terraform/services/storagetransfer/resource_storage_transfer_job_test.go
@@ -2358,7 +2358,6 @@ resource "google_storage_transfer_job" "transfer_job" {
       path  = "foo/bar/"
     }
     transfer_options {
-      delete_objects_from_source_after_transfer = false
       metadata_options {
         symlink = "SYMLINK_SKIP"
         uid = "UID_NUMBER"
