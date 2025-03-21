2bc3d1cdd5ca93d6d65abd3b6959ee5579861b95
Scott Suarez <ScottSuarez@google.com>
2025-03-21 09:16:33 -0700

    implement sweeper dependencies and parent relationships (#13396)

--- mmv1/third_party/terraform/services/vmwareengine/resource_vmwareengine_network_sweeper.go
+++ mmv1/third_party/terraform/services/vmwareengine/resource_vmwareengine_network_sweeper.go
@@ -13,7 +13,7 @@ import (
 )
 
 func init() {
-	sweeper.AddTestSweepers("VmwareengineNetwork", testSweepVmwareengineNetwork)
+	sweeper.AddTestSweepersLegacy("VmwareengineNetwork", testSweepVmwareengineNetwork)
 }
 
 // At the time of writing, the CI only passes us-central1 as the region
