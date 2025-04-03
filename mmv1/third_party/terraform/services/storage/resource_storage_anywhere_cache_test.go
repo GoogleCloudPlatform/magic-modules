0b82aa7cadb0da1af19ee5b0c1d3cbabf944fbcd
Margubur Rahman <margubur@google.com>
2025-04-04 02:49:25 +0530

    updated tests to run the added workflow

--- mmv1/third_party/terraform/services/storage/resource_storage_anywhere_cache_test.go
+++ mmv1/third_party/terraform/services/storage/resource_storage_anywhere_cache_test.go
@@ -53,20 +53,15 @@ func TestAccStorageAnywhereCache_update(t *testing.T) {
 func testAccStorageAnywhereCache_full(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 resource "google_storage_bucket" "bucket" {
-  name                        = "tf-test-bucket-name%{random_suffix}"
-  location                    = "US"
-}
-
-resource "time_sleep" "destroy_wait_5000_seconds" {
-  depends_on = [google_storage_bucket.bucket]
-  destroy_duration = "5000s"
+  name = "tf-test-bucket-name%{random_suffix}"
+  location = "US"
+  force_destroy = "true"
 }
 
 resource "google_storage_anywhere_cache" "cache" {
   bucket = google_storage_bucket.bucket.name
   zone = "us-central1-f"
   ttl = "3601s"
-  depends_on = [time_sleep.destroy_wait_5000_seconds]
 }
 `, context)
 }
