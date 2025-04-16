a564555c5400622a0dffecd066b5c42dc7b84ac4
Nick Elliot <nickelliot@google.com>
2025-04-16 14:16:22 -0700

    fmt

--- mmv1/third_party/terraform/acctest/vcr_utils.go
+++ mmv1/third_party/terraform/acctest/vcr_utils.go
@@ -23,8 +23,8 @@ import (
 
 	"github.com/hashicorp/terraform-provider-google/google/fwprovider"
 	tpgprovider "github.com/hashicorp/terraform-provider-google/google/provider"
-	"github.com/hashicorp/terraform-provider-google/google/services/pubsublite"
 	"github.com/hashicorp/terraform-provider-google/google/services/compute"
+	"github.com/hashicorp/terraform-provider-google/google/services/pubsublite"
 	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
 
