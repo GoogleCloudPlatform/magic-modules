34e2dcb86ad71ce71e380f3cccaa7903bd89e83b
Iris Chen <ciris@google.com>
2025-04-28 02:18:42 -0700

    test template check

---
+++ tools/template-check/cmd/root.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+const rootCmdDesc = "Utilities for template checks."
+
+type rootOptions struct {
+}
+
+func newRootCmd() (*cobra.Command, *rootOptions, error) {
+	o := &rootOptions{}
+	cmd := &cobra.Command{
+		Use:           "template-check",
+		Short:         rootCmdDesc,
+		Long:          rootCmdDesc,
+		SilenceUsage:  true,
+		SilenceErrors: true,
+	}
+	cmd.AddCommand(newversionGuardCmd(o))
+	cmd.AddCommand(newUnusedTmplCmd(o))
+	return cmd, o, nil
+}
+
+// Execute is the entry-point for all commands.
+// This lets us keep all new command functions private.
+func Execute() {
+	rootCmd, _, err := newRootCmd()
+	if err != nil {
+		fmt.Printf("Error creating root logger: %s", err)
+		os.Exit(1)
+	}
+	err = rootCmd.Execute()
+	if err == nil {
+		os.Exit(0)
+	} else {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
--- tools/template-check/main.go
+++ tools/template-check/main.go
@@ -1,66 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"flag"
-	"fmt"
-	"os"
-
-	"github.com/GoogleCloudPlatform/magic-modules/tools/template-check/gotemplate"
-)
-
-func isValidTemplate(filename string) (bool, error) {
-	results, err := gotemplate.CheckVersionGuardsForFile(filename)
-	if err != nil {
-		return false, err
-	}
-
-	if len(results) > 0 {
-		fmt.Fprintf(os.Stderr, "error: invalid version checks found in %s:\n", filename)
-		for _, result := range results {
-			fmt.Fprintf(os.Stderr, "  %s\n", result)
-		}
-		return false, nil
-	}
-
-	return true, nil
-}
-
-func checkTemplate(filename string) bool {
-	valid, err := isValidTemplate(filename)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		return false
-	}
-	return valid
-}
+import "github.com/GoogleCloudPlatform/magic-modules/tools/template-check/cmd"
 
 func main() {
-	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "template-check - check that a template file is valid\n    template-check [file]\n")
-	}
-
-	flag.Parse()
-
-	// Handle file as a positional argument
-	if flag.Arg(0) != "" {
-		if !checkTemplate(flag.Arg(0)) {
-			os.Exit(1)
-		}
-		os.Exit(0)
-	}
-
-	// Handle files coming from a linux pipe
-	fileInfo, _ := os.Stdin.Stat()
-	if fileInfo.Mode()&os.ModeCharDevice == 0 {
-		exitStatus := 0
-		scanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
-		for scanner.Scan() {
-			if !checkTemplate(scanner.Text()) {
-				exitStatus = 1
-			}
-		}
-
-		os.Exit(exitStatus)
-	}
+	cmd.Execute()
 }
