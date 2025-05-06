27001327d48d9b609cc65656212ca2544defabe4
scawful <scawful@google.com>
2025-05-06 14:59:09 +0000

    Fix product name in go test

--- mmv1/third_party/terraform/services/contactcenterinsights/resource_contact_center_insights_view_test.go
+++ mmv1/third_party/terraform/services/contactcenterinsights/resource_contact_center_insights_view_test.go
@@ -79,7 +79,7 @@ func testAccInsightsView(context map[string]interface{}) string {
 
 func testAccContactCenterInsightsView_full(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_contact_center_ai_insights_view" "full_view" {
+resource "google_contact_center_insights_view" "full_view" {
   location = "us-central1"
   display_name = "view-display-name-%{random_suffix}"
   create_time = "2025-01-01T00:00:00Z"
@@ -91,7 +91,7 @@ resource "google_contact_center_ai_insights_view" "full_view" {
 
 func testAccContactCenterInsightsView_update(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_contact_center_ai_insights_view" "full_view" {
+resource "google_contact_center_insights_view" "full_view" {
   location = "us-central1"
   display_name = "view-display-name-%{random_suffix}-updated"
   create_time = "2025-01-02T00:00:00Z"
