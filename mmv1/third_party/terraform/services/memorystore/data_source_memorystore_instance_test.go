a42617d9160b4e167c7522c73c3f78571dc59c89
NA2047 <12290725+NA2047@users.noreply.github.com>
2025-05-06 13:45:28 -0700

    Fixed datasource test

--- mmv1/third_party/terraform/services/memorystore/data_source_memorystore_instance_test.go
+++ mmv1/third_party/terraform/services/memorystore/data_source_memorystore_instance_test.go
@@ -12,7 +12,6 @@ func TestAccMemorystoreInstanceDatasourceConfig(t *testing.T) {
 
 	context := map[string]interface{}{
 		"random_suffix": acctest.RandString(t, 10),
-		"network_name":  acctest.BootstrapSharedServiceNetworkingConnection(t, "memorystore-instance-ds"),
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
@@ -22,6 +21,9 @@ func TestAccMemorystoreInstanceDatasourceConfig(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccMemorystoreInstanceDatasourceConfig(context),
+				Check: resource.ComposeTestCheckFunc(
+					acctest.CheckDataSourceStateMatchesResourceState("data.google_memorystore_instance.default", "google_memorystore_instance.instance-basic"),
+				),
 			},
 		},
 	})
@@ -32,46 +34,13 @@ func testAccMemorystoreInstanceDatasourceConfig(context map[string]interface{}) 
 resource "google_memorystore_instance" "instance-basic" {
   instance_id                 = "tf-test-memorystore-instance%{random_suffix}"
   shard_count                 = 1
-  desired_auto_created_endpoints {
-    network                   = google_compute_network.producer_net.id
-    project_id                = data.google_project.project.project_id
-  }
   location                    = "us-central1"
   deletion_protection_enabled = false
-  depends_on                  = [google_network_connectivity_service_connection_policy.default]
-
 }
-
-resource "google_network_connectivity_service_connection_policy" "default" {
-  name                        = "%{network_name}-policy"
-  location                    = "us-central1"
-  service_class               = "gcp-memorystore"
-  description                 = "my basic service connection policy"
-  network                     = google_compute_network.producer_net.id
-  psc_config {
-    subnetworks               = [google_compute_subnetwork.producer_subnet.id]
-  }
-}
-
-resource "google_compute_subnetwork" "producer_subnet" {
-	name                      = "%{network_name}-sn"
-	ip_cidr_range             = "10.0.0.248/29"
-	region                    = "us-central1"
-	network                   = google_compute_network.producer_net.id
-}
-
-resource "google_compute_network" "producer_net" {
-  name                        = "%{network_name}-vpc"
-  auto_create_subnetworks     = false
-}
-
- data "google_project" "project" {
- }
 
 data "google_memorystore_instance" "default" {
   instance_id                 = google_memorystore_instance.instance-basic.instance_id
   location                    = "us-central1"
-
 }
 `, context)
 }
