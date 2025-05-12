94aa22ae1a0cd3d55789083a18c3bfe15fb13547
Michael Lopez <mickael.lopez@gmail.com>
2025-05-12 21:36:43 +0200

    gkehub: datasource for gke_hub_membership resource

---
+++ mmv1/third_party/terraform/services/gkehub2/data_source_google_gke_hub_membership.go
@@ -0,0 +1,39 @@
+package gkehub2
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
+	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
+)
+
+func DataSourceGoogleGkeHubMembership() *schema.Resource {
+	dsSchema := tpgresource.DatasourceSchemaFromResourceSchema(ResourceGKEHub2Membership().Schema)
+	tpgresource.AddRequiredFieldsToSchema(dsSchema, "membership_id")
+	tpgresource.AddRequiredFieldsToSchema(dsSchema, "location")
+	tpgresource.AddOptionalFieldsToSchema(dsSchema, "project")
+
+	return &schema.Resource{
+		Read:   dataSourceGoogleGkeHubMembershipRead,
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceGoogleGkeHubMembershipRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*transport_tpg.Config)
+
+	id, err := tpgresource.ReplaceVars(d, config, "projects/{{project}}/locations/{{location}}/memberships/{{membership_id}}")
+	if err != nil {
+		return fmt.Errorf("Error constructing id: %s", err)
+	}
+	d.SetId(id)
+
+	err = resourceGKEHub2MembershipRead(d, meta)
+	if err != nil {
+		return err
+	}
+
+	// No labels or annotations for Membership datasource
+	return nil
+}
