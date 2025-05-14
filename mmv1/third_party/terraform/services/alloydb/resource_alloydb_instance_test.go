29706611a31503e7cc9f0192cab4570496218d55
uaditya70 <upadhyayaditya@google.com>
2025-05-14 22:55:53 +0530

    added missing ","

--- mmv1/third_party/terraform/services/alloydb/resource_alloydb_instance_test.go
+++ mmv1/third_party/terraform/services/alloydb/resource_alloydb_instance_test.go
@@ -143,13 +143,13 @@ func TestAccAlloydbInstance_stopstart(t *testing.T) {
 	contextStop := map[string]interface{}{
 		"random_suffix": suffix,
 		"network_name":  networkName,
-		"activation_policy": "NEVER"
+		"activation_policy": "NEVER",
 	}
 
 	contextStart := map[string]interface{}{
 		"random_suffix": suffix,
 		"network_name":  networkName,
-		"activation_policy": "ALWAYS"
+		"activation_policy": "ALWAYS",
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
