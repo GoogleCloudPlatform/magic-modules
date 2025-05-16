2d416d9d9e414724f1559c18813ef885285ea81f
Thomas Rodgers <thomasrodgers@google.com>
2025-05-16 21:39:53 +0000

    Retain original tests

--- .ci/magician/vcr/tester.go
+++ .ci/magician/vcr/tester.go
@@ -13,10 +13,13 @@ import (
 )
 
 type Result struct {
-	PassedTests  []string
-	SkippedTests []string
-	FailedTests  []string
-	Panics       []string
+	PassedTests     []string
+	SkippedTests    []string
+	FailedTests     []string
+	PassedSubtests  []string
+	SkippedSubtests []string
+	FailedSubtests  []string
+	Panics          []string
 }
 
 type Mode int
@@ -606,33 +609,38 @@ func collectResult(output string) Result {
 		resultSets[submatches[1]][submatches[2]] = struct{}{}
 	}
 	matches = subtestResultsExpression.FindAllStringSubmatch(output, -1)
+	subtestResultSets := make(map[string]map[string]struct{}, 4)
 	for _, submatches := range matches {
 		if len(submatches) != 4 {
 			fmt.Printf("Warning: unexpected regex match found in test output: %v", submatches)
 			continue
 		}
-		// Remove the compound test from all result sets.
-		for _, kind := range []string{"PASS", "FAIL", "SKIP"} {
-			delete(resultSets[kind], submatches[2])
+		if _, ok := subtestResultSets[submatches[1]]; !ok {
+			subtestResultSets[submatches[1]] = make(map[string]struct{})
 		}
-		if _, ok := resultSets[submatches[1]]; !ok {
-			resultSets[submatches[1]] = make(map[string]struct{})
-		}
-		resultSets[submatches[1]][fmt.Sprintf("%s__%s", submatches[2], submatches[3])] = struct{}{}
+		subtestResultSets[submatches[1]][fmt.Sprintf("%s__%s", submatches[2], submatches[3])] = struct{}{}
 	}
 	results := make(map[string][]string, 4)
 	results["PANIC"] = testPanicExpression.FindAllString(output, -1)
 	sort.Strings(results["PANIC"])
+	subtestResults := make(map[string][]string, 3)
 	for _, kind := range []string{"FAIL", "PASS", "SKIP"} {
 		for test := range resultSets[kind] {
 			results[kind] = append(results[kind], test)
 		}
 		sort.Strings(results[kind])
+		for subtest := range subtestResultSets[kind] {
+			subtestResults[kind] = append(subtestResults[kind], subtest)
+		}
+		sort.Strings(subtestResults[kind])
 	}
 	return Result{
-		FailedTests:  results["FAIL"],
-		PassedTests:  results["PASS"],
-		SkippedTests: results["SKIP"],
-		Panics:       results["PANIC"],
+		FailedTests:     results["FAIL"],
+		PassedTests:     results["PASS"],
+		SkippedTests:    results["SKIP"],
+		FailedSubtests:  subtestResults["FAIL"],
+		PassedSubtests:  subtestResults["PASS"],
+		SkippedSubtests: subtestResults["SKIP"],
+		Panics:          results["PANIC"],
 	}
 }
