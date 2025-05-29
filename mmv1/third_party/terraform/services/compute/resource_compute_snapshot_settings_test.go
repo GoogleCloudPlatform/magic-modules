6413156afce061b4acd7f7821035d1a0522d2f5c
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2025-05-29 19:23:21 +0200

    fix: apply suggestions in batch from code review (enable compute.googleapis.com)
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>

--- mmv1/third_party/terraform/services/compute/resource_compute_snapshot_settings_test.go
+++ mmv1/third_party/terraform/services/compute/resource_compute_snapshot_settings_test.go
@@ -58,6 +58,17 @@ resource "google_project" "project" {
   deletion_policy = "DELETE"
 }
 
+resource "google_project_service" "compute" {
+  project = google_project.project.project_id
+  service = "compute.googleapis.com"
+}
+
+resource "time_sleep" "wait_120_seconds" {
+  create_duration = "120s"
+  depends_on = [google_project_service.compute]
+}
+
+
 resource "google_compute_snapshot_settings" "tf_test_snapshot_settings" {
     project   = google_project.project.project_id
     storage_location {
@@ -67,6 +78,7 @@ resource "google_compute_snapshot_settings" "tf_test_snapshot_settings" {
             location = "us-central1"
         }
     }
+    depends_on = [time_sleep.wait_120_seconds]
 }
 `, context)
 }
