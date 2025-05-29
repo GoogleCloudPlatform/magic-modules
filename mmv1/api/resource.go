57f933530648d25a53f80d053fbef14d3dade809
Stephen Lewis <stephenrlewis@google.com>
2025-05-29 15:01:14 -0700

    Consider self link fields for InPostCreateComputed as well

--- mmv1/api/resource.go
+++ mmv1/api/resource.go
@@ -1218,8 +1218,15 @@ func (r Resource) GetIdFormat() string {
 
 // Returns true if the Type is in the ID format and false otherwise.
 func (r Resource) InPostCreateComputed(prop Type) bool {
-	fields := r.ExtractIdentifiers(r.GetIdFormat())
-	return slices.Contains(fields, google.Underscore(prop.Name))
+	fields := map[string]struct{}{}
+	for _, f := range r.ExtractIdentifiers(r.GetIdFormat()) {
+		fields[f] = struct{}{}
+	}
+	for _, f := range r.ExtractIdentifiers(r.SelfLinkUri()) {
+		fields[f] = struct{}{}
+	}
+	_, ok := fields[google.Underscore(prop.Name)]
+	return ok
 }
 
 // Returns true if at least one of the fields in the ID format is computed
