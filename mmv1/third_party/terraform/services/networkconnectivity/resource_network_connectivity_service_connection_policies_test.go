7e02d12fa0b072bc44b8c56aa08ea466fb9127b2
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2025-06-02 13:28:32 +0200

    test: fix allowed hierarchy level

--- mmv1/third_party/terraform/services/networkconnectivity/resource_network_connectivity_service_connection_policies_test.go
+++ mmv1/third_party/terraform/services/networkconnectivity/resource_network_connectivity_service_connection_policies_test.go
@@ -74,13 +74,9 @@ func testAccNetworkConnectivityServiceConnectionPolicy_basic(context map[string]
     service_class = "gcp-memorystore-redis"
     network = google_compute_network.producer_net.id
     psc_config {
-      producer_instance_location 						= "PRODUCER_INSTANCE_LOCATION_UNSPECIFIED"
-      allowed_google_producers_resource_hierarchy_level = [
-          "projects/foo-bar",
-          "organizations/baz-qux",
-      ]
-      subnetworks 				 						= [google_compute_subnetwork.producer_subnet.id]
-      limit 					 						= 2
+      producer_instance_location = "PRODUCER_INSTANCE_LOCATION_UNSPECIFIED"
+      subnetworks 				 = [google_compute_subnetwork.producer_subnet.id]
+      limit 					 = 2
     }
   }
 `, context)
@@ -106,9 +102,13 @@ resource "google_network_connectivity_service_connection_policy" "default" {
   service_class = "gcp-memorystore-redis"
   network = google_compute_network.producer_net.id
   psc_config {
-    producer_instance_location = "CUSTOM_RESOURCE_HIERARCHY_LEVELS"
-    subnetworks                = [google_compute_subnetwork.producer_subnet1.id]
-    limit                      = 4
+    producer_instance_location                        = "CUSTOM_RESOURCE_HIERARCHY_LEVELS"
+    subnetworks                                       = [google_compute_subnetwork.producer_subnet1.id]
+    limit                                             = 4
+    allowed_google_producers_resource_hierarchy_level = [
+        "projects/foo-bar",
+        "organizations/baz-qux",
+    ]
   }
   labels      = {
     foo = "bar"
