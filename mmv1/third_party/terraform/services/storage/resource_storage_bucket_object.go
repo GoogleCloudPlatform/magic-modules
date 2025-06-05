4e295f0d332b491e87b8ab299ab0c8f8b93ccc9f
Guru Sai Rama Subbarao Voleti <gvoleti@google.com>
2025-06-05 10:05:51 +0000

    update

--- mmv1/third_party/terraform/services/storage/resource_storage_bucket_object.go
+++ mmv1/third_party/terraform/services/storage/resource_storage_bucket_object.go
@@ -641,27 +641,13 @@ func flattenObjectRetention(objectRetention *storage.ObjectRetention) []map[stri
 }
 
 func resourceStorageBucketObjectCustomizeDiff(ctx context.Context, d *schema.ResourceDiff, meta interface{}) error {
-
-	localMd5Hash := ""
-	if source, ok := d.GetOkExists("source"); ok {
-		localMd5Hash = tpgresource.GetFileMd5Hash(source.(string))
-	}
-	if content, ok := d.GetOkExists("content"); ok {
-		localMd5Hash = tpgresource.GetContentMd5Hash([]byte(content.(string)))
-	}
-	if localMd5Hash == "" {
-		return nil
-	}
-
-	oldMd5Hash, ok := d.GetOkExists("md5hash")
-	if d.HasChange("source_md5hash") || (ok && oldMd5Hash != localMd5Hash) {
-		return diffChange(d)
+	if d.HasChange("source_md5hash") {
+		return showDiffChange(d)
 	}
 	return nil
-
 }
 
-func diffChange(d *schema.ResourceDiff) error {
+func showDiffChange(d *schema.ResourceDiff) error {
 	err := d.SetNewComputed("md5hash")
 	if err != nil {
 		return fmt.Errorf("Error re-setting md5hash: %s", err)
