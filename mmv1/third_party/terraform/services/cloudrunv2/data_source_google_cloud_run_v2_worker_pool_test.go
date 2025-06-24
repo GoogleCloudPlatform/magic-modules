9c9b349ac2ad9b1b32bbcb4fa332cde058022484
paridhishah18 <166548459+paridhishah18@users.noreply.github.com>
2025-06-24 11:40:27 -0700

    Promote launch stage to BETA (#14294)

--- mmv1/third_party/terraform/services/cloudrunv2/data_source_google_cloud_run_v2_worker_pool_test.go
+++ mmv1/third_party/terraform/services/cloudrunv2/data_source_google_cloud_run_v2_worker_pool_test.go
@@ -40,7 +40,7 @@ resource "google_cloud_run_v2_worker_pool" "hello" {
   name     = "%s"
   location = "%s"
   deletion_protection = false
-  launch_stage = "ALPHA"
+  launch_stage = "BETA"
   
   template {
     containers {
