2b103cde01977f483fd25d9847ed1a97ef8eb828
Maayan Beltzer <maayanch@google.com>
2025-06-26 15:38:09 +0000

    Update VpcFlowLogs to support subnet, vpc and organizations

--- mmv1/third_party/terraform/services/networkmanagement/resource_network_management_vpc_flow_logs_config_test.go
+++ mmv1/third_party/terraform/services/networkmanagement/resource_network_management_vpc_flow_logs_config_test.go
@@ -182,6 +182,161 @@ resource "google_network_management_vpc_flow_logs_config" "example" {
 	return fmt.Sprintf("%s\n\n%s\n\n", vpcFlowLogsCfg, testAccNetworkManagementVpcFlowLogsConfig_baseResources(context))
 }
 
+func TestAccNetworkManagementVpcFlowLogsConfig_network(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckNetworkManagementVpcFlowLogsConfigDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetworkManagementVpcFlowLogsConfig_network(context),
+			},
+			{
+				ResourceName:            "google_network_management_vpc_flow_logs_config.network-test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "vpc_flow_logs_config_id"},
+			},
+			{
+				Config: testAccNetworkManagementVpcFlowLogsConfig_networkUpdate(context),
+			},
+			{
+				ResourceName:            "google_network_management_vpc_flow_logs_config.network-test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "vpc_flow_logs_config_id"},
+			},
+		},
+	})
+}
+
+func testAccNetworkManagementVpcFlowLogsConfig_network(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_compute_network" "network" {
+  name = "tf-test-flow-logs-network-%{random_suffix}"
+}
+
+resource "google_network_management_vpc_flow_logs_config" "network-test" {
+  vpc_flow_logs_config_id = "tf-test-network-id-%{random_suffix}"
+  location                = "global"
+  network                 = google_compute_network.network.id
+  state                   = "ENABLED"
+}
+`, context)
+}
+
+func testAccNetworkManagementVpcFlowLogsConfig_networkUpdate(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_compute_network" "network" {
+  name = "tf-test-flow-logs-network-%{random_suffix}"
+}
+
+resource "google_network_management_vpc_flow_logs_config" "network-test" {
+  vpc_flow_logs_config_id = "tf-test-network-id-%{random_suffix}"
+  location                = "global"
+  network                 = google_compute_network.network.id
+
+  // Updated fields
+  state                   = "DISABLED"
+  aggregation_interval    = "INTERVAL_10_MIN"
+  flow_sampling           = 0.05
+  metadata                = "INCLUDE_ALL_METADATA"
+  description             = "Updated description for network test"
+}
+`, context)
+}
+
+func TestAccNetworkManagementVpcFlowLogsConfig_subnet(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckNetworkManagementVpcFlowLogsConfigDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetworkManagementVpcFlowLogsConfig_subnet(context),
+			},
+			{
+				ResourceName:            "google_network_management_vpc_flow_logs_config.subnet-test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "vpc_flow_logs_config_id"},
+			},
+			{
+				Config: testAccNetworkManagementVpcFlowLogsConfig_subnetUpdate(context),
+			},
+			{
+				ResourceName:            "google_network_management_vpc_flow_logs_config.subnet-test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "vpc_flow_logs_config_id"},
+			},
+		},
+	})
+}
+
+func testAccNetworkManagementVpcFlowLogsConfig_subnet(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_compute_network" "network" {
+  name                    = "tf-test-subnet-network-%{random_suffix}"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "tf-test-flow-logs-subnet-%{random_suffix}"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.network.id
+}
+
+resource "google_network_management_vpc_flow_logs_config" "subnet-test" {
+  vpc_flow_logs_config_id = "tf-test-subnet-id-%{random_suffix}"
+  location                = "global"
+  subnet                  = google_compute_subnetwork.subnet.id
+}
+`, context)
+}
+
+func testAccNetworkManagementVpcFlowLogsConfig_subnetUpdate(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_compute_network" "network" {
+  name                    = "tf-test-subnet-network-%{random_suffix}"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "tf-test-flow-logs-subnet-%{random_suffix}"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.network.id
+}
+
+resource "google_network_management_vpc_flow_logs_config" "subnet-test" {
+  vpc_flow_logs_config_id = "tf-test-subnet-id-%{random_suffix}"
+  location                = "global"
+  subnet                  = google_compute_subnetwork.subnet.id
+  
+  // Updated fields
+  state                   = "DISABLED"
+  aggregation_interval    = "INTERVAL_30_SEC"
+  flow_sampling           = 0.5
+  metadata                = "EXCLUDE_ALL_METADATA"
+  description             = "Updated description for subnet test"
+}
+`, context)
+}
+
 func testAccNetworkManagementVpcFlowLogsConfig_baseResources(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 resource "google_compute_vpn_tunnel" "tunnel" {
@@ -239,6 +394,105 @@ resource "google_compute_route" "route" {
   dest_range          = "15.0.0.0/24"
   priority            = 1000
   next_hop_vpn_tunnel = google_compute_vpn_tunnel.tunnel.id
+}
+`, context)
+}
+
+func TestAccNetworkManagementVpcFlowLogsConfig_organization(t *testing.T) {
+	t.Parallel()
+	// This helper function retrieves the organization ID from the test environment.
+	// For local runs, it reads the GOOGLE_ORG environment variable.
+	// In CI, the test runner provides the correct test organization ID.
+	orgID := acctest.GetOrgID(t)
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+		"org_id":        orgID,
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckNetworkManagementVpcFlowLogsConfigDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetworkManagementVpcFlowLogsConfig_organization_basic_with_iam(context),
+			},
+			{
+				ResourceName:            "google_network_management_vpc_flow_logs_config.org-test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "vpc_flow_logs_config_id"},
+			},
+			{
+				Config: testAccNetworkManagementVpcFlowLogsConfig_organization_update_with_iam(context),
+			},
+			{
+				ResourceName:            "google_network_management_vpc_flow_logs_config.org-test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "vpc_flow_logs_config_id"},
+			},
+		},
+	})
+}
+
+func testAccNetworkManagementVpcFlowLogsConfig_organization_basic_with_iam(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+data "google_project" "project" {}
+
+data "google_service_account" "test_runner" {
+    account_id = "hashicorp-test-runner"
+    project    = data.google_project.project.project_id
+}
+
+// This resource grants the 'Network Management Admin' role to the test service account.
+resource "google_organization_iam_member" "nm_admin_binding" {
+    org_id = "%{org_id}"
+    role   = "roles/networkmanagement.admin"
+    member = "serviceAccount:${data.google_service_account.test_runner.email}"
+}
+
+resource "google_network_management_vpc_flow_logs_config" "org-test" {
+  vpc_flow_logs_config_id = "tf-test-org-id-%{random_suffix}"
+  location                = "global"
+  parent                  = "organizations/%{org_id}"
+  state                   = "ENABLED"
+
+  // This ensures the IAM role is granted before this resource is created.
+  depends_on = [google_organization_iam_member.nm_admin_binding]
+}
+`, context)
+}
+
+func testAccNetworkManagementVpcFlowLogsConfig_organization_update_with_iam(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+data "google_project" "project" {}
+
+data "google_service_account" "test_runner" {
+    account_id = "hashicorp-test-runner"
+    project    = data.google_project.project.project_id
+}
+
+resource "google_organization_iam_member" "nm_admin_binding" {
+    org_id = "%{org_id}"
+    role   = "roles/networkmanagement.admin"
+    member = "serviceAccount:${data.google_service_account.test_runner.email}"
+}
+
+resource "google_network_management_vpc_flow_logs_config" "org-test" {
+  vpc_flow_logs_config_id = "tf-test-org-id-%{random_suffix}"
+  location                = "global"
+  parent                  = "organizations/%{org_id}"
+
+  // Updated fields
+  state                   = "DISABLED"
+  aggregation_interval    = "INTERVAL_15_MIN"
+  flow_sampling           = 0.2
+  metadata                = "INCLUDE_ALL_METADATA"
+  description             = "Updated description for org test"
+  
+  depends_on = [google_organization_iam_member.nm_admin_binding]
 }
 `, context)
 }
