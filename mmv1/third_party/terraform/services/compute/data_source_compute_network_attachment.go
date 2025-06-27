1a8d58e79764deea33531db2a8d7f02d4478e277
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2025-06-27 14:28:04 +0200

    feat: draft implementation of data source network attachment

---
+++ mmv1/third_party/terraform/services/compute/data_source_compute_network_attachment.go
@@ -0,0 +1,51 @@
+package compute
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
+	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
+)
+
+func DataSourceGoogleComputeNetworkAttachment() *schema.Resource {
+	dsSchema := tpgresource.DatasourceSchemaFromResourceSchema(ResourceComputeNetworkAttachment().Schema)
+
+	tpgresource.AddRequiredFieldsToSchema(dsSchema, "name", "region")
+	tpgresource.AddOptionalFieldsToSchema(dsSchema, "project")
+
+	return &schema.Resource{
+		Read:   dataSourceComputeNetworkEndpointGroupRead,
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceComputeNetworkAttachmentRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*transport_tpg.Config)
+	project, err := tpgresource.GetProject(d, config)
+	if err != nil {
+		return fmt.Errorf("Error fetching project: %s", err)
+	}
+
+	name := d.Get("name").(string)
+	region := d.Get("region").(string)
+
+	id := fmt.Sprintf("projects/%s/regions/%s/networkAttachments/%s", project, region, name)
+	d.SetId(id)
+
+	err = resourceComputeNetworkAttachmentRead(d, meta)
+	if err != nil {
+		return fmt.Errorf("Error reading Network Attachment %q: %s", id, err)
+	}
+
+	if err := tpgresource.SetDataSourceLabels(d); err != nil {
+		return err
+	}
+
+	if d.Id() == "" {
+		return fmt.Errorf("%s not found", id)
+	}
+
+	return nil
+}
