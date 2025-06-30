f4d8a65f5d21437a0329ea5c656bb7c3ccc22631
Eliza Huang <heliza@google.com>
2025-06-30 20:30:19 +0000

    Add Terraform support for Vertex Model Garden model deploy

---
+++ mmv1/third_party/terraform/services/vertexai/resource_vertex_ai_deploy_test.go
@@ -0,0 +1,52 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package vertexai_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
+)
+
+func TestAccVertexAIEndpointWithModelGardenDeployment_basic(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"project":       envvar.GetTestProjectFromEnv(),
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             nil,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVertexAIEndpointWithModelGardenDeployment_basic(context),
+			},
+			{
+				ResourceName:      "google_vertex_ai_endpoint_with_model_garden_deployment.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					"etag",
+					"id",
+					"publisher_model_name",
+					"project",
+				},
+			},
+		},
+	})
+}
+
+func testAccVertexAIEndpointWithModelGardenDeployment_basic(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_vertex_ai_endpoint_with_model_garden_deployment" "test" {
+  publisher_model_name = "publishers/google/models/paligemma@paligemma-224-float32"
+  location             = "us-central1"
+  project              = "%{project}"
+}
+`, context)
+}
