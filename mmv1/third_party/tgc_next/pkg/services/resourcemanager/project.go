83fee12c17911c6c788dd264a0d01c14ea19ded0
Zhenhua Li <zhenhuali@google.com>
2025-07-02 11:34:05 -0700

    tgc-revival: put services folder to top level

--- mmv1/third_party/tgc_next/pkg/services/resourcemanager/project.go
+++ mmv1/third_party/tgc_next/pkg/services/resourcemanager/project.go
@@ -1,23 +1,19 @@
 package resourcemanager
 
 import (
-	"fmt"
-	"strconv"
 	"strings"
+
+	"github.com/GoogleCloudPlatform/terraform-google-conversion/v6/pkg/verify"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-
-	"github.com/GoogleCloudPlatform/terraform-google-conversion/v6/pkg/caiasset"
-	"github.com/GoogleCloudPlatform/terraform-google-conversion/v6/pkg/tfplan2cai/converters/cai"
-
-	"github.com/GoogleCloudPlatform/terraform-google-conversion/v6/pkg/tpgresource"
-	transport_tpg "github.com/GoogleCloudPlatform/terraform-google-conversion/v6/pkg/transport"
-	"github.com/GoogleCloudPlatform/terraform-google-conversion/v6/pkg/verify"
-
-	"google.golang.org/api/cloudbilling/v1"
-	"google.golang.org/api/cloudresourcemanager/v1"
 )
+
+// ProjectAssetType is the CAI asset type name for project.
+const ProjectAssetType string = "cloudresourcemanager.googleapis.com/Project"
+
+// ProjectSchemaName is the TF resource schema name for resourcemanager project.
+const ProjectSchemaName string = "google_project"
 
 func ParseFolderId(v interface{}) string {
 	folderId := v.(string)
@@ -118,137 +114,4 @@ func ResourceGoogleProject() *schema.Resource {
 		},
 		UseJSONNumber: true,
 	}
-}
-
-func ResourceConverterProject() cai.ResourceConverter {
-	return cai.ResourceConverter{
-		Convert: GetProjectAndBillingInfoCaiObjects,
-	}
-}
-
-func GetProjectAndBillingInfoCaiObjects(d tpgresource.TerraformResourceData, config *transport_tpg.Config) ([]caiasset.Asset, error) {
-	if projectAsset, err := GetProjectCaiObject(d, config); err == nil {
-		assets := []caiasset.Asset{projectAsset}
-		if _, ok := d.GetOk("billing_account"); !ok {
-			return assets, nil
-		} else {
-			if billingAsset, err := GetProjectBillingInfoCaiObject(d, config); err == nil {
-				assets = append(assets, billingAsset)
-				return assets, nil
-			} else {
-				return []caiasset.Asset{}, err
-			}
-		}
-	} else {
-		return []caiasset.Asset{}, err
-	}
-}
-
-func GetProjectCaiObject(d tpgresource.TerraformResourceData, config *transport_tpg.Config) (caiasset.Asset, error) {
-	linkTmpl := "//cloudresourcemanager.googleapis.com/projects/{{number}}"
-	name, err := cai.AssetName(d, config, linkTmpl)
-	if err != nil {
-		return caiasset.Asset{}, err
-	}
-	if data, err := GetProjectData(d, config); err == nil {
-		return caiasset.Asset{
-			Name: name,
-			Type: "cloudresourcemanager.googleapis.com/Project",
-			Resource: &caiasset.AssetResource{
-				Version:              "v1",
-				DiscoveryDocumentURI: "https://cloudresourcemanager.googleapis.com/$discovery/rest?version=v1",
-				DiscoveryName:        "Project",
-				Data:                 data,
-			},
-		}, nil
-	} else {
-		return caiasset.Asset{}, err
-	}
-}
-
-func GetProjectData(d tpgresource.TerraformResourceData, config *transport_tpg.Config) (map[string]interface{}, error) {
-	pid := d.Get("project_id").(string)
-
-	project := &cloudresourcemanager.Project{
-		ProjectId: pid,
-		Name:      d.Get("name").(string),
-	}
-
-	if res, ok := d.GetOk("number"); ok {
-		num, err := strconv.ParseInt(res.(string), 10, 64)
-		if err != nil {
-			return nil, err
-		}
-
-		project.ProjectNumber = num
-	}
-
-	if err := getParentResourceId(d, project); err != nil {
-		return nil, err
-	}
-
-	if _, ok := d.GetOk("effective_labels"); ok {
-		project.Labels = tpgresource.ExpandEffectiveLabels(d)
-	}
-
-	return cai.JsonMap(project)
-}
-
-func getParentResourceId(d tpgresource.TerraformResourceData, p *cloudresourcemanager.Project) error {
-	orgId := d.Get("org_id").(string)
-	folderId := d.Get("folder_id").(string)
-
-	if orgId != "" && folderId != "" {
-		return fmt.Errorf("'org_id' and 'folder_id' cannot be both set.")
-	}
-
-	if orgId != "" {
-		p.Parent = &cloudresourcemanager.ResourceId{
-			Id:   orgId,
-			Type: "organization",
-		}
-	}
-
-	if folderId != "" {
-		p.Parent = &cloudresourcemanager.ResourceId{
-			Id:   strings.TrimPrefix(folderId, "folders/"),
-			Type: "folder",
-		}
-	}
-
-	return nil
-}
-
-func GetProjectBillingInfoCaiObject(d tpgresource.TerraformResourceData, config *transport_tpg.Config) (caiasset.Asset, error) {
-	linkTmpl := "//cloudbilling.googleapis.com/projects/{{project_id_or_project}}/billingInfo"
-	name, err := cai.AssetName(d, config, linkTmpl)
-	if err != nil {
-		return caiasset.Asset{}, err
-	}
-	project := strings.Split(name, "/")[4]
-	if data, err := GetProjectBillingInfoData(d, project); err == nil {
-		return caiasset.Asset{
-			Name: name,
-			Type: "cloudbilling.googleapis.com/ProjectBillingInfo",
-			Resource: &caiasset.AssetResource{
-				Version:              "v1",
-				DiscoveryDocumentURI: "https://cloudbilling.googleapis.com/$discovery/rest",
-				DiscoveryName:        "ProjectBillingInfo",
-				Data:                 data,
-				Location:             "global",
-			},
-		}, nil
-	} else {
-		return caiasset.Asset{}, err
-	}
-}
-
-func GetProjectBillingInfoData(d tpgresource.TerraformResourceData, project string) (map[string]interface{}, error) {
-	ba := &cloudbilling.ProjectBillingInfo{
-		BillingAccountName: fmt.Sprintf("billingAccounts/%s", d.Get("billing_account")),
-		Name:               fmt.Sprintf("projects/%s/billingInfo", project),
-		ProjectId:          d.Get("project_id").(string),
-	}
-
-	return cai.JsonMap(ba)
 }
