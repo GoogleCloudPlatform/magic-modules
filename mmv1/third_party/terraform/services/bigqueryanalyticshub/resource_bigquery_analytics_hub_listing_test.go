ab6b69fb9a97014929f3fef56a795cd398eb1fd9
Arnav Dham <arnavdham@google.com>
2025-07-09 06:42:09 +0000

    Adding Multiregion support to Listings and Listing Subscriptions.
    removing unnecssary changes.
    
    Adding Multiregion support to Listings and Listing Subscriptions.

--- mmv1/third_party/terraform/services/bigqueryanalyticshub/resource_bigquery_analytics_hub_listing_test.go
+++ mmv1/third_party/terraform/services/bigqueryanalyticshub/resource_bigquery_analytics_hub_listing_test.go
@@ -130,3 +130,62 @@ resource "google_bigquery_analytics_hub_listing" "listing_pubsub" {
 }
 `, updatedContext)
 }
+
+func TestAccBigqueryAnalyticsHubListing_bigqueryAnalyticshubListingMultiregionExample(t *testing.T) {
+	t.Parallel()
+
+	bqdataset, err := acctest.AddBigQueryDatasetReplica(acctest.GoogleProviderConfig(t).Project, "my_listing_example2", "us", "eu")
+	if err != nil {
+		// If an error occurs, fail the test immediately and log the error.
+		t.Fatalf("Failed to create BigQuery dataset and add replica: %v", err)
+	}
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+		"bqdataset":     bqdataset,
+	}
+
+	t.Cleanup(func() {
+		acctest.CleanupBigQueryDatasetAndReplica(acctest.GoogleProviderConfig(t).Project, "my_listing_example2", "eu")
+	})
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckBigqueryAnalyticsHubListingDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBigqueryAnalyticsHubListing_bigqueryAnalyticshubListingMultiregionExample(context),
+			},
+			{
+				ResourceName:            "google_bigquery_analytics_hub_listing.listing",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"data_exchange_id", "listing_id", "location"},
+			},
+		},
+	})
+}
+
+func testAccBigqueryAnalyticsHubListing_bigqueryAnalyticshubListingMultiregionExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_bigquery_analytics_hub_data_exchange" "listing" {
+  location         = "us"
+  data_exchange_id = "tf_test_my_data_exchange%{random_suffix}"
+  display_name     = "tf_test_my_data_exchange%{random_suffix}" 
+  description      = "example listing for multiregion%{random_suffix}"
+}
+
+resource "google_bigquery_analytics_hub_listing" "listing" {
+  location         = "us"
+  data_exchange_id = google_bigquery_analytics_hub_data_exchange.listing.data_exchange_id
+  listing_id       = "tf_test_my_listing%{random_suffix}"
+  display_name     = "tf_test_my_listing%{random_suffix}"
+  description      = "example listing for multiregion%{random_suffix}"
+
+  bigquery_dataset {
+    dataset = "%{bqdataset}"
+    replica_locations = ["eu"]
+  }
+}
+`, context)
+}
