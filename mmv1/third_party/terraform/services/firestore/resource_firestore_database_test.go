2a4d6612d0cc94236169281acd58c71744fbfdd8
jingqizz <jingqiz@google.com>
2025-07-10 18:55:09 +0000

    Fix the example ":" to "=", convert the tabs to spaces for indentation

--- mmv1/third_party/terraform/services/firestore/resource_firestore_database_test.go
+++ mmv1/third_party/terraform/services/firestore/resource_firestore_database_test.go
@@ -1,45 +1,45 @@
 package firestore_test
 
 import (
-	"testing"
+  "testing"
 
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-provider-google/google/acctest"
-	"github.com/hashicorp/terraform-provider-google/google/envvar"
+  "github.com/hashicorp/terraform-plugin-testing/helper/resource"
+  "github.com/hashicorp/terraform-provider-google/google/acctest"
+  "github.com/hashicorp/terraform-provider-google/google/envvar"
 )
 
 func TestAccFirestoreDatabase_tags(t *testing.T) {
-	t.Parallel()
+  t.Parallel()
 
-	// Bootstrap shared tag key and value
-	tagKey := acctest.BootstrapSharedTestProjectTagKey(t, "firestore-databases-tagkey", map[string]interface{}{})
-	context := map[string]interface{}{
-		"pid":           envvar.GetTestProjectFromEnv(),
-		"tagKey":        tagKey,
-		"tagValue":      acctest.BootstrapSharedTestProjectTagValue(t, "firestore-databases-tagvalue", tagKey),
-		"random_suffix": acctest.RandString(t, 10),
-	}
+  // Bootstrap shared tag key and value
+  tagKey := acctest.BootstrapSharedTestProjectTagKey(t, "firestore-databases-tagkey", map[string]interface{}{})
+  context := map[string]interface{}{
+    "pid":           envvar.GetTestProjectFromEnv(),
+    "tagKey":        tagKey,
+    "tagValue":      acctest.BootstrapSharedTestProjectTagValue(t, "firestore-databases-tagvalue", tagKey),
+    "random_suffix": acctest.RandString(t, 10),
+  }
 
-	acctest.VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckFirestoreDatabaseDestroyProducer(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccFirestoreDatabaseTags(context),
-			},
-			{
-				ResourceName:            "google_firestore_database.database",
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"project", "etag", "deletion_policy", "tags"},
-			},
-		},
-	})
+  acctest.VcrTest(t, resource.TestCase{
+    PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+    ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+    CheckDestroy:             testAccCheckFirestoreDatabaseDestroyProducer(t),
+    Steps: []resource.TestStep{
+      {
+        Config: testAccFirestoreDatabaseTags(context),
+      },
+      {
+        ResourceName:            "google_firestore_database.database",
+        ImportState:             true,
+        ImportStateVerify:       true,
+        ImportStateVerifyIgnore: []string{"project", "etag", "deletion_policy", "tags"},
+      },
+    ,
+  })
 }
 
 func testAccFirestoreDatabaseTags(context map[string]interface{}) string {
-	return acctest.Nprintf(`
+  return acctest.Nprintf(`
     resource "google_firestore_database" "database" {
       name                              = "tf-test-database-%{random_suffix}"
       location_id                       = "nam5"
