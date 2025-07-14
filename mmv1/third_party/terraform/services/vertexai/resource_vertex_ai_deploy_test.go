1291bf61dad5d91cd4ed5071ca610a8a457419d5
francis-ohara <francisohara@google.com>
2025-07-14 04:02:20 -0700

    Refactor: remove `project` from resource-level config in basic create test
    Removed the project field from the resource-level config of the test as it will be retrieved from provider-level config (environment variables) by default.

--- mmv1/third_party/terraform/services/vertexai/resource_vertex_ai_deploy_test.go
+++ mmv1/third_party/terraform/services/vertexai/resource_vertex_ai_deploy_test.go
@@ -7,14 +7,12 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
-	"github.com/hashicorp/terraform-provider-google/google/envvar"
 )
 
 func TestAccVertexAIEndpointWithModelGardenDeployment_basic(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"project":       envvar.GetTestProjectFromEnv(),
 		"random_suffix": acctest.RandString(t, 10),
 	}
 
@@ -47,7 +45,6 @@ func testAccVertexAIEndpointWithModelGardenDeployment_basic(context map[string]i
 resource "google_vertex_ai_endpoint_with_model_garden_deployment" "test" {
   publisher_model_name = "publishers/google/models/paligemma@paligemma-224-float32"
   location             = "us-central1"
-  project              = "%{project}"
   model_config {
     accept_eula =  true
   }
