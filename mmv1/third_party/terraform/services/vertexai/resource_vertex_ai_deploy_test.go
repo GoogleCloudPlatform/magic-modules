19a765cd0da4b1119d7277ec800041d668c09a39
Eliza Huang <heliza@google.com>
2025-07-14 20:57:03 +0000

    chore: add model config and deploy config unit test

--- mmv1/third_party/terraform/services/vertexai/resource_vertex_ai_deploy_test.go
+++ mmv1/third_party/terraform/services/vertexai/resource_vertex_ai_deploy_test.go
@@ -51,3 +51,57 @@ resource "google_vertex_ai_endpoint_with_model_garden_deployment" "test" {
 }
 `, context)
 }
+
+func TestAccVertexAIEndpointWithModelGardenDeployment_withConfigs(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             nil,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVertexAIEndpointWithModelGardenDeployment_withConfigs(context),
+			},
+			{
+				ResourceName:      "google_vertex_ai_endpoint_with_model_garden_deployment.test_with_configs",
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					"etag",
+					"id",
+					"publisher_model_name",
+					"project",
+					"model_config",
+					"deploy_config",
+				},
+			},
+		},
+	})
+}
+
+func testAccVertexAIEndpointWithModelGardenDeployment_withConfigs(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_vertex_ai_endpoint_with_model_garden_deployment" "test_with_configs" {
+  publisher_model_name = "publishers/google/models/paligemma@paligemma-224-float32"
+  location             = "us-central1"
+  model_config {
+    accept_eula =  true
+  }
+  deploy_config {
+    dedicated_resources {
+      machine_spec {
+        machine_type      = "g2-standard-16"
+        accelerator_type  = "NVIDIA_L4"
+        accelerator_count = 1
+      }
+      min_replica_count = 1
+    }
+  }
+}
+`, context)
+}
