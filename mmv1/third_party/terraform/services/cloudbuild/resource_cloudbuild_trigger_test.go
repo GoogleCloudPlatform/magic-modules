e7aeacbd8d8ea3f14e516373ba6c4999e75c7d00
Mattladdy <matthewladd19@gmail.com>
2025-07-15 14:57:24 -0400

    Adding developer_connect_event_config to fields

--- mmv1/third_party/terraform/services/cloudbuild/resource_cloudbuild_trigger_test.go
+++ mmv1/third_party/terraform/services/cloudbuild/resource_cloudbuild_trigger_test.go
@@ -703,3 +703,294 @@ resource "google_cloudbuild_trigger" "build_trigger" {
 }
 `, name)
 }
+
+func TestAccCloudBuildTrigger_developerConnect_pullRequest(t *testing.T) {
+	t.Parallel()
+	name := fmt.Sprintf("tf-test-%d", acctest.RandInt(t))
+	connectionName := fmt.Sprintf("tf-test-conn-%d", acctest.RandInt(t))
+	repoLinkName := fmt.Sprintf("tf-test-repo-link-%d", acctest.RandInt(t))
+	location := acctest.GetEnvDefaultOverride("LOCATION", "us-central1")
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy: testAccCheckCloudBuildTriggerDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudBuildTrigger_developerConnect_pullRequestConfig(name, connectionName, repoLinkName, location),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("google_cloudbuild_trigger.build_trigger", "name", name),
+					resource.TestCheckResourceAttrSet("google_cloudbuild_trigger.build_trigger", "developer_connect_event_config.0.git_repository_link"),
+					resource.TestCheckResourceAttr("google_cloudbuild_trigger.build_trigger", "developer_connect_event_config.0.pull_request.0.branch", "main"),
+					resource.TestCheckResourceAttr("google_cloudbuild_trigger.build_trigger", "developer_connect_event_config.0.pull_request.0.comment_control", "COMMENTS_ENABLED_FOR_EXTERNAL_CONTRIBUTORS_ONLY"),
+					resource.TestCheckResourceAttr("google_cloudbuild_trigger.build_trigger", "developer_connect_event_config.0.pull_request.0.invert_regex", "true"),
+				),
+			},
+			{
+				ResourceName:      "google_cloudbuild_trigger.build_trigger",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccCloudBuildTrigger_developerConnect_pullRequestConfigUpdated(name, connectionName, repoLinkName, location),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("google_cloudbuild_trigger.build_trigger", "name", name),
+					resource.TestCheckResourceAttr("google_cloudbuild_trigger.build_trigger", "developer_connect_event_config.0.pull_request.0.branch", "feature/.*"),
+					resource.TestCheckResourceAttr("google_cloudbuild_trigger.build_trigger", "developer_connect_event_config.0.pull_request.0.comment_control", "COMMENTS_DISABLED"),
+				),
+			},
+			{
+				ResourceName:      "google_cloudbuild_trigger.build_trigger",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccCloudBuildTrigger_developerConnect_push(t *testing.T) {
+	t.Parallel()
+	name := fmt.Sprintf("tf-test-%d", acctest.RandInt(t))
+	connectionName := fmt.Sprintf("tf-test-conn-%d", acctest.RandInt(t))
+	repoLinkName := fmt.Sprintf("tf-test-repo-link-%d", acctest.RandInt(t))
+	location := acctest.GetEnvDefaultOverride("LOCATION", "us-central1")
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy: testAccCheckCloudBuildTriggerDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudBuildTrigger_developerConnect_pushConfig(name, connectionName, repoLinkName, location),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("google_cloudbuild_trigger.build_trigger", "name", name),
+					resource.TestCheckResourceAttrSet("google_cloudbuild_trigger.build_trigger", "developer_connect_event_config.0.git_repository_link"),
+					resource.TestCheckResourceAttr("google_cloudbuild_trigger.build_trigger", "developer_connect_event_config.0.push.0.branch", "release/v.*"),
+					resource.TestCheckResourceAttr("google_cloudbuild_trigger.build_trigger", "developer_connect_event_config.0.push.0.invert_regex", "false"),
+					resource.TestCheckNoResourceAttr("google_cloudbuild_trigger.build_trigger", "developer_connect_event_config.0.push.0.tag"),
+				),
+			},
+			{
+				ResourceName:      "google_cloudbuild_trigger.build_trigger",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccCloudBuildTrigger_developerConnect_pushConfigUpdated(name, connectionName, repoLinkName, location),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("google_cloudbuild_trigger.build_trigger", "name", name),
+					resource.TestCheckResourceAttr("google_cloudbuild_trigger.build_trigger", "developer_connect_event_config.0.push.0.tag", "v\\d+\\.\\d+\\.\\d+"),
+					resource.TestCheckResourceAttr("google_cloudbuild_trigger.build_trigger", "developer_connect_event_config.0.push.0.invert_regex", "true"),
+					resource.TestCheckNoResourceAttr("google_cloudbuild_trigger.build_trigger", "developer_connect_event_config.0.push.0.branch"),
+				),
+			},
+			{
+				ResourceName:      "google_cloudbuild_trigger.build_trigger",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCloudBuildTrigger_developerConnect_pullRequestConfig(name, connectionName, repoLinkName, location string) string {
+	return fmt.Sprintf(`
+resource "google_developer_connect_connection" "dev_connect_connection" {
+  project = %[1]s
+  location = "%[4]s"
+  name = "%[2]s"
+  github_config {
+    app_installation_id = "1234567"
+    authorizer_credential {
+        oauth_token_secret_version = "projects/%[1]s/secrets/my-github-pat/versions/latest"
+    }
+  }
+}
+
+resource "google_developer_connect_git_repository_link" "dev_connect_repo_link" {
+  parent = google_developer_connect_connection.dev_connect_connection.id
+  name = "%[3]s"
+  github {
+    repo_uri = "https://github.com/GoogleCloudPlatform/my-test-repo"
+  }
+}
+
+resource "google_cloudbuild_trigger" "build_trigger" {
+  project = %[1]s
+  location = "%[4]s"
+  name = "%[5]s"
+  description = "Developer Connect Pull Request Trigger"
+
+  developer_connect_event_config {
+    git_repository_link = google_developer_connect_git_repository_link.dev_connect_repo_link.id
+    pull_request {
+      branch          = "main"
+      comment_control = "COMMENTS_ENABLED_FOR_EXTERNAL_CONTRIBUTORS_ONLY"
+      invert_regex    = true
+    }
+  }
+
+  build {
+    step {
+      name = "gcr.io/cloud-builders/gcloud"
+      args = ["builds", "triggers", "describe", google_cloudbuild_trigger.build_trigger.name, "--location=%[4]s", "--format=json"]
+    }
+  }
+
+  depends_on = [
+    google_developer_connect_git_repository_link.dev_connect_repo_link
+  ]
+}
+`, acctest.ProjectID(), connectionName, repoLinkName, location, name)
+}
+
+func testAccCloudBuildTrigger_developerConnect_pullRequestConfigUpdated(name, connectionName, repoLinkName, location string) string {
+	return fmt.Sprintf(`
+resource "google_developer_connect_connection" "dev_connect_connection" {
+  project = %[1]s
+  location = "%[4]s"
+  name = "%[2]s"
+  github_config {
+    app_installation_id = "1234567"
+    authorizer_credential {
+        oauth_token_secret_version = "projects/%[1]s/secrets/my-github-pat/versions/latest"
+    }
+  }
+}
+
+resource "google_developer_connect_git_repository_link" "dev_connect_repo_link" {
+  parent = google_developer_connect_connection.dev_connect_connection.id
+  name = "%[3]s"
+  github {
+    repo_uri = "https://github.com/GoogleCloudPlatform/my-test-repo"
+  }
+}
+
+resource "google_cloudbuild_trigger" "build_trigger" {
+  project = %[1]s
+  location = "%[4]s"
+  name = "%[5]s"
+  description = "Developer Connect Pull Request Trigger Updated"
+
+  developer_connect_event_config {
+    git_repository_link = google_developer_connect_git_repository_link.dev_connect_repo_link.id
+    pull_request {
+      branch          = "feature/.*"
+      comment_control = "COMMENTS_DISABLED"
+      invert_regex    = false
+    }
+  }
+
+  build {
+    step {
+      name = "gcr.io/cloud-builders/gcloud"
+      args = ["builds", "triggers", "describe", google_cloudbuild_trigger.build_trigger.name, "--location=%[4]s", "--format=json"]
+    }
+  }
+
+  depends_on = [
+    google_developer_connect_git_repository_link.dev_connect_repo_link
+  ]
+}
+`, acctest.ProjectID(), connectionName, repoLinkName, location, name)
+}
+
+func testAccCloudBuildTrigger_developerConnect_pushConfig(name, connectionName, repoLinkName, location string) string {
+	return fmt.Sprintf(`
+resource "google_developer_connect_connection" "dev_connect_connection" {
+  project = %[1]s
+  location = "%[4]s"
+  name = "%[2]s"
+  github_config {
+    app_installation_id = "1234567"
+    authorizer_credential {
+        oauth_token_secret_version = "projects/%[1]s/secrets/my-github-pat/versions/latest"
+    }
+  }
+}
+
+resource "google_developer_connect_git_repository_link" "dev_connect_repo_link" {
+  parent = google_developer_connect_connection.dev_connect_connection.id
+  name = "%[3]s"
+  github {
+    repo_uri = "https://github.com/GoogleCloudPlatform/my-test-repo"
+  }
+}
+
+resource "google_cloudbuild_trigger" "build_trigger" {
+  project = %[1]s
+  location = "%[4]s"
+  name = "%[5]s"
+  description = "Developer Connect Push Trigger"
+
+  developer_connect_event_config {
+    git_repository_link = google_developer_connect_git_repository_link.dev_connect_repo_link.id
+    push {
+      branch       = "release/v.*"
+      invert_regex = false
+    }
+  }
+
+  build {
+    step {
+      name = "gcr.io/cloud-builders/gcloud"
+      args = ["builds", "triggers", "describe", google_cloudbuild_trigger.build_trigger.name, "--location=%[4]s", "--format=json"]
+    }
+  }
+
+  depends_on = [
+    google_developer_connect_git_repository_link.dev_connect_repo_link
+  ]
+}
+`, acctest.ProjectID(), connectionName, repoLinkName, location, name)
+}
+
+func testAccCloudBuildTrigger_developerConnect_pushConfigUpdated(name, connectionName, repoLinkName, location string) string {
+	return fmt.Sprintf(`
+resource "google_developer_connect_connection" "dev_connect_connection" {
+  project = %[1]s
+  location = "%[4]s"
+  name = "%[2]s"
+  github_config {
+    app_installation_id = "1234567"
+    authorizer_credential {
+        oauth_token_secret_version = "projects/%[1]s/secrets/my-github-pat/versions/latest"
+    }
+  }
+}
+
+resource "google_developer_connect_git_repository_link" "dev_connect_repo_link" {
+  parent = google_developer_connect_connection.dev_connect_connection.id
+  name = "%[3]s"
+  github {
+    repo_uri = "https://github.com/GoogleCloudPlatform/my-test-repo"
+  }
+}
+
+resource "google_cloudbuild_trigger" "build_trigger" {
+  project = %[1]s
+  location = "%[4]s"
+  name = "%[5]s"
+  description = "Developer Connect Push Trigger Updated"
+
+  developer_connect_event_config {
+    git_repository_link = google_developer_connect_git_repository_link.dev_connect_repo_link.id
+    push {
+      tag          = "v\\d+\\.\\d+\\.\\d+"
+      invert_regex = true
+    }
+  }
+
+  build {
+    step {
+      name = "gcr.io/cloud-builders/gcloud"
+      args = ["builds", "triggers", "describe", google_cloudbuild_trigger.build_trigger.name, "--location=%[4]s", "--format=json"]
+    }
+  }
+
+  depends_on = [
+    google_developer_connect_git_repository_link.dev_connect_repo_link
+  ]
+}
+`, acctest.ProjectID(), connectionName, repoLinkName, location, name)
+}
