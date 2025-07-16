26f8911cc15438d87f759c704cbb91dbed531f3a
Thomas Rodgers <thomasrodgers@google.com>
2025-07-16 10:48:21 -0700

    tgc-revival: support cloudfunctions2 function in tgc next (#14539)

--- mmv1/api/resource.go
+++ mmv1/api/resource.go
@@ -2052,7 +2052,7 @@ func (r Resource) TGCTestIgnorePropertiesToStrings(e resource.Examples) []string
 // Filters out computed properties during cai2hcl
 func (r Resource) ReadPropertiesForTgc() []*Type {
 	return google.Reject(r.AllUserProperties(), func(v *Type) bool {
-		return v.Output
+		return v.Output || v.UrlParamOnly
 	})
 }
 
