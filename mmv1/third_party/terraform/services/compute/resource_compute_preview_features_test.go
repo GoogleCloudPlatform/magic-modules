1e9c5dbb2244a62b827f0b0442791fd0b42e5ff2
Huici Pan <huici@google.com>
2025-07-16 18:09:53 +0000

    remove enable test as missing testing org policy override

--- mmv1/third_party/terraform/services/compute/resource_compute_preview_features_test.go
+++ mmv1/third_party/terraform/services/compute/resource_compute_preview_features_test.go
@@ -20,12 +20,12 @@ func TestAccComputePreviewFeature_update(t *testing.T) {
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		Steps: []resource.TestStep{
-			// Step 1: Enable the "alpha-api-access" feature and verify its attributes.
+			// Step 1: Disable the "alpha-api-access" feature and verify its attributes.
 			{
-				Config: testAccComputePreviewFeature_enable(featureName),
+				Config: testAccComputePreviewFeature_disable(featureName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "name", featureName),
-					resource.TestCheckResourceAttr(resourceName, "activation_status", "ENABLED"),
+					resource.TestCheckResourceAttr(resourceName, "activation_status", "DISABLED"),
 				),
 			},
 			// Step 2: Verify that the resource can be successfully imported.
@@ -35,38 +35,8 @@ func TestAccComputePreviewFeature_update(t *testing.T) {
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{"rollout_operation"},
 			},
-			// Step 3: Disable the "alpha-api-access" feature and verify its attributes.
-			{
-				Config: testAccComputePreviewFeature_disable(featureName),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "name", featureName),
-					resource.TestCheckResourceAttr(resourceName, "activation_status", "DISABLED"),
-				),
-			},
-			// Step 4: Verify that the resource can be successfully imported.
-			{
-				ResourceName:            resourceName,
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"rollout_operation"},
-			},
 		},
 	})
-}
-
-func testAccComputePreviewFeature_enable(name string) string {
-	return fmt.Sprintf(`
-resource "google_compute_preview_feature" "acceptance" {
-  name              = "%s"
-  activation_status = "ENABLED"
-  
-  rollout_operation {
-    rollout_input {
-      predefined_rollout_plan = "ROLLOUT_PLAN_FAST_ROLLOUT"
-    }
-  }
-}
-`, name)
 }
 
 func testAccComputePreviewFeature_disable(name string) string {
