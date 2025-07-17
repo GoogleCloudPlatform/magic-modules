102f5fb0e7107ec1b9eb94229a4db3cec0df01b6
Arnav Dham <arnavdham@google.com>
2025-07-17 07:00:26 +0000

    changes made as asked.

--- mmv1/third_party/terraform/services/bigqueryanalyticshub/resource_bigquery_analytics_hub_query_template_test.go
+++ mmv1/third_party/terraform/services/bigqueryanalyticshub/resource_bigquery_analytics_hub_query_template_test.go
@@ -52,28 +52,28 @@ func TestAccBigqueryAnalyticsHubQueryTemplate_bigqueryAnalyticshubQuerytemplateU
 func testAccBigqueryAnalyticsHubQueryTemplate_bigqueryAnalyticshubQuerytemplateUpdate(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 resource "google_bigquery_analytics_hub_data_exchange" "querytemplate" {
-display_name = "My Audience Data Exchange"
-data_exchange_id = "tf_test_my_data_exchange%{random_suffix}"
-description = "example of query template%{random_suffix}"
-location = "us"
-sharing_environment_config {
-dcr_exchange_config {}
-}
+	display_name = "My Audience Data Exchange"
+	data_exchange_id = "tf_test_my_data_exchange%{random_suffix}"
+	description = "example of query template%{random_suffix}"
+	location = "us"
+	sharing_environment_config {
+	dcr_exchange_config {}
+	}
 }
 
 resource "google_bigquery_analytics_hub_query_template" "querytemplate" {
-location = "us"
-data_exchange_id = google_bigquery_analytics_hub_data_exchange.querytemplate.data_exchange_id
-query_template_id = "qt1%{random_suffix}"
-display_name = "qt1%{random_suffix}"
-description = "updated example of query template%{random_suffix}"
-primary_contact = "adminupdated@example.com"
-documentation = "This TVF takes a table t1 as input and returns all columns. Useful for basic data pass-through."
-routine {
-routine_type="TABLE_VALUED_FUNCTION"
-definition_body="qt1%{random_suffix}() as (select * from t1updated)"
-}
-submit=true
+	location = "us"
+	data_exchange_id = google_bigquery_analytics_hub_data_exchange.querytemplate.data_exchange_id
+	query_template_id = "qt1%{random_suffix}"
+	display_name = "qt1%{random_suffix}"
+	description = "updated example of query template%{random_suffix}"
+	primary_contact = "adminupdated@example.com"
+	documentation = "This TVF takes a table t1 as input and returns all columns. Useful for basic data pass-through."
+	routine {
+		routine_type="TABLE_VALUED_FUNCTION"
+		definition_body="qt1%{random_suffix}() as (select * from t1updated)"
+	}
+	submit=true
 } 
 `, context)
 }
