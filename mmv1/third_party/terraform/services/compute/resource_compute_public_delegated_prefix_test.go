4a1e20425f188fcfbeaf687dfdff00fcbef04719
malhotrasagar2212 <sagar.malhotra@hashicorp.com>
2025-07-24 16:27:10 -0400

    updated test configs

--- mmv1/third_party/terraform/services/compute/resource_compute_public_delegated_prefix_test.go
+++ mmv1/third_party/terraform/services/compute/resource_compute_public_delegated_prefix_test.go
@@ -15,10 +15,12 @@ import (
 
 func TestAccComputePublicDelegatedPrefix_computePublicDelegatedPrefixWithSubPrefixExample(t *testing.T) {
 	t.Parallel()
+	subPrefixResourceName := "google_compute_public_delegated_prefix.subprefix"
+	parentDataSourceName := "data.google_compute_public_delegated_prefix.parent"
 
 	context := map[string]interface{}{
 		"parent_pdp_id": "projects/tf-static-byoip/regions/us-central1/publicDelegatedPrefixes/tf-test-delegation-mode-sub-pdp",
-		"project":       "tf-static-byoip",
+		"project":       "byoipv6-fr-prober",
 		"random_suffix": acctest.RandString(t, 10),
 	}
 
@@ -30,17 +32,34 @@ func TestAccComputePublicDelegatedPrefix_computePublicDelegatedPrefixWithSubPref
 			{
 				Config: testAccComputePublicDelegatedPrefix_computePublicDelegatedPrefixWithSubPrefixExample(context),
 				Check: resource.ComposeTestCheckFunc(
-					// 1. Verify that the sub-prefixes list contains exactly one block.
-					resource.TestCheckResourceAttr("google_compute_public_delegated_prefix.prefixes", "public_delegated_sub_prefixs.#", "1"),
+					// // 1. Verify that the sub-prefixes list contains exactly one block.
+					// resource.TestCheckResourceAttr("google_compute_public_delegated_prefix.prefixes", "public_delegated_sub_prefixs.#", "1"),
 
-					// 2. Verify the description of the first sub-prefix in the list.
-					resource.TestCheckResourceAttr("google_compute_public_delegated_prefix.prefixes", "public_delegated_sub_prefixs.0.description", "A nested address"),
+					// // 2. Verify the description of the first sub-prefix in the list.
+					// resource.TestCheckResourceAttr("google_compute_public_delegated_prefix.prefixes", "public_delegated_sub_prefixs.0.description", "A nested address"),
 
-					// 3. Verify the 'is_address' attribute of the first sub-prefix.
-					resource.TestCheckResourceAttr("google_compute_public_delegated_prefix.prefixes", "public_delegated_sub_prefixs.0.is_address", "true"),
+					// // 3. Verify the 'is_address' attribute of the first sub-prefix.
+					// resource.TestCheckResourceAttr("google_compute_public_delegated_prefix.prefixes", "public_delegated_sub_prefixs.0.is_address", "false"),
 
-					// 4. Verify that the name of the first sub-prefix is set to some value.
-					resource.TestCheckResourceAttrSet("google_compute_public_delegated_prefix.prefixes", "public_delegated_sub_prefixs.0.name"),
+					// // 4. Verify that the name of the first sub-prefix is set to some value.
+					// resource.TestCheckResourceAttrSet("google_compute_public_delegated_prefix.prefixes", "public_delegated_sub_prefixs.0.name"),
+
+					// Verify that the parent's sub-prefix list now contains at least one item.
+					// We check for "1" assuming a clean test environment.
+					resource.TestCheckResourceAttr(parentDataSourceName, "public_delegated_sub_prefixes.#", "1"),
+
+					// Verify that the 'name' of the first sub-prefix in the parent's list
+					// matches the name of the sub-prefix resource we just created.
+					resource.TestCheckResourceAttrPair(
+						parentDataSourceName, "public_delegated_sub_prefixes.0.name",
+						subPrefixResourceName, "name",
+					),
+
+					// Verify its IP range as well
+					resource.TestCheckResourceAttrPair(
+						parentDataSourceName, "public_delegated_sub_prefixes.0.ip_cidr_range",
+						subPrefixResourceName, "ip_cidr_range",
+					),
 				),
 			},
 			{
@@ -55,24 +74,28 @@ func TestAccComputePublicDelegatedPrefix_computePublicDelegatedPrefixWithSubPref
 
 func testAccComputePublicDelegatedPrefix_computePublicDelegatedPrefixWithSubPrefixExample(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_compute_public_delegated_prefix" "prefixes" {
-  name = "tf-test-prefix-with-sub-prefixes%{random_suffix}"
-  region = "us-central1"
-  description = "Public delegated prefix with sub prefix for testing "
-  ip_cidr_range = "2600:1901:4500:2::/64"
-  parent_prefix = "%{parent_pdp_id}"
-  project = "%{project}"
-  mode = "DELEGATION"
+// resource "google_compute_public_delegated_prefix" "prefixes" {
+//   name = "tf-test-prefix-with-sub-prefixes%{random_suffix}"
+//   region = "us-central1"
+//   description = "Public delegated prefix with sub prefix for testing "
+//   ip_cidr_range = "2600:1901:4500:2::/64"
+//   parent_prefix = "%{parent_pdp_id}"
+//   project = "%{project}"
+//   mode = "DELEGATION"
 
   
+// }
+data "google_compute_public_delegated_prefix" "parent" {
+  name = "tf-test-delegation-mode-sub-pdp"
+  region = "us-central1"
+  project = "%{project}"
 }
-
 resource "google_compute_public_delegated_prefix" "subprefix" {
   name = "tf-test-sub-prefix-1%{random_suffix}"
   description = "A nested address"
   region = "us-central1"
   ip_cidr_range = "2600:1901:4500:1:2::/96"
-  parent_prefix = google_compute_public_delegated_prefix.prefixes.id
+  parent_prefix = data.google_compute_public_delegated_prefix.parent.id
   mode = "DELEGATION"
   allocatable_prefix_length = 64
 }
