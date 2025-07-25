4436002c4f0ad018a0f721f28c1464e6a9175ad5
malhotrasagar2212 <sagar.malhotra@hashicorp.com>
2025-07-24 23:02:08 -0400

    updated tests

--- mmv1/third_party/terraform/services/compute/resource_compute_public_delegated_prefix_test.go
+++ mmv1/third_party/terraform/services/compute/resource_compute_public_delegated_prefix_test.go
@@ -16,7 +16,9 @@ import (
 func TestAccComputePublicDelegatedPrefix_computePublicDelegatedPrefixWithSubPrefixExample(t *testing.T) {
 	t.Parallel()
 	subPrefixResourceName := "google_compute_public_delegated_prefix.subprefix"
-	parentDataSourceName := "data.google_compute_public_delegated_prefix.parent"
+	parentProject := "byoipv6-fr-prober"
+	parentRegion := "us-central1"
+	parentName := "tf-test-delegation-mode-sub-pdp"
 
 	context := map[string]interface{}{
 		"parent_pdp_id": "projects/tf-static-byoip/regions/us-central1/publicDelegatedPrefixes/tf-test-delegation-mode-sub-pdp",
@@ -32,34 +34,11 @@ func TestAccComputePublicDelegatedPrefix_computePublicDelegatedPrefixWithSubPref
 			{
 				Config: testAccComputePublicDelegatedPrefix_computePublicDelegatedPrefixWithSubPrefixExample(context),
 				Check: resource.ComposeTestCheckFunc(
-					// // 1. Verify that the sub-prefixes list contains exactly one block.
-					// resource.TestCheckResourceAttr("google_compute_public_delegated_prefix.prefixes", "public_delegated_sub_prefixs.#", "1"),
+					// First, a basic check that the sub-prefix was created
+					resource.TestCheckResourceAttrSet(subPrefixResourceName, "id"),
 
-					// // 2. Verify the description of the first sub-prefix in the list.
-					// resource.TestCheckResourceAttr("google_compute_public_delegated_prefix.prefixes", "public_delegated_sub_prefixs.0.description", "A nested address"),
-
-					// // 3. Verify the 'is_address' attribute of the first sub-prefix.
-					// resource.TestCheckResourceAttr("google_compute_public_delegated_prefix.prefixes", "public_delegated_sub_prefixs.0.is_address", "false"),
-
-					// // 4. Verify that the name of the first sub-prefix is set to some value.
-					// resource.TestCheckResourceAttrSet("google_compute_public_delegated_prefix.prefixes", "public_delegated_sub_prefixs.0.name"),
-
-					// Verify that the parent's sub-prefix list now contains at least one item.
-					// We check for "1" assuming a clean test environment.
-					resource.TestCheckResourceAttr(parentDataSourceName, "public_delegated_sub_prefixes.#", "1"),
-
-					// Verify that the 'name' of the first sub-prefix in the parent's list
-					// matches the name of the sub-prefix resource we just created.
-					resource.TestCheckResourceAttrPair(
-						parentDataSourceName, "public_delegated_sub_prefixes.0.name",
-						subPrefixResourceName, "name",
-					),
-
-					// Verify its IP range as well
-					resource.TestCheckResourceAttrPair(
-						parentDataSourceName, "public_delegated_sub_prefixes.0.ip_cidr_range",
-						subPrefixResourceName, "ip_cidr_range",
-					),
+					// Now, the custom check function
+					testAccCheckParentHasSubPrefix(t, parentProject, parentRegion, parentName, subPrefixResourceName),
 				),
 			},
 			{
@@ -85,21 +64,52 @@ func testAccComputePublicDelegatedPrefix_computePublicDelegatedPrefixWithSubPref
 
   
 // }
-data "google_compute_public_delegated_prefix" "parent" {
-  name = "tf-test-delegation-mode-sub-pdp"
-  region = "us-central1"
-  project = "%{project}"
-}
+
 resource "google_compute_public_delegated_prefix" "subprefix" {
   name = "tf-test-sub-prefix-1%{random_suffix}"
   description = "A nested address"
   region = "us-central1"
   ip_cidr_range = "2600:1901:4500:1:2::/96"
-  parent_prefix = data.google_compute_public_delegated_prefix.parent.id
+  parent_prefix = "%{parent_pdp_id}"
   mode = "DELEGATION"
   allocatable_prefix_length = 64
 }
 `, context)
+}
+
+func testAccCheckParentHasSubPrefix(t *testing.T, project, region, parentName, subPrefixResourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		// Find the newly created sub-prefix resource in the Terraform state
+		rs, ok := s.RootModule().Resources[subPrefixResourceName]
+		if !ok {
+			return fmt.Errorf("Not found: %s", subPrefixResourceName)
+		}
+		newSubPrefixName := rs.Primary.Attributes["name"]
+
+		// Get the authenticated GCP client from the test helper
+		config := acctest.GoogleProviderConfig(t)
+		computeService, err := config.NewComputeClient(config.UserAgent)
+		if err != nil {
+			return err
+		}
+
+		// Make a direct API call to GCP to get the parent's details
+		parent, err := computeService.PublicDelegatedPrefixes.Get(project, region, parentName).Do()
+		if err != nil {
+			return err
+		}
+
+		// Loop through the list of sub-prefixes returned by the API
+		for _, sub := range parent.PublicDelegatedSubPrefixs {
+			if sub.Name == newSubPrefixName {
+				// Success! We found the new sub-prefix in the parent's list.
+				return nil
+			}
+		}
+
+		// If the loop finishes without finding the sub-prefix, return an error.
+		return fmt.Errorf("sub-prefix %q not found in parent %q's sub-prefix list", newSubPrefixName, parentName)
+	}
 }
 
 func testAccCheckComputePublicDelegatedPrefixWithSubPrefixDestroyProducer(t *testing.T) func(s *terraform.State) error {
