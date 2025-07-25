41f2e29110ba640e38047681b448ab258e84b32d
Rajesh Guptha <rajeshguptha@google.com>
2025-07-25 09:10:39 +0000

    Adding Tags support for GkeBackup - Backup Plan and Restore Plan

--- mmv1/third_party/terraform/services/gkebackup/resource_gke_backup_restore_plan_test.go
+++ mmv1/third_party/terraform/services/gkebackup/resource_gke_backup_restore_plan_test.go
@@ -202,3 +202,116 @@ resource "google_gke_backup_restore_plan" "restore_plan" {
 }
 `, context)
 }
+
+func TestAccGKEBackupRestorePlan_tags(t *testing.T) {
+	t.Parallel()
+
+	tagKey := acctest.BootstrapSharedTestOrganizationTagKey(t, "gke_backup_restore_plan_tagkey", map[string]interface{}{})
+	context := map[string]interface{}{
+		"project":             envvar.GetTestProjectFromEnv(),
+		"deletion_protection": false,
+		"network_name":        acctest.BootstrapSharedTestNetwork(t, "gke-cluster"),
+		"subnetwork_name":     acctest.BootstrapSubnet(t, "gke-cluster", acctest.BootstrapSharedTestNetwork(t, "gke-cluster")),
+		"random_suffix":       acctest.RandString(t, 10),
+		"org":                 envvar.GetTestOrgFromEnv(t),
+		"tagKey":              tagKey,
+		"tagValue":            acctest.BootstrapSharedTestOrganizationTagValue(t, "gke_backup_restore_plan_tagvalue", tagKey),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccGKEBackupRestorePlanTags(context),
+				Check: resource.TestCheckFunc(
+					resource.TestCheckResourceAttrSet(
+						"google_gke_backup_restore_plan.restore_plan", "tags.%"),
+				),
+			},
+			{
+				ResourceName:            "google_gke_backup_restore_plan.restore_plan",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "location", "terraform_labels", "tags"},
+			},
+		},
+	})
+}
+
+func testAccGKEBackupRestorePlanTags(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_container_cluster" "primary" {
+  name               = "tf-test-restore-plan%{random_suffix}-cluster"
+  location           = "us-central1"
+  initial_node_count = 1
+  workload_identity_config {
+    workload_pool = "%{project}.svc.id.goog"
+  }
+  addons_config {
+    gke_backup_agent_config {
+      enabled = true
+    }
+  }
+  deletion_protection  = "%{deletion_protection}"
+  network       = "%{network_name}"
+  subnetwork    = "%{subnetwork_name}"
+}
+
+resource "google_gke_backup_backup_plan" "basic" {
+  name = "tf-test-restore-plan%{random_suffix}"
+  cluster = google_container_cluster.primary.id
+  location = "us-central1"
+  backup_config {
+    include_volume_data = true
+    include_secrets = true
+    all_namespaces = true
+  }
+}
+
+resource "google_gke_backup_restore_plan" "restore_plan" {
+  name = "tf-test-restore-plan%{random_suffix}"
+  location = "us-central1"
+  backup_plan = google_gke_backup_backup_plan.basic.id
+  cluster = google_container_cluster.primary.id
+  tags = {
+	"%{org}/%{tagKey}" = "%{tagValue}"
+  }
+  restore_config {
+    all_namespaces = true
+    namespaced_resource_restore_mode = "MERGE_SKIP_ON_CONFLICT"
+    volume_data_restore_policy = "RESTORE_VOLUME_DATA_FROM_BACKUP"
+    cluster_resource_restore_scope {
+      all_group_kinds = true
+    }
+    cluster_resource_conflict_policy = "USE_EXISTING_VERSION"
+    restore_order {
+        group_kind_dependencies {
+            satisfying {
+                resource_group = "stable.example.com"
+                resource_kind = "kindA"
+            }
+            requiring {
+                resource_group = "stable.example.com"
+                resource_kind = "kindB"
+            }
+        }
+        group_kind_dependencies {
+            satisfying {
+                resource_group = "stable.example.com"
+                resource_kind = "kindB"
+            }
+            requiring {
+                resource_group = "stable.example.com"
+                resource_kind = "kindC"
+            }
+        }
+    }
+    volume_data_restore_policy_bindings {
+        policy = "RESTORE_VOLUME_DATA_FROM_BACKUP"
+        volume_type = "GCE_PERSISTENT_DISK"
+    }
+  }
+}
+`, context)
+}
