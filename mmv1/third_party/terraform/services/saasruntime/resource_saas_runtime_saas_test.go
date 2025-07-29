501c8885a472b8c923f0703e3a852610db634d77
Andres Varela <dreva@google.com>
2025-07-29 10:15:09 +0000

    fix test import

--- mmv1/third_party/terraform/services/saasruntime/resource_saas_runtime_saas_test.go
+++ mmv1/third_party/terraform/services/saasruntime/resource_saas_runtime_saas_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 
-	"github.com/hashicorp/terraform-provider-google-beta/google-beta/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
 )
 
 func TestAccSaasRuntimeSaas_saasRuntimeSaasBasicExample(t *testing.T) {
