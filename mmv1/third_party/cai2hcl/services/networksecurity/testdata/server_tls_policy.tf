e411c4030debc2bd24bc7cf06d9202d8541ef780
Laurenz Kirchmayr <laurenzk@google.com>
2025-07-30 08:47:55 +0000

    Reorder terraform resource block fields.
    Context: Tests compare strings 1:1. Cai2Hcl outputs fields in alphabetical order.

--- mmv1/third_party/cai2hcl/services/networksecurity/testdata/server_tls_policy.tf
+++ mmv1/third_party/cai2hcl/services/networksecurity/testdata/server_tls_policy.tf
@@ -1,120 +1,154 @@
 resource "google_network_security_server_tls_policy" "lb_mtls_policy" {
-  name                   = "lb_mtls_policy"
-  description            = "my description"
-  location               = "global"
-  labels                 = {
+  allow_open  = "false"
+  description = "my description"
+
+  labels = {
     foo = "bar"
   }
+
+  location = "global"
+
   mtls_policy {
     client_validation_mode         = "REJECT_INVALID"
     client_validation_trust_config = "projects/307841421122/locations/global/trustConfigs/id-4adf7779-1e9f-4124-9438-652c80886074"
   }
+
+  name = "lb_mtls_policy"
 }
 
 resource "google_network_security_server_tls_policy" "td_mtls_policy" {
-  name                   = "td_mtls_policy"
-  description            = "my description"
-  location               = "global"
-  labels                 = {
+  allow_open  = "false"
+  description = "my description"
+
+  labels = {
     foo = "bar"
   }
-  allow_open = "false"
+
+  location = "global"
+
+  mtls_policy {
+    client_validation_ca {
+      certificate_provider_instance {
+        plugin_instance = "google_cloud_private_spiffe"
+      }
+    }
+  }
+
+  name = "td_mtls_policy"
+
   server_certificate {
     certificate_provider_instance {
       plugin_instance = "google_cloud_private_spiffe"
     }
   }
-  mtls_policy {
-    client_validation_ca {
-      certificate_provider_instance {
-        plugin_instance = "google_cloud_private_spiffe" 
-      }
+}
+
+resource "google_network_security_server_tls_policy" "td_with_server_cert_policy" {
+  allow_open  = "false"
+  description = "my description"
+  location    = "global"
+  name        = "td_with_server_cert_policy"
+
+  server_certificate {
+    grpc_endpoint {
+      target_uri = "unix:mypath"
     }
   }
 }
 
-resource "google_network_security_server_tls_policy" "td_with_server_cert_policy" {
-  name                   = "td_with_server_cert_policy"
-  description            = "my description"
-  location               = "global"
-  allow_open             = "false"
-  server_certificate {
-    grpc_endpoint {
-        target_uri = "unix:mypath"
-      }
-  }
-}
-
 resource "google_network_security_server_tls_policy" "empty_description_policy" {
-  name                   = "empty_description_policy"
-  location               = "global"
-  labels                 = {
+  allow_open = "false"
+
+  labels = {
     foo = "bar"
   }
+
+  location = "global"
+
   mtls_policy {
     client_validation_mode         = "REJECT_INVALID"
     client_validation_trust_config = "projects/307841421122/locations/global/trustConfigs/id-4adf7779-1e9f-4124-9438-652c80886074"
   }
+
+  name = "empty_description_policy"
 }
 
 resource "google_network_security_server_tls_policy" "empty_labels_policy" {
-  name                   = "empty_labels_policy"
-  description            = "my description"
-  location               = "global"
+  allow_open  = "false"
+  description = "my description"
+  location    = "global"
+
   mtls_policy {
     client_validation_mode         = "REJECT_INVALID"
     client_validation_trust_config = "projects/307841421122/locations/global/trustConfigs/id-4adf7779-1e9f-4124-9438-652c80886074"
   }
+
+  name = "empty_labels_policy"
 }
 
 resource "google_network_security_server_tls_policy" "regional_location_policy" {
-  name                   = "regional_location_policy"
-  description            = "my description"
-  location               = "us-central1"
-  labels                 = {
+  allow_open  = "false"
+  description = "my description"
+
+  labels = {
     foo = "bar"
   }
+
+  location = "us-central1"
+
   mtls_policy {
     client_validation_mode         = "REJECT_INVALID"
     client_validation_trust_config = "projects/307841421122/locations/us-central1/trustConfigs/tsmx-20250609-tc1"
   }
+
+  name = "regional_location_policy"
 }
 
 resource "google_network_security_server_tls_policy" "lb_mtls_allow_invalid_cert_policy" {
-  name                   = "lb_mtls_allow_invalid_cert_policy"
-  labels                 = {
+  allow_open  = "false"
+  description = "my description"
+
+  labels = {
     foo = "bar"
   }
-  description            = "my description"
-  location               = "global"
+
+  location = "global"
+
   mtls_policy {
     client_validation_mode = "ALLOW_INVALID_OR_MISSING_CLIENT_CERT"
   }
+
+  name = "lb_mtls_allow_invalid_cert_policy"
 }
 
 resource "google_network_security_server_tls_policy" "td_allow_open_policy" {
-  name                   = "td_allow_open_policy"
-  description            = "my description"
-  location               = "global"
-  allow_open             = "true"
-  server_certificate {
-    grpc_endpoint {
-        target_uri = "unix:mypath"
-      }
-  }
+  allow_open  = "true"
+  description = "my description"
+  location    = "global"
+
   mtls_policy {
     client_validation_ca {
       certificate_provider_instance {
-        plugin_instance = "google_cloud_private_spiffe" 
+        plugin_instance = "google_cloud_private_spiffe"
       }
+    }
+  }
+
+  name = "td_allow_open_policy"
+
+  server_certificate {
+    grpc_endpoint {
+      target_uri = "unix:mypath"
     }
   }
 }
 
 resource "google_network_security_server_tls_policy" "td_with_cert_provider_policy" {
-  name                   = "td_with_cert_provider_policy"
-  description            = "my description"
-  location               = "global"
+  allow_open  = "false"
+  description = "my description"
+  location    = "global"
+  name        = "td_with_cert_provider_policy"
+
   server_certificate {
     certificate_provider_instance {
       plugin_instance = "google_cloud_private_spiffe"
@@ -123,23 +157,28 @@ resource "google_network_security_server_tls_policy" "td_with_cert_provider_poli
 }
 
 resource "google_network_security_server_tls_policy" "td_mtls_client_validation_grpc_policy" {
-  name                   = "td_mtls_client_validation_grpc_policy"
-  description            = "my description"
-  location               = "global"
-  labels                 = {
+  allow_open  = "false"
+  description = "my description"
+
+  labels = {
     foo = "bar"
   }
-  allow_open = "false"
-  server_certificate {
-    certificate_provider_instance {
-      plugin_instance = "google_cloud_private_spiffe"
-    }
-  }
+
+  location = "global"
+
   mtls_policy {
     client_validation_ca {
       grpc_endpoint {
         target_uri = "unix:mypath"
       }
+    }
+  }
+
+  name = "td_mtls_client_validation_grpc_policy"
+
+  server_certificate {
+    certificate_provider_instance {
+      plugin_instance = "google_cloud_private_spiffe"
     }
   }
 }
