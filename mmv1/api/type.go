eac6b4fa4414ee7cbe679f374dfb3c9b793dcae2
Stephen Lewis <stephenrlewis@google.com>
2025-07-31 09:26:13 -0700

    Added wildcard coverage for json fields

--- mmv1/api/type.go
+++ mmv1/api/type.go
@@ -1274,3 +1274,13 @@ func (t Type) GetPropertySchemaPathList(propertyList []string) []string {
 	}
 	return list
 }
+
+func (t Type) CoversDescendants() bool {
+	if t.CustomFlatten == "templates/terraform/custom_flatten/json_schema.tmpl" {
+		return true
+	}
+	if t.CustomExpand == "templates/terraform/custom_expand/json_schema.tmpl" || t.CustomExpand == "templates/terraform/custom_expand/json_value.tmpl" {
+		return true
+	}
+	return false
+}
