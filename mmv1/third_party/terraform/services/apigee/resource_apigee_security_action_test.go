f05cc8bbfaf233286d36ca6832abcaadf755c860
Shuya Ma <shuyama@google.com>
2025-07-31 14:54:08 -0700

    add delay

--- mmv1/third_party/terraform/services/apigee/resource_apigee_security_action_test.go
+++ mmv1/third_party/terraform/services/apigee/resource_apigee_security_action_test.go
@@ -57,7 +57,9 @@ func TestAccApigeeSecurityAction_apigeeSecurityActionFull(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"random_suffix": acctest.RandString(t, 10),
+		"billing_account": envvar.GetTestBillingAccountFromEnv(t),
+		"org_id":          envvar.GetTestOrgFromEnv(t),
+		"random_suffix":   acctest.RandString(t, 10),
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
@@ -177,29 +179,72 @@ func TestAccApigeeSecurityAction_apigeeSecurityActionFull(t *testing.T) {
 
 func testAccApigeeSecurityAction_apigeeBase(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-data "google_client_config" "current" {}
+resource "google_project" "project" {
+  project_id      = "tf-test%{random_suffix}"
+  name            = "tf-test%{random_suffix}"
+  org_id          = "%{org_id}"
+  billing_account = "%{billing_account}"
+  deletion_policy = "DELETE"
+}
+
+resource "time_sleep" "wait_60_seconds" {
+  create_duration = "60s"
+  depends_on = [google_project.project]
+}
+
+resource "google_project_service" "apigee" {
+  project = google_project.project.project_id
+  service = "apigee.googleapis.com"
+  depends_on = [time_sleep.wait_60_seconds]
+}
+
+resource "google_project_service" "servicenetworking" {
+  project = google_project.project.project_id
+  service = "servicenetworking.googleapis.com"
+  depends_on = [google_project_service.apigee]
+}
+
+resource "google_project_service" "compute" {
+  project = google_project.project.project_id
+  service = "compute.googleapis.com"
+  depends_on = [google_project_service.servicenetworking]
+}
+
+resource "time_sleep" "wait_120_seconds" {
+  create_duration = "120s"
+  depends_on = [google_project_service.compute]
+}
 
 resource "google_compute_network" "apigee_network" {
-    name = "tf-test-network-%{random_suffix}"
+  name       = "apigee-network"
+  project    = google_project.project.project_id
+  depends_on = [time_sleep.wait_120_seconds]
 }
 
 resource "google_compute_global_address" "apigee_range" {
-    name          = "tf-test-address-%{random_suffix}"
-    purpose       = "VPC_PEERING"
-    address_type  = "INTERNAL"
-    prefix_length = 16
-    network       = google_compute_network.apigee_network.id
+  name          = "apigee-range"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.apigee_network.id
+  project       = google_project.project.project_id
+}
+
+resource "time_sleep" "wait_60_seconds_1" {
+  create_duration = "60s"
+  depends_on = [google_compute_global_address.apigee_range]
 }
 
 resource "google_service_networking_connection" "apigee_vpc_connection" {
-    network                 = google_compute_network.apigee_network.id
-    service                 = "servicenetworking.googleapis.com"
-    reserved_peering_ranges = [google_compute_global_address.apigee_range.name]
+  network                 = google_compute_network.apigee_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.apigee_range.name]
+  depends_on              = [time_sleep.wait_60_seconds_1]
 }
 
 resource "google_apigee_organization" "apigee_org" {
     analytics_region   = "us-central1"
-    project_id         = data.google_client_config.current.project
+    project_id         = google_project.project.project_id
     authorized_network = google_compute_network.apigee_network.id
     depends_on         = [google_service_networking_connection.apigee_vpc_connection]
 }
