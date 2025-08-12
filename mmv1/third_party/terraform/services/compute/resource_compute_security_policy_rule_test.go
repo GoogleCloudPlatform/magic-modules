b323daf08c6e0c7f9249c6054c3461bd382b1924
Cezary Sobczak <Cezary.Sobczak@infogain.com>
2025-08-12 12:35:26 +0200

    Add labels and labels_fingerprint fields
    Signed-off-by: Cezary Sobczak <Cezary.Sobczak@infogain.com>

--- mmv1/third_party/terraform/services/compute/resource_compute_security_policy_rule_test.go
+++ mmv1/third_party/terraform/services/compute/resource_compute_security_policy_rule_test.go
@@ -2,9 +2,10 @@ package compute_test
 
 import (
 	"fmt"
-	"github.com/hashicorp/terraform-provider-google/google/acctest"
 	"regexp"
 	"testing"
+
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
