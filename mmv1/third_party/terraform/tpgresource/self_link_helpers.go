c10a8719ae1c288533880ace7b35986323fb28e2
Nick Elliot <nickelliot@google.com>
2025-08-15 14:18:21 -0700

    moved to tpgresource

--- mmv1/third_party/terraform/tpgresource/self_link_helpers.go
+++ mmv1/third_party/terraform/tpgresource/self_link_helpers.go
@@ -92,6 +92,35 @@ func SelfLinkNameHash(selfLink interface{}) int {
 	return Hashcode(name)
 }
 
+// Hash based on relative url for a nested object containing a URL field.
+func NestedUrlSetHashFunc(v interface{}) int {
+	if v == nil {
+		return 0
+	}
+
+  var buf bytes.Buffer
+	m := v.(map[string]interface{})
+	log.Printf("[DEBUG] hashing %v", m)
+
+	if v, ok := m["url"]; ok {
+		if v == nil {
+      v = ""
+    } else {
+      if relUrl, err := GetRelativePath(v.(string)); err != nil {
+        log.Printf("[WARN] Error on retrieving relative path of network url: %s", err)
+      } else {
+        v = relUrl
+      }
+    }
+
+		buf.WriteString(fmt.Sprintf("%v-", v))
+	}
+
+	log.Printf("[DEBUG] computed hash value of %v from %v", Hashcode(buf.String()), buf.String())
+	return Hashcode(buf.String())
+}
+
+
 func ConvertSelfLinkToV1(link string) string {
 	reg := regexp.MustCompile("/compute/[a-zA-Z0-9]*/projects/")
 	return reg.ReplaceAllString(link, "/compute/v1/projects/")
