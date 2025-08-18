e0df7cfb60486d84bf9d6a647a79e7de9dcaffa0
Stéphane Charité <cstephane@google.com>
2025-08-18 15:49:50 -0700

    bumping resource timeout for insert to be 120 minutes

--- mmv1/third_party/terraform/services/lustre/resource_lustre_instance_test.go
+++ mmv1/third_party/terraform/services/lustre/resource_lustre_instance_test.go
@@ -93,9 +93,6 @@ resource "google_lustre_instance" "instance" {
   labels                      = {
     test = "test-label"
   }
-  timeouts {
-	create = "120m"
-  }
 }
 
 // This example assumes this network already exists.
