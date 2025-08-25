f1b5c7c8fa3aeadad8beb975412fd3e4b06dccde
Samantha Mathews <samathews@google.com>
2025-08-25 15:39:12 +0530

    Update resource_certificate_manager_issuance_config_test.go

--- mmv1/third_party/terraform/services/certificatemanager/resource_certificate_manager_issuance_config_test.go
+++ mmv1/third_party/terraform/services/certificatemanager/resource_certificate_manager_issuance_config_test.go
@@ -1,22 +1,31 @@
 package certificatemanager_test
 
 import (
+	"context"
+	"fmt"
 	"testing"
 
+	cmanager "cloud.google.com/go/certificatemanager/apiv1"
+	certificatemanagerpb "cloud.google.com/go/certificatemanager/apiv1/certificatemanagerpb"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 	"github.com/hashicorp/terraform-provider-google/google/envvar"
 )
 
 func TestAccCertificateManagerIssuanceConfig_tags(t *testing.T) {
 	t.Parallel()
+	tagKey := acctest.BootstrapSharedTestOrganizationTagKey(t, "issuance-config-tagkey", map[string]interface{}{})
+	tagValue := acctest.BootstrapSharedTestOrganizationTagValue(t, "issuance-config-tagvalue", tagKey)
 
-	tagKey := acctest.BootstrapSharedTestOrganizationTagKey(t, "certificate_manager_issuance_config-tagkey", map[string]interface{}{})
-	context := map[string]interface{}{
+	randomSuffix := acctest.RandString(t, 10)
+	resourceName := "google_certificate_manager_certificate_issuance_config.test"
+
+	testContext := map[string]interface{}{
 		"org":           envvar.GetTestOrgFromEnv(t),
 		"tagKey":        tagKey,
-		"tagValue":      acctest.BootstrapSharedTestOrganizationTagValue(t, "certificate_manager_issuance_config-tagvalue", tagKey),
-		"random_suffix": acctest.RandString(t, 10),
+		"tagValue":      tagValue,
+		"random_suffix": randomSuffix,
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
@@ -24,25 +33,24 @@ func TestAccCertificateManagerIssuanceConfig_tags(t *testing.T) {
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCertificateManagerIssuanceConfigTags(context),
-				Check: resource.TestCheckFunc(
-					resource.TestCheckResourceAttrSet(
-						"google_certificate_manager_certificate_issuance_config.issuanceconfig", "tags.%"),
+				Config: testAccCertificateManagerIssuanceConfigWithTags(testContext),
+				Check: resource.ComposeTestCheckFunc(
+					checkCertificateManagerIssuanceConfigWithTags(resourceName, testContext),
 				),
 			},
 			{
-				ResourceName:            "google_certificate_manager_certificate_issuance_config.issuanceconfig",
+				ResourceName:            resourceName,
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"labels", "terraform_labels", "tags"},
+				ImportStateVerifyIgnore: []string{"certificate_authority_config", "tags"},
 			},
 		},
 	})
 }
 
-func testAccCertificateManagerIssuanceConfigTags(context map[string]interface{}) string {
+func testAccCertificateManagerIssuanceConfigWithTags(testContext map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_certificate_manager_certificate_issuance_config" "issuanceconfig" {
+	resource "google_certificate_manager_certificate_issuance_config" "test" {
   name    = "tf-test-issuance-config%{random_suffix}"
   description = "sample description for the certificate issuanceConfigs"
   certificate_authority_config {
@@ -54,19 +62,16 @@ resource "google_certificate_manager_certificate_issuance_config" "issuanceconfi
   rotation_window_percentage = 34
   key_algorithm = "ECDSA_P256"
   labels = { "name": "wrench", "count": "3" }
-
   depends_on=[google_privateca_certificate_authority.ca_authority]
   tags = {
 	"%{org}/%{tagKey}" = "%{tagValue}"
   }
 }
-
 resource "google_privateca_ca_pool" "pool" {
   name     = "ca-pool%{random_suffix}"
   location = "us-central1"
   tier     = "ENTERPRISE"
 }
-
 resource "google_privateca_certificate_authority" "ca_authority" {
   location = "us-central1"
   pool = google_privateca_ca_pool.pool.name
@@ -99,11 +104,63 @@ resource "google_privateca_certificate_authority" "ca_authority" {
   key_spec {
     algorithm = "RSA_PKCS1_4096_SHA256"
   }
-
   // Disable CA deletion related safe checks for easier cleanup.
   deletion_protection                    = false
   skip_grace_period                      = true
   ignore_active_certificates_on_deletion = true
+}`, testContext)
 }
-`, context)
+
+// This function gets the issuance config via the Certificate Manager API and inspects its labels.
+func checkCertificateManagerIssuanceConfigWithTags(resourceName string, testContext map[string]interface{}) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("Resource not found: %s", resourceName)
+		}
+
+		// Get resource attributes from state
+		project := rs.Primary.Attributes["project"]
+		location := rs.Primary.Attributes["location"]
+		issuanceConfigName := rs.Primary.Attributes["name"]
+
+		ctx := context.Background()
+
+		// Use the corrected NewClient() to instantiate the client.
+		certificateManagerClient, err := cmanager.NewClient(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to create certificate manager client: %v", err)
+		}
+		defer certificateManagerClient.Close()
+
+		// Construct the request to get the issuance config details
+		req := &certificatemanagerpb.GetCertificateIssuanceConfigRequest{
+			Name: fmt.Sprintf("projects/%s/locations/%s/certificateIssuanceConfigs/%s", project, location, issuanceConfigName),
+		}
+
+		// Get the Certificate Manager issuance config
+		issuanceConfig, err := certificateManagerClient.GetCertificateIssuanceConfig(ctx, req)
+		if err != nil {
+			return fmt.Errorf("failed to get issuance config '%s': %v", req.Name, err)
+		}
+
+		// Check the issuance config's labels for the expected tag
+		expectedTagKey := fmt.Sprintf("%s/%s", testContext["org"], testContext["tagKey"])
+		expectedTagValue := fmt.Sprintf("%s", testContext["tagValue"])
+
+		labels := issuanceConfig.GetLabels()
+		if labels == nil {
+			return fmt.Errorf("expected labels not found on issuance config '%s'", req.Name)
+		}
+
+		if actualValue, ok := labels[expectedTagKey]; ok {
+			if actualValue == expectedTagValue {
+				// The tag was found with the correct value. Success!
+				return nil
+			}
+			return fmt.Errorf("tag key '%s' found with incorrect value. Expected: %s, Got: %s", expectedTagKey, expectedTagValue, actualValue)
+		}
+
+		return fmt.Errorf("expected tag key '%s' not found on issuance config '%s'", expectedTagKey, req.Name)
+	}
 }
