0ac6d9da29177c0c4c4ee7e897efff6489403bf6
Stephen Lewis <stephenrlewis@google.com>
2025-08-26 10:37:05 -0700

    Added missing import

--- mmv1/third_party/terraform/services/apigee/resource_apigee_security_action_test.go
+++ mmv1/third_party/terraform/services/apigee/resource_apigee_security_action_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
 	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
 )
