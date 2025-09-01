97150a5fb74a6e759daade91b6d527223433ef18
Laurenz Kirchmayr <laurenzk@google.com>
2025-09-01 14:59:29 +0000

    Reorder terraform resource block fields.
    Context: Tests compare strings 1:1. Cai2Hcl outputs fields in alphabetical order.

--- mmv1/third_party/cai2hcl/services/networksecurity/testdata/backend_authentication_config.tf
+++ mmv1/third_party/cai2hcl/services/networksecurity/testdata/backend_authentication_config.tf
@@ -1,38 +1,38 @@
 resource "google_network_security_backend_authentication_config" "laurenzk-test1" {
-  well_known_roots = "PUBLIC_ROOTS"
   name             = "laurenzk-test1"
+  well_known_roots = "PUBLIC_ROOTS"
 }
 
 resource "google_network_security_backend_authentication_config" "laurenzk-test2" {
+  name             = "laurenzk-test2"
   trust_config     = "projects/ccm-breakit/locations/global/trustConfigs/id-2de0d4b7-89cf-476f-893d-4567b3791ca9"
   well_known_roots = "PUBLIC_ROOTS"
-  name             = "laurenzk-test2"
 }
 
 resource "google_network_security_backend_authentication_config" "laurenzk-test3" {
   client_certificate = "projects/ccm-breakit/locations/global/certificates/anatolisaukhin-27101"
-  well_known_roots   = "PUBLIC_ROOTS"
   name               = "laurenzk-test3"
+  well_known_roots   = "PUBLIC_ROOTS"
 }
 
 resource "google_network_security_backend_authentication_config" "laurenzk-test4" {
   client_certificate = "projects/ccm-breakit/locations/global/certificates/anatolisaukhin-27101"
+  name               = "laurenzk-test4"
   trust_config       = "projects/ccm-breakit/locations/global/trustConfigs/id-2de0d4b7-89cf-476f-893d-4567b3791ca9"
   well_known_roots   = "PUBLIC_ROOTS"
-  name               = "laurenzk-test4"
 }
 
 resource "google_network_security_backend_authentication_config" "laurenzk-test5" {
+  name             = "laurenzk-test5"
   trust_config     = "projects/ccm-breakit/locations/global/trustConfigs/id-2de0d4b7-89cf-476f-893d-4567b3791ca9"
   well_known_roots = "NONE"
-  name             = "laurenzk-test5"
 }
 
 resource "google_network_security_backend_authentication_config" "laurenzk-test7" {
   client_certificate = "projects/ccm-breakit/locations/global/certificates/anatolisaukhin-27101"
+  name               = "laurenzk-test7"
   trust_config       = "projects/ccm-breakit/locations/global/trustConfigs/id-2de0d4b7-89cf-476f-893d-4567b3791ca9"
   well_known_roots   = "NONE"
-  name               = "laurenzk-test7"
 }
 
 resource "google_network_security_backend_authentication_config" "laurenzk-test8" {
@@ -42,18 +42,18 @@ resource "google_network_security_backend_authentication_config" "laurenzk-test8
     foo = "bar"
   }
 
-  well_known_roots = "PUBLIC_ROOTS"
   name             = "laurenzk-test8"
+  well_known_roots = "PUBLIC_ROOTS"
 }
 
 resource "google_network_security_backend_authentication_config" "laurenzk-test9" {
   location         = "europe-west1"
-  well_known_roots = "PUBLIC_ROOTS"
   name             = "laurenzk-test9"
+  well_known_roots = "PUBLIC_ROOTS"
 }
 
 resource "google_network_security_backend_authentication_config" "laurenzk-test10" {
+  name             = "laurenzk-test10"
   project          = "ccm-breakit"
   well_known_roots = "PUBLIC_ROOTS"
-  name             = "laurenzk-test10"
 }
