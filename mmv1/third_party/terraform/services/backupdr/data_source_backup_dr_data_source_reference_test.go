4b7e07820eba0552ac2baaf5eabd57d73d45a306
Kushal Lunkad <kushallunkad@google.com>
2025-09-16 05:35:20 +0000

    resolved some errors

--- mmv1/third_party/terraform/services/backupdr/data_source_backup_dr_data_source_reference_test.go
+++ mmv1/third_party/terraform/services/backupdr/data_source_backup_dr_data_source_reference_test.go
@@ -48,15 +48,20 @@ func testAccDataSourceGoogleBackupDRListDataSourceReferences_basic(context map[s
 data "google_project" "project" {
 }
 
+resource "google_service_account" "default" {
+ account_id   = "tf-test-my-custom-%{random_suffix}"
+ display_name = "Custom SA for VM Instance"
+}
+
 resource "google_sql_database_instance" "instance" {
   name                = "tf-test-ds-list-%{random_suffix}"
   database_version    = "MYSQL_8_0"
   region              = "%{location}"
   deletion_protection = false
-
   settings {
     tier              = "db-n1-standard-1"
     availability_type = "ZONAL"
+	activation_policy = "ALWAYS"
   }
 }
 
@@ -64,7 +69,18 @@ resource "google_backup_dr_backup_vault" "vault" {
   location          = "%{location}"
   backup_vault_id   = "tf-test-bv-list-%{random_suffix}"
   description       = "Acceptance test vault"
-  force_delete      = true
+  backup_minimum_enforced_retention_duration = "100000s"
+  labels = {
+	foo = "bar1"
+	bar = "baz1"
+  }
+  annotations = {
+	annotations1 = "bar1"
+	annotations2 = "baz1"
+  }
+  force_update      = "true"
+  force_delete      = "true"
+  allow_missing     = "true"
 }
 
 resource "google_backup_dr_backup_plan" "plan" {
@@ -72,18 +88,23 @@ resource "google_backup_dr_backup_plan" "plan" {
   backup_plan_id  = "tf-test-bp-list-%{random_suffix}"
   resource_type   = "sqladmin.googleapis.com/Instance"
   backup_vault    = google_backup_dr_backup_vault.vault.name
-  backup_rules {
-    rule_id               = "daily-backups"
-    backup_retention_days = 1
-    standard_schedule {
-      recurrence_type = "DAILY"
-			backup_window {
-				start_hour_of_day = 0
-				end_hour_of_day = 6
-			}
-			time_zone = "UTC"
-    }
-  }
+   backup_rules {
+   rule_id                = "rule-1"
+   backup_retention_days  = 2
+
+
+   standard_schedule {
+     recurrence_type     = "HOURLY"
+     hourly_frequency    = 6
+     time_zone           = "UTC"
+
+
+     backup_window {
+       start_hour_of_day = 12
+       end_hour_of_day   = 18
+     }
+   }
+ }
 }
 
 resource "google_backup_dr_backup_plan_association" "bpa" {
@@ -91,6 +112,7 @@ resource "google_backup_dr_backup_plan_association" "bpa" {
   backup_plan_association_id = "tf-test-bpa-list-%{random_suffix}"
   resource                   = google_sql_database_instance.instance.self_link
   backup_plan                = google_backup_dr_backup_plan.plan.name
+  depends_on                 = [google_sql_database_instance.instance]
 }
 
 data "google_backup_dr_list_data_source_references" "all" {
