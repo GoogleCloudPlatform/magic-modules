0403ff97a698efbba8729cc20895981312bac03b
Max W. Portocarrero <mportocarrero@ciandt.com>
2025-09-18 17:42:19 -0500

    added ipblocks

--- mmv1/third_party/terraform/services/networksecurity/resource_network_services_authz_policy_test.go
+++ mmv1/third_party/terraform/services/networksecurity/resource_network_services_authz_policy_test.go
@@ -163,6 +163,16 @@ resource "google_network_security_authz_policy" "default" {
   http_rules {
     from {
       not_sources {
+        ip_blocks {
+          length = 24
+          prefix = "10.1.5.0"
+        }
+
+        ip_blocks {
+          length = 24
+          prefix = "10.1.6.0"
+        }
+
         # Prefix
         principals {
           principal_selector = "CLIENT_CERT_URI_SAN"
@@ -241,6 +251,16 @@ resource "google_network_security_authz_policy" "default" {
       }
 
       sources {
+        ip_blocks {
+          length = 24
+          prefix = "10.10.5.0"
+        }
+
+        ip_blocks {
+          length = 24
+          prefix = "10.10.6.0"
+        }
+
         # Prefix
         principals {
           principal_selector = "CLIENT_CERT_URI_SAN"
