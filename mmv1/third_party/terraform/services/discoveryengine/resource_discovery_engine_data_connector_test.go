5e087f9617558264c10fdb014e9e8d9c3249dc77
Jialei Chen <jialeichen@google.com>
2025-09-22 20:43:59 +0000

    Change to use resource intead of request as HTTP body for create.

--- mmv1/third_party/terraform/services/discoveryengine/resource_discovery_engine_data_connector_test.go
+++ mmv1/third_party/terraform/services/discoveryengine/resource_discovery_engine_data_connector_test.go
@@ -27,7 +27,7 @@ func TestAccDiscoveryEngineDataConnector_discoveryengineDataconnectorJiraBasicEx
 				ResourceName:            "google_discovery_engine_data_connector.jira-basic",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"collection_display_name", "collection_id", "data_connector", "location"},
+				ImportStateVerifyIgnore: []string{"collection_display_name", "collection_id", "location", "params"},
 			},
 			{
 				Config: testAccDiscoveryEngineDataConnector_discoveryengineDataconnectorJiraBasicExample_update(context),
@@ -36,7 +36,7 @@ func TestAccDiscoveryEngineDataConnector_discoveryengineDataconnectorJiraBasicEx
 				ResourceName:            "google_discovery_engine_data_connector.jira-basic",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"collection_display_name", "collection_id", "data_connector", "location"},
+				ImportStateVerifyIgnore: []string{"collection_display_name", "collection_id", "location", "params"},
 			},
 		},
 	})
@@ -48,35 +48,31 @@ resource "google_discovery_engine_data_connector" "jira-basic" {
   location                  = "global"
   collection_id             = "tf-test-collection-id%{random_suffix}"
   collection_display_name   = "tf-test-dataconnector-jira"
-  data_connector {
-    data_source             = "jira"
-    params = {
-        instance_id         = "33db20a3-dc45-4305-a505-d70b68599840"
-        instance_uri        = "https://vaissptbots1.atlassian.net/"
-        client_secret       = "%{client_secret}"
-        client_id           = "%{client_id}"
-        refresh_token       = "fill-in-the-blank"
-    }
-    refresh_interval {
-        seconds             = 86400
-    }
-    entities {
-        entity_name         = "project"
-    }
-    entities {
-        entity_name         = "issue"
-    }
-    entities {
-        entity_name         = "attachment"
-    }
-    entities {
-        entity_name         = "comment"
-    }
-    entities {
-        entity_name         = "worklog"
-    }
-    static_ip_enabled       = true
+  data_source               = "jira"
+  params = {
+      instance_id           = "33db20a3-dc45-4305-a505-d70b68599840"
+      instance_uri          = "https://vaissptbots1.atlassian.net/"
+      client_secret         = "%{client_secret}"
+      client_id             = "%{client_id}"
+      refresh_token         = "fill-in-the-blank"
   }
+  refresh_interval          = "86400s"
+  entities {
+      entity_name           = "project"
+  }
+  entities {
+      entity_name           = "issue"
+  }
+  entities {
+      entity_name           = "attachment"
+  }
+  entities {
+      entity_name           = "comment"
+  }
+  entities {
+      entity_name           = "worklog"
+  }
+  static_ip_enabled         = true
 }
 `, context)
 }
@@ -86,36 +82,33 @@ func testAccDiscoveryEngineDataConnector_discoveryengineDataconnectorJiraBasicEx
 resource "google_discovery_engine_data_connector" "jira-basic" {
   location                  = "global"
   collection_id             = "tf-test-collection-id%{random_suffix}"
-  data_connector {
-    data_source             = "jira"
-    params = {
-        instance_id         = "33db20a3-dc45-4305-a505-d70b68599840"
-        instance_uri        = "https://vaissptbots1.atlassian.net/"
-        client_secret       = "%{client_secret}"
-        client_id           = "%{client_id}"
-        refresh_token       = "fill-in-the-blank"
-    }
-    refresh_interval {
-        seconds             = 86400
-    }
-    entities {
-        entity_name         = "project"
-    }
-    entities {
-        entity_name         = "issue"
-    }
-    entities {
-        entity_name         = "attachment"
-    }
-    entities {
-        entity_name         = "comment"
-    }
-    entities {
-        entity_name         = "worklog"
-    }
-    static_ip_enabled       = true
-    kms_key_name            = "%{kms_key_name}"
+  collection_display_name   = "tf-test-dataconnector-jira"
+  data_source               = "jira"
+  params = {
+      instance_id           = "33db20a3-dc45-4305-a505-d70b68599840"
+      instance_uri          = "https://vaissptbots1.atlassian.net/"
+      client_secret         = "%{client_secret}"
+      client_id             = "%{client_id}"
+      refresh_token         = "fill-in-the-blank"
   }
+  refresh_interval          = "86400s"
+  entities {
+      entity_name           = "project"
+  }
+  entities {
+      entity_name           = "issue"
+  }
+  entities {
+      entity_name           = "attachment"
+  }
+  entities {
+      entity_name           = "comment"
+  }
+  entities {
+      entity_name           = "worklog"
+  }
+  static_ip_enabled         = true
+  kms_key_name              = "%{kms_key_name}"
 }
 `, context)
 }
