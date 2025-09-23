af5788469f5df67b357ce6e55bc3341145b063df
Jialei Chen <jialeichen@google.com>
2025-09-23 23:45:22 +0000

    Fix the update test.

--- mmv1/third_party/terraform/services/discoveryengine/resource_discovery_engine_data_connector_test.go
+++ mmv1/third_party/terraform/services/discoveryengine/resource_discovery_engine_data_connector_test.go
@@ -13,7 +13,6 @@ func TestAccDiscoveryEngineDataConnector_discoveryengineDataconnectorJiraBasicEx
     t.Parallel()
 
 	context := map[string]interface{}{
-		"kms_key_name":  acctest.BootstrapKMSKeyWithPurposeInLocationAndName(t, "ENCRYPT_DECRYPT", "us", "tftest-shared-key-dataconnector-0").CryptoKey.Name,
 		"client_id":     "tf-test-client-id",
 		"client_secret": "tf-test-client-secret",
 		"random_suffix": acctest.RandString(t, 10),
@@ -50,12 +49,8 @@ func TestAccDiscoveryEngineDataConnector_discoveryengineDataconnectorJiraBasicEx
 
 func testAccDiscoveryEngineDataConnector_discoveryengineDataconnectorJiraBasicExample_basic(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "time_sleep" "wait_1_hour" {
-  create_duration = "3600s"
-}
 
 resource "google_discovery_engine_data_connector" "jira-basic" {
-  depends_on                = [time_sleep.wait_1_hour]
   location                  = "global"
   collection_id             = "tf-test-collection-id%{random_suffix}"
   collection_display_name   = "tf-test-dataconnector-jira"
@@ -90,17 +85,17 @@ resource "google_discovery_engine_data_connector" "jira-basic" {
 
 func testAccDiscoveryEngineDataConnector_discoveryengineDataconnectorJiraBasicExample_update(context map[string]interface{}) string {
 	return acctest.Nprintf(`
+resource "time_sleep" "wait_1_hour" {
+  create_duration = "3s"
+}
+
 resource "google_discovery_engine_data_connector" "jira-basic" {
+  depends_on                = [time_sleep.wait_1_hour]
   location                  = "global"
   collection_id             = "tf-test-collection-id%{random_suffix}"
   collection_display_name   = "tf-test-dataconnector-jira"
   data_source               = "jira"
   params = {
-      instance_id           = "33db20a3-dc45-4305-a505-d70b68599840"
-      instance_uri          = "https://vaissptbots1.atlassian.net/"
-      client_secret         = "%{client_secret}"
-      client_id             = "%{client_id}"
-      refresh_token         = "fill-in-the-blank"
       max_qps               = "100"
   }
   refresh_interval          = "172800s"
@@ -120,7 +115,6 @@ resource "google_discovery_engine_data_connector" "jira-basic" {
       entity_name           = "worklog"
   }
   static_ip_enabled         = true
-  kms_key_name              = "%{kms_key_name}"
 }
 `, context)
 }
