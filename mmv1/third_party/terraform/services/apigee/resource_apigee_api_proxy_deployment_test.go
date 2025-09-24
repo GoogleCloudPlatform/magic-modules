3c1c80616e99f2e49312491593f0c151c6a52e1b
malhotrasagar2212 <sagar.malhotra@hashicorp.com>
2025-09-24 16:26:26 -0400

    removed trailing space

--- mmv1/third_party/terraform/services/apigee/resource_apigee_api_proxy_deployment_test.go
+++ mmv1/third_party/terraform/services/apigee/resource_apigee_api_proxy_deployment_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func TestAccApigeeApiProxyDeployment_apigeeApiProxyDeploymentBasicExample(t *testing.T) {
-	acctest.SkipIfVcr(t)
+	// acctest.SkipIfVcr(t)
 	t.Parallel()
 	context := map[string]interface{}{
 		"org_id":          envvar.GetTestOrgFromEnv(t),
