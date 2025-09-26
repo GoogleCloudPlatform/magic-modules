b970883d927bfa34c040e053d708d6bbcbd67ab4
Chris Hawk <hawk@google.com>
2025-09-25 19:29:06 -0700

    POC for removing goimports build step

--- mmv1/provider/template_data.go
+++ mmv1/provider/template_data.go
@@ -318,7 +318,7 @@ func FixImports(outputPath string, dumpDiffs bool) {
 		})
 
 		if hasFiles {
-			cmd := exec.Command("goimports", args...)
+			cmd := exec.Command("echo", args...)
 			cmd.Dir = outputPath
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
