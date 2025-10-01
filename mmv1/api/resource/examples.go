4cf9aea64dd33ec563cf0cab519fd681e1a61e6a
Thomas Rodgers <thomasrodgers@google.com>
2025-10-01 15:14:56 -0700

    Update mmv1/api/resource/examples.go
    Co-authored-by: Zhenhua Li <zhenhuali@google.com>

--- mmv1/api/resource/examples.go
+++ mmv1/api/resource/examples.go
@@ -177,7 +177,7 @@ type Examples struct {
 	// TGC
 	// ====================
 	TGCSkipTest string `yaml:"tgc_skip_test,omitempty"`
-	// When a test is generated from a handwritten provder test, its name will be stored here
+	// When a test is generated from a handwritten provider test, its name will be stored here
 	TGCHandwrittenTestName string `yaml:"-"`
 }
 
