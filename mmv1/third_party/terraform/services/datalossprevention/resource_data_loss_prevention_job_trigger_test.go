fcb80f9007d168873d4fe618faf76cb667b89e38
Jessica Wise <jessicawise@google.com>
2025-10-08 22:12:53 +0000

    fixed unit test

--- mmv1/third_party/terraform/services/datalossprevention/resource_data_loss_prevention_job_trigger_test.go
+++ mmv1/third_party/terraform/services/datalossprevention/resource_data_loss_prevention_job_trigger_test.go
@@ -139,6 +139,31 @@ func TestAccDataLossPreventionJobTrigger_dlpJobTriggerPubsub(t *testing.T) {
 	})
 }
 
+func TestAccDataLossPreventionJobTrigger_dlpJobTriggerDataplexCatalog(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"project": envvar.GetTestProjectFromEnv(),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckDataLossPreventionJobTriggerDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataLossPreventionJobTrigger_publishFindingsToDataplexCatalog(context),
+			},
+			{
+				ResourceName:            "google_data_loss_prevention_job_trigger.actions",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"parent"},
+			},
+		},
+	})
+}
+
 func TestAccDataLossPreventionJobTrigger_dlpJobTriggerDeidentifyUpdate(t *testing.T) {
 	t.Parallel()
 
@@ -938,9 +963,16 @@ resource "google_data_loss_prevention_job_trigger" "actions" {
 			publish_findings_to_dataplex_catalog {}
 		}
 		storage_config {
-			cloud_storage_options {
-				file_set {
-					url = "gs://mybucket/directory/"
+			big_query_options {
+				table_reference {
+					project_id = "project"
+					dataset_id = "dataset"
+					table_id = "table_to_scan"
+				}
+				rows_limit = 1000
+				sample_method = "RANDOM_START"
+				identifying_fields {
+					name = "field"
 				}
 			}
 		}
