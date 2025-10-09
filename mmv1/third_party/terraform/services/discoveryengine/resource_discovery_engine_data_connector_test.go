f20d32181ee27c362db84b20b0c75d477f8f40ab
Kevin Fang <kefang@google.com>
2025-10-09 21:24:47 +0000

    change entity.params to a json to support struct

--- mmv1/third_party/terraform/services/discoveryengine/resource_discovery_engine_data_connector_test.go
+++ mmv1/third_party/terraform/services/discoveryengine/resource_discovery_engine_data_connector_test.go
@@ -66,12 +66,33 @@ resource "google_discovery_engine_data_connector" "servicenow-basic" {
   refresh_interval             = "86400s"
   entities {
     entity_name                = "catalog"
+    params                     = jsonencode({
+      "inclusion_filters": {
+        "knowledgeBaseSysId": [
+          "123"
+        ]
+      }
+    })
   }
   entities {
     entity_name                = "incident"
+    params                     = jsonencode({
+      "inclusion_filters": {
+        "knowledgeBaseSysId": [
+          "123"
+        ]
+      }
+    })
   }
   entities {
     entity_name                = "knowledge_base"
+    params                     = jsonencode({
+      "inclusion_filters": {
+        "knowledgeBaseSysId": [
+          "123"
+        ]
+      }
+    })
   }
   static_ip_enabled            = false
   incremental_refresh_interval = "21600s"
@@ -99,12 +120,33 @@ resource "google_discovery_engine_data_connector" "servicenow-basic" {
   refresh_interval             = "172800s"
   entities {
     entity_name                = "catalog"
+    params                     = jsonencode({
+      "inclusion_filters": {
+        "knowledgeBaseSysId": [
+          "123"
+        ]
+      }
+    })
   }
   entities {
     entity_name                = "incident"
+    params                     = jsonencode({
+      "inclusion_filters": {
+        "knowledgeBaseSysId": [
+          "123"
+        ]
+      }
+    })
   }
   entities {
     entity_name                = "knowledge_base"
+    params                     = jsonencode({
+      "inclusion_filters": {
+        "knowledgeBaseSysId": [
+          "123"
+        ]
+      }
+    })
   }
   static_ip_enabled            = false
   incremental_refresh_interval = "21600s"
