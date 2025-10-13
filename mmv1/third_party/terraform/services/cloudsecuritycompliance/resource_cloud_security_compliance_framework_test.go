c17c9802329ebd55b78ddba34c7edafa5786cc74
Thoufic <39660808+thoufic67@users.noreply.github.com>
2025-10-13 16:59:37 +0530

    Update Cloud Security Compliance Framework tests and templates with new organization ID and enhanced configurations

--- mmv1/third_party/terraform/services/cloudsecuritycompliance/resource_cloud_security_compliance_framework_test.go
+++ mmv1/third_party/terraform/services/cloudsecuritycompliance/resource_cloud_security_compliance_framework_test.go
@@ -31,15 +31,80 @@ func TestAccCloudSecurityComplianceFramework_basic(t *testing.T) {
 	})
 }
 
+func TestAccCloudSecurityComplianceFramework_update(t *testing.T) {
+	t.Parallel()
+
+	suffix := acctest.RandString(t, 10)
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudSecurityComplianceFrameworkConfig(t, suffix),
+			},
+			{
+				ResourceName:      "google_cloud_security_compliance_framework.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccCloudSecurityComplianceFrameworkUpdate(t, suffix),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("google_cloud_security_compliance_framework.test", "display_name", "Updated TF Test Framework"),
+					resource.TestCheckResourceAttr("google_cloud_security_compliance_framework.test", "description", "An updated test framework description for cloud security compliance created from terraform"),
+					resource.TestCheckResourceAttr("google_cloud_security_compliance_framework.test", "category.#", "2"),
+					resource.TestCheckResourceAttr("google_cloud_security_compliance_framework.test", "cloud_control_details.#", "2"),
+				),
+			},
+			{
+				ResourceName:      "google_cloud_security_compliance_framework.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func testAccCloudSecurityComplianceFrameworkConfig(t *testing.T, suffix string) string {
 	return fmt.Sprintf(`
+provider "google" {
+  billing_project = "terraform-test-472610"
+  user_project_override = true
+}
+
 resource "google_cloud_security_compliance_framework" "test" {
-	organization   = "123456789"
+	organization   = "1035865795181"
+	name 		   = "organizations/1035865795181/locations/global/frameworks/tf-test-%s"
 	location       = "global"
 	framework_id   = "tf-test-%s"
-	display_name   = "Test Framework"
-	description    = "A test framework for cloud security compliance"
+	display_name   = "Created TF Test Framework"
+	description    = "A test framework for cloud security compliance created from terraform"
+	category       = ["CUSTOM_FRAMEWORK"]
+	
+	cloud_control_details {
+		name              = "organizations/1035865795181/locations/global/cloudControls/builtin-assess-resource-availability"
+		major_revision_id = "1"
+	}
+}
+`, suffix, suffix)
+}
+
+func testAccCloudSecurityComplianceFrameworkUpdate(t *testing.T, suffix string) string {
+	return fmt.Sprintf(`
+provider "google" {
+  billing_project = "terraform-test-472610"
+  user_project_override = true
+}
+
+resource "google_cloud_security_compliance_framework" "test" {
+	organization   = "1035865795181"
+	name 		   = "organizations/1035865795181/locations/global/frameworks/tf-test-%s"
+	location       = "global"
+	framework_id   = "tf-test-%s"
+	display_name   = "Updated TF Test Framework"
+	description    = "An updated test framework description for cloud security compliance created from terraform"
 	category       = ["CUSTOM_FRAMEWORK"]
 }
-`, suffix)
+`, suffix, suffix)
 }
