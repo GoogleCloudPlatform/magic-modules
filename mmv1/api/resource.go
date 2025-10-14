37fc63902c6c3c16aa62e7927b5e44027530e3ed
alvarez.mauriciotm@gmail.com <alvarez.mauriciotm@gmail.com>
2025-10-14 13:06:39 -0700

    update to refer write-only fields as arguments

--- mmv1/api/resource.go
+++ mmv1/api/resource.go
@@ -864,7 +864,7 @@ func buildWriteOnlyField(name string, versionFieldName string, originalField *Ty
 }
 
 func buildWriteOnlyVersionField(name string, originalField *Type, writeOnlyField *Type) *Type {
-	description := fmt.Sprintf("Triggers update of %s write-only. For more info see [updating write-only attributes](/docs/providers/google/guides/using_write_only_attributes.html#updating-write-only-attributes)", google.Underscore(writeOnlyField.Name))
+	description := fmt.Sprintf("Triggers update of %s write-only. For more info see [updating write-only arguments](/docs/providers/google/guides/using_write_only_arguments.html#updating-write-only-arguments)", google.Underscore(writeOnlyField.Name))
 	requiredWith := strings.ReplaceAll(originalField.TerraformLineage(), google.Underscore(originalField.Name), google.Underscore(writeOnlyField.Name))
 
 	options := []func(*Type){
