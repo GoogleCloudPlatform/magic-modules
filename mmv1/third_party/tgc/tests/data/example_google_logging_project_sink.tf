ac124b1df4c174190f20c3a9404c1ef26c4c19de
Hemant Adil <hemantadil@google.com>
2025-10-14 23:46:48 +0530

    fixing test data

--- mmv1/third_party/tgc/tests/data/example_google_logging_project_sink.tf
+++ mmv1/third_party/tgc/tests/data/example_google_logging_project_sink.tf
@@ -1,7 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google-beta"
+      version = "~> {{.Provider.version}}"
+    }
+  }
+}
+
+provider "google" {
+  {{if .Provider.credentials }}credentials = "{{.Provider.credentials}}"{{end}}
+}
+
 resource "google_logging_project_sink" "my-sink" {
   name   = "my-sink"
-  project = "my-project"
-  destination = "bigquery.googleapis.com/projects/my-project/datasets/my_dataset"
+  project = "{{.Provider.project}}"
+  destination = "bigquery.googleapis.com/projects/{{.Provider.project}}/datasets/my_dataset"
   filter = "severity >= ERROR"
   description = "A sink for errors"
   disabled = false
