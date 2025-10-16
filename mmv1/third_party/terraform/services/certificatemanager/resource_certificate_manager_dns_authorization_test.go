2defbf7801f2cb676779fcff154389f772e8d9a7
Samantha Mathews <samathews@google.com>
2025-10-16 16:02:09 +0530

    Update resource_certificate_manager_dns_authorization_test.go

--- mmv1/third_party/terraform/services/certificatemanager/resource_certificate_manager_dns_authorization_test.go
+++ mmv1/third_party/terraform/services/certificatemanager/resource_certificate_manager_dns_authorization_test.go
@@ -166,9 +166,9 @@ func checkCertificateManagerDnsAuthorizationWithTags(t *testing.T) func(s *terra
 			}
 			project := parts[1]
 			location := parts[3]
-			instance_id := parts[5]
+			dnsAuthorizations_id := parts[5]
 
-			parentURL := fmt.Sprintf("//certificatemanager.googleapis.com/projects/%s/locations/%s/dnsAuthorizations/%s", project, location, instance_id)
+			parentURL := fmt.Sprintf("//certificatemanager.googleapis.com/projects/%s/locations/%s/dnsAuthorizations/%s", project, location, dnsAuthorizations_id)
 			listBindingsURL := fmt.Sprintf("https://%s-cloudresourcemanager.googleapis.com/v3/tagBindings?parent=%s", location, url.QueryEscape(parentURL))
 
 			resp, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
