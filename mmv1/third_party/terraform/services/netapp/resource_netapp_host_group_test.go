11fc3a575d5301578aeef7797b7899000cbd9700
FirstName LastName <goyalmehul@google.com>
2025-10-16 17:17:01 +0000

    fix host group test

--- mmv1/third_party/terraform/services/netapp/resource_netapp_host_group_test.go
+++ mmv1/third_party/terraform/services/netapp/resource_netapp_host_group_test.go
@@ -21,7 +21,6 @@ func TestAccNetappHostGroup_HostGroupCreateExample_Update(t *testing.T) {
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckNetappHostGroupDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccNetappHostGroup_HostGroupCreateExample_Full(context),
