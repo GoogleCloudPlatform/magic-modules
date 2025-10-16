e2789c66027f3c442131a0c4bd3782048301db2e
Samantha Mathews <samathews@google.com>
2025-10-16 12:10:25 +0530

    Update resource_gke_backup_restore_plan_test.go

--- mmv1/third_party/terraform/services/gkebackup/resource_gke_backup_restore_plan_test.go
+++ mmv1/third_party/terraform/services/gkebackup/resource_gke_backup_restore_plan_test.go
@@ -1,11 +1,16 @@
 package gkebackup_test
 
 import (
+	"fmt"
 	"testing"
+	"net/url"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 	"github.com/hashicorp/terraform-provider-google/google/envvar"
+	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
 )
 
 func TestAccGKEBackupRestorePlan_update(t *testing.T) {
@@ -203,30 +208,29 @@ resource "google_gke_backup_restore_plan" "restore_plan" {
 `, context)
 }
 
-func TestAccGKEBackupRestorePlan_tags(t *testing.T) {
+func TestAccGkeBackupRestorePlan_tags(t *testing.T) {
 	t.Parallel()
-
-	tagKey := acctest.BootstrapSharedTestOrganizationTagKey(t, "gke_backup_restore_plan_tagkey", map[string]interface{}{})
+	tagKey := acctest.BootstrapSharedTestOrganizationTagKey(t, "gkebackup-rp-tagkey", map[string]interface{}{})
 	context := map[string]interface{}{
-		"project":             envvar.GetTestProjectFromEnv(),
-		"deletion_protection": false,
-		"network_name":        acctest.BootstrapSharedTestNetwork(t, "gke-cluster"),
-		"subnetwork_name":     acctest.BootstrapSubnet(t, "gke-cluster", acctest.BootstrapSharedTestNetwork(t, "gke-cluster")),
-		"random_suffix":       acctest.RandString(t, 10),
-		"org":                 envvar.GetTestOrgFromEnv(t),
-		"tagKey":              tagKey,
-		"tagValue":            acctest.BootstrapSharedTestOrganizationTagValue(t, "gke_backup_restore_plan_tagvalue", tagKey),
+		"random_suffix": acctest.RandString(t, 10),
+		"org":           envvar.GetTestOrgFromEnv(t),
+		"tagKey":        tagKey,
+		"tagValue":      acctest.BootstrapSharedTestOrganizationTagValue(t, "gkebackup-rp-tagvalue", tagKey),
+		"cluster_name":  "tf-test-cluster-" + acctest.RandString(t, 10),
+		"location": "us-east1",
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		PreCheck: func() {
+			acctest.AccTestPreCheck(t)
+		},
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccGKEBackupRestorePlanTags(context),
-				Check: resource.TestCheckFunc(
-					resource.TestCheckResourceAttrSet(
-						"google_gke_backup_restore_plan.restore_plan", "tags.%"),
+				Config: testAccGkeBackupRestorePlanTags(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("google_gke_backup_restore_plan.restore_plan", "tags.%"),
+					testAccCheckGkeBackupRestorePlanHasTagBindings(t),
 				),
 			},
 			{
@@ -239,29 +243,125 @@ func TestAccGKEBackupRestorePlan_tags(t *testing.T) {
 	})
 }
 
-func testAccGKEBackupRestorePlanTags(context map[string]interface{}) string {
+func testAccCheckGkeBackupRestorePlanHasTagBindings(t *testing.T) func(s *terraform.State) error {
+	return func(s *terraform.State) error {
+		for name, rs := range s.RootModule().Resources {
+			if rs.Type != "google_gke_backup_restore_plan" {
+				continue
+			}
+			if strings.HasPrefix(name, "data.") {
+				continue
+			}
+
+			config := acctest.GoogleProviderConfig(t)
+
+			var configuredTagValueNamespacedName string
+			var tagKeyNamespacedName, tagValueShortName string
+			for key, val := range rs.Primary.Attributes {
+				if strings.HasPrefix(key, "tags.") && key != "tags.%" {
+					tagKeyNamespacedName = strings.TrimPrefix(key, "tags.")
+					tagValueShortName = val
+					if tagValueShortName != "" {
+						configuredTagValueNamespacedName = fmt.Sprintf("%s/%s", tagKeyNamespacedName, tagValueShortName)
+						break
+					}
+				}
+			}
+
+			if configuredTagValueNamespacedName == "" {
+				return fmt.Errorf("could not find a configured tag value in the state for resource %s", rs.Primary.ID)
+			}
+
+			if strings.Contains(configuredTagValueNamespacedName, "%{") {
+				return fmt.Errorf("tag namespaced name contains unsubstituted variables: %q. Ensure the context map in the test step is populated", configuredTagValueNamespacedName)
+			}
+
+			safeNamespacedName := url.QueryEscape(configuredTagValueNamespacedName)
+			describeTagValueURL := fmt.Sprintf("https://cloudresourcemanager.googleapis.com/v3/tagValues/namespaced?name=%s", safeNamespacedName)
+
+			respDescribe, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
+				Config:    config,
+				Method:    "GET",
+				RawURL:    describeTagValueURL,
+				UserAgent: config.UserAgent,
+			})
+
+			if err != nil {
+				return fmt.Errorf("error describing tag value using namespaced name %q: %v", configuredTagValueNamespacedName, err)
+			}
+
+			fullTagValueName, ok := respDescribe["name"].(string)
+			if !ok || fullTagValueName == "" {
+				return fmt.Errorf("tag value details (name) not found in response for namespaced name: %q, response: %v", configuredTagValueNamespacedName, respDescribe)
+			}
+
+			parts := strings.Split(rs.Primary.ID, "/")
+			if len(parts) != 6 {
+				return fmt.Errorf("invalid resource ID format for GKE Backup Plan: %s", rs.Primary.ID)
+			}
+			location := parts[3]
+
+			parentURL := fmt.Sprintf("//gkebackup.googleapis.com/%s", rs.Primary.ID)
+			listBindingsURL := fmt.Sprintf("https://%s-cloudresourcemanager.googleapis.com/v3/tagBindings?parent=%s", location, url.QueryEscape(parentURL))
+
+			resp, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
+				Config:    config,
+				Method:    "GET",
+				RawURL:    listBindingsURL,
+				UserAgent: config.UserAgent,
+			})
+
+			if err != nil {
+				return fmt.Errorf("error calling TagBindings API for %s: %v", parentURL, err)
+			}
+
+			tagBindingsVal, exists := resp["tagBindings"]
+			if !exists {
+				tagBindingsVal = []interface{}{}
+			}
+
+			tagBindings, ok := tagBindingsVal.([]interface{})
+			if !ok {
+				return fmt.Errorf("'tagBindings' is not a slice in response for resource %s. Response: %v", rs.Primary.ID, resp)
+			}
+
+			foundMatch := false
+			for _, binding := range tagBindings {
+				bindingMap, ok := binding.(map[string]interface{})
+				if !ok {
+					continue
+				}
+				if bindingMap["tagValue"] == fullTagValueName {
+					foundMatch = true
+					break
+				}
+			}
+
+			if !foundMatch {
+				return fmt.Errorf("expected tag value %s (from namespaced %q) not found in tag bindings for resource %s. Bindings: %v", fullTagValueName, configuredTagValueNamespacedName, rs.Primary.ID, tagBindings)
+			}
+
+			t.Logf("Successfully found matching tag binding for %s with tagValue %s", rs.Primary.ID, fullTagValueName)
+		}
+
+		return nil
+	}
+}
+
+func testAccGkeBackupRestorePlanTags(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 resource "google_container_cluster" "primary" {
-  name               = "tf-test-restore-plan%{random_suffix}-cluster"
-  location           = "us-central1"
+  name               = "tf-test-gke-backup-cluster-%{random_suffix}"
+  location           = "us-east1"
   initial_node_count = 1
-  workload_identity_config {
-    workload_pool = "%{project}.svc.id.goog"
-  }
-  addons_config {
-    gke_backup_agent_config {
-      enabled = true
-    }
-  }
-  deletion_protection  = "%{deletion_protection}"
-  network       = "%{network_name}"
-  subnetwork    = "%{subnetwork_name}"
+  deletion_protection = false
+  network            = "samathews-test"
 }
 
 resource "google_gke_backup_backup_plan" "basic" {
   name = "tf-test-restore-plan%{random_suffix}"
   cluster = google_container_cluster.primary.id
-  location = "us-central1"
+  location = "us-east1"
   backup_config {
     include_volume_data = true
     include_secrets = true
@@ -271,7 +371,7 @@ resource "google_gke_backup_backup_plan" "basic" {
 
 resource "google_gke_backup_restore_plan" "restore_plan" {
   name = "tf-test-restore-plan%{random_suffix}"
-  location = "us-central1"
+  location = "us-east1"
   backup_plan = google_gke_backup_backup_plan.basic.id
   cluster = google_container_cluster.primary.id
   tags = {
