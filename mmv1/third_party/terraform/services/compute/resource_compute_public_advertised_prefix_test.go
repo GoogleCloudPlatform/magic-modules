73d3d14e19f45a88ccbf5907afe1c34511de5b50
Luca Prete <preteluca@gmail.com>
2025-10-27 07:33:17 +0100

    delegatee: get project_id instead of projects/project_id

--- mmv1/third_party/terraform/services/compute/resource_compute_public_advertised_prefix_test.go
+++ mmv1/third_party/terraform/services/compute/resource_compute_public_advertised_prefix_test.go
@@ -265,7 +265,7 @@ resource "google_compute_public_delegated_prefix" "prefix" {
     name              = "tf-test-sub-1%{random_suffix}"
     description       = "My delegated sub-prefix 1"
     ip_cidr_range     = "127.127.0.0/25"
-    delegatee_project = data.google_project.project.project_id
+    delegatee_project = split("/", data.google_project.project.id)[1]
     is_address        = true
   }
 
@@ -273,7 +273,7 @@ resource "google_compute_public_delegated_prefix" "prefix" {
     name              = "tf-test-sub-2%{random_suffix}"
     description       = "My delegated sub-prefix 2"
     ip_cidr_range     = "127.127.0.128/25"
-    delegatee_project = data.google_project.project.project_id
+    delegatee_project = split("/", data.google_project.project.id)[1]
   }
 }
 //
@@ -376,7 +376,7 @@ resource "google_compute_public_delegated_prefix" "prefix" {
     name              				= "tf-test-sub-pdp-1%{random_suffix}"
     description       				= "My delegated IPv6 sub-prefix 1"
     ip_cidr_range     				= "2001:db8:1::/48"
-    delegatee_project 				= data.google_project.project.project_id
+    delegatee_project 				= split("/", data.google_project.project.id)[1]
     is_address        				= true
     mode              				= "EXTERNAL_IPV6_FORWARDING_RULE_CREATION"
     allocatable_prefix_length = 49
