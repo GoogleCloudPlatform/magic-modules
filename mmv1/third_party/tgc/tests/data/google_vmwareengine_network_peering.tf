557503e9b90e1d3b07ca51a31d4acf7f995b6add
Hemant Adil <hemantadil@google.com>
2025-11-05 08:58:05 +0530

    Removing the hard coded resource

--- mmv1/third_party/tgc/tests/data/google_vmwareengine_network_peering.tf
+++ mmv1/third_party/tgc/tests/data/google_vmwareengine_network_peering.tf
@@ -1,15 +1,3 @@
-resource "google_compute_network" "peered_network" {
-  name                    = "network-peering-test-nw"
-  auto_create_subnetworks = false
-}
-
-resource "google_vmwareengine_network" "vmware_network" {
-  name        = "network-peering-test-ven"
-  location    = "global"
-  type        = "STANDARD"
-  description = "VMware Engine network for peering test"
-}
-
 resource "google_vmwareengine_network_peering" "peering" {
   name                  = "network-peering-test"
   peer_network          = "projects/{{.Provider.Project}}/locations/global/vmwareEngineNetworks/network-peering-test-nw"
