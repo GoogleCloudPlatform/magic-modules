6956a0a0ce1c13521ba2d88a757fcc5d24d6a067
Hemant Adil <hemantadil@google.com>
2025-11-05 08:51:21 +0530

    hardcoding vmware engine network id

--- mmv1/third_party/tgc/tests/data/vmwareengine_network_policy.tf
+++ mmv1/third_party/tgc/tests/data/vmwareengine_network_policy.tf
@@ -1,16 +1,9 @@
-resource "google_vmwareengine_network" "gg_asset_17806_51c7" {
-  name     = "gg-asset-17806-51c7-network"
-  location = "global"
-  project  = "{{.Provider.project}}"
-  type     = "STANDARD"
-}
-
 resource "google_vmwareengine_network_policy" "gg_asset_17806_51c7" {
   name                  = "gg-asset-17806-51c7"
   location              = "us-central1"
   project               = "{{.Provider.project}}"
   edge_services_cidr    = "192.168.30.0/26"
-  vmware_engine_network = google_vmwareengine_network.gg_asset_17806_51c7.id
+  vmware_engine_network = "projects/{{.Provider.project}}/locations/global/vmwareEngineNetworks/gg-asset-17806-51c7-network"
   internet_access {
     enabled = true
   }
