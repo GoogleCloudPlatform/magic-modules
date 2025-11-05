972096d61f22341fdedbb7505430d828d61482af
Hemant Adil <hemantadil@google.com>
2025-11-05 08:38:10 +0530

    Hardcoding network peer and vmware network id in test cases

--- mmv1/third_party/tgc/tests/data/google_vmwareengine_network_peering.tf
+++ mmv1/third_party/tgc/tests/data/google_vmwareengine_network_peering.tf
@@ -12,7 +12,7 @@ resource "google_vmwareengine_network" "vmware_network" {
 
 resource "google_vmwareengine_network_peering" "peering" {
   name                  = "network-peering-test"
-  peer_network          = google_compute_network.peered_network.id
+  peer_network          = "projects/{{.Provider.Project}}/locations/global/vmwareEngineNetworks/network-peering-test-nw"
   peer_network_type     = "STANDARD"
-  vmware_engine_network = google_vmwareengine_network.vmware_network.id
+  vmware_engine_network = "projects/{{.Provider.Project}}/locations/global/vmwareEngineNetworks/network-peering-test-ven"
 }
