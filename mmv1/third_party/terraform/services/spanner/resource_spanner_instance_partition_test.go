2d0a4d4375101cfc333395b6bae82dedfb5af5eb
rahul yadav <rahulyadavsep92@gmail.com>
2025-11-11 13:16:18 +0530

    feat(spanner): add support for autoscaling config in spanner's instance partition

--- mmv1/third_party/terraform/services/spanner/resource_spanner_instance_partition_test.go
+++ mmv1/third_party/terraform/services/spanner/resource_spanner_instance_partition_test.go
@@ -1,10 +1,17 @@
 package spanner_test
 
 import (
+	"context"
+	"fmt"
+	"strings"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"google.golang.org/api/googleapi"
+	"google.golang.org/api/option"
+	"google.golang.org/api/spanner/v1"
 )
 
 func TestAccSpannerInstancePartition_basic(t *testing.T) {
@@ -61,6 +68,38 @@ func TestAccSpannerInstancePartition_processingUnits(t *testing.T) {
 			},
 			{
 				Config: testAccSpannerInstancePartition_processingUnitsUpdate(context),
+			},
+			{
+				ResourceName:      "google_spanner_instance_partition.partition",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccSpannerInstancePartition_autoscaling(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckSpannerInstancePartitionDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSpannerInstancePartition_autoscaling(context),
+			},
+			{
+				ResourceName:      "google_spanner_instance_partition.partition",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccSpannerInstancePartition_autoscaling_update(context),
 			},
 			{
 				ResourceName:      "google_spanner_instance_partition.partition",
@@ -145,4 +184,94 @@ resource "google_spanner_instance_partition" "partition" {
   processing_units = 2000
 }
 `, context)
+}
+
+func testAccSpannerInstancePartition_autoscaling(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_spanner_instance" "main" {
+  name         = "tf-test-spanner-main-%{random_suffix}"
+  config       = "nam6"
+  display_name = "main-instance"
+  num_nodes    = 2
+}
+
+resource "google_spanner_instance_partition" "partition" {
+  name         = "tf-test-partition-%{random_suffix}"
+  instance     = google_spanner_instance.main.name
+  config       = "regional-us-central1"
+  display_name = "test-spanner-partition"
+  autoscaling_config {
+    autoscaling_limits {
+	  min_nodes = 1
+	  max_nodes = 2
+    }
+    autoscaling_targets {
+      high_priority_cpu_utilization_percent = 65
+      storage_utilization_percent           = 95
+    }
+  }
+}
+`, context)
+}
+
+func testAccSpannerInstancePartition_autoscaling_update(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_spanner_instance" "main" {
+  name         = "tf-test-spanner-main-%{random_suffix}"
+  config       = "nam6"
+  display_name = "main-instance"
+  num_nodes    = 3
+}
+
+resource "google_spanner_instance_partition" "partition" {
+  name         = "tf-test-partition-%{random_suffix}"
+  instance     = google_spanner_instance.main.name
+  config       = "regional-us-central1"
+  display_name = "updated-spanner-partition"
+  autoscaling_config {
+    autoscaling_limits {
+	  min_nodes = 1
+	  max_nodes = 3
+    }
+    autoscaling_targets {
+      high_priority_cpu_utilization_percent = 75
+      storage_utilization_percent           = 90
+    }
+  }
+}
+`, context)
+}
+
+func testAccCheckSpannerInstancePartitionDestroyProducer(t *testing.T) func(s *terraform.State) error {
+	return func(s *terraform.State) error {
+		for name, rs := range s.RootModule().Resources {
+			if rs.Type != "google_spanner_instance_partition" {
+				continue
+			}
+			if strings.HasPrefix(name, "data.") {
+				continue
+			}
+
+			config := acctest.GoogleProviderConfig(t)
+			spannerService, err := spanner.NewService(context.Background(), option.WithHTTPClient(config.Client))
+			if err != nil {
+				return err
+			}
+
+			_, err = spannerService.Projects.Instances.InstancePartitions.Get(rs.Primary.ID).Do()
+			if err != nil {
+				if isGoogleApiErrorWithCode(err, 404) {
+					return nil
+				}
+				return fmt.Errorf("Error retrieving instance partition %s: %s", rs.Primary.ID, err)
+			}
+			return fmt.Errorf("Instance partition %s still exists", rs.Primary.ID)
+		}
+		return nil
+	}
+}
+
+func isGoogleApiErrorWithCode(err error, code int) bool {
+	gerr, ok := err.(*googleapi.Error)
+	return ok && gerr.Code == code
 }
