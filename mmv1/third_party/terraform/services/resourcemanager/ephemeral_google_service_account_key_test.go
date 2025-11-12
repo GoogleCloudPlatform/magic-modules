dbe439507061f898dc0b574525cd0beb6a78c39a
alvarez.mauriciotm@gmail.com <alvarez.mauriciotm@gmail.com>
2025-11-12 14:19:36 -0800

    provide unique service accounts to each TestAcc

--- mmv1/third_party/terraform/services/resourcemanager/ephemeral_google_service_account_key_test.go
+++ mmv1/third_party/terraform/services/resourcemanager/ephemeral_google_service_account_key_test.go
@@ -12,27 +12,57 @@ import (
 func TestAccEphemeralServiceAccountKey_create(t *testing.T) {
 	t.Parallel()
 
-	serviceAccount := envvar.GetTestServiceAccountFromEnv(t)
-	targetServiceAccountEmail := acctest.BootstrapServiceAccount(t, "key-basic", serviceAccount)
+	accountID := "a" + acctest.RandString(t, 10)
+	displayName := "Terraform Test"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"time": {},
+		},
 		Steps: []resource.TestStep{
 			{
-				Config: testAccEphemeralServiceAccountKey_create(targetServiceAccountEmail),
+				Config: testAccEphemeralServiceAccountKey_create_setup(accountID, displayName),
+			},
+			{
+				Config: testAccEphemeralServiceAccountKey_create(accountID, displayName),
 			},
 		},
 	})
 }
 
-func testAccEphemeralServiceAccountKey_create(serviceAccount string) string {
+func testAccEphemeralServiceAccountKey_create_setup(accountID, displayName string) string {
 	return fmt.Sprintf(`
+resource "google_service_account" "service_account" {
+	account_id   = "%s"
+	display_name = "%s"
+}
+
+resource "time_sleep" "wait_10_seconds" {
+  create_duration = "10s"
+  depends_on = [google_service_account.service_account]
+}
+`, accountID, displayName)
+}
+
+func testAccEphemeralServiceAccountKey_create(accountID, displayName string) string {
+	return fmt.Sprintf(`
+resource "google_service_account" "service_account" {
+	account_id   = "%s"
+	display_name = "%s"
+}
+
+resource "time_sleep" "wait_10_seconds" {
+  create_duration = "10s"
+  depends_on = [google_service_account.service_account]
+}
+
 ephemeral "google_service_account_key" "key" {
-  service_account_id = "%s"
+  service_account_id = google_service_account.service_account.email
   public_key_type = "TYPE_X509_PEM_FILE"
 }
-`, serviceAccount)
+`, accountID, displayName)
 }
 
 func TestAccEphemeralServiceAccountKey_upload(t *testing.T) {
@@ -57,37 +87,35 @@ func TestAccEphemeralServiceAccountKey_upload(t *testing.T) {
 	})
 }
 
-func testAccEphemeralServiceAccountKey_upload_setup(accountID, name string) string {
+func testAccEphemeralServiceAccountKey_upload_setup(accountID, displayName string) string {
 	return fmt.Sprintf(`
-resource "google_service_account" "acceptance" {
-  account_id   = "%s"
-  display_name = "%s"
+resource "google_service_account" "service_account" {
+	account_id   = "%s"
+	display_name = "%s"
 }
-
 resource "time_sleep" "wait_10_seconds" {
   create_duration = "10s"
-  depends_on = [google_service_account.acceptance]
+  depends_on = [google_service_account.service_account]
 }
-`, accountID, name)
+`, accountID, displayName)
 }
 
-func testAccEphemeralServiceAccountKey_upload(serviceAccount, name string) string {
+func testAccEphemeralServiceAccountKey_upload(accountID, displayName string) string {
 	return fmt.Sprintf(`
-resource "google_service_account" "acceptance" {
-  account_id   = "%s"
-  display_name = "%s"
+resource "google_service_account" "service_account" {
+	account_id   = "%s"
+	display_name = "%s"
 }
-
 resource "time_sleep" "wait_10_seconds" {
   create_duration = "10s"
-  depends_on = [google_service_account.acceptance]
+  depends_on = [google_service_account.service_account]
 }
 
 ephemeral "google_service_account_key" "key" {
-  service_account_id = google_service_account.acceptance.email
+  service_account_id = google_service_account.service_account.email
   public_key_data    = filebase64("test-fixtures/public_key.pem")
 }
-`, serviceAccount, name)
+`, accountID, displayName)
 }
 
 func TestAccEphemeralServiceAccountKey_fetch(t *testing.T) {
