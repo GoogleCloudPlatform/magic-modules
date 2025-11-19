a2b983c2e0e187dc094fbfe149baacaa02006157
Shuya Ma <shuyama@google.com>
2025-11-19 12:37:26 -0800

    update org policy to use secondary test org

--- mmv1/third_party/terraform/services/orgpolicy/resource_org_policy_policy_test.go
+++ mmv1/third_party/terraform/services/orgpolicy/resource_org_policy_policy_test.go
@@ -77,7 +77,7 @@ func TestAccOrgPolicyPolicy_OrganizationPolicy(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"org_id":        envvar.GetTestOrgFromEnv(t),
+		"org_id":        envvar.GetTestOrgTargetFromEnv(t),
 		"random_suffix": acctest.RandString(t, 10),
 	}
 
@@ -154,7 +154,7 @@ func TestAccOrgPolicyPolicy_DryRunSpecHandWritten(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"org_id":        envvar.GetTestOrgFromEnv(t),
+		"org_id":        envvar.GetTestOrgTargetFromEnv(t),
 		"random_suffix": acctest.RandString(t, 10),
 	}
 
