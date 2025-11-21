90cde66ce23ad3364828cdc07dbb2f608449295e
Abhishek Sharma <notyouravgabhi@google.com>
2025-11-21 18:09:55 +0000

    Add Import for Plancheck

--- mmv1/third_party/terraform/services/modelarmorglobal/resource_model_armor_floorsetting_test.go
+++ mmv1/third_party/terraform/services/modelarmorglobal/resource_model_armor_floorsetting_test.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource/plancheck"
+
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 
 	"github.com/hashicorp/terraform-provider-google/google/envvar"
