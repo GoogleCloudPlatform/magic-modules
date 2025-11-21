bd7843d0451ca5db45ea745637e98d0b33a2082c
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2025-11-21 08:09:15 +0100

    fix: add specific test case for the image upgrade (causes recreate on runtime)

--- mmv1/third_party/terraform/services/colab/resource_colab_runtime_test.go
+++ mmv1/third_party/terraform/services/colab/resource_colab_runtime_test.go
@@ -68,9 +68,98 @@ func TestAccColabRuntime_update(t *testing.T) {
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{"desired_state", "location", "name", "auto_upgrade"},
 			},
+			{
+				Config: testAccColabRuntime_image_upgrade(context),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("google_colab_runtime.runtime", plancheck.ResourceActionDestroyBeforeCreate),
+					},
+				},
+			},
+			{
+				ResourceName:            "google_colab_runtime.runtime",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"desired_state", "location", "name", "auto_upgrade"},
+			},
 		},
 	})
 }
+
+func testAccColabRuntime_image_upgrade(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_colab_runtime_template" "my_template" {
+  name        = "tf-test-colab-runtime%{random_suffix}"
+  display_name = "Runtime template full"
+  location    = "us-central1"
+  description = "Full runtime template"
+  machine_spec {
+    machine_type     = "n1-standard-2"
+    accelerator_type = "NVIDIA_TESLA_T4"
+    accelerator_count = "1"
+  }
+
+  data_persistent_disk_spec {
+    disk_type    = "pd-standard"
+    disk_size_gb = 200
+  }
+
+  network_spec {
+    enable_internet_access = true
+  }
+
+  labels = {
+    k = "val"
+  }
+
+  idle_shutdown_config {
+    idle_timeout = "3600s"
+  }
+
+  euc_config {
+    euc_disabled = true
+  }
+
+  shielded_vm_config {
+    enable_secure_boot = true
+  }
+
+  network_tags = ["abc", "def"]
+
+  encryption_spec {
+    kms_key_name = "%{key_name}"
+  }
+
+  software_config {
+    colab_image {
+      release_name = "py310"
+    }
+  }
+}
+
+resource "google_colab_runtime" "runtime" {
+  name = "tf-test-colab-runtime%{random_suffix}"
+  location = "us-central1" 
+  
+  notebook_runtime_template_ref {
+    notebook_runtime_template = google_colab_runtime_template.my_template.id
+  }
+  
+  display_name = "Runtime full"
+  runtime_user = "gterraformtestuser@gmail.com"
+  description = "Full runtime"
+
+  desired_state = "RUNNING"
+
+  auto_upgrade = true
+
+  depends_on = [
+    google_colab_runtime_template.my_template
+  ]
+}
+`, context)
+}
+
 
 func testAccColabRuntime_full(context map[string]interface{}) string {
 	return acctest.Nprintf(`
@@ -192,7 +281,7 @@ resource "google_colab_runtime_template" "my_template" {
 
   software_config {
     colab_image {
-      release_name = "py310"
+      release_name = "py311"
     }
   }
 }
@@ -260,6 +349,12 @@ resource "google_colab_runtime_template" "my_template" {
 
   encryption_spec {
     kms_key_name = "%{key_name}"
+  }
+	
+	software_config {
+    colab_image {
+      release_name = "py311"
+    }
   }
 }
 
