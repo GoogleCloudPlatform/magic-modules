b9d66e24527f237143034006ae56cbfb8e0803c9
Shuya Ma <shuyama@google.com>
2025-12-04 09:37:19 -0800

    modify update test to exclude immutable field changes

--- mmv1/third_party/terraform/services/datastream/resource_datastream_stream_test.go
+++ mmv1/third_party/terraform/services/datastream/resource_datastream_stream_test.go
@@ -27,7 +27,7 @@ func TestAccDatastreamStream_update(t *testing.T) {
 		CheckDestroy: testAccCheckDatastreamStreamDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDatastreamStream_datastreamStreamBasicExample(context),
+				Config: testAccDatastreamStream_datastreamStreamBasic(context),
 				Check:  resource.TestCheckResourceAttr("google_datastream_stream.default", "state", "NOT_STARTED"),
 			},
 			{
@@ -83,6 +83,139 @@ func TestAccDatastreamStream_update(t *testing.T) {
 		},
 	})
 }
+
+func testAccDatastreamStream_datastreamStreamBasic(context map[string]interface{}) string {
+    return acctest.Nprintf(`
+data "google_project" "project" {
+}
+
+resource "google_sql_database_instance" "instance" {
+    name             = "tf-test-my-instance%{random_suffix}"
+    database_version = "MYSQL_8_0"
+    region           = "us-central1"
+    settings {
+        tier = "db-f1-micro"
+        backup_configuration {
+            enabled            = true
+            binary_log_enabled = true
+        }
+
+        ip_configuration {
+
+            // Datastream IPs will vary by region.
+            authorized_networks {
+                value = "34.71.242.81"
+            }
+
+            authorized_networks {
+                value = "34.72.28.29"
+            }
+
+            authorized_networks {
+                value = "34.67.6.157"
+            }
+
+            authorized_networks {
+                value = "34.67.234.134"
+            }
+
+            authorized_networks {
+                value = "34.72.239.218"
+            }
+        }
+    }
+
+    deletion_protection  = %{deletion_protection}
+}
+
+resource "google_sql_database" "db" {
+    instance = google_sql_database_instance.instance.name
+    name     = "db"
+}
+
+resource "random_password" "pwd" {
+    length = 16
+    special = false
+}
+
+resource "google_sql_user" "user" {
+    name     = "user"
+    instance = google_sql_database_instance.instance.name
+    host     = "%"
+    password = random_password.pwd.result
+}
+
+resource "google_datastream_connection_profile" "source_connection_profile" {
+    display_name          = "Source connection profile"
+    location              = "us-central1"
+    connection_profile_id = "tf-test-source-profile%{random_suffix}"
+
+    mysql_profile {
+        hostname = google_sql_database_instance.instance.public_ip_address
+        username = google_sql_user.user.name
+        password = google_sql_user.user.password
+    }
+}
+
+resource "google_storage_bucket" "bucket" {
+  name                        = "tf-test-my-bucket%{random_suffix}"
+  location                    = "US"
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_iam_member" "viewer" {
+    bucket = google_storage_bucket.bucket.name
+    role   = "roles/storage.objectViewer"
+    member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-datastream.iam.gserviceaccount.com"
+}
+
+resource "google_storage_bucket_iam_member" "creator" {
+    bucket = google_storage_bucket.bucket.name
+    role   = "roles/storage.objectCreator"
+    member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-datastream.iam.gserviceaccount.com"
+}
+
+resource "google_storage_bucket_iam_member" "reader" {
+    bucket = google_storage_bucket.bucket.name
+    role   = "roles/storage.legacyBucketReader"
+    member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-datastream.iam.gserviceaccount.com"
+}
+
+resource "google_datastream_connection_profile" "destination_connection_profile" {
+    display_name          = "Connection profile"
+    location              = "us-central1"
+    connection_profile_id = "tf-test-destination-profile%{random_suffix}"
+
+    gcs_profile {
+        bucket    = google_storage_bucket.bucket.name
+        root_path = "/path"
+    }
+}
+
+resource "google_datastream_stream" "default" {
+    stream_id = "tf-test-my-stream%{random_suffix}"
+    location = "us-central1"
+    display_name = "my stream"
+    create_without_validation = true
+    source_config {
+        source_connection_profile = google_datastream_connection_profile.source_connection_profile.id
+        mysql_source_config {
+          max_concurrent_backfill_tasks = 15
+        }
+    }
+    destination_config {
+        destination_connection_profile = google_datastream_connection_profile.destination_connection_profile.id
+        gcs_destination_config {
+            avro_file_format {}
+        }
+    }
+
+    backfill_none {
+    }
+}
+`, context)
+}
+
 
 func testAccDatastreamStream_datastreamStreamBasicUpdate(context map[string]interface{}, desiredState string, preventDestroy bool) string {
 	context["lifecycle_block"] = ""
