dd3f6e5f3b0831b20300fae6a9eadf38ff786e64
jgforsberg <35646377+jgforsberg@users.noreply.github.com>
2025-12-08 16:19:28 -0800

    Update resource_network_services_multicast_domain_test.go

--- mmv1/third_party/terraform/services/networkservices/resource_network_services_multicast_domain_test.go
+++ mmv1/third_party/terraform/services/networkservices/resource_network_services_multicast_domain_test.go
@@ -58,9 +58,7 @@ resource "google_network_services_multicast_domain" md_test {
   admin_network = google_compute_network.network.id
   connection_config  {
     connection_type="SAME_VPC"
-    ncc_hub = ""
   }
-  multicast_domain_group  = ""
   depends_on = [google_compute_network.network]
 }
 `, context)
@@ -82,9 +80,7 @@ resource "google_network_services_multicast_domain" md_test {
   admin_network = google_compute_network.network.id
   connection_config  {
     connection_type="SAME_VPC"
-    ncc_hub = ""
   }
-  multicast_domain_group  = ""
   depends_on = [google_compute_network.network]
 }
 `, context)
