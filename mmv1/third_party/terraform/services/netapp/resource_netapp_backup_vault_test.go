249bfaa8689696781a8007562b70df5e807359d9
Raaz <rajannd@google.com>
2025-12-09 23:17:44 +0530

    added support for kms_comfig field

--- mmv1/third_party/terraform/services/netapp/resource_netapp_backup_vault_test.go
+++ mmv1/third_party/terraform/services/netapp/resource_netapp_backup_vault_test.go
@@ -67,6 +67,52 @@ resource "google_netapp_backup_vault" "test_backup_vault" {
 `, context)
 }
 
+// TestAccNetappBackupVault_Kms: Tests Backup Vault creation with KMS configuration.
+func TestAccNetappBackupVault_Kms(t *testing.T) {
+	location := "us-east4"
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+		"location":      location,
+		"kms_key_name":  acctest.BootstrapKMSKeyWithPurposeInLocationAndName(t, "ENCRYPT_DECRYPT", location, "tf-test-netapp-bv-key").CryptoKey.Name,
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetappBackupVault_withKms(context),
+			},
+			{
+				ResourceName:            "google_netapp_backup_vault.test_backup_vault_kms",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"location", "name", "labels", "terraform_labels"},
+			},
+		},
+	})
+}
+
+func testAccNetappBackupVault_withKms(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_netapp_kmsconfig" "test_kms_config" {
+  name            = "tf-test-kms-config-%{random_suffix}"
+  provider        = google-beta
+  location        = "%{location}"
+  crypto_key_name = "%{kms_key_name}"
+  description     = "Test KMS config for Backup Vault"
+}
+
+resource "google_netapp_backup_vault" "test_backup_vault_kms" {
+  name        = "tf-test-bv-kms-%{random_suffix}"
+  provider    = google-beta
+  location    = "%{location}"
+  kms_config  = google_netapp_kmsconfig.test_kms_config.id
+  description = "Vault with KMS"
+}
+	`, context)
+}
+
 func testAccCheckNetappBackupVaultDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
