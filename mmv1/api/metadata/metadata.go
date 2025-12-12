da9914877b5b90fa067bd98818a2c4dd3dbb1ad6
Stephen Lewis <stephenrlewis@google.com>
2025-12-12 15:21:55 -0800

    Split field logic into a separate file

--- mmv1/api/metadata/metadata.go
+++ mmv1/api/metadata/metadata.go
@@ -34,39 +34,6 @@ func FromResource(r api.Resource) Metadata {
 	return m
 }
 
-func FromProperties(props []*api.Type) []Field {
-	var fields []Field
-	for _, p := range props {
-		f := Field{
-			Json:         p.IsJsonField(),
-			ProviderOnly: p.ProviderOnly(),
-		}
-		if !p.ProviderOnly() {
-			f.ApiField = p.MetadataApiLineage()
-		}
-		if p.ProviderOnly() || p.MetadataLineage() != p.MetadataDefaultLineage() {
-			f.Field = p.MetadataLineage()
-		}
-		fields = append(fields, f)
-	}
-	return fields
-}
-
-// Field is a field in a metadata.yaml file.
-type Field struct {
-	// The name of the field in the REST API, including the path. For example, "buildConfig.source.storageSource.bucket".
-	ApiField string `yaml:"api_field,omitempty"`
-	// The name of the field in Terraform, including the path. For example, "build_config.source.storage_source.bucket". Defaults to the value
-	// of `api_field` converted to snake_case.
-	Field string `yaml:"field,omitempty"`
-	// If true, the field is only present in the provider. This primarily applies for virtual fields and url-only parameters. When set to true,
-	// `field` should be set and `api_field` should be left empty. Default: `false`.
-	ProviderOnly bool `yaml:"provider_only,omitempty"`
-	// If true, this is a JSON field which "covers" all child API fields. As a special case, JSON fields which cover an entire resource can
-	// have `api_field` set to `*`.
-	Json bool `yaml:"json,omitempty"`
-}
-
 // Metadata represents a metadata.yaml file for a single Terraform resource.
 type Metadata struct {
 	// The name of the Terraform resource. For example, "google_cloudfunctions2_function".
