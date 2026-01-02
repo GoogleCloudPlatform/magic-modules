dbac10ba2ca24471c1972ea0d72fe19c61120cbe
Nick Elliot <nickelliot@google.com>
2026-01-02 14:58:04 -0800

    add code for generating deletion policy fields

--- mmv1/api/resource.go
+++ mmv1/api/resource.go
@@ -276,6 +276,14 @@ type Resource struct {
 	// public ca external account keys
 	ExcludeRead bool `yaml:"exclude_read,omitempty"`
 
+	// Set to true for resources that are to generate deletion policy fields by default
+	// TODO: this will be inverted to an opt-out flag very soon
+	DeletionPolicy bool `yaml:"deletion_policy,omitempty"`
+
+	// Set to the default deletion policy value for the resource.
+	// By default this will be "DELETE".
+	DeletionPolicyDefault string `yaml:"deletion_policy_default,omitempty"`
+
 	// Set to true for resources that wish to disable automatic generation of default provider
 	// value customdiff functions
 	// TODO rewrite: 1 instance used
@@ -482,6 +490,11 @@ func (r *Resource) setShallowDefaults() {
 	}
 	if r.Timeouts == nil {
 		r.Timeouts = NewTimeouts() // This only sets defaults if Timeouts is nil
+	}
+	if r.DeletionPolicy {
+		if r.DeletionPolicyDefault == "" {
+			r.DeletionPolicyDefault == "DELETE"
+		}
 	}
 }
 
