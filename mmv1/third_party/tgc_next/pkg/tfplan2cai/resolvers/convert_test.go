d0281f0012cc5d2cc9fa4183ab7496940dd984ef
hao-nan-li <100219545+hao-nan-li@users.noreply.github.com>
2026-02-05 15:04:54 -0800

    tgc-revival: Build Parent to child map for parent resolver (#16252)

--- mmv1/third_party/tgc_next/pkg/tfplan2cai/resolvers/convert_test.go
+++ mmv1/third_party/tgc_next/pkg/tfplan2cai/resolvers/convert_test.go
@@ -25,3 +25,34 @@ func TestConvert_iamBinding(t *testing.T) {
 	assert.Equal(t, 2, len(idToResourceChangeMap["instance_name/google_compute_instance.tgc-iam.name/project/terraform-dev-zhenhuali/zone/us-central1-a/"]), "Expected iam list to be size 2")
 	assert.Equal(t, 0, len(idToResourceChangeMap["google_compute_instance_iam_member.foo1"]), "Expected this key to return null")
 }
+
+func TestResolveParentsBasic(t *testing.T) {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		t.Fatalf("Error initializing logger %s", err)
+	}
+	f := "compute_network.tfplan.json"
+	jsonPlan, err := os.ReadFile(f)
+	if err != nil {
+		t.Fatalf("Error parsing %s: %s", f, err)
+	}
+
+	parentToChildMap := NewParentResourceResolver(logger).Resolve(jsonPlan)
+	assert.Equal(t, "google_vmwareengine_network_peering.peering", parentToChildMap["google_compute_network.peered_network"][0])
+	assert.Equal(t, "google_vmwareengine_network_peering.peering", parentToChildMap["google_vmwareengine_network.vmware_network"][0])
+}
+
+func TestResolveParentsNested(t *testing.T) {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		t.Fatalf("Error initializing logger %s", err)
+	}
+	f := "compute_disk_nestedId.tfplan.json"
+	jsonPlan, err := os.ReadFile(f)
+	if err != nil {
+		t.Fatalf("Error parsing %s: %s", f, err)
+	}
+
+	parentToChildMap := NewParentResourceResolver(logger).Resolve(jsonPlan)
+	assert.Equal(t, "google_compute_disk.secondary", parentToChildMap["google_compute_disk.primary"][0])
+}
