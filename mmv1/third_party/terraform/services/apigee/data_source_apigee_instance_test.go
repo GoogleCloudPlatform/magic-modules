07bd4c8b7db40d3e9322965b620fd25352fb37d6
Michael Lopez <mickael.lopez@gmail.com>
2026-02-19 10:59:26 +0100

    Endpoint attachment reworking

--- mmv1/third_party/terraform/services/apigee/data_source_apigee_instance_test.go
+++ mmv1/third_party/terraform/services/apigee/data_source_apigee_instance_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
 )
 
 func TestAccDataSourceApigeeInstance_basic(t *testing.T) {
@@ -12,13 +13,18 @@ func TestAccDataSourceApigeeInstance_basic(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"instance_name": "tf-test-" + acctest.RandString(t, 10),
-		"random_suffix": acctest.RandString(t, 10),
+		"billing_account": envvar.GetTestBillingAccountFromEnv(t),
+		"instance_name":   "tf-test-" + acctest.RandString(t, 10),
+		"location":        envvar.GetTestRegionFromEnv(),
+		"random_suffix":   acctest.RandString(t, 10),
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"time": {},
+		},
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataSourceApigeeInstanceConfig(context),
@@ -40,36 +46,70 @@ func TestAccDataSourceApigeeInstance_basic(t *testing.T) {
 
 func testAccDataSourceApigeeInstanceConfig(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_compute_network" "net" {
-  name = "net-%{random_suffix}"
+resource "google_project" "project" {
+  project_id      = "tf-test%{random_suffix}"
+  name            = "tf-test%{random_suffix}"
+  billing_account = "%{billing_account}"
+  deletion_policy = "DELETE"
 }
 
-resource "google_compute_global_address" "range" {
-  name          = "range-%{random_suffix}"
+resource "google_project_service" "apigee" {
+  project = google_project.project.project_id
+  service = "apigee.googleapis.com"
+}
+
+resource "google_project_service" "servicenetworking" {
+  project = google_project.project.project_id
+  service = "servicenetworking.googleapis.com"
+}
+
+resource "google_project_service" "compute" {
+  project = google_project.project.project_id
+  service = "compute.googleapis.com"
+  depends_on = [google_project_service.servicenetworking]
+}
+
+resource "time_sleep" "wait_300_seconds" {
+  create_duration = "300s"
+  depends_on = [google_project_service.compute]
+}
+
+resource "google_compute_network" "apigee_network" {
+  name       = "apigee-network"
+  project    = google_project.project.project_id
+  depends_on = [time_sleep.wait_300_seconds]
+}
+
+resource "google_compute_global_address" "apigee_range" {
+  name          = "tf-test-apigee-range%{random_suffix}"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
-  network       = google_compute_network.net.id
+  network       = google_compute_network.apigee_network.id
+  project       = google_project.project.project_id
 }
 
-resource "google_service_networking_connection" "con" {
-  network                 = google_compute_network.net.id
+resource "google_service_networking_connection" "apigee_vpc_connection" {
+  network                 = google_compute_network.apigee_network.id
   service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.range.name]
+  reserved_peering_ranges = [google_compute_global_address.apigee_range.name]
+  depends_on              = [google_project_service.servicenetworking]
 }
 
-resource "google_apigee_organization" "org" {
-  project_id                           = acctest.GetTestProject()
-  analytics_region                     = "us-central1"
-  runtime_type                         = "CLOUD"
-  authorized_network                   = google_compute_network.net.id
-  depends_on                           = [google_service_networking_connection.con]
+resource "google_apigee_organization" "test" {
+  analytics_region   = "%{location}"
+  project_id         = google_project.project.project_id
+  authorized_network = google_compute_network.apigee_network.id
+  depends_on         = [
+    google_service_networking_connection.apigee_vpc_connection,
+    google_project_service.apigee,
+  ]
 }
 
 resource "google_apigee_instance" "test" {
   name     = "%{instance_name}"
   location = "%{location}"
-  org_id   = google_apigee_organization.org.id
+  org_id   = google_apigee_organization.test.id
 }
 
 data "google_apigee_instance" "test" {
