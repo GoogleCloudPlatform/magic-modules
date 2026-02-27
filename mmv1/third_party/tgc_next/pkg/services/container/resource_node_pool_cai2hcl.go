e2365705ba96e963cbee973e2d951552c4d64204
Zhenhua Li <zhenhuali@google.com>
2026-02-27 15:59:26 -0800

    tgc-revival: refactor container flatten functions to use interface{} (#16519)

--- mmv1/third_party/tgc_next/pkg/services/container/resource_node_pool_cai2hcl.go
+++ mmv1/third_party/tgc_next/pkg/services/container/resource_node_pool_cai2hcl.go
@@ -7,10 +7,8 @@ import (
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v7/pkg/cai2hcl/converters/utils"
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v7/pkg/cai2hcl/models"
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v7/pkg/caiasset"
-	"github.com/GoogleCloudPlatform/terraform-google-conversion/v7/pkg/tpgresource"
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v7/pkg/transport"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"google.golang.org/api/container/v1"
 )
 
 // ContainerNodePoolCai2hclConverter for container node pool resource.
@@ -56,18 +54,13 @@ func (c *ContainerNodePoolCai2hclConverter) convertResourceData(asset caiasset.A
 	}
 	d := fakeResource.TestResourceData()
 
-	var nodePool *container.NodePool
-	if err := utils.DecodeJSON(asset.Resource.Data, &nodePool); err != nil {
-		return nil, err
-	}
-
 	hclData := make(map[string]interface{})
 
 	outputFields := map[string]struct{}{}
 	an := strings.Replace(asset.Name, "/zones/", "/locations/", 1)
 	utils.ParseUrlParamValuesFromAssetName(an, "//container.googleapis.com/projects/{{project}}/locations/{{location}}/clusters/{{cluster}}/nodePools/{{name}}", outputFields, hclData)
 
-	npMap, err := flattenNodePool(d, config, nodePool, "")
+	npMap, err := flattenNodePool(d, config, asset.Resource.Data, "")
 	if err != nil {
 		return nil, err
 	}
@@ -81,109 +74,129 @@ func (c *ContainerNodePoolCai2hclConverter) convertResourceData(asset caiasset.A
 		return nil, err
 	}
 	return &models.TerraformResourceBlock{
-		Labels: []string{c.name, nodePool.Name},
+		Labels: []string{c.name, asset.Resource.Data["name"].(string)},
 		Value:  ctyVal,
 	}, nil
 }
 
-func flattenNodePoolStandardRolloutPolicy(rp *container.StandardRolloutPolicy) []map[string]interface{} {
-	if rp == nil {
+func flattenNodePoolStandardRolloutPolicy(v interface{}) []map[string]interface{} {
+	if v == nil {
 		return nil
 	}
-
-	return []map[string]interface{}{
-		{
-			"batch_node_count":    rp.BatchNodeCount,
-			"batch_percentage":    rp.BatchPercentage,
-			"batch_soak_duration": rp.BatchSoakDuration,
-		},
+	rp, ok := v.(map[string]interface{})
+	if !ok {
+		return nil
 	}
+	transformed := map[string]interface{}{
+		"batch_node_count":    rp["batchNodeCount"],
+		"batch_percentage":    rp["batchPercentage"],
+		"batch_soak_duration": rp["batchSoakDuration"],
+	}
+	return []map[string]interface{}{transformed}
 }
 
-func flattenNodePoolBlueGreenSettings(bg *container.BlueGreenSettings) []map[string]interface{} {
-	if bg == nil {
+func flattenNodePoolBlueGreenSettings(v interface{}) []map[string]interface{} {
+	if v == nil {
 		return nil
 	}
-	return []map[string]interface{}{
-		{
-			"node_pool_soak_duration": bg.NodePoolSoakDuration,
-			"standard_rollout_policy": flattenNodePoolStandardRolloutPolicy(bg.StandardRolloutPolicy),
-		},
+	bg, ok := v.(map[string]interface{})
+	if !ok {
+		return nil
 	}
+
+	transformed := map[string]interface{}{
+		"node_pool_soak_duration": bg["nodePoolSoakDuration"],
+		"standard_rollout_policy": flattenNodePoolStandardRolloutPolicy(bg["standardRolloutPolicy"]),
+	}
+	return []map[string]interface{}{transformed}
 }
 
-func flattenNodePoolUpgradeSettings(us *container.UpgradeSettings) []map[string]interface{} {
-	if us == nil {
+func flattenNodePoolUpgradeSettings(v interface{}) []map[string]interface{} {
+	if v == nil {
 		return nil
 	}
-
+	us, ok := v.(map[string]interface{})
+	if !ok {
+		return nil
+	}
 	upgradeSettings := make(map[string]interface{})
 
-	upgradeSettings["blue_green_settings"] = flattenNodePoolBlueGreenSettings(us.BlueGreenSettings)
-	upgradeSettings["max_surge"] = us.MaxSurge
-	upgradeSettings["max_unavailable"] = us.MaxUnavailable
+	upgradeSettings["blue_green_settings"] = flattenNodePoolBlueGreenSettings(us["blueGreenSettings"])
+	upgradeSettings["max_surge"] = us["maxSurge"]
+	upgradeSettings["max_unavailable"] = us["maxUnavailable"]
 
-	upgradeSettings["strategy"] = us.Strategy
+	// "SHORT_LIVED" strategy is not supported by the Terraform provider yet.
+	if strategy, ok := us["strategy"].(string); ok && strategy != "SHORT_LIVED" {
+		upgradeSettings["strategy"] = strategy
+	}
+
 	return []map[string]interface{}{upgradeSettings}
 }
 
-func flattenNodePoolNodeDrainConfig(ndc *container.NodeDrainConfig) []map[string]interface{} {
-	if ndc == nil {
+func flattenNodePoolNodeDrainConfig(v interface{}) []map[string]interface{} {
+	if v == nil {
 		return nil
 	}
-
+	ndc, ok := v.(map[string]interface{})
+	if !ok {
+		return nil
+	}
 	nodeDrainConfig := make(map[string]interface{})
+	nodeDrainConfig["respect_pdb_during_node_pool_deletion"] = ndc["respectPdbDuringNodePoolDeletion"]
 
-	nodeDrainConfig["respect_pdb_during_node_pool_deletion"] = ndc.RespectPdbDuringNodePoolDeletion
 	return []map[string]interface{}{nodeDrainConfig}
 }
 
-func flattenNodePool(d *schema.ResourceData, config *transport.Config, np *container.NodePool, prefix string) (map[string]interface{}, error) {
+func flattenNodePool(d *schema.ResourceData, config *transport.Config, np map[string]interface{}, prefix string) (map[string]interface{}, error) {
 	// Node pools don't expose the current node count in their API, so read the
 	// instance groups instead. They should all have the same size, but in case a resize
 	// failed or something else strange happened, we'll just use the average size.
 	size := 0
 	igmUrls := []string{}
-	managedIgmUrls := []string{}
-	for _, url := range np.InstanceGroupUrls {
-		// retrieve instance group manager (InstanceGroupUrls are actually URLs for InstanceGroupManagers)
-		matches := instanceGroupManagerURL.FindStringSubmatch(url)
-		if len(matches) < 4 {
-			return nil, fmt.Errorf("Error reading instance group manage URL '%q'", url)
-		}
-		if strings.HasPrefix("gk3", matches[3]) {
-			// IGM is autopilot so we know it will not be found, skip it
-			continue
-		}
 
-		// TODO: get igms
+	if v, ok := np["instanceGroupUrls"].([]interface{}); ok {
+		for _, url := range v {
+			if urlStr, ok := url.(string); ok {
+				// retrieve instance group manager (InstanceGroupUrls are actually URLs for InstanceGroupManagers)
+				matches := instanceGroupManagerURL.FindStringSubmatch(urlStr)
+				if len(matches) < 4 {
+					return nil, fmt.Errorf("Error reading instance group manage URL '%q'", urlStr)
+				}
+				if strings.HasPrefix("gk3", matches[3]) {
+					// IGM is autopilot so we know it will not be found, skip it
+					continue
+				}
+				igmUrls = append(igmUrls, urlStr)
+			}
+		}
 	}
+
 	nodeCount := 0
 	if len(igmUrls) > 0 {
 		nodeCount = size / len(igmUrls)
 	}
 	nodePool := map[string]interface{}{
-		"name":                        np.Name,
-		"name_prefix":                 d.Get(prefix + "name_prefix"),
-		"initial_node_count":          np.InitialNodeCount,
-		"node_locations":              schema.NewSet(schema.HashString, tpgresource.ConvertStringArrToInterface(np.Locations)),
-		"node_count":                  nodeCount,
-		"node_config":                 flattenNodeConfig(np.Config, d.Get(prefix+"node_config")),
-		"instance_group_urls":         igmUrls,
-		"managed_instance_group_urls": managedIgmUrls,
-		"version":                     np.Version,
-		"network_config":              flattenNodeNetworkConfig(np.NetworkConfig, d, prefix),
+		"name":               np["name"],
+		"initial_node_count": np["initialNodeCount"],
+		"node_locations":     np["locations"],
+		"node_config":        flattenNodeConfig(np["config"], d.Get(prefix+"node_config")),
+		"version":            np["version"],
+		"network_config":     flattenNodeNetworkConfig(np["networkConfig"], d, prefix),
 	}
 
-	if np.Autoscaling != nil {
-		if np.Autoscaling.Enabled {
+	if nodeCount > 0 {
+		nodePool["node_count"] = nodeCount
+	}
+
+	if v, ok := np["autoscaling"].(map[string]interface{}); ok {
+		if enabled, ok := v["enabled"].(bool); ok && enabled {
 			nodePool["autoscaling"] = []map[string]interface{}{
 				{
-					"min_node_count":       np.Autoscaling.MinNodeCount,
-					"max_node_count":       np.Autoscaling.MaxNodeCount,
-					"total_min_node_count": np.Autoscaling.TotalMinNodeCount,
-					"total_max_node_count": np.Autoscaling.TotalMaxNodeCount,
-					"location_policy":      np.Autoscaling.LocationPolicy,
+					"min_node_count":       v["minNodeCount"],
+					"max_node_count":       v["maxNodeCount"],
+					"total_min_node_count": v["totalMinNodeCount"],
+					"total_max_node_count": v["totalMaxNodeCount"],
+					"location_policy":      v["locationPolicy"],
 				},
 			}
 		} else {
@@ -191,106 +204,118 @@ func flattenNodePool(d *schema.ResourceData, config *transport.Config, np *conta
 		}
 	}
 
-	if np.PlacementPolicy != nil {
+	if v, ok := np["placementPolicy"].(map[string]interface{}); ok {
 		nodePool["placement_policy"] = []map[string]interface{}{
 			{
-				"type":         np.PlacementPolicy.Type,
-				"policy_name":  np.PlacementPolicy.PolicyName,
-				"tpu_topology": np.PlacementPolicy.TpuTopology,
+				"type":         v["type"],
+				"policy_name":  v["policyName"],
+				"tpu_topology": v["tpuTopology"],
 			},
 		}
 	}
 
-	if np.QueuedProvisioning != nil {
+	if v, ok := np["queuedProvisioning"].(map[string]interface{}); ok {
 		nodePool["queued_provisioning"] = []map[string]interface{}{
 			{
-				"enabled": np.QueuedProvisioning.Enabled,
+				"enabled": v["enabled"],
 			},
 		}
 	}
 
-	if np.MaxPodsConstraint != nil {
-		nodePool["max_pods_per_node"] = np.MaxPodsConstraint.MaxPodsPerNode
+	if v, ok := np["maxPodsConstraint"].(map[string]interface{}); ok {
+		nodePool["max_pods_per_node"] = v["maxPodsPerNode"]
 	}
 
-	if np.Management != nil {
+	if v, ok := np["management"].(map[string]interface{}); ok {
 		nodePool["management"] = []map[string]interface{}{
 			{
-				"auto_repair":  np.Management.AutoRepair,
-				"auto_upgrade": np.Management.AutoUpgrade,
+				"auto_repair":  v["autoRepair"],
+				"auto_upgrade": v["autoUpgrade"],
 			},
 		}
 	}
 
-	if np.UpgradeSettings != nil {
-		nodePool["upgrade_settings"] = flattenNodePoolUpgradeSettings(np.UpgradeSettings)
-	} else {
-		delete(nodePool, "upgrade_settings")
-	}
+	nodePool["upgrade_settings"] = flattenNodePoolUpgradeSettings(np["upgradeSettings"])
 
-	if np.NodeDrainConfig != nil {
-		nodePool["node_drain_config"] = flattenNodePoolNodeDrainConfig(np.NodeDrainConfig)
-	}
+	nodePool["node_drain_config"] = flattenNodePoolNodeDrainConfig(np["nodeDrainConfig"])
 
 	return nodePool, nil
 }
 
-func flattenNodeNetworkConfig(c *container.NodeNetworkConfig, d *schema.ResourceData, prefix string) []map[string]interface{} {
-	result := []map[string]interface{}{}
-	if c != nil {
-		result = append(result, map[string]interface{}{
+func flattenNodeNetworkConfig(c interface{}, d *schema.ResourceData, prefix string) []map[string]interface{} {
+	if c == nil {
+		return nil
+	}
+
+	if config, ok := c.(map[string]interface{}); ok {
+		transformed := map[string]interface{}{
 			// TODO: investigate why create_pod_range is not returned by the API
 			// "create_pod_range": d.Get(prefix + "network_config.0.create_pod_range"), // API doesn't return this value so we set the old one. Field is ForceNew + Required
-			"pod_ipv4_cidr_block":             c.PodIpv4CidrBlock,
-			"pod_range":                       c.PodRange,
-			"enable_private_nodes":            c.EnablePrivateNodes,
-			"pod_cidr_overprovision_config":   flattenPodCidrOverprovisionConfig(c.PodCidrOverprovisionConfig),
-			"network_performance_config":      flattenNodeNetworkPerformanceConfig(c.NetworkPerformanceConfig),
-			"additional_node_network_configs": flattenAdditionalNodeNetworkConfig(c.AdditionalNodeNetworkConfigs),
-			"additional_pod_network_configs":  flattenAdditionalPodNetworkConfig(c.AdditionalPodNetworkConfigs),
-			"subnetwork":                      c.Subnetwork,
-		})
+			"pod_ipv4_cidr_block":             config["podIpv4CidrBlock"],
+			"pod_range":                       config["podRange"],
+			"enable_private_nodes":            config["enablePrivateNodes"],
+			"pod_cidr_overprovision_config":   flattenPodCidrOverprovisionConfig(config["podCidrOverprovisionConfig"]),
+			"network_performance_config":      flattenNodeNetworkPerformanceConfig(config["networkPerformanceConfig"]),
+			"additional_node_network_configs": flattenAdditionalNodeNetworkConfig(config["additionalNodeNetworkConfigs"]),
+			"additional_pod_network_configs":  flattenAdditionalPodNetworkConfig(config["additionalPodNetworkConfigs"]),
+		}
+		return []map[string]interface{}{transformed}
 	}
-	return result
+	return nil
 }
 
-func flattenNodeNetworkPerformanceConfig(c *container.NetworkPerformanceConfig) []map[string]interface{} {
-	result := []map[string]interface{}{}
-	if c != nil {
-		result = append(result, map[string]interface{}{
-			"total_egress_bandwidth_tier": c.TotalEgressBandwidthTier,
-		})
-	}
-	return result
-}
-
-func flattenAdditionalNodeNetworkConfig(c []*container.AdditionalNodeNetworkConfig) []map[string]interface{} {
+func flattenNodeNetworkPerformanceConfig(c interface{}) []map[string]interface{} {
 	if c == nil {
 		return nil
 	}
+	if config, ok := c.(map[string]interface{}); ok {
+		transformed := map[string]interface{}{
+			"total_egress_bandwidth_tier": config["totalEgressBandwidthTier"],
+		}
+		return []map[string]interface{}{transformed}
+	}
+	return nil
+}
 
+func flattenAdditionalNodeNetworkConfig(c interface{}) []map[string]interface{} {
+	if c == nil {
+		return nil
+	}
 	result := []map[string]interface{}{}
-	for _, nodeNetworkConfig := range c {
-		result = append(result, map[string]interface{}{
-			"network":    nodeNetworkConfig.Network,
-			"subnetwork": nodeNetworkConfig.Subnetwork,
-		})
+	if configs, ok := c.([]interface{}); ok {
+		for _, v := range configs {
+			if config, ok := v.(map[string]interface{}); ok {
+				transformed := map[string]interface{}{
+					"network":    config["network"],
+					"subnetwork": config["subnetwork"],
+				}
+				result = append(result, transformed)
+			}
+		}
 	}
 	return result
 }
 
-func flattenAdditionalPodNetworkConfig(c []*container.AdditionalPodNetworkConfig) []map[string]interface{} {
+func flattenAdditionalPodNetworkConfig(c interface{}) []map[string]interface{} {
 	if c == nil {
 		return nil
 	}
-
 	result := []map[string]interface{}{}
-	for _, podNetworkConfig := range c {
-		result = append(result, map[string]interface{}{
-			"subnetwork":          podNetworkConfig.Subnetwork,
-			"secondary_pod_range": podNetworkConfig.SecondaryPodRange,
-			"max_pods_per_node":   podNetworkConfig.MaxPodsPerNode.MaxPodsPerNode,
-		})
+	if configs, ok := c.([]interface{}); ok {
+		for _, v := range configs {
+			if config, ok := v.(map[string]interface{}); ok {
+				var maxPodsPerNode interface{}
+				if mp, ok := config["maxPodsPerNode"].(map[string]interface{}); ok {
+					maxPodsPerNode = mp["maxPodsPerNode"]
+				}
+				transformed := map[string]interface{}{
+					"subnetwork":          config["subnetwork"],
+					"secondary_pod_range": config["secondaryPodRange"],
+					"max_pods_per_node":   maxPodsPerNode,
+				}
+				result = append(result, transformed)
+			}
+		}
 	}
 	return result
 }
