b7a365e9c12301f24f44cdf74cf8cc93ba05b11f
Stephen Lewis <stephenrlewis@google.com>
2026-03-03 11:01:52 -0800

    Fixed TF<>API mapping for ACM service perimiter fine-grained resources
    Also fixed ordering of the resource.go to expect api_resource_field to live alongside the other api-related fields
    
    Technically the mapping for etag will be wrong after this, but that's less wrong than the current state

--- mmv1/api/resource.go
+++ mmv1/api/resource.go
@@ -54,6 +54,11 @@ type Resource struct {
 	// organization variant, however most resources do not need it.
 	ApiVariantPatterns []string `yaml:"api_variant_patterns,omitempty"`
 
+	// ApiResourceField indicates what field on the API resource is managed by a resource.
+	// This is generally relevant for fine-grained resources. For example,
+	// google_compute_router_nat manages the `nat` field on the `Router` resource.
+	ApiResourceField string `yaml:"api_resource_field,omitempty"`
+
 	// [Required] A description of the resource that's surfaced in provider
 	// documentation.
 	Description string
@@ -322,11 +327,6 @@ type Resource struct {
 
 	// The version name provided by the user through CI
 	TargetVersionName string `yaml:"-"`
-
-	// ApiResourceField indicates what field on the API resource is managed by a resource.
-	// This is generally relevant for fine-grained resources. For example,
-	// google_compute_router_nat manages the `nat` field on the `Router` resource.
-	ApiResourceField string `yaml:"api_resource_field,omitempty"`
 
 	ImportPath     string `yaml:"-"`
 	SourceYamlFile string `yaml:"-"`
