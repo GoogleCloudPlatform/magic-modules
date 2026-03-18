c6c2fd1843d998985f7bd7cc970436d41c7d46e5
Sachin Rungta <rungtasachin@google.com>
2026-03-18 13:23:12 +0000

    add wait time resource block for sync process completion

--- mmv1/third_party/terraform/services/dataplex/resource_dataplex_entry_link_test.go
+++ mmv1/third_party/terraform/services/dataplex/resource_dataplex_entry_link_test.go
@@ -66,6 +66,11 @@ resource "google_dataplex_glossary_term" "term_test_id_full" {
   display_name = "terraform term"
   description = "term created by Terraform"
 }
+# Introduce a 45-second wait after the glossary resource creation
+resource "time_sleep" "wait-for-sync" {
+  create_duration = "45s"
+  depends_on = [google_dataplex_glossary_term.term_test_id_full]
+}
 resource "google_dataplex_entry_link" "basic_entry_link" {
   project = "%{project_number}"
   location = "us-central1"
@@ -80,7 +85,7 @@ resource "google_dataplex_entry_link" "basic_entry_link" {
     name = "projects/${google_dataplex_entry_group.entry-group-basic.project}/locations/us-central1/entryGroups/@dataplex/entries/projects/${google_dataplex_entry_group.entry-group-basic.project}/locations/us-central1/glossaries/${google_dataplex_glossary.term_test_id_full.glossary_id}/terms/${google_dataplex_glossary_term.term_test_id_full.term_id}"
 	type = "TARGET"
   }
-  depends_on = [google_dataplex_glossary_term.term_test_id_full]
+  depends_on = [time_sleep.wait-for-sync]
 }
 `, context)
 }
