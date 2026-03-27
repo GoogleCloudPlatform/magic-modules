b2f3cf2a7715b7c282fd15a3f387e899145c0b0e
Chris Hawk <hawk@google.com>
2026-03-27 15:15:14 -0700

    Convert DCL resources to handwritten (#16759)

---
+++ mmv1/third_party/terraform/services/containerazure/provider_dcl_client_creation.go
@@ -0,0 +1,30 @@
+package containerazure
+
+import (
+	dcl "github.com/hashicorp/terraform-provider-google/google/tpgdclresource"
+	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
+	"time"
+)
+
+func NewDCLContainerAzureClient(config *transport_tpg.Config, userAgent, billingProject string, timeout time.Duration) *Client {
+	configOptions := []dcl.ConfigOption{
+		dcl.WithHTTPClient(config.Client),
+		dcl.WithUserAgent(userAgent),
+		dcl.WithLogger(dcl.DCLLogger{}),
+		dcl.WithBasePath(config.ContainerAzureBasePath),
+	}
+
+	if timeout != 0 {
+		configOptions = append(configOptions, dcl.WithTimeout(timeout))
+	}
+
+	if config.UserProjectOverride {
+		configOptions = append(configOptions, dcl.WithUserProjectOverride())
+		if billingProject != "" {
+			configOptions = append(configOptions, dcl.WithBillingProject(billingProject))
+		}
+	}
+
+	dclConfig := dcl.NewConfig(configOptions...)
+	return NewClient(dclConfig)
+}
