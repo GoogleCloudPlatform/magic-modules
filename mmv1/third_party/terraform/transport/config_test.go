82fd9089ebb3cdc48c389c3e4a09895ab89dfa10
Sam Levenick <slevenick@google.com>
2026-04-07 16:17:49 -0700

    Add tests, err propagaation

--- mmv1/third_party/terraform/transport/config_test.go
+++ mmv1/third_party/terraform/transport/config_test.go
@@ -557,7 +557,7 @@ func TestResourceBasePathDefault(t *testing.T) {
 	for tn, tc := range cases {
 		t.Run(tn, func(t *testing.T) {
 
-			basePath := transport_tpg.ResourceBasePath(tc.BasePath, tc.RepPath, tc.BasePathKey, tc.Config, tc.Location)
+			basePath, _ := transport_tpg.ResourceBasePath(tc.BasePath, tc.RepPath, tc.BasePathKey, tc.Config, tc.Location)
 
 			if basePath != tc.ExpectedOutput {
 				t.Fatalf("want %s,  got %s", tc.ExpectedOutput, basePath)
@@ -602,7 +602,52 @@ func TestResourceBasePathPreferGlobal(t *testing.T) {
 	for tn, tc := range cases {
 		t.Run(tn, func(t *testing.T) {
 
-			basePath := transport_tpg.ResourceBasePath(tc.BasePath, tc.RepPath, tc.BasePathKey, tc.Config, tc.Location)
+			basePath, _ := transport_tpg.ResourceBasePath(tc.BasePath, tc.RepPath, tc.BasePathKey, tc.Config, tc.Location)
+
+			if basePath != tc.ExpectedOutput {
+				t.Fatalf("want %s,  got %s", tc.ExpectedOutput, basePath)
+			}
+		})
+	}
+}
+
+func TestResourceBasePathPreferRegional(t *testing.T) {
+	config := &transport_tpg.Config{
+		Credentials:             transport_tpg.TestFakeCredentialsPath,
+		Project:                 "my-gce-project",
+		Region:                  "us-central1",
+		PreferRegionalEndpoints: true,
+	}
+	cases := map[string]struct {
+		BasePath       string
+		RepPath        string
+		BasePathKey    string
+		Config         *transport_tpg.Config
+		Location       string
+		ExpectedOutput string
+	}{
+		"Default to regional path": {
+			BasePath:       "https://clouddeploy.googleapis.com/v1/",
+			RepPath:        "https://www.clouddeploy.{{location}}.rep.googleapis.com/v1/",
+			BasePathKey:    "Clouddeploy",
+			Config:         config,
+			Location:       "us-central1",
+			ExpectedOutput: "https://www.clouddeploy.us-central1.rep.googleapis.com/v1/",
+		},
+		"Overridden path takes priority": {
+			BasePath:       "https://override.{{location}}.googleapis.com/v1/",
+			RepPath:        "https://www.clouddeploy.{{location}}.rep.googleapis.com/v1/",
+			BasePathKey:    "Clouddeploy",
+			Config:         config,
+			Location:       "us-central1",
+			ExpectedOutput: "https://override.us-central1.googleapis.com/v1/",
+		},
+	}
+
+	for tn, tc := range cases {
+		t.Run(tn, func(t *testing.T) {
+
+			basePath, _ := transport_tpg.ResourceBasePath(tc.BasePath, tc.RepPath, tc.BasePathKey, tc.Config, tc.Location)
 
 			if basePath != tc.ExpectedOutput {
 				t.Fatalf("want %s,  got %s", tc.ExpectedOutput, basePath)
