266a30706b92a2e40a28bf26984e5980c6e85f75
Stephen Lewis <stephenrlewis@google.com>
2026-04-07 16:54:47 -0700

    Added mtls tests for ListProducts

--- mmv1/third_party/terraform/provider/mtls_util_test.go
+++ mmv1/third_party/terraform/provider/mtls_util_test.go
@@ -4,11 +4,18 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/hashicorp/terraform-provider-google/google/registry"
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
 )
 
 func TestUnitMtls_urlSwitching(t *testing.T) {
 	t.Parallel()
+	for _, p := range registry.ListProducts() {
+		url = getMtlsEndpoint(p.BaseUrl)
+		if !strings.Contains(url, ".mtls.") {
+			t.Errorf("%s: mtls conversion unsuccessful preconv - %s postconv - %s", key, bp, url)
+		}
+	}
 	for key, bp := range transport_tpg.DefaultBasePaths {
 		url := getMtlsEndpoint(bp)
 		if !strings.Contains(url, ".mtls.") {
