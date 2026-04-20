5d3a27aabd0f4d47c690afd81809ee9829cc8710
malhotrasagar2212 <sagar.malhotra@hashicorp.com>
2026-04-20 13:34:05 -0400

    Register list resource in framework

--- mmv1/third_party/terraform/fwprovider/framework_provider_mmv1_resources.go
+++ mmv1/third_party/terraform/fwprovider/framework_provider_mmv1_resources.go
@@ -4,6 +4,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/list"
 
 	"github.com/hashicorp/terraform-provider-google/google/services/resourcemanager"
+	"github.com/hashicorp/terraform-provider-google/services/dns"
 )
 
 func listResourceFunc(lr list.ListResource) func() list.ListResource {
@@ -17,4 +18,5 @@ var generatedListResources = []func() list.ListResource{}
 var handwrittenListResources = []func() list.ListResource{
 	listResourceFunc(resourcemanager.NewGoogleServiceAccountListResource()),
 	listResourceFunc(resourcemanager.NewGoogleProjectServiceListResource()),
+	listResourceFunc(dns.NewGoogleDnsRecordSetListResource()),
 }
