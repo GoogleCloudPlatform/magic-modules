3e46864c46250baa4191f0323b2126aaba53efec
alvarez.mauriciotm@gmail.com <alvarez.mauriciotm@gmail.com>
2026-04-23 13:54:25 -0700

    refactors in templates

--- mmv1/provider/terraform.go
+++ mmv1/provider/terraform.go
@@ -162,6 +162,9 @@ func (t *Terraform) GenerateResource(object api.Resource, templateData TemplateD
 			if object.ExcludeIdentityGeneration {
 				log.Fatalf("generate_list_resource requires identity support; remove exclude_identity_generation from resource %q or disable generate_list_resource", object.Name)
 			}
+			if object.ExcludeRead {
+				log.Fatalf("generate_list_resource requires read support; remove exclude_read from resource %q or disable generate_list_resource", object.Name)
+			}
 			targetFilePath := path.Join(targetFolder, fmt.Sprintf("list_resource_%s.go", t.ResourceGoFilename(object)))
 			templateData.GenerateFile(targetFilePath, "templates/terraform/list_resource.go.tmpl", object, true,
 				"templates/terraform/list_resource.go.tmpl",
