b500f6932641071bead2f9203b129acf0d5ffea3
Shuya Ma <shuyama@google.com>
2026-04-23 10:59:48 -0700

    Revert "Fixed compilation error for google_billing_project_info tests (#17268)"
    This reverts commit 741419368d010380e08823dcd9bae9be783e2412.

--- mmv1/third_party/terraform/services/corebilling/resource_google_billing_project_info_test.go
+++ mmv1/third_party/terraform/services/corebilling/resource_google_billing_project_info_test.go
@@ -1,4 +1,4 @@
-package cloudbilling_test
+package corebilling_test
 
 import (
 	"fmt"
@@ -20,7 +20,7 @@ func TestAccBillingProjectInfo_update(t *testing.T) {
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckCloudBillingProjectInfoDestroyProducer(t),
+		CheckDestroy:             testAccCheckCoreBillingProjectInfoDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccBillingProjectInfo_basic(projectId, orgId, billingAccount),
