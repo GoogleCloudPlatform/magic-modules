0a051ac978a3bda8ffd9af490e53605f618f0655
Stephen Lewis <stephenrlewis@google.com>
2026-04-27 13:49:13 -0700

    Moved healthcare and storagetransfer clients to service packages

--- mmv1/third_party/terraform/services/storagetransfer/resource_storage_transfer_job.go
+++ mmv1/third_party/terraform/services/storagetransfer/resource_storage_transfer_job.go
@@ -1021,7 +1021,7 @@ func resourceStorageTransferJobCreate(d *schema.ResourceData, meta interface{}) 
 
 	err = transport_tpg.Retry(transport_tpg.RetryOptions{
 		RetryFunc: func() error {
-			createCall := config.NewStorageTransferClient(userAgent).TransferJobs.Create(transferJob)
+			createCall := NewClient(config, userAgent).TransferJobs.Create(transferJob)
 			if config.UserProjectOverride {
 				createCall.Header().Add("X-Goog-User-Project", billingProject)
 			}
@@ -1064,7 +1064,7 @@ func resourceStorageTransferJobRead(d *schema.ResourceData, meta interface{}) er
 	}
 
 	name := d.Get("name").(string)
-	readCall := config.NewStorageTransferClient(userAgent).TransferJobs.Get(name, project)
+	readCall := NewClient(config, userAgent).TransferJobs.Get(name, project)
 	if config.UserProjectOverride {
 		readCall.Header().Add("X-Goog-User-Project", billingProject)
 	}
@@ -1233,7 +1233,7 @@ func resourceStorageTransferJobUpdate(d *schema.ResourceData, meta interface{}) 
 		billingProject = bp
 	}
 
-	updateCall := config.NewStorageTransferClient(userAgent).TransferJobs.Patch(d.Get("name").(string), updateRequest)
+	updateCall := NewClient(config, userAgent).TransferJobs.Patch(d.Get("name").(string), updateRequest)
 	if config.UserProjectOverride {
 		updateCall.Header().Add("X-Goog-User-Project", billingProject)
 	}
@@ -1284,7 +1284,7 @@ func resourceStorageTransferJobDelete(d *schema.ResourceData, meta interface{}) 
 	// Update transfer job with status set to DELETE
 	log.Printf("[DEBUG] Setting status to DELETE for: %v\n\n", transferJobName)
 	err = retry.Retry(1*time.Minute, func() *retry.RetryError {
-		deleteCall := config.NewStorageTransferClient(userAgent).TransferJobs.Patch(transferJobName, updateRequest)
+		deleteCall := NewClient(config, userAgent).TransferJobs.Patch(transferJobName, updateRequest)
 		if config.UserProjectOverride {
 			deleteCall.Header().Add("X-Goog-User-Project", billingProject)
 		}
