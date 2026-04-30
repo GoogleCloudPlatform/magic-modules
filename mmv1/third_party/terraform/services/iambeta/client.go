e6fd2b4a4a23ac80a5f517b1e5cccd2af4706a9b
Stephen Lewis <stephenrlewis@google.com>
2026-04-30 13:36:09 -0700

    Moved clients for iambeta, iamcredentials, serviceusage, cloudbilling, cloudfunctions, bigquery, cloudidentity, and cloudrunv2

---
+++ mmv1/third_party/terraform/services/iambeta/client.go
@@ -0,0 +1,24 @@
+package iambeta
+
+import (
+	"log"
+
+	"google.golang.org/api/iam/v1"
+	"google.golang.org/api/option"
+
+	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
+)
+
+func NewClient(c *transport_tpg.Config, userAgent string) *iam.Service {
+	iamClientBasePath := transport_tpg.RemoveBasePathVersion(c.IAMBetaBasePath)
+	log.Printf("[INFO] Instantiating Google Cloud IAM client for path %s", iamClientBasePath)
+	clientIAM, err := iam.NewService(c.Context, option.WithHTTPClient(c.Client))
+	if err != nil {
+		log.Printf("[WARN] Error creating client iam: %s", err)
+		return nil
+	}
+	clientIAM.UserAgent = userAgent
+	clientIAM.BasePath = iamClientBasePath
+
+	return clientIAM
+}
