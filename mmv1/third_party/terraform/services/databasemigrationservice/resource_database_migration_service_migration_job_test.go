dff8fcd4c5d7baf010d260fdc3269ce4ae480714
Anna Levadnaia <alevadnaia@google.com>
2026-05-07 20:52:25 +0000

    databasemigrationservice: implement state handling and validation skip for MigrationJob

--- mmv1/third_party/terraform/services/databasemigrationservice/resource_database_migration_service_migration_job_test.go
+++ mmv1/third_party/terraform/services/databasemigrationservice/resource_database_migration_service_migration_job_test.go
@@ -27,7 +27,7 @@ func TestAccDatabaseMigrationServiceMigrationJob_update(t *testing.T) {
 				ResourceName:            "google_database_migration_service_migration_job.mysqltomysql",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"labels", "location", "migration_job_id", "terraform_labels"},
+				ImportStateVerifyIgnore: []string{"labels", "location", "migration_job_id", "terraform_labels", "state", "create_without_validation"},
 			},
 			{
 				Config: testAccDatabaseMigrationServiceMigrationJob_update(context),
@@ -36,7 +36,7 @@ func TestAccDatabaseMigrationServiceMigrationJob_update(t *testing.T) {
 				ResourceName:            "google_database_migration_service_migration_job.mysqltomysql",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"labels", "location", "migration_job_id", "terraform_labels"},
+				ImportStateVerifyIgnore: []string{"labels", "location", "migration_job_id", "terraform_labels", "state", "create_without_validation"},
 			},
 		},
 	})
@@ -143,9 +143,10 @@ resource "google_database_migration_service_migration_job" "mysqltomysql" {
       value = "1073741824"
     }
   }
-  source          = google_database_migration_service_connection_profile.source_cp.name
   destination     = google_database_migration_service_connection_profile.destination_cp.name
   type            = "CONTINUOUS"
+
+  create_without_validation = true
 }
 `, context)
 }
@@ -253,6 +254,8 @@ resource "google_database_migration_service_migration_job" "mysqltomysql" {
   source          = google_database_migration_service_connection_profile.source_cp.name
   destination     = google_database_migration_service_connection_profile.destination_cp.name
   type            = "ONE_TIME"
+
+  create_without_validation = true
 }
 `, context)
 }
