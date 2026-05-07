a8d54202a2697d60d282a23b7ca36ffaa73eaa6e
Igor Nadenenko <nadenenko@google.com>
2026-05-07 21:06:46 +0200

    sort imports

--- mmv1/third_party/terraform/services/compute/resource_compute_target_https_proxy_test.go
+++ mmv1/third_party/terraform/services/compute/resource_compute_target_https_proxy_test.go
@@ -2,16 +2,15 @@ package compute_test
 
 import (
 	"fmt"
-	"github.com/hashicorp/terraform-provider-google/google/acctest"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
 	_ "github.com/hashicorp/terraform-provider-google/google/services/certificatemanager"
 	_ "github.com/hashicorp/terraform-provider-google/google/services/networksecurity"
 	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
-
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
 const (
