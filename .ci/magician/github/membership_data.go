70c4468a75ff16e5ed7c6dc971d63bf1bf24b333
Ryan Oaks <ryanoaks@google.com>
2026-05-07 14:50:40 -0400

    Add roaks3 vacation

--- .ci/magician/github/membership_data.go
+++ .ci/magician/github/membership_data.go
@@ -130,12 +130,8 @@ var (
 		"roaks3": {
 			vacations: []Vacation{
 				{
-					startDate: newDate(2025, 8, 1),
-					endDate:   newDate(2025, 8, 11),
-				},
-				{
-					startDate: newDate(2025, 9, 23),
-					endDate:   newDate(2025, 9, 29),
+					startDate: newDate(2026, 5, 7),
+					endDate:   newDate(2026, 5, 11),
 				},
 			},
 		},
