3b57a7115c59c4b328e5de7ff65035089bdda222
Cameron Thornton <camthornton@google.com>
2026-05-14 11:30:41 -0500

    Revert "Add support for sending AIP-155 requestId query parameter (#17259)"
    This reverts commit 5c15fa5d9899417801e8866ad8845a2a01eb505e.

--- mmv1/api/resource.go
+++ mmv1/api/resource.go
@@ -128,10 +128,6 @@ type Resource struct {
 	// updates. This is typical of newer GCP APIs.
 	UpdateMask bool `yaml:"update_mask,omitempty"`
 
-	// [Optional] If set to true, this resource supports requestId for idempotency.
-	// This is typical of newer GCP APIs complying with AIP-155.
-	SupportsRequestId bool `yaml:"supports_request_id,omitempty"`
-
 	// [Optional] The HTTP verb used during create. Defaults to POST.
 	CreateVerb string `yaml:"create_verb,omitempty"`
 
@@ -819,14 +815,6 @@ func (r Resource) GetAsync() *Async {
 	}
 
 	return r.ProductMetadata.Async
-}
-
-// Returns true if the resource supports requestId for idempotency.
-func (r Resource) HasSupportsRequestId() bool {
-	if r.SupportsRequestId {
-		return true
-	}
-	return false
 }
 
 // Return the resource-specific identity properties, or a best guess of the
