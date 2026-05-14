86773c9e76d9288849fbaba6285a26856e5031fd
Stephen Lewis <stephenrlewis@google.com>
2026-05-14 16:01:44 -0700

    Force compute run and also show the message for >5 tests (which should be exceeded for compute)

--- mmv1/third_party/terraform/services/compute/data_source_google_compute_disk.go
+++ mmv1/third_party/terraform/services/compute/data_source_google_compute_disk.go
@@ -9,6 +9,7 @@ import (
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
 )
 
+// FORCE compute VCR run
 func DataSourceGoogleComputeDisk() *schema.Resource {
 
 	dsSchema := tpgresource.DatasourceSchemaFromResourceSchema(ResourceComputeDisk().Schema)
