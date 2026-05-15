b7128eb17c1d77e8ffdb09a94798f0e62f60dba5
Stephen Lewis (Burrows) <stephenrlewis@google.com>
2026-05-15 08:14:21 -0700

    Moved remaining bootstrap functions to service packages (#17550)

--- mmv1/third_party/terraform/services/dataproc/resource_dataproc_cluster_test.go
+++ mmv1/third_party/terraform/services/dataproc/resource_dataproc_cluster_test.go
@@ -18,6 +18,7 @@ import (
 	tpgcompute "github.com/hashicorp/terraform-provider-google/google/services/compute"
 	dataproc_tpg "github.com/hashicorp/terraform-provider-google/google/services/dataproc"
 	"github.com/hashicorp/terraform-provider-google/google/services/kms"
+	"github.com/hashicorp/terraform-provider-google/google/services/resourcemanager"
 	"github.com/hashicorp/terraform-provider-google/google/services/storage"
 	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
@@ -367,7 +368,7 @@ func TestAccDataprocCluster_withResourceManagerTags(t *testing.T) {
 	subnetworkName := tpgcompute.BootstrapSubnet(t, "dataproc-cluster", networkName)
 	BootstrapFirewallForDataprocSharedNetwork(t, "dataproc-cluster", networkName)
 	// TODO: remove this IAM binding once tagUser permissions are present in Dataproc Service Agent role.
-	acctest.BootstrapIamMembers(t, []acctest.IamMember{
+	resourcemanager.BootstrapIamMembers(t, []resourcemanager.IamMember{
 		{
 			Member: fmt.Sprintf("serviceAccount:service-%s@dataproc-accounts.iam.gserviceaccount.com", projectNumber),
 			Role:   "roles/resourcemanager.tagUser",
@@ -1250,7 +1251,7 @@ func TestAccDataprocCluster_KMS(t *testing.T) {
 	subnetworkName := tpgcompute.BootstrapSubnet(t, "dataproc-cluster", networkName)
 	BootstrapFirewallForDataprocSharedNetwork(t, "dataproc-cluster", networkName)
 
-	acctest.BootstrapIamMembers(t, []acctest.IamMember{
+	resourcemanager.BootstrapIamMembers(t, []resourcemanager.IamMember{
 		{
 			Member: "serviceAccount:service-{project_number}@compute-system.iam.gserviceaccount.com",
 			Role:   "roles/cloudkms.cryptoKeyEncrypterDecrypter",
