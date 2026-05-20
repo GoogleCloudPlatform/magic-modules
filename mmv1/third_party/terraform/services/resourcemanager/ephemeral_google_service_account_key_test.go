ef7da35ef9ff24b63a93e379339363b189d7c40a
Mauricio Alvarez Leon <65101411+BBBmau@users.noreply.github.com>
2026-05-20 13:26:34 -0700

    convert all ephemeral resource tests to use acctest.VcrTest (#17651)

--- mmv1/third_party/terraform/services/resourcemanager/ephemeral_google_service_account_key_test.go
+++ mmv1/third_party/terraform/services/resourcemanager/ephemeral_google_service_account_key_test.go
@@ -19,7 +19,7 @@ func TestAccEphemeralServiceAccountKey_create(t *testing.T) {
 	project := envvar.GetTestProjectFromEnv()
 	expectedServiceAccountEmail := fmt.Sprintf("%s@%s.iam.gserviceaccount.com", accountID, project)
 
-	resource.Test(t, resource.TestCase{
+	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories(t),
@@ -71,7 +71,7 @@ func TestAccEphemeralServiceAccountKey_upload(t *testing.T) {
 	project := envvar.GetTestProjectFromEnv()
 	expectedServiceAccountEmail := fmt.Sprintf("%s@%s.iam.gserviceaccount.com", accountID, project)
 
-	resource.Test(t, resource.TestCase{
+	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories(t),
@@ -130,7 +130,7 @@ func TestAccEphemeralServiceAccountKey_fetch(t *testing.T) {
 
 	serviceAccount := envvar.GetTestServiceAccountFromEnv(t)
 	targetServiceAccountEmail := iambeta.BootstrapServiceAccount(t, "key-basic", serviceAccount)
-	resource.Test(t, resource.TestCase{
+	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories(t),
