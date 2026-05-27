58b15922e13db5a81ea29bb43e0824c262908250
gurusai-voleti <gvoleti@google.com>
2026-05-27 04:32:27 +0000

    update

--- mmv1/third_party/terraform/services/storagecontrol/data_source_storage_control_folder_intelligence_findings_summary_test.go
+++ mmv1/third_party/terraform/services/storagecontrol/data_source_storage_control_folder_intelligence_findings_summary_test.go
@@ -6,13 +6,16 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 	"github.com/hashicorp/terraform-provider-google/google/envvar"
+	_ "github.com/hashicorp/terraform-provider-google/google/services/resourcemanager"
+	_ "github.com/hashicorp/terraform-provider-google/google/services/storagecontrol"
 )
 
 func TestAccDataSourceGoogleStorageControlFolderIntelligenceFindingsSummary_empty(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"org_id": envvar.GetTestOrgFromEnv(t),
+		"org_id":        envvar.GetTestOrgFromEnv(t),
+		"random_suffix": acctest.RandString(t, 10),
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
@@ -31,9 +34,15 @@ func TestAccDataSourceGoogleStorageControlFolderIntelligenceFindingsSummary_empt
 
 func testAccDataSourceGoogleStorageControlFolderIntelligenceFindingsSummary_empty(context map[string]interface{}) string {
 	return acctest.Nprintf(`
+resource "google_folder" "folder" {
+  parent              = "organizations/%{org_id}"
+  display_name        = "tf-test-folder-name%{random_suffix}"
+  deletion_protection = false
+}
 
 data "google_storage_control_folder_intelligence_findings_summary" "empty" {
-  folder = "%{org_id}"
+  folder     = google_folder.folder.folder_id
+  depends_on = [google_folder.folder]
 }
 `, context)
 }
