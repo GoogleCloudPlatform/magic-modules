ec81b593b10a73778eebeb76e42e7dda98530413
Stephen Lewis <stephenrlewis@google.com>
2026-06-01 14:16:19 -0700

    Removed additional references to examples

--- mmv1/provider/terraform_tgc_next.go
+++ mmv1/provider/terraform_tgc_next.go
@@ -348,25 +348,9 @@ func (tgc TerraformGoogleConversionNext) replaceImportPath(outputFolder, target 
 	}
 }
 
-func (tgc TerraformGoogleConversionNext) addTestsFromExamples(object *api.Resource) {
-	for _, example := range object.Examples {
-		if example.ExcludeTest {
-			continue
-		}
-		if object.ProductMetadata.VersionObjOrClosest(tgc.Product.Version.Name).CompareTo(object.ProductMetadata.VersionObjOrClosest(example.MinVersion)) < 0 {
-			continue
-		}
-		object.TGCTests = append(object.TGCTests, resource.TGCTest{
-			Name: "TestAcc" + example.TestSlug(object.ProductMetadata.Name, object.Name),
-			Skip: example.TGCSkipTest,
-		})
-	}
-}
-
 func (tgc TerraformGoogleConversionNext) addTestsFromSamples(object *api.Resource) {
 	if object.Examples != nil {
-		tgc.addTestsFromExamples(object)
-		return
+		log.Fatalf("Examples block exists in %v", object.Name)
 	}
 	for _, sample := range object.Samples {
 		if sample.ExcludeTest {
