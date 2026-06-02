d9a124ec0406dd09b59c10c740e87a97302a7769
NickASC4 <saffron195@gmail.com>
2026-06-02 09:43:52 -0400

    Update mmv1/third_party/terraform/services/bigtable/resource_bigtable_table_test.go
    Co-authored-by: Zhenhua Li <zhenhuali@google.com>

--- mmv1/third_party/terraform/services/bigtable/resource_bigtable_table_test.go
+++ mmv1/third_party/terraform/services/bigtable/resource_bigtable_table_test.go
@@ -1234,9 +1234,9 @@ resource "google_bigtable_table" "table" {
   name          = "%s"
   instance_name = google_bigtable_instance.instance.name
   automated_backup_policy {
-    %s
-    %s
-    %s
+    retention_period = "%s"
+    frequency = "%s"
+    locations = []
   }
   column_family {
     family = "%s"
