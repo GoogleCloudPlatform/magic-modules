0817ee167b65c77c1fe34d75bfdb971028b4975e
gurusai-voleti <gvoleti@google.com>
2026-06-03 05:15:52 +0000

    seperate test cases for finding

--- mmv1/third_party/terraform/services/storagecontrol/data_source_storage_control_project_intelligence_findings_test.go
+++ mmv1/third_party/terraform/services/storagecontrol/data_source_storage_control_project_intelligence_findings_test.go
@@ -1,7 +1,6 @@
 package storagecontrol_test
 
 import (
-	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -25,32 +24,9 @@ func TestAccDataSourceGoogleStorageControlProjectIntelligenceFindings_empty(t *t
 	})
 }
 
-func TestAccDataSourceGoogleStorageControlProjectIntelligenceFinding_notFound(t *testing.T) {
-	t.Parallel()
-
-	acctest.VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		Steps: []resource.TestStep{
-			{
-				Config:      testAccDataSourceGoogleStorageControlProjectIntelligenceFinding_notFound(),
-				ExpectError: regexp.MustCompile(".*not found.*"),
-			},
-		},
-	})
-}
-
 func testAccDataSourceGoogleStorageControlProjectIntelligenceFindings_empty() string {
 	return `
 data "google_storage_control_project_intelligence_findings" "empty" {
-}
-`
-}
-
-func testAccDataSourceGoogleStorageControlProjectIntelligenceFinding_notFound() string {
-	return `
-data "google_storage_control_project_intelligence_finding" "not_found" {
-  finding_id = "nonexistent-finding-id"
 }
 `
 }
