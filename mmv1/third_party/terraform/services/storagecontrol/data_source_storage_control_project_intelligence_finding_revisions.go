fd82b1ce6c0b38d55cc747872cc7c7af9a1e5b11
gurusai-voleti <gvoleti@google.com>
2026-06-08 04:09:02 +0000

    update

--- mmv1/third_party/terraform/services/storagecontrol/data_source_storage_control_project_intelligence_finding_revisions.go
+++ mmv1/third_party/terraform/services/storagecontrol/data_source_storage_control_project_intelligence_finding_revisions.go
@@ -43,124 +43,7 @@ func DataSourceGoogleStorageControlProjectIntelligenceFindingRevisions() *schema
 				Computed:    true,
 				Description: `The list of intelligence finding revisions.`,
 				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"name": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: `The resource name of the finding revision.`,
-						},
-						"create_time": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: `The time when the finding revision was created.`,
-						},
-						"snapshot": {
-							Type:        schema.TypeList,
-							Computed:    true,
-							Description: `The snapshot of the finding at revision creation time.`,
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"name": {
-										Type:        schema.TypeString,
-										Computed:    true,
-										Description: `The resource name of the finding.`,
-									},
-									"description": {
-										Type:        schema.TypeString,
-										Computed:    true,
-										Description: `A short description of the finding.`,
-									},
-									"type": {
-										Type:        schema.TypeString,
-										Computed:    true,
-										Description: `The type of this finding.`,
-									},
-									"category": {
-										Type:        schema.TypeString,
-										Computed:    true,
-										Description: `The category of the finding.`,
-									},
-									"severity": {
-										Type:        schema.TypeString,
-										Computed:    true,
-										Description: `The severity of the finding.`,
-									},
-									"create_time": {
-										Type:        schema.TypeString,
-										Computed:    true,
-										Description: `The time when the finding was created.`,
-									},
-									"update_time": {
-										Type:        schema.TypeString,
-										Computed:    true,
-										Description: `The time when the finding was last updated.`,
-									},
-									"target_resource": {
-										Type:        schema.TypeString,
-										Computed:    true,
-										Description: `The fully qualified resource name of the resource that this IntelligenceFinding applies to.`,
-									},
-									"associated_resources": {
-										Type:        schema.TypeList,
-										Computed:    true,
-										Description: `Google Cloud resource names that are relevant to the IntelligenceFinding. This list also includes the targetResource.`,
-										Elem: &schema.Schema{
-											Type: schema.TypeString,
-										},
-									},
-									"observation_period": {
-										Type:        schema.TypeList,
-										Computed:    true,
-										Description: `The time interval from which the underlying data generated this IntelligenceFinding was observed.`,
-										Elem: &schema.Resource{
-											Schema: map[string]*schema.Schema{
-												"start_time": {
-													Type:     schema.TypeString,
-													Computed: true,
-												},
-												"end_time": {
-													Type:     schema.TypeString,
-													Computed: true,
-												},
-											},
-										},
-									},
-									"coldline_and_archival_storage_operations_spike": {
-										Type:        schema.TypeList,
-										Computed:    true,
-										Description: "A finding about a spike in Class A or Class B operations on Coldline or Archive Cloud Storage objects.",
-										Elem: &schema.Resource{
-											Schema: storageControlColdlineSpikeSchema(),
-										},
-									},
-									"throttled_requests_spike": {
-										Type:        schema.TypeList,
-										Computed:    true,
-										Description: "A finding about a spike in throttled requests (429 errors) within a project.",
-										Elem: &schema.Resource{
-											Schema: storageControlThrottledRequestsSpikeSchema(),
-										},
-									},
-									"cross_region_egress_spike": {
-										Type:        schema.TypeList,
-										Computed:    true,
-										Description: "A finding about a spike in cross-region egress from Cloud Storage.",
-										Elem: &schema.Resource{
-											Schema: storageControlCrossRegionEgressSpikeSchema(),
-										},
-									},
-									"storage_growth_above_trend": {
-										Type:        schema.TypeList,
-										Computed:    true,
-										Description: "A finding about a spike in storage growth (bytes or object count) that is outside the normal historical trend.",
-										Elem: &schema.Resource{
-											Schema: storageControlStorageGrowthSpikeSchema(),
-										},
-									},
-								},
-							},
-						},
-					},
+					Schema: storageControlProjectIntelligenceFindingRevisionSchema(),
 				},
 			},
 		},
@@ -297,6 +180,127 @@ func init() {
 		Type:        registry.SchemaTypeDataSource,
 		Schema:      DataSourceGoogleStorageControlProjectIntelligenceFindingRevisions(),
 	}.Register()
+}
+
+func storageControlProjectIntelligenceFindingRevisionSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"name": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: `The resource name of the finding revision.`,
+		},
+		"create_time": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: `The time when the finding revision was created.`,
+		},
+		"snapshot": {
+			Type:        schema.TypeList,
+			Computed:    true,
+			Description: `The snapshot of the finding at revision creation time.`,
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					"name": {
+						Type:        schema.TypeString,
+						Computed:    true,
+						Description: `The resource name of the finding.`,
+					},
+					"description": {
+						Type:        schema.TypeString,
+						Computed:    true,
+						Description: `A short description of the finding.`,
+					},
+					"type": {
+						Type:        schema.TypeString,
+						Computed:    true,
+						Description: `The type of this finding.`,
+					},
+					"category": {
+						Type:        schema.TypeString,
+						Computed:    true,
+						Description: `The category of the finding.`,
+					},
+					"severity": {
+						Type:        schema.TypeString,
+						Computed:    true,
+						Description: `The severity of the finding.`,
+					},
+					"create_time": {
+						Type:        schema.TypeString,
+						Computed:    true,
+						Description: `The time when the finding was created.`,
+					},
+					"update_time": {
+						Type:        schema.TypeString,
+						Computed:    true,
+						Description: `The time when the finding was last updated.`,
+					},
+					"target_resource": {
+						Type:        schema.TypeString,
+						Computed:    true,
+						Description: `The fully qualified resource name of the resource that this IntelligenceFinding applies to.`,
+					},
+					"associated_resources": {
+						Type:        schema.TypeList,
+						Computed:    true,
+						Description: `Google Cloud resource names that are relevant to the IntelligenceFinding. This list also includes the targetResource.`,
+						Elem: &schema.Schema{
+							Type: schema.TypeString,
+						},
+					},
+					"observation_period": {
+						Type:        schema.TypeList,
+						Computed:    true,
+						Description: `The time interval from which the underlying data generated this IntelligenceFinding was observed.`,
+						Elem: &schema.Resource{
+							Schema: map[string]*schema.Schema{
+								"start_time": {
+									Type:     schema.TypeString,
+									Computed: true,
+								},
+								"end_time": {
+									Type:     schema.TypeString,
+									Computed: true,
+								},
+							},
+						},
+					},
+					"coldline_and_archival_storage_operations_spike": {
+						Type:        schema.TypeList,
+						Computed:    true,
+						Description: "A finding about a spike in Class A or Class B operations on Coldline or Archive Cloud Storage objects.",
+						Elem: &schema.Resource{
+							Schema: storageControlColdlineSpikeSchema(),
+						},
+					},
+					"throttled_requests_spike": {
+						Type:        schema.TypeList,
+						Computed:    true,
+						Description: "A finding about a spike in throttled requests (429 errors) within a project.",
+						Elem: &schema.Resource{
+							Schema: storageControlThrottledRequestsSpikeSchema(),
+						},
+					},
+					"cross_region_egress_spike": {
+						Type:        schema.TypeList,
+						Computed:    true,
+						Description: "A finding about a spike in cross-region egress from Cloud Storage.",
+						Elem: &schema.Resource{
+							Schema: storageControlCrossRegionEgressSpikeSchema(),
+						},
+					},
+					"storage_growth_above_trend": {
+						Type:        schema.TypeList,
+						Computed:    true,
+						Description: "A finding about a spike in storage growth (bytes or object count) that is outside the normal historical trend.",
+						Elem: &schema.Resource{
+							Schema: storageControlStorageGrowthSpikeSchema(),
+						},
+					},
+				},
+			},
+		},
+	}
 }
 
 func storageControlColdlineSpikeSchema() map[string]*schema.Schema {
