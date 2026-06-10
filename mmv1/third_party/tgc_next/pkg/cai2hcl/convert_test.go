60f5786c7a7361964870b70e45b2f0f15dede731
Zhenhua Li <zhenhuali@google.com>
2026-06-10 10:33:24 -0700

    [TGC] Add TestConvertWithoutResourceName unit test

--- mmv1/third_party/tgc_next/pkg/cai2hcl/convert_test.go
+++ mmv1/third_party/tgc_next/pkg/cai2hcl/convert_test.go
@@ -43,3 +43,37 @@ func TestConvertWithResourceName(t *testing.T) {
 		t.Errorf("expected:\n%s\ngot:\n%s", expected, string(got))
 	}
 }
+
+func TestConvertWithoutResourceName(t *testing.T) {
+	assets := []caiasset.Asset{
+		{
+			Name: "//cloudresourcemanager.googleapis.com/projects/example-project",
+			Type: "cloudresourcemanager.googleapis.com/Project",
+			Resource: &caiasset.AssetResource{
+				Version:              "v1",
+				DiscoveryDocumentURI: "https://www.googleapis.com/discovery/v1/apis/compute/v1/rest",
+				DiscoveryName:        "Project",
+				Parent:               "//cloudresourcemanager.googleapis.com/folders/456",
+				Data: map[string]interface{}{
+					"name":      "My Project",
+					"projectId": "example-project",
+				},
+			},
+		},
+	}
+
+	got, err := converters.ConvertResource(assets, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `resource "google_project" "example-project" {
+  folder_id  = "456"
+  name       = "My Project"
+  project_id = "example-project"
+}
+`
+	if string(got) != expected {
+		t.Errorf("expected:\n%s\ngot:\n%s", expected, string(got))
+	}
+}
