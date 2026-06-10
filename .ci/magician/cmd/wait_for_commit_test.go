e459f61cf4ce76624bc7fd593d19c6df8d7113cb
Majed Alfaraj <malfaraj@google.com>
2026-06-10 16:25:34 -0700

    test(magician): refactor wait-for-commit to use testing sandbox
    Replace the use of the `mockRunner` with the `execRunner` in the testing sandbox. The test now creates a temporary git repo and simulates the commit timeline.

--- .ci/magician/cmd/wait_for_commit_test.go
+++ .ci/magician/cmd/wait_for_commit_test.go
@@ -1,162 +1,71 @@
 package cmd
 
 import (
-	"fmt"
 	"strings"
 	"testing"
-
-	"github.com/google/go-cmp/cmp"
 )
-
-type runResult struct {
-	out string
-	err error
-}
-type waitForCommitMockRunner struct {
-	mockRunner
-	runResults map[string][]runResult
-}
-
-func (mr *waitForCommitMockRunner) Run(name string, args []string, env map[string]string) (string, error) {
-	mr.calledMethods["Run"] = append(mr.calledMethods["Run"], ParameterList{mr.cwd, name, args, env})
-	cmd := fmt.Sprintf("%s %s %v %s", mr.cwd, name, args, sortedEnvString(env))
-	if result, ok := mr.runResults[cmd]; ok {
-		if result == nil {
-			return "", fmt.Errorf("no results")
-		}
-		headRet := result[0]
-		result = result[1:]
-		mr.runResults[cmd] = result
-		return headRet.out, headRet.err
-	}
-	return "", fmt.Errorf("unknown command %s\n", cmd)
-}
 
 func TestExecWaitForCommit(t *testing.T) {
 	testCases := []struct {
-		name          string
-		baseBranch    string
-		calledMethods []string
-		runResults    map[string][]runResult
+		name       string
+		baseBranch string
+		syncBranch string
 	}{
 		{
 			name:       "base branch is main",
 			baseBranch: "main",
-			calledMethods: []string{
-				"git merge-base --is-ancestor sha origin/sync-branch",
-				"git rev-parse --short origin/sync-branch",
-				"git rev-parse --short sha~",
-				"git fetch origin sync-branch",
-				"git rev-parse --short origin/sync-branch",
-				"git rev-parse --short sha~",
-			},
-			runResults: map[string][]runResult{
-				"cwd git [merge-base --is-ancestor sha origin/sync-branch] map[]": {
-					{
-						out: "",
-						err: fmt.Errorf("exit error 1"),
-					},
-				},
-				"cwd git [rev-parse --short origin/sync-branch] map[]": {
-					{
-						out: "sha-x",
-					},
-					{
-						out: "sha-z",
-					},
-				},
-				"cwd git [rev-parse --short sha~] map[]": {
-					{
-						out: "sha-y",
-					},
-					{
-						out: "sha-z",
-					},
-				},
-				"cwd git [fetch origin sync-branch] map[]": {
-					{
-						out: "",
-					},
-				},
-			},
+			syncBranch: "sync-branch",
 		},
 		{
 			name:       "base branch is not main",
 			baseBranch: "feature-branch",
-			calledMethods: []string{
-				"git merge-base --is-ancestor sha origin/sync-branch-feature-branch",
-				"git rev-parse --short origin/sync-branch-feature-branch",
-				"git rev-parse --short sha~",
-				"git fetch origin sync-branch-feature-branch",
-				"git rev-parse --short origin/sync-branch-feature-branch",
-				"git rev-parse --short sha~",
-			},
-			runResults: map[string][]runResult{
-				"cwd git [merge-base --is-ancestor sha origin/sync-branch-feature-branch] map[]": {
-					{
-						out: "",
-						err: fmt.Errorf("exit error 1"),
-					},
-				},
-				"cwd git [rev-parse --short origin/sync-branch-feature-branch] map[]": {
-					{
-						out: "sha-x",
-					},
-					{
-						out: "sha-z",
-					},
-				},
-				"cwd git [rev-parse --short sha~] map[]": {
-					{
-						out: "sha-y",
-					},
-					{
-						out: "sha-z",
-					},
-				},
-				"cwd git [fetch origin sync-branch-feature-branch] map[]": {
-					{
-						out: "",
-					},
-				},
-			},
+			syncBranch: "sync-branch-feature-branch",
+		},
+		{
+			name:       "already in sync branch",
+			baseBranch: "main",
+			syncBranch: "sync-branch",
 		},
 	}
 
-	for _, test := range testCases {
-		t.Run(test.name, func(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			sb := newSandbox(t)
+
+			originDir := t.TempDir()
+			sb.Runner.MustRun("git", []string{"init", "--bare", "-b", "main", originDir}, nil)
+			sb.Runner.MustRun("git", []string{"remote", "add", "origin", originDir}, nil)
+
+			sb.Runner.MustRun("git", []string{"commit", "--allow-empty", "-m", "Commit A"}, nil)
+			shaA := strings.TrimSpace(sb.Runner.MustRun("git", []string{"rev-parse", "HEAD"}, nil))
+
+			sb.Runner.MustRun("git", []string{"commit", "--allow-empty", "-m", "Commit B"}, nil)
+			shaB := strings.TrimSpace(sb.Runner.MustRun("git", []string{"rev-parse", "HEAD"}, nil))
+
+			sb.Runner.MustRun("git", []string{"commit", "--allow-empty", "-m", "Commit C"}, nil)
+			shaC := strings.TrimSpace(sb.Runner.MustRun("git", []string{"rev-parse", "HEAD"}, nil))
+
+			if tc.name == "already in sync branch" {
+				sb.Runner.MustRun("git", []string{"push", "origin", shaC + ":refs/heads/" + tc.syncBranch}, nil)
+			} else {
+				sb.Runner.MustRun("git", []string{"push", "origin", shaA + ":refs/heads/" + tc.syncBranch}, nil)
+			}
+
+			sb.Runner.MustRun("git", []string{"fetch", "origin"}, nil)
+
 			origWaitFunc := waitFunc
-			defer func() {
-				waitFunc = origWaitFunc
-			}()
-			waitFunc = func() {}
+			defer func() { waitFunc = origWaitFunc }()
 
-			runner := &waitForCommitMockRunner{
-				mockRunner: mockRunner{
-					cwd:           "cwd",
-					calledMethods: make(map[string][]ParameterList),
-				},
-				runResults: test.runResults,
-			}
-
-			err := execWaitForCommit("sync-branch", test.baseBranch, "sha", runner)
-			if err != nil {
-				t.Fatalf("execWaitForCommit = %s, want = nil", err)
-			}
-
-			got, ok := runner.Calls("Run")
-			if !ok {
-				t.Fatalf("execWaitForCommit() got no calls")
-			}
-			var want []ParameterList
-			for _, cmd := range test.calledMethods {
-				words := strings.Split(cmd, " ")
-				if len(words) > 0 {
-					want = append(want, []any{"cwd", words[0], words[1:], map[string]string(nil)})
+			waitFunc = func() {
+				if tc.name == "already in sync branch" {
+					t.Fatalf("waitFunc was called, but test should have exited early!")
 				}
+				sb.Runner.MustRun("git", []string{"push", "origin", shaB + ":refs/heads/" + tc.syncBranch}, nil)
 			}
-			if diff := cmp.Diff(want, got); diff != "" {
-				t.Fatalf("execWaitForCommit() executed commands diff = %s\n want = %+v, got = %+v", diff, want, got)
+
+			err := execWaitForCommit("sync-branch", tc.baseBranch, shaC, sb.Runner)
+			if err != nil {
+				t.Fatalf("execWaitForCommit() failed: %v", err)
 			}
 		})
 	}
