d9550d2f8e069a777f18a3001e772caec28c5bea
Umesh Kumhar <umesh.kumhar@us.mcd.com>
2026-06-17 20:49:05 +0530

    fix: gofmt coverage test

--- mmv1/third_party/terraform/services/vertexai/resource_vertex_ai_persistent_resource_coverage_test.go
+++ mmv1/third_party/terraform/services/vertexai/resource_vertex_ai_persistent_resource_coverage_test.go
@@ -3,8 +3,8 @@ package vertexai_test
 import (
 	"testing"
 
-	"github.com/hashicorp/terraform-provider-google-beta/google-beta/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-google-beta/google-beta/acctest"
 )
 
 func TestAccVertexAIPersistentResource_fullCoverage(t *testing.T) {
