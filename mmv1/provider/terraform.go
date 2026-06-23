147649c7852537556d6248cf7f659fc2d0656c7f
malhotrasagar2212 <sagar.malhotra@hashicorp.com>
2026-06-23 12:24:25 -0400

    Updated documentation template

--- mmv1/provider/terraform.go
+++ mmv1/provider/terraform.go
@@ -169,6 +169,12 @@ func (t *Terraform) GenerateResource(object api.Resource, templateData TemplateD
 			listDocFilePath := path.Join(listDocFolder, fmt.Sprintf("%s.html.markdown", object.TerraformName()))
 			templateData.GenerateListResourceDocumentationFile(listDocFilePath, object)
 		}
+
+		if object.IamPolicy != nil && object.IamPolicy.GenerateListResource {
+			listDocFolder := t.makeFolder(outputFolder, "website", "docs", "list-resources")
+			listDocFilePath := path.Join(listDocFolder, fmt.Sprintf("%s_iam_member.html.markdown", object.TerraformName()))
+			templateData.GenerateIamListResourceDocumentationFile(listDocFilePath, object)
+		}
 	}
 }
 
@@ -415,11 +421,6 @@ func (t *Terraform) GenerateIamPolicy(object api.Resource, templateData Template
 		// New: Iam member list resource (terraform query support)
 		t.GenerateIamListResource(object, templateData, targetFolder)
 
-		if generateDocs && object.IamPolicy != nil && object.IamPolicy.GenerateListResource {
-			listDocFolder := t.makeFolder(outputFolder, "website", "docs", "list-resources")
-			listDocFilePath := path.Join(listDocFolder, fmt.Sprintf("%s_iam_member.html.markdown", object.TerraformName()))
-			templateData.GenerateIamListResourceDocumentationFile(listDocFilePath, object)
-		}
 		// Only generate test if testable example configs exist.
 		samples := google.Reject(object.Samples, func(s *resource.Sample) bool {
 			return s.ExcludeTest
