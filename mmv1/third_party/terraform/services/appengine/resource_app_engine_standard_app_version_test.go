41bd98f5f7a2f868d154d1deda41084155e07f8b
alvarez.mauriciotm@gmail.com <alvarez.mauriciotm@gmail.com>
2026-06-24 14:06:03 -0700

    remove skip on update test

--- mmv1/third_party/terraform/services/appengine/resource_app_engine_standard_app_version_test.go
+++ mmv1/third_party/terraform/services/appengine/resource_app_engine_standard_app_version_test.go
@@ -13,7 +13,6 @@ import (
 )
 
 func TestAccAppEngineStandardAppVersion_update(t *testing.T) {
-	t.Skip("https://github.com/hashicorp/terraform-provider-google/issues/18936")
 	t.Parallel()
 
 	context := map[string]interface{}{
