3273563b4d8436f5c087e52c999426688727a0b0
malhotrasagar2212 <sagar.malhotra@hashicorp.com>
2026-06-25 22:27:29 -0400

    updated project to skip from iam list schema when absent

--- mmv1/third_party/terraform/tpgiamresource/resource_iam_member_list.go
+++ mmv1/third_party/terraform/tpgiamresource/resource_iam_member_list.go
@@ -92,11 +92,14 @@ func NewIamMemberListResource(typeName string, memberResource *schema.Resource, 
 
 	iamResourceSchema := make(map[string]*schema.Schema)
 	for _, field := range listConfigFields {
-		if field.Name == "role" || field.Name == "member" || field.Name == "project" {
+		if field.Name == "role" || field.Name == "member" {
 			continue
 		}
 		schemaField, ok := memberResource.Schema[field.Name]
 		if !ok {
+			if field.Name == "project" {
+				continue
+			}
 			panic(fmt.Sprintf("tpgiamresource: list config field %q not found in member resource schema", field.Name))
 		}
 
