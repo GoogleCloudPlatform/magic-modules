f3675a630a8d594018edd9e92e6449b46f8d2ad0
gurusai-voleti <gvoleti@google.com>
2026-07-03 12:34:28 +0000

    added new resource rapid_cache

---
+++ mmv1/third_party/terraform/services/storage/resource_storage_rapid_cache_test.go
@@ -0,0 +1,85 @@
+package storage_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	_ "github.com/hashicorp/terraform-provider-google/google/services/storage"
+)
+
+func TestAccStorageRapidCache_update(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"time": {},
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccStorageRapidCache_full(context),
+			},
+			{
+				ResourceName:            "google_storage_rapid_cache.cache",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"bucket", "update_time"},
+			},
+			{
+				Config: testAccStorageRapidCache_update(context),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("google_storage_rapid_cache.cache", plancheck.ResourceActionUpdate),
+					},
+				},
+			},
+			{
+				ResourceName:            "google_storage_rapid_cache.cache",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"bucket", "update_time"},
+			},
+		},
+	})
+}
+
+func testAccStorageRapidCache_full(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_storage_bucket" "bucket" {
+  name = "tf-test-bucket-name%{random_suffix}"
+  location = "US"
+  force_destroy = "true"
+}
+
+resource "google_storage_rapid_cache" "cache" {
+  bucket          = google_storage_bucket.bucket.name
+  zone            = "us-central1-f"
+  ttl             = "3601s"
+  ingest_on_write = false
+}
+`, context)
+}
+
+func testAccStorageRapidCache_update(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_storage_bucket" "bucket" {
+  name = "tf-test-bucket-name%{random_suffix}"
+  location = "US"
+  force_destroy = "true"
+}
+
+resource "google_storage_rapid_cache" "cache" {
+  bucket           = google_storage_bucket.bucket.name
+  zone             = "us-central1-f"
+  ttl              = "3620s"
+  ingest_on_write  = true
+}
+`, context)
+}
