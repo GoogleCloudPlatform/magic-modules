f6013f601968f7cdcd0c5ee19ba5cc556c242f2a
vr-ibm <vrau@ibm.com>
2026-07-09 16:19:53 -0500

    feat(resourcemanager): add list resource for google_service_account_key

--- mmv1/third_party/terraform/services/resourcemanager/resource_google_service_account_key.go
+++ mmv1/third_party/terraform/services/resourcemanager/resource_google_service_account_key.go
@@ -23,6 +23,22 @@ func ResourceGoogleServiceAccountKey() *schema.Resource {
 		Update: resourceGoogleServiceAccountKeyUpdate,
 		Delete: resourceGoogleServiceAccountKeyDelete,
 
+		Identity: &schema.ResourceIdentity{
+			Version: 1,
+			SchemaFunc: func() map[string]*schema.Schema {
+				return map[string]*schema.Schema{
+					"service_account_id": {
+						Type:              schema.TypeString,
+						RequiredForImport: true,
+					},
+					"name": {
+						Type:              schema.TypeString,
+						RequiredForImport: true,
+					},
+				}
+			},
+		},
+
 		CustomizeDiff: customdiff.All(
 			tpgresource.DefaultProviderDeletionPolicy("DELETE"),
 		),
@@ -142,6 +158,12 @@ func resourceGoogleServiceAccountKeyCreate(d *schema.ResourceData, meta interfac
 	}
 
 	d.SetId(sak.Name)
+	if err := tpgresource.SetResourceIdentityAttributes(d, map[string]interface{}{
+		"service_account_id": serviceAccountName,
+		"name":               sak.Name,
+	}); err != nil {
+		return err
+	}
 	// Data only available on create.
 	if err := d.Set("valid_after", sak.ValidAfterTime); err != nil {
 		return fmt.Errorf("Error setting valid_after: %s", err)
@@ -192,7 +214,14 @@ func resourceGoogleServiceAccountKeyRead(d *schema.ResourceData, meta interface{
 		}
 	}
 
-	return flattenServiceAccountKey(d, config, sak)
+	if err := flattenServiceAccountKey(d, config, sak); err != nil {
+		return err
+	}
+
+	return tpgresource.SetResourceIdentityAttributes(d, map[string]interface{}{
+		"service_account_id": d.Get("service_account_id").(string),
+		"name":               sak.Name,
+	})
 }
 
 func flattenServiceAccountKey(d *schema.ResourceData, config *transport_tpg.Config, sak *iam.ServiceAccountKey) error {
