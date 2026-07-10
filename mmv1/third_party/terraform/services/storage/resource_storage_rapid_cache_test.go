f06d389e18272003ddcc0021fe7323fcd19c09f4
gurusai-voleti <gvoleti@google.com>
2026-07-10 07:06:46 +0000

    added cache_type

--- mmv1/third_party/terraform/services/storage/resource_storage_rapid_cache_test.go
+++ mmv1/third_party/terraform/services/storage/resource_storage_rapid_cache_test.go
@@ -61,6 +61,7 @@ resource "google_storage_bucket" "bucket" {
 resource "google_storage_rapid_cache" "cache" {
   bucket          = google_storage_bucket.bucket.name
   zone            = "us-central1-f"
+  cache_type      = "rapid-cache"
   ttl             = "3601s"
   ingest_on_write = false
 }
@@ -78,8 +79,70 @@ resource "google_storage_bucket" "bucket" {
 resource "google_storage_rapid_cache" "cache" {
   bucket           = google_storage_bucket.bucket.name
   zone             = "us-central1-f"
+  admission_policy = "no_read_admission"
+  cache_type       = "rapid-cache"
   ttl              = "3620s"
   ingest_on_write  = true
+}
+`, context)
+}
+
+func TestAccStorageRapidCache_cacheType(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"time": {},
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccStorageRapidCache_full(context),
+			},
+			{
+				ResourceName:            "google_storage_rapid_cache.cache",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"bucket", "update_time", "cache_type"},
+			},
+			{
+				Config: testAccStorageRapidCache_cacheTypeUltra(context),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("google_storage_rapid_cache.cache", plancheck.ResourceActionReplace),
+					},
+				},
+			},
+			{
+				ResourceName:            "google_storage_rapid_cache.cache",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"bucket", "update_time", "cache_type"},
+			},
+		},
+	})
+}
+
+func testAccStorageRapidCache_cacheTypeUltra(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_storage_bucket" "bucket" {
+  name = "tf-test-bucket-name%{random_suffix}"
+  location = "US"
+  force_destroy = "true"
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_rapid_cache" "cache" {
+  bucket          = google_storage_bucket.bucket.name
+  zone            = "us-central1-a"
+  cache_type      = "rapid-cache-ultra"
+  ttl             = "3601s"
+  ingest_on_write = false
 }
 `, context)
 }
