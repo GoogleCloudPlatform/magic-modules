c7203e5112d93f98644533612a0215fed60aa771
Stephen Lewis <stephenrlewis@google.com>
2026-07-17 15:10:37 -0700

    Revert "Removed max_concurrent_run_count from update tests - not supported for create_notebook_execution_job_request"
    This reverts commit a2c147e00a2e10cf0c5b4932718f2c92bcfa815e.

--- mmv1/third_party/terraform/services/colab/resource_colab_schedule_test.go
+++ mmv1/third_party/terraform/services/colab/resource_colab_schedule_test.go
@@ -247,6 +247,7 @@ resource "google_colab_schedule" "schedule" {
   display_name = "tf-test-schedule%{random_suffix}"
   location = "%{location}"
   allow_queueing = true
+  max_concurrent_run_count = 2
   max_concurrent_active_run_count = 2
   cron = "TZ=America/Los_Angeles * * * * *"
   max_run_count = 5
@@ -343,6 +344,7 @@ resource "google_colab_schedule" "schedule" {
   display_name = "tf-test-schedule%{random_suffix}"
   location = "%{location}"
   allow_queueing = true
+  max_concurrent_run_count = 2
   max_concurrent_active_run_count = 2
   cron = "TZ=America/Los_Angeles * * * * *"
   max_run_count = 5
@@ -441,6 +443,7 @@ resource "google_colab_schedule" "schedule" {
   display_name = "tf-test-schedule%{random_suffix}"
   location = "%{location}"
   allow_queueing = true
+  max_concurrent_run_count = 2
   max_concurrent_active_run_count = 2
   cron = "TZ=America/Los_Angeles * * * * *"
   max_run_count = 5
@@ -539,6 +542,7 @@ resource "google_colab_schedule" "schedule" {
   display_name = "tf-test-schedule-updated%{random_suffix}"
   location = "%{location}"
   allow_queueing = false
+  max_concurrent_run_count = 1
   max_concurrent_active_run_count = 1
   cron = "TZ=America/Los_Angeles 0 * * * *"
   max_run_count = 3
