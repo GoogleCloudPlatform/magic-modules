9af0fb5917c928eadf4b4e5f264069d07b6ddb39
Karan Ma <mazd@google.com>
2026-07-23 16:55:19 +0000

    Fix TestAccBigqueryDatapolicyv2DataPolicy_dataGovernanceTag to update tag instead of clearing it

--- mmv1/third_party/terraform/services/bigquerydatapolicyv2/resource_bigquery_datapolicyv2_data_policy_test.go
+++ mmv1/third_party/terraform/services/bigquerydatapolicyv2/resource_bigquery_datapolicyv2_data_policy_test.go
@@ -87,14 +87,15 @@ func TestAccBigqueryDatapolicyv2DataPolicy_dataGovernanceTag(t *testing.T) {
 	project := envvar.GetTestProjectFromEnv()
 	policyID := fmt.Sprintf("tf_test_dg_tag_%s", acctest.RandString(t, 10))
 	tagKeyShortName := fmt.Sprintf("tag_key_%s", acctest.RandString(t, 5))
-	tagValueShortName := fmt.Sprintf("tag_val_%s", acctest.RandString(t, 5))
+	tagValueShortName1 := fmt.Sprintf("tag_val1_%s", acctest.RandString(t, 5))
+	tagValueShortName2 := fmt.Sprintf("tag_val2_%s", acctest.RandString(t, 5))
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccBigqueryDatapolicyv2DataPolicy_dataGovernanceTag(project, tagKeyShortName, tagValueShortName, policyID),
+				Config: testAccBigqueryDatapolicyv2DataPolicy_dataGovernanceTag(project, tagKeyShortName, tagValueShortName1, policyID),
 			},
 			{
 				ResourceName:            "google_bigquery_datapolicyv2_data_policy.policy",
@@ -103,7 +104,7 @@ func TestAccBigqueryDatapolicyv2DataPolicy_dataGovernanceTag(t *testing.T) {
 				ImportStateVerifyIgnore: []string{"location"},
 			},
 			{
-				Config: testAccBigqueryDatapolicyv2DataPolicy_dataGovernanceTagClear(project, tagKeyShortName, tagValueShortName, policyID),
+				Config: testAccBigqueryDatapolicyv2DataPolicy_dataGovernanceTagUpdate(project, tagKeyShortName, tagValueShortName1, tagValueShortName2, policyID),
 			},
 			{
 				ResourceName:            "google_bigquery_datapolicyv2_data_policy.policy",
@@ -123,7 +124,7 @@ resource "google_tags_tag_key" "key" {
   purpose    = "DATA_GOVERNANCE"
 }
 
-resource "google_tags_tag_value" "value" {
+resource "google_tags_tag_value" "value1" {
   parent     = "tagKeys/${google_tags_tag_key.key.name}"
   short_name = "%s"
 }
@@ -138,13 +139,13 @@ resource "google_bigquery_datapolicyv2_data_policy" "policy" {
   data_policy_id   = "%s"
   data_governance_tag {
     key   = google_tags_tag_key.key.namespaced_name
-    value = google_tags_tag_value.value.short_name
+    value = google_tags_tag_value.value1.short_name
   }
 }
 `, project, tagKeyShortName, tagValueShortName, policyID)
 }
 
-func testAccBigqueryDatapolicyv2DataPolicy_dataGovernanceTagClear(project, tagKeyShortName, tagValueShortName, policyID string) string {
+func testAccBigqueryDatapolicyv2DataPolicy_dataGovernanceTagUpdate(project, tagKeyShortName, tagValueShortName1, tagValueShortName2, policyID string) string {
 	return fmt.Sprintf(`
 resource "google_tags_tag_key" "key" {
   parent     = "projects/%s"
@@ -152,7 +153,12 @@ resource "google_tags_tag_key" "key" {
   purpose    = "DATA_GOVERNANCE"
 }
 
-resource "google_tags_tag_value" "value" {
+resource "google_tags_tag_value" "value1" {
+  parent     = "tagKeys/${google_tags_tag_key.key.name}"
+  short_name = "%s"
+}
+
+resource "google_tags_tag_value" "value2" {
   parent     = "tagKeys/${google_tags_tag_key.key.name}"
   short_name = "%s"
 }
@@ -165,6 +171,10 @@ resource "google_bigquery_datapolicyv2_data_policy" "policy" {
   }
   grantees = []
   data_policy_id   = "%s"
+  data_governance_tag {
+    key   = google_tags_tag_key.key.namespaced_name
+    value = google_tags_tag_value.value2.short_name
+  }
 }
-`, project, tagKeyShortName, tagValueShortName, policyID)
+`, project, tagKeyShortName, tagValueShortName1, tagValueShortName2, policyID)
 }
