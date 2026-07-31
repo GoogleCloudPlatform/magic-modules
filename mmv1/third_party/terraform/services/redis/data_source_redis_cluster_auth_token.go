bf271fa92c9b4d7807fc77e8ce9c210306a3d3c7
Andrew Garcia-Corley <andrewgc@google.com>
2026-07-31 14:23:44 -0700

    Setting name in import

--- mmv1/third_party/terraform/services/redis/data_source_redis_cluster_auth_token.go
+++ mmv1/third_party/terraform/services/redis/data_source_redis_cluster_auth_token.go
@@ -26,6 +26,9 @@ func dataSourceRedisClusterAuthTokenRead(d *schema.ResourceData, meta interface{
 	tokenId := d.Get("token_id").(string)
 	id := fmt.Sprintf("%s/authTokens/%s", tokenAuthUser, tokenId)
 	d.SetId(id)
+	if err := d.Set("name", id); err != nil {
+		return fmt.Errorf("Error setting name: %s", err)
+	}
 
 	err := resourceRedisClusterAuthTokenRead(d, meta)
 	if err != nil {
