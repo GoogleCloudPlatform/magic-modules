90fc4d76f13a6a136d98a981b7f1a24ecb5d6f02
Guna M <gmxxiv@google.com>
2026-08-03 05:53:06 +0000

    Add data source for google_iam_workload_identity_pool_openid_config Adds a read-only data source to retrieve OIDC discovery configuration (/.well-known/openid-configuration) for BYOID Identity pools

---
+++ mmv1/third_party/terraform/services/iambeta/data_source_iam_workload_identity_pool_openid_config.go
@@ -0,0 +1,136 @@
+package iambeta
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-google/google/registry"
+	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
+	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
+)
+
+func DataSourceIAMBetaWorkloadIdentityPoolOpenIdConfig() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceIAMBetaWorkloadIdentityPoolOpenIdConfigRead,
+		Schema: map[string]*schema.Schema{
+			"workload_identity_pool_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The name of the pool whose OpenID provider configuration to retrieve.",
+			},
+			"issuer": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "URL using the https scheme with no query or fragment components that the OP asserts as its issuer identifier.",
+			},
+			"jwks_uri": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "URL of the OP's JWK Set [JWK] document, which MUST use the https scheme.",
+			},
+			"authorization_endpoint": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "URL pointing to an authorization endpoint under this issuer.",
+			},
+			"token_endpoint": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "URL pointing to a token endpoint under this issuer.",
+			},
+			"response_types_supported": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "JSON array containing a list of the OAuth 2.0 response_type values that this OP supports.",
+				Elem:        &schema.Schema{Type: schema.TypeString},
+			},
+			"subject_types_supported": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "JSON array containing a list of the subject identifier types that this OP supports.",
+				Elem:        &schema.Schema{Type: schema.TypeString},
+			},
+			"id_token_signing_alg_values_supported": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "JSON array containing a list of the JWS signing algorithms (alg values) supported by the OP for the ID token to encode the claims in a JWT [JWT].",
+				Elem:        &schema.Schema{Type: schema.TypeString},
+			},
+		},
+	}
+}
+
+func dataSourceIAMBetaWorkloadIdentityPoolOpenIdConfigRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*transport_tpg.Config)
+	userAgent, err := tpgresource.GenerateUserAgentString(d, config.UserAgent)
+	if err != nil {
+		return err
+	}
+
+	poolName := d.Get("workload_identity_pool_id").(string)
+	url := fmt.Sprintf("https://sts.googleapis.com/v1/%s/.well-known/openid-configuration", poolName)
+
+	res, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
+		Config:    config,
+		Method:    "GET",
+		RawURL:    url,
+		UserAgent: userAgent,
+	})
+	if err != nil {
+		return fmt.Errorf("Error fetching OpenID configuration: %s", err)
+	}
+
+	if res == nil {
+		return fmt.Errorf("OpenID configuration not found for %s", poolName)
+	}
+
+	if err := d.Set("issuer", res["issuer"]); err != nil {
+		return fmt.Errorf("Error setting issuer: %s", err)
+	}
+	if err := d.Set("jwks_uri", res["jwks_uri"]); err != nil {
+		return fmt.Errorf("Error setting jwks_uri: %s", err)
+	}
+	if err := d.Set("authorization_endpoint", res["authorization_endpoint"]); err != nil {
+		return fmt.Errorf("Error setting authorization_endpoint: %s", err)
+	}
+	if err := d.Set("token_endpoint", res["token_endpoint"]); err != nil {
+		return fmt.Errorf("Error setting token_endpoint: %s", err)
+	}
+	if err := d.Set("response_types_supported", flattenStringList(res["response_types_supported"])); err != nil {
+		return fmt.Errorf("Error setting response_types_supported: %s", err)
+	}
+	if err := d.Set("subject_types_supported", flattenStringList(res["subject_types_supported"])); err != nil {
+		return fmt.Errorf("Error setting subject_types_supported: %s", err)
+	}
+	if err := d.Set("id_token_signing_alg_values_supported", flattenStringList(res["id_token_signing_alg_values_supported"])); err != nil {
+		return fmt.Errorf("Error setting id_token_signing_alg_values_supported: %s", err)
+	}
+
+	d.SetId(poolName)
+
+	return nil
+}
+
+func flattenStringList(val interface{}) []string {
+	if val == nil {
+		return nil
+	}
+	if list, ok := val.([]interface{}); ok {
+		result := make([]string, 0, len(list))
+		for _, v := range list {
+			if str, ok := v.(string); ok {
+				result = append(result, str)
+			}
+		}
+		return result
+	}
+	return nil
+}
+
+func init() {
+	registry.Schema{
+		Name:        "google_iam_workload_identity_pool_openid_config",
+		ProductName: "iambeta",
+		Type:        registry.SchemaTypeDataSource,
+		Schema:      DataSourceIAMBetaWorkloadIdentityPoolOpenIdConfig(),
+	}.Register()
+}
---
+++ mmv1/third_party/terraform/services/iambeta/data_source_iam_workload_identity_pool_openid_config_test.go
@@ -0,0 +1,50 @@
+package iambeta_test
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	_ "github.com/hashicorp/terraform-provider-google/google/services/iambeta"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccDataSourceIAMBetaWorkloadIdentityPoolOpenIdConfig_basic(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		// Pre-existing Google-owned organization (google.com) with Agent System Pool
+		"org_id": "433637338589",
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceIAMBetaWorkloadIdentityPoolOpenIdConfigBasic(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestMatchResourceAttr("data.google_iam_workload_identity_pool_openid_config.example", "issuer", regexp.MustCompile(`^https://sts\.googleapis\.com/v1/organizations/433637338589/locations/global/workloadIdentityPools/agents\.global\.org-433637338589\.system\.id\.goog$`)),
+					resource.TestMatchResourceAttr("data.google_iam_workload_identity_pool_openid_config.example", "jwks_uri", regexp.MustCompile(`^https://sts\.googleapis\.com/v1/organizations/433637338589/locations/global/workloadIdentityPools/agents\.global\.org-433637338589\.system\.id\.goog/openid/jwks$`)),
+					resource.TestMatchResourceAttr("data.google_iam_workload_identity_pool_openid_config.example", "authorization_endpoint", regexp.MustCompile(`^https://sts\.googleapis\.com/v1/organizations/433637338589/locations/global/workloadIdentityPools/agents\.global\.org-433637338589\.system\.id\.goog/authorize$`)),
+					resource.TestMatchResourceAttr("data.google_iam_workload_identity_pool_openid_config.example", "token_endpoint", regexp.MustCompile(`^https://sts\.googleapis\.com/v1/organizations/433637338589/locations/global/workloadIdentityPools/agents\.global\.org-433637338589\.system\.id\.goog/token$`)),
+					resource.TestCheckResourceAttr("data.google_iam_workload_identity_pool_openid_config.example", "response_types_supported.#", "1"),
+					resource.TestCheckResourceAttr("data.google_iam_workload_identity_pool_openid_config.example", "response_types_supported.0", "id_token"),
+					resource.TestCheckResourceAttr("data.google_iam_workload_identity_pool_openid_config.example", "subject_types_supported.#", "1"),
+					resource.TestCheckResourceAttr("data.google_iam_workload_identity_pool_openid_config.example", "subject_types_supported.0", "public"),
+					resource.TestCheckResourceAttr("data.google_iam_workload_identity_pool_openid_config.example", "id_token_signing_alg_values_supported.#", "1"),
+					resource.TestCheckResourceAttr("data.google_iam_workload_identity_pool_openid_config.example", "id_token_signing_alg_values_supported.0", "RS256"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceIAMBetaWorkloadIdentityPoolOpenIdConfigBasic(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+data "google_iam_workload_identity_pool_openid_config" "example" {
+	workload_identity_pool_id = "organizations/%{org_id}/locations/global/workloadIdentityPools/agents.global.org-%{org_id}.system.id.goog"
+}
+`, context)
+}
