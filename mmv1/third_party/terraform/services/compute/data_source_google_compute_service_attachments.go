21585c7c6a8121a741152a65170f5b0d2ccf07fb
vr-ibm <vrau@ibm.com>
2026-08-07 15:04:42 -0500

    fix: use Go client and full resource schema for service_attachments data source

--- mmv1/third_party/terraform/services/compute/data_source_google_compute_service_attachments.go
+++ mmv1/third_party/terraform/services/compute/data_source_google_compute_service_attachments.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
 	"github.com/hashicorp/terraform-provider-google/google/registry"
 	"github.com/hashicorp/terraform-provider-google/google/tpgresource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
@@ -29,50 +30,7 @@ func DataSourceGoogleComputeServiceAttachments() *schema.Resource {
 				Type:     schema.TypeList,
 				Computed: true,
 				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"name": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"description": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"self_link": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"target_service": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"connection_preference": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"nat_subnets": {
-							Type:     schema.TypeList,
-							Computed: true,
-							Elem:     &schema.Schema{Type: schema.TypeString},
-						},
-						"enable_proxy_protocol": {
-							Type:     schema.TypeBool,
-							Computed: true,
-						},
-						"domain_names": {
-							Type:     schema.TypeList,
-							Computed: true,
-							Elem:     &schema.Schema{Type: schema.TypeString},
-						},
-						"fingerprint": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"region": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-					},
+					Schema: tpgresource.DatasourceSchemaFromResourceSchema(ResourceComputeServiceAttachment().Schema),
 				},
 			},
 		},
@@ -96,94 +54,92 @@ func datasourceGoogleComputeServiceAttachmentsRead(d *schema.ResourceData, meta 
 		return err
 	}
 
-	params := make(map[string]string)
+	id := fmt.Sprintf("projects/%s/regions/%s/serviceAttachments", project, region)
+	d.SetId(id)
+
+	call := NewClient(config, userAgent).ServiceAttachments.List(project, region)
 	if filter, ok := d.GetOk("filter"); ok {
-		params["filter"] = filter.(string)
+		call = call.Filter(filter.(string))
 	}
 
 	serviceAttachments := make([]map[string]interface{}, 0)
 
 	for {
-		url, err := tpgresource.ReplaceVars(d, config, "{{ComputeBasePath}}projects/{{project}}/regions/{{region}}/serviceAttachments")
+		list, err := call.Do()
 		if err != nil {
-			return err
+			return transport_tpg.HandleNotFoundError(err, d, fmt.Sprintf("Service Attachments Not Found in project %s, region %s", project, region))
 		}
 
-		url, err = transport_tpg.AddQueryParams(url, params)
-		if err != nil {
-			return err
+		for _, sa := range list.Items {
+			consumerAcceptLists := make([]map[string]interface{}, 0, len(sa.ConsumerAcceptLists))
+			for _, cal := range sa.ConsumerAcceptLists {
+				consumerAcceptLists = append(consumerAcceptLists, map[string]interface{}{
+					"project_id_or_num": cal.ProjectIdOrNum,
+					"connection_limit":  cal.ConnectionLimit,
+					"network_url":       cal.NetworkUrl,
+				})
+			}
+
+			connectedEndpoints := make([]map[string]interface{}, 0, len(sa.ConnectedEndpoints))
+			for _, ce := range sa.ConnectedEndpoints {
+				connectedEndpoints = append(connectedEndpoints, map[string]interface{}{
+					"endpoint":          ce.Endpoint,
+					"status":            ce.Status,
+					"psc_connection_id": ce.PscConnectionId,
+					"consumer_network":  ce.ConsumerNetwork,
+				})
+			}
+
+			var pscId []map[string]interface{}
+			if sa.PscServiceAttachmentId != nil {
+				pscId = []map[string]interface{}{
+					{
+						"high": sa.PscServiceAttachmentId.High,
+						"low":  sa.PscServiceAttachmentId.Low,
+					},
+				}
+			}
+
+			mapped := map[string]interface{}{
+				"name":                        sa.Name,
+				"description":                 sa.Description,
+				"self_link":                   sa.SelfLink,
+				"target_service":              sa.TargetService,
+				"connection_preference":       sa.ConnectionPreference,
+				"nat_subnets":                 sa.NatSubnets,
+				"enable_proxy_protocol":       sa.EnableProxyProtocol,
+				"domain_names":                sa.DomainNames,
+				"fingerprint":                 sa.Fingerprint,
+				"region":                      sa.Region,
+				"reconcile_connections":       sa.ReconcileConnections,
+				"propagated_connection_limit": sa.PropagatedConnectionLimit,
+				"consumer_reject_lists":       sa.ConsumerRejectLists,
+				"consumer_accept_lists":       consumerAcceptLists,
+				"connected_endpoints":         connectedEndpoints,
+				"psc_service_attachment_id":   pscId,
+			}
+			serviceAttachments = append(serviceAttachments, mapped)
 		}
 
-		res, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
-			Config:    config,
-			Method:    "GET",
-			RawURL:    url,
-			UserAgent: userAgent,
-		})
-		if err != nil {
-			return fmt.Errorf("error retrieving service attachments: %s", err)
-		}
-
-		pageItems := flattenDatasourceGoogleComputeServiceAttachmentsList(res["items"])
-		serviceAttachments = append(serviceAttachments, pageItems...)
-
-		pToken, ok := res["nextPageToken"]
-		if ok && pToken != nil && pToken.(string) != "" {
-			params["pageToken"] = pToken.(string)
-		} else {
+		if list.NextPageToken == "" {
 			break
 		}
+		call = call.PageToken(list.NextPageToken)
 	}
 
 	if err := d.Set("service_attachments", serviceAttachments); err != nil {
 		return fmt.Errorf("error setting service_attachments: %s", err)
 	}
 
-	d.SetId(fmt.Sprintf("projects/%s/regions/%s/serviceAttachments", project, region))
+	if err := d.Set("project", project); err != nil {
+		return fmt.Errorf("error setting project: %s", err)
+	}
+
+	if err := d.Set("region", region); err != nil {
+		return fmt.Errorf("error setting region: %s", err)
+	}
 
 	return nil
-}
-
-func flattenDatasourceGoogleComputeServiceAttachmentsList(v interface{}) []map[string]interface{} {
-	if v == nil {
-		return make([]map[string]interface{}, 0)
-	}
-
-	ls := v.([]interface{})
-	serviceAttachments := make([]map[string]interface{}, 0, len(ls))
-	for _, raw := range ls {
-		p := raw.(map[string]interface{})
-
-		var natSubnets []interface{}
-		if ns, ok := p["natSubnets"]; ok && ns != nil {
-			natSubnets = ns.([]interface{})
-		}
-
-		var domainNames []interface{}
-		if dn, ok := p["domainNames"]; ok && dn != nil {
-			domainNames = dn.([]interface{})
-		}
-
-		var enableProxyProtocol bool
-		if epp, ok := p["enableProxyProtocol"]; ok && epp != nil {
-			enableProxyProtocol = epp.(bool)
-		}
-
-		serviceAttachments = append(serviceAttachments, map[string]interface{}{
-			"name":                  p["name"],
-			"description":           p["description"],
-			"self_link":             p["selfLink"],
-			"target_service":        p["targetService"],
-			"connection_preference": p["connectionPreference"],
-			"nat_subnets":           natSubnets,
-			"enable_proxy_protocol": enableProxyProtocol,
-			"domain_names":          domainNames,
-			"fingerprint":           p["fingerprint"],
-			"region":                p["region"],
-		})
-	}
-
-	return serviceAttachments
 }
 
 func init() {
