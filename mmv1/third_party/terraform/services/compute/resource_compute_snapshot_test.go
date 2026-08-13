71d047ed946b320ec6788cb45c5b2d1fcf92abdf
Sampath Gorla <sampathgo@google.com>
2026-08-13 06:49:33 +0000

    feat(compute): Add UpdateKmsKey support for Disks and Snapshots
    Support in-place updates for disk_encryption_key and snapshot_encryption_key via the UpdateKmsKey API for zonal disks, regional disks, and snapshots.

--- mmv1/third_party/terraform/services/compute/resource_compute_snapshot_test.go
+++ mmv1/third_party/terraform/services/compute/resource_compute_snapshot_test.go
@@ -2,15 +2,79 @@ package compute_test
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 	"github.com/hashicorp/terraform-provider-google/google/envvar"
-	_ "github.com/hashicorp/terraform-provider-google/google/services/compute"
+	tpgcompute "github.com/hashicorp/terraform-provider-google/google/services/compute"
 	"github.com/hashicorp/terraform-provider-google/google/services/kms"
 	_ "github.com/hashicorp/terraform-provider-google/google/services/resourcemanager"
 )
+
+func TestSnapshotKmsKeyDiffSuppress(t *testing.T) {
+	cases := map[string]struct {
+		Old                string
+		New                string
+		ExpectDiffSuppress bool
+	}{
+		"matching base keys": {
+			Old:                "projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/my-key",
+			New:                "projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/my-key",
+			ExpectDiffSuppress: true,
+		},
+		"base key old, versioned new": {
+			Old:                "projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/my-key",
+			New:                "projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/my-key/cryptoKeyVersions/1",
+			ExpectDiffSuppress: true,
+		},
+		"versioned old, base key new": {
+			Old:                "projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/my-key/cryptoKeyVersions/1",
+			New:                "projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/my-key",
+			ExpectDiffSuppress: true,
+		},
+		"different versions same key": {
+			Old:                "projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/my-key/cryptoKeyVersions/1",
+			New:                "projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/my-key/cryptoKeyVersions/2",
+			ExpectDiffSuppress: true,
+		},
+		"self link old, relative versioned new": {
+			Old:                "https://www.googleapis.com/compute/v1/projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/my-key",
+			New:                "projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/my-key/cryptoKeyVersions/1",
+			ExpectDiffSuppress: true,
+		},
+		"different keys": {
+			Old:                "projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/my-key",
+			New:                "projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/other-key",
+			ExpectDiffSuppress: false,
+		},
+		"different keys with versions": {
+			Old:                "projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/my-key/cryptoKeyVersions/1",
+			New:                "projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/other-key/cryptoKeyVersions/1",
+			ExpectDiffSuppress: false,
+		},
+		"empty old": {
+			Old:                "",
+			New:                "projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/my-key",
+			ExpectDiffSuppress: false,
+		},
+		"empty new": {
+			Old:                "projects/my-project/locations/us-central1/keyRings/my-keyring/cryptoKeys/my-key",
+			New:                "",
+			ExpectDiffSuppress: false,
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			if tpgcompute.SnapshotKmsKeyDiffSuppress("snapshot_encryption_key.0.kms_key_self_link", tc.Old, tc.New, nil) != tc.ExpectDiffSuppress {
+				t.Fatalf("SnapshotKmsKeyDiffSuppress %q failed: old=%s, new=%s, expected=%v, got=%v",
+					name, tc.Old, tc.New, tc.ExpectDiffSuppress, !tc.ExpectDiffSuppress)
+			}
+		})
+	}
+}
 
 func TestAccComputeSnapshot_encryption(t *testing.T) {
 	t.Parallel()
@@ -61,6 +125,149 @@ func TestAccComputeSnapshot_encryptionCMEK(t *testing.T) {
 			},
 		},
 	})
+}
+
+
+func TestAccComputeSnapshot_kmsKeyVersionBehaviors(t *testing.T) {
+	t.Parallel()
+	acctest.SkipIfVcr(t) // Based on the other test having this
+
+	snapshotName := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
+	diskName := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
+	kmsKey1 := kms.BootstrapKMSKeyWithPurposeInLocationAndName(t, "ENCRYPT_DECRYPT", "us-central1", "tf-bootstrap-compute-snapshot-key3").CryptoKey.Name
+	kmsKey2 := kms.BootstrapKMSKeyWithPurposeInLocationAndName(t, "ENCRYPT_DECRYPT", "us-central1", "tf-bootstrap-compute-snapshot-key4").CryptoKey.Name
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckComputeSnapshotDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeSnapshot_updateKmsKey_step2(snapshotName, diskName, kmsKey1),
+			},
+			{
+				Config:             testAccComputeSnapshot_updateKmsKey_step2(snapshotName, diskName, kmsKey1+"/cryptoKeyVersions/1"),
+				PlanOnly:           true,
+				ExpectNonEmptyPlan: false,
+			},
+			{
+				Config:      testAccComputeSnapshot_updateKmsKey_step2(snapshotName, diskName, kmsKey2+"/cryptoKeyVersions/1"),
+				ExpectError: regexp.MustCompile("Changing the KMS key to a specific version is not supported\\."),
+			},
+		},
+	})
+}
+func TestAccComputeSnapshot_updateKmsKey(t *testing.T) {
+	t.Parallel()
+	acctest.SkipIfVcr(t)
+
+	snapshotName := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
+	diskName := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
+	kmsKey1 := kms.BootstrapKMSKeyWithPurposeInLocationAndName(t, "ENCRYPT_DECRYPT", "us-central1", "tf-bootstrap-compute-snapshot-key1").CryptoKey.Name
+	kmsKey2 := kms.BootstrapKMSKeyWithPurposeInLocationAndName(t, "ENCRYPT_DECRYPT", "us-central1", "tf-bootstrap-compute-snapshot-key2").CryptoKey.Name
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckComputeSnapshotDestroyProducer(t),
+		Steps: []resource.TestStep{
+			// Step 1: Create snapshot with CMEK Key 1
+			{
+				Config: testAccComputeSnapshot_updateKmsKey_step2(snapshotName, diskName, kmsKey1),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"google_compute_snapshot.foobar", "snapshot_encryption_key.0.kms_key_self_link", kmsKey1),
+				),
+			},
+			// Step 2: In-place update to change to CMEK Key 2
+			{
+				Config: testAccComputeSnapshot_updateKmsKey_step2(snapshotName, diskName, kmsKey2),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"google_compute_snapshot.foobar", "snapshot_encryption_key.0.kms_key_self_link", kmsKey2),
+				),
+			},
+			{
+				ResourceName:            "google_compute_snapshot.foobar",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"zone", "source_disk_encryption_key"},
+			},
+		},
+	})
+}
+
+func TestAccComputeSnapshot_disallowCmekToGmek(t *testing.T) {
+	t.Parallel()
+	acctest.SkipIfVcr(t)
+
+	snapshotName := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
+	diskName := fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10))
+	kmsKey := kms.BootstrapKMSKeyWithPurposeInLocationAndName(t, "ENCRYPT_DECRYPT", "us-central1", "tf-bootstrap-compute-snapshot-key1").CryptoKey.Name
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckComputeSnapshotDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeSnapshot_updateKmsKey_step2(snapshotName, diskName, kmsKey),
+			},
+			{
+				Config:      testAccComputeSnapshot_updateKmsKey_step1(snapshotName, diskName),
+				ExpectError: regexp.MustCompile("Removing 'snapshot_encryption_key' is not supported in-place. To remove the KMS key, please destroy and recreate the snapshot without a KMS key."),
+			},
+		},
+	})
+}
+
+func testAccComputeSnapshot_updateKmsKey_step1(snapshotName, diskName string) string {
+	return fmt.Sprintf(`
+data "google_compute_image" "my_image" {
+  family  = "debian-12"
+  project = "debian-cloud"
+}
+
+resource "google_compute_disk" "foobar" {
+  name  = "%s"
+  image = data.google_compute_image.my_image.self_link
+  size  = 10
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+}
+
+resource "google_compute_snapshot" "foobar" {
+  name        = "%s"
+  source_disk = google_compute_disk.foobar.name
+  zone        = "us-central1-a"
+}
+`, diskName, snapshotName)
+}
+
+func testAccComputeSnapshot_updateKmsKey_step2(snapshotName, diskName, kmsKey string) string {
+	return fmt.Sprintf(`
+data "google_compute_image" "my_image" {
+  family  = "debian-12"
+  project = "debian-cloud"
+}
+
+resource "google_compute_disk" "foobar" {
+  name  = "%s"
+  image = data.google_compute_image.my_image.self_link
+  size  = 10
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+}
+
+resource "google_compute_snapshot" "foobar" {
+  name        = "%s"
+  source_disk = google_compute_disk.foobar.name
+  zone        = "us-central1-a"
+  snapshot_encryption_key {
+    kms_key_self_link = "%s"
+  }
+}
+`, diskName, snapshotName, kmsKey)
 }
 
 func testAccComputeSnapshot_encryption(snapshotName string, diskName string) string {
