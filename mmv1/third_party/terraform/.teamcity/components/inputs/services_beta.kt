9c900e073479b6f58a20ad715ef3fa3a77470ad4
Medha Shah <shahmedha@google.com>
2026-08-20 20:43:15 +0000

    Register serviceusagev2 in TeamCity configuration

--- mmv1/third_party/terraform/.teamcity/components/inputs/services_beta.kt
+++ mmv1/third_party/terraform/.teamcity/components/inputs/services_beta.kt
@@ -856,6 +856,10 @@ var ServicesListBeta = mapOf(
         "displayName" to "Serviceusage",
         "path" to "./google-beta/services/serviceusage"
     ),
+    "serviceusagev2" to mapOf(
+            "displayName" to "ServiceUsageV2",
+            "path" to "./google-beta/services/serviceusagev2"
+    ),
     "siteverification" to mapOf(
         "name" to "siteverification",
         "displayName" to "Siteverification",
