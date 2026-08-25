17a1982e829719814883f2cefcad9b76b202c8d3
Chris Hawk <hawk@google.com>
2026-08-25 19:41:48 +0000

    apigee: address review comments on instance consumer_accept_list diff suppress
    - Use strings.HasSuffix for tenant project string checking
    - Update unit test suite to provide consumer_accept_list slice in mock diff
    - Add unit test cases for positive and negative tenant project diff suppression
    - Use reflect.DeepEqual in test runner to safely compare slice values

--- mmv1/third_party/terraform/services/apigee/resource_apigee_instance_test.go
+++ mmv1/third_party/terraform/services/apigee/resource_apigee_instance_test.go
@@ -1,6 +1,7 @@
 package apigee_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/hashicorp/terraform-provider-google/google/services/apigee"
@@ -23,13 +24,15 @@ type ApigeeInstanceDiffSuppressTestCase struct {
 var apigeeInstanceDiffSuppressTestCases = []ApigeeInstanceDiffSuppressTestCase{
 	{
 		Name:           "projects with the same length and one project entry is converted to project id",
-		KeysToSuppress: []string{"consumer_accept_list.0"},
+		KeysToSuppress: []string{"consumer_accept_list", "consumer_accept_list.0"},
 		Before: map[string]interface{}{
+			"consumer_accept_list":   []interface{}{"45796856818", "12345"},
 			"consumer_accept_list.#": 2,
 			"consumer_accept_list.0": "45796856818",
 			"consumer_accept_list.1": "12345",
 		},
 		After: map[string]interface{}{
+			"consumer_accept_list":   []interface{}{"tf-test8v1bd04pxa", "12345"},
 			"consumer_accept_list.#": 2,
 			"consumer_accept_list.0": "tf-test8v1bd04pxa",
 			"consumer_accept_list.1": "12345",
@@ -39,11 +42,13 @@ var apigeeInstanceDiffSuppressTestCases = []ApigeeInstanceDiffSuppressTestCase{
 		Name:           "projects with the same length and no project conversion",
 		KeysToSuppress: []string{},
 		Before: map[string]interface{}{
+			"consumer_accept_list":   []interface{}{"tf-test8v1bd04pxa", "12345"},
 			"consumer_accept_list.#": 2,
 			"consumer_accept_list.0": "tf-test8v1bd04pxa",
 			"consumer_accept_list.1": "12345",
 		},
 		After: map[string]interface{}{
+			"consumer_accept_list":   []interface{}{"tf-test8v1bd04pxa", "12345"},
 			"consumer_accept_list.#": 2,
 			"consumer_accept_list.0": "tf-test8v1bd04pxa",
 			"consumer_accept_list.1": "12345",
@@ -60,9 +65,113 @@ var apigeeInstanceDiffSuppressTestCases = []ApigeeInstanceDiffSuppressTestCase{
 		KeysToSuppress: []string{},
 		Before:         map[string]interface{}{},
 		After: map[string]interface{}{
+			"consumer_accept_list":   []interface{}{"tf-test8v1bd04pxa", "12345"},
 			"consumer_accept_list.#": 2,
 			"consumer_accept_list.0": "tf-test8v1bd04pxa",
 			"consumer_accept_list.1": "12345",
+		},
+	},
+	{
+		Name:           "positive case: state contains configured projects plus auto-injected tenant project",
+		KeysToSuppress: []string{"consumer_accept_list", "consumer_accept_list.#", "consumer_accept_list.1"},
+		Before: map[string]interface{}{
+			"consumer_accept_list":   []interface{}{"my-project", "f0d55e5054793ca7d-tp"},
+			"consumer_accept_list.#": 2,
+			"consumer_accept_list.0": "my-project",
+			"consumer_accept_list.1": "f0d55e5054793ca7d-tp",
+		},
+		After: map[string]interface{}{
+			"consumer_accept_list":   []interface{}{"my-project"},
+			"consumer_accept_list.#": 1,
+			"consumer_accept_list.0": "my-project",
+		},
+	},
+	{
+		Name:           "positive case: state contains transformed project ID plus auto-injected tenant project",
+		KeysToSuppress: []string{"consumer_accept_list", "consumer_accept_list.#", "consumer_accept_list.0", "consumer_accept_list.2"},
+		Before: map[string]interface{}{
+			"consumer_accept_list":   []interface{}{"proj-1-id", "proj-2", "auto-injected-tp"},
+			"consumer_accept_list.#": 3,
+			"consumer_accept_list.0": "proj-1-id",
+			"consumer_accept_list.1": "proj-2",
+			"consumer_accept_list.2": "auto-injected-tp",
+		},
+		After: map[string]interface{}{
+			"consumer_accept_list":   []interface{}{"1234567890", "proj-2"},
+			"consumer_accept_list.#": 2,
+			"consumer_accept_list.0": "1234567890",
+			"consumer_accept_list.1": "proj-2",
+		},
+	},
+	{
+		Name:           "negative case: state contains different numbers of non-tenant projects",
+		KeysToSuppress: []string{},
+		Before: map[string]interface{}{
+			"consumer_accept_list":   []interface{}{"project-1", "tenant-tp"},
+			"consumer_accept_list.#": 2,
+			"consumer_accept_list.0": "project-1",
+			"consumer_accept_list.1": "tenant-tp",
+		},
+		After: map[string]interface{}{
+			"consumer_accept_list":   []interface{}{"project-1", "project-2"},
+			"consumer_accept_list.#": 2,
+			"consumer_accept_list.0": "project-1",
+			"consumer_accept_list.1": "project-2",
+		},
+	},
+	{
+		Name:           "negative case: state contains non-tenant project and tenant project vs empty config",
+		KeysToSuppress: []string{},
+		Before: map[string]interface{}{
+			"consumer_accept_list":   []interface{}{"project-1", "tenant-tp"},
+			"consumer_accept_list.#": 2,
+			"consumer_accept_list.0": "project-1",
+			"consumer_accept_list.1": "tenant-tp",
+		},
+		After: map[string]interface{}{
+			"consumer_accept_list":   []interface{}{},
+			"consumer_accept_list.#": 0,
+		},
+	},
+	{
+		Name:           "negative case: state contains only tenant project vs 1 configured project",
+		KeysToSuppress: []string{},
+		Before: map[string]interface{}{
+			"consumer_accept_list":   []interface{}{"tenant-tp"},
+			"consumer_accept_list.#": 1,
+			"consumer_accept_list.0": "tenant-tp",
+		},
+		After: map[string]interface{}{
+			"consumer_accept_list":   []interface{}{"project-1"},
+			"consumer_accept_list.#": 1,
+			"consumer_accept_list.0": "project-1",
+		},
+	},
+	{
+		Name:           "fallback: same length without slice",
+		KeysToSuppress: []string{"consumer_accept_list.0"},
+		Before: map[string]interface{}{
+			"consumer_accept_list.#": 2,
+			"consumer_accept_list.0": "45796856818",
+			"consumer_accept_list.1": "12345",
+		},
+		After: map[string]interface{}{
+			"consumer_accept_list.#": 2,
+			"consumer_accept_list.0": "tf-test8v1bd04pxa",
+			"consumer_accept_list.1": "12345",
+		},
+	},
+	{
+		Name:           "fallback: different length without slice",
+		KeysToSuppress: []string{},
+		Before: map[string]interface{}{
+			"consumer_accept_list.#": 1,
+			"consumer_accept_list.0": "project-1",
+		},
+		After: map[string]interface{}{
+			"consumer_accept_list.#": 2,
+			"consumer_accept_list.0": "project-1",
+			"consumer_accept_list.1": "project-2",
 		},
 	},
 }
@@ -79,7 +188,7 @@ func (tc *ApigeeInstanceDiffSuppressTestCase) Test(t *testing.T) {
 		val2, ok := tc.After[key]
 		if !ok {
 			keysHavingDiff[key] = true
-		} else if val1 != val2 {
+		} else if !reflect.DeepEqual(val1, val2) {
 			keysHavingDiff[key] = true
 		}
 	}
@@ -88,7 +197,7 @@ func (tc *ApigeeInstanceDiffSuppressTestCase) Test(t *testing.T) {
 		val2, ok := tc.Before[key]
 		if !ok {
 			keysHavingDiff[key] = true
-		} else if val1 != val2 {
+		} else if !reflect.DeepEqual(val1, val2) {
 			keysHavingDiff[key] = true
 		}
 	}
@@ -108,7 +217,13 @@ func (tc *ApigeeInstanceDiffSuppressTestCase) Test(t *testing.T) {
 	for key := range keysHavingDiff {
 		actual := apigee.ProjectListDiffSuppressFunc(mockResourceDiff)
 		if actual != keySuppressionMap[key] {
-			t.Errorf("Test %s: expected key `%s` to be suppressed", tc.Name, key)
+			var expectation string
+			if keySuppressionMap[key] {
+				expectation = "be"
+			} else {
+				expectation = "not be"
+			}
+			t.Errorf("Test %s: expected key `%s` to %s suppressed", tc.Name, key, expectation)
 		}
 	}
 }
