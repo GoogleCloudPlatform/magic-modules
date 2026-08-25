83fd65b55bc2b5f0949f87d9f9052e0f6ac3bd35
Stephen Lewis <stephenrlewis@google.com>
2026-08-25 16:43:45 +0000

    networkservices: add acceptance test for agent_connectivity_template

--- mmv1/third_party/terraform/services/networkservices/resource_network_services_agent_gateway_test.go
+++ mmv1/third_party/terraform/services/networkservices/resource_network_services_agent_gateway_test.go
@@ -1,14 +1,18 @@
 package networkservices_test
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 
 	"github.com/hashicorp/terraform-provider-google/google/acctest"
 	"github.com/hashicorp/terraform-provider-google/google/envvar"
-	_ "github.com/hashicorp/terraform-provider-google/google/services/networkservices"
+	"github.com/hashicorp/terraform-provider-google/google/services/networkservices"
+	transport_tpg "github.com/hashicorp/terraform-provider-google/google/transport"
 )
 
 func TestAccNetworkServicesAgentGateway_networkServicesAgentGatewayUpdate(t *testing.T) {
@@ -162,4 +166,154 @@ resource "google_network_services_agent_gateway" "default" {
   depends_on = [google_project_service.agent_registry]
 }
 `, context) + testAccNetworkServicesAgentGateway_sharedNetworkResources(context)
+}
+
+func TestAccNetworkServicesAgentGateway_networkServicesAgentGatewayAgentConnectivityTemplateUpdate(t *testing.T) {
+	t.Parallel()
+
+	acctest.SkipTestUntil(t, "2026-10-31") // b/548696678: Backend connectivity actuation rollout pending
+
+	randomSuffix := acctest.RandString(t, 10)
+	template1 := bootstrapAgentConnectivityTemplate(t, "tf-test-act-1-"+randomSuffix, "us-central1")
+	template2 := bootstrapAgentConnectivityTemplate(t, "tf-test-act-2-"+randomSuffix, "us-central1")
+
+	context := map[string]interface{}{
+		"project":       envvar.GetTestProjectFromEnv(),
+		"name":          "tf-test-my-agent-gateway" + randomSuffix,
+		"random_suffix": randomSuffix,
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckNetworkServicesAgentGatewayDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetworkServicesAgentGateway_networkServicesAgentGatewayAgentConnectivityTemplateUpdate(context, template1),
+			},
+			{
+				ResourceName:            "google_network_services_agent_gateway.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "location", "name", "terraform_labels"},
+			},
+			{
+				Config: testAccNetworkServicesAgentGateway_networkServicesAgentGatewayAgentConnectivityTemplateUpdate(context, template2),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(
+							"google_network_services_agent_gateway.default",
+							plancheck.ResourceActionUpdate,
+						),
+					},
+				},
+			},
+			{
+				ResourceName:            "google_network_services_agent_gateway.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "location", "name", "terraform_labels"},
+			},
+		},
+	})
+}
+
+func testAccNetworkServicesAgentGateway_networkServicesAgentGatewayAgentConnectivityTemplateUpdate(context map[string]interface{}, template string) string {
+	ctx := make(map[string]interface{})
+	for k, v := range context {
+		ctx[k] = v
+	}
+	ctx["template"] = template
+	return acctest.Nprintf(`
+resource "google_network_services_agent_gateway" "default" {
+  name     = "%{name}"
+  location = "us-central1"
+
+  google_managed {
+    governed_access_path = "CLIENT_TO_AGENT"
+  }
+
+  agent_connectivity_template = "%{template}"
+
+  registries = [
+    "//agentregistry.googleapis.com/projects/%{project}/locations/us-central1"
+  ]
+
+  depends_on = [google_project_service.agent_registry]
+}
+
+resource "google_project_service" "agent_registry" {
+  service            = "agentregistry.googleapis.com"
+  disable_on_destroy = false
+}
+`, ctx)
+}
+
+func bootstrapAgentConnectivityTemplate(t *testing.T, templateId, location string) string {
+	t.Helper()
+	config := transport_tpg.BootstrapConfig(t)
+	if config == nil {
+		t.Fatal("Could not bootstrap config.")
+	}
+
+	project := envvar.GetTestProjectFromEnv()
+	baseURL := transport_tpg.BaseUrl(networkservices.Product, config)
+	getURL := fmt.Sprintf("%sprojects/%s/locations/%s/agentConnectivityTemplates/%s",
+		baseURL, project, location, templateId)
+
+	headers := make(http.Header)
+	_, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
+		Config:    config,
+		Method:    "GET",
+		Project:   project,
+		RawURL:    getURL,
+		UserAgent: config.UserAgent,
+		Headers:   headers,
+	})
+
+	if err != nil && transport_tpg.IsGoogleApiErrorWithCode(err, 404) {
+		postURL := fmt.Sprintf("%sprojects/%s/locations/%s/agentConnectivityTemplates?agentConnectivityTemplateId=%s",
+			baseURL, project, location, templateId)
+		obj := map[string]interface{}{
+			"description": "Bootstrapped AgentConnectivityTemplate for acceptance test",
+		}
+		res, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
+			Config:    config,
+			Method:    "POST",
+			Project:   project,
+			RawURL:    postURL,
+			UserAgent: config.UserAgent,
+			Body:      obj,
+			Headers:   headers,
+		})
+		if err != nil {
+			t.Fatalf("Error creating bootstrapped AgentConnectivityTemplate %s: %s", templateId, err)
+		}
+
+		err = networkservices.NetworkServicesOperationWaitTime(
+			config, res, project, "Creating AgentConnectivityTemplate", config.UserAgent,
+			4*time.Minute)
+		if err != nil {
+			t.Fatalf("Error waiting for bootstrapped AgentConnectivityTemplate %s: %s", templateId, err)
+		}
+	}
+
+	t.Cleanup(func() {
+		deleteURL := fmt.Sprintf("%sprojects/%s/locations/%s/agentConnectivityTemplates/%s",
+			baseURL, project, location, templateId)
+		res, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
+			Config:    config,
+			Method:    "DELETE",
+			Project:   project,
+			RawURL:    deleteURL,
+			UserAgent: config.UserAgent,
+		})
+		if err == nil {
+			_ = networkservices.NetworkServicesOperationWaitTime(
+				config, res, project, "Deleting AgentConnectivityTemplate", config.UserAgent,
+				4*time.Minute)
+		}
+	})
+
+	return fmt.Sprintf("projects/%s/locations/%s/agentConnectivityTemplates/%s", project, location, templateId)
 }
