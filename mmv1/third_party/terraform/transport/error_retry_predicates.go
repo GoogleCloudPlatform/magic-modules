f6d26535944efb5587a52a14683b2f2c98fc52b8
Chris Hawk <hawk@google.com>
2026-08-25 17:24:33 -0700

    Apply suggestion from @SirGitsalot

--- mmv1/third_party/terraform/transport/error_retry_predicates.go
+++ mmv1/third_party/terraform/transport/error_retry_predicates.go
@@ -744,7 +744,9 @@ func IsNetworkAttachmentConnectedEndpointsError(err error) (bool, string) {
 
 func IsCloudIdentityGroup409(err error) (bool, string) {
 	if gerr, ok := err.(*googleapi.Error); ok {
-		if gerr.Code == 409 {
+		if gerr.Code == 409 && strings.Contains(strings.ToLower(gerr.Body), "operation was aborted") {
+			return true, "saw a 409 - The operation was aborted"
+		}
 			return true, "saw a 409 - The operation was aborted"
 		}
 	}
