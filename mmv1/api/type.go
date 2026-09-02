0673b667075e997d69b4f8210de2394dd8a0be18
Wiktor Niesiobędzki <wiktorn@google.com>
2026-09-02 20:04:40 +0000

    Honor versioning information on fields nested within Maps

--- mmv1/api/type.go
+++ mmv1/api/type.go
@@ -784,6 +784,8 @@ func (t *Type) ExcludeIfNotInVersion(version *product.Version) {
 		}
 	} else if t.IsA("Array") && t.ItemType.IsA("NestedObject") {
 		t.ItemType.ExcludeIfNotInVersion(version)
+	} else if t.IsA("Map") && t.ValueType != nil {
+		t.ValueType.ExcludeIfNotInVersion(version)
 	}
 }
 
