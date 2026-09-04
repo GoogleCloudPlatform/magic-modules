18cea66ea100b647a2f65976b884d7f0498ceb2a
Sam Levenick <slevenick@google.com>
2026-09-04 17:13:11 +0000

    ces: add evaluation_metrics_thresholds hallucination fields to App

--- mmv1/third_party/terraform/services/ces/ces_app_test.go
+++ mmv1/third_party/terraform/services/ces/ces_app_test.go
@@ -155,6 +155,8 @@ resource "google_ces_app" "ces_app_basic" {
         tool_invocation_parameter_correctness_threshold = 1.0
       }
     }
+    golden_hallucination_metric_behavior   = "ENABLED"
+    scenario_hallucination_metric_behavior = "ENABLED"
   }
 
   variable_declarations {
@@ -364,6 +366,8 @@ resource "google_ces_app" "ces_app_basic" {
         tool_invocation_parameter_correctness_threshold = 0.1
       }
     }
+    golden_hallucination_metric_behavior   = "DISABLED"
+    scenario_hallucination_metric_behavior = "DISABLED"
   }
 
   variable_declarations {
