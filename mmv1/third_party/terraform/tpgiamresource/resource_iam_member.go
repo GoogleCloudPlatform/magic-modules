a9778b6589c1af34bf267846810b49a11fb52f9d
alvarez.mauriciotm@gmail.com <alvarez.mauriciotm@gmail.com>
2026-09-08 09:30:46 -0700

    remove unnecessary conditionTitle logic

--- mmv1/third_party/terraform/tpgiamresource/resource_iam_member.go
+++ mmv1/third_party/terraform/tpgiamresource/resource_iam_member.go
@@ -247,9 +247,7 @@ func setIamMemberResourceIdentity(identity *schema.IdentityData, d *schema.Resou
 	populateIamParentIdentity(identity, d, parentSpecificSchema)
 	identity.Set("role", role)
 	identity.Set("member", tpgresource.NormalizeIamPrincipalCasing(member))
-	if conditionTitle == "" {
-		identity.Set("condition_title", nil)
-	} else {
+	if conditionTitle != "" {
 		identity.Set("condition_title", conditionTitle)
 	}
 }
