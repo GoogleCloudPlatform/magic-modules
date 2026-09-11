22937a9d8fb52c12df8524aa486d732ca87afdce
Chris Hawk <hawk@google.com>
2026-09-11 06:10:19 +0000

    accesscontextmanager: make test resources sweepable

--- mmv1/third_party/terraform/services/accesscontextmanager/resource_access_context_manager_services_perimeters_test.go
+++ mmv1/third_party/terraform/services/accesscontextmanager/resource_access_context_manager_services_perimeters_test.go
@@ -25,7 +25,7 @@ func testAccAccessContextManagerServicePerimeters_basicTest(t *testing.T) {
 		CheckDestroy:             testAccCheckAccessContextManagerServicePerimetersDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAccessContextManagerServicePerimeters_basic(org, "my policy", "level", "storage_perimeter", "bigtable_perimeter", "bigquery_omni_perimeter"),
+				Config: testAccAccessContextManagerServicePerimeters_basic(org, "my policy", "tf_test_level", "tf_test_storage_perimeter", "tf_test_bigtable_perimeter", "tf_test_bigquery_omni_perimeter"),
 			},
 			{
 				ResourceName:            "google_access_context_manager_service_perimeters.test-access",
@@ -34,7 +34,7 @@ func testAccAccessContextManagerServicePerimeters_basicTest(t *testing.T) {
 				ImportStateVerifyIgnore: []string{"service_perimeters"},
 			},
 			{
-				Config: testAccAccessContextManagerServicePerimeters_update(org, "my policy", "level", "storage_perimeter", "bigquery_perimeter", "bigtable_perimeter", "bigquery_omni_perimeter", projectNumber),
+				Config: testAccAccessContextManagerServicePerimeters_update(org, "my policy", "tf_test_level", "tf_test_storage_perimeter", "tf_test_bigquery_perimeter", "tf_test_bigtable_perimeter", "tf_test_bigquery_omni_perimeter", projectNumber),
 			},
 			{
 				ResourceName:            "google_access_context_manager_service_perimeters.test-access",
@@ -43,7 +43,7 @@ func testAccAccessContextManagerServicePerimeters_basicTest(t *testing.T) {
 				ImportStateVerifyIgnore: []string{"service_perimeters"},
 			},
 			{
-				Config: testAccAccessContextManagerServicePerimeters_empty(org, "my policy", "level"),
+				Config: testAccAccessContextManagerServicePerimeters_empty(org, "my policy", "tf_test_level"),
 			},
 			{
 				ResourceName:            "google_access_context_manager_service_perimeters.test-access",
