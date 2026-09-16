4937fe2475587104044c1050008218e1d8b1f825
Chris Hawk <hawk@google.com>
2026-09-16 17:15:28 +0000

    hypercomputecluster: make sample and test resources sweepable (#19023)

--- mmv1/third_party/terraform/services/hypercomputecluster/resource_hypercomputecluster_cluster_test.go
+++ mmv1/third_party/terraform/services/hypercomputecluster/resource_hypercomputecluster_cluster_test.go
@@ -20,7 +20,8 @@ func TestAccHypercomputeclusterCluster_update(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"random_suffix": acctest.RandString(t, 8),
+		"random_suffix":  acctest.RandString(t, 8),
+		"cluster_suffix": acctest.RandString(t, 5),
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
@@ -63,7 +64,8 @@ func TestAccHypercomputeclusterCluster_existing(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"random_suffix": acctest.RandString(t, 8),
+		"random_suffix":  acctest.RandString(t, 8),
+		"cluster_suffix": acctest.RandString(t, 5),
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
@@ -87,7 +89,8 @@ func TestAccHypercomputeclusterCluster_new(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"random_suffix": acctest.RandString(t, 8),
+		"random_suffix":  acctest.RandString(t, 8),
+		"cluster_suffix": acctest.RandString(t, 5),
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
@@ -117,7 +120,7 @@ locals {
 }
 
 resource "google_hypercomputecluster_cluster" "cluster" {
-  cluster_id                  = "tf%{random_suffix}"
+  cluster_id                  = "tfgen%{cluster_suffix}"
   location                    = "us-central1"
   description                 = "Cluster Director instance created through Terraform"
   labels = {
@@ -146,7 +149,7 @@ resource "google_hypercomputecluster_cluster" "cluster" {
     config {
       new_bucket {
         storage_class = "STANDARD"
-        bucket = "bucket-old-%{random_suffix}"
+        bucket = "tf-test-bucket-old-%{random_suffix}"
       }
     }
   }
@@ -214,7 +217,7 @@ locals {
 }
 
 resource "google_hypercomputecluster_cluster" "cluster" {
-  cluster_id                  = "tf%{random_suffix}"
+  cluster_id                  = "tfgen%{cluster_suffix}"
   location                    = "us-central1"
   description                 = "Cluster Director instance created through Terraform (updated)"
   labels = {
@@ -243,7 +246,7 @@ resource "google_hypercomputecluster_cluster" "cluster" {
     config {
       new_bucket {
         storage_class = "STANDARD"
-        bucket = "bucket-new-%{random_suffix}"
+        bucket = "tf-test-bucket-new-%{random_suffix}"
       }
     }
   }
@@ -321,7 +324,7 @@ resource "google_compute_subnetwork" "subnet" {
 }
 
 resource "google_compute_firewall" "allow_internal_subnet_communication" {
-  name    = "allow-internal-%{random_suffix}"
+  name    = "tf-test-allow-internal-%{random_suffix}"
   network = google_compute_network.vpc.id
   direction = "INGRESS"
   allow {
@@ -332,14 +335,14 @@ resource "google_compute_firewall" "allow_internal_subnet_communication" {
 }
 
 resource "google_storage_bucket" "bucket" {
-  name     = "bucket-%{random_suffix}"
+  name     = "tf-test-bucket-%{random_suffix}"
   location = "US"
   project  = "cloud-hypercomp-dev"
   uniform_bucket_level_access = true
 }
 
 resource "google_filestore_instance" "filestore_instance" {
-  name     = "filestore-%{random_suffix}"
+  name     = "tf-test-filestore-%{random_suffix}"
   location = "us-central1-b"
   tier     = "BASIC_HDD"
   file_shares {
@@ -354,7 +357,7 @@ resource "google_filestore_instance" "filestore_instance" {
 }
 
 resource "google_compute_global_address" "private_ip_alloc" {
-  name          = "lustre-ip-alloc-%{random_suffix}"
+  name          = "tf-test-lustre-ip-alloc-%{random_suffix}"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 24
@@ -368,7 +371,7 @@ resource "google_service_networking_connection" "servicenetworking_conn" {
 }
 
 resource "google_lustre_instance" "lustre_instance" {
-  instance_id                 = "lustre-%{random_suffix}"
+  instance_id                 = "tf-test-lustre-%{random_suffix}"
   location                    = "us-central1-b"
   filesystem                  = "tffs"
   capacity_gib                = 18000
@@ -381,7 +384,7 @@ resource "google_lustre_instance" "lustre_instance" {
 }
 
 resource "google_hypercomputecluster_cluster" "cluster" {
-  cluster_id                  = "tf%{random_suffix}"
+  cluster_id                  = "tfgen%{cluster_suffix}"
   location                    = "us-central1"
   description                 = "Cluster Director instance created through Terraform"
   network_resources {
@@ -477,7 +480,7 @@ locals {
 }
 
 resource "google_compute_reservation" "gce_reservation" {
-  name = "gce-reservation-%{random_suffix}"
+  name = "tf-test-gce-reservation-%{random_suffix}"
   zone = "us-central1-a"
   specific_reservation {
     count = 1
@@ -489,7 +492,7 @@ resource "google_compute_reservation" "gce_reservation" {
 }
 
 resource "google_hypercomputecluster_cluster" "cluster" {
-  cluster_id                  = "tf%{random_suffix}"
+  cluster_id                  = "tfgen%{cluster_suffix}"
   location                    = "us-central1"
   description                 = "Cluster Director instance created through Terraform"
   network_resources {
@@ -506,7 +509,7 @@ resource "google_hypercomputecluster_cluster" "cluster" {
     config {
       new_bucket {
         storage_class = "STANDARD"
-        bucket = "bucket-new-1-%{random_suffix}"
+        bucket = "tf-test-bucket-new-1-%{random_suffix}"
         hierarchical_namespace {
           enabled = false
         }
@@ -517,7 +520,7 @@ resource "google_hypercomputecluster_cluster" "cluster" {
     id = "bucket-new-2"
     config {
       new_bucket {
-        bucket = "bucket-new-2-%{random_suffix}"
+        bucket = "tf-test-bucket-new-2-%{random_suffix}"
         autoclass {
           enabled = true
           terminal_storage_class = "NEARLINE"
@@ -533,7 +536,7 @@ resource "google_hypercomputecluster_cluster" "cluster" {
     config {
       new_filestore {
         description = "Filestore instance created via Terraform"
-        filestore = "projects/${local.project_id}/locations/us-central1-a/instances/filestore-%{random_suffix}"
+        filestore = "projects/${local.project_id}/locations/us-central1-a/instances/tf-test-filestore-%{random_suffix}"
         protocol = "NFSV3"
         tier = "ZONAL"
         file_shares {
@@ -550,7 +553,7 @@ resource "google_hypercomputecluster_cluster" "cluster" {
         capacity_gb = "18000"
         description = "Lustre instance created via Terraform"
         filesystem = "lustrefs"
-        lustre = "projects/${local.project_id}/locations/us-central1-a/instances/lustre-%{random_suffix}"
+        lustre = "projects/${local.project_id}/locations/us-central1-a/instances/tf-test-lustre-%{random_suffix}"
           per_unit_storage_throughput = "1000"
       }
     }
@@ -631,7 +634,8 @@ func TestAccHypercomputeclusterCluster_inPlaceUpdates(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"random_suffix": acctest.RandString(t, 4),
+		"random_suffix":  acctest.RandString(t, 4),
+		"cluster_suffix": acctest.RandString(t, 5),
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
@@ -684,7 +688,7 @@ locals {
 }
 
 resource "google_hypercomputecluster_cluster" "cluster" {
-  cluster_id                  = "tf%{random_suffix}"
+  cluster_id                  = "tfgen%{cluster_suffix}"
   location                    = "us-central1"
   description                 = "In-place update test base"
   network_resources {
@@ -748,7 +752,7 @@ locals {
 }
 
 resource "google_hypercomputecluster_cluster" "cluster" {
-  cluster_id                  = "tf%{random_suffix}"
+  cluster_id                  = "tfgen%{cluster_suffix}"
   location                    = "us-central1"
   description                 = "In-place update test updated" # Updated description
   network_resources {
