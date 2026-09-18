87fcdc88b75e7b92408caf7827ebef0d0dd3d893
Chris Hawk <hawk@google.com>
2026-09-18 16:43:01 +0000

    Move container acceptance tests to us-east1 and update machine types

--- mmv1/third_party/terraform/services/compute/bootstrap_test_utils.go
+++ mmv1/third_party/terraform/services/compute/bootstrap_test_utils.go
@@ -170,9 +170,19 @@ func BootstrapSubnet(t *testing.T, subnetName string, networkName string) string
 	return BootstrapSubnetWithOverrides(t, subnetName, networkName, make(map[string]interface{}))
 }
 
+func BootstrapSubnetInRegion(t *testing.T, subnetName string, networkName string, region string, ipCidrRange string) string {
+	return BootstrapSubnetWithOverrides(t, subnetName, networkName, map[string]interface{}{
+		"region":      region,
+		"ipCidrRange": ipCidrRange,
+	})
+}
+
 func BootstrapSubnetWithOverrides(t *testing.T, subnetName string, networkName string, subnetOptions map[string]interface{}) string {
 	projectID := envvar.GetTestProjectFromEnv()
 	region := envvar.GetTestRegionFromEnv()
+	if r, ok := subnetOptions["region"].(string); ok && r != "" {
+		region = r
+	}
 
 	config := transport_tpg.BootstrapConfig(t)
 	if config == nil {
@@ -301,8 +311,11 @@ func BootstrapNetworkAttachment(t *testing.T, networkAttachmentName string, subn
 const SharedStoragePoolPrefix = "tf-bootstrap-storage-pool-"
 
 func BootstrapComputeStoragePool(t *testing.T, storagePoolName, storagePoolType string) string {
+	return BootstrapComputeStoragePoolInZone(t, storagePoolName, storagePoolType, envvar.GetTestZoneFromEnv())
+}
+
+func BootstrapComputeStoragePoolInZone(t *testing.T, storagePoolName, storagePoolType, zone string) string {
 	projectID := envvar.GetTestProjectFromEnv()
-	zone := envvar.GetTestZoneFromEnv()
 
 	storagePoolName = SharedStoragePoolPrefix + storagePoolType + "-" + storagePoolName
 
