349b8d27ed17222144000ca88855817cd9fb1667
Zak <zakaria.hass2019@gmail.com>
2026-09-21 16:30:27 +0100

    fix: remove acceptance test that passes without the fix

--- mmv1/third_party/terraform/services/bigquery/resource_bigquery_dataset_test.go
+++ mmv1/third_party/terraform/services/bigquery/resource_bigquery_dataset_test.go
@@ -366,28 +366,6 @@ func TestAccBigQueryDataset_accessMixedCase_groupByEmail(t *testing.T) {
 	})
 }
 
-func TestAccBigQueryDataset_accessNilVsEmptyStringPermadiff(t *testing.T) {
-	t.Parallel()
-
-	datasetID := fmt.Sprintf("tf_test_access_%s", acctest.RandString(t, 10))
-
-	acctest.VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckBigQueryDatasetDestroyProducer(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccBigQueryDatasetWithThreeAccess(datasetID),
-			},
-			{
-				Config:             testAccBigQueryDatasetWithThreeAccess(datasetID),
-				PlanOnly:           true,
-				ExpectNonEmptyPlan: false,
-			},
-		},
-	})
-}
-
 func TestAccBigQueryDataset_regionalLocation(t *testing.T) {
 	t.Parallel()
 
